4beaa8d85c8cc4716d8aad76f7c0bf28e7be093c
limin <1121044949@qq.com>
2023-05-18 14:15:58 +0800

    录屏

--- common/yak/yaklib/upload_screenrecorders.go
+++ common/yak/yaklib/upload_screenrecorders.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
