0af4e7c87766ed515208d82ea63a6829bc8199e7
go0p <gp827782797@qq.com>
2023-06-01 17:36:04 +0800

    修复 IP 不显示的问题，内网 IP 显示优化

--- scannode/node.go
+++ scannode/node.go
@@ -21,7 +21,7 @@ type WebServerConfig struct {
 	WebServerPort string `json:"web_server_port"`
 }
 
-func NewScanNodeWithAMQPUrl(id string, amqpUrl string, serverIp string) (*ScanNode, error) {
+func NewScanNodeWithAMQPUrl(id, serverPort string, amqpUrl string, serverIp string) (*ScanNode, error) {
 	base, err := node.NewNodeBase(
 		spec.NodeType_Scanner,
 		spec.CommonRPCExchange,
@@ -36,7 +36,7 @@ func NewScanNodeWithAMQPUrl(id string, amqpUrl string, serverIp string) (*ScanNo
 	agent := node
 	agent.node.HookAfterRegisteringFinished(
 		func() {
-			node.GetIpecho(serverIp, node.node.WebServerPort)
+			node.GetIpecho(serverIp, serverPort)
 		},
 	)
 	// 回传日志信息
@@ -62,8 +62,8 @@ func NewScanNodeWithAMQPUrl(id string, amqpUrl string, serverIp string) (*ScanNo
 	return node, nil
 }
 
-func NewScanNode(id string, amqpConfig *spec.AMQPConfig) (*ScanNode, error) {
-	return NewScanNodeWithAMQPUrl(id, amqpConfig.GetAMQPUrl(), amqpConfig.Host)
+func NewScanNode(id, serverPort string, amqpConfig *spec.AMQPConfig) (*ScanNode, error) {
+	return NewScanNodeWithAMQPUrl(id, serverPort, amqpConfig.GetAMQPUrl(), amqpConfig.Host)
 }
 
 func (s *ScanNode) Run() {
