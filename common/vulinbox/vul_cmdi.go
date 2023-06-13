0225aa561b72ed832c2877b18b95a9b8658e4c7a
go0p <gp827782797@qq.com>
2023-06-13 12:03:22 +0800

    windows 下的乱码

--- common/vulinbox/vul_cmdi.go
+++ common/vulinbox/vul_cmdi.go
@@ -3,7 +3,6 @@ package vulinbox
 import (
 	"context"
 	"fmt"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/google/shlex"
 	"github.com/yaklang/yaklang/common/utils"
 	"net/http"
@@ -21,24 +20,23 @@ func (s *VulinServer) registerPingCMDI() {
 		}
 		var raw = fmt.Sprintf("ping %v", ip)
 		list, err := shlex.Split(raw)
-		spew.Dump(list)
 		if err != nil {
 			writer.Write([]byte(`shlex parse failed: ` + err.Error()))
 			return
 		}
-		ctx, cancel := context.WithTimeout(context.Background(), 10000*time.Second)
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
-		outputs, err := exec.CommandContext(ctx, list[0], list[1:]...).CombinedOutput()
-		if err != nil {
-			writer.Write([]byte("exec : " + err.Error()))
-			//return
-		}
-		// 将 GBK 转换为 UTF-8
-		utf8Outputs, err := utils.GbkToUtf8(outputs)
-		if err != nil {
+		outputs, err1 := exec.CommandContext(ctx, list[0], list[1:]...).CombinedOutput()
+		// 尝试将 GBK 转换为 UTF-8
+		utf8Outputs, err2 := utils.GbkToUtf8(outputs)
+		if err2 != nil {
 			writer.Write(outputs)
 		} else {
 			writer.Write(utf8Outputs)
+		}
+		if err1 != nil {
+			writer.Write([]byte("exec : " + err1.Error()))
+			return
 		}
 	})
 	r.HandleFunc("/ping/cmd/bash", func(writer http.ResponseWriter, request *http.Request) {
@@ -50,10 +48,16 @@ func (s *VulinServer) registerPingCMDI() {
 		var raw = fmt.Sprintf("ping %v", ip)
 		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 		defer cancel()
-		outputs, err := exec.CommandContext(ctx, `bash`, "-c", raw).CombinedOutput()
-		writer.Write(outputs)
-		if err != nil {
-			writer.Write([]byte(`` + err.Error()))
+		outputs, err1 := exec.CommandContext(ctx, `bash`, "-c", raw).CombinedOutput()
+		// 尝试将 GBK 转换为 UTF-8
+		utf8Outputs, err2 := utils.GbkToUtf8(outputs)
+		if err2 != nil {
+			writer.Write(outputs)
+		} else {
+			writer.Write(utf8Outputs)
+		}
+		if err1 != nil {
+			writer.Write([]byte("exec : " + err1.Error()))
 			return
 		}
 	})
