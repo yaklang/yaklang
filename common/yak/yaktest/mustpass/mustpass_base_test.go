87c357a3757f4c879a1deb4cc216107b626a3d4a
v1ll4n <v1ll4n@qq.com>
2023-06-19 11:32:25 +0800

    improve mixcaller rules

--- common/yak/yaktest/mustpass/mustpass_base_test.go
+++ common/yak/yaktest/mustpass/mustpass_base_test.go
@@ -48,7 +48,7 @@ func init() {
 }
 
 func TestMustPassDebug(t *testing.T) {
-	var debugName = "mixcaller2.yak"
+	var debugName = "mixcaller.yak"
 	var debugCases [][]string
 	for k, v := range files {
 		if k == debugName {
