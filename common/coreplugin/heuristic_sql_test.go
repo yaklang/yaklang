96caa488b840dbeaf44ad94773a03c254b253316
Matrix-Cain <zwh_china@outlook.com>
2023-06-21 15:42:58 +0800

    add sql-injection fuzz test with json param add vulnerability info in sql plugin fix param.Value when param is json field

--- common/coreplugin/heuristic_sql_test.go
+++ common/coreplugin/heuristic_sql_test.go
@@ -31,11 +31,21 @@ func TestGRPCMUSTPASS_SQL(t *testing.T) {
 		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字型[id] 无边界闭合】": 3, "存在基于UNION SQL 注入: [参数名:id 原值:1]": 3},
 	}
 	vul3 := VulInfo{
+		Path:           "/user/id-json?id=%7B%22uid%22%3A1%2C%22id%22%3A%221%22%7D",
+		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字型[id] 无边界闭合】": 3, "存在基于UNION SQL 注入: [参数名:id 原值:1]": 3},
+	}
+	vul4 := VulInfo{
+		Path:           "/user/id-b64-json?id=eyJ1aWQiOjEsImlkIjoiMSJ9",
+		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字型[id] 无边界闭合】": 3, "存在基于UNION SQL 注入: [参数名:id 原值:1]": 3},
+	}
+	vul5 := VulInfo{
 		Path:           "/user/name?name=admin",
 		ExpectedResult: map[string]int{"疑似SQL注入：【参数：字符串[name] 单引号闭合】": 3, "存在基于UNION SQL 注入: [参数名:name 原值:admin]": 3},
 	}
 
 	Must(TestCoreMitmPlug(pluginName, server, vul1, client, t), "SQL插件对于安全的SQL注入检测结果不符合预期")
 	Must(TestCoreMitmPlug(pluginName, server, vul2, client, t), "SQL插件对于不安全的SQL注入(ID)检测结果不符合预期")
-	Must(TestCoreMitmPlug(pluginName, server, vul3, client, t), "SQL插件对于不安全的SQL注入(NAME)检测结果不符合预期")
+	Must(TestCoreMitmPlug(pluginName, server, vul3, client, t), "SQL插件对于不安全的SQL注入(JSON-ID)检测结果不符合预期")
+	Must(TestCoreMitmPlug(pluginName, server, vul4, client, t), "SQL插件对于不安全的SQL注入(BASE64-JSON-ID)检测结果不符合预期")
+	Must(TestCoreMitmPlug(pluginName, server, vul5, client, t), "SQL插件对于不安全的SQL注入(NAME)检测结果不符合预期")
 }
--- common/vulinbox/vul_sqli.go
+++ common/vulinbox/vul_sqli.go
@@ -2,6 +2,7 @@ package vulinbox
 
 import (
 	"encoding/json"
+	"github.com/yaklang/yaklang/common/yak/yaklib/codec"
 	"net/http"
 	"strconv"
 )
@@ -34,6 +35,76 @@ func (s *VulinServer) registerSQLinj() {
 	})
 	router.HandleFunc("/user/id", func(writer http.ResponseWriter, request *http.Request) {
 		var a = request.URL.Query().Get("id")
+		u, err := s.database.GetUserByIdUnsafe(a)
+		if err != nil {
+			writer.Write([]byte(err.Error()))
+			writer.WriteHeader(500)
+			return
+		}
+		d, err := json.Marshal(u)
+		if err != nil {
+			writer.Write([]byte(err.Error()))
+			writer.WriteHeader(500)
+			return
+		}
+		writer.Write(d)
+		writer.WriteHeader(200)
+		return
+	})
+	router.HandleFunc("/user/id-json", func(writer http.ResponseWriter, request *http.Request) {
+		var a = request.URL.Query().Get("id")
+		var jsonMap map[string]any
+		err := json.Unmarshal([]byte(a), &jsonMap)
+		if err != nil {
+			writer.Write([]byte(err.Error()))
+			writer.WriteHeader(500)
+			return
+		}
+		a, ok := jsonMap["id"].(string)
+		if !ok {
+			writer.Write([]byte("Failed to retrieve the 'id' field"))
+			writer.WriteHeader(500)
+			return
+		}
+
+		u, err := s.database.GetUserByIdUnsafe(a)
+		if err != nil {
+			writer.Write([]byte(err.Error()))
+			writer.WriteHeader(500)
+			return
+		}
+		d, err := json.Marshal(u)
+		if err != nil {
+			writer.Write([]byte(err.Error()))
+			writer.WriteHeader(500)
+			return
+		}
+		writer.Write(d)
+		writer.WriteHeader(200)
+		return
+	})
+	router.HandleFunc("/user/id-b64-json", func(writer http.ResponseWriter, request *http.Request) {
+		var a = request.URL.Query().Get("id")
+		decodedB64, err := codec.DecodeBase64(a)
+		if err != nil {
+			writer.Write([]byte(err.Error()))
+			writer.WriteHeader(500)
+			return
+		}
+		var jsonMap map[string]any
+		err = json.Unmarshal(decodedB64, &jsonMap)
+		if err != nil {
+			writer.Write([]byte(err.Error()))
+			writer.WriteHeader(500)
+			return
+		}
+		a, ok := jsonMap["id"].(string)
+		if !ok {
+			writer.Write([]byte("Failed to retrieve the 'id' field"))
+			writer.WriteHeader(500)
+			return
+		}
+
 		u, err := s.database.GetUserByIdUnsafe(a)
 		if err != nil {
 			writer.Write([]byte(err.Error()))
