00e55303b3dca0cad355ea6f5fa967fcf07a0cea
z3 <z3r0ne@163.com>
2023-07-04 14:05:54 +0800

    .

--- common/yakgrpc/grpc_fuzzer_yaml.go
+++ common/yakgrpc/grpc_fuzzer_yaml.go
@@ -7,6 +7,7 @@ import (
 	"github.com/yaklang/yaklang/common/yak/httptpl"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 	"os"
+	"time"
 )
 
 func (s *Server) ImportHTTPFuzzerTaskFromYaml(ctx context.Context, req *ypb.ImportHTTPFuzzerTaskFromYamlRequest) (*ypb.ImportHTTPFuzzerTaskFromYamlResponse, error) {
@@ -91,7 +92,67 @@ func (s *Server) ExportHTTPFuzzerTaskToYaml(ctx context.Context, req *ypb.Export
 		Ok:     true,
 		Reason: "",
 	}
-	//fuzzerRequest := req.GetFuzzerRequests()
-
+	fuzzerRequests := req.GetFuzzerRequests()
+	template := &httptpl.YakTemplate{}
+	template.HTTPRequestSequences = make([]*httptpl.YakRequestBulkConfig, len(fuzzerRequests))
+	//requestBulkConfig := template.HTTPRequestSequences[0]
+	//requestBulkConfig.HTTPRequests = funk.Map(fuzzerRequests, func(fuzzerRequest *ypb.FuzzerRequest) *httptpl.YakHTTPRequestPacket {
+	//	return &httptpl.YakHTTPRequestPacket{
+	//		Request: fuzzerRequest.Request,
+	//		Timeout: time.Duration(fuzzerRequest.PerRequestTimeoutSeconds) * time.Second,
+	//	}
+	//}).([]*httptpl.YakHTTPRequestPacket)
+	var HttpResponseMatchers2YakMatchers func(matchers []*ypb.HTTPResponseMatcher) []*httptpl.YakMatcher
+	HttpResponseMatchers2YakMatchers = func(matchers []*ypb.HTTPResponseMatcher) []*httptpl.YakMatcher {
+		return funk.Map(matchers, func(matcher *ypb.HTTPResponseMatcher) *httptpl.YakMatcher {
+			return &httptpl.YakMatcher{
+				SubMatchers:         HttpResponseMatchers2YakMatchers(matcher.SubMatchers),
+				SubMatcherCondition: matcher.SubMatcherCondition,
+				MatcherType:         matcher.MatcherType,
+				Scope:               matcher.Scope,
+				Condition:           matcher.Condition,
+				Group:               matcher.Group,
+				GroupEncoding:       matcher.GroupEncoding,
+				Negative:            matcher.Negative,
+				ExprType:            matcher.ExprType,
+			}
+		}).([]*httptpl.YakMatcher)
+	}
+	for i, request := range fuzzerRequests {
+		template.HTTPRequestSequences[i].HTTPRequests = make([]*httptpl.YakHTTPRequestPacket, 1)
+		template.HTTPRequestSequences[i].HTTPRequests[0] = &httptpl.YakHTTPRequestPacket{
+			Request: request.Request,
+			Timeout: time.Duration(request.PerRequestTimeoutSeconds) * time.Second,
+		}
+		template.HTTPRequestSequences[i].Matcher = &httptpl.YakMatcher{
+			SubMatchers:         HttpResponseMatchers2YakMatchers(request.Matchers),
+			SubMatcherCondition: request.MatchersCondition,
+		}
+		template.HTTPRequestSequences[i].Extractor = funk.Map(request.Extractors, func(extractor *ypb.HTTPResponseExtractor) *httptpl.YakExtractor {
+			return &httptpl.YakExtractor{
+				Name:             extractor.Name,
+				Type:             extractor.Type,
+				Scope:            extractor.Scope,
+				Groups:           extractor.Groups,
+				RegexpMatchGroup: funk.Map(extractor.RegexpMatchGroup, func(n int64) int { return int(n) }).([]int),
+				XPathAttribute:   extractor.XPathAttribute,
+			}
+		}).([]*httptpl.YakExtractor)
+		template.HTTPRequestSequences[i].EnableRedirect = request.RedirectTimes > 0
+		template.HTTPRequestSequences[i].MaxRedirects = int(request.RedirectTimes)
+		template.HTTPRequestSequences[i].NoFixContentLength = request.NoFixContentLength
+	}
+	byts, err := template.MarshalToYaml()
+	if err != nil {
+		res.Ok = false
+		res.Reason = err.Error()
+		return res, nil
+	}
+	err = os.WriteFile(req.YamlPath, byts, os.ModePerm)
+	if err != nil {
+		res.Ok = false
+		res.Reason = err.Error()
+		return res, nil
+	}
 	return res, nil
 }
