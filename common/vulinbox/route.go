5c6b6124447ce1b657e16b226fe52431c0b89b8e
v1ll4n <v1ll4n@qq.com>
2023-07-04 11:57:19 +0800

    优化靶场前端

--- common/vulinbox/route.go
+++ common/vulinbox/route.go
@@ -9,16 +9,20 @@ import (
 //go:embed route.html
 var routeHtml []byte
 
-//go:embed route_safe.html
-var routeSafeHtml []byte
-
 func (s *VulinServer) init() {
 	router := s.router
 
 	router.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		writer.Header().Set("Content-Type", "text/html; charset=UTF8")
 		if s.safeMode {
-			writer.Write(routeSafeHtml)
+			bytes, err := unsafeTemplate(string(routeHtml), map[string]any{
+				"safescript": `<script>const c = document.getElementById("safestyle"); if (c) c.style.display='none';</script>`,
+			})
+			if err != nil {
+				writer.Write(routeHtml)
+			} else {
+				writer.Write(bytes)
+			}
 		} else {
 			writer.Write(routeHtml)
 		}
