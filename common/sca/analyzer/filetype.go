73bb9998e76532beb0b3d2e084c8e2c20f2c9462
WAY29 <1162410187@qq.com>
2023-07-06 16:35:05 +0800

    refactor(matchinfo) feat(match header)  feat(gobinary analyzer)
    refactor(matchinfo): refactor analzyer.Match function sign
    feat(match header): allow get file header to analzyer.Match
    feat(gobinary analyzer):

---
+++ common/sca/analyzer/filetype.go
@@ -0,0 +1,18 @@
+package analyzer
+
+import "github.com/h2non/filetype"
+
+var fastELFType = filetype.NewType("fast-elf", "application/x-executable")
+
+func fastELFTypeMatcher(buf []byte) bool {
+	return buf[0] == 0x7F && buf[1] == 0x45 &&
+		buf[2] == 0x4C && buf[3] == 0x46
+}
+
+func init() {
+	filetype.AddMatcher(fastELFType, fastELFTypeMatcher)
+}
+
+func IsExecutable(buf []byte) bool {
+	return filetype.Is(buf, "exe") || filetype.Is(buf, "fast-elf")
+}
