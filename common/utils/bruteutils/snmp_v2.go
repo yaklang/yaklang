ad4068286f16725f9cd8422add83dc259b335a9d
rookie <2797055720@qq.com>
2023-07-07 11:32:11 +0800

    snmp爆破

---
+++ common/utils/bruteutils/snmp_v2.go
@@ -0,0 +1,56 @@
+package bruteutils
+
+import (
+	"github.com/gosnmp/gosnmp"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils"
+	"time"
+)
+
+var snmp_v2Auth = &DefaultServiceAuthInfo{
+	ServiceName:      "snmp_v2",
+	DefaultPorts:     "161",
+	DefaultUsernames: append([]string{"snmp"}, CommonUsernames...),
+	DefaultPasswords: CommonPasswords,
+	UnAuthVerify:     nil,
+	BrutePass: func(item *BruteItem) *BruteItemResult {
+		target := fixToTarget(item.Target, 161)
+		result := item.Result()
+		result.OnlyNeedPassword = true
+
+		host, port, _ := utils.ParseStringToHostPort(target)
+		if port <= 0 {
+			result.Finished = true
+			return result
+		}
+
+		snmpConfig := &gosnmp.GoSNMP{
+			Target:             host,
+			Port:               uint16(port),
+			Transport:          "udp",
+			Community:          item.Password,
+			Version:            gosnmp.Version2c,
+			Timeout:            time.Duration(2) * time.Second,
+			Retries:            3,
+			ExponentialTimeout: true,
+			MaxOids:            60,
+		}
+
+		// 尝试连接连接失败不再爆破
+		err := snmpConfig.Connect()
+		if err != nil {
+			result.Finished = true
+			return result
+		}
+
+		oid := []string{"1.3.6.1.2.1.1.1.0"}
+		_, err = snmpConfig.Get(oid)
+		if err != nil {
+			log.Errorf("brute failed: %s", err)
+			return result
+		}
+		result.Ok = true
+		result.Username = ""
+		return result
+	},
+}
