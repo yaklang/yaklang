b22d55364a1dff728f545e45436e52be3a0e1f0d
go0p <gp827782797@qq.com>
2023-07-10 22:46:19 +0800

    test

--- common/yak/yaklib/tools/fingerprint_scan_test.go
+++ common/yak/yaklib/tools/fingerprint_scan_test.go
@@ -1,25 +1,25 @@
 package tools
 
 import (
-	"github.com/davecgh/go-spew/spew"
+	"fmt"
 	"github.com/yaklang/yaklang/common/fp"
 	"testing"
 )
 
 func Test_scanFingerprint(t *testing.T) {
 	//target := "150.129.109.26"
-	//target := "47.98.176.118"
+	target := "47.98.176.118"
 	//target := "118.171.54.61"
 	//target := "192.168.3.113"
-	target := "117.212.17.42"
-	target = "120.96.38.58"
+	//target := "117.212.17.42"
+	//target = "120.96.38.58"
 
 	//port := "3307"
-	port := "21"
-	//port := "80,22,443,8080,3306,161"
+	//port := "21"
+	port := "80,22,443,8080,3306,161"
 
-	//protoList := []interface{}{"tcp", "udp"}
-	protoList := []interface{}{"tcp"}
+	protoList := []interface{}{"tcp", "udp"}
+	//protoList := []interface{}{"tcp"}
 
 	pp := func(proto ...interface{}) fp.ConfigOption {
 		return fp.WithTransportProtos(fp.ParseStringToProto(proto...)...)
@@ -33,6 +33,6 @@ func Test_scanFingerprint(t *testing.T) {
 	}
 
 	for v := range ch {
-		spew.Dump(v)
+		fmt.Println(v.String())
 	}
 }
