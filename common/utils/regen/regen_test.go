58b0081e6ea532e4f31fa6bbd87e404d6c1d02b1
go0p <gp827782797@qq.com>
2023-07-11 23:16:37 +0800

    add : mock udp server 根据 Probe.Payload，给出 regen MatchRule

--- common/utils/regen/regen_test.go
+++ common/utils/regen/regen_test.go
@@ -16,7 +16,6 @@ func Test_ExampleGenerate(t *testing.T) {
 func Test_GenerateOne(t *testing.T) {
 	//pattern := `session=[a-zA-Z0-9+/]{20,300}([a-zA-Z0-9+/]{1}[a-zA-Z0-9+/=]{1}|==)`
 	pattern := `^0.*\x02\x01\x00\x04\x06public\xa2.*\x06\x08\+\x06\x01\x02\x01\x01\x05\x00\x04[^\x00]([^\x00]+)`
-	pattern = `08\x02\x01\x00\x04\x06public\xef\xbf\xbd+\x02\x04L3\xef\xbf\xbd\x02\x01\x00\x02\x01\x000\x1d0\x1b\x06\x08+\x06\x01\x02\x01\x01\x05\x00\x04\x0fH8 Nas-4_Static`
 	results, _ := GenerateOne(pattern)
 	spew.Dump(results)
 }
