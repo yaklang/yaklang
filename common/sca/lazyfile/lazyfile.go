02cc471fdad0d9fb33d36ddda8f6d1ac933e1c5d
WAY29 <1162410187@qq.com>
2023-07-12 10:30:28 +0800

    refactor(LazyFile) impl ReadAt,Stat,Name, refactor LazyOpenStreamByFilePath,LazyOpenStreamByFile

--- common/sca/lazyfile/lazyfile.go
+++ common/sca/lazyfile/lazyfile.go
@@ -1,24 +1,30 @@
 package lazyfile
 
 import (
-	"github.com/yaklang/yaklang/common/log"
 	"io"
+	"io/fs"
 	"os"
 	"sync"
+
+	"github.com/yaklang/yaklang/common/log"
 )
 
-type lazyFile struct {
-	fileName  string
-	finalOpen func() (file *os.File, err error)
-	finalErr  error
+type LazyFile struct {
 	*os.File
+
+	fileName string
 	openOnce *sync.Once
+	finalErr error
+
+	finalOpen func() (file *os.File, err error)
 }
 
 func LazyOpenReadCloserFile(name string, flag int, perm os.FileMode) io.ReadCloser {
-	lf := &lazyFile{
+	lf := &LazyFile{
 		openOnce: new(sync.Once),
 	}
+	lf.fileName = name
+
 	lf.finalOpen = func() (file *os.File, err error) {
 		if lf.finalErr != nil {
 			return nil, lf.finalErr
@@ -35,16 +41,18 @@ func LazyOpenReadCloserFile(name string, flag int, perm os.FileMode) io.ReadClos
 	return lf
 }
 
-func LazyOpenStreamByFile(name string, flag int, perm os.FileMode) io.ReadWriteCloser {
-	lf := &lazyFile{
+func LazyOpenStreamByFilePath(name string) *LazyFile {
+	lf := &LazyFile{
 		openOnce: new(sync.Once),
 	}
+	lf.fileName = name
+
 	lf.finalOpen = func() (file *os.File, err error) {
 		if lf.finalErr != nil {
 			return nil, lf.finalErr
 		}
 
-		f, err := os.OpenFile(name, flag, perm)
+		f, err := os.OpenFile(name, os.O_RDWR, os.ModePerm)
 		if err != nil {
 			lf.finalErr = err
 			return nil, err
@@ -55,7 +63,11 @@ func LazyOpenStreamByFile(name string, flag int, perm os.FileMode) io.ReadWriteC
 	return lf
 }
 
-func (f *lazyFile) lazyOpen() error {
+func LazyOpenStreamByFile(rf *os.File) *LazyFile {
+	return LazyOpenStreamByFilePath(rf.Name())
+}
+
+func (f *LazyFile) lazyOpen() error {
 	f.openOnce.Do(func() {
 		_, err := f.finalOpen()
 		if err != nil {
@@ -65,7 +77,15 @@ func (f *lazyFile) lazyOpen() error {
 	return f.finalErr
 }
 
-func (f *lazyFile) Read(b []byte) (int, error) {
+func (f *LazyFile) ReadAt(b []byte, off int64) (n int, err error) {
+	err = f.lazyOpen()
+	if err != nil {
+		return 0, err
+	}
+	return f.File.ReadAt(b, off)
+}
+
+func (f *LazyFile) Read(b []byte) (int, error) {
 	err := f.lazyOpen()
 	if err != nil {
 		return 0, err
@@ -73,7 +93,15 @@ func (f *lazyFile) Read(b []byte) (int, error) {
 	return f.File.Read(b)
 }
 
-func (f *lazyFile) Write(b []byte) (int, error) {
+func (f *LazyFile) Stat() (fs.FileInfo, error) {
+	err := f.lazyOpen()
+	if err != nil {
+		return nil, err
+	}
+	return f.File.Stat()
+}
+
+func (f *LazyFile) Write(b []byte) (int, error) {
 	err := f.lazyOpen()
 	if err != nil {
 		return 0, err
@@ -81,13 +109,12 @@ func (f *lazyFile) Write(b []byte) (int, error) {
 	return f.File.Write(b)
 }
 
-func (f *lazyFile) Close() error {
+func (f *LazyFile) Close() error {
 	if f.File == nil {
-		log.Warnf("")
 		return nil
 	}
 
-	err := f.lazyOpen()
+	err := f.finalErr
 	if err != nil {
 		return err
 	}
@@ -95,4 +122,8 @@ func (f *lazyFile) Close() error {
 		return f.File.Close()
 	}
 	return nil
+}
+
+func (f *LazyFile) Name() string {
+	return f.fileName
 }
