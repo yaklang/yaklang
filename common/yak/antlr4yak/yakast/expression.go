5a784a8c098330f454caa3addb7b8c8748aa6da2
z3 <z3r0ne@163.com>
2023-07-12 14:50:01 +0800

    .

--- common/yak/antlr4yak/yakast/expression.go
+++ common/yak/antlr4yak/yakast/expression.go
@@ -97,9 +97,6 @@ func (y *YakCompiler) VisitExpression(raw yak.IExpressionContext) interface{} {
 			y.pushIdentifierName(s.GetText())
 			if y.strict {
 				id := s.GetText()
-				if id == "checkTimeBasedBlind" {
-					print()
-				}
 				if _, ok := y.extVarsMap[id]; !ok {
 					y.currentStartPosition.ColumnNumber += 1
 					y.currentEndPosition.ColumnNumber += 2
