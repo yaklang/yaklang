bcde93c83dcdccc4fabe06aaea7703d041fbbfc0
go0p <gp827782797@qq.com>
2023-07-12 19:12:58 +0800

    add: 端口扫描添加 U: 前缀的方式

--- common/yak/yaklib/tools/fingerprint_scan_test.go
+++ common/yak/yaklib/tools/fingerprint_scan_test.go
@@ -12,20 +12,28 @@ func Test_scanFingerprint(t *testing.T) {
 	//target := "118.171.54.61"
 	//target := "192.168.3.113"
 	//target := "117.212.17.42"
-	//target = "120.96.38.58"
+	target = "37.131.221.151"
+	//target = "213.100.240.79"
 
 	//port := "3307"
 	//port := "21"
-	port := "80,22,443,8080,3306,161"
+	//port := "80,22,443,8080,3306,161"
+	//port := "80,161,U:162,554"
+	//port := "554"
+	port := "U:162"
 
-	protoList := []interface{}{"tcp", "udp"}
-	//protoList := []interface{}{"tcp"}
+	//protoList := []interface{}{"tcp", "udp"}
+	//protoList := []interface{}{"udp"}
+	protoList := []interface{}{"tcp"}
 
 	pp := func(proto ...interface{}) fp.ConfigOption {
 		return fp.WithTransportProtos(fp.ParseStringToProto(proto...)...)
 	}
 
-	ch, err := scanFingerprint(target, port, pp(protoList...), fp.WithProbeTimeoutHumanRead(5))
+	ch, err := scanFingerprint(target, port, pp(protoList...),
+		fp.WithProbeTimeoutHumanRead(5),
+		fp.WithProbesMax(5),
+	)
 	//ch, err := scanFingerprint(target, "162", pp(protoList...), fp.WithProbeTimeoutHumanRead(5))
 
 	if err != nil {
