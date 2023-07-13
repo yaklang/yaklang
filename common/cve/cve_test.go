46b13c465312c3af04a1910c816e7fa728a21fd6
rookie <2797055720@qq.com>
2023-07-13 11:35:13 +0800

    fix:将用于测试的数据库嵌入到embed

--- common/cve/cve_test.go
+++ common/cve/cve_test.go
@@ -1,10 +1,17 @@
 package cve
 
 import (
+	"embed"
 	"github.com/yaklang/yaklang/common/cve/cvequeryops"
 	"github.com/yaklang/yaklang/common/cve/cveresources"
+	"github.com/yaklang/yaklang/common/log"
+	"io"
+	"os"
 	"testing"
 )
+
+//go:embed CIDate.db
+var DbFs embed.FS
 
 func TestMigrate(t *testing.T) {
 	_migrateTable()
@@ -33,9 +40,33 @@ func TestQueryCVEFixName(t *testing.T) {
 			version: "2.2",
 			target:  "CVE-2006-1542",
 		},
+		{
+			name:    "linux-2019",
+			version: "9.0",
+			target:  "CVE-2003-0780",
+		},
 	}
-	M := cveresources.GetManager("./CIDate.db")
 
+	//读 embed 文件
+	DbFp, err := DbFs.Open("CIDate.db")
+	if err != nil {
+		log.Errorf("%v", err)
+	}
+	defer DbFp.Close()
+
+	//写到临时目录
+	tempFp, err := os.CreateTemp("", "Date.db")
+	if err != nil {
+		log.Errorf("%v", err)
+	}
+	defer tempFp.Close()
+
+	_, err = io.Copy(tempFp, DbFp)
+	if err != nil {
+		log.Errorf("%v", err)
+	}
+
+	M := cveresources.GetManager(tempFp.Name())
 	for _, datum := range data {
 		cve := cvequeryops.QueryCVEYields(M.DB, cvequeryops.ProductWithVersion(datum.name, datum.version))
 		count := 0
