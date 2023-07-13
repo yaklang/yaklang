a108c4c5e1d5ab27aeb0971decef405a16a5484c
WAY29 <1162410187@qq.com>
2023-07-13 17:36:37 +0800

    feat(ScanContainerFromContext)

--- common/sca/exports.go
+++ common/sca/exports.go
@@ -3,11 +3,12 @@ package sca
 import "github.com/yaklang/yaklang/common/sca/analyzer"
 
 var Exports = map[string]interface{}{
-	"ScanImageFromContext": ScanDockerImageFromContext,
-	"ScanImageFromFile":    ScanDockerImageFromFile,
-	"endpoint":             _withEndPoint,
-	"scanMode":             _withScanMode,
-	"concurrent":           _withConcurrent,
+	"ScanImageFromContext":     ScanDockerImageFromContext,
+	"ScanContainerFromContext": ScanDockerContainerFromContext,
+	"ScanImageFromFile":        ScanDockerImageFromFile,
+	"endpoint":                 _withEndPoint,
+	"scanMode":                 _withScanMode,
+	"concurrent":               _withConcurrent,
 
 	"ALL_MODE":      analyzer.AllMode,
 	"PKG_MODE":      analyzer.PkgMode,
