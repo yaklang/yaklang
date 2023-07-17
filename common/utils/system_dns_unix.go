57a30313994d2374e41b4c017f6c6b14ccfba6bf
z3 <z3r0ne@163.com>
2023-07-17 17:52:18 +0800

    添加获取系统dns的grpc && mitm和webfuzzer在未配置dns时默认使用系统dns

---
+++ common/utils/system_dns_unix.go
@@ -0,0 +1,42 @@
+package utils
+
+import (
+	"bufio"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func GetSystemDnsServers() ([]string, error) {
+	var servers []string
+	file, err := os.Open("/etc/resolv.conf")
+	if err != nil {
+		return nil, Errorf("open /etc/resolv.conf failed: %v", err)
+	}
+	defer file.Close()
+	readLine := bufio.NewReader(file)
+	for {
+		line, _, err := readLine.ReadLine()
+		if err != nil {
+			break
+		}
+		if len(line) > 0 && (line[0] == ';' || line[0] == '#') {
+			// comment.
+			continue
+		}
+		regex := regexp.MustCompile(" |\r|\t|\n")
+		f := regex.Split(strings.TrimSpace(string(line)), -1)
+		if len(f) < 1 {
+			continue
+		}
+		switch f[0] {
+		case "nameserver": // add one name server
+			if len(f) > 1 { // small, but the standard limit
+				servers = append(servers, f[1])
+			}
+		default:
+			continue
+		}
+	}
+	return servers, nil
+}
