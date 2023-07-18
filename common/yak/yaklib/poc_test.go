ee3d43331b4f95f788f805523dcea402a149fb6e
WAY29 <1162410187@qq.com>
2023-07-18 18:00:44 +0800

    test(poc_test): add poc.Do test

--- common/yak/yaklib/poc_test.go
+++ common/yak/yaklib/poc_test.go
@@ -1,9 +1,11 @@
 package yaklib
 
 import (
+	"net/http/httputil"
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/yaklang/yaklang/common/utils/lowhttp"
 )
 
 func TestPoC(t *testing.T) {
@@ -12,4 +14,23 @@ Host: dppt98.guangdong.chinatax.gov.cn:8443
 
 `)
 	spew.Dump(rsp, req, err)
+}
+
+func TestPoCmethod(t *testing.T) {
+	// remove useragent
+	_, req, err := do("GET", "https://baidu.com/", _pocOptDeleteHeader("User-Agent"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	actual, err := httputil.DumpRequest(req, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	wanted := lowhttp.FixHTTPPacketCRLF([]byte(`GET / HTTP/1.1
+Host: baidu.com
+`), false)
+
+	if string(actual) != string(wanted) {
+		t.Fatalf("actual: %s, wanted: %s", actual, wanted)
+	}
 }
