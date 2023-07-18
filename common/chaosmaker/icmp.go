6288a4b0f4e4b6101d271b3501d82a03db9f574f
BaiMeow <baishu2003@outlook.com>
2023-07-18 17:58:28 +0800

    refactor(chaos): code struct change

--- common/chaosmaker/icmp.go
+++ common/chaosmaker/icmp.go
@@ -12,133 +12,142 @@ import (
 	"strings"
 )
 
-var icmpHandler = &chaosHandler{
-	Generator: func(maker *ChaosMaker, makerRule *ChaosMakerRule, rule *suricata.Rule) chan *ChaosTraffic {
-		if rule.Protocol != "icmp" {
+func init() {
+	chaosMap.Store("suricata-icmp", &icmpHandler{})
+}
+
+type icmpHandler struct {
+}
+
+func (h *icmpHandler) Generator(maker *ChaosMaker, makerRule *ChaosMakerRule, rule *suricata.Rule) chan *ChaosTraffic {
+	if rule.Protocol != "icmp" {
+		return nil
+	}
+
+	if rule.ContentRuleConfig == nil {
+		return nil
+	}
+
+	var toServer bool
+	var toClient bool
+
+	if rule.ContentRuleConfig.Flow != nil {
+		toServer = rule.ContentRuleConfig.Flow.ToServer
+		toClient = rule.ContentRuleConfig.Flow.ToClient
+	} else {
+		toServer = true
+		toClient = true
+	}
+
+	var baseICMPLayer = &layers.ICMPv4{}
+
+	if IcmpConfig := rule.ContentRuleConfig.IcmpConfig; IcmpConfig != nil {
+		var code, typeCode uint8
+		if IcmpConfig.ICode != "" {
+			code = uint8(parseCondition(IcmpConfig.ICode))
+		}
+		if IcmpConfig.IType != "" {
+			typeCode = uint8(parseCondition(IcmpConfig.IType))
+		}
+		baseICMPLayer.TypeCode = layers.CreateICMPv4TypeCode(code, typeCode)
+		baseICMPLayer.Id = uint16(IcmpConfig.ICMPId)
+		baseICMPLayer.Seq = uint16(IcmpConfig.ICMPSeq)
+	}
+
+	// 定义IPv4报文头部
+	baseIPLayer := &layers.IPv4{
+		Version:  4,                       // 版本号
+		TTL:      64,                      // 生存时间
+		Protocol: layers.IPProtocolICMPv4, // 协议类型
+	}
+
+	toBytes := func(ipLayer *layers.IPv4, icmpLayer *layers.ICMPv4, payloads ...gopacket.Payload) []byte {
+		var actPayloads []byte
+		if len(payloads) > 0 {
+			for _, p := range payloads {
+				actPayloads = append(actPayloads, p...)
+			}
+		}
+
+		buffer := gopacket.NewSerializeBuffer()
+		err := gopacket.SerializeLayers(buffer, gopacket.SerializeOptions{
+			FixLengths:       true,
+			ComputeChecksums: true,
+		}, ipLayer, icmpLayer, gopacket.Payload(actPayloads))
+		if err != nil {
+			log.Error(err)
 			return nil
 		}
+		return buffer.Bytes()
+	}
 
-		if rule.ContentRuleConfig == nil {
-			return nil
+	ch := make(chan *ChaosTraffic)
+	feedback := func(raw []byte) {
+		if raw == nil {
+			return
+		}
+		if toClient {
+			ch <- ICMPIPInboundBytesToChaosTraffic(makerRule, rule, raw)
 		}
 
-		var toServer bool
-		var toClient bool
-
-		if rule.ContentRuleConfig.Flow != nil {
-			toServer = rule.ContentRuleConfig.Flow.ToServer
-			toClient = rule.ContentRuleConfig.Flow.ToClient
-		} else {
-			toServer = true
-			toClient = true
+		if toServer {
+			ch <- ICMPIPOutboundBytesToChaosTraffic(makerRule, rule, raw)
 		}
 
-		var baseICMPLayer = &layers.ICMPv4{}
+	}
+	go func() {
+		defer close(ch)
 
-		if IcmpConfig := rule.ContentRuleConfig.IcmpConfig; IcmpConfig != nil {
-			var code, typeCode uint8
-			if IcmpConfig.ICode != "" {
-				code = uint8(parseCondition(IcmpConfig.ICode))
+		var payloads string
+		var extraRules []*suricata.ContentRule
+		for _, r := range rule.ContentRuleConfig.ContentRules {
+			if r.Negative {
+				continue
 			}
-			if IcmpConfig.IType != "" {
-				typeCode = uint8(parseCondition(IcmpConfig.IType))
+			payloads += string(r.Content)
+			if r.PCRE == "" {
+				continue
 			}
-			baseICMPLayer.TypeCode = layers.CreateICMPv4TypeCode(code, typeCode)
-			baseICMPLayer.Id = uint16(IcmpConfig.ICMPId)
-			baseICMPLayer.Seq = uint16(IcmpConfig.ICMPSeq)
+			extraRules = append(extraRules, r.PCREStringGenerator(1)...)
 		}
 
-		// 定义IPv4报文头部
-		baseIPLayer := &layers.IPv4{
-			Version:  4,                       // 版本号
-			TTL:      64,                      // 生存时间
-			Protocol: layers.IPProtocolICMPv4, // 协议类型
+		for _, r := range extraRules {
+			payloads += string(r.Content)
 		}
 
-		toBytes := func(ipLayer *layers.IPv4, icmpLayer *layers.ICMPv4, payloads ...gopacket.Payload) []byte {
-			var actPayloads []byte
-			if len(payloads) > 0 {
-				for _, p := range payloads {
-					actPayloads = append(actPayloads, p...)
-				}
-			}
-
-			buffer := gopacket.NewSerializeBuffer()
-			err := gopacket.SerializeLayers(buffer, gopacket.SerializeOptions{
-				FixLengths:       true,
-				ComputeChecksums: true,
-			}, ipLayer, icmpLayer, gopacket.Payload(actPayloads))
-			if err != nil {
-				log.Error(err)
-				return nil
-			}
-			return buffer.Bytes()
-		}
-
-		ch := make(chan *ChaosTraffic)
-		feedback := func(raw []byte) {
-			if raw == nil {
+		if toServer {
+			// IP 层
+			baseIPLayer.SrcIP = net.ParseIP(maker.LocalIPAddress)
+			if baseIPLayer.SrcIP == nil {
+				log.Error("fetch local ip address failed")
 				return
 			}
-			if toClient {
-				ch <- ICMPIPInboundBytesToChaosTraffic(makerRule, rule, raw)
-			}
+			baseIPLayer.DstIP = net.ParseIP(utils.GetRandomIPAddress())
 
-			if toServer {
-				ch <- ICMPIPOutboundBytesToChaosTraffic(makerRule, rule, raw)
+			for i := 0; i < rule.ContentRuleConfig.Thresholding.Repeat(); i++ {
+				feedback(toBytes(baseIPLayer, baseICMPLayer, gopacket.Payload(payloads)))
 			}
-
 		}
-		go func() {
-			defer close(ch)
 
-			var payloads string
-			var extraRules []*suricata.ContentRule
-			for _, r := range rule.ContentRuleConfig.ContentRules {
-				if r.Negative {
-					continue
-				}
-				payloads += string(r.Content)
-				if r.PCRE == "" {
-					continue
-				}
-				extraRules = append(extraRules, r.PCREStringGenerator(1)...)
+		if toClient {
+			baseIPLayer.DstIP = net.ParseIP(maker.LocalIPAddress)
+			if baseIPLayer.DstIP == nil {
+				log.Error("fetch local ip address failed")
+				return
 			}
+			baseIPLayer.SrcIP = net.ParseIP(utils.GetRandomIPAddress())
 
-			for _, r := range extraRules {
-				payloads += string(r.Content)
+			for i := 0; i < rule.ContentRuleConfig.Thresholding.Repeat(); i++ {
+				feedback(toBytes(baseIPLayer, baseICMPLayer, gopacket.Payload(payloads)))
 			}
+		}
+	}()
+	return ch
+}
 
-			if toServer {
-				// IP 层
-				baseIPLayer.SrcIP = net.ParseIP(maker.LocalIPAddress)
-				if baseIPLayer.SrcIP == nil {
-					log.Error("fetch local ip address failed")
-					return
-				}
-				baseIPLayer.DstIP = net.ParseIP(utils.GetRandomIPAddress())
-
-				for i := 0; i < rule.ContentRuleConfig.Thresholding.Repeat(); i++ {
-					feedback(toBytes(baseIPLayer, baseICMPLayer, gopacket.Payload(payloads)))
-				}
-			}
-
-			if toClient {
-				baseIPLayer.DstIP = net.ParseIP(maker.LocalIPAddress)
-				if baseIPLayer.DstIP == nil {
-					log.Error("fetch local ip address failed")
-					return
-				}
-				baseIPLayer.SrcIP = net.ParseIP(utils.GetRandomIPAddress())
-
-				for i := 0; i < rule.ContentRuleConfig.Thresholding.Repeat(); i++ {
-					feedback(toBytes(baseIPLayer, baseICMPLayer, gopacket.Payload(payloads)))
-				}
-			}
-		}()
-		return ch
-	},
-	MatchBytes: nil,
+func (h *icmpHandler) MatchBytes(i interface{}) bool {
+	//TODO implement me
+	panic("implement me")
 }
 
 func parseCondition(condition string) int {
@@ -180,7 +189,7 @@ func parseCondition(condition string) int {
 }
 
 func init() {
-	chaosMap.Store("suricata-icmp", icmpHandler)
+	chaosMap.Store("suricata-icmp", &icmpHandler{})
 }
 
 func ICMPIPInboundBytesToChaosTraffic(makerRule *ChaosMakerRule, r *suricata.Rule, raw []byte) *ChaosTraffic {
