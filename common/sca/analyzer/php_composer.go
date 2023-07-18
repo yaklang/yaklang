d401df1d4298ab956f2af31a36ad03ada93ab604
wlingze <lingze.wl@gmail.com>
2023-07-18 10:56:48 +0800

    style(analyzer apk php-composer) remove useless code

--- common/sca/analyzer/php_composer.go
+++ common/sca/analyzer/php_composer.go
@@ -1,15 +1,12 @@
 package analyzer
 
 import (
-	"encoding/json"
-	"io"
 	"path/filepath"
 	"strings"
 
 	"github.com/yaklang/yaklang/common/sca/dxtypes"
 
 	"github.com/aquasecurity/go-dep-parser/pkg/php/composer"
-	"github.com/yaklang/yaklang/common/utils"
 	"golang.org/x/exp/slices"
 )
 
@@ -33,10 +30,6 @@ func NewPHPComposerAnalyzer() *composerAnalyzer {
 	return &composerAnalyzer{}
 }
 
-type composerJson struct {
-	Require map[string]string `json:"require"`
-}
-
 func (a composerAnalyzer) Analyze(afi AnalyzeFileInfo) ([]*dxtypes.Package, error) {
 	fi := afi.Self
 	switch fi.MatchStatus {
@@ -50,14 +43,6 @@ func (a composerAnalyzer) Analyze(afi AnalyzeFileInfo) ([]*dxtypes.Package, erro
 		return pkgs, nil
 	}
 	return nil, nil
-}
-func (a composerAnalyzer) parseComposerJson(f io.Reader) (map[string]string, error) {
-	jsonFile := composerJson{}
-	err := json.NewDecoder(f).Decode(&jsonFile)
-	if err != nil {
-		return nil, utils.Errorf("json decode error: %v", err)
-	}
-	return jsonFile.Require, nil
 }
 
 func (a composerAnalyzer) Match(info MatchInfo) int {
