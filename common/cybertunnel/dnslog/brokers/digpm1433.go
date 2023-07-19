09501d8933307b44e9535f8455a3d4563ddb3bdf
v1ll4n <v1ll4n@qq.com>
2023-07-19 13:21:13 +0800

    fix dig.pm 1433

--- common/cybertunnel/dnslog/brokers/digpm1433.go
+++ common/cybertunnel/dnslog/brokers/digpm1433.go
@@ -1,7 +1,9 @@
 package dnslogbrokers
 
 import (
+	"github.com/davecgh/go-spew/spew"
 	"github.com/yaklang/yaklang/common/cybertunnel/tpb"
+	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/lowhttp"
 	"time"
@@ -10,27 +12,96 @@ import (
 type digpm1433Broker struct {
 }
 
+func (c *digpm1433Broker) Name() string {
+	return "dig.pm-1433"
+}
+
 var defaultDigPm1433 = &digpm1433Broker{}
 
 func init() {
-	register("dig.pm-1433", &digpm1433Broker{})
+	register(defaultDigPm1433)
 }
 
-func (s *digpm1433Broker) GetResult(du time.Duration, proxy ...string) ([]*tpb.DNSLogEvent, error) {
+func (s *digpm1433Broker) GetResult(token string, du time.Duration, proxy ...string) ([]*tpb.DNSLogEvent, error) {
+	var packet = `POST /get_results HTTP/1.1
+Host: dig.pm
+Accept-Encoding: identity
+Content-type: application/x-www-form-urlencoded
+Origin: https://dig.pm
+Referer: https://dig.pm/
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36
+
+domain=ipv6.1433.eu.org.&token=ab`
+
+	rsp, err := lowhttp.HTTP(
+		lowhttp.WithPacket(lowhttp.ReplaceHTTPPacketPostParam([]byte(packet), "token", token)),
+		lowhttp.WithHttps(true),
+		lowhttp.WithTimeout(du),
+		lowhttp.WithProxy(proxy...),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	/*
+		{
+		    "0": {
+		        "ip": "172.253.0.3:63509",
+		        "subdomain": "Bf3B80Fb.ipv6.1433.eu.org.",
+		        "time": "2023-07-19T12:58:15+08:00"
+		    }
+		}
+	*/
+	_, body := lowhttp.SplitHTTPPacketFast(rsp.RawPacket)
+	var results []*tpb.DNSLogEvent
+	for _, data := range utils.ParseStringToGeneralMap(body) {
+		params := utils.InterfaceToMapInterface(data)
+		var remoteAddr string
+		var remoteIP string
+		var remotePort int32
+		if ret := utils.MapGetString(params, "ip"); ret != "" {
+			remoteAddr = ret
+			host, port, _ := utils.ParseStringToHostPort(remoteAddr)
+			remoteIP = host
+			remotePort = int32(port)
+		}
+		var ts int64
+		t, err := time.Parse(time.RFC3339, utils.MapGetString(params, "time"))
+		if err != nil {
+			log.Errorf(`time.Parse(time.RFC3339, utils.MapGetString(params, "time")) err: %v`, err)
+		}
+		if !t.IsZero() {
+			ts = t.Unix()
+		}
+		var raw = []byte(spew.Sdump(data))
+		results = append(results, &tpb.DNSLogEvent{
+			Type:       s.Name(),
+			Token:      token,
+			Domain:     utils.MapGetString(params, "subdomain"),
+			RemoteAddr: remoteAddr,
+			RemoteIP:   remoteIP,
+			RemotePort: remotePort,
+			Raw:        raw,
+			Timestamp:  ts,
+			Mode:       s.Name(),
+		})
+	}
+	if len(results) > 0 {
+		return results, nil
+	}
+
 	return nil, utils.Error("emtpy result or not implemented")
 }
 
 func (s *digpm1433Broker) Require(du time.Duration, proxy ...string) (string, string, error) {
-	packet := `
-POST /new_gen HTTP/1.1
+	packet := `POST /new_gen HTTP/1.1
 Host: dig.pm
 Content-Type: application/x-www-form-urlencoded
 Origin: https://dig.pm
 Referer: https://dig.pm/
 User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36
 
-domain=ipv6.1433.eu.org.
-`
+domain=ipv6.1433.eu.org.`
 	/*
 
 	 */
