f919a0c2d333df08f21449b2d1facd8218ddecab
OrangeWatermelon <z3r0ne@163.com>
2023-07-23 17:30:45 +0800

    解析器

--- common/fuzztagx/fuzztag.go
+++ common/fuzztagx/fuzztag.go
@@ -1,14 +1,47 @@
 package fuzztagx
 
-type FuzzTag struct {
-	label  string
-	Method []FuzzTagMethod
-}
-type FuzzTagMethod struct {
-	name  string
-	param string
+type Node interface {
+	Strings() []string
 }
 
-func (f *FuzzTag) Exec() []string {
+// String
+type StringNode string
+
+func NewStringNode(s string) *StringNode {
+	n := StringNode(s)
+	return &n
+}
+func (s *StringNode) Strings() []string {
+	return []string{string(*s)}
+}
+
+// String
+type ExpressionNode string
+
+func NewExpressionNode(s string) *ExpressionNode {
+	n := ExpressionNode(s)
+	return &n
+}
+func (s *ExpressionNode) Strings() []string {
+	return []string{string(*s)}
+}
+
+// FuzzTag/ExpressionTag
+type Tag struct {
+	IsExpTag bool
+	Nodes []Node
+}
+
+func (f *Tag) Strings() []string {
+	return nil
+}
+
+// FuzzTagMethod
+type FuzzTagMethod struct {
+	name  string
+	params []Node
+}
+
+func (f *FuzzTagMethod) Strings() []string {
 	return nil
 }
--- common/fuzztagx/parser_test.go
+++ common/fuzztagx/parser_test.go
@@ -1,27 +1,80 @@
 package fuzztagx
 
 import (
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/yaklang/yaklang/common/utils"
+	"strings"
 	"testing"
 )
 
-func TestExecuteExpTag(t *testing.T) {
+func DumpResult(data []any) string {
+	var dumpNodes func(tag []Node) string
+	dumpNodes = func(nodes []Node) string {
+		res := ""
+		for _, inode := range nodes {
+			switch node := inode.(type) {
+			case *StringNode, *ExpressionNode:
+				res += strings.Join(node.Strings(), "")
+			case *Tag:
+				tagName := "tag"
+				if node.IsExpTag {
+					tagName = "exp"
+				}
+				res += fmt.Sprintf(" %s{%s} ", tagName, dumpNodes(node.Nodes))
+			case *FuzzTagMethod:
+				res += fmt.Sprintf("%s(%s)", node.name, dumpNodes(node.params))
+			}
+		}
+		return res
+	}
 
+	res := ""
+	for _, d := range data {
+		switch ret := d.(type) {
+		case string:
+			res += ret
+		case *StringNode:
+			res += strings.Join(ret.Strings(), "")
+		case *Tag:
+			tagName := "tag"
+			if ret.IsExpTag {
+				tagName = "exp"
+			}
+			res += fmt.Sprintf(" %s{%s} ", tagName, dumpNodes(ret.Nodes))
+		}
+	}
+	return res
+}
+func TestParse(t *testing.T) {
 	for _, testCase := range [][2]string{
 		{
-			"asd{{{ int(1)  }}}}}{}",
-			"1",
+			"asd{{{ int({{ int(1) }})  }}}}}{}",
+			"asd{ tag{int( tag{int(1)} )} }}}{}",
+		},
+		{
+			"asd{{{= int({{= int(1) }})  }}}}}{}",
+			"asd{ exp{ int({{= int(1) } )  }}}}}{}",
+		},
+		{
+			"{{ " +
+				"" +
+				"int({{=1+1}}-{{=1+3}}) }}",
+			" tag{int( exp{1+1} - exp{1+3} )} ",
+		},
+		{
+			"{{{int(a)}{{int(a)}}",
+			"{{{int(a)} tag{int(a)} ",
 		},
 	} {
-		res, err := ExecuteWithStringHandler(testCase[0], nil)
+		res, err := Parse(testCase[0])
 		if err != nil {
 			panic(utils.Errorf("test data [%v] error: %v", testCase[0], err))
 		}
-		if len(res) == 0 {
-			panic("generate error")
-		}
-		if res[0] != testCase[1] {
-			panic(utils.Errorf("test data [%v] failed", testCase[0]))
+		r := DumpResult(res)
+		spew.Dump(r)
+		if r != testCase[1] {
+			panic(utils.Errorf("test data failed, expect: %v, got: %v", testCase[1], r))
 		}
 	}
 }
