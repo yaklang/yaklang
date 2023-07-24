7c28eb10093b30a31697faec7e67eab111ab4cd4
v1ll4n <v1ll4n@v1ll4ndeMac-Studio.local>
2023-07-24 11:23:13 +0800

    fix testcase

--- common/coreplugin/heuristic_sql_test.go
+++ common/coreplugin/heuristic_sql_test.go
@@ -25,27 +25,27 @@ func TestGRPCMUSTPASS_SQL(t *testing.T) {
 	}
 	vul1 := VulInfo{
 		Path:           "/user/by-id-safe?id=1",
-		ExpectedResult: map[string]int{"参数:id未检测到闭合边界": 3},
+		ExpectedResult: map[string]int{"参数:id未检测到闭合边界": 1},
 	}
 	vul2 := VulInfo{
 		Path:           "/user/id?id=1",
-		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字型[id] 无边界闭合】": 3, "存在基于UNION SQL 注入: [参数名:id 原值:1]": 3},
+		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字型[id] 无边界闭合】": 1, "存在基于UNION SQL 注入: [参数名:id 原值:1]": 1},
 	}
 	vul3 := VulInfo{
 		Path:           "/user/id-json?id=%7B%22uid%22%3A1%2C%22id%22%3A%221%22%7D",
-		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字型[id] 无边界闭合】": 3, "存在基于UNION SQL 注入: [参数名:id 原值:1]": 3},
+		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字型[id] 无边界闭合】": 1, "存在基于UNION SQL 注入: [参数名:id 原值:1]": 1},
 	}
 	vul4 := VulInfo{
 		Path:           "/user/id-b64-json?id=eyJ1aWQiOjEsImlkIjoiMSJ9",
-		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字型[id] 无边界闭合】": 3, "存在基于UNION SQL 注入: [参数名:id 原值:1]": 3},
+		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字型[id] 无边界闭合】": 1, "存在基于UNION SQL 注入: [参数名:id 原值:1]": 1},
 	}
 	vul5 := VulInfo{
 		Path:           "/user/name?name=admin",
-		ExpectedResult: map[string]int{"疑似SQL注入：【参数：字符串[name] 单引号闭合】": 3, "存在基于UNION SQL 注入: [参数名:name 原值:admin]": 3},
+		ExpectedResult: map[string]int{"疑似SQL注入：【参数：字符串[name] 单引号闭合】": 1, "存在基于UNION SQL 注入: [参数名:name 原值:admin]": 1},
 	}
 	vul6 := VulInfo{
 		Path:           "/user/id-error?id=1",
-		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字[id] 无边界闭合】": 3, "存在基于UNION SQL 注入: [参数名:id 值:[1]]": 3},
+		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字[id] 无边界闭合】": 1, "存在基于UNION SQL 注入: [参数名:id 值:[1]]": 1},
 	}
 	vul7 := VulInfo{
 		Path: "/user/cookie-id",
@@ -54,15 +54,15 @@ func TestGRPCMUSTPASS_SQL(t *testing.T) {
 			Value: "ID=1",
 		},
 		},
-		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字[ID] 双引号闭合】": 3, "存在基于UNION SQL 注入: [参数名:ID 值:[1]]": 3},
+		ExpectedResult: map[string]int{"疑似SQL注入：【参数：数字[ID] 双引号闭合】": 1, "存在基于UNION SQL 注入: [参数名:ID 值:[1]]": 1},
 	}
 	vul8 := VulInfo{
 		Path:           "/user/name/like?name=a",
-		ExpectedResult: map[string]int{"疑似SQL注入：【参数：字符串[name] like注入( %' )】": 3, "存在基于UNION SQL 注入: [参数名:name 值:[a]]": 3},
+		ExpectedResult: map[string]int{"疑似SQL注入：【参数：字符串[name] like注入( %' )】": 1, "存在基于UNION SQL 注入: [参数名:name 值:[a]]": 1},
 	}
 	vul9 := VulInfo{
 		Path:           "/user/name/like/2?name=a",
-		ExpectedResult: map[string]int{"疑似SQL注入：【参数：字符串[name] like注入( %' )】": 3},
+		ExpectedResult: map[string]int{"疑似SQL注入：【参数：字符串[name] like注入( %' )】": 1},
 	}
 	//vul10 := VulInfo{
 	//	Path:           "/user/name/like/b64?nameb64=%59%51%3d%3d",
@@ -70,7 +70,7 @@ func TestGRPCMUSTPASS_SQL(t *testing.T) {
 	//}
 	vul11 := VulInfo{
 		Path:           "/user/name/like/b64j?data=eyJuYW1lYjY0aiI6ImEifQ%3D%3D",
-		ExpectedResult: map[string]int{"疑似SQL注入：【参数：字符串[data] like注入( %' )】": 3},
+		ExpectedResult: map[string]int{"疑似SQL注入：【参数：字符串[data] like注入( %' )】": 1},
 	}
 
 	Must(TestCoreMitmPlug(pluginName, server, vul1, client, t), "SQL插件对于安全的SQL注入检测结果不符合预期")
