ba5f3ca9e9e43a95c5185e32628f3c682eb4d17b
z3 <z3r0ne@163.com>
2023-07-25 10:53:28 +0800

    修复请求失败时响应包没有请求时长信息的问题 && 支持字符串比较 && 优化yakit log，只有log类型的信息才在console显示，日志信息携带yak文件名和行号
    .
    
    修复yakit client对象复用导致的bug

--- common/coreplugin/fastjson_test.go
+++ common/coreplugin/fastjson_test.go
@@ -2,18 +2,11 @@ package coreplugin
 
 import (
 	"context"
-	"errors"
-	"github.com/yaklang/yaklang/common/cybertunnel/tpb"
 	"github.com/yaklang/yaklang/common/log"
-	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/vulinbox"
-	"github.com/yaklang/yaklang/common/yak/yaklang"
-	"github.com/yaklang/yaklang/common/yak/yaklib"
 	"github.com/yaklang/yaklang/common/yak/yaklib/codec"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
-	"strings"
 	"testing"
-	"time"
 )
 
 func TestGRPCMUSTPASS_Fastjson(t *testing.T) {
