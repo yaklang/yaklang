687de369a42e940d943ecf13797ecb5abae3093b
wlingze <lingze.wl@gmail.com>
2023-07-28 17:55:25 +0800

    feat(function readVariable) remove trivial Phi instruction

--- common/yak/ssa/variable.go
+++ common/yak/ssa/variable.go
@@ -27,22 +27,13 @@ func (f *Function) readVariableByBlock(variable string, block *BasicBlock) Value
 }
 
 func (f *Function) readVariableRecursive(variable string, block *BasicBlock) Value {
+	// if block in sealedBlock
 	if len(block.Preds) == 1 {
 		return f.readVariableByBlock(variable, block.Preds[0])
 	} else {
-		phi := &Phi{
-			anInstruction: anInstruction{
-				Parent: f,
-				Block:  block,
-			},
-			Edge: []Value{},
-		}
-		f.wirteVariableByBlock(variable, phi, block)
-		for _, p := range phi.Block.Preds {
-			phi.Edge = append(phi.Edge, f.readVariableByBlock(variable, p))
-		}
-		block.Phis = append(block.Phis, phi)
-		return phi
+		v := f.newPhi(block, variable)
+		f.wirteVariableByBlock(variable, v, block)
+		return v
 	}
 	return nil
 }
