e91684731ad3b4731b6b3638d9caf21ef635099f
WAY29 <1162410187@qq.com>
2023-07-28 15:54:20 +0800

    test(xss) add more xss plugin testcases

--- common/coreplugin/heuristic_xss_test.go
+++ common/coreplugin/heuristic_xss_test.go
@@ -3,8 +3,9 @@ package coreplugin
 import (
 	"context"
 	"fmt"
-	"github.com/yaklang/yaklang/common/vulinbox"
 	"testing"
+
+	"github.com/yaklang/yaklang/common/vulinbox"
 )
 
 func TestGRPCMUSTPASS_XSS(t *testing.T) {
@@ -29,12 +30,34 @@ func TestGRPCMUSTPASS_XSS(t *testing.T) {
 			"/xss/safe?name=admin",
 			"/xss/echo?name=admin",
 			"/xss/replace/nocase?name=admin",
+			"/xss/js/in-str?name=admin",
+			"/xss/js/in-str2?name=admin",
+			"/xss/js/in-str-temp?name=admin",
+			"/xss/attr/onclick?code=2-1",
+			"/xss/attr/onclick2?code=2-1",
+			`/xss/attr/alt/json?json={"value":"value=visitor-name"}`,
+			`/xss/attr/alt/b64/json?b64json=eyJ2YWx1ZSI6InZhbHVlPXZpc2l0b3ItbmFtZSJ9`,
+			`/xss/attr/src?src=/static/logo.png`,
+			`/xss/attr/href?href=/static/logo.png`,
+			`/xss/cookie/name`,
+			`/xss/cookie/b64/json/name`,
 		},
 		ExpectedResult: map[string]int{
 			fmt.Sprintf("XSS for: %s/xss/echo?name=admin", vulAddr):           1,
 			fmt.Sprintf("XSS for: %s/xss/replace/nocase?name=admin", vulAddr): 1,
+			fmt.Sprintf("XSS for: %s/xss/js/in-str?name=admin", vulAddr):      1,
+			fmt.Sprintf("XSS for: %s/xss/js/in-str2?name=admin", vulAddr):     1,
+			fmt.Sprintf("XSS for: %s/xss/js/in-str-temp?name=admin", vulAddr): 1,
+			fmt.Sprintf("XSS for: %s/xss/attr/onclick?code=2-1", vulAddr):     1,
+			fmt.Sprintf(`XSS for: %s/xss/attr/onclick2?code=2-1`, vulAddr):    1,
+			fmt.Sprintf(`XSS for: %s/xss/attr/alt/json`, vulAddr):             1,
+			fmt.Sprintf(`XSS for: %s/xss/attr/alt/b64/json`, vulAddr):         1,
+			fmt.Sprintf(`XSS for: %s/xss/attr/src`, vulAddr):                  1,
+			fmt.Sprintf(`XSS for: %s/xss/attr/href`, vulAddr):                 1,
+			fmt.Sprintf(`XSS for: %s/xss/cookie/name`, vulAddr):               1,
+			fmt.Sprintf(`XSS for: %s/xss/cookie/b64/json/name`, vulAddr):      1,
 		},
-		StrictMode: true,
+		StrictMode: false,
 	}
 
 	Must(TestCoreMitmPlug(pluginName, server, vul, client, t), " ")
