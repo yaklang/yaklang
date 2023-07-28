edc22aab9af60890c463bad2fe531f5bb14f6030
baochenyang <baochenyang@4dogs.cn>
2023-07-28 15:27:53 +0800

    code update

--- common/crawlerx/main_test.go
+++ common/crawlerx/main_test.go
@@ -2,7 +2,10 @@
 // @Author bcy2007  2023/7/17 14:25
 package crawlerx
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+)
 
 func TestUrlCheck(t *testing.T) {
 	urlA := `baidu.com`
@@ -16,4 +19,38 @@ func TestUrlCheck(t *testing.T) {
 	urlC := `http://www.baidu.com`
 	urlCResult, err := TargetUrlCheck(urlC, nil)
 	t.Log(urlCResult, err)
+}
+
+func TestStartCrawler(t *testing.T) {
+	urlStr := `http://testphp.vulnweb.com/`
+	opts := make([]ConfigOpt, 0)
+	browserInfo := BrowserInfo{
+		ExePath:       "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+		WsAddress:     "",
+		ProxyAddress:  "",
+		ProxyUsername: "",
+		ProxyPassword: "",
+	}
+	browserBytes, _ := json.Marshal(&browserInfo)
+	opts = append(opts,
+		WithBrowserInfo(string(browserBytes)),
+		WithFormFill(map[string]string{"username": "admin", "password": "password"}),
+		WithFileInput(map[string]string{"default": "/Users/chenyangbao/1.txt"}),
+		WithBlackList("logout"),
+		WithMaxDepth(2),
+		WithLeakless("true"),
+		WithExtraWaitLoadTime(500),
+		WithLocalStorage(map[string]string{"test": "abc"}),
+		WithConcurrent(3),
+	)
+	ch, err := StartCrawler(urlStr, opts...)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	for item := range ch {
+		t.Logf(`%s %s from %s`, item.Method(), item.Url(), item.From())
+	}
+	t.Log(`done!`)
+	return
 }
