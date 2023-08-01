2abb8578baf1a7ea0ebc8227601fb340cee4e19f
rookie <rookie_is@qq.com>
2023-08-01 18:03:13 +0800

    .
    .
    
    .
    
    .
    
    feat(core-plug):url重定向漏洞插件
    
    feat(vulinbox):增加重定向靶场,重定向漏洞插件测试
    
    feat(CI):增加CI
    
    fix(CI):CI逻辑修复

--- common/yak/yaklib/js_test.go
+++ common/yak/yaklib/js_test.go
@@ -1,1 +1,32 @@
 package yaklib
+
+import (
+	"fmt"
+	"github.com/yaklang/yaklang/common/javascript/otto/parser"
+	"strconv"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+	code := `
+	setTimeout(function() {
+	window.location.replace(` + strconv.Quote("http://baidu.com") + `);
+	console.log("1111")
+	}, 3000)
+console.log("1111")
+for (var i=0; i<5; i++)
+{
+	console.log("1111")
+}
+var a = 1
+var b = 2
+var a = 2 
+if (a == b){
+	console.log("1111")
+}
+	`
+	res, err := parser.ParseFile(nil, "", code, 0)
+	if err == nil {
+		fmt.Printf("%v", res)
+	}
+}
