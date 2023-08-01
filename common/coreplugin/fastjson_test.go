0e9aae344a5677dd47a93a41887ed3677136752c
z3 <z3r0ne@163.com>
2023-08-01 17:46:54 +0800

    增加 fastjson 插件测试案例

--- common/coreplugin/fastjson_test.go
+++ common/coreplugin/fastjson_test.go
@@ -2,7 +2,9 @@ package coreplugin
 
 import (
 	"context"
+	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/vulinbox"
+	"github.com/yaklang/yaklang/common/yak/yaklib/codec"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 	"testing"
 )
@@ -17,48 +19,143 @@ func TestGRPCMUSTPASS_Fastjson(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-
-	pluginName := "启发式SQL注入检测"
+	log.Infof("vulAddr: %v", vulAddr)
+	//time.Sleep(5 * time.Hour)
+	pluginName := "Fastjson 综合检测"
 	server := VulServerInfo{
 		VulServerAddr: vulAddr,
 		IsHttps:       true,
 	}
-	vul := VulInfo{
+	vulInGet := VulInfo{
 		Path: []string{
-			"/user/by-id-safe?id=1",
-			"/user/cookie-id",
-			"/user/id?id=1",
-			"/user/id-json?id=%7B%22uid%22%3A1%2C%22id%22%3A%221%22%7D",
-			"/user/id-b64-json?id=eyJ1aWQiOjEsImlkIjoiMSJ9",
-			"/user/name?name=admin",
-			"/user/id-error?id=1",
-			"/user/name/like?name=a",
-			"/user/name/like/2?name=a",
-			"/user/name/like/b64j?data=eyJuYW1lYjY0aiI6ImEifQ%3D%3D",
+			"/fastjson/json-in-query?auth=" + codec.EncodeUrlCode(`{"user":"admin","password":"password"}`) + "&action=login",
 		},
-		Headers: []*ypb.KVPair{{
-			Key:   "Cookie",
-			Value: "ID=1",
-		}},
 		ExpectedResult: map[string]int{
-			//"参数:id未检测到闭合边界":                         1,
-			"疑似SQL注入：【参数：数字[id] 无边界闭合】":                        4,
-			"存在基于UNION SQL 注入: [参数名:id 值:[1]]":                 4,
-			"疑似SQL注入：【参数：字符串[name] 单引号闭合】":                     1,
-			"存在基于UNION SQL 注入: [参数名:name 值:[admin]":            1,
-			"疑似SQL注入：【参数：数字[ID] 双引号闭合】":                        1,
-			"存在基于UNION SQL 注入: [参数名:ID 值:[1]]":                 1,
-			"疑似SQL注入：【参数：字符串[name] like注入( %' )】":              2,
-			"存在基于UNION SQL 注入: [参数名:name 值:[a]]":               1,
-			"疑似SQL注入：【参数：字符串[data] like注入( %' )】":              1,
-			"可能存在基于错误的 SQL 注入: [参数名:id 原值:[1]] 猜测数据库类型: MySQL": 1,
+			"目标 fastjson 框架可能存在 RCE 漏洞 (DNSLog Check)": 1,
 		},
 		StrictMode: false,
 	}
-
-	//vul10 := VulInfo{
-	//	Path:           "/user/name/like/b64?nameb64=%59%51%3d%3d",
-	//	ExpectedResult: map[string]int{"疑似SQL注入：【参数：字符串[name] like注入( %' )】": 3},
+	Must(TestCoreMitmPlug(pluginName, server, vulInGet, client, t), "Fastjson 综合检测插件检测结果不符合预期")
+	vulInForm := VulInfo{
+		Method: "POST",
+		Path: []string{
+			"/fastjson/json-in-form",
+		},
+		Headers: []*ypb.KVPair{
+			{
+				Key:   "Content-Type",
+				Value: "application/x-www-form-urlencoded",
+			},
+		},
+		Body: []byte(`auth={"user":"admin","password":"password"}`),
+		ExpectedResult: map[string]int{
+			"目标 fastjson 框架可能存在 RCE 漏洞 (DNSLog Check)": 1,
+		},
+		StrictMode: false,
+	}
+	Must(TestCoreMitmPlug(pluginName, server, vulInForm, client, t), "Fastjson 综合检测插件检测结果不符合预期")
+	vulInBodyJson := VulInfo{
+		Method: "POST",
+		Path: []string{
+			"/fastjson/json-in-body",
+		},
+		Body: []byte(`{"user":"admin","password":"password"}`),
+		Headers: []*ypb.KVPair{
+			{
+				Key:   "Content-Type",
+				Value: "application/json",
+			},
+		},
+		ExpectedResult: map[string]int{
+			"目标 fastjson 框架可能存在 RCE 漏洞 (DNSLog Check)": 1,
+		},
+		StrictMode: true,
+	}
+	Must(TestCoreMitmPlug(pluginName, server, vulInBodyJson, client, t), "Fastjson 综合检测插件检测结果不符合预期")
+	vulInGetServeByJackson := VulInfo{ // 这里不应该检出任何漏洞，并且发包数量应该为 1
+		Method: "GET",
+		Path: []string{
+			"/fastjson/jackson-in-query?auth=" + codec.EncodeUrlCode(`{"user":"admin","password":"password"}`) + "&action=login",
+		},
+		ExpectedResult: map[string]int{},
+		StrictMode:     true,
+	}
+	Must(TestCoreMitmPlug(pluginName, server, vulInGetServeByJackson, client, t), "Fastjson 综合检测插件检测结果不符合预期")
+	// TODO: 需要先修复 fuzz 请求出错后不能获取Duration的问题
+	//vulInGetIntranet := VulInfo{
+	//	Method: "GET",
+	//	Path: []string{
+	//		"/fastjson/get-in-query-intranet?auth=" + codec.EncodeUrlCode(`{"user":"admin","password":"password"}`) + "&action=login",
+	//	},
+	//	ExpectedResult: map[string]int{
+	//		"目标 fastjson 框架可能存在 RCE 漏洞 (Delay Check)": 1,
+	//	},
+	//	StrictMode: true,
 	//}
-	Must(TestCoreMitmPlug(pluginName, server, vul, client, t), "SQL插件对于SQL注入检测结果不符合预期")
+	//Must(TestCoreMitmPlug(pluginName, server, vulInGetIntranet, client, t), "Fastjson 综合检测插件检测结果不符合预期")
+	// TODO: Cookie Fuzz 需要支持自动解码
+	//vulInGet := VulInfo{
+	//	Method: "GET",
+	//	Path: []string{
+	//		"/fastjson/json-in-cookie?action=login",
+	//	},
+	//	Headers: []*ypb.KVPair{
+	//		{
+	//			Key:   "Cookie",
+	//			Value: `auth=` + codec.EncodeBase64Url(`{"id":"-1"}`),
+	//		},
+	//	},
+	//	ExpectedResult: map[string]int{
+	//		"目标 fastjson 框架可能存在 RCE 漏洞 (DNSLog Check)": 1,
+	//	},
+	//	StrictMode: true,
+	//}
+	//Must(TestCoreMitmPlug(pluginName, server, vulInGet, client, t), "Fastjson 综合检测插件检测结果不符合预期")
+	// TODO: Authorization Fuzz 需要支持自动解码
+	//vulInGet := VulInfo{
+	//	Method: "GET",
+	//	Path: []string{
+	//		"/fastjson/json-in-authorization?action=login",
+	//	},
+	//	Headers: []*ypb.KVPair{
+	//		{
+	//			Key:   "Authorization",
+	//			Value: `Basic ` + codec.EncodeBase64Url(`{"user":"admin","password":"password"}`),
+	//		},
+	//	},
+	//	ExpectedResult: map[string]int{
+	//		"目标 fastjson 框架可能存在 RCE 漏洞 (DNSLog Check)": 1,
+	//	},
+	//	StrictMode: true,
+	//}
+	//Must(TestCoreMitmPlug(pluginName, server, vulInGet, client, t), "Fastjson 综合检测插件检测结果不符合预期")
+}
+func TestFastjson(t *testing.T) {
+	client, err := NewLocalClient()
+	if err != nil {
+		panic(err)
+	}
+
+	vulAddr, err := vulinbox.NewVulinServer(context.Background())
+	if err != nil {
+		panic(err)
+	}
+	log.Infof("vulAddr: %v", vulAddr)
+	//time.Sleep(5 * time.Hour)
+	pluginName := "Fastjson 综合检测"
+	server := VulServerInfo{
+		VulServerAddr: vulAddr,
+		IsHttps:       true,
+	}
+	vulInGet := VulInfo{
+		Method: "GET",
+		Path: []string{
+			"/fastjson/get-in-query-intranet?auth=" + codec.EncodeUrlCode(`{"user":"admin","password":"password"}`) + "&action=login",
+		},
+		ExpectedResult: map[string]int{
+			"目标 fastjson 框架可能存在 RCE 漏洞 (Delay Check)": 1,
+		},
+		StrictMode: true,
+	}
+	Must(TestCoreMitmPlug(pluginName, server, vulInGet, client, t), "Fastjson 综合检测插件检测结果不符合预期")
 }
