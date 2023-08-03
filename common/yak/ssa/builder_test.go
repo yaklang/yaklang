2b6b63e31a8376db36bcfcd9204f3f36be2a2daf
wlingze <lingze.wl@gmail.com>
2023-08-03 10:26:12 +0800

    test(update testcase) fix:bc7b2c\969925

--- common/yak/ssa/builder_test.go
+++ common/yak/ssa/builder_test.go
@@ -64,6 +64,9 @@ func CheckProgram(t *testing.T, prog *Program) {
 						t.Fatalf("fatal instruction %s[%d] error pointer to function", inst, i)
 					}
 
+					if slices.Contains(inst.GetValues(), inst.(Value)) {
+						t.Fatalf("fatal inst %s has this self", inst)
+					}
 					// value-user check
 					for _, value := range inst.GetValues() {
 						if !slices.Contains(value.GetUsers(), inst.(User)) {
@@ -84,12 +87,15 @@ func CheckProgram(t *testing.T, prog *Program) {
 
 }
 
-func showProg(prog *Program) {
+func showProg(prog *Program) string {
+	ret := ""
 	for _, pkg := range prog.Packages {
 		for _, f := range pkg.funcs {
-			fmt.Printf("%s\n", f)
+			ret += f.String()
 		}
 	}
+	fmt.Println(ret)
+	return ret
 }
 
 type TestProgram struct {
@@ -150,7 +156,6 @@ func Compare(t *testing.T, prog *Program, want *TestProgram) {
 			}
 			got := f.String()
 			CompareIR(t, got, want)
-
 		}
 	}
 
@@ -206,13 +211,14 @@ d = a + 11
 		ir := `
 yak-main
 entry0:
-	%0 = 42 add 42
-	%1 = %0 add 33
-	%2 = %1 add 23
-	%3 = %2 add 11
+	t0 = 42 add 42
+	t1 = t0 add 33
+	t2 = t1 add 23
+	t3 = t2 add 11
 		`
 		prog := parseSSA(src)
 		CheckProgram(t, prog)
+		// showProg(prog)
 		CompareYakMain(t, prog, ir)
 	})
 }
@@ -230,15 +236,15 @@ d = 1 + 2
 		ir := `
 yak-main
 entry0:
-	%0 = 5 lt 2
-	%1 = If [%0] true -> if.true2, false -> if.done1
+	t0 = 5 lt 2
+	If [t0] true -> if.true2, false -> if.done1
 if.done1: <- if.true2 entry0
-	%2 = jump -> b3
+	jump -> b3
 if.true2: <- entry0
-	%3 = 5 add 6
-	%4 = jump -> if.done1
+	t3 = 5 add 6
+	jump -> if.done1
 b3: <- if.done1
-	%5 = 1 add 2
+	t5 = 1 add 2
 		`
 		prog := parseSSA(src)
 		CheckProgram(t, prog)
@@ -268,42 +274,43 @@ d = 1 + 2
 		ir := `
 yak-main
 entry0:
-	%0 = 5 lt 2
-	%1 = If [%0] true -> if.true2, false -> if.elif3
+	t0 = 5 lt 2
+	If [t0] true -> if.true2, false -> if.elif3
 if.done1: <- if.true2 if.true4 if.true6 if.true8 if.true10 if.false11
-	%2 = jump -> b12
+	t3 = phi [t12, if.true6] [t16, if.true8] [t20, if.true10] [t22, if.false11]
+	jump -> b12
 if.true2: <- entry0
-	%3 = 5 add 6
-	%4 = jump -> if.done1
+	t4 = 5 add 6
+	jump -> if.done1
 if.elif3: <- entry0
-	%5 = 5 lt 4
-	%6 = If [%5] true -> if.true4, false -> if.elif5
+	t6 = 5 lt 4
+	If [t6] true -> if.true4, false -> if.elif5
 if.true4: <- if.elif3
-	%7 = 5 add 9
-	%8 = jump -> if.done1
+	t8 = 5 add 9
+	jump -> if.done1
 if.elif5: <- if.elif3
-	%9 = 5 lt 6
-	%10 = If [%9] true -> if.true6, false -> if.elif7
+	t10 = 5 lt 6
+	If [t10] true -> if.true6, false -> if.elif7
 if.true6: <- if.elif5
-	%11 = 5 add 5
-	%12 = jump -> if.done1
+	t12 = 5 add 5
+	jump -> if.done1
 if.elif7: <- if.elif5
-	%13 = 5 lt 10
-	%14 = If [%13] true -> if.true8, false -> if.elif9
+	t14 = 5 lt 10
+	If [t14] true -> if.true8, false -> if.elif9
 if.true8: <- if.elif7
-	%15 = 5 add 20
-	%16 = jump -> if.done1
+	t16 = 5 add 20
+	jump -> if.done1
 if.elif9: <- if.elif7
-	%17 = 5 lt 20
-	%18 = If [%17] true -> if.true10, false -> if.false11
+	t18 = 5 lt 20
+	If [t18] true -> if.true10, false -> if.false11
 if.true10: <- if.elif9
-	%19 = 5 add 30
-	%20 = jump -> if.done1
+	t20 = 5 add 30
+	jump -> if.done1
 if.false11: <- if.elif9
-	%21 = 5 add 40
-	%22 = jump -> if.done1
+	t22 = 5 add 40
+	jump -> if.done1
 b12: <- if.done1
-	%23 = 1 add 2
+	t24 = 1 add 2
 		`
 		prog := parseSSA(src)
 		CheckProgram(t, prog)
@@ -334,42 +341,42 @@ d = 1 + 2
 		ir := `
 yak-main
 entry0:
-	%0 = 5 lt 2
-	%1 = If [%0] true -> if.true2, false -> if.elif3
+	t0 = 5 lt 2
+	If [t0] true -> if.true2, false -> if.elif3
 if.done1: <- if.true2 if.true4 if.true6 if.true8 if.true10 if.false11
-	%2 = jump -> b12
+	jump -> b12
 if.true2: <- entry0
-	%3 = 5 add 6
-	%4 = jump -> if.done1
+	t3 = 5 add 6
+	jump -> if.done1
 if.elif3: <- entry0
-	%5 = 5 lt 4
-	%6 = If [%5] true -> if.true4, false -> if.elif5
+	t5 = 5 lt 4
+	If [t5] true -> if.true4, false -> if.elif5
 if.true4: <- if.elif3
-	%7 = 5 add 9
-	%8 = jump -> if.done1
+	t7 = 5 add 9
+	jump -> if.done1
 if.elif5: <- if.elif3
-	%9 = 5 lt 6
-	%10 = If [%9] true -> if.true6, false -> if.elif7
+	t9 = 5 lt 6
+	If [t9] true -> if.true6, false -> if.elif7
 if.true6: <- if.elif5
-	%11 = 5 add 10
-	%12 = jump -> if.done1
+	t11 = 5 add 10
+	jump -> if.done1
 if.elif7: <- if.elif5
-	%13 = 5 lt 10
-	%14 = If [%13] true -> if.true8, false -> if.elif9
+	t13 = 5 lt 10
+	If [t13] true -> if.true8, false -> if.elif9
 if.true8: <- if.elif7
-	%15 = 5 add 20
-	%16 = jump -> if.done1
+	t15 = 5 add 20
+	jump -> if.done1
 if.elif9: <- if.elif7
-	%17 = 5 lt 20
-	%18 = If [%17] true -> if.true10, false -> if.false11
+	t17 = 5 lt 20
+	If [t17] true -> if.true10, false -> if.false11
 if.true10: <- if.elif9
-	%19 = 5 add 30
-	%20 = jump -> if.done1
+	t19 = 5 add 30
+	jump -> if.done1
 if.false11: <- if.elif9
-	%21 = 5 add 7
-	%22 = jump -> if.done1
+	t21 = 5 add 7
+	jump -> if.done1
 b12: <- if.done1
-	%23 = 1 add 2
+	t23 = 1 add 2
 		`
 		prog := parseSSA(src)
 		CheckProgram(t, prog)
@@ -393,23 +400,23 @@ func TestLoop(t *testing.T) {
 		ir := `
 yak-main
 entry0:
-	%0 = 10 add 1
-	%1 = jump -> loop.header1
+	t0 = 10 add 1
+	jump -> loop.header1
 loop.header1: <- entry0 loop.latch4
-	%4 = phi [0, entry0] [%9, loop.latch4]
-	%5 = phi [%0, entry0] [%6, loop.latch4]
-	%2 = %4 lt %5
-	%3 = If [%2] true -> loop.body2, false -> loop.exit3
+	t4 = phi [t0, entry0] [t6, loop.latch4]
+	t5 = phi [0, entry0] [t9, loop.latch4]
+	t2 = t5 lt t4
+	If [t2] true -> loop.body2, false -> loop.exit3
 loop.body2: <- loop.header1
-	%6 = %5 add %4
-	%7 = jump -> loop.latch4
+	t6 = t4 add t5
+	jump -> loop.latch4
 loop.exit3: <- loop.header1
-	%8 = jump -> b5
+	jump -> b5
 loop.latch4: <- loop.body2
-	%9 = %4 add 1
-	%10 = jump -> loop.header1
+	t9 = t5 add 1
+	jump -> loop.header1
 b5: <- loop.exit3
-	%11 = %5 add 3
+	t11 = t4 add 3
 		`
 		prog := parseSSA(code)
 		CheckProgram(t, prog)
@@ -429,26 +436,94 @@ b5: <- loop.exit3
 		ir := `
 yak-main
 entry0:
-	%0 = 10 add 1
-	%1 = jump -> loop.header1
+	t0 = 10 add 1
+	jump -> loop.header1
 loop.header1: <- entry0 loop.latch4
-	%3 = phi [0, entry0] [%8, loop.latch4]
-	%4 = phi [%0, entry0] [%5, loop.latch4]
-	%2 = If [true] true -> loop.body2, false -> loop.exit3
+	t3 = phi [t0, entry0] [t5, loop.latch4]
+	t4 = phi [0, entry0] [t8, loop.latch4]
+	If [true] true -> loop.body2, false -> loop.exit3
 loop.body2: <- loop.header1
-	%5 = %4 add %3
-	%6 = jump -> loop.latch4
+	t5 = t3 add t4
+	jump -> loop.latch4
 loop.exit3: <- loop.header1
-	%7 = jump -> b5
+	jump -> b5
 loop.latch4: <- loop.body2
-	%8 = %3 add 1
-	%9 = jump -> loop.header1
+	t8 = t4 add 1
+	jump -> loop.header1
 b5: <- loop.exit3
-	%10 = %4 add 3
+	t10 = t3 add 3
 		`
 		prog := parseSSA(code)
 		CheckProgram(t, prog)
-		showProg(prog)
+		// showProg(prog)
+		CompareYakMain(t, prog, ir)
+	})
+
+	// 	t.Run("looptest_range", func(*testing.T) {
+	// 		code := `
+	// a = 0
+	// for  range 10 {
+	// 	a += 1
+	// }
+	// b = a + 2
+	// 		`
+	// 		prog := parseSSA(code)
+	// 		CheckProgram(t, prog)
+	// 		showProg(prog)
+	// 	})
+
+	t.Run("looptest_breakcontinue", func(t *testing.T) {
+		code := `
+a = 0 
+for i=0; i<10; i++{
+	if a >= 3{
+		if a == 3{
+			break
+		}
+		continue
+	}
+	a += 2
+}
+b = a + 1
+		`
+		ir := `
+yak-main
+entry0:
+	jump -> loop.header1
+loop.header1: <- entry0 loop.latch4
+	t3 = phi [0, entry0] [t10, loop.latch4]
+	t4 = phi [0, entry0] [t8, loop.latch4]
+	t1 = t4 lt 10
+	If [t1] true -> loop.body2, false -> loop.exit3
+loop.body2: <- loop.header1
+	t5 = t3 gt-eq 3
+	If [t5] true -> if.true6, false -> if.done5
+loop.exit3: <- loop.header1 if.true8
+	jump -> b11
+loop.latch4: <- b9 b10
+	t10 = phi [t3, b9] [t17, b10]
+	t8 = t4 add 1
+	jump -> loop.header1
+if.done5: <- loop.body2
+	jump -> b10
+if.true6: <- loop.body2
+	t12 = t3 eq 3
+	If [t12] true -> if.true8, false -> if.done7
+if.done7: <- if.true6
+	jump -> b9
+if.true8: <- if.true6
+	jump -> loop.exit3
+b9: <- if.done7
+	jump -> loop.latch4
+b10: <- if.done5
+	t17 = t3 add 2
+	jump -> loop.latch4
+b11: <- loop.exit3
+	t19 = t3 add 1
+		`
+		prog := parseSSA(code)
+		CheckProgram(t, prog)
+		// showProg(prog)
 		CompareYakMain(t, prog, ir)
 	})
 
@@ -457,16 +532,17 @@ b5: <- loop.exit3
 func TestClosure(t *testing.T) {
 	t.Run("closure_simple", func(t *testing.T) {
 		code := `
-a = (pa) => {return pa * 11}
-va = a(1) + 11
+ca = 33
+a = (pa) => {return pa * 11 + ca}
 
-func b(pb1, pb2) {
-	return 12 + pb1 + pb2
-}
-vb = b(1, 2) + 12
+// ca = 33
+va = a(1) 
+
+ca = 44
+va = a(1)
 
 c = fn(pc1, pc2, pc3) {
-	return 13 + pc1 + pc2 * pc3
+	return 13 + a(va) + pc2 * pc3
 }
 vc = c(1, 2, 3) + 13
 		`
@@ -474,48 +550,91 @@ vc = c(1, 2, 3) + 13
 			`
 yak-main
 entry0:
-	%0 = makeClosure Anonymousfunc1
-	%1 = call %0 (1)
-	%2 = %1 add 11
-	%3 = makeClosure b
-	%4 = call %3 (1, 2)
-	%5 = %4 add 12
-	%6 = makeClosure Anonymousfunc3
-	%7 = call %6 (1, 2, 3)
-	%8 = %7 add 13
+	t0 = alloc
+	*t0 = 33
+	t2 = Closure yak-main$1 [t0]
+	t3 = call t2 (1)
+	*t0 = 44
+	t5 = call t2 (1)
+	t6 = alloc
+	*t6 = t2
+	t8 = alloc
+	*t8 = t5
+	t10 = Closure yak-main$2 [t6, t8]
+	t11 = call t10 (1, 2, 3)
+	t12 = t11 add 13
 			`,
-
 			`
-Anonymousfunc1 pa
+yak-main$1 pa
 parent: yak-main
+freeValue: t0
 entry0:
-	%0 = pa mul 11
-	%1 = ret %0
+	t1 = pa mul 11
+	t2 = *t0
+	t3 = t1 add t2
+	ret t3
+
 			`,
-
 			`
-b pb1, pb2
+yak-main$2 pc1, pc2, pc3
 parent: yak-main
+freeValue: t0, t1
 entry0:
-	%0 = 12 add pb1
-	%1 = %0 add pb2
-	%2 = ret %1
-			`,
-
-			`
-Anonymousfunc3 pc1, pc2, pc3
-parent: yak-main
-entry0:
-	%0 = 13 add pc1
-	%1 = pc2 mul pc3
-	%2 = %0 add %1
-	%3 = ret %2
+	t2 = *t0
+	t3 = *t1
+	t4 = call t2 (t3)
+	t5 = 13 add t4
+	t6 = pc2 mul pc3
+	t7 = t5 add t6
+	ret t7
 			`,
 		}
 
 		prog := parseSSA(code)
 		CheckProgram(t, prog)
-		showProg(prog)
+		// showProg(prog)
+		CompareYakFunc(t, prog, ir)
+	})
+
+	t.Run("closure_factor", func(t *testing.T) {
+		code := `
+set = (a)=>{
+	return () => {
+		return a
+	}
+}
+f0 = set(1)
+f1 = set(2)
+		`
+		ir := []string{
+			`
+yak-main
+entry0:
+	t0 = Closure yak-main$1 []
+	t1 = call t0 (1)
+	t2 = call t0 (2)
+`,
+			`
+yak-main$1 a
+parent: yak-main
+entry0:
+	t0 = alloc
+	*t0 = a
+	t2 = Closure yak-main$1$2 [t0]
+	ret t2
+`,
+			`
+yak-main$1$2
+parent: yak-main$1
+freeValue: t0
+entry0:
+	t1 = *t0
+	ret t1
+`,
+		}
+		prog := parseSSA(code)
+		CheckProgram(t, prog)
+		// showProg(prog)
 		CompareYakFunc(t, prog, ir)
 	})
 }
