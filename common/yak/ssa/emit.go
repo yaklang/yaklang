ec976e56c5e0df9b5419bee8b65dcdff4c6205d1
wlingze <lingze.wl@gmail.com>
2023-08-08 18:09:22 +0800

    feat(closure function) build and call

--- common/yak/ssa/emit.go
+++ common/yak/ssa/emit.go
@@ -93,11 +93,88 @@ func (f *Function) emitReturn(vs []Value) *Return {
 	return r
 }
 
-
-
 func (f *Function) emitCall(target Value, args []Value, isDropError bool) *Call {
 	if f.currentBlock.finish {
 		return nil
+	}
+
+	var freevalue []Value
+	var parent *Function
+	binding := make([]Value, 0, len(freevalue))
+
+	switch inst := target.(type) {
+	case *Field:
+		// field
+		fun := inst.GetLastValue().(*Function)
+		freevalue = fun.FreeValues
+		parent = fun.parent
+	case *Function:
+		// Function
+		freevalue = inst.FreeValues
+	case *Parameter:
+		// is a freevalue, pass
+	case *Call:
+		// call, check the function
+		switch method := inst.Method.(type) {
+		case *Function:
+			fun := method.ReturnValue()[0].(*Function)
+			freevalue = fun.FreeValues
+			parent = fun.parent
+		}
+	default:
+		// other
+		// con't call
+		panic("call target is con't call: " + target.String())
+	}
+
+	if parent == nil {
+		parent = f
+	}
+	getField := func(fun *Function, key string) bool {
+		if v := fun.readField(key); v != nil {
+			binding = append(binding, v)
+			return true
+		}
+		return false
+	}
+	for index := range freevalue {
+		if para, ok := freevalue[index].(*Parameter); ok { // not modify
+			// find freevalue in parent function
+			if v := parent.readVariable(para.variable); v != nil {
+				switch v := v.(type) {
+				case *Parameter:
+					if !v.isFreevalue {
+						// is parameter, just abort
+						continue
+					}
+					// is freevalue, find in current function
+				default:
+					binding = append(binding, v)
+					continue
+				}
+			}
+			if parent != f {
+				// find freevalue in current function
+				if v := f.readVariable(para.variable); v != nil {
+					binding = append(binding, v)
+					continue
+				}
+			}
+			fmt.Printf("debug %v\n", para.variable)
+			panic("call target clouse binding variable not found")
+		}
+
+		if field, ok := freevalue[index].(*Field); ok { // will modify in function must field
+			if getField(parent, field.Key.String()) {
+				continue
+			}
+			if parent != f {
+				if getField(f, field.Key.String()) {
+					continue
+				}
+			}
+			panic("call target clouse binding field not found")
+		}
 	}
 	c := &Call{
 		anInstruction: anInstruction{
@@ -108,6 +185,7 @@ func (f *Function) emitCall(target Value, args []Value, isDropError bool) *Call 
 		Args:        args,
 		user:        []User{},
 		isDropError: isDropError,
+		binding:     binding,
 	}
 
 	fixupUseChain(c)
