7d138d1e9bcce6366019102a826111e032e3af4e
WAY29 <41830147+WAY29@users.noreply.github.com>
2023-08-10 18:02:18 +0800

    feat(testcases): add funcCallYakTestCase

--- common/yak/antlr4yak/dap/generate_testcases.go
+++ common/yak/antlr4yak/dap/generate_testcases.go
@@ -5,19 +5,32 @@ import "os"
 var (
 	simpleYakTestCase = `a=1
 b=2`
+	funcCallYakTestCase = `func test() {
+println("hello")	
+}
+
+test()`
 )
 
 type GernerateFuncTyp func() (path string, removeFunc func())
 
-func GenerateSimpleYakTestCase() (path string, removeFunc func()) {
+func GenerateYakTestCase(raw string) (path string, removeFunc func()) {
 	f, err := os.CreateTemp("", "yak")
 	if err != nil {
 		return
 	}
-	f.WriteString(simpleYakTestCase)
+	f.WriteString(raw)
 	f.Close()
 	return f.Name(), func() {
 		f.Close()
 		os.Remove(f.Name())
 	}
+}
+
+func GenerateSimpleYakTestCase() (path string, removeFunc func()) {
+	return GenerateYakTestCase(simpleYakTestCase)
+}
+
+func GenerateFuncCallYakTestCase() (path string, removeFunc func()) {
+	return GenerateYakTestCase(funcCallYakTestCase)
 }
