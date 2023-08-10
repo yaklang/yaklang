5ecbb2ddd62936504bbeee6304bdf0cb04fd5831
v1ll4n <v1ll4n.a5k@gmail.com>
2023-08-10 17:35:53 +0800

    add arp builder test

---
+++ common/pcapx/builder_arp_test.go
@@ -0,0 +1,26 @@
+package pcapx
+
+import (
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/yaklang/yaklang/common/log"
+	"testing"
+)
+
+func TestArpBuilder(t *testing.T) {
+	packets, err := PacketBuilder(
+		WithArp_RequestAuto("8.8.8.8"),
+	)
+	if err != nil {
+		panic(err)
+	}
+	packet := gopacket.NewPacket(packets, layers.LayerTypeEthernet, gopacket.Default)
+	if packet.ErrorLayer() != nil {
+		log.Infof("error layer: %v", packet.ErrorLayer().Error())
+	}
+	if arp := packet.Layer(layers.LayerTypeARP); arp.LayerType() != layers.LayerTypeARP {
+		panic("build arp error")
+	}
+	fmt.Println(packet.String())
+}
