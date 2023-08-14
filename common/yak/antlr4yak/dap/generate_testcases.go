7d069dc048f698eead180523127d13ac566d7563
WAY29 <41830147+WAY29@users.noreply.github.com>
2023-08-14 17:39:33 +0800

    refactor: move testcase to _fixup dir
    fix: TestStopWithTarget

--- common/yak/antlr4yak/dap/generate_testcases.go
+++ common/yak/antlr4yak/dap/generate_testcases.go
@@ -1,38 +1,20 @@
 package dap
 
-import "os"
-
-var (
-	simpleYakTestCase = `a=1
-b=2`
-	funcCallYakTestCase = `func test() {
-println("hello")	
-}
-
-a = 1
-b = a + 1
-test()`
+import (
+	"path"
+	"runtime"
 )
 
-type GernerateFuncTyp func() (path string, removeFunc func())
+type GernerateFuncTyp func() (path string)
 
-func GenerateYakTestCase(raw string) (path string, removeFunc func()) {
-	f, err := os.CreateTemp("", "yak")
-	if err != nil {
-		return
-	}
-	f.WriteString(raw)
-	f.Close()
-	return f.Name(), func() {
-		f.Close()
-		os.Remove(f.Name())
-	}
-}
+var (
+	SimpleYakTestCase = "simple.yak"
+	FuncCallTestcase  = "func_call.yak"
+	IncrementTestcase = "increment.yak"
+)
 
-func GenerateSimpleYakTestCase() (path string, removeFunc func()) {
-	return GenerateYakTestCase(simpleYakTestCase)
-}
-
-func GenerateFuncCallYakTestCase() (path string, removeFunc func()) {
-	return GenerateYakTestCase(funcCallYakTestCase)
+func GetYakTestCasePath(p string) string {
+	_, file, _, _ := runtime.Caller(0)
+	dirPath := path.Dir(file)
+	return path.Join(dirPath, "_fixup", p)
 }
