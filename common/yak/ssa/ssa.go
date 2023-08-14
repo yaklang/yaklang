4c206c6d7878186ddb3045c3cffb77cf57cc276c
wlingze <lingze.wl@gmail.com>
2023-08-14 16:46:37 +0800

    refactor(type.go and inference_type.go)

--- common/yak/ssa/ssa.go
+++ common/yak/ssa/ssa.go
@@ -2,7 +2,6 @@ package ssa
 
 import (
 	"fmt"
-	"go/types"
 	"reflect"
 	"strings"
 	"sync"
@@ -12,36 +11,6 @@ import (
 	"github.com/yaklang/yaklang/common/yak/antlr4yak/yakvm"
 	"golang.org/x/exp/slices"
 )
-
-type Type types.Type
-type Types []Type
-
-// return true  if org != typs
-// return false if org == typs
-func (org Types) Compare(typs Types) bool {
-	if len(org) == 0 && len(typs) != 0 {
-		return true
-	}
-	return slices.CompareFunc(org, typs, func(org, typ Type) int {
-		if types.Identical(org, typ) {
-			return 0
-		}
-		return 1
-	}) != 0
-}
-
-func (t Types) String() string {
-	return strings.Join(
-		lo.Map(t, func(typ Type, _ int) string {
-			if typ == nil {
-				return "nil"
-			} else {
-				return typ.String()
-			}
-		}),
-		", ",
-	)
-}
 
 var (
 	ConstMap = make(map[any]*Const)
