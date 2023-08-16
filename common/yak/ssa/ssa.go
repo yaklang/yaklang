a8944ea0ac8efada77c5479aa49f9fc03ba5af2f
wlingze <lingze.wl@gmail.com>
2023-08-16 14:19:17 +0800

    refactor(const)

--- common/yak/ssa/ssa.go
+++ common/yak/ssa/ssa.go
@@ -1,8 +1,6 @@
 package ssa
 
 import (
-	"fmt"
-	"reflect"
 	"sync"
 
 	yak "github.com/yaklang/yaklang/common/yak/antlr4yak/parser"
@@ -415,37 +413,6 @@ type Field struct {
 	// i can add the map[users]update,
 	// to point what update value when user use this field
 
-}
-
-// ----------- Const
-// create const
-
-func NewConstWithUnary(i any, un int) *Const {
-	c := NewConst(i)
-	c.Unary = un
-	return c
-}
-func NewConst(i any) *Const {
-	// build new const
-	typestr := reflect.TypeOf(i).String()
-	if typestr == "int" {
-		i = int64(i.(int))
-		typestr = "int64"
-	}
-	// after update i
-	if c, ok := ConstMap[i]; ok {
-		return c
-	}
-	c := &Const{
-		user:  make([]User, 0),
-		value: i,
-		typ:   Types{basicTypesStr[typestr]},
-		str:   fmt.Sprintf("%v", i),
-	}
-	// const should same
-	// assert newConst(1) ==newConst(1)
-	ConstMap[i] = c
-	return c
 }
 
 var _ Node = (*Field)(nil)
