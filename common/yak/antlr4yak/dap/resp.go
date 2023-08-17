29e95cf179e2029dbe2b889469a9ec276623a27f
WAY29 <41830147+WAY29@users.noreply.github.com>
2023-08-17 10:15:31 +0800

    feat: add capabilities

--- common/yak/antlr4yak/dap/resp.go
+++ common/yak/antlr4yak/dap/resp.go
@@ -52,6 +52,8 @@ func (c *Client) ExpectInitializeResponseAndCapabilities(t *testing.T) *dap.Init
 		SupportsDataBreakpoints:          true,
 		SupportsDisassembleRequest:       true,
 		SupportTerminateDebuggee:         true,
+		SupportsSetVariable:              true,
+		SupportsSetExpression:            true,
 	}
 	if !reflect.DeepEqual(initResp.Body, wantCapabilities) {
 		t.Errorf("capabilities in initializeResponse: got %+v, want %v", pretty(initResp.Body), pretty(wantCapabilities))
