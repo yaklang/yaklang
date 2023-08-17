cde97985be13b306c98c5407d0d2826305d4b800
wlingze <lingze.wl@gmail.com>
2023-08-17 18:06:03 +0800

    feat(new type-system) simple base type and interfaceType containing "slice/map/struct"

--- common/yak/ssa/builder_object.go
+++ common/yak/ssa/builder_object.go
@@ -1,54 +1,76 @@
 package ssa
 
 import (
-	"github.com/davecgh/go-spew/spew"
-	"github.com/yaklang/yaklang/common/log"
 	yak "github.com/yaklang/yaklang/common/yak/antlr4yak/parser"
 )
 
-func (b *builder) buildSliceLiteral(ctx *yak.SliceLiteralContext) Value {
-	slm := ctx.ExpressionListMultiline()
-	if slm == nil {
-		return NewConst(make([]any, 0))
-	}
+// slice literal
+func (b *builder) buildSliceLiteral(stmt *yak.SliceLiteralContext) Value {
+	recover := b.SetRange(stmt.BaseParserRuleContext)
+	defer recover()
 
-	var ssaVals []Value
-	for _, exprIface := range slm.(*yak.ExpressionListMultilineContext).AllExpression() {
-		if exprIface == nil {
-			continue
-		}
-		expr := exprIface.(*yak.ExpressionContext)
-		ssaVals = append(ssaVals, b.buildExpression(expr))
+	_s := stmt.ExpressionListMultiline()
+	if _s == nil {
+		b.NewError(Warn, "slice literal not have expression")
+		return b.emitInterfaceBuildWithType(nil, NewConst(0), NewConst(0))
 	}
-	panic("UNIMPLEMENTED build SliceLiteral")
-	return NewConst(nil)
+	s, ok := _s.(*yak.ExpressionListMultilineContext)
+	if !ok {
+		b.NewError(Error, "slice literal expression parse error")
+		return nil
+	}
+	vs := b.buildExpressionListMultiline(s)
+	return b.CreateInterfaceWithVs(nil, vs)
 }
 
-func (b *builder) buildMapLiteral(ctx *yak.MapLiteralContext) Value {
-	rawPairs := ctx.MapPairs()
-	if rawPairs == nil {
-		return NewConst(make(map[string]any))
-	}
-	pairs, ok := ctx.MapPairs().(*yak.MapPairsContext)
-	if !ok {
-		log.Errorf("buildMapLiteral error! parse MapPairs failed: %v", ctx.GetText())
-		return NewConst(make(map[string]any))
-	}
+// slice typed literal
+func (b *builder) buildSliceTypedLiteral(stmt *yak.SliceTypedLiteralContext) Value {
+	recover := b.SetRange(stmt.BaseParserRuleContext)
+	defer recover()
 
-	var keyTypes Types
-	var valueTypes Types
-	_ = keyTypes
-	_ = valueTypes
-	for _, pair := range pairs.AllMapPair() {
-		p := pair.(*yak.MapPairContext)
-		if p == nil {
-			continue
+	return nil
+}
+
+// map literal
+func (b *builder) buildMapLiteral(stmt *yak.MapLiteralContext) Value {
+	recover := b.SetRange(stmt.BaseParserRuleContext)
+	defer recover()
+
+	if s := stmt.MapTypedLiteral(); s != nil {
+		if s, ok := s.(*yak.MapTypedLiteralContext); ok {
+			b.buildMapTypedLiteral(s)
+		} else {
+			b.NewError(Error, "map typed literal parse error")
 		}
-		keySSAVal := b.buildExpression(p.Expression(0).(*yak.ExpressionContext))
-		valueSSAVal := b.buildExpression(p.Expression(1).(*yak.ExpressionContext))
-		spew.Dump(keySSAVal, valueSSAVal)
 	}
 
-	panic("UNIMPLEMENTED build MapLiteral")
-	return NewConst(nil)
+	_s := stmt.MapPairs()
+	if _s == nil {
+		b.NewError(Warn, "map literal not have map pairs")
+		return b.emitInterfaceBuildWithType(nil, NewConst(0), NewConst(0))
+	}
+	s, ok := _s.(*yak.MapPairsContext)
+	if !ok {
+		b.NewError(Error, "map literal map pairs parse error")
+		return nil
+	}
+	allPair := s.AllMapPair()
+
+	// itf :=
+	keys := make([]Value, 0, len(allPair))
+	values := make([]Value, 0, len(allPair))
+	for _, p := range allPair {
+		p := p.(*yak.MapPairContext)
+		keys = append(keys, b.buildExpression(p.Expression(0).(*yak.ExpressionContext)))
+		values = append(values, b.buildExpression(p.Expression(1).(*yak.ExpressionContext)))
+	}
+	return b.CreateInterfaceWithVs(keys, values)
+}
+
+// map typed literal
+func (b *builder) buildMapTypedLiteral(stmt *yak.MapTypedLiteralContext) Value {
+	recover := b.SetRange(stmt.BaseParserRuleContext)
+	defer recover()
+
+	return nil
 }
