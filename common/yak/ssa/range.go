80a0e6687a844b256640ed6b45d3d09d583a492b
wlingze <lingze.wl@gmail.com>
2023-08-21 11:32:40 +0800

    fix(get endtoken line and column) handler with "\n" in end token

--- common/yak/ssa/range.go
+++ common/yak/ssa/range.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	"github.com/antlr/antlr4/runtime/Go/antlr/v4"
+	"github.com/yaklang/yaklang/common/yak/antlr4yak/yakast"
 )
 
 type canStartStopToken interface {
@@ -16,12 +17,13 @@ func (b *builder) SetRange(token canStartStopToken) func() {
 	pos := b.currtenPos
 	// fmt.Printf("debug %v\n", b.GetText())
 	source := strings.Split(token.GetText(), "\n")[0]
+	endline, endcolumn := yakast.GetEndPosition(token.GetStop())
 	b.currtenPos = &Position{
 		SourceCode:  source,
 		StartLine:   token.GetStart().GetLine(),
 		StartColumn: token.GetStart().GetColumn(),
-		EndLine:     token.GetStop().GetLine(),
-		EndColumn:   token.GetStop().GetColumn(),
+		EndLine:     endline,
+		EndColumn:   endcolumn,
 	}
 
 	return func() {
