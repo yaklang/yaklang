a3fb6dda2174b79b1a77def11b18b380a40966f7
v1ll4n <v1ll4n.a5k@gmail.com>
2023-08-21 13:21:27 +0800

    fix sm2 encrypt

--- common/yakgrpc/grpc_fuzzer_hotpath_test.go
+++ common/yakgrpc/grpc_fuzzer_hotpath_test.go
@@ -2,7 +2,9 @@ package yakgrpc
 
 import (
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/yak/yaklib/codec"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 	"net/http"
 	"testing"
@@ -38,5 +40,66 @@ func TestGRPCMUSTPASS_HTTPFuzzer_HotPatch(t *testing.T) {
 	}
 	if count != 10 {
 		t.Fatalf("expect 10, got %v", count)
+	}
+}
+
+/*
+handle = func(param) {
+a = codec.Sm2EncryptC1C3C2("0487c856a4a19e2cdc4271e839ea0ca3f8e6622f5de3a3190bb339641e225d28ef3d26348621d373d40c750af60e8dfd2154f4fd1d43fc0405faeeb15235715512", param)~
+dump(a)
+
+return "aaa" + sprintf("_origin(%v)", param)
+}
+*/
+
+func TestGRPCMUSTPASS_HTTPFuzzer_HotPatch2(t *testing.T) {
+	pri, key, err := codec.GenerateSM2PrivateKeyHEX()
+	if err != nil {
+		panic(err)
+	}
+	data, _ := codec.SM2EncryptC1C3C2(key, []byte("aaa"))
+	spew.Dump(data)
+	dec, _ := codec.SM2DecryptC1C3C2(pri, data)
+	spew.Dump(dec)
+	if string(dec) != "aaa" {
+		panic("dec c1c3c2 error")
+	}
+	client, err := NewLocalClient()
+	if err != nil {
+		panic(err)
+	}
+
+	host, port := utils.DebugMockHTTPHandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("abc"))
+	})
+	target := utils.HostPort(host, port)
+	recv, err := client.HTTPFuzzer(utils.TimeoutContextSeconds(10), &ypb.FuzzerRequest{
+		Request: "GET / HTTP/1.1\r\nHost: " + target + "\r\n\r\n{{yak(handle|{{params(a)}})}}",
+		HotPatchCode: `
+handle = func(param) {
+dump("************************************" * 2)
+a = codec.Sm2EncryptC1C3C2("0487c856a4a19e2cdc4271e839ea0ca3f8e6622f5de3a3190bb339641e225d28ef3d26348621d373d40c750af60e8dfd2154f4fd1d43fc0405faeeb15235715512", "aaa")~
+dump(a)
+
+return "aaa" + sprintf("_origin(%v)", param)
+}
+`,
+		ForceFuzz: true,
+	})
+	if err != nil {
+		t.Fatalf("expect nil, got %v", err)
+	}
+	count := 0
+	for {
+		rsp, err := recv.Recv()
+		if err != nil {
+			break
+		}
+		count++
+		fmt.Println(string(rsp.RequestRaw))
+		fmt.Println(string(rsp.ResponseRaw))
+	}
+	if count != 1 {
+		t.Fatalf("expect 1, got %v", count)
 	}
 }
