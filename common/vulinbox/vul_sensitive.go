c6205028352c287cf4806e33afa6a8616bb5b9ad
VillanCh <188228725@qq.com>
2023-08-25 23:56:10 +0800

    add vulinbox server to test clone and git repos

--- common/vulinbox/vul_sensitive.go
+++ common/vulinbox/vul_sensitive.go
@@ -1,15 +1,35 @@
 package vulinbox
 
 import (
+	"archive/zip"
+	"bytes"
 	"embed"
+	"errors"
+	"github.com/go-git/go-billy/v5/osfs"
+	"github.com/go-git/go-git/v5/plumbing/format/pktline"
+	"github.com/go-git/go-git/v5/plumbing/protocol/packp"
+	"github.com/go-git/go-git/v5/plumbing/transport"
+	"github.com/yaklang/yaklang/common/consts"
 	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils/ziputil"
+	"os"
+	"path/filepath"
+
+	gitServer "github.com/go-git/go-git/v5/plumbing/transport/server"
 	"io"
 	"net/http"
 	"path"
+	"strings"
 )
 
 //go:embed sensitivefs
 var _sensitiveFS embed.FS
+
+//go:embed fakegit/website.zip
+var _fakeGitWebsite []byte
+
+//go:embed fakegit/website-repository.git.zip
+var _fakeGitRepository []byte
 
 func GetSensitiveFile(name string) []byte {
 	f, err := _sensitiveFS.Open(path.Join("sensitivefs", name))
@@ -39,6 +59,16 @@ func (s *VulinServer) registerSensitive() {
 
 		{path}?/swagger/index.html
 	*/
+
+	zipGitFS, err := zip.NewReader(bytes.NewReader(_fakeGitWebsite), int64(len(_fakeGitWebsite)))
+	if err != nil {
+		log.Errorf("cannot open zip file: %s", err)
+	}
+
+	zipGitRepositoryFS, err := zip.NewReader(bytes.NewReader(_fakeGitRepository), int64(len(_fakeGitRepository)))
+	if err != nil {
+		log.Errorf("cannot open zip file: %s", err)
+	}
 
 	var sensitiveGroup = r.PathPrefix("/sensitive").Name("敏感信息与敏感文件泄漏").Subrouter()
 	var swaggerGroup = r.PathPrefix("/swagger").Name("敏感信息与敏感文件泄漏（Swagger）").Subrouter()
@@ -70,6 +100,15 @@ func (s *VulinServer) registerSensitive() {
 			},
 			RiskDetected: true,
 		},
+		{
+			Path:  `/website/`,
+			Title: "Git Repository 泄漏",
+			Handler: func(writer http.ResponseWriter, request *http.Request) {
+				writer.Header().Set("Location", "/git/website/index.html")
+				writer.WriteHeader(302)
+			},
+			RiskDetected: true,
+		},
 	}
 	for _, v := range vuls {
 		addRouteWithVulInfo(sensitiveGroup, v)
@@ -84,4 +123,165 @@ func (s *VulinServer) registerSensitive() {
 		},
 		RiskDetected: true,
 	})
+	fakeGitSubrouter := s.router.PathPrefix("/git/")
+	fakeGitSubrouter.PathPrefix("/website/").HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		if zipGitFS == nil {
+			Failed(writer, request, "Create FAKE GIT Website FAILED")
+			return
+		}
+
+		request.RequestURI = strings.TrimPrefix(request.RequestURI, `/git/`)
+		request.URL.Path = strings.TrimPrefix(request.URL.Path, `/git/`)
+
+		filePath := request.URL.Path
+		if filePath == "website/" {
+			filePath = "website/index.html"
+		}
+
+		var fp, err = zipGitFS.Open(filePath)
+		if err != nil {
+			Failed(writer, request, "Cannot found file(%v) in fake git website", request.URL.Path)
+			return
+		}
+		defer fp.Close()
+		raw, _ := io.ReadAll(fp)
+
+		if strings.Contains(filePath, ".git/") {
+			writer.Header().Set("Content-Type", `text/plain`)
+		} else {
+			writer.Header().Set("Content-Type", `text/html`)
+		}
+		writer.Write(raw)
+	})
+
+	var localBareRepos = filepath.Join(consts.GetDefaultYakitBaseTempDir(), "bare-repos")
+	os.RemoveAll(localBareRepos)
+	ziputil.DeCompressFromRaw(_fakeGitRepository, localBareRepos)
+	var localBareReposPath = filepath.Join(localBareRepos, "website-repository.git")
+
+	s.router.PathPrefix("/gitserver/website-repository.git/").HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		request.URL.Path = strings.TrimPrefix(request.URL.Path, "/gitserver/")
+		request.RequestURI = strings.TrimPrefix(request.RequestURI, "/gitserver/")
+
+		rootDir := "website-repository.git"
+		fileName := strings.TrimPrefix(request.URL.Path, "/")
+		log.Infof("fetch %v: %s", rootDir, fileName)
+		// website-repository.git
+		serviceParams := request.URL.Query().Get("service")
+		log.Infof("service: %s to %v", serviceParams, fileName)
+		if serviceParams == "git-upload-pack" && strings.HasSuffix(fileName, `/info/refs`) {
+			s.gitInfoRefs(localBareReposPath, writer, request)
+			return
+		}
+
+		if strings.HasSuffix(request.URL.Path, `/git-upload-pack`) && request.Method == "POST" {
+			s.gitUploadPack(localBareReposPath, writer, request)
+			return
+		}
+
+		//if strings.HasSuffix(request.URL.Path, `/git-receive-pack`) && request.Method == "POST" {
+		//	s.gitReceivePack(localBareRepos, writer, request)
+		//	return
+		//}
+		filePath := path.Join(rootDir, fileName)
+		var fp, err = zipGitRepositoryFS.Open(filePath)
+		if err != nil {
+			writer.WriteHeader(404)
+			return
+		}
+		defer fp.Close()
+		raw, _ := io.ReadAll(fp)
+
+		if strings.Contains(filePath, ".git/") {
+			writer.Header().Set("Content-Type", `text/plain`)
+		} else {
+			writer.Header().Set("Content-Type", `text/html`)
+		}
+		writer.Write(raw)
+	})
+}
+
+func (d *VulinServer) gitInfoRefs(localDir string, w http.ResponseWriter, r *http.Request) {
+	repo := localDir
+
+	w.Header().Set("content-type", "application/x-git-upload-pack-advertisement")
+
+	ep, err := transport.NewEndpoint("/")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Printf("git: %s", err)
+		return
+	}
+
+	billyfs := osfs.New(repo)
+	loader := gitServer.NewFilesystemLoader(billyfs)
+	srv := gitServer.NewServer(loader)
+	session, err := srv.NewUploadPackSession(ep, nil)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Printf("git: %s", err)
+		return
+	}
+
+	ar, err := session.AdvertisedReferencesContext(r.Context())
+	if errors.Is(err, transport.ErrRepositoryNotFound) {
+		http.Error(w, err.Error(), 404)
+		return
+	} else if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	ar.Prefix = [][]byte{
+		[]byte("# service=git-upload-pack"),
+		pktline.Flush,
+	}
+
+	if err = ar.Encode(w); err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Printf("git: %s", err)
+		return
+	}
+}
+
+func (d *VulinServer) gitUploadPack(localDir string, w http.ResponseWriter, r *http.Request) {
+	repo := localDir
+	w.Header().Set("content-type", "application/x-git-upload-pack-result")
+	upr := packp.NewUploadPackRequest()
+	err := upr.Decode(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		log.Printf("git: %s", err)
+		return
+	}
+
+	ep, err := transport.NewEndpoint("/")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Printf("git: %s", err)
+		return
+	}
+
+	billyfs := osfs.New(repo)
+	loader := gitServer.NewFilesystemLoader(billyfs)
+	svr := gitServer.NewServer(loader)
+	session, err := svr.NewUploadPackSession(ep, nil)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Printf("git: %s", err)
+		return
+	}
+
+	res, err := session.UploadPack(r.Context(), upr)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Printf("git: %s", err)
+		return
+	}
+
+	if err = res.Encode(w); err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Printf("git: %s", err)
+		return
+	}
 }
