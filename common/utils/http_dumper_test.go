9fcfc2b9c966d183728d3f18d512f0399f7f93b2
VillanCh <188228725@qq.com>
2023-08-29 00:40:24 +0800

    add dumper test

--- common/utils/http_dumper_test.go
+++ common/utils/http_dumper_test.go
@@ -64,3 +64,60 @@ func TestHTTPRequestDumper_Stream_BodyIsSmall(t *testing.T) {
 		t.Fatal("body should be abcccddef[SP][SP][SP]")
 	}
 }
+
+func TestHTTPResponseDumper_BodyIsLager(t *testing.T) {
+	packet := `HTTP/1.1 200 OK` + CRLF +
+		`Server: Test-ABC` + CRLF +
+		`Content-Length: 3` + CRLF + CRLF + "abccccddef"
+	req, err := ReadHTTPRequestFromBytes([]byte(packet))
+	if err != nil {
+		panic(err)
+	}
+	if req.ContentLength == 3 {
+		t.Fatal("ContentLength should be 10")
+	}
+}
+
+func TestHTTPResponseDumper_BodyIsSmall(t *testing.T) {
+	packet := `HTTP/1.1 200 OK` + CRLF +
+		`Server: Test-ABC` + CRLF +
+		`Content-Length: 13` + CRLF + CRLF + "abccccddef"
+	req, err := ReadHTTPRequestFromBytes([]byte(packet))
+	if err != nil {
+		panic(err)
+	}
+	if req.ContentLength == 13 {
+		t.Fatal("ContentLength should be 10")
+	}
+}
+
+func TestHTTPResponseDumper_Stream_BodyIsLager(t *testing.T) {
+	packet := `HTTP/1.1 200 OK` + CRLF +
+		`Server: Test-ABC` + CRLF +
+		`Content-Length: 3` + CRLF + CRLF + "abccccddef"
+	req, err := ReadHTTPRequestFromBufioReader(bufio.NewReader(bytes.NewBufferString(packet)))
+	if err != nil {
+		panic(err)
+	}
+	if req.ContentLength != 3 {
+		t.Fatal("ContentLength should be 3")
+	}
+}
+
+func TestHTTPResponseDumper_Stream_BodyIsSmall(t *testing.T) {
+	packet := `HTTP/1.1 200 OK` + CRLF +
+		`Server: Test-ABC` + CRLF +
+		`Content-Length: 13` + CRLF + CRLF + "abccccddef"
+	req, err := ReadHTTPRequestFromBufioReader(bufio.NewReader(bytes.NewBufferString(packet)))
+	if err != nil {
+		panic(err)
+	}
+	if req.ContentLength != 13 {
+		t.Fatal("ContentLength should be 13")
+	}
+	raw, _ := io.ReadAll(req.Body)
+	if string(raw) != "abccccddef   " && len(string(raw)) != 13 {
+		spew.Dump(raw)
+		t.Fatal("body should be abcccddef[SP][SP][SP]")
+	}
+}
