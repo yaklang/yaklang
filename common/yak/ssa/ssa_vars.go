86875d7d31abf87f6f0ea1d21f007f2fa0b15846
wlz <lingze.wl@gmail.com>
2023-08-30 16:59:17 +0800

    feat(ssa InspectVariableResult) add MustTypes/MustValue field

--- common/yak/ssa/ssa_vars.go
+++ common/yak/ssa/ssa_vars.go
@@ -1,9 +1,12 @@
 package ssa
 
 type InspectVariableResult struct {
-	VariableName   string
+	VariableName string
+	// values         []Value
 	ProbablyTypes  []string
 	ProbablyValues []string
+	MustTypes      []string
+	MustValue      []string
 }
 
 func (r *InspectVariableResult) Merge(other *InspectVariableResult) {
@@ -12,6 +15,23 @@ func (r *InspectVariableResult) Merge(other *InspectVariableResult) {
 	}
 	r.ProbablyTypes = append(r.ProbablyTypes, other.ProbablyTypes...)
 	r.ProbablyValues = append(r.ProbablyValues, other.ProbablyValues...)
+	r.MustTypes = append(r.MustTypes, other.MustTypes...)
+	r.MustValue = append(r.MustValue, other.MustValue...)
+}
+
+func (p *Program) InspectVariableLast(varName string) *InspectVariableResult {
+	var result = new(InspectVariableResult)
+	for _, pkg := range p.Packages {
+		for _, funcIns := range pkg.Funcs {
+			if res, ok := funcIns.symbolTable[varName]; ok {
+				last := res[len(res)-1]
+				result.ProbablyTypes = append(result.ProbablyTypes, last.GetType().String())
+				result.ProbablyValues = append(result.ProbablyValues, last.String())
+			}
+		}
+	}
+	result.VariableName = varName
+	return result
 }
 
 func (p *Program) InspectVariable(varName string) *InspectVariableResult {
@@ -40,14 +60,26 @@ func (f *Function) InspectVariable(varName string) *InspectVariableResult {
 	}
 	var probablyTypes []string
 	var probablyValue []string
+	var mustValue []string
+	var mustTypes []string
+	values := make([]Value, 0)
 	for _, v := range res {
+		values = append(values, v)
 		probablyValue = append(probablyValue, v.String())
-		valType := v.GetType()
-		if valType != nil {
-			probablyTypes = append(probablyTypes, valType.String())
+		probablyTypes = append(probablyTypes, v.GetType().String())
+		if inst, ok := v.(Instruction); ok {
+			reachable := inst.GetBlock().Reachable()
+			if reachable == 1 {
+				mustTypes = append(mustTypes, v.GetType().String())
+				mustValue = append(mustValue, v.String())
+			}
 		}
 	}
 	result.ProbablyTypes = probablyTypes
 	result.ProbablyValues = probablyValue
+	result.MustTypes = mustTypes
+	result.MustValue = mustValue
+	_ = values
+	// result.values = values
 	return result
 }
