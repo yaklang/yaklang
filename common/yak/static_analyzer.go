9795802bed84006ae9c1db64825e3edc0974c326
wlz <lingze.wl@gmail.com>
2023-08-30 16:05:56 +0800

    feat(yak static analyzer) add ssa parse and error

--- common/yak/static_analyzer.go
+++ common/yak/static_analyzer.go
@@ -2,12 +2,15 @@ package yak
 
 import (
 	"fmt"
+	"os"
+	"regexp"
+
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/yak/antlr4yak/yakast"
+	"github.com/yaklang/yaklang/common/yak/ssa"
+	"github.com/yaklang/yaklang/common/yak/ssa4yak"
 	"github.com/yaklang/yaklang/common/yak/yaklang"
-	"os"
-	"regexp"
 )
 
 type StaticAnalyzeResult struct {
@@ -30,31 +33,56 @@ func AnalyzeStaticYaklang(r interface{}) []*StaticAnalyzeResult {
 }
 
 func AnalyzeStaticYaklangEx(r interface{}, strictMode bool) []*StaticAnalyzeResult {
-	code := utils.InterfaceToBytes(r)
+	code := string(utils.InterfaceToBytes(r))
+	var results []*StaticAnalyzeResult
+
+	// compiler
 	newEngine := yaklang.New()
 	newEngine.SetStrictMode(strictMode)
-	opcodes, err := newEngine.Compile(string(code))
+	_, err := newEngine.Compile(code)
 	if err != nil {
 		switch ret := err.(type) {
 		case yakast.YakMergeError:
-			var results []*StaticAnalyzeResult
 			for _, e := range ret {
 				results = append(results, &StaticAnalyzeResult{
 					Message:         fmt.Sprintf("基础语法错误（Syntax Error）：%v", e.Message),
 					Severity:        "error",
 					StartLineNumber: e.StartPos.LineNumber,
-					StartColumn:     e.StartPos.ColumnNumber,
+					StartColumn:     e.StartPos.ColumnNumber + 1,
 					EndLineNumber:   e.EndPos.LineNumber,
-					EndColumn:       e.EndPos.ColumnNumber,
+					EndColumn:       e.EndPos.ColumnNumber + 2,
 					RawMessage:      e.Error(),
 				})
 			}
-			return results
 		default:
 			log.Error("静态分析失败：Yaklang 返回错误不标准")
-			return nil
 		}
 	}
-	_ = opcodes
-	return nil
+
+	// ssa
+	prog := ssa4yak.ParseSSA(code)
+	for _, err := range prog.GetErrors() {
+		var severity string
+		switch err.Kind {
+		case ssa.Warn:
+			severity = "warning"
+		case ssa.Error:
+			severity = "error"
+		}
+		results = append(results, &StaticAnalyzeResult{
+			Message:         err.Message,
+			Severity:        severity,
+			StartLineNumber: err.Pos.StartLine,
+			StartColumn:     err.Pos.StartColumn + 1,
+			EndLineNumber:   err.Pos.EndLine,
+			EndColumn:       err.Pos.EndColumn + 2,
+			RawMessage:      err.String(),
+		})
+	}
+	// debug printf json
+	for _, result := range results {
+		fmt.Printf("%+v\n", result)
+	}
+
+	return results
 }
