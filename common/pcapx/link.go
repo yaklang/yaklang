79801bb675099890664271019712c95a2b6223e3
BaiMeow <baishu2003@outlook.com>
2023-08-30 16:15:41 +0800

    feat(pcapx): link layer cache

--- common/pcapx/link.go
+++ common/pcapx/link.go
@@ -54,26 +54,34 @@ func GetPublicToServerLinkLayerIPv4() (*layers.Ethernet, error) {
 	if ethIPv4ToServer != nil {
 		return ethIPv4ToServer, nil
 	}
-	return GetPublicLinkLayer(layers.EthernetTypeIPv4, true)
+	var err error
+	ethIPv4ToServer, err = GetPublicLinkLayer(layers.EthernetTypeIPv4, true)
+	return ethIPv4ToServer, err
 }
 
 func GetPublicToServerLinkLayerIPv6() (*layers.Ethernet, error) {
 	if ethIPv6ToServer != nil {
 		return ethIPv6ToServer, nil
 	}
-	return GetPublicLinkLayer(layers.EthernetTypeIPv6, true)
+	var err error
+	ethIPv6ToServer, err = GetPublicLinkLayer(layers.EthernetTypeIPv6, true)
+	return ethIPv6ToServer, err
 }
 
 func GetPublicToClientLinkLayerIPv4() (*layers.Ethernet, error) {
 	if ethIPv4 != nil {
 		return ethIPv4, nil
 	}
-	return GetPublicLinkLayer(layers.EthernetTypeIPv4, false)
+	var err error
+	ethIPv4, err = GetPublicLinkLayer(layers.EthernetTypeIPv4, false)
+	return ethIPv4, err
 }
 
 func GetPublicToClientLinkLayerIPv6() (*layers.Ethernet, error) {
 	if ethIPv6 != nil {
 		return ethIPv6, nil
 	}
-	return GetPublicLinkLayer(layers.EthernetTypeIPv6, false)
+	var err error
+	ethIPv6, err = GetPublicLinkLayer(layers.EthernetTypeIPv6, false)
+	return ethIPv6, err
 }
