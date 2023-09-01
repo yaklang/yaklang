a1953d98212604cd11d767e3a7b1accbf5e37e20
v1ll4n <v1ll4n.a5k@gmail.com>
2023-09-01 11:41:31 +0800

    add sca fs scan support and testcase

--- common/yak/yaktest/mustpass/mustpass_base_test.go
+++ common/yak/yaktest/mustpass/mustpass_base_test.go
@@ -49,7 +49,7 @@ func init() {
 }
 
 func TestMustPassDebug(t *testing.T) {
-	var debugName = "mitm_connect_assert.yak"
+	var debugName = "git_to_sca.yak"
 	var debugCases [][]string
 	for k, v := range files {
 		if k == debugName {
