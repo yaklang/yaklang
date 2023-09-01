a8bbf9add0701e5ca3ebda674eb30c0c40f42990
WAY29 <41830147+WAY29@users.noreply.github.com>
2023-09-01 13:14:15 +0800

    fix: circle call

--- common/yak/antlr4yak/dap/prettyprint.go
+++ common/yak/antlr4yak/dap/prettyprint.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"unsafe"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/yaklang/yaklang/common/yak/antlr4yak/yakvm"
 )
 
@@ -19,25 +20,32 @@ func SafeReflectValue(refV reflect.Value) reflect.Value {
 	return refV
 }
 
-func SafeReflectStructFieldInterface(refV reflect.Value, field reflect.Value) interface{} {
+func SafeReflectStructField(refV reflect.Value, field reflect.Value) reflect.Value {
+	var f reflect.Value
 	if refV.CanAddr() {
-		field := reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem()
-		return field.Interface()
+		f = reflect.NewAt(field.Type(), unsafe.Pointer(field.UnsafeAddr())).Elem()
 	}
-	return nil
+	return f
 }
 
 func AsDebugString(i interface{}, raws ...bool) string {
+	raw := false
+	if len(raws) > 0 && raws[0] {
+		raw = true
+	}
+	return asDebugString(i, raw, make(map[uintptr]struct{}))
+}
+
+func asDebugString(i interface{}, raw bool, pointers map[uintptr]struct{}) (ret string) {
+	spew.Sdump()
+
 	refV := reflect.ValueOf(i)
 	if !refV.IsValid() {
 		return fmt.Sprintf("%#v", i)
 	}
 	typ := refV.Type()
 	kind := typ.Kind()
-	raw := false
-	if len(raws) > 0 && raws[0] {
-		raw = true
-	}
+
 	// todo: 美化输出,主要使用省略号来省略过长的字符串
 	_ = raw
 
@@ -55,7 +63,7 @@ func AsDebugString(i interface{}, raws ...bool) string {
 		}
 		content := make([]string, length)
 		for i := 0; i < length; i++ {
-			content[i] = AsDebugString(refV.Index(i).Interface())
+			content[i] = asDebugString(refV.Index(i).Interface(), raw, pointers)
 		}
 		return fmt.Sprintf("%T{%s}", i, strings.Join(content, ", "))
 	} else if kind == reflect.String {
@@ -67,15 +75,22 @@ func AsDebugString(i interface{}, raws ...bool) string {
 			return fmt.Sprintf("%v", keys[i]) < fmt.Sprintf("%v", keys[j])
 		})
 		for i, key := range keys {
-			content[i] = fmt.Sprintf("%s: %s", AsDebugString(key.Interface()), AsDebugString(refV.MapIndex(key).Interface()))
+			content[i] = fmt.Sprintf("%s: %s", asDebugString(key.Interface(), raw, pointers), asDebugString(refV.MapIndex(key).Interface(), raw, pointers))
 		}
 		return fmt.Sprintf("%T{%s}", i, strings.Join(content, ", "))
 	} else if f, ok := i.(*yakvm.Function); ok {
 		return fmt.Sprintf("%s", f.String())
 	} else if kind == reflect.Ptr {
+		// fix circle call
+		p := refV.Pointer()
+		if _, ok := pointers[p]; ok {
+			return "<Already printed>"
+		} else {
+			pointers[p] = struct{}{}
+		}
 		elem := refV.Elem()
 		if elem.IsValid() {
-			return fmt.Sprintf("&%s", AsDebugString(elem.Interface()))
+			return fmt.Sprintf("&%s", asDebugString(elem.Interface(), raw, pointers))
 		} else {
 			return fmt.Sprintf("%#v", i)
 		}
@@ -89,9 +104,10 @@ func AsDebugString(i interface{}, raws ...bool) string {
 		for i := 0; i < refV.NumField(); i++ {
 			field := refV.Field(i)
 			fieldStr := "<Unavailable>"
-			iField := SafeReflectStructFieldInterface(refV, field)
+			newField := SafeReflectStructField(refV, field)
+			iField := newField.Interface()
 			if iField != nil {
-				fieldStr = AsDebugString(iField)
+				fieldStr = asDebugString(iField, raw, pointers)
 			}
 			content[i] = fmt.Sprintf("%s: %s", typ.Field(i).Name, fieldStr)
 		}
