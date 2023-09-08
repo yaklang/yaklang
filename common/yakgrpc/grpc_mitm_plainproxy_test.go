53a1e6bd0ce87b8d45a9a7c53de4686afdcb5a80
v1ll4n <v1ll4n.a5k@gmail.com>
2023-09-08 12:55:07 +0800

    donot merge header

--- common/yakgrpc/grpc_mitm_plainproxy_test.go
+++ common/yakgrpc/grpc_mitm_plainproxy_test.go
@@ -10,7 +10,104 @@ import (
 	"net/http"
 	"strings"
 	"testing"
+	"time"
 )
+
+func TestDemoServer(t *testing.T) {
+	mockHost, mockPort := utils.DebugMockHTTPHandlerFuncContext(context.Background(), func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Add("Set-Cookie", "abc="+"aaa")
+		writer.Header().Add("Set-Cookie", "ddd=111"+"bbb")
+		writer.Write([]byte("hello"))
+	})
+	fmt.Println("http://" + utils.HostPort(mockHost, mockPort))
+	time.Sleep(time.Hour)
+}
+
+func TestGRPCMUSTPASS_MITM_PlainProxy2(t *testing.T) {
+	var ctx, cancel = context.WithCancel(utils.TimeoutContextSeconds(5))
+	defer cancel()
+
+	token1 := utils.RandStringBytes(10)
+	token2 := utils.RandStringBytes(10)
+	mockHost, mockPort := utils.DebugMockHTTPHandlerFuncContext(ctx, func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Add("Set-Cookie", "abc="+token1)
+		writer.Header().Add("Set-Cookie", "ddd=111"+token2)
+		writer.Write([]byte("hello"))
+	})
+
+	mitmPort := utils.GetRandomAvailableTCPPort()
+	client, err := NewLocalClient()
+	if err != nil {
+		panic(err)
+	}
+	stream, err := client.MITM(ctx)
+	if err != nil {
+		panic(err)
+	}
+	stream.Send(&ypb.MITMRequest{
+		Host: "127.0.0.1",
+		Port: uint32(mitmPort),
+	})
+	token := utils.RandStringBytes(19)
+	for {
+		data, err := stream.Recv()
+		if err != nil {
+			break
+		}
+		if data.GetMessage().GetIsMessage() {
+			var msg = string(data.GetMessage().GetMessage())
+			fmt.Println(msg)
+			if strings.Contains(msg, "starting mitm server") {
+				var packet = `GET http://` + utils.HostPort(mockHost, mockPort) + `/mh/zwww/hlwjjg/index.jsp?a=` + token + ` HTTP/1.1
+Host: ` + utils.HostPort(mockHost, mockPort) + `
+Proxy-Connection: keep-alive
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
+Referer: http://` + utils.HostPort(mockHost, mockPort) + `/mh/index.jsp?ticket=ST-116464-K9vybT12B8ngtOdc5vYmTj0Cie0-host-10-18-127-7
+Accept-Encoding: gzip, deflate
+Accept-Language: zh-CN,zh;q=0.9
+Cookie: name=value; JSESSIONID=ChIBvh-RZPqDGFAv9DOn-0BVpVvzy73DGnYA
+Connection: close
+
+`
+				packetBytes := lowhttp.FixHTTPRequest([]byte(packet))
+				_, err := yak.Execute(`
+host, port = str.ParseStringToHostPort(target)~
+conn = tcp.Connect(host, port)~
+conn.Write(packet)
+sleep(0.5)
+conn.Close()
+`, map[string]any{
+					"packet": string(packetBytes),
+					"target": utils.HostPort("127.0.0.1", mitmPort),
+				})
+				if err != nil {
+					t.Fatal(err)
+				}
+				cancel()
+			}
+		}
+	}
+
+	data, err := client.QueryHTTPFlows(context.Background(), &ypb.QueryHTTPFlowRequest{Keyword: token})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	request := string(data.GetData()[0].Request)
+	response := string(data.GetData()[0].Response)
+	fmt.Println(request)
+	if utils.MatchAnyOfSubString(request, "Proxy-Connection: ", "GET http://", "GET https://") {
+		fmt.Println(request)
+		t.Fatal("request should not contains proxy connection")
+	}
+
+	fmt.Println(string(response))
+	if strings.Count(response, `Set-Cookie: `) != 2 {
+		t.Fatal("response should contains two set-cookie")
+	}
+}
 
 func TestGRPCMUSTPASS_MITM_PlainProxy(t *testing.T) {
 	var ctx, cancel = context.WithCancel(utils.TimeoutContextSeconds(5))
