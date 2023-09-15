2013849196cba70e459fb1e9088085ab484e39d4
go0p <gp827782797@qq.com>
2023-09-15 23:42:11 +0800

    add(wsm) add yakurl cache

--- common/yak/yakurl/iface.go
+++ common/yak/yakurl/iface.go
@@ -1,8 +1,11 @@
 package yakurl
 
 import (
+	"context"
 	"github.com/yaklang/yaklang/common/wsm"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"sync"
+	"time"
 )
 
 type Action interface {
@@ -14,9 +17,47 @@ type Action interface {
 	Do(params *ypb.RequestYakURLParams) (*ypb.RequestYakURLResponse, error)
 }
 
-type DefaultActionFactory struct{}
+type ActionService struct {
+	actions map[string]Action
+	mutex   sync.Mutex
+	ctx     context.Context
+	cancel  context.CancelFunc
+}
 
-func (f DefaultActionFactory) CreateAction(schema string) Action {
+var actionServiceInstance *ActionService
+var once sync.Once
+
+func GetActionService() *ActionService {
+	once.Do(func() {
+		ctx, cancel := context.WithCancel(context.Background())
+		actionServiceInstance = &ActionService{
+			actions: make(map[string]Action),
+			ctx:     ctx,
+			cancel:  cancel,
+		}
+		go actionServiceInstance.clearCachePeriodically()
+	})
+	return actionServiceInstance
+}
+
+func (s *ActionService) Stop() {
+	s.cancel()
+}
+
+func (s *ActionService) GetAction(schema string) Action {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	action, exists := s.actions[schema]
+	if !exists {
+		action = s.CreateAction(schema)
+		if action != nil {
+			s.actions[schema] = action
+		}
+	}
+	return action
+}
+
+func (s *ActionService) CreateAction(schema string) Action {
 	switch schema {
 	case "file":
 		return &fileSystemAction{}
@@ -27,4 +68,24 @@ func (f DefaultActionFactory) CreateAction(schema string) Action {
 	default:
 		return nil
 	}
+}
+
+func (s *ActionService) clearCachePeriodically() {
+	ticker := time.NewTicker(10 * time.Minute)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			s.clearCache()
+		case <-s.ctx.Done():
+			return
+		}
+	}
+}
+
+func (s *ActionService) clearCache() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.actions = make(map[string]Action)
 }
