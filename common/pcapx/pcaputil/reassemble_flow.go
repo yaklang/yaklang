d896b04d7e2f079fecd3a1dbc291734243928001
v1ll4n <v1ll4n.a5k@gmail.com>
2023-09-19 18:04:26 +0800

    feature: callback api

---
+++ common/pcapx/pcaputil/reassemble_flow.go
@@ -0,0 +1,146 @@
+package pcaputil
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/gopacket/layers"
+	"github.com/yaklang/yaklang/common/log"
+	"sync"
+)
+
+// TrafficFrame is a tcp frame
+type TrafficFrame struct {
+	ConnHash string // connection local -> remote
+	Seq      uint32
+	Payload  []byte
+	Done     bool
+
+	Connection *TrafficConnection
+}
+
+// TrafficFlow is a tcp flow
+// lifecycle is created -> data-feeding -> closed(fin/rst/timeout)
+// OnFrame: frame -> flow -> connection
+// OnClosed: reason(fin/rst/timeout) -> flow
+// OnCreated: flow created
+type TrafficFlow struct {
+	// ClientConn
+	ClientConn *TrafficConnection
+	ServerConn *TrafficConnection
+	Hash       string
+	Index      uint64
+
+	ctx    context.Context
+	cancel context.CancelFunc
+
+	pool *TrafficPool
+
+	frames []*TrafficFrame
+
+	createdOnce *sync.Once
+	closedOnce  *sync.Once
+
+	onCloseHandler         func(reason TrafficFlowCloseReason, frame *TrafficFlow)
+	onDataFrameReassembled func(*TrafficFlow, *TrafficConnection, *TrafficFrame)
+	onDataFrameArrived     func(*TrafficFlow, *TrafficConnection, *TrafficFrame)
+}
+
+func (t *TrafficFlow) IsClosed() bool {
+	select {
+	case <-t.ctx.Done():
+		t.triggerCloseEvent(TrafficFlowCloseReason_CTX_CANCEL)
+		return true
+	default:
+		if t.ServerConn.IsClosed() && t.ClientConn.IsClosed() {
+			t.triggerCloseEvent(TrafficFlowCloseReason_FIN)
+			t.cancel()
+			return true
+		}
+		return false
+	}
+}
+
+func (t *TrafficFlow) String() string {
+	return fmt.Sprintf("stream[%3d]: %v <-> %v", t.Index, t.ClientConn.localAddr, t.ServerConn.localAddr)
+}
+
+func (t *TrafficFlow) feed(packet *layers.TCP) {
+	if t != nil {
+		if t.pool != nil {
+			t.pool.flowCache.Set(t.Hash, t)
+		}
+	}
+
+	if t.ClientConn.localPort == int(packet.SrcPort) {
+		t.ClientConn.FeedClient(packet)
+	} else {
+		t.ServerConn.FeedServer(packet)
+	}
+}
+
+func (t *TrafficFlow) onFrame(frame *TrafficFrame) {
+	if t.onDataFrameArrived != nil {
+		t.onDataFrameArrived(t, frame.Connection, frame)
+	}
+
+	if len(t.frames) > 0 {
+		lastFrame := t.frames[len(t.frames)-1]
+		if lastFrame.ConnHash != frame.ConnHash {
+			if t.onDataFrameReassembled != nil {
+				t.onDataFrameReassembled(t, lastFrame.Connection, lastFrame)
+			}
+			t.frames = append(t.frames, frame)
+		} else {
+			lastFrame.Payload = append(lastFrame.Payload, frame.Payload...)
+		}
+	} else {
+		t.frames = append(t.frames, frame)
+	}
+
+	if t.IsClosed() {
+		if len(t.frames) > 0 && t.onDataFrameReassembled != nil {
+			lastFrame := t.frames[len(t.frames)-1]
+			t.onDataFrameReassembled(t, lastFrame.Connection, lastFrame)
+		}
+		log.Warnf("writing frame to a closed flow: %v", t.String())
+	}
+}
+
+func (t *TrafficFlow) init(
+	handle func(*TrafficFlow),
+	onReassembledFrame func(flow *TrafficFlow, conn *TrafficConnection, frame *TrafficFrame),
+	onArrivedFrame func(flow *TrafficFlow, conn *TrafficConnection, frame *TrafficFrame),
+	onClose func(reason TrafficFlowCloseReason, flow *TrafficFlow),
+) {
+	t.createdOnce.Do(func() {
+		if handle == nil {
+			return
+		}
+		handle(t)
+	})
+	t.onDataFrameReassembled = onReassembledFrame
+	t.onDataFrameArrived = onArrivedFrame
+	t.onCloseHandler = onClose
+}
+
+type TrafficFlowCloseReason string
+
+const (
+	TrafficFlowCloseReason_FIN        TrafficFlowCloseReason = "fin"
+	TrafficFlowCloseReason_RST        TrafficFlowCloseReason = "rst"
+	TrafficFlowCloseReason_CTX_CANCEL TrafficFlowCloseReason = "ctx-canceled"
+	TrafficFlowCloseReason_INACTIVE   TrafficFlowCloseReason = "inactive"
+)
+
+func (t *TrafficFlow) triggerCloseEvent(reason TrafficFlowCloseReason) {
+	t.closedOnce.Do(func() {
+		if t.onCloseHandler == nil {
+			return
+		}
+		t.onCloseHandler(reason, t)
+	})
+}
+
+func (t *TrafficFlow) onCloseFlow(h func(reason TrafficFlowCloseReason, frame *TrafficFlow)) {
+	t.onCloseHandler = h
+}
--- common/yak/cmd/yakcmds/pcap.go
+++ common/yak/cmd/yakcmds/pcap.go
@@ -1,15 +1,13 @@
 package yakcmds
 
 import (
-	"bufio"
 	"github.com/urfave/cli"
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/pcapx/pcaputil"
 	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/utils/lowhttp"
 	"github.com/yaklang/yaklang/common/utils/tlsutils"
-	"io"
 	"strings"
-	"time"
 )
 
 var PcapCommand = cli.Command{
@@ -55,63 +53,19 @@ var PcapCommand = cli.Command{
 			opts = append(opts, pcaputil.WithSuricataFilter(suricata))
 		}
 
-		trafficHandler := func(verbose string, f io.Reader) {
-			br := bufio.NewReader(f)
-			firstByte, err := br.ReadByte()
-			if err != nil && err != io.EOF {
-				log.Errorf("read first byte failed: %s", err)
+		opts = append(opts, pcaputil.WithOnTrafficFlowOnDataFrameReassembled(func(flow *pcaputil.TrafficFlow, conn *pcaputil.TrafficConnection, frame *pcaputil.TrafficFrame) {
+			if len(frame.Payload) <= 0 {
 				return
 			}
 
-			// SNI
-			if firstByte == 0x16 {
-				br.UnreadByte()
-				raw := utils.StableReader(br, time.Second, 65535)
-				clientHello, err := tlsutils.ParseClientHello(raw)
-				if err != nil {
-					log.Errorf("parse client hello failed: %s", err)
-					return
+			if hellospec, err := tlsutils.ParseClientHello(frame.Payload); err == nil {
+				log.Infof("%v SNI: %v", flow.String(), hellospec.SNI())
+			} else if req, err := utils.ReadHTTPRequestFromBytes(frame.Payload); err == nil {
+				u, _ := lowhttp.ExtractURLFromHTTPRequest(req, false)
+				if u != nil {
+					log.Infof("%v %v %v", flow.String(), req.Method, u.String())
 				}
-				log.Infof("flow: %v SNI: %s", verbose, clientHello.SNI())
-				//} else if firstByte == 'H' {
-				//	br.UnreadByte()
-				//	log.Infof("checking first byte for http response: %x", firstByte)
-				//	rsp, err := lowhttp.ReadHTTPResponseFromBufioReader(br, nil)
-				//	if err != nil {
-				//		return
-				//	}
-				//	raw, _ := utils.DumpHTTPResponse(rsp, true)
-				//	fmt.Println(string(raw))
-				//} else if (firstByte >= 'a' && firstByte <= 'z') ||
-				//	(firstByte >= 'A' && firstByte <= 'Z') ||
-				//	(firstByte >= '0' && firstByte <= '9') {
-				//	br.UnreadByte()
-				//	// HTTP
-				//	log.Infof("checking first byte for http request: %x", firstByte)
-				//	req, err := lowhttp.ReadHTTPRequestFromBufioReader(br)
-				//	if err != nil {
-				//		return
-				//	}
-				//	raw, _ := utils.DumpHTTPRequest(req, true)
-				//	if req.Header.Get("Host") != "" {
-				//		fmt.Println(string(raw))
-				//	}
-				//} else {
-				//	io.Copy(io.Discard, br)
 			}
-		}
-		_ = trafficHandler
-		// httpflow
-		// sni
-		opts = append(opts, pcaputil.WithOnTrafficFlow(func(f *pcaputil.TrafficFlow) {
-			go func() {
-				for !f.IsClosed() {
-					time.Sleep(time.Second)
-					clientBuffer := f.ClientConn.GetBuffer()
-					// serverBuffer := f.ServerConn.GetBuffer()
-					trafficHandler(f.String(), clientBuffer)
-				}
-			}()
 		}))
 		return pcaputil.Start(opts...)
 	},
