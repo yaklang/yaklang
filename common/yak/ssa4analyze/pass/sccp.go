4d16023714876dabd4065161fbb02140551eb211
wlz <lingze.wl@gmail.com>
2023-09-24 15:28:27 +0800

    refactor(analyze sccp) refactor sccp pass

--- common/yak/ssa4analyze/pass/sccp.go
+++ common/yak/ssa4analyze/pass/sccp.go
@@ -4,15 +4,14 @@ import (
 	"github.com/yaklang/yaklang/common/yak/ssa"
 )
 
-const SCCPTAG ssa.ErrorTag = "sccppass"
+const BCTag ssa.ErrorTag = "BlockCondition"
 
 func init() {
-	RegisterFunctionPass(&SCCP{})
+	RegisterFunctionPass(&BlockCondition{})
 }
 
-// sccp
-// implement simple conditional constant propagation
-type SCCP struct {
+// block condition
+type BlockCondition struct {
 	edge   Edge
 	Finish map[*ssa.BasicBlock]struct{}
 }
@@ -20,7 +19,7 @@ type SCCP struct {
 // map to -> from -> condition
 type Edge map[*ssa.BasicBlock]map[*ssa.BasicBlock]ssa.Value
 
-func (s *SCCP) RunOnFunction(fun *ssa.Function) {
+func (s *BlockCondition) RunOnFunction(fun *ssa.Function) {
 	s.edge = make(Edge)
 	s.Finish = make(map[*ssa.BasicBlock]struct{})
 	newEdge := func(to, from *ssa.BasicBlock, condition ssa.Value) {
@@ -55,7 +54,7 @@ func (s *SCCP) RunOnFunction(fun *ssa.Function) {
 				x = cond.X
 				y = l.Init
 			}
-			canReach := newBinOpOnly(cond.Op, x, y, cond.Block)
+			canReach := newBinOp(cond.Op, x, y, cond.Block)
 			can, ok := canReach.(*ssa.Const)
 			if ok && can.IsBoolean() {
 				return can.Boolean()
@@ -82,7 +81,7 @@ func (s *SCCP) RunOnFunction(fun *ssa.Function) {
 			if defaultCond == nil {
 				defaultCond = newUnOp(ssa.OpNot, cond, sw.DefaultBlock)
 			} else {
-				defaultCond = newBinOp(ssa.OpOr, defaultCond, newUnOp(ssa.OpNot, cond, sw.DefaultBlock), sw.DefaultBlock)
+				defaultCond = newBinOp(ssa.OpLogicOr, defaultCond, newUnOp(ssa.OpNot, cond, sw.DefaultBlock), sw.DefaultBlock)
 			}
 		}
 		newEdge(sw.DefaultBlock, from, defaultCond)
@@ -93,15 +92,6 @@ func (s *SCCP) RunOnFunction(fun *ssa.Function) {
 	for _, b := range fun.Blocks {
 		for _, inst := range b.Instrs {
 			switch inst := inst.(type) {
-			// calc instruction
-			case *ssa.BinOp:
-				if ret := handlerBinOp(inst); ret != inst {
-					deleteStmt = append(deleteStmt, inst)
-				}
-			case *ssa.UnOp:
-				if ret := handlerUnOp(inst); ret != inst {
-					deleteStmt = append(deleteStmt, inst)
-				}
 			// call function
 			case *ssa.Call:
 				// TODO: config can set funciton return is a const
@@ -149,6 +139,10 @@ func (s *SCCP) RunOnFunction(fun *ssa.Function) {
 		s.Finish[bb] = struct{}{}
 		bb.Condition = cond
 
+		if bb.Reachable() == -1 {
+			bb.NewError(ssa.Warn, BCTag, "this block unreachable!")
+		}
+
 		// dfs
 		for _, succ := range bb.Succs {
 			handlerBlock(succ)
@@ -160,7 +154,7 @@ func (s *SCCP) RunOnFunction(fun *ssa.Function) {
 	}
 }
 
-func (s *SCCP) calcCondition(block *ssa.BasicBlock) ssa.Value {
+func (s *BlockCondition) calcCondition(block *ssa.BasicBlock) ssa.Value {
 	getCondition := func(to, from *ssa.BasicBlock) ssa.Value {
 		var edgeCond ssa.Value
 		if fromtable, ok := s.edge[to]; ok {
@@ -171,7 +165,7 @@ func (s *SCCP) calcCondition(block *ssa.BasicBlock) ssa.Value {
 		if edgeCond == nil {
 			return from.Condition
 		} else {
-			return newBinOp(ssa.OpAnd, from.Condition, edgeCond, from)
+			return newBinOp(ssa.OpLogicAnd, from.Condition, edgeCond, from)
 		}
 	}
 
@@ -199,253 +193,24 @@ func (s *SCCP) calcCondition(block *ssa.BasicBlock) ssa.Value {
 		if cond == nil {
 			cond = getCondition(block, pre)
 		} else {
-			cond = newBinOp(ssa.OpOr, cond, getCondition(block, pre), pre)
+			cond = newBinOp(ssa.OpLogicOr, cond, getCondition(block, pre), pre)
 		}
 	}
 	return cond
 }
 
-func emit(v ssa.Value, block *ssa.BasicBlock) ssa.Value {
-	if inst, ok := v.(ssa.Instruction); ok {
-		if _, ok := inst.GetParent().InstReg[inst]; !ok {
-			ssa.EmitBefore(block.LastInst(), inst)
-		} else {
-			return v
-		}
+func newBinOp(op ssa.BinaryOpcode, x ssa.Value, y ssa.Value, block *ssa.BasicBlock) ssa.Value {
+	b := ssa.NewBinOp(op, x, y, block)
+	if inst, ok := b.(ssa.Instruction); ok {
+		ssa.EmitInst(inst)
 	}
-	return v
-}
-func newBinOpOnly(op ssa.BinaryOpcode, x, y ssa.Value, block *ssa.BasicBlock) ssa.Value {
-	return handlerBinOpWithOutput(ssa.NewBinOpOnly(op, x, y, block), false)
-}
-
-func newBinOp(op ssa.BinaryOpcode, x, y ssa.Value, block *ssa.BasicBlock) ssa.Value {
-	return emit(newBinOpOnly(op, x, y, block), block)
+	return b
 }
 
 func newUnOp(op ssa.UnaryOpcode, x ssa.Value, block *ssa.BasicBlock) ssa.Value {
-	return emit(handlerUnOpWithOutput(ssa.NewUnOp(op, x, block), false), block)
-}
-
-func handlerBinOp(bin *ssa.BinOp) ssa.Value {
-	return handlerBinOpWithOutput(bin, true)
-}
-
-func handlerBinOpWithOutput(bin *ssa.BinOp, output bool) ssa.Value {
-	replace := func(ret ssa.Value) {
-		if output {
-			bin.NewError(ssa.Warn, SCCPTAG, "this binary expression can be replace with %s", ret)
-		}
-		ssa.ReplaceValue(bin, ret)
-	}
-	// merge const
-	x, xIsConst := bin.X.(*ssa.Const)
-	y, yIsConst := bin.Y.(*ssa.Const)
-	if xIsConst && yIsConst {
-		if ret := ConstBinary(x, y, bin.Op); ret != nil {
-			replace(ret)
-			return ret
-		}
-	}
-	// one side
-	if xIsConst {
-		if ret := ConstOneSide(bin.Op, x, bin.Y); ret != nil {
-			replace(ret)
-			return ret
-		}
-	}
-	if yIsConst {
-		if ret := ConstOneSide(bin.Op, y, bin.X); ret != nil {
-			replace(ret)
-			return ret
-		}
-	}
-
-	f := func(x, y ssa.Value) bool {
-		if un, ok := x.(*ssa.UnOp); ok {
-			if un.X == y && un.Op == ssa.OpNot {
-				return true
-			}
-		}
-		return false
-	}
-
-	if bin.Op == ssa.OpOr {
-		// ~x | x
-		if f(bin.X, bin.Y) || f(bin.Y, bin.X) {
-			return ssa.NewConst(true)
-		}
-	}
-
-	return bin
-}
-
-func handlerUnOp(u *ssa.UnOp) ssa.Value {
-	return handlerUnOpWithOutput(u, true)
-}
-func handlerUnOpWithOutput(u *ssa.UnOp, output bool) ssa.Value {
-	if x, ok := u.X.(*ssa.Const); ok {
-		if ret := ConstUnary(x, u.Op); ret != nil {
-			if output {
-				u.NewError(ssa.Warn, SCCPTAG, "this unary expression can be replace with %s", ret.String())
-			}
-			ssa.ReplaceValue(u, ret)
-			return ret
-		}
+	u := ssa.NewUnOp(op, x, block)
+	if inst, ok := u.(ssa.Instruction); ok {
+		ssa.EmitInst(inst)
 	}
 	return u
-}
-
-// OpShl BinaryOpcode = iota // <<
-
-// OpShr    // >>
-// OpAnd    // &
-// OpAndNot // &^
-// OpOr     // |
-// OpXor    // ^
-// OpAdd    // +
-// OpSub    // -
-// OpDiv    // /
-// OpMod    // %
-// // mul
-// OpMul // *
-
-// // boolean opcode
-// OpGt    // >
-// OpLt    // <
-// OpGtEq  // >=
-// OpLtEq  // <=
-// OpEq    // ==
-// OpNotEq // != <>
-
-func ConstOneSide(op ssa.BinaryOpcode, b *ssa.Const, v ssa.Value) ssa.Value {
-	if op == ssa.OpAnd && b.IsBoolean() {
-		if b.Boolean() {
-			// true && A = A
-			return v
-		} else {
-			// false && A = false
-			return b
-		}
-	}
-	if op == ssa.OpOr && b.IsBoolean() {
-		if b.Boolean() {
-			// true || A = true
-			return b
-		} else {
-			// false || A = A
-			return v
-		}
-	}
-
-	if op == ssa.OpMul && b.IsNumber() && b.Number() == 1 {
-		// 1 * A = A
-		return v
-	}
-	return nil
-}
-
-func ConstBinary(x, y *ssa.Const, op ssa.BinaryOpcode) *ssa.Const {
-	switch op {
-	case ssa.OpShl:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() << y.Number())
-		}
-	case ssa.OpShr:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() >> y.Number())
-		}
-	case ssa.OpAnd:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() & y.Number())
-		} else if x.IsBoolean() && y.IsBoolean() {
-			return ssa.NewConst(x.Boolean() && y.Boolean())
-		}
-	case ssa.OpAndNot:
-
-	case ssa.OpOr:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() | y.Number())
-		} else if x.IsBoolean() && y.IsBoolean() {
-			return ssa.NewConst(x.Boolean() || y.Boolean())
-		}
-	case ssa.OpXor:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() ^ y.Number())
-		}
-	case ssa.OpAdd:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() + y.Number())
-		}
-		if x.IsString() && y.IsString() {
-			return ssa.NewConst(x.VarString() + y.VarString())
-		}
-	case ssa.OpSub:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() - y.Number())
-		}
-	case ssa.OpDiv:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() / y.Number())
-		}
-	case ssa.OpMod:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() % y.Number())
-		}
-	case ssa.OpMul:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() * y.Number())
-		}
-	case ssa.OpGt:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() > y.Number())
-		}
-	case ssa.OpLt:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() < y.Number())
-		}
-	case ssa.OpGtEq:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() >= y.Number())
-		}
-	case ssa.OpLtEq:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() <= y.Number())
-		}
-	case ssa.OpEq:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() == y.Number())
-		}
-	case ssa.OpNotEq:
-		if x.IsNumber() && y.IsNumber() {
-			return ssa.NewConst(x.Number() != y.Number())
-		}
-	}
-	return nil
-}
-
-// OpNone UnaryOpcode = iota
-// OpNot              // !
-// OpPlus             // +
-// OpNeg              // -
-// OpChan             // ->
-func ConstUnary(x *ssa.Const, op ssa.UnaryOpcode) *ssa.Const {
-	switch op {
-	case ssa.OpNone:
-		return x
-	case ssa.OpNot:
-		if x.IsBoolean() {
-			return ssa.NewConst(!x.Boolean())
-		}
-	case ssa.OpPlus:
-		if x.IsNumber() {
-			return ssa.NewConst(+x.Number())
-		}
-	case ssa.OpNeg:
-		if x.IsNumber() {
-			return ssa.NewConst(-x.Number())
-		}
-	case ssa.OpChan:
-	}
-	return nil
 }
