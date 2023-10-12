f4f8affc6bd82b14507f619119b8a9c5ab1a255b
go0p <gp827782797@qq.com>
2023-10-12 15:07:33 +0800

    fix(fingerprint) update wapalyzer fingerprint && allow custom fingerprint

--- common/fp/specific_service_test.go
+++ common/fp/specific_service_test.go
@@ -1,26 +1,43 @@
 package fp
 
 import (
-	"github.com/davecgh/go-spew/spew"
-	"github.com/stretchr/testify/assert"
+	"github.com/yaklang/yaklang/common/fp/webfingerprint"
+	"strings"
 	"testing"
 )
 
-func TestWithFingerprintRule_Redis(t *testing.T) {
-	t.SkipNow()
+func TestMUSTPASS_FingerprintRule(t *testing.T) {
+	// debug
+	//resp, _ := ioutil.ReadFile("./webfingerprint/fingerprint-rules.yml")
+	//
+	//rules, _ := webfingerprint.ParseWebFingerprintRules(resp)
 
-	//log.SetLevel(golog.DebugLevel)
+	rules, _ := webfingerprint.LoadDefaultDataSource()
 
-	test := assert.New(t)
-	matcher, err := NewFingerprintMatcher(nil, nil)
-	if !test.Nil(err) {
+	config := NewConfig(WithWebFingerprintRule(rules))
+	matcher, err := NewFingerprintMatcher(nil, config)
+	if err != nil {
 		t.FailNow()
 	}
 
-	result, err := matcher.Match("10.3.0.127", 6379)
-	if !test.Nil(err) {
+	wantRules, host, port := webfingerprint.MockRandomWebFingerPrints()
+
+	result, err := matcher.Match(host, port)
+
+	if err != nil {
 		t.FailNow()
 	}
+	//spew.Dump(wantRules)
+	resultMap := make(map[string]bool)
+	for _, cpe := range result.Fingerprint.CPEs {
+		productName := strings.Split(cpe, ":")[3]
+		//fmt.Println(productName)
+		resultMap[productName] = true
+	}
 
-	spew.Dump(result)
+	for _, want := range wantRules {
+		if _, found := resultMap[want]; !found {
+			t.Errorf("Expected product [%s] not found in the actual results", want)
+		}
+	}
 }
