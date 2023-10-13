22c9a064a9de2d54a328d6cde0ba9ecf15f8e32a
z3 <z3r0ne@163.com>
2023-10-13 11:33:44 +0800

    支持自定义tag的边界符、编译和执行过程

--- common/fuzztagx/standard-parser/generator.go
+++ common/fuzztagx/standard-parser/generator.go
@@ -4,7 +4,6 @@ import (
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/yak/httptpl"
 	"reflect"
-	"strings"
 	"unsafe"
 )
 
@@ -15,18 +14,35 @@ type GeneratorNode interface {
 }
 
 type MethodContext struct {
-	methodTable map[string]func(s string) []string
-	labelTable  map[string][]*TagGenerator
+	methodTable    map[string]TagMethod
+	labelTable     map[string]map[*TagGenerator]struct{}
+	tagToLabelsMap map[*TagGenerator][]string
+}
+
+// UpdateLabels 更新全局labelTable，先删除当前tag的所有label映射，再增加
+func (m *MethodContext) UpdateLabels(tag *TagGenerator) {
+	for _, label := range m.tagToLabelsMap[tag] {
+		if set, ok := m.labelTable[label]; ok {
+			delete(set, tag)
+		}
+	}
+	m.tagToLabelsMap[tag] = tag.data.GetLabels()
+	for _, label := range tag.data.GetLabels() {
+		if _, ok := m.labelTable[label]; !ok {
+			m.labelTable[label] = map[*TagGenerator]struct{}{}
+		}
+		m.labelTable[label][tag] = struct{}{}
+	}
 }
 
 // String
 type StringGenerator struct {
-	backpropagation func(s string)
+	backpropagation func(s FuzzResult)
 	data            string
 }
 
 func (s *StringGenerator) GenerateOne() bool {
-	s.backpropagation(s.data)
+	s.backpropagation(FuzzResult(s.data))
 	return false
 }
 func (s *StringGenerator) Reset() {
@@ -37,9 +53,9 @@ func (s *StringGenerator) IsRep() bool {
 
 // Expression
 type ExpressionGenerator struct {
-	backpropagation func(s string)
+	backpropagation func(s FuzzResult)
 	index           int
-	data            *FuzzTag
+	data            TagNode
 	isRep           bool
 	cache           string
 }
@@ -47,19 +63,19 @@ type ExpressionGenerator struct {
 func (f *ExpressionGenerator) GenerateOne() bool {
 	if f.index > 0 {
 		if f.isRep {
-			f.backpropagation(f.cache)
+			f.backpropagation(FuzzResult(f.cache))
 			return false
 		}
-		f.backpropagation("")
+		f.backpropagation(FuzzResult(""))
 		return false
 	} else {
 		box := httptpl.NewNucleiDSLYakSandbox()
-		res, err := box.Execute(utils.InterfaceToString(f.data.Data[0])) // 可能越界
+		res, err := box.Execute(utils.InterfaceToString(f.data.GetData()[0])) // 可能越界
 		if err != nil {
-			f.backpropagation("")
+			f.backpropagation(FuzzResult{})
 			return false
 		} else {
-			f.backpropagation(utils.InterfaceToString(res))
+			f.backpropagation(FuzzResult(utils.InterfaceToString(res)))
 			return true
 		}
 	}
@@ -71,42 +87,49 @@ func (s *ExpressionGenerator) IsRep() bool {
 	return s.isRep
 }
 
-// FuzzTag
 type TagGenerator struct {
-	data            *FuzzTag
-	cache           *[]string
+	data            TagNode
+	cache           *[]FuzzResult
 	isRep           bool
 	params          []GeneratorNode
 	methodCtx       *MethodContext
 	generator       *Generator
 	index           int
-	backpropagation func(s string)
+	backpropagation func(s FuzzResult)
 }
 
-func NewTagGenerator(tag *FuzzTag, ctx *MethodContext) *TagGenerator {
+func NewTagGenerator(tag TagNode, ctx *MethodContext) *TagGenerator {
 	return &TagGenerator{
 		data:      tag,
 		methodCtx: ctx,
 	}
 }
-
+func (f *TagGenerator) Exec(data string) ([]FuzzResult, error) {
+	res, err := f.data.Exec(data, f.methodCtx.methodTable)
+	if err != nil {
+		return nil, err
+	}
+	f.methodCtx.UpdateLabels(f)
+	return res, nil
+}
 func (f *TagGenerator) GenerateOne() bool {
 	if f.generator == nil { // 未初始化
 		f.generator = NewBackpropagationGenerator(f.backpropagation, f.params)
 	}
 	if f.cache == nil {
-		fun, ok := (f.methodCtx.methodTable)[f.data.Method]
+		s, ok := f.generator.Generate()
 		if ok {
-			s, ok := f.generator.Generate()
-			if ok {
-				result := fun(s)
-				f.cache = &result
+			result, err := f.Exec(s)
+			if err != nil {
+				f.backpropagation([]byte(""))
+				return false
 			}
+			f.cache = &result
 		}
 	}
 
 	if f.cache == nil || len(*f.cache) == 0 {
-		f.backpropagation("")
+		f.backpropagation([]byte(""))
 		return false
 	}
 	if f.index >= len(*f.cache) {
@@ -114,16 +137,18 @@ func (f *TagGenerator) GenerateOne() bool {
 			f.backpropagation(utils.GetLastElement(*f.cache))
 			return false
 		}
-		fun := (f.methodCtx.methodTable)[f.data.Method]
 		s, ok := f.generator.Generate()
-		if ok {
-			result := fun(s)
-			f.cache = &result
-			f.index = 0
-		} else {
-			f.backpropagation("")
+		if !ok {
+			f.backpropagation(FuzzResult(""))
 			return false
 		}
+		result, err := f.Exec(s)
+		if err != nil {
+			f.backpropagation(FuzzResult(""))
+			return false
+		}
+		f.cache = &result
+		f.index = 0
 	}
 	defer func() {
 		f.index++
@@ -144,34 +169,34 @@ func (s *TagGenerator) IsRep() bool {
 }
 
 type Generator struct {
-	container []string
+	container []FuzzResult
 	//index     int
 	data            []GeneratorNode
 	first           bool
-	backpropagation func(s string)
+	backpropagation func(s FuzzResult)
 }
 
-func NewBackpropagationGenerator(f func(s string), nodes []GeneratorNode) *Generator {
-	g := &Generator{data: nodes, container: make([]string, len(nodes)), first: true, backpropagation: f}
+func NewBackpropagationGenerator(f func(s FuzzResult), nodes []GeneratorNode) *Generator {
+	g := &Generator{data: nodes, container: make([]FuzzResult, len(nodes)), first: true, backpropagation: f}
 	for index, d := range g.data {
 		switch ret := d.(type) {
 		case *TagGenerator:
-			ret.backpropagation = func(index int) func(s string) {
-				return func(s string) {
+			ret.backpropagation = func(index int) func(s FuzzResult) {
+				return func(s FuzzResult) {
 					g.container[index] = s
 					g.backpropagation(s)
 				}
 			}(index)
 		case *StringGenerator:
-			ret.backpropagation = func(index int) func(s string) {
-				return func(s string) {
+			ret.backpropagation = func(index int) func(s FuzzResult) {
+				return func(s FuzzResult) {
 					g.container[index] = s
 					g.backpropagation(s)
 				}
 			}(index)
 		case *ExpressionGenerator:
-			ret.backpropagation = func(index int) func(s string) {
-				return func(s string) {
+			ret.backpropagation = func(index int) func(s FuzzResult) {
+				return func(s FuzzResult) {
 					g.container[index] = s
 					g.backpropagation(s)
 				}
@@ -181,37 +206,38 @@ func NewBackpropagationGenerator(f func(s string), nodes []GeneratorNode) *Gener
 	}
 	return g
 }
-func NewGenerator(nodes []Node,table map[string]func(string) []string) *Generator {
-	labelTable := map[string][]*TagGenerator{}
+func NewGenerator(nodes []Node, table map[string]TagMethod) (*Generator, error) {
+	methodCtx := &MethodContext{
+		methodTable:    table,
+		labelTable:     map[string]map[*TagGenerator]struct{}{},
+		tagToLabelsMap: map[*TagGenerator][]string{},
+	}
 	var node2generator func(nodes []Node) []GeneratorNode
 	node2generator = func(nodes []Node) []GeneratorNode {
-		methodCtx := &MethodContext{
-			methodTable: table,
-			labelTable:  labelTable,
-		}
 		generatorNodes := []GeneratorNode{}
 		for _, node := range nodes {
 			switch ret := node.(type) {
-			case *FuzzTag:
+			case TagNode:
 				gener := NewTagGenerator(ret, methodCtx)
-				gener.params = node2generator(ret.Data)
+				gener.params = node2generator(ret.GetData())
+				methodCtx.tagToLabelsMap[gener] = ret.GetLabels()
 				generatorNodes = append(generatorNodes, gener)
-				for _, label := range ret.Labels {
+				for _, label := range ret.GetLabels() {
 					switch label {
 					case "rep":
 						gener.isRep = true
 					default:
-						labelTable[label] = append(labelTable[label], gener)
+						methodCtx.labelTable[label][gener] = struct{}{}
 					}
 				}
 
-			case string:
-				generatorNodes = append(generatorNodes, &StringGenerator{data: ret})
+			case StringNode:
+				generatorNodes = append(generatorNodes, &StringGenerator{data: string(ret)})
 			}
 		}
 		return generatorNodes
 	}
-	return NewBackpropagationGenerator(func(s string) {}, node2generator(nodes))
+	return NewBackpropagationGenerator(func(s FuzzResult) {}, node2generator(nodes)), nil
 }
 
 func (g *Generator) Generate() (string, bool) {
@@ -219,7 +245,11 @@ func (g *Generator) Generate() (string, bool) {
 		defer func() {
 			g.first = false
 		}()
-		return strings.Join(g.container, ""), true
+		res := ""
+		for _, result := range g.container {
+			res += string(result)
+		}
+		return res, true
 	} else {
 		isOk := false
 		i := 0
@@ -237,9 +267,9 @@ func (g *Generator) Generate() (string, bool) {
 			renderedNode[reflect.ValueOf(g.data[i]).UnsafePointer()] = struct{}{}
 			genOneOk := g.data[i].GenerateOne()
 			if v, ok := g.data[i].(*TagGenerator); ok {
-				for _, label := range v.data.Labels {
+				for _, label := range v.data.GetLabels() {
 					if ms, ok := v.methodCtx.labelTable[label]; ok {
-						for _, m := range ms {
+						for m := range ms {
 							uid1 := reflect.ValueOf(m).UnsafePointer()
 							if uid1 == uid {
 								continue
@@ -269,6 +299,10 @@ func (g *Generator) Generate() (string, bool) {
 			}
 			i++
 		}
-		return strings.Join(g.container, ""), isOk
+		res := ""
+		for _, result := range g.container {
+			res += string(result)
+		}
+		return res, isOk
 	}
 }
---
+++ common/fuzztagx/standard-parser/tag.go
@@ -0,0 +1,75 @@
+package standard_parser
+
+import (
+	"reflect"
+	"sync"
+)
+
+type FuzzResult []byte
+type TagMethod func(string) ([]FuzzResult, error)
+type Node interface {
+	IsNode()
+}
+
+type StringNode string
+
+func (s StringNode) IsNode() {
+}
+
+type TagNode interface {
+	IsNode()
+	Exec(string, ...map[string]TagMethod) ([]FuzzResult, error)
+
+	AddData(node ...Node)
+	AddLabel(label string)
+
+	GetData() []Node
+	GetLabels() []string
+}
+
+type BaseTag struct {
+	Data    []Node
+	Labels  []string
+	Methods *map[string]TagMethod
+	// initOnce 用来对子结构体的初始化
+	initOnce sync.Once
+}
+
+// DoOnce 用来对子结构体的初始化
+func (b *BaseTag) DoOnce(f func()) {
+	b.initOnce.Do(f)
+}
+func (b *BaseTag) AddLabel(label string) {
+	b.Labels = append(b.Labels, label)
+}
+func (b *BaseTag) AddData(node ...Node) {
+	b.Data = append(b.Data, node...)
+}
+
+func (b *BaseTag) GetData() []Node {
+	return b.Data
+}
+
+func (b *BaseTag) GetLabels() []string {
+	return b.Labels
+}
+
+// TagDefine 自定义tag类型
+type TagDefine struct {
+	name      string
+	start     string
+	end       string
+	tagStruct TagNode
+}
+
+func (t *TagDefine) NewTag() TagNode {
+	return reflect.New(reflect.ValueOf(t.tagStruct).Type().Elem()).Interface().(TagNode)
+}
+func NewTagDefine(name, start, end string, tagStruct TagNode) *TagDefine {
+	return &TagDefine{
+		name:      name,
+		start:     start,
+		end:       end,
+		tagStruct: tagStruct,
+	}
+}
