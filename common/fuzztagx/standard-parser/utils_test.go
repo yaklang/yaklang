96ed46dd72b33c68503ebc8000b47832d122e658
z3 <z3r0ne@163.com>
2023-10-13 15:57:10 +0800

    增加转义类

---
+++ common/fuzztagx/standard-parser/utils_test.go
@@ -0,0 +1,60 @@
+package standard_parser
+
+import (
+	"errors"
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/yaklang/yaklang/common/utils"
+	"testing"
+)
+
+func TestEscaper_Unescape(t *testing.T) {
+	chars := []string{"{{", "("} // 单双字符
+	escaper := NewDefaultEscaper(chars...)
+	for _, testcase := range [][2]string{
+		// 测试边界
+		{
+			`\%s\%s`,
+			`%s%s`,
+		},
+		{
+			`\%saaa\%s`,
+			`%saaa%s`,
+		},
+		{
+			`aa\%saaa\%saa`,
+			`aa%saaa%saa`,
+		},
+		//转义未定义字符(转义后应该是字符本身)
+		{
+			`aa\a%saaa\a%saa`,
+			`aaa%saaaa%saa`,
+		},
+		// 转义转义符
+		{
+			`aa\\%saaa\\%saa`,
+			`aa\%saaa\%saa`,
+		},
+		{
+			`aa\\\%saaa\\\%saa`,
+			`aa\%saaa\%saa`,
+		},
+	} {
+		s1 := fmt.Sprintf(testcase[0], utils.InterfaceToSliceInterface(chars)...)
+		s2 := fmt.Sprintf(testcase[1], utils.InterfaceToSliceInterface(chars)...)
+		res, err := escaper.Unescape(s1)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if res != s2 {
+			t.Fatal(errors.New(spew.Sprintf("unescape string `%s` error", s1)))
+		}
+	}
+	res, err := escaper.Unescape("\\")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res != "\\" {
+		t.Fatal(errors.New(spew.Sprintf("unescape string `%s` error", "\\\\")))
+	}
+}
