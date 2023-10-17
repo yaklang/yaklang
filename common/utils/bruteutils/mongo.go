94b4a66269b08b5bf42af14274ce4430382ecdf5
WAY29 <41830147+WAY29@users.noreply.github.com>
2023-10-17 14:18:07 +0800

    refactor(brute-mongo): use mongo-driver instead

--- common/utils/bruteutils/mongo.go
+++ common/utils/bruteutils/mongo.go
@@ -1,139 +1,78 @@
 package bruteutils
 
 import (
+	"context"
 	"fmt"
-	"github.com/yaklang/yaklang/common/log"
-	"github.com/yaklang/yaklang/common/netx"
-	"github.com/yaklang/yaklang/common/utils"
-	"github.com/yaklang/yaklang/common/utils/lowhttp"
-	"strings"
-	"time"
 
-	"gopkg.in/mgo.v2"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// https://github.com/k8gege/LadonGo/
 var mongoAuth = &DefaultServiceAuthInfo{
 	ServiceName:      "mongodb",
 	DefaultPorts:     "27017",
 	DefaultUsernames: append([]string{"root", "admin", "mongodb"}, CommonUsernames...),
 	DefaultPasswords: CommonPasswords,
 	UnAuthVerify: func(i *BruteItem) *BruteItemResult {
-		result := i.Result()
 		i.Target = appendDefaultPort(i.Target, 27017)
+		result := i.Result()
 
-		conn, err := netx.DialTCPTimeout(10*time.Second, i.Target)
-		if err != nil {
-			res := i.Result()
-			res.Finished = true
-			return res
-		}
-		conn.Close()
-
+		ctx := context.Background()
 		host, port, _ := utils.ParseStringToHostPort(i.Target)
-		forceHttps := strings.Contains(fmt.Sprint(port), "443")
-		addr := fmt.Sprintf("%s:%d", host, port)
-
-		rspIns, _ := lowhttp.HTTP(
-			lowhttp.WithRequest(fmt.Sprintf(`GET / HTTP/1.1
-Host: %v
-User-Agent: %v
-
-`, addr, netx.DefaultUserAgent)),
-			lowhttp.WithHttps(forceHttps),
-		)
-
-		var bytes []byte
-		if rspIns != nil {
-			bytes = rspIns.RawPacket
+		addr := fmt.Sprintf("mongodb://%s:%d", host, port)
+		clientOptions := options.Client().ApplyURI(addr)
+		mgoCli, err := mongo.Connect(ctx, clientOptions)
+		if err != nil {
+			log.Errorf("connect unauath mongodb failed: %s", err)
+			return result
 		}
+		defer mgoCli.Disconnect(ctx)
 
-		// 指纹识别验证
-		if !utils.IContains(string(bytes), "It looks like you are trying to access MongoDB over HTTP on the native driver port.") {
-			result.Finished = true
+		err = mgoCli.Ping(ctx, nil)
+		if err != nil {
+			log.Errorf("ping unauth mongodb failed: %s", err)
 			return result
 		}
 
-		if r, err := MongodbUnauth(host, port); err != nil {
-			log.Errorf("check mongodb unauth failed: %s", err)
-		} else {
-			if r {
-				result.Ok = true
-				result.Username = ""
-				result.Password = ""
-				return result
-			}
+		_, err = mgoCli.ListDatabaseNames(ctx, bson.M{})
+		if err != nil {
+			log.Errorf("ping unauth mongodb failed: %s", err)
+			return result
 		}
+		result.Username = ""
+		result.Password = ""
+		result.Finished = true
+		result.Ok = true
 		return result
 	},
 	BrutePass: func(i *BruteItem) *BruteItemResult {
-		r := i.Result()
+		result := i.Result()
 		username := i.Username
 		password := i.Password
 		host, port, _ := utils.ParseStringToHostPort(appendDefaultPort(i.Target, 27017))
-		session, err := mgo.DialWithTimeout("mongodb://"+username+":"+password+"@"+host+":"+fmt.Sprint(port)+"/"+"admin", time.Second*3)
-		if err != nil {
-			autoSetFinishedByConnectionError(err, r)
-			return r
-		}
-		defer session.Close()
+		ctx := context.Background()
 
-		err = session.Ping()
+		addr := fmt.Sprintf("mongodb://%s:%d", host, port)
+		clientOptions := options.Client().ApplyURI(addr).SetAuth(options.Credential{Username: username, Password: password})
+
+		mgoCli, err := mongo.Connect(ctx, clientOptions)
 		if err != nil {
-			autoSetFinishedByConnectionError(err, r)
-			return r
+			autoSetFinishedByConnectionError(err, result)
+			return result
 		}
-		if session.Run("serverStatus", nil) == nil {
-			r.Ok = true
-			return r
+		defer mgoCli.Disconnect(ctx)
+
+		err = mgoCli.Ping(ctx, nil)
+		if err != nil {
+			autoSetFinishedByConnectionError(err, result)
+			return result
 		}
-		return r
+
+		result.Finished = true
+		result.Ok = true
+		return result
 	},
-}
-
-func MongodbUnauth(host string, port int) (flag bool, err error) {
-	timeoutSeconds := 10
-
-	flag = false
-	senddata := []byte{58, 0, 0, 0, 167, 65, 0, 0, 0, 0, 0, 0, 212, 7, 0, 0, 0, 0, 0, 0, 97, 100, 109, 105, 110, 46, 36, 99, 109, 100, 0, 0, 0, 0, 0, 255, 255, 255, 255, 19, 0, 0, 0, 16, 105, 115, 109, 97, 115, 116, 101, 114, 0, 1, 0, 0, 0, 0}
-	getlogdata := []byte{72, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 212, 7, 0, 0, 0, 0, 0, 0, 97, 100, 109, 105, 110, 46, 36, 99, 109, 100, 0, 0, 0, 0, 0, 1, 0, 0, 0, 33, 0, 0, 0, 2, 103, 101, 116, 76, 111, 103, 0, 16, 0, 0, 0, 115, 116, 97, 114, 116, 117, 112, 87, 97, 114, 110, 105, 110, 103, 115, 0, 0}
-	realhost := fmt.Sprintf("%s:%v", host, port)
-	conn, err := netx.DialTCPTimeout(time.Duration(timeoutSeconds)*time.Second, realhost)
-	defer func() {
-		if conn != nil {
-			conn.Close()
-		}
-	}()
-	if err != nil {
-		return flag, err
-	}
-	err = conn.SetReadDeadline(time.Now().Add(time.Duration(timeoutSeconds) * time.Second))
-	if err != nil {
-		return flag, err
-	}
-	_, err = conn.Write(senddata)
-	if err != nil {
-		return flag, err
-	}
-	buf := make([]byte, 1024)
-	count, err := conn.Read(buf)
-	if err != nil {
-		return flag, err
-	}
-	text := string(buf[0:count])
-	if strings.Contains(text, "ismaster") {
-		_, err = conn.Write(getlogdata)
-		if err != nil {
-			return flag, err
-		}
-		count, err := conn.Read(buf)
-		if err != nil {
-			return flag, err
-		}
-		text := string(buf[0:count])
-		if strings.Contains(text, "totalLinesWritten") {
-			return true, nil
-		}
-	}
-	return flag, err
 }
--- go.mod
+++ go.mod
@@ -109,6 +109,7 @@ require (
 	github.com/vjeantet/grok v1.0.0
 	github.com/xiecat/wsm v0.1.3
 	github.com/ysmood/gson v0.7.3
+	go.mongodb.org/mongo-driver v1.12.1
 	golang.org/x/crypto v0.10.0
 	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1
 	golang.org/x/net v0.11.0
@@ -183,6 +184,7 @@ require (
 	github.com/mdlayher/raw v0.0.0-20190313224157-43dbcdd7739d // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/moby/term v0.5.0 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/nxadm/tail v1.4.8 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0-rc3 // indirect
@@ -206,6 +208,10 @@ require (
 	github.com/vmihailenco/tagparser v0.1.2 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/xanzy/ssh-agent v0.3.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	github.com/ysmood/fetchup v0.2.2 // indirect
 	github.com/ysmood/goob v0.4.0 // indirect
 	github.com/ysmood/leakless v0.8.0 // indirect
