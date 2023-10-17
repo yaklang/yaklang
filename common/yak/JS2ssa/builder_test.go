47933d8d154b366df8d85af7afc960cfb107d4b4
Jche143 <1246027231@qq.com>
2023-10-17 15:25:25 +0800

    feature(builder_ast) ifstatement

--- common/yak/JS2ssa/builder_test.go
+++ common/yak/JS2ssa/builder_test.go
@@ -5,6 +5,14 @@ import (
 )
 
 func TestMain(t *testing.T) {
-	prog := parseSSA(`a = 1 + 2;`)
+	prog := parseSSA(`
+	a = 0
+	for(;;a++){a= 1}
+	for(a=0;;){}
+	for(;a>0;){}
+	for(a=0;a>0;){}
+	for(a=0;a>0;a++){}
+	for(;;){}
+	`)
 	prog.Show()
 }
