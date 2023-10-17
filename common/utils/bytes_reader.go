34c78efd9c086ad6e2a7b281b66c3b179d2ac128
v1ll4n <v1ll4n.a5k@gmail.com>
2023-10-17 13:14:08 +0800

    unset read ddl

--- common/utils/bytes_reader.go
+++ common/utils/bytes_reader.go
@@ -69,7 +69,7 @@ func ReadConnUntil(conn net.Conn, timeout time.Duration, sep ...byte) ([]byte, e
 	var result bytes.Buffer
 	conn.SetReadDeadline(time.Now().Add(timeout))
 	defer func() {
-		conn.SetDeadline(time.Time{})
+		conn.SetReadDeadline(time.Time{})
 	}()
 
 	var stopWord = make(map[byte]struct{})
