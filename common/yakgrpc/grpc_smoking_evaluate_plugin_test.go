746e3abefccb0fee228ef53aa6669ad819289d35
wlz <lingze.wl@gmail.com>
2023-10-18 15:44:26 +0800

    fix(smoking plugin) check analyze static result severity and mark where error from

--- common/yakgrpc/grpc_smoking_evaluate_plugin_test.go
+++ common/yakgrpc/grpc_smoking_evaluate_plugin_test.go
@@ -15,54 +15,76 @@ func TestGRPCMUSTPASS_SMOKING_EVALUATE_PLUGIN(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	name, err := yakit.CreateTemporaryYakScript("port-scan", `yakit.AutoInitYakit()
+	type testCase struct {
+		code      string
+		err       string
+		zeroScore bool // is score == 0 ?
+	}
+	TestSmokingEvaluatePlugin := func(tc testCase) {
+		name, err := yakit.CreateTemporaryYakScript("port-scan", tc.code)
+		if err != nil {
+			t.Fatal(err)
+		}
+		rsp, err := client.SmokingEvaluatePlugin(context.Background(), &ypb.SmokingEvaluatePluginRequest{
+			PluginName: name,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		var checking = false
+		// spew.Dump("result: ", rsp)
+		if tc.zeroScore && rsp.Score != 0 {
+			// want score == 0 but get !0
+			t.Fatal("this test should have score = 0")
+		}
+		if !tc.zeroScore && rsp.Score == 0 {
+			// want score != 0 but get 0
+			t.Fatal("this test shouldn't have score = 0")
+		}
+		for _, r := range rsp.Results {
+			if strings.Contains(r.String(), tc.err) {
+				checking = true
+			}
+		}
+		if !checking {
+			t.Fatalf("should have %s", tc.err)
+		}
+	}
+
+	t.Run("test negative alarm", func(t *testing.T) {
+		TestSmokingEvaluatePlugin(testCase{
+			code: `
+yakit.AutoInitYakit()
 handle = result => {
 	yakit.Info("HELLO")
 	risk.NewRisk("http://baidu.com")
-}`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rsp, err := client.SmokingEvaluatePlugin(context.Background(), &ypb.SmokingEvaluatePluginRequest{
-		PluginName: name,
+}`,
+			err:       "[Negative Alarm]",
+			zeroScore: false,
+		})
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
-	var checking = false
-	for _, r := range rsp.Results {
-		// spew.Dump(r)
-		if strings.Contains(r.String(), "[Negative Alarm]") {
-			checking = true
-		}
-	}
-	if !checking {
-		panic("should have negative alarm")
-	}
 
-	name, err = yakit.CreateTemporaryYakScript("port-scan", `yakit.AutoInitYakit()
+	t.Run("test undefine variable", func(t *testing.T) {
+		TestSmokingEvaluatePlugin(testCase{
+			code: `
+yakit.AutoInitYakit()
 handle = result => {
 	yakit.Info(bacd)
 	risk.NewRisk("http://baidu.com")
-}`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	rsp, err = client.SmokingEvaluatePlugin(context.Background(), &ypb.SmokingEvaluatePluginRequest{
-		PluginName: name,
+}`,
+			err:       "Value undefine",
+			zeroScore: true,
+		})
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
-	// spew.Dump(rsp)
-	checking = false
-	for _, r := range rsp.Results {
-		// spew.Dump(r)
-		if strings.Contains(r.String(), "undefined variable") {
-			checking = true
-		}
-	}
-	if !checking {
-		t.Fatal("should have negative alarm")
-	}
+
+	t.Run("test just warning", func(t *testing.T) {
+		TestSmokingEvaluatePlugin(testCase{
+			code: `
+yakit.AutoInitYakit()
+handle = result => {
+}`,
+			err:       "empty block",
+			zeroScore: false,
+		})
+	})
 }
