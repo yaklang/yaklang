e48e370f7d28af92dbbf93c8004bed95026182f1
v1ll4n <v1ll4n.a5k@gmail.com>
2023-10-23 11:23:30 +0800

    remove active panic from eval

--- common/yak/antlr4yak/engine.go
+++ common/yak/antlr4yak/engine.go
@@ -322,7 +322,7 @@ func (n *Engine) EvalWithInline(ctx context.Context, code string, inline bool) e
 
 	compiler, err := n._compile(code)
 	if err != nil {
-		panic(err)
+		return utils.Errorf("compile error: \b%s", err)
 	}
 	n.vm.SetDebug(n.debug)
 	n.vm.SetDebugMode(n.debugMode, code, compiler.GetOpcodes(), n.debugInit, n.debugCallBack)
