e390666d26f2c275647c0788b41bdf7a4126c1d7
wlz <lingze.wl@gmail.com>
2023-10-24 14:18:34 +0800

    refactor(ssa) inspect variable from function symbolTable

--- common/yak/ssa/ssa_vars.go
+++ common/yak/ssa/ssa_vars.go
@@ -1,11 +1,11 @@
 package ssa
 
+import "github.com/samber/lo"
+
 type InspectVariableResult struct {
 	VariableName   string
-	ProbablyTypes  []string
+	ProbablyTypes  Types
 	ProbablyValues Values
-	MustTypes      []string
-	MustValue      Values
 }
 
 func (r *InspectVariableResult) Merge(other *InspectVariableResult) {
@@ -14,23 +14,21 @@ func (r *InspectVariableResult) Merge(other *InspectVariableResult) {
 	}
 	r.ProbablyTypes = append(r.ProbablyTypes, other.ProbablyTypes...)
 	r.ProbablyValues = append(r.ProbablyValues, other.ProbablyValues...)
-	r.MustTypes = append(r.MustTypes, other.MustTypes...)
-	r.MustValue = append(r.MustValue, other.MustValue...)
 }
 
 func (p *Program) InspectVariableLast(varName string) *InspectVariableResult {
 	var result = new(InspectVariableResult)
-	// for _, pkg := range p.Packages {
-	// 	for _, funcIns := range pkg.Funcs {
-	// 		if res, ok := funcIns.symbolTable[varName]; ok {
-	// 			_ = res
-	// 			// last := res[len(res)-1]
-	// 			// result.ProbablyTypes = append(result.ProbablyTypes, last.GetType().String())
-	// 			// result.ProbablyValues = append(result.ProbablyValues, last)
-	// 		}
-	// 	}
-	// }
-	// result.VariableName = varName
+	for _, pkg := range p.Packages {
+		for _, funcIns := range pkg.Funcs {
+			if res, ok := funcIns.symbolTable[varName]; ok {
+				vs := res[funcIns.ExitBlock]
+				v := vs[len(vs)-1]
+				result.ProbablyValues = append(result.ProbablyValues, v)
+				result.ProbablyTypes = append(result.ProbablyTypes, v.GetType())
+			}
+		}
+	}
+	result.VariableName = varName
 	return result
 }
 
@@ -50,37 +48,21 @@ func (f *Function) InspectVariable(varName string) *InspectVariableResult {
 	var result = new(InspectVariableResult)
 	result.VariableName = varName
 
-	// if f == nil || f.symbolTable == nil {
-	// 	return result
-	// }
+	if f == nil || f.symbolTable == nil {
+		return result
+	}
 
-	// res, ok := f.symbolTable[varName]
-	// if !ok || res == nil {
-	// 	return result
-	// }
-	// var probablyTypes []string
-	// var probablyValue Values
-	// var mustValue Values
-	// var mustTypes []string
-	// values := make([]Value, 0)
-	// for _, v := range res {
-	// 	_ = v
-	// 	// values = append(values, v)
-	// 	// probablyValue = append(probablyValue, v)
-	// 	// probablyTypes = append(probablyTypes, v.GetType().String())
-	// 	// if inst, ok := v.(Instruction); ok {
-	// 	// 	reachable := inst.GetBlock().Reachable()
-	// 	// 	if reachable == 1 {
-	// 	// 		mustTypes = append(mustTypes, v.GetType().String())
-	// 	// 		mustValue = append(mustValue, v)
-	// 	// 	}
-	// 	// }
-	// }
-	// result.ProbablyTypes = probablyTypes
-	// result.ProbablyValues = probablyValue
-	// result.MustTypes = mustTypes
-	// result.MustValue = mustValue
-	// _ = values
-	// // result.values = values
+	res, ok := f.symbolTable[varName]
+	if !ok || res == nil {
+		return result
+	}
+	var probablyTypes Types
+	var probablyValue Values
+	for _, v := range res {
+		probablyValue = append(probablyValue, v...)
+		probablyTypes = append(probablyTypes, lo.Map(v, func(v Value, _ int) Type { return v.GetType() })...)
+	}
+	result.ProbablyTypes = probablyTypes
+	result.ProbablyValues = probablyValue
 	return result
 }
