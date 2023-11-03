1925fd680188bf85f9d29891c20853a169932e35
wlz <lingze.wl@gmail.com>
2023-11-03 15:12:41 +0800

    feat(ssa) complement opcode support

--- common/yak/ssaapi/values.go
+++ common/yak/ssaapi/values.go
@@ -23,26 +23,24 @@ func (value Values) Ref(name string) Values {
 	return getValuesWithUpdate(ret)
 }
 
-// func (v Values) UseDefChain(f func(*Value, *UseDefChain)) {
-// 	for _, v := range v {
-// 		f(v, defaultUseDefChain(v))
-// 	}
-// }
-
-func (v Values) String() string {
+func (v Values) StringEx(flag int) string {
 	ret := ""
 	ret += fmt.Sprintf("Values: %d\n", len(v))
 	for i, v := range v {
-		ret += fmt.Sprintf("\t%d: %5s: %s\n", i, v.node.GetOpcode(), v)
+		switch flag {
+		case 0:
+			ret += fmt.Sprintf("\t%d: %5s: %s\n", i, v.node.GetOpcode(), v)
+		case 1:
+			ret += fmt.Sprintf("\t%d: %s\n", i, v.StringWithSource())
+		}
 	}
 	return ret
 }
 
-func (v Values) Show() { fmt.Println(v) }
+func (v Values) Show()           { fmt.Println(v.StringEx(0)) }
+func (v Values) ShowWithSource() { fmt.Println(v.StringEx(1)) }
 
-func (v Values) Get(i int) *Value {
-	return v[i]
-}
+func (v Values) Get(i int) *Value { return v[i] }
 
 func (v Values) ForEach(f func(*Value)) {
 	for _, v := range v {
@@ -60,10 +58,11 @@ func NewValue(n ssa.InstructionNode) *Value {
 	}
 }
 func (v *Value) String() string { return v.node.LineDisasm() }
-func (i *Value) Show()          { fmt.Println(i) }
-func (i *Value) ShowWithSource() {
-	fmt.Printf("[%6s] %s\t%s\n", i.node.GetOpcode(), i.node.LineDisasm(), i.node.GetPosition())
+func (i *Value) StringWithSource() string {
+	return fmt.Sprintf("[%-6s] %s\t%s", i.node.GetOpcode(), i.node.LineDisasm(), i.node.GetPosition())
 }
+func (i *Value) Show()           { fmt.Println(i) }
+func (i *Value) ShowWithSource() { fmt.Println(i.StringWithSource()) }
 
 func (v *Value) IsSame(other *Value) bool { return *v == *other }
 
@@ -99,16 +98,7 @@ func (value *Value) ShowUseDefChain() {
 	defaultUseDefChain(value).Show()
 }
 
-func (v *Value) IsUpdate() bool { return v.node.GetOpcode() == ssa.OpUpdate }
-func (v *Value) IsConst() bool  { return v.node.GetOpcode() == ssa.OpConst }
-func (v *Value) IsBinOp() bool  { return v.node.GetOpcode() == ssa.OpBinOp }
-func (v *Value) IsCall() bool   { return v.node.GetOpcode() == ssa.OpCall }
-func (v *Value) IsField() bool  { return v.node.GetOpcode() == ssa.OpField }
-
 // for function
-func (v *Value) IsFunction() bool {
-	return v.node.GetOpcode() == ssa.OpFunction
-}
 
 func (v *Value) GetReturn() Values {
 	ret := make(Values, 0)
@@ -129,3 +119,27 @@ func (v *Value) GetParameter() Values {
 	}
 	return ret
 }
+
+func (v *Value) IsFunction() bool     { return v.node.GetOpcode() == ssa.OpFunction }
+func (v *Value) IsBasicBlock() bool   { return v.node.GetOpcode() == ssa.OpBasicBlock }
+func (v *Value) IsPhi() bool          { return v.node.GetOpcode() == ssa.OpPhi }
+func (v *Value) IsConstInst() bool    { return v.node.GetOpcode() == ssa.OpConstInst }
+func (v *Value) IsUndefined() bool    { return v.node.GetOpcode() == ssa.OpUndefined }
+func (v *Value) IsBinOp() bool        { return v.node.GetOpcode() == ssa.OpBinOp }
+func (v *Value) IsUnOp() bool         { return v.node.GetOpcode() == ssa.OpUnOp }
+func (v *Value) IsCall() bool         { return v.node.GetOpcode() == ssa.OpCall }
+func (v *Value) IsReturn() bool       { return v.node.GetOpcode() == ssa.OpReturn }
+func (v *Value) IsMake() bool         { return v.node.GetOpcode() == ssa.OpMake }
+func (v *Value) IsField() bool        { return v.node.GetOpcode() == ssa.OpField }
+func (v *Value) IsUpdate() bool       { return v.node.GetOpcode() == ssa.OpUpdate }
+func (v *Value) IsNext() bool         { return v.node.GetOpcode() == ssa.OpNext }
+func (v *Value) IsAssert() bool       { return v.node.GetOpcode() == ssa.OpAssert }
+func (v *Value) IsTypeCast() bool     { return v.node.GetOpcode() == ssa.OpTypeCast }
+func (v *Value) IsTypeValue() bool    { return v.node.GetOpcode() == ssa.OpTypeValue }
+func (v *Value) IsErrorHandler() bool { return v.node.GetOpcode() == ssa.OpErrorHandler }
+func (v *Value) IsPanic() bool        { return v.node.GetOpcode() == ssa.OpPanic }
+func (v *Value) IsRecover() bool      { return v.node.GetOpcode() == ssa.OpRecover }
+func (v *Value) IsJump() bool         { return v.node.GetOpcode() == ssa.OpJump }
+func (v *Value) IsIf() bool           { return v.node.GetOpcode() == ssa.OpIf }
+func (v *Value) IsLoop() bool         { return v.node.GetOpcode() == ssa.OpLoop }
+func (v *Value) IsSwitch() bool       { return v.node.GetOpcode() == ssa.OpSwitch }
