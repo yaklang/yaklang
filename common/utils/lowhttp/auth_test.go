369c3bb6379e304f1895b4725c34dfe7c272e21d
rookie <rookie_is@qq.com>
2023-11-03 13:33:06 +0800

    feat(lowhttp):取消外部测试

--- common/utils/lowhttp/auth_test.go
+++ common/utils/lowhttp/auth_test.go
@@ -1,26 +1,26 @@
 package lowhttp
 
-import (
-	"fmt"
-	"github.com/davecgh/go-spew/spew"
-	"github.com/yaklang/yaklang/common/utils/bruteutils/grdp/protocol/nla"
-	"github.com/yaklang/yaklang/common/yak/yaklib/codec"
-	"testing"
-)
-
-func TestNtlmV2(t *testing.T) { // not ci
-	rsp, err := HTTPWithoutRedirect(WithPacketBytes([]byte("GET / HTTP/1.1\r\nHost: 117.50.163.235\r\n\r\n")), WithUsername("test"), WithPassword("test123"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	spew.Dump(rsp)
-}
-
-func TestResp(t *testing.T) {
-	auth := nla.NewNTLMv2("", "test", "test123")
-	auth.GetNegotiateMessage()
-	challenge, _ := codec.DecodeBase64("TlRMTVNTUAACAAAAHgAeADgAAAAFAoqibpP+q8pzH8cAAAAAAAAAAJAAkABWAAAACgBjRQAAAA9XAEkATgAtADAANgAxAEUAVgBJAEgASwBEADgANgACAB4AVwBJAE4ALQAwADYAMQBFAFYASQBIAEsARAA4ADYAAQAeAFcASQBOAC0AMAA2ADEARQBWAEkASABLAEQAOAA2AAQAGgAxADAALQA2ADAALQAxADQAOQAtADEANQAxAAMAGgAxADAALQA2ADAALQAxADQAOQAtADEANQAxAAcACACHI0ZLqw3aAQAAAAA=")
-	authMessage, _ := auth.GetAuthenticateMessage(challenge)
-	authstring := authMessage.Serialize()
-	fmt.Println(codec.EncodeBase64(authstring))
-}
+//import (
+//	"fmt"
+//	"github.com/davecgh/go-spew/spew"
+//	"github.com/yaklang/yaklang/common/utils/bruteutils/grdp/protocol/nla"
+//	"github.com/yaklang/yaklang/common/yak/yaklib/codec"
+//	"testing"
+//)
+//
+//func TestNtlmV2(t *testing.T) { // not ci
+//	rsp, err := HTTPWithoutRedirect(WithPacketBytes([]byte("GET / HTTP/1.1\r\nHost: 117.50.163.235\r\n\r\n")), WithUsername("test"), WithPassword("test123"))
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//	spew.Dump(rsp)
+//}
+//
+//func TestResp(t *testing.T) {
+//	auth := nla.NewNTLMv2("", "test", "test123")
+//	auth.GetNegotiateMessage()
+//	challenge, _ := codec.DecodeBase64("TlRMTVNTUAACAAAAHgAeADgAAAAFAoqibpP+q8pzH8cAAAAAAAAAAJAAkABWAAAACgBjRQAAAA9XAEkATgAtADAANgAxAEUAVgBJAEgASwBEADgANgACAB4AVwBJAE4ALQAwADYAMQBFAFYASQBIAEsARAA4ADYAAQAeAFcASQBOAC0AMAA2ADEARQBWAEkASABLAEQAOAA2AAQAGgAxADAALQA2ADAALQAxADQAOQAtADEANQAxAAMAGgAxADAALQA2ADAALQAxADQAOQAtADEANQAxAAcACACHI0ZLqw3aAQAAAAA=")
+//	authMessage, _ := auth.GetAuthenticateMessage(challenge)
+//	authstring := authMessage.Serialize()
+//	fmt.Println(codec.EncodeBase64(authstring))
+//}
