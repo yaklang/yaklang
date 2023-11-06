e4314ef50a90511b31ab7660e31232ae9a0db25e
WAY29 <41830147+WAY29@users.noreply.github.com>
2023-11-06 11:38:52 +0800

    doc(git):

--- common/utils/yakgit/commit.go
+++ common/utils/yakgit/commit.go
@@ -8,6 +8,11 @@ import (
 	"github.com/yaklang/yaklang/common/utils"
 )
 
+// IterateCommit 用于指定一个本地仓库，遍历其所有的提交记录(commit)，并对过滤后的每个提交记录执行指定的操作，它还可以接收零个到多个选项函数，用于配置回调函数
+// Example:
+// ```
+// git.IterateCommit("D:/coding/golang/src/yaklang", git.filterReference((ref) => {return !ref.Name().Contains("ci")}), git.filterCommit((c) => { return c.Author.Name != "xxx" }), git.handleCommit((c) => { println(c.String()) })) // 遍历提交记录，过滤名字中包含ci的引用记录，过滤作者名字为xxx的提交记录，打印剩余的每个提交记录
+// ```
 func EveryCommit(localRepos string, opt ...Option) error {
 	c := NewConfig()
 	for _, i := range opt {
@@ -45,6 +50,7 @@ func EveryCommit(localRepos string, opt ...Option) error {
 			if c.FilterGitCommit != nil && !c.FilterGitCommit(commit) {
 				return nil
 			}
+
 			if c.HandleGitCommit != nil {
 				err := c.HandleGitCommit(commit)
 				if err != nil {
@@ -52,8 +58,6 @@ func EveryCommit(localRepos string, opt ...Option) error {
 				}
 			}
 
-			// handle files?
-			log.Infof("found: %v", commit.String())
 			return nil
 		})
 		return nil
