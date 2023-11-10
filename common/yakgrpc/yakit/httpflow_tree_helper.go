6d0982dc513dc4525ed750b6734723b99178ab1a
v1ll4n <v1ll4n.a5k@gmail.com>
2023-11-10 16:23:49 +0800

    website nextpart query from database

---
+++ common/yakgrpc/yakit/httpflow_tree_helper.go
@@ -0,0 +1,112 @@
+package yakit
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/yaklang/yaklang/common/log"
+	"sort"
+	"strings"
+)
+
+func FilterHTTPFlowBySchema(db *gorm.DB, schema string) *gorm.DB {
+	if schema != "" {
+		db = db.Where("url LIKE ?", schema+"://")
+	}
+	return db
+}
+
+type WebsiteNextPart struct {
+	NextPart     string
+	HaveChildren bool
+	Count        int
+}
+
+func trimOneSlash(path string) string {
+	if strings.HasPrefix(path, "/") {
+		path = path[1:]
+	}
+	if strings.HasSuffix(path, "/") {
+		path = path[:len(path)-1]
+	}
+	return path
+}
+
+func GetHTTPFlowNextPartPathByPathPrefix(db *gorm.DB, originPathPrefix string) []*WebsiteNextPart {
+	if originPathPrefix != "" {
+		pathPrefix := trimOneSlash(originPathPrefix)
+		db := FilterHTTPFlowPathPrefix(db, originPathPrefix)
+		db = db.Select(fmt.Sprintf(`DISTINCT SUBSTR(
+   url,
+   INSTR(url, '://') + 3 + INSTR(SUBSTR(url, INSTR(url, '://') + 3), '/') + %d,
+   CASE
+	   WHEN INSTR(SUBSTR(url, INSTR(url, '://') + 3), ?) > 0
+		   THEN
+			   INSTR(SUBSTR(url, INSTR(url, '://') + 3), ?) -
+			   INSTR(SUBSTR(url, INSTR(url, '://') + 3), '/') -
+			   1 - %d
+	   ELSE LENGTH(url)
+	   END
+) as next_part`, len(pathPrefix), len(pathPrefix)), "?", "?").Table("http_flows").Limit(1000) // .Debug()
+		if rows, err := db.Rows(); err != nil {
+			log.Error("query nextPart for website tree failed: %s", err)
+			return nil
+		} else {
+			var resultMap = make(map[string]*WebsiteNextPart)
+			for rows.Next() {
+				var nextPart string
+				rows.Scan(&nextPart)
+				if nextPart == "" {
+					continue
+				}
+				if nextPart[0] == '/' {
+					nextPart = nextPart[1:]
+				}
+				haveChildren := false
+				nextPartItem, after, splited := strings.Cut(nextPart, "/")
+				if splited && after != "" {
+					haveChildren = true
+				}
+				if result, ok := resultMap[nextPartItem]; ok {
+					result.Count++
+				} else {
+					resultMap[nextPartItem] = &WebsiteNextPart{
+						NextPart: nextPartItem, HaveChildren: haveChildren, Count: 1,
+					}
+				}
+			}
+			var data []*WebsiteNextPart
+			for _, r := range resultMap {
+				data = append(data, r)
+			}
+			sort.SliceStable(data, func(i, j int) bool {
+				return data[i].NextPart > data[j].NextPart
+			})
+			return data
+		}
+	}
+	return nil
+}
+
+func FilterHTTPFlowPathPrefix(db *gorm.DB, pathPrefix string) *gorm.DB {
+	if pathPrefix != "" {
+		pathPrefix := trimOneSlash(pathPrefix)
+		template := `SUBSTR(url,INSTR(url, '://') + 3 + INSTR(SUBSTR(url, INSTR(url, '://') + 3), '/'),CASE WHEN INSTR(SUBSTR(url, INSTR(url, '://') + 3), ?) > 0 THEN INSTR(SUBSTR(url, INSTR(url, '://') + 3), ?) - INSTR(SUBSTR(url, INSTR(url, '://') + 3), '/') - 1 ELSE LENGTH(url) END) LIKE ?`
+		db = db.Where(template, "?", "?", pathPrefix+"%")
+	}
+	return db
+}
+
+func FilterHTTPFlowByDomain(db *gorm.DB, domain string) *gorm.DB {
+	// query url
+	// schema://domain
+	// no '/' in domain and schema
+	if strings.Contains(domain, "%") {
+		domain = strings.ReplaceAll(domain, "%", "%%")
+		domain = strings.Trim(domain, "%")
+	}
+
+	if domain != "" {
+		db = db.Where(`SUBSTR(url, INSTR(url, '://') + 3, INSTR(SUBSTR(url, INSTR(url, '://') + 3), '/') - 1) LIKE ?`, "%"+domain)
+	}
+	return db
+}
