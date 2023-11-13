f5168cdf5a6dd0329ab73e6e80d5e007c94679b4
wlz <lingze.wl@gmail.com>
2023-11-13 14:07:03 +0800

    feat(grpc) evaluate batch add message info

--- common/yakgrpc/grpc_smoking_evaluate_plugin_batch.go
+++ common/yakgrpc/grpc_smoking_evaluate_plugin_batch.go
@@ -2,6 +2,7 @@ package yakgrpc
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
@@ -19,27 +20,34 @@ func (s *Server) SmokingEvaluatePluginBatch(req *ypb.SmokingEvaluatePluginBatchR
 			MessageType: messageType,
 		})
 	}
-	// names := make([]string, 0, len(req.GetScriptNames()))
+	names := make([]string, 0, len(req.GetScriptNames()))
 
 	pluginSize := len(req.GetScriptNames())
 	for index, name := range req.GetScriptNames() {
 		progress := float64(index+1) / float64(pluginSize)
-		fmt.Println("check name: ", index, name, pluginSize, progress)
+		// fmt.Println("check name: ", index, name, pluginSize, progress)
 		ins, err := yakit.GetYakScriptByName(s.GetProfileDatabase(), name)
 		if err != nil {
-			send(progress, name, "error")
+			msg := fmt.Sprintf("%s: 无法获取该插件", name)
+			send(progress, msg, "error")
 		}
 		code := ins.Content
 		pluginType := ins.Type
-		if res, err := s.EvaluatePlugin(stream.Context(), code, pluginType); err == nil {
-			if res.Score > 60 {
-				send(progress, name, "success")
-				// names = append(names, name)
-				continue
-			}
+		res, err := s.EvaluatePlugin(stream.Context(), code, pluginType)
+		if err != nil {
+			msg := fmt.Sprintf("%s 启动插件检测失败", name)
+			send(progress, msg, "error")
 		}
-		send(progress, name, "error")
+		if res.Score >= 60 {
+			msg := fmt.Sprintf("%s 插件得分: %d", name, res.Score)
+			send(progress, msg, "success")
+			names = append(names, name)
+			continue
+		} else {
+			msg := fmt.Sprintf("%s 插件得分: %d (<60)", name, res.Score)
+			send(progress, msg, "error")
+		}
 	}
-	// send(1, strings.Join(names, ","), "success-again")
+	send(2, strings.Join(names, ","), "success-again")
 	return nil
 }
