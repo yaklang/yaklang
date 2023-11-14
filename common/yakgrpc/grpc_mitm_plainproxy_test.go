a729c888589b7a978bebc12123702d555c8d5b59
rookie <rookie_is@qq.com>
2023-11-14 11:15:05 +0800

    fix(filter_test):单元测试添加清理过滤器，以防影响其他测试

--- common/yakgrpc/grpc_mitm_plainproxy_test.go
+++ common/yakgrpc/grpc_mitm_plainproxy_test.go
@@ -128,6 +128,10 @@ conn.Close()
 		t.Fatal(err)
 	}
 
+	if len(data.GetData()) < 1 {
+		t.Fatalf("need more than 1 data,but got %d", len(data.GetData()))
+	}
+
 	request := string(data.GetData()[0].Request)
 	response := string(data.GetData()[0].Response)
 	if utils.MatchAnyOfSubString(request, "Proxy-Connection: ", "GET http://", "GET https://") {
