63d96ecd52064b6a2745641f115d412d47ba9fdb
WAY29 <41830147+WAY29@users.noreply.github.com>
2023-11-15 11:05:41 +0800

    doc(env):

--- common/yak/yaklib/env.go
+++ common/yak/yaklib/env.go
@@ -2,11 +2,27 @@ package yaklib
 
 import "os"
 
+// Get 获取对应键名的环境变量值
+// ! 已弃用，可以使用 `os.Getenv` 代替
+// Example:
+// ```
+// env.Get("PATH")
+// ```
+func _getEnv(key string) string {
+	return os.Getenv(key)
+}
+
+// Set 设置对应键名的环境变量值
+// ! 已弃用，可以使用 `os.Setenv` 代替
+// Example:
+// ```
+// env.Set("YAK_PROXY", "http://127.0.0.1:10808")
+// ```
+func _setEnv(key string, value string) {
+	os.Setenv(key, value)
+}
+
 var EnvExports = map[string]interface{}{
-	"Get": func(key string) string {
-		return os.Getenv(key)
-	},
-	"Set": func(key string, value string) {
-		_ = os.Setenv(key, value)
-	},
+	"Get": _getEnv,
+	"Set": _setEnv,
 }
