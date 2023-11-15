a440736889a1adb7058fdc2cb99dc287da29fc0a
wlz <lingze.wl@gmail.com>
2023-11-15 16:34:26 +0800

    feat(ssa): phi lineDisasm: "phi(i)[0, i+1"

--- common/yak/ssa/disasmLine.go
+++ common/yak/ssa/disasmLine.go
@@ -4,18 +4,57 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+	"sync"
 
 	"github.com/samber/lo"
 )
 
-func lineDisasm(vs Values) string {
+var symbol map[Value]string
+var symbolLock sync.RWMutex
+
+func init() {
+	symbol = make(map[Value]string)
+}
+
+func setSymbolVariable(v Value, name string) {
+	symbolLock.Lock()
+	defer symbolLock.Unlock()
+	symbol[v] = name
+}
+
+func unsetSymbolVariable(v Value) {
+	symbolLock.Lock()
+	defer symbolLock.Unlock()
+	delete(symbol, v)
+}
+
+func readSymbolVariable(v Value) string {
+	symbolLock.RLock()
+	defer symbolLock.RUnlock()
+	if name, ok := symbol[v]; ok {
+		return name
+	} else {
+		return ""
+	}
+}
+
+func lineDisasm(v Value) string {
+	if id := readSymbolVariable(v); id != "" {
+		return id
+	} else {
+		return v.LineDisasm()
+	}
+}
+
+func lineDisasms(vs Values) string {
 	return strings.Join(
 		lo.Map(vs, func(a Value, _ int) string {
-			return a.LineDisasm()
+			return lineDisasm(a)
 		}),
 		",",
 	)
 }
+
 func (f *Function) LineDisasm() string {
 	return fmt.Sprintf("%s", f.GetVariable())
 	// return fmt.Sprintf("fun(%s)", f.GetVariable())
@@ -32,7 +71,14 @@ func (p *Parameter) LineDisasm() string {
 }
 
 func (p *Phi) LineDisasm() string {
-	return fmt.Sprintf("phi(%s)", lineDisasm(p.Edge))
+	setSymbolVariable(p, p.GetVariable())
+	ret := fmt.Sprintf("phi(%s)[%s]", p.GetVariable(), lineDisasms(p.Edge))
+	unsetSymbolVariable(p)
+	return ret
+	// ret := p.GetVariable()
+	// ret := ""
+
+	// return ret
 }
 
 func (c *ConstInst) LineDisasm() string {
@@ -50,33 +96,31 @@ func (u *Undefined) LineDisasm() string {
 }
 
 func (b *BinOp) LineDisasm() string {
-	return fmt.Sprintf("%s(%s, %s)", BinaryOpcodeName[b.Op], b.X.LineDisasm(), b.Y.LineDisasm())
-	// return fmt.Sprintf("%s %s %s)", b.X.LineDisasm(), BinaryOpcodeName[b.Op], b.Y.LineDisasm())
+	return fmt.Sprintf("%s(%s, %s)", BinaryOpcodeName[b.Op], lineDisasm(b.X), lineDisasm(b.Y))
 }
 
 func (u *UnOp) LineDisasm() string {
-	return fmt.Sprintf("%s(%s)", UnaryOpcodeName[u.Op], u.X.LineDisasm())
-	// return fmt.Sprintf("%s%s", UnaryOpcodeName[u.Op], u.X.LineDisasm())
+	return fmt.Sprintf("%s(%s)", UnaryOpcodeName[u.Op], lineDisasm(u.X))
 }
 
 func (c *Call) LineDisasm() string {
 	arg := ""
 	if len(c.Args) != 0 {
-		arg = lineDisasm(c.Args)
+		arg = lineDisasms(c.Args)
 	}
 	binding := ""
 	if len(c.binding) != 0 {
-		binding = ", binding(" + lineDisasm(c.binding) + ")"
+		binding = ", binding(" + lineDisasms(c.binding) + ")"
 	}
 
 	return fmt.Sprintf("%s(%s%s)",
-		c.Method.LineDisasm(),
+		lineDisasm(c.Method),
 		arg, binding,
 	)
 }
 
 func (s *SideEffect) LineDisasm() string {
-	return fmt.Sprintf("side-effect(%s, %s)", s.target.LineDisasm(), s.GetVariable())
+	return fmt.Sprintf("side-effect(%s, %s)", lineDisasm(s.target), s.GetVariable())
 }
 
 func (m *Make) LineDisasm() string {
@@ -84,21 +128,19 @@ func (m *Make) LineDisasm() string {
 }
 
 func (f *Field) LineDisasm() string {
-	// return fmt.Sprintf("field(%s, %s)", f.Obj.LineDisasm(), f.Key.LineDisasm())
-	return fmt.Sprintf("%s.%s", f.Obj.LineDisasm(), f.Key.LineDisasm())
-	// return fmt.Sprintf("%s[%s]", f.Obj.LineDisasm(), f.Key.LineDisasm())
+	return fmt.Sprintf("%s.%s", lineDisasm(f.Obj), lineDisasm(f.Key))
 }
 
 func (u *Update) LineDisasm() string {
-	return fmt.Sprintf("update(%s, %s)", u.Address.LineDisasm(), u.Value.LineDisasm())
+	return fmt.Sprintf("update(%s, %s)", lineDisasm(u.Address), lineDisasm(u.Value))
 }
 
 func (u *Next) LineDisasm() string {
-	return fmt.Sprintf("next(%s)", u.Iter.LineDisasm())
+	return fmt.Sprintf("next(%s)", lineDisasm(u.Iter))
 }
 
 func (t *TypeCast) LineDisasm() string {
-	return fmt.Sprintf("castType(%s, %s)", t.GetType().String(), t.Value.LineDisasm())
+	return fmt.Sprintf("castType(%s, %s)", t.GetType().String(), lineDisasm(t.Value))
 }
 
 func (t *TypeValue) LineDisasm() string {
@@ -110,34 +152,34 @@ func (r *Recover) LineDisasm() string {
 }
 
 func (r *Return) LineDisasm() string {
-	return fmt.Sprintf("return(%s)", lineDisasm(r.Results))
+	return fmt.Sprintf("return(%s)", lineDisasms(r.Results))
 }
 
 func (a *Assert) LineDisasm() string {
-	return fmt.Sprintf("assert(%s, %s)", a.Cond.LineDisasm(), a.MsgValue.LineDisasm())
+	return fmt.Sprintf("assert(%s, %s)", lineDisasm(a.Cond), lineDisasm(a.MsgValue))
 }
 
 func (p *Panic) LineDisasm() string {
-	return fmt.Sprintf("panic(%s)", p.Info.LineDisasm())
+	return fmt.Sprintf("panic(%s)", lineDisasm(p.Info))
 }
 
 func (p *Jump) LineDisasm() string         { return "" }
 func (p *ErrorHandler) LineDisasm() string { return "" }
 
 func (i *If) LineDisasm() string {
-	return fmt.Sprintf("if (%s) {%s} else {%s}", i.Cond.LineDisasm(), i.True.LineDisasm(), i.False.LineDisasm())
+	return fmt.Sprintf("if (%s) {%s} else {%s}", lineDisasm(i.Cond), lineDisasm(i.True), lineDisasm(i.False))
 }
 func (l *Loop) LineDisasm() string {
-	return fmt.Sprintf("loop(%s)", l.Cond.LineDisasm())
+	return fmt.Sprintf("loop(%s)", lineDisasm(l.Cond))
 }
 
 func (s Switch) LineDisasm() string {
 	return fmt.Sprintf(
 		"switch(%s) {case:%s}",
-		s.Cond.LineDisasm(),
+		lineDisasm(s.Cond),
 		strings.Join(
 			lo.Map(s.Label, func(label SwitchLabel, _ int) string {
-				return fmt.Sprintf("%s: %s", label.Value.LineDisasm(), label.Dest.LineDisasm())
+				return fmt.Sprintf("%s: %s", lineDisasm(label.Value), lineDisasm(label.Dest))
 			}),
 			",",
 		),
