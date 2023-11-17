992a72e18c7808bbd6313eaf577088c03273668f
v1ll4n <v1ll4n.a5k@gmail.com>
2023-11-17 15:18:03 +0800

    fix udp bug

--- common/yak/yaktest/mustpass/mustpass_base_test.go
+++ common/yak/yaktest/mustpass/mustpass_base_test.go
@@ -50,7 +50,7 @@ func init() {
 }
 
 func TestMustPassDebug(t *testing.T) {
-	var debugName = "nuclei-json-format.yak"
+	var debugName = "udp2.yak"
 	var debugCases [][]string
 	for k, v := range files {
 		if k == debugName {
