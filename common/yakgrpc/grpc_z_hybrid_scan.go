64bb7c191840bbf8f2b471a5cf6994ad07d0058c
VillanCh <188228725@qq.com>
2023-11-18 00:11:07 +0800

    add some log for debug

--- common/yakgrpc/grpc_z_hybrid_scan.go
+++ common/yakgrpc/grpc_z_hybrid_scan.go
@@ -1,6 +1,7 @@
 package yakgrpc
 
 import (
+	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 	"strings"
@@ -18,6 +19,7 @@ func (s *Server) HybridScan(stream ypb.Yak_HybridScanServer) error {
 
 	switch strings.ToLower(firstRequest.HybridScanMode) {
 	case "new":
+		log.Info("start to create new hybrid scan task")
 		return s.hybridScanNewTask(stream, firstRequest)
 	default:
 		return utils.Error("not implemented")
