591fe44e3fd6c05a6ed048a2838519e125feedba
WAY29 <41830147+WAY29@users.noreply.github.com>
2023-11-17 11:15:20 +0800

    test(yakdoc):

--- common/yak/yakdoc/generate_doc/generate_doc_test.go
+++ common/yak/yakdoc/generate_doc/generate_doc_test.go
@@ -11,6 +11,71 @@ import (
 	"github.com/yaklang/yaklang/common/yak/yaklang"
 )
 
+func CheckDocumentHelper(t *testing.T, helper *yakdoc.DocumentHelper) {
+	t.Helper()
+
+	checkFuncDecl := func(decl *yakdoc.FuncDecl) {
+		t.Helper()
+		libName, methodName := decl.LibName, decl.MethodName
+		if decl.Decl == "" {
+			t.Fatalf("%s.%s check failed: decl empty", libName, methodName)
+		}
+		if decl.Document == "" {
+			t.Fatalf("%s.%s check failed: document empty", libName, methodName)
+		}
+		if decl.VSCodeSnippets == "" {
+			t.Fatalf("%s.%s check failed: vscode snippets empty", libName, methodName)
+		}
+	}
+
+	checkNormalFunction := func(libName string, methodName string) {
+		t.Helper()
+		var decl *yakdoc.FuncDecl
+		var ok bool
+
+		if libName == "" {
+			decl, ok = helper.Functions[methodName]
+			if !ok {
+				t.Fatalf("%s.%s check failed: function not exist", libName, methodName)
+			}
+		} else {
+			lib, ok := helper.Libs[libName]
+			if !ok {
+				t.Fatalf("%s.%s check failed: library not exist", libName, methodName)
+			}
+			decl, ok = lib.Functions[methodName]
+			if !ok {
+				t.Fatalf("%s.%s check failed: function not exist", libName, methodName)
+			}
+		}
+		checkFuncDecl(decl)
+	}
+
+	checkStructMethods := func(structName string, method ...string) {
+		lib, ok := helper.StructMethods[structName]
+		if !ok {
+			t.Fatalf("%s check failed: struct not exist", structName)
+		}
+		if len(method) == 0 {
+			for _, decl := range lib.Functions {
+				checkFuncDecl(decl)
+			}
+		} else {
+			for _, methodName := range method {
+				decl, ok := lib.Functions[methodName]
+				if !ok {
+					t.Fatalf("%s.%s check failed: function not exist", structName, methodName)
+				}
+				checkFuncDecl(decl)
+			}
+		}
+	}
+
+	checkNormalFunction("", "eval")
+	checkNormalFunction("", "yakfmt")
+	checkStructMethods("github.com/yaklang/yaklang/common/mutate.FuzzHTTPRequestIf", "Repeat")
+}
+
 func TestGetnerateDoc(t *testing.T) {
 	helper := yak.EngineToDocumentHelperWithVerboseInfo(yaklang.New())
 	var buf bytes.Buffer
@@ -33,4 +98,5 @@ func TestGetnerateDoc(t *testing.T) {
 	if err := decoder.Decode(&newHelper); err != nil {
 		t.Fatalf("load embed yak document error: %v", err)
 	}
+	CheckDocumentHelper(t, newHelper)
 }
