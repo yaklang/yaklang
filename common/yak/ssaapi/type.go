ba5caf3722babafc61aa21cc27355808e745f166
wlz <lingze.wl@gmail.com>
2023-11-17 16:51:56 +0800

    feat(ssaapi): create type

--- common/yak/ssaapi/type.go
+++ common/yak/ssaapi/type.go
@@ -1,6 +1,9 @@
 package ssaapi
 
-import "github.com/yaklang/yaklang/common/yak/ssa"
+import (
+	"github.com/samber/lo"
+	"github.com/yaklang/yaklang/common/yak/ssa"
+)
 
 type Type struct {
 	t ssa.Type
@@ -22,6 +25,30 @@ func (t *Type) Compare(t2 *Type) bool {
 	return TypeCompare(t, t2)
 }
 
-func (t *Type) GetKind() ssa.TypeKind {
-	return t.t.GetTypeKind()
+var (
+	Number        = newType(ssa.BasicTypes[ssa.Number])
+	String        = newType(ssa.BasicTypes[ssa.String])
+	Bytes         = newType(ssa.BasicTypes[ssa.Bytes])
+	Boolean       = newType(ssa.BasicTypes[ssa.Boolean])
+	UndefinedType = newType(ssa.BasicTypes[ssa.UndefinedType])
+	Null          = newType(ssa.BasicTypes[ssa.Null])
+	Any           = newType(ssa.BasicTypes[ssa.Any])
+	ErrorType     = newType(ssa.BasicTypes[ssa.ErrorType])
+)
+
+func SliceOf(t *Type) *Type {
+	return newType(ssa.NewSliceType(t.t))
+}
+
+func MapOf(key, value *Type) *Type {
+	return newType(ssa.NewMapType(key.t, value.t))
+}
+
+func FuncOf(name string, args, ret []*Type, isVariadic bool) *Type {
+	return newType(
+		ssa.NewFunctionTypeDefine(name,
+			lo.Map(args, func(t *Type, _ int) ssa.Type { return t.t }),
+			lo.Map(ret, func(t *Type, _ int) ssa.Type { return t.t }),
+			isVariadic),
+	)
 }
