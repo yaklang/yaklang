ab94fc2076cf7bd5f4cfee9cc9b5463c84596bc6
WAY29 <41830147+WAY29@users.noreply.github.com>
2023-11-17 11:33:32 +0800

    feat(context): export WithTimeoutSeconds

--- common/yak/yaklib/context.go
+++ common/yak/yaklib/context.go
@@ -2,15 +2,25 @@ package yaklib
 
 import (
 	"context"
+
 	"github.com/yaklang/yaklang/common/utils"
 )
 
+func _seconds(d float64) context.Context {
+	return utils.TimeoutContextSeconds(d)
+}
+
+func _withTimeoutSeconds(d float64) context.Context {
+	return utils.TimeoutContextSeconds(d)
+}
+
 var ContextExports = map[string]interface{}{
-	"Seconds":      utils.TimeoutContextSeconds,
-	"New":          context.Background,
-	"Background":   context.Background,
-	"WithCancel":   context.WithCancel,
-	"WithTimeout":  context.WithTimeout,
-	"WithDeadline": context.WithDeadline,
-	"WithValue":    context.WithValue,
+	"Seconds":            _seconds,
+	"New":                context.Background,
+	"Background":         context.Background,
+	"WithCancel":         context.WithCancel,
+	"WithTimeout":        context.WithTimeout,
+	"WithTimeoutSeconds": _withTimeoutSeconds,
+	"WithDeadline":       context.WithDeadline,
+	"WithValue":          context.WithValue,
 }
