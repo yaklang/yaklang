45c5b62ff6fd15641dd53b91e3e88673554f519e
WAY29 <41830147+WAY29@users.noreply.github.com>
2023-11-22 11:06:46 +0800

    test(ssa-rules-cli): add paramName test

--- common/yak/static_analyzer_test.go
+++ common/yak/static_analyzer_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestCounter(t *testing.T) {
 	_ = yaklang.New()
-	var count = 0
+	count := 0
 	for key, value := range yaklangspec.Fntable {
 		val, ok := value.(map[string]interface{})
 		if !ok {
@@ -44,36 +44,50 @@ func check(t *testing.T, code string, want []string) {
 	}
 }
 
-func TestSSARuleMustPassYakCliParameter(t *testing.T) {
+func TestSSARuleMustPassYakCliParamName(t *testing.T) {
+	t.Run("cli same paramName", func(t *testing.T) {
+		check(t, `
+cli.String("a")
+cli.String("a")
+cli.check()
+	`, []string{rules.ErrorStrSameParamName("a", 2)})
+	})
 
+	t.Run("cli invalid paramName", func(t *testing.T) {
+		check(t, `
+cli.String("!@#")
+cli.String("a")
+cli.check()
+	`, []string{rules.ErrorStrInvalidParamName("!@#")})
+	})
+}
+
+func TestSSARuleMustPassYakCliCheck(t *testing.T) {
 	t.Run("cli with check", func(t *testing.T) {
 		check(t, `
 	cli.String("a")
 	cli.check()
 		`, []string{})
 	})
-
 	t.Run("cli not check", func(t *testing.T) {
 		check(t, `
-	cli.String("a")
-		`, []string{
-			rules.NotCallCliCheck(),
+		cli.String("a")
+			`, []string{
+			rules.ErrorStrNotCallCliCheck(),
 		})
 	})
-
 	t.Run("cli not check in last", func(t *testing.T) {
 		check(t, `
-	cli.String("a")
-	cli.check()
-	cli.String("b")
-		`, []string{
-			rules.NotCallCliCheck(),
+		cli.String("a")
+		cli.check()
+		cli.String("b")
+			`, []string{
+			rules.ErrorStrNotCallCliCheck(),
 		})
 	})
-
 	t.Run("not cli function", func(t *testing.T) {
 		check(t, `
-		println("aaaa")
-		`, []string{})
+			println("aaaa")
+			`, []string{})
 	})
 }
