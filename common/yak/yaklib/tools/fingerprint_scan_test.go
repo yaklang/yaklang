c4ecc78f086ef94f420278a4f400230d89eadad1
go0p <gp827782797@qq.com>
2023-11-22 15:04:48 +0800

    fix(synscan) exclude port option

--- common/yak/yaklib/tools/fingerprint_scan_test.go
+++ common/yak/yaklib/tools/fingerprint_scan_test.go
@@ -3,7 +3,10 @@ package tools
 import (
 	"fmt"
 	"github.com/yaklang/yaklang/common/fp"
+	"github.com/yaklang/yaklang/common/synscan"
+	"sync"
 	"testing"
+	"time"
 )
 
 func Test_scanFingerprint(t *testing.T) {
@@ -31,4 +34,67 @@ func Test_scanFingerprint(t *testing.T) {
 	for v := range ch {
 		fmt.Println(v.String())
 	}
+}
+
+func Test_scanFingerprint1(t *testing.T) {
+	target := "192.168.3.104"
+
+	tcpPorts := "3306,9090"
+	synPorts := "6379,9090"
+
+	tcpScan := func(addr string) {
+		ch, err := scanFingerprint(
+			addr, tcpPorts,
+		)
+
+		if err != nil {
+			t.FailNow()
+		}
+
+		for v := range ch {
+			fmt.Println("TCPGOT " + v.String())
+		}
+	}
+
+	Scan := func(target string, port string, opts ...scanOpt) (chan *synscan.SynScanResult, error) {
+		config := &_yakPortScanConfig{
+			waiting:           5 * time.Second,
+			rateLimitDelayMs:  1,
+			rateLimitDelayGap: 5,
+		}
+		for _, opt := range opts {
+			opt(config)
+		}
+		return _synScanDo(hostsToChan(target), port, config)
+	}
+
+	synScan := func(addr string) {
+		//res, err := Scan(target, synPorts, _scanOptExcludePorts(tcpPorts))
+		res, err := Scan(target, synPorts, _scanOptOpenPortInitPortFilter("6379"))
+		//res, err := Scan(target, synPorts)
+		if err != nil {
+			t.FailNow()
+		}
+		res2, err := _scanFromTargetStream(res)
+		if err != nil {
+			t.FailNow()
+		}
+		for result := range res2 {
+			fmt.Println("SYNGOT " + result.String())
+		}
+	}
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		synScan(target)
+	}()
+
+	go func() {
+		defer wg.Done()
+		tcpScan(target)
+	}()
+
+	wg.Wait()
 }
