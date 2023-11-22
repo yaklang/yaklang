e1618ff71fbfdae06e9b0d96d7801f0f2dac15e5
wlz <lingze.wl@gmail.com>
2023-11-22 16:56:45 +0800

    fix(static-analyzer): check prog.IsNil

--- common/yakgrpc/grpc_yaklang_inspect_information.go
+++ common/yakgrpc/grpc_yaklang_inspect_information.go
@@ -42,7 +42,7 @@ func yaklangInfo2Grpc(infos []*pta.YaklangInfo) []*ypb.YaklangInformation {
 func (s *Server) YaklangInspectInformation(ctx context.Context, req *ypb.YaklangInspectInformationRequest) (*ypb.YaklangInspectInformationResponse, error) {
 	ret := &ypb.YaklangInspectInformationResponse{}
 	prog := ssaapi.Parse(req.YakScriptCode, pta.GetPluginSSAOpt(req.YakScriptType)...)
-	if prog == nil {
+	if prog.IsNil() {
 		return nil, errors.New("ssa parse error")
 	}
 	if req.StartPos != nil || req.EndPos != nil {
