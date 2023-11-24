84481fcb41aa2f0d362d5e3b7823c4b835557102
go0p <gp827782797@qq.com>
2023-11-24 15:52:10 +0800

    feat(history) website tree

--- common/yakgrpc/yakit/httpflow_tree_helper.go
+++ common/yakgrpc/yakit/httpflow_tree_helper.go
@@ -1,18 +1,26 @@
 package yakit
 
 import (
+	"context"
 	"fmt"
 	"github.com/jinzhu/gorm"
 	"github.com/yaklang/yaklang/common/log"
+	"net/url"
 	"sort"
 	"strings"
 )
 
 func FilterHTTPFlowBySchema(db *gorm.DB, schema string) *gorm.DB {
 	if schema != "" {
-		db = db.Where("url LIKE ?", schema+"://%")
+		db = db.Where("url LIKE ?", schema+"://%").Debug()
 	}
 	return db
+}
+
+type WebsiteTree struct {
+	Path         string
+	NextParts    []*WebsiteNextPart
+	HaveChildren bool
 }
 
 type WebsiteNextPart struct {
@@ -20,6 +28,8 @@ type WebsiteNextPart struct {
 	NextPart     string
 	HaveChildren bool
 	Count        int
+	IsQuery      bool
+	IsFile       bool
 }
 
 func trimPathWithOneSlash(path string) string {
@@ -77,6 +87,103 @@ func GetHTTPFlowDomainsByDomainSuffix(db *gorm.DB, domainSuffix string) []*Websi
 }
 
 func GetHTTPFlowNextPartPathByPathPrefix(db *gorm.DB, originPathPrefix string) []*WebsiteNextPart {
+	pathPrefix := trimPathWithOneSlash(originPathPrefix)
+	pathPrefix2 := "/" + pathPrefix
+	db = db.Select("url").Table("http_flows").Where("url LIKE ?", `%`+originPathPrefix+`%`).Limit(1000)
+	var urls []string
+	for u := range YieldHTTPUrl(db, context.Background()) {
+		up, _ := url.Parse(u.Url)
+		if strings.HasPrefix(up.Path, pathPrefix2) &&
+			(strings.HasPrefix(up.RequestURI()[len(pathPrefix2):], "/") ||
+				strings.HasPrefix(up.RequestURI()[len(pathPrefix2):], "?") ||
+				pathPrefix == "") || up.Path == originPathPrefix || up.Path == pathPrefix2 {
+			urls = append(urls, u.Url)
+		}
+	}
+
+	// 初始化一个映射，用于存储网站结构
+	resultMap := make(map[string]*WebsiteNextPart)
+
+	// 假设 urls 是您的 URL 列表
+	for _, us := range urls {
+		u, _ := url.Parse(us)
+		if u.Path == "" {
+			continue
+		}
+		haveChildren := false
+
+		path := strings.ReplaceAll(u.Path, "//", "/")
+
+		suffix := strings.TrimPrefix(strings.TrimPrefix(path[1:], pathPrefix), "/")
+
+		nextSegment, after, splited := strings.Cut(suffix, "/")
+		if splited && after != "" {
+			haveChildren = true
+		}
+
+		if nextSegment == "" && u.RawQuery == "" {
+			continue
+		}
+
+		if nextSegment != "" {
+			// 检查根路径段是否已经在resultMap中
+			if _, ok := resultMap[nextSegment]; !ok {
+				if u.RawQuery != "" {
+					haveChildren = true
+				}
+				resultMap[nextSegment] = &WebsiteNextPart{
+					NextPart:     nextSegment,
+					HaveChildren: haveChildren, // 如果有多个路径段，说明有子节点
+					Count:        1,            // 初始化计数为1
+					Schema:       u.Scheme,
+				}
+				if strings.Contains(nextSegment, ".") {
+					resultMap[nextSegment].IsFile = true
+				}
+
+			} else {
+				if u.RawQuery != "" {
+					haveChildren = true
+				}
+				resultMap[nextSegment].HaveChildren = haveChildren
+
+				// 如果已经存在，且不是文件，则增加计数
+				if !strings.Contains(nextSegment, ".") {
+					resultMap[nextSegment].Count++
+				}
+			}
+		}
+
+		// 如果存在查询参数，将其添加到根路径段
+		if u.RawQuery != "" && (u.Path == originPathPrefix || u.Path == "/"+originPathPrefix) {
+			for key := range u.Query() {
+				resultMap[key] = &WebsiteNextPart{
+					NextPart:     key,
+					HaveChildren: false, // 如果有多个路径段，说明有子节点
+					IsQuery:      true,
+					Schema:       u.Scheme,
+				}
+				resultMap[key].Count++
+
+				if strings.Contains(key, ".") {
+					resultMap[key].HaveChildren = true
+				}
+			}
+		}
+	}
+
+	// resultMap 现在包含了所有的路径和查询参数，以及它们的层级关系和计数
+	var data []*WebsiteNextPart
+	for _, r := range resultMap {
+		data = append(data, r)
+	}
+	sort.SliceStable(data, func(i, j int) bool {
+		return data[i].NextPart > data[j].NextPart
+	})
+	return data
+}
+
+func GetHTTPFlowNextPartPathByPathPrefixb(db *gorm.DB, originPathPrefix string) []*WebsiteNextPart {
 	pathPrefix := trimPathWithOneSlash(originPathPrefix)
 	db = FilterHTTPFlowPathPrefix(db, originPathPrefix)
 	db = db.Select(fmt.Sprintf(
