9f461913e93720dab4e094ba75bc0af01e3082fe
VillanCh <188228725@qq.com>
2023-11-26 11:02:47 +0800

    improve spaceengine api

--- common/utils/spacengine/hunter/qaxhunter.go
+++ common/utils/spacengine/hunter/qaxhunter.go
@@ -1,9 +1,11 @@
 package hunter
 
 import (
-	"encoding/base64"
 	"fmt"
-	"io/ioutil"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils/lowhttp"
+	"github.com/yaklang/yaklang/common/yak/yaklib/codec"
 	"net/url"
 
 	"github.com/tidwall/gjson"
@@ -13,25 +15,46 @@ import (
 var defaultHttpClient = utils.NewDefaultHTTPClient()
 
 func HunterQuery(username, key, query string, page, pageSize int) (*gjson.Result, error) {
+	if key == "" {
+		return nil, utils.Error("empty api key")
+	}
 	values := make(url.Values)
-	values.Set("username", username)
 	values.Set("api-key", key)
-	values.Set("search", base64.URLEncoding.EncodeToString([]byte(query)))
+	values.Set("search", codec.EncodeBase64Url(query))
 	values.Set("page", fmt.Sprint(page))
 	values.Set("page_size", fmt.Sprint(pageSize))
-	var res, err = defaultHttpClient.Get(fmt.Sprintf("https://hunter.qianxin.com/openApi/search?%s", values.Encode()))
-	if err != nil {
-		return nil, utils.Errorf("query hunter search api failed: %s", err)
+
+	packet := []byte(`GET /openApi/search HTTP/1.1
+Host: hunter.qianxin.com
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0
+
+`)
+	for _, kv := range [][]string{
+		{"api-key", key},
+		{"search", codec.EncodeBase64Url(query)},
+		{"page", fmt.Sprint(page)},
+		{"page_size", fmt.Sprint(pageSize)},
+	} {
+		packet = lowhttp.ReplaceHTTPPacketQueryParam(packet, kv[0], kv[1])
 	}
-	raw, err := ioutil.ReadAll(res.Body)
+	rsp, err := lowhttp.HTTP(lowhttp.WithPacketBytes(packet), lowhttp.WithHttps(true))
 	if err != nil {
-		return nil, utils.Errorf("read body failed: %s", err)
+		spew.Dump(rsp.RawPacket)
+		spew.Dump(rsp.RawRequest)
+		return nil, err
+	}
+	_, raw := lowhttp.SplitHTTPPacketFast(rsp.RawPacket)
+	if lowhttp.GetStatusCodeFromResponse(rsp.RawPacket) != 200 {
+		spew.Dump(rsp.RawPacket)
+		return nil, utils.Errorf("invalid status code")
 	}
 
 	result := gjson.ParseBytes(raw)
-	code, errmsg := result.Get("code").Int(), result.Get("msg").String()
+	code, errmsg := result.Get("code").Int(), result.Get("message").String()
 
 	if result.Get("code").Int() != 200 {
+		log.Warnf("met error! %v", string(raw))
+		fmt.Println(string(rsp.RawRequest))
 		return &result, utils.Errorf("[%v]: %v", code, errmsg)
 	}
 
--- common/yak/yaklib/spacengine.go
+++ common/yak/yaklib/spacengine.go
@@ -1,8 +1,11 @@
 package yaklib
 
 import (
+	"github.com/yaklang/yaklang/common/consts"
 	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils"
 	spacengine2 "github.com/yaklang/yaklang/common/utils/spacengine"
+	"strings"
 )
 
 var SpaceEngineExports = map[string]interface{}{
@@ -12,15 +15,138 @@ var SpaceEngineExports = map[string]interface{}{
 	"HunterQuery":  _hunter,
 	"ZoomeyeQuery": _zoomeye,
 
+	"Query": Query,
+
 	"maxPage":   _spaceEngine_MaxPage,
 	"maxRecord": _spaceEngine_MaxRecord,
 	"pageSize":  _spaceEngine_PageSize,
+	"zoomeye":   withUseZoomeye,
+	"shodan":    withUseShodan,
+	"quake":     withUseQuake,
+	"hunter":    withUseHunter,
+	"fofa":      withUseFofa,
+	"engine":    withEngine,
+}
+
+func withEngine(i string, auth ...string) _spaceEngineConfigOpt {
+	switch strings.TrimSpace(strings.ToLower(i)) {
+	case "zoomeye":
+		return withUseZoomeye(auth...)
+	case "shodan":
+		return withUseShodan(auth...)
+	case "quake":
+		return withUseQuake(auth...)
+	case "hunter":
+		return withUseHunter(auth...)
+	case "fofa":
+		return withUseFofa(auth...)
+	}
+	return func(c *_spaceEngineConfig) {
+		c.engine = i
+		if len(auth) == 1 {
+			c.apiKey = auth[0]
+		} else {
+			c.apiKey = consts.GetThirdPartyApplicationConfig(i).APIKey
+		}
+		if len(auth) == 2 {
+			c.user = auth[0]
+			c.apiKey = auth[1]
+		} else {
+			c.apiKey = consts.GetThirdPartyApplicationConfig(i).APIKey
+			c.user = consts.GetThirdPartyApplicationConfig(i).UserIdentifier
+		}
+	}
+}
+
+func withUseZoomeye(api ...string) _spaceEngineConfigOpt {
+	return func(c *_spaceEngineConfig) {
+		c.engine = "zoomeye"
+		if len(api) > 0 {
+			c.apiKey = api[0]
+		} else {
+			c.apiKey = consts.GetThirdPartyApplicationConfig("zoomeye").APIKey
+		}
+	}
+}
+
+func withUseShodan(api ...string) _spaceEngineConfigOpt {
+	return func(c *_spaceEngineConfig) {
+		c.engine = "shodan"
+		if len(api) > 0 {
+			c.apiKey = api[0]
+		} else {
+			c.apiKey = consts.GetThirdPartyApplicationConfig("shodan").APIKey
+		}
+	}
+}
+
+func withUseQuake(api ...string) _spaceEngineConfigOpt {
+	return func(c *_spaceEngineConfig) {
+		c.engine = "quake"
+		if len(api) > 0 {
+			c.apiKey = api[0]
+		} else {
+			c.apiKey = consts.GetThirdPartyApplicationConfig("quake").APIKey
+		}
+	}
+}
+
+func withUseHunter(auth ...string) _spaceEngineConfigOpt {
+	return func(c *_spaceEngineConfig) {
+		c.engine = "hunter"
+		if len(auth) > 0 {
+			c.apiKey = auth[0]
+		} else {
+			c.apiKey = consts.GetThirdPartyApplicationConfig("hunter").APIKey
+		}
+	}
+}
+
+func withUseFofa(auth ...string) _spaceEngineConfigOpt {
+	return func(c *_spaceEngineConfig) {
+		c.engine = "fofa"
+		if len(auth) > 1 {
+			c.user = auth[0]
+			c.apiKey = auth[1]
+		} else {
+			c.apiKey = consts.GetThirdPartyApplicationConfig("fofa").APIKey
+			c.user = consts.GetThirdPartyApplicationConfig("fofa").UserIdentifier
+		}
+	}
+}
+
+func Query(filter string, opts ..._spaceEngineConfigOpt) (chan *spacengine2.NetSpaceEngineResult, error) {
+	config := &_spaceEngineConfig{
+		maxRecord: 100,
+		maxPage:   10,
+		pageSize:  10,
+	}
+	for _, op := range opts {
+		op(config)
+	}
+	switch strings.ToLower(config.engine) {
+	case "shodan":
+		return _shodan(config.apiKey, filter, opts...)
+	case "fofa":
+		return _fofa(config.user, config.apiKey, filter, opts...)
+	case "quake":
+		return _quake(config.apiKey, filter, opts...)
+	case "hunter":
+		return _hunter(config.user, config.apiKey, filter, opts...)
+	case "zoomeye":
+		return _zoomeye(config.apiKey, filter, opts...)
+	default:
+		return nil, utils.Error("invalid engine " + config.engine)
+	}
 }
 
 type _spaceEngineConfig struct {
 	maxRecord int
 	maxPage   int
 	pageSize  int
+	engine    string
+	apiKey    string
+	user      string
 }
 
 type _spaceEngineConfigOpt func(c *_spaceEngineConfig)
@@ -47,6 +173,7 @@ func _shodan(token string, filter string, opts ..._spaceEngineConfigOpt) (chan *
 	config := &_spaceEngineConfig{
 		maxRecord: 100,
 		maxPage:   10,
+		pageSize:  10,
 	}
 
 	for _, opt := range opts {
@@ -60,6 +187,7 @@ func _quake(token string, filter string, opts ..._spaceEngineConfigOpt) (chan *s
 	config := &_spaceEngineConfig{
 		maxRecord: 100,
 		maxPage:   10,
+		pageSize:  10,
 	}
 
 	for _, opt := range opts {
@@ -73,6 +201,7 @@ func _hunter(name, key string, filter string, opts ..._spaceEngineConfigOpt) (ch
 	config := &_spaceEngineConfig{
 		maxRecord: 100,
 		maxPage:   10,
+		pageSize:  10,
 	}
 
 	for _, opt := range opts {
@@ -105,6 +234,7 @@ func _zoomeye(key string, filter string, opts ..._spaceEngineConfigOpt) (chan *s
 	config := &_spaceEngineConfig{
 		maxRecord: 100,
 		maxPage:   10,
+		pageSize:  10,
 	}
 
 	for _, opt := range opts {
