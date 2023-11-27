b21f955abd5e3074618da45edcd71b47d3035382
v1ll4n <v1ll4n.a5k@gmail.com>
2023-11-27 14:45:49 +0800

    fix log

--- common/yak/yaktest/mustpass/mustpass_base_test.go
+++ common/yak/yaktest/mustpass/mustpass_base_test.go
@@ -50,7 +50,7 @@ func init() {
 }
 
 func TestMustPassDebug(t *testing.T) {
-	var debugName = "mock_syn_exclude_port.yak"
+	var debugName = "misc.yak"
 	var debugCases [][]string
 	for k, v := range files {
 		if k == debugName {
