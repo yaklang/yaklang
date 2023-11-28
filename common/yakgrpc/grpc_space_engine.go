fe5fe3b3a320406531341f0e142ac09f6afd5f0d
v1ll4n <v1ll4n.a5k@gmail.com>
2023-11-28 11:40:48 +0800

    add space-engine fetcher

--- common/yakgrpc/grpc_space_engine.go
+++ common/yakgrpc/grpc_space_engine.go
@@ -2,9 +2,14 @@ package yakgrpc
 
 import (
 	"context"
+	_ "embed"
+	uuid "github.com/satori/go.uuid"
 	"github.com/yaklang/yaklang/common/consts"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/spacengine/zoomeye"
+	"github.com/yaklang/yaklang/common/yak"
+	"github.com/yaklang/yaklang/common/yak/antlr4yak"
+	"github.com/yaklang/yaklang/common/yak/yaklib"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
@@ -35,4 +40,26 @@ func (s *Server) GetSpaceEngineStatus(ctx context.Context, req *ypb.GetSpaceEngi
 	default:
 		return nil, utils.Errorf("invalid type: %v", req.GetType())
 	}
+}
+
+//go:embed grpc_space_engine.yak
+var spaceEngineExecCode string
+
+func (s *Server) FetchPortAssetFromSpaceEngine(req *ypb.FetchPortAssetFromSpaceEngineRequest, stream ypb.Yak_FetchPortAssetFromSpaceEngineServer) error {
+	engine := yak.NewYakitVirtualClientScriptEngine(yaklib.NewVirtualYakitClient(stream.Send))
+	runtimeId := uuid.NewV4().String()
+	engine.RegisterEngineHooks(func(engine *antlr4yak.Engine) error {
+		engine.SetVar("FILTER", req.GetFilter())
+		engine.SetVar("SCAN_VERIFY", req.GetScanBeforeSave())
+		engine.SetVar("TOTAL_PAGE", req.GetMaxPage())
+		engine.SetVar("ENGINE_TYPE", req.GetType())
+		engine.SetVar("CONCURRENT", req.GetConcurrent())
+		yak.BindYakitPluginContextToEngine(engine, &yak.YakitPluginContext{
+			PluginName: "space-engine",
+			RuntimeId:  runtimeId,
+			Proxy:      req.GetProxy(),
+		})
+		return nil
+	})
+	return engine.ExecuteWithContext(stream.Context(), spaceEngineExecCode)
 }
