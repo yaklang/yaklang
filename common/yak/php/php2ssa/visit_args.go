1e7a3a1fc6738fee51740d3e9f18d8c92493d60f
VillanCh <188228725@qq.com>
2023-12-06 22:43:15 +0800

    fix g4

--- common/yak/php/php2ssa/visit_args.go
+++ common/yak/php/php2ssa/visit_args.go
@@ -12,7 +12,29 @@ func (y *builder) VisitArguments(raw phpparser.IArgumentsContext) interface{} {
 		return nil
 	}
 
-	//
+	for _, arg := range i.AllActualArgument() {
+		y.VisitActualArgument(arg)
+	}
 
+	return nil
+}
+
+func (y *builder) VisitActualArgument(raw phpparser.IActualArgumentContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.ActualArgumentContext)
+	if i == nil {
+		return nil
+	}
+
+	if i.Expression() != nil {
+		return y.VisitExpression(i.Expression())
+	} else if i.Ampersand() != nil {
+
+	} else if i.YieldExpression() != nil {
+		return y.VisitYieldExpression(i.YieldExpression())
+	}
 	return nil
 }
