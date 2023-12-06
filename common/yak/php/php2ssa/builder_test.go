e24ab3bcd54d4b1221452c06fe06b98f5310c9b8
VillanCh <188228725@qq.com>
2023-12-06 10:19:10 +0800

    smoking for php parsing

---
+++ common/yak/php/php2ssa/builder_test.go
@@ -0,0 +1,11 @@
+package php2ssa
+
+import "testing"
+
+func TestParseSSA_Smoking(t *testing.T) {
+	ParseSSA(`<?php echo 111 ?>`, nil)
+}
+
+func TestParseSSA_Smoking2(t *testing.T) {
+	ParseSSA(`<?php echo "Hello world"; // comment ?>`, nil)
+}
