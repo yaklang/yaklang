994c4dc3e6f3d8dee70d602958b1de0b541dc5c0
v1ll4n <v1ll4n.a5k@gmail.com>
2023-12-06 15:42:47 +0800

    info

--- common/yak/php/php2ssa/builder_test.go
+++ common/yak/php/php2ssa/builder_test.go
@@ -14,7 +14,47 @@ func TestParseSSA_Smoking2(t *testing.T) {
 func TestParseSSA_1(t *testing.T) {
 	ParseSSA(`<?php
 
+id: 
+	echo "test123";
+
+{
+	echo "11";
+}
+
+if (true) echo "abc";
+if (true) echo "abc"; else if true 1+1;
+if (true) echo "abc"; else if true 1+1; else "abc"."ccc";
+
+$a=1;
+
+if ($a > 0) echo "abc";
+if ($a > 0) echo "abc"; else echo "ghi";
+if ($a > 0) echo "abc"; else if ($a < 0) echo "def"; else echo "ghi";
 
 
-?>`, nil)
+
+?>
+
+<?php
+$condition = true;
+$anotherCondition = false;
+
+if ($condition):
+    echo "Condition is true.";
+elseif ($anotherCondition):
+    echo "Another condition is true.";
+else:
+    echo "Both conditions are false.";
+endif;
+?>
+
+<?php if ($condition): ?>
+    <p>The condition is true.</p>
+<?php elseif ($anotherCondition): ?>
+    <p>Another condition is true.</p>
+<?php else: ?>
+    <p>Both conditions are false.</p>
+<?php endif; ?>
+
+`, nil)
 }
