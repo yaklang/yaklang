48495f55cd99b43f6984210a44bd463a40e4a3fb
v1ll4n <v1ll4n.a5k@gmail.com>
2023-12-06 14:25:10 +0800

    add error lis for legacy

--- common/yak/antlr4util/errorlis_legacy.go
+++ common/yak/antlr4util/errorlis_legacy.go
@@ -1,1 +1,20 @@
 package antlr4util
+
+import "github.com/antlr/antlr4/runtime/Go/antlr/v4"
+
+// error listener for lexer and parser
+type ErrorListener struct {
+	err []string
+	*antlr.DefaultErrorListener
+}
+
+func (el *ErrorListener) SyntaxError(recognizer antlr.Recognizer, offendingSymbol interface{}, line, column int, msg string, e antlr.RecognitionException) {
+	el.err = append(el.err, msg)
+}
+
+func NewLegacyErrorListener() *ErrorListener {
+	return &ErrorListener{
+		err:                  []string{},
+		DefaultErrorListener: antlr.NewDefaultErrorListener(),
+	}
+}
