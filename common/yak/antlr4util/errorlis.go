c72c3db01927c8e9ee9b51e0f69b09fd7257a66d
VillanCh <188228725@qq.com>
2023-12-06 19:10:09 +0800

    handle error with util

--- common/yak/antlr4util/errorlis.go
+++ common/yak/antlr4util/errorlis.go
@@ -1,1 +1,48 @@
 package antlr4util
+
+import "github.com/antlr4-go/antlr/v4"
+import (
+	legacyAntlr "github.com/antlr/antlr4/runtime/Go/antlr/v4"
+)
+
+// error listener for lexer and parser
+type ErrorListener struct {
+	err []string
+	*antlr.DefaultErrorListener
+}
+
+func (el *ErrorListener) GetErrors() []string {
+	return el.err
+}
+
+func (el *ErrorListener) SyntaxError(recognizer antlr.Recognizer, offendingSymbol interface{}, line, column int, msg string, e antlr.RecognitionException) {
+	el.err = append(el.err, msg)
+}
+
+func NewErrorListener() *ErrorListener {
+	return &ErrorListener{
+		err:                  []string{},
+		DefaultErrorListener: antlr.NewDefaultErrorListener(),
+	}
+}
+
+// error listener for lexer and parser
+type LegacyErrorListener struct {
+	err []string
+	*legacyAntlr.DefaultErrorListener
+}
+
+func (el *LegacyErrorListener) GetErrors() []string {
+	return el.err
+}
+
+func (el *LegacyErrorListener) SyntaxError(recognizer legacyAntlr.Recognizer, offendingSymbol interface{}, line, column int, msg string, e legacyAntlr.RecognitionException) {
+	el.err = append(el.err, msg)
+}
+
+func NewLegacyErrorListener() *LegacyErrorListener {
+	return &LegacyErrorListener{
+		err:                  []string{},
+		DefaultErrorListener: legacyAntlr.NewDefaultErrorListener(),
+	}
+}
