4c9e2ef2cf04a6804082b2526c9ae2379b39d895
VillanCh <188228725@qq.com>
2023-12-08 09:38:30 +0800

    feature assign n bin op

--- common/yak/php/php2ssa/visit_expr.go
+++ common/yak/php/php2ssa/visit_expr.go
@@ -16,7 +16,8 @@ func (y *builder) VisitExpressionStatement(raw phpparser.IExpressionStatementCon
 		return nil
 	}
 
-	return nil
+	va := y.VisitExpression(i.Expression())
+	return va
 }
 
 func (y *builder) VisitParentheses(raw phpparser.IParenthesesContext) ssa.Value {
@@ -77,6 +78,31 @@ func (y *builder) VisitExpression(raw phpparser.IExpressionContext) ssa.Value {
 	case *phpparser.LambdaFunctionExpressionContext:
 	case *phpparser.MatchExpressionContext:
 	case *phpparser.ArithmeticExpressionContext:
+		op1 := y.VisitExpression(ret.Expression(0))
+		op2 := y.VisitExpression(ret.Expression(1))
+		var o ssa.BinaryOpcode
+		opStr := ret.GetOp().GetText()
+		switch opStr {
+		case "**":
+			o = ssa.OpPow
+		case "+":
+			o = ssa.OpAnd
+		case "-":
+			o = ssa.OpSub
+		case "*":
+			o = ssa.OpMul
+		case "/":
+			o = ssa.OpDiv
+		case "%":
+			o = ssa.OpMod
+		case ".":
+			// o = ssa.OpConcat
+			// concat string????
+		default:
+
+			return nil
+		}
+		return ssa.NewBinOp(o, op1, op2)
 	case *phpparser.InstanceOfExpressionContext:
 	case *phpparser.ComparisonExpressionContext:
 	case *phpparser.BitwiseExpressionContext:
@@ -85,9 +111,349 @@ func (y *builder) VisitExpression(raw phpparser.IExpressionContext) ssa.Value {
 	case *phpparser.SpaceshipExpressionContext:
 	case *phpparser.ArrayDestructExpressionContext:
 	case *phpparser.AssignmentExpressionContext:
+		if ret.AssignmentOperator() != nil {
+			// assignable assignmentOperator attributes? expression        # AssignmentExpression
+
+			// left value: chain array creation
+			leftValues := y.VisitAssignable(ret.Assignable())
+			_ = leftValues
+
+			operator := ret.AssignmentOperator()
+			_ = operator
+
+			var annotation any
+			if ret.Attributes() != nil {
+				annotation = y.VisitAttributes(ret.Attributes())
+				_ = annotation
+			}
+
+			rightValue := y.VisitExpression(ret.Expression())
+			_ = rightValue
+		} else if ret.Ampersand() != nil {
+			// assignable Eq attributes? '&' (chain | newExpr)
+			leftValues := y.VisitAssignable(ret.Assignable())
+			if ret.Attributes() != nil {
+				y.VisitAttributes(ret.Attributes())
+			}
+
+			// right val
+			if i := ret.Chain(); i != nil {
+				y.VisitChain(i)
+			} else if i := ret.NewExpr(); i != nil {
+				y.VisitNewExpr(i)
+			}
+			_ = leftValues
+		}
+
 	case *phpparser.LogicalExpressionContext:
 	default:
 		_ = ret
+	}
+
+	return nil
+}
+
+func (y *builder) VisitAssignable(raw phpparser.IAssignableContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.AssignableContext)
+	if i == nil {
+		return nil
+	}
+
+	if i.Chain() != nil {
+		return y.VisitChain(i.Chain())
+	} else if i.ArrayCreation() != nil {
+		return y.VisitArrayCreation(i.ArrayCreation())
+	} else {
+		return nil
+	}
+
+	return nil
+}
+
+func (y *builder) VisitChain(raw phpparser.IChainContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.ChainContext)
+	if i == nil {
+		return nil
+	}
+
+	y.VisitChainOrigin(i.ChainOrigin())
+
+	for _, m := range i.AllMemberAccess() {
+		y.VisitMemberAccess(m)
+	}
+
+	return nil
+}
+
+func (y *builder) VisitMemberAccess(raw phpparser.IMemberAccessContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.MemberAccessContext)
+	if i == nil {
+		return nil
+	}
+
+	y.VisitKeyedFieldName(i.KeyedFieldName())
+	if i.ActualArguments() != nil {
+		y.VisitActualArguments(i.ActualArguments())
+	}
+
+	return nil
+}
+
+func (y *builder) VisitActualArguments(raw phpparser.IActualArgumentsContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.ActualArgumentsContext)
+	if i == nil {
+		return nil
+	}
+
+	// PHP8 annotation
+	for _, a := range i.AllArguments() {
+		y.VisitArguments(a)
+	}
+
+	for _, a := range i.AllSquareCurlyExpression() {
+		y.VisitSquareCurlyExpression(a)
+	}
+
+	return nil
+}
+
+func (y *builder) VisitKeyedFieldName(raw phpparser.IKeyedFieldNameContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.KeyedFieldNameContext)
+	if i == nil {
+		return nil
+	}
+
+	if i.KeyedSimpleFieldName() != nil {
+		y.VisitKeyedSimpleFieldName(i.KeyedSimpleFieldName())
+	} else if i.KeyedVariable() != nil {
+		y.VisitKeyedVariable(i.KeyedVariable())
+	}
+
+	return nil
+}
+
+func (y *builder) VisitKeyedVariable(raw phpparser.IKeyedVariableContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.KeyedVariableContext)
+	if i == nil {
+		return nil
+	}
+
+	dollarCount := 0
+	if i.VarName() != nil {
+		dollarCount = len(i.AllDollar())
+	} else {
+		dollarCount = len(i.AllDollar()) - 1
+	}
+	_ = dollarCount
+
+	v := y.VisitExpression(i.Expression())
+	_ = v
+	var sv []any
+	for _, a := range i.AllSquareCurlyExpression() {
+		sv = append(sv, y.VisitSquareCurlyExpression(a))
+	}
+
+	return nil
+}
+
+func (y *builder) VisitKeyedSimpleFieldName(raw phpparser.IKeyedSimpleFieldNameContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.KeyedSimpleFieldNameContext)
+	if i == nil {
+		return nil
+	}
+
+	if i.Identifier() != nil {
+		v := y.VisitIdentifier(i.Identifier())
+		_ = v
+	} else if i.Expression() != nil {
+		v := y.VisitExpression(i.Expression())
+		_ = v
+	}
+
+	for _, sce := range i.AllSquareCurlyExpression() {
+		y.VisitSquareCurlyExpression(sce)
+	}
+
+	return nil
+}
+
+func (y *builder) VisitSquareCurlyExpression(raw phpparser.ISquareCurlyExpressionContext) ssa.Value {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.SquareCurlyExpressionContext)
+	if i == nil {
+		return nil
+	}
+
+	if i.OpenSquareBracket() != nil {
+		if i.Expression() != nil {
+			v := y.VisitExpression(i.Expression())
+			_ = v
+		}
+	} else {
+		return y.VisitExpression(i.Expression())
+	}
+
+	return nil
+}
+
+func (y *builder) VisitChainOrigin(raw phpparser.IChainOriginContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.ChainOriginContext)
+	if i == nil {
+		return nil
+	}
+
+	return nil
+}
+
+func (y *builder) VisitArrayCreation(raw phpparser.IArrayCreationContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.ArrayCreationContext)
+	if i == nil {
+		return nil
+	}
+
+	if i.ArrayItemList() != nil {
+		y.VisitArrayItemList(i.ArrayItemList())
+	}
+
+	y.VisitExpression(i.Expression())
+
+	return nil
+}
+
+func (y *builder) VisitArrayItemList(raw phpparser.IArrayItemListContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.ArrayItemListContext)
+	if i == nil {
+		return nil
+	}
+
+	for _, a := range i.AllArrayItem() {
+		y.VisitArrayItem(a)
+	}
+
+	return nil
+}
+
+func (y *builder) VisitArrayItem(raw phpparser.IArrayItemContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.ArrayItemContext)
+	if i == nil {
+		return nil
+	}
+
+	if i.Ampersand() != nil {
+		// (expression '=>')? '&' chain
+		var v any
+		if i.Expression(0) != nil {
+			v = y.VisitExpression(i.Expression(0))
+		}
+		_ = v
+		return y.VisitChain(i.Chain())
+	} else {
+		// expression ('=>' expression)?
+		v := y.VisitExpression(i.Expression(0))
+		if i.Expression(1) != nil {
+			v = y.VisitExpression(i.Expression(1))
+		}
+		_ = v
+		return v
+	}
+}
+
+func (y *builder) VisitAttributes(raw phpparser.IAttributesContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.AttributesContext)
+	if i == nil {
+		return nil
+	}
+
+	for _, g := range i.AllAttributeGroup() {
+		y.VisitAttributeGroup(g)
+	}
+
+	return nil
+}
+
+func (y *builder) VisitAttributeGroup(raw phpparser.IAttributeGroupContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.AttributeGroupContext)
+	if i == nil {
+		return nil
+	}
+
+	y.VisitIdentifier(i.Identifier())
+
+	for _, a := range i.AllAttribute() {
+		y.VisitAttribute(a)
+	}
+
+	return nil
+}
+
+func (y *builder) VisitAttribute(raw phpparser.IAttributeContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*phpparser.AttributeContext)
+	if i == nil {
+		return nil
+	}
+
+	y.VisitQualifiedNamespaceName(i.QualifiedNamespaceName())
+	if i.Arguments() != nil {
+		y.VisitArguments(i.Arguments())
 	}
 
 	return nil
