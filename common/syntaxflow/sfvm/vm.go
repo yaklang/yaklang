456becfd03fc396e10674cb123d68fdd525f6c3a
VillanCh <188228725@qq.com>
2023-12-14 10:11:10 +0800

    add omap and sfi

--- common/syntaxflow/sfvm/vm.go
+++ common/syntaxflow/sfvm/vm.go
@@ -1,5 +1,18 @@
 package sfvm
 
-type SyntaxFlowVirtualMachine struct {
+import (
+	"github.com/yaklang/yaklang/common/utils/omap"
+)
+
+type SyntaxFlowVirtualMachine[K comparable, V any] struct {
+	vars *omap.OrderedMap[K, V]
+
+	frames []SFFrame
 }
 
+func NewSyntaxFlowVirtualMachine[K comparable, V any]() *SyntaxFlowVirtualMachine[K, V] {
+	sfv := &SyntaxFlowVirtualMachine[K, V]{
+		vars: omap.NewEmptyOrderedMap[K, V](),
+	}
+	return sfv
+}
