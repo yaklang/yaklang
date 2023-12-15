ddc9df5d21eaa44a74e13dac38bd76ac157c0e90
v1ll4n <v1ll4n.a5k@gmail.com>
2023-12-15 12:01:19 +0800

    general sf vm

--- common/syntaxflow/sfvm/frame.go
+++ common/syntaxflow/sfvm/frame.go
@@ -7,78 +7,84 @@ import (
 	"github.com/yaklang/yaklang/common/utils/omap"
 )
 
-type SFFrame[T comparable, V any] struct {
-	symbolTable *omap.OrderedMap[string, *omap.OrderedMap[T, V]]
-	stack       *utils.Stack[*Value[T, V]]
+type SFFrame[V any] struct {
+	symbolTable *omap.OrderedMap[string, *omap.OrderedMap[string, V]]
+	stack       *utils.Stack[*Value[V]]
 	Text        string
-	Codes       []*SFI[T, V]
+	Codes       []*SFI[V]
 	toLeft      bool
 	debug       bool
 }
 
-func NewSFFrame[T comparable, V any](vars *omap.OrderedMap[string, *omap.OrderedMap[T, V]], text string, codes []*SFI[T, V]) *SFFrame[T, V] {
+func NewSFFrame[V any](vars *omap.OrderedMap[string, *omap.OrderedMap[string, V]], text string, codes []*SFI[V]) *SFFrame[V] {
 	v := vars
 	if v == nil {
-		v = omap.NewEmptyOrderedMap[string, *omap.OrderedMap[T, V]]()
+		v = omap.NewEmptyOrderedMap[string, *omap.OrderedMap[string, V]]()
 	}
-	return &SFFrame[T, V]{
+	return &SFFrame[V]{
 		symbolTable: v,
-		stack:       utils.NewStack[*Value[T, V]](),
+		stack:       utils.NewStack[*Value[V]](),
 		Text:        text,
 		Codes:       codes,
 	}
 }
 
-func (s *SFFrame[T, V]) Debug(v ...bool) *SFFrame[T, V] {
+func (s *SFFrame[V]) Debug(v ...bool) *SFFrame[V] {
 	if len(v) > 0 {
 		s.debug = v[0]
 	}
 	return s
 }
 
-func (s *SFFrame[T, V]) GetSymbolTable() *omap.OrderedMap[string, *omap.OrderedMap[T, V]] {
+func (s *SFFrame[V]) GetSymbolTable() *omap.OrderedMap[string, *omap.OrderedMap[string, V]] {
 	return s.symbolTable
 }
 
-func (s *SFFrame[T, V]) ToLeft() bool {
+func (s *SFFrame[V]) ToLeft() bool {
 	return s.toLeft
 }
 
-func (s *SFFrame[T, V]) ToRight() bool {
+func (s *SFFrame[V]) ToRight() bool {
 	return !s.ToLeft()
 }
 
-func (s *SFFrame[T, V]) Execute(input *omap.OrderedMap[T, V]) error {
+func (s *SFFrame[V]) exec(input *omap.OrderedMap[string, V]) (ret error) {
+	defer func() {
+		if err := recover(); err != nil {
+			ret = utils.Errorf("sft panic: %v", err)
+		}
+	}()
 	for _, i := range s.Codes {
 		s.debugLog(i.String())
 		switch i.OpCode {
 		case OpPushNumber:
-			s.stack.Push(NewValue[T, V](i.UnaryInt))
+			s.stack.Push(NewValue[V](i.UnaryInt))
 		case OpPushString:
-			s.stack.Push(NewValue[T, V](i.UnaryStr))
+			s.stack.Push(NewValue[V](i.UnaryStr))
 		case OpPushBool:
-			s.stack.Push(NewValue[T, V](i.UnaryInt))
+			s.stack.Push(NewValue[V](i.UnaryInt))
 		case OpPushMatch:
-			s.debugLog(" |-- search: %v", i.UnaryStr)
+			s.debugSubLog("search: %v", i.UnaryStr)
 			res, err := input.SearchGlobKey(i.UnaryStr)
 			if err != nil {
 				return utils.Wrapf(err, "search glob key failed")
 			}
 			if res.Len() == 0 {
-				s.debugLog(" |-- result: %v, not found", i.UnaryStr)
+				s.debugSubLog("result: %v, not found", i.UnaryStr)
 				return nil
 			}
-			s.debugLog(" |-- result: (len: %v)", res.Len())
-			s.stack.Push(NewValue[T, V](res))
-			s.debugLog(" |<- push")
+			res = res.ValuesMap()
+			s.debugSubLog("result: (len: %v)", res.Len())
+			s.stack.Push(NewValue[V](res))
+			s.debugSubLog("<< push")
 		case OpPushIndex:
 			panic("PushIndex not implemented")
 		case OpPushRef:
 			result, ok := s.symbolTable.Get(i.UnaryStr)
 			if !ok {
-				result = omap.NewEmptyOrderedMap[T, V]()
+				result = omap.NewEmptyOrderedMap[string, V]()
 			}
-			s.stack.Push(NewValue[T, V](result))
+			s.stack.Push(NewValue[V](result))
 		case OpNewRef:
 			val := s.stack.Peek()
 			s.symbolTable.Set(i.UnaryStr, val.AsMap())
@@ -86,26 +92,32 @@ func (s *SFFrame[T, V]) Execute(input *omap.OrderedMap[T, V]) error {
 			val := s.stack.Pop()
 			s.symbolTable.Set(i.UnaryStr, val.AsMap())
 		case OpFetchField:
-			results := s.stack.Pop().AsMap().Map(func(T, V) (T, V, error) {
+			results := s.stack.Pop().AsMap().Map(func(string, V) (string, V, error) {
 				panic("FetchField not implemented")
 			})
-			s.stack.Push(NewValue[T, V](results))
+			s.stack.Push(NewValue[V](results))
 		case OpFetchIndex:
-			results := s.stack.Pop().AsMap().Map(func(T, V) (T, V, error) {
+			results := s.stack.Pop().AsMap().Map(func(string, V) (string, V, error) {
 				panic("FetchIndex not implemented")
 			})
-			s.stack.Push(NewValue[T, V](results))
+			s.stack.Push(NewValue[V](results))
 		case OpSetDirection:
 			s.toLeft = i.UnaryStr == "<<"
 		case OpFlat:
+			s.debugSubLog(">> pop %v then merge", i.UnaryInt)
 			result := s.stack.PopN(i.UnaryInt)
-			var mergedMap []*omap.OrderedMap[T, V]
-			for _, v := range result {
-				_ = v
-				panic("Flat not implemented")
+			var mergedMap []*omap.OrderedMap[string, V]
+			for index, v := range result {
+				if v == nil {
+					s.debugSubLog("%2d: empty value", index)
+					continue
+				}
+				s.debugSubLog("%2d: merge-map %v", index, v.AsMap().Len())
+				mergedMap = append(mergedMap, v.AsMap())
 			}
-			_ = mergedMap
-			panic("Flat not implemented")
+			merged := omap.Merge(mergedMap...).ValuesMap()
+			s.debugSubLog("<< push map(len: %v)", merged.Len())
+			s.stack.Push(NewValue[V](merged))
 		case OpMap:
 			panic("Map is not implemented")
 		case OpTypeCast:
@@ -113,15 +125,20 @@ func (s *SFFrame[T, V]) Execute(input *omap.OrderedMap[T, V]) error {
 		case OpEq:
 			op2 := s.stack.Pop()
 			op1 := s.stack.Pop()
-			if !op1.IsMap() {
+			ret := op1.IsMap()
+			s.debugSubLog(">> pop 2 values, op1 must be map: %v", ret)
+			if !ret {
 				return utils.Errorf("opEq op1 is not filter/map")
 			}
-			result, err := op1.AsMap().SearchKey(op2.AsString())
+			search := op2.AsString()
+			s.debugSubLog("search op2: %v", op2.AsString())
+			result, err := op1.AsMap().SearchKey(search)
 			if err != nil {
 				return utils.Errorf("opEq search key failed: %v", err)
 			}
-			result.Values()
-
+			var a = NewValue[V](result)
+			s.stack.Push(a)
+			s.debugSubLog("<< push map(len: %v)", result.Len())
 		case OpNotEq, OpGt, OpGtEq, OpLt, OpLtEq, OpLogicAnd, OpLogicOr:
 			vals := s.stack.PopN(2)
 			op1 := vals[0]
@@ -129,7 +146,7 @@ func (s *SFFrame[T, V]) Execute(input *omap.OrderedMap[T, V]) error {
 			_ = op1
 			_ = op2
 		case OpNot:
-			s.stack.Push(NewValue[T, V](!s.stack.Pop().AsBool()))
+			s.stack.Push(NewValue[V](!s.stack.Pop().AsBool()))
 		case OpReMatch, OpGlobMatch:
 			op1 := s.stack.Pop()
 			op2 := i.UnaryStr
@@ -142,7 +159,7 @@ func (s *SFFrame[T, V]) Execute(input *omap.OrderedMap[T, V]) error {
 	return nil
 }
 
-func (s *SFFrame[T, V]) debugLog(i string, item ...any) {
+func (s *SFFrame[V]) debugLog(i string, item ...any) {
 	if !s.debug {
 		return
 	}
@@ -153,6 +170,6 @@ func (s *SFFrame[T, V]) debugLog(i string, item ...any) {
 	}
 }
 
-func (s *SFFrame[T, V]) debugSubLog(i string, item ...any) {
+func (s *SFFrame[V]) debugSubLog(i string, item ...any) {
 	s.debugLog("  |-- "+i, item...)
 }
