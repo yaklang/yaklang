48cd015ef4aaba54bcdbb96f4eb4a441e3a3fa5c
wlz <lingze.wl@gmail.com>
2023-12-19 10:54:48 +0800

    refactor(plugin type): new cli api and information

--- common/yakgrpc/grpc_yaklang_inspect_information.go
+++ common/yakgrpc/grpc_yaklang_inspect_information.go
@@ -4,41 +4,60 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 
 	pta "github.com/yaklang/yaklang/common/yak/plugin_type_analyzer"
 	"github.com/yaklang/yaklang/common/yak/ssaapi"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
-func yaklangInfo2Grpc(infos []*pta.YaklangInfo) []*ypb.YaklangInformation {
-	ret := make([]*ypb.YaklangInformation, 0, len(infos))
-
-	var infoKV2grpc func(kvs []*pta.YaklangInfoKV) []*ypb.YaklangInformationKV
-	infoKV2grpc = func(kvs []*pta.YaklangInfoKV) []*ypb.YaklangInformationKV {
-		ret := make([]*ypb.YaklangInformationKV, 0, len(kvs))
-		for _, kv := range kvs {
-			data, err := json.Marshal(kv.Value)
-			if err != nil {
-				continue
-			}
-			ret = append(ret, &ypb.YaklangInformationKV{
-				Key:    kv.Key,
-				Value:  data,
-				Extern: infoKV2grpc(kv.Extern),
-			})
-		}
-		return ret
-	}
-
-	for _, info := range infos {
-		ret = append(ret, &ypb.YaklangInformation{
-			Name: info.Name,
-			Data: infoKV2grpc(info.KV),
-		})
-	}
-	return ret
+type PluginParamSelect struct {
+	Double bool                    `json:"double"`
+	Data   []PluginParamSelectData `json:"data"`
 }
 
+type PluginParamSelectData struct {
+	Label string `json:"label"`
+	Value string `json:"value"`
+}
+
+func cliParam2grpc(params []*pta.CliParameter) []*ypb.YakScriptParam {
+	ret := make([]*ypb.YakScriptParam, 0, len(params))
+
+	for _, param := range params {
+		defaultValue := ""
+		if param.Default != nil {
+			defaultValue = fmt.Sprintf("%v", param.Default)
+		}
+		extra := []byte{}
+		if param.Type == "select" {
+			paramSelect := &PluginParamSelect{
+				Double: param.MultipleSelect,
+				Data:   make([]PluginParamSelectData, 0),
+			}
+			for k, v := range param.SelectOption {
+				paramSelect.Data = append(paramSelect.Data, PluginParamSelectData{
+					Label: k,
+					Value: v,
+				})
+			}
+			extra, _ = json.Marshal(paramSelect)
+		}
+
+		ret = append(ret, &ypb.YakScriptParam{
+			Field:        param.Name,
+			DefaultValue: string(defaultValue),
+			TypeVerbose:  param.Type,
+			FieldVerbose: param.NameVerbose,
+			Help:         param.Help,
+			Required:     param.Required,
+			Group:        param.Group,
+			ExtraSetting: string(extra),
+		})
+	}
+
+	return ret
+}
 
 func (s *Server) YaklangInspectInformation(ctx context.Context, req *ypb.YaklangInspectInformationRequest) (*ypb.YaklangInspectInformationResponse, error) {
 	ret := &ypb.YaklangInspectInformationResponse{}
@@ -46,8 +65,7 @@ func (s *Server) YaklangInspectInformation(ctx context.Context, req *ypb.Yaklang
 	if prog.IsNil() {
 		return nil, errors.New("ssa parse error")
 	}
-
-	ret.Information = yaklangInfo2Grpc(pta.GetPluginInfo(req.YakScriptType, prog))
+	ret.CliParameter = cliParam2grpc(pta.ParseCliParameter(prog))
 
 	return ret, nil
 }
--- common/yakgrpc/grpc_yaklang_inspect_information_test.go
+++ common/yakgrpc/grpc_yaklang_inspect_information_test.go
@@ -2,21 +2,13 @@ package yakgrpc
 
 import (
 	"context"
-	"strings"
+	"encoding/json"
 	"testing"
 
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
-// import (
-// 	"context"
-// 	"reflect"
-// 	"testing"
-
-// 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
-// )
-
-func yaklangInspectInformationSend(client ypb.YakClient, inspectType, yakScriptType, code string, r *ypb.Range) *ypb.YaklangInspectInformationResponse {
+func yaklangInspectInformationSend(client ypb.YakClient, yakScriptType, code string, r *ypb.Range) *ypb.YaklangInspectInformationResponse {
 	rsp, err := client.YaklangInspectInformation(context.Background(), &ypb.YaklangInspectInformationRequest{
 		YakScriptType: yakScriptType,
 		YakScriptCode: code,
@@ -28,27 +20,73 @@ func yaklangInspectInformationSend(client ypb.YakClient, inspectType, yakScriptT
 	return rsp
 }
 
-func CheckKV(t *testing.T, wants map[string]string, infos []*ypb.YaklangInformation) {
-	if len(wants) != len(infos) {
-		t.Fatal("length of kvs and infos not match")
+func CompareScriptParams(got, want []*ypb.YakScriptParam) error {
+
+	if len(got) != len(want) {
+		return utils.Errorf("cli parameter length not match")
 	}
 
-	compare := func(s1, s2 string) bool {
-		s1 = strings.ReplaceAll(s1, "\n", "")
-		s1 = strings.ReplaceAll(s1, " ", "")
-		s2 = strings.ReplaceAll(s2, "\n", "")
-		s2 = strings.ReplaceAll(s2, " ", "")
-		return s1 == s2
-	}
-	for _, info := range infos {
-		if want, ok := wants[info.Name]; ok {
-			if compare(want, info.String()) {
-				t.Fatalf("want (%s) vs got (%s)", want, info.String())
+	for i := range want {
+		log.Infof("want: %v", want[i])
+		log.Infof("got: %v", got[i])
+		// compare want and got
+		if got[i].Field != want[i].Field {
+			return utils.Errorf("cli parameter %d field not match", i)
+		}
+		if got[i].DefaultValue != want[i].DefaultValue {
+			return utils.Errorf("cli parameter %d default value not match", i)
+		}
+		if got[i].TypeVerbose != want[i].TypeVerbose {
+			return utils.Errorf("cli parameter %d type verbose not match", i)
+		}
+		if got[i].FieldVerbose != want[i].FieldVerbose {
+			return utils.Errorf("cli parameter %d field verbose not match", i)
+		}
+		if got[i].Help != want[i].Help {
+			return utils.Errorf("cli parameter %d help not match", i)
+		}
+		if got[i].Required != want[i].Required {
+			return utils.Errorf("cli parameter %d required not match", i)
+		}
+		if got[i].Group != want[i].Group {
+			return utils.Errorf("cli parameter %d group not match", i)
+		}
+		if got[i].ExtraSetting == "" && want[i].ExtraSetting == "" {
+			continue
+		}
+
+		var extraWant, extraGot *PluginParamSelect
+		err1 := json.Unmarshal([]byte(want[i].ExtraSetting), &extraWant)
+		err2 := json.Unmarshal([]byte(got[i].ExtraSetting), &extraGot)
+		if err1 != nil {
+			return utils.Errorf("cli parameter %d want extra setting unmarshal error %v", i, err1)
+		}
+		if err2 != nil {
+			return utils.Errorf("cli parameter %d got extra setting unmarshal error %v", i, err2)
+		}
+		if extraWant.Double != extraGot.Double {
+			return utils.Errorf("cli parameter %d extra setting double not match", i)
+		}
+		if len(extraWant.Data) != len(extraGot.Data) {
+			return utils.Errorf("cli parameter %d extra setting data length not match", i)
+		}
+		// sort extra*.Data by label
+		sort.Slice(extraWant.Data, func(i, j int) bool {
+			return extraWant.Data[i].Label < extraWant.Data[j].Label
+		})
+		sort.Slice(extraGot.Data, func(i, j int) bool {
+			return extraGot.Data[i].Label < extraGot.Data[j].Label
+		})
+		for j := range extraWant.Data {
+			if extraWant.Data[j].Label != extraGot.Data[j].Label {
+				return utils.Errorf("cli parameter %d extra setting data %d label not match", i, j)
 			}
-		} else {
-			t.Fatalf("key %s in got, but not in want", info.Name)
+			if extraWant.Data[j].Value != extraGot.Data[j].Value {
+				return utils.Errorf("cli parameter %d extra setting data %d value not match", i, j)
+			}
 		}
 	}
+	return nil
 }
 
 func TestGRPCMUSTPASS_LANGUAGE_InspectInformation(t *testing.T) {
@@ -56,55 +94,88 @@ func TestGRPCMUSTPASS_LANGUAGE_InspectInformation(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	rsp := yaklangInspectInformationSend(local, "info", "yak",
-		`
-		cli.String("arg1", cli.setDefault("default variable"), cli.setHelp("help information"), cli.setRequired(true))
-		cli.Int("arg2", cli.setDefault(1), cli.setHelp("help information 2"))
 
-		risk.NewRisk(
-				url,
-				risk.title(sprintf("XSS for: %v", url)),
-				risk.titleVerbose(sprintf("检测到xss漏洞: %v", url)),
-				risk.details("report"),
-				risk.description("description info "),
-				risk.solution("solution info"),
-				risk.type("xss"),
-				risk.payload("payloadString"),
-				risk.request("reqRaw"),
-				risk.response("respRaw"),
-				risk.severity("warning"),
-			)
-	`,
-		nil)
-	if rsp == nil {
-		t.Fatal("no response")
+	check := func(code string, want []*ypb.YakScriptParam, t *testing.T) {
+		rsp := yaklangInspectInformationSend(local, "yak", code, nil)
+		if rsp == nil {
+			t.Fatal("no response")
+		}
+		// check cli parameter
+		if err := CompareScriptParams(rsp.GetCliParameter(), want); err != nil {
+			t.Fatal(err)
+		}
 	}
 
-	CheckKV(t, map[string]string{
-		"cli": `
-		Name:"cli" 
-				Data:{
-					Key:"Name" Value:"\"arg1\"" 
-					Extern:{Key:"Type" Value:"\"string\""} 
-					Extern:{Key:"Help" Value:"\"help information\""} 
-					Extern:{Key:"Required" Value:"false"} 
-					Extern:{Key:"Default" Value:"\"default variable\""}
-				} 
-				Data:{
-					Key:"Name" Value:"\"arg2\"" 
-					Extern:{Key:"Type" Value:"\"int\""} 
-					Extern:{Key:"Help" Value:"\"help information 2\""} 
-					Extern:{Key:"Required" Value:"false"} 
-					Extern:{Key:"Default" Value:"1"}
-				}`,
-		"risk": `
-		Name:"risk" 
-			Data:{
-				Key:"Name" Value:"\"risk\"" 
-				Extern:{Key:"Level" Value:"\"warning\""} 
-				Extern:{Key:"CVE" Value:"\"\""} 
-				Extern:{Key:"Type" Value:"\"xss\""} 
-				Extern:{Key:"TypeVerbose" Value:"\"XSS\""}
-			}`,
-	}, rsp.Information)
+	t.Run("simple cli parameter", func(t *testing.T) {
+		check(
+			`
+		cli.String(
+			"arg1", 
+			cli.setDefault("default variable"), 
+			cli.setHelp("help information"), 
+			cli.setRequired(true),
+		)
+		cli.Int(
+			"arg2", 
+			cli.setVerboseName("参数2"),
+			cli.setCliGroup("group2"),
+			cli.setDefault(1), 
+			cli.setHelp("help information 2"),
+		)
+	`,
+
+			[]*ypb.YakScriptParam{
+				{
+					Field:        "arg1",
+					DefaultValue: "default variable",
+					TypeVerbose:  "string",
+					FieldVerbose: "arg1",
+					Help:         "help information",
+					Required:     true,
+					Group:        "",
+					ExtraSetting: "",
+				},
+				{
+					Field:        "arg2",
+					DefaultValue: "1",
+					TypeVerbose:  "uint",
+					FieldVerbose: "参数2",
+					Help:         "help information 2",
+					Required:     false,
+					Group:        "group2",
+					ExtraSetting: "",
+				},
+			},
+			t,
+		)
+	})
+
+	t.Run("cli parameter with select", func(t *testing.T) {
+		check(
+			`
+		cli.StringSlice(
+			"arg1", 
+			cli.setSelectOption("a", "A"),
+			cli.setSelectOption("b", "B"),
+			cli.setSelectOption("c", "c"),
+			cli.setMultipleSelect(true),
+			cli.setHelp("help information"),
+		)
+	`,
+
+			[]*ypb.YakScriptParam{
+				{
+					Field:        "arg1",
+					TypeVerbose:  "select",
+					FieldVerbose: "arg1",
+					Help:         "help information",
+					Required:     false,
+					Group:        "",
+					ExtraSetting: "{\"double\":true,\"data\":[{\"label\":\"c\",\"value\":\"c\"},{\"label\":\"a\",\"value\":\"A\"},{\"label\":\"b\",\"value\":\"B\"}]}",
+				},
+			},
+			t,
+		)
+	})
+
 }
