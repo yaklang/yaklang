fa661a3e2678da3944e0d29f2944f7b055b716f6
wlz <lingze.wl@gmail.com>
2023-12-20 14:50:16 +0800

    fix(antlr4utils): use SetRange in JS2ssa

--- common/yak/JS2ssa/range.go
+++ common/yak/JS2ssa/range.go
@@ -1,46 +1,11 @@
 package js2ssa
 
 import (
-	"github.com/antlr/antlr4/runtime/Go/antlr/v4"
-	"strings"
-
-	"github.com/yaklang/yaklang/common/yak/ssa"
+	"github.com/yaklang/yaklang/common/yak/antlr4util"
 )
 
-type CanStartStopToken interface {
-	GetStop() antlr.Token
-	GetStart() antlr.Token
-	GetText() string
-}
-
-func GetEndPosition(t antlr.Token) (int, int) {
-	var line, column int
-	str := strings.Split(t.GetText(), "\n")
-	if len(str) > 1 {
-		line = t.GetLine() + len(str) - 1
-		column = len(str[len(str)-1])
-	} else {
-		line = t.GetLine()
-		column = t.GetColumn() + len(str[0])
-	}
-	return line, column
-}
-
-func GetRange(token CanStartStopToken) *ssa.Range {
-	startToken := token.GetStart()
-	start := ssa.NewPosition(int64(startToken.GetStart()), int64(startToken.GetLine()), int64(startToken.GetColumn()))
-
-	endToken := token.GetStop()
-	endLine, endColumn := GetEndPosition(endToken)
-	end := ssa.NewPosition(int64(endToken.GetStop()), int64(endLine), int64(endColumn))
-
-	return ssa.NewRange(start, end, token.GetText())
-}
-
-func (b *astbuilder) SetRange(token CanStartStopToken) func() {
-
-	// TODO: use antlr4utils.GetRange, when new JS parser ready
-	r := GetRange(token)
+func (b *astbuilder) SetRange(token antlr4util.CanStartStopToken) func() {
+	r := antlr4util.GetRange(token)
 	backup := b.CurrentRange
 	b.CurrentRange = r
 
