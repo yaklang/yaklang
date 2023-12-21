c9cd8f941ad553ab7a3fa083763d192119804685
wlz <lingze.wl@gmail.com>
2023-12-21 17:22:19 +0800

    feat(CorePluginTest): add CorePlugin to AnalyzerTest in ci

---
+++ common/coreplugin/static_analyze_test.go
@@ -0,0 +1,41 @@
+package coreplugin
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/samber/lo"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/yak"
+)
+
+func Check(code string, t *testing.T) {
+	if res := yak.AnalyzeStaticYaklangWithType(string(code), "yak"); len(lo.Filter(res, func(item *yak.StaticAnalyzeResult, index int) bool {
+		return item.Severity == "error"
+	})) != 0 {
+		t.Fatalf("plugin : static analyzer failed: \n%s", res)
+	}
+}
+
+func TestAnalyzeMustPASS_CorePlugin(t *testing.T) {
+	files, err := basePlugin.ReadDir("base-yak-plugin")
+	if err != nil {
+		log.Error("Failed to read directory:", err)
+		return
+	}
+
+	for _, file := range files {
+		if !file.IsDir() {
+			filePath := fmt.Sprintf("base-yak-plugin/%s", file.Name())
+			codeBytes, err := basePlugin.ReadFile(filePath)
+			if err != nil {
+				log.Error("Failed to read file:", err)
+				continue
+			}
+
+			t.Run(fmt.Sprintf("plugin %s", file.Name()), func(t *testing.T) {
+				Check(string(codeBytes), t)
+			})
+		}
+	}
+}
