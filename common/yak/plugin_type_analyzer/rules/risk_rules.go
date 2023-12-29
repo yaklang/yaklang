ec8c9329ccc0d4652f1a57b6e14808f0bdc964e7
wlz <lingze.wl@gmail.com>
2023-12-29 11:39:19 +0800

    feat(risk rule): risk check risk Created saved ?

--- common/yak/plugin_type_analyzer/rules/risk_rules.go
+++ common/yak/plugin_type_analyzer/rules/risk_rules.go
@@ -13,7 +13,7 @@ func init() {
 func RuleRisk(prog *ssaapi.Program) {
 	tag := "cli.risk"
 
-	checkRisk := func(funcName string) {
+	checkRiskOption := func(funcName string) {
 		prog.Ref(funcName).GetUsers().Filter(func(v *ssaapi.Value) bool {
 			return v.IsCall() && v.IsReachable() != -1
 		}).ForEach(func(v *ssaapi.Value) {
@@ -41,12 +41,37 @@ func RuleRisk(prog *ssaapi.Program) {
 
 			}
 			if !(RiskCVE || (RiskDescription && RiskSolution)) {
-				v.NewError(tag, funcName + " should be called with (risk.description and risk.solution) or risk.cve")
+				v.NewError(tag, funcName+" should be called with (risk.description and risk.solution) or risk.cve")
 			}
 		})
 	}
 
-	checkRisk("risk.NewRisk")
+	checkRiskOption("risk.NewRisk")
+	checkRiskOption("risk.CreateRisk")
+
+	// check CreateRisk is saved ?
+	prog.Ref("risk.CreateRisk").GetUsers().Filter(func(v *ssaapi.Value) bool {
+		return v.IsCall() && v.IsReachable() != -1
+	}).ForEach(func(v *ssaapi.Value) {
+		// this v is risk.CreateRisk()
+		flag := false
+		v.GetUsers().Filter(func(v *ssaapi.Value) bool {
+			return v.IsCall()
+		}).ForEach(func(v *ssaapi.Value) {
+			// this is user of risk.CreateRisk()
+			if v.GetCallee().String() == "risk.Save" {
+				flag = true
+			}
+		})
+		if !flag {
+			v.NewError(tag, ErrorRiskCreateNotSave())
+		}
+	})
+
+}
+
+func ErrorRiskCreateNotSave() string {
+	return "risk.CreateRisk should be saved use `risk.Save`"
 }
 
 func ErrorRiskCheck() string {
