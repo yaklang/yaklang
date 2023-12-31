43730b1d9825556f4e940d6e9170741617e80f0b
VillanCh <188228725@qq.com>
2023-12-31 22:34:30 +0800

    ssautil

---
+++ common/yak/ssa/ssautil/phi.go
@@ -0,0 +1,58 @@
+package ssautil
+
+type PhiContext[T any] struct {
+	scope *ScopedVersionedTable[T]
+	name  string
+	phi   []*Versioned[T]
+}
+
+func (p *PhiContext[T]) AddPhi(i *Versioned[T]) {
+	p.phi = append(p.phi, i)
+}
+
+// ProducePhi produce the phi for the captured variable
+// note: this function will be called after the block sealed
+func ProducePhi[T any](
+	factory func(...any) any,
+	scopes ...*ScopedVersionedTable[T]) {
+	var ctxs = map[*ScopedVersionedTable[T]]map[string]*PhiContext[T]{}
+	for _, v := range scopes {
+		if v.IsRoot() {
+			return
+		}
+		for _, name := range v.GetAllCapturedVariableNames() {
+			capturedValue := v.parent.GetLatestVersion(name)
+			if capturedValue == nil {
+				continue
+			}
+			currentValue := v.GetLatestVersionInCurrentLexicalScope(name)
+			if currentValue == nil {
+				continue
+			}
+			targetScope := capturedValue.scope
+			if _, ok := ctxs[targetScope]; !ok {
+				ctxs[targetScope] = map[string]*PhiContext[T]{}
+			}
+			if _, ok := ctxs[targetScope][name]; !ok {
+				ctxs[targetScope][name] = &PhiContext[T]{
+					scope: targetScope,
+					name:  name,
+					phi:   []*Versioned[T]{},
+				}
+			}
+			ctxs[targetScope][name].AddPhi(currentValue)
+		}
+	}
+
+	for _, vars := range ctxs {
+		for _, ctx := range vars {
+			var vals = make([]any, 0, len(ctx.phi))
+			for _, v := range ctx.phi {
+				vals = append(vals, v.Value)
+			}
+			phi := factory(vals...)
+			result := ctx.scope.CreateLexicalVariable(ctx.name, phi)
+			result.SetPhi(true)
+		}
+	}
+}
---
+++ common/yak/ssa/ssautil/versioned.go
@@ -0,0 +1,71 @@
+package ssautil
+
+import (
+	"fmt"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils"
+)
+
+type Versioned[T any] struct {
+	// origin desc the variable's last or renamed version
+	origin       *Versioned[T]
+	versionIndex int
+	globalIndex  int
+	lexicalName  string
+
+	// the version of variable in current scope
+	scope *ScopedVersionedTable[T]
+
+	isPhi bool
+
+	isAssigned *utils.AtomicBool
+	Value      T
+}
+
+func (v *Versioned[T]) SetPhi(b bool) {
+	v.isPhi = b
+}
+
+func (v *Versioned[T]) IsPhi() bool {
+	return v.isPhi
+}
+
+func (v *Versioned[T]) String() string {
+	if v.lexicalName == "" {
+		return fmt.Sprintf("symbolic #%d", v.globalIndex)
+	}
+
+	if v.IsPhi() {
+		return fmt.Sprintf("#%d(phi) %s_%d", v.globalIndex, v.lexicalName, v.versionIndex)
+	}
+	return fmt.Sprintf("#%d %s_%d", v.globalIndex, v.lexicalName, v.versionIndex)
+}
+
+func (v *Versioned[T]) IsRoot() bool {
+	return v.origin == nil
+}
+
+func (v *Versioned[T]) GetRootVersion() *Versioned[T] {
+	if v.IsRoot() {
+		return v
+	}
+	return v.origin.GetRootVersion()
+}
+
+func (v *Versioned[T]) GetId() int {
+	return v.globalIndex
+}
+
+func (v *Versioned[T]) Assign(val T) error {
+	if v.isAssigned.IsSet() {
+		log.Warnf("ssa: #%v have been assigned by %v", v.globalIndex, v.Value)
+		return utils.Error("ssautil.VersionedVar should be assigned once")
+	}
+
+	if val == nil {
+		log.Warnf("ssa: #%v is trying to be assigned by nil")
+	}
+	v.isAssigned.Set()
+	v.Value = val
+	return nil
+}
