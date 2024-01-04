b50f461807ccacf80539dce8bcb77f06f5fdb72d
v1ll4n <v1ll4n.a5k@gmail.com>
2024-01-04 18:01:49 +0800

    kin openapi

--- common/openapi/v2mock.go
+++ common/openapi/v2mock.go
@@ -3,6 +3,8 @@ package openapi
 import (
 	"github.com/yaklang/yaklang/common/go-funk"
 	"github.com/yaklang/yaklang/common/openapi/openapi2"
+	"github.com/yaklang/yaklang/common/openapi/openapi3"
+	"github.com/yaklang/yaklang/common/utils/omap"
 	"strings"
 )
 
@@ -40,12 +42,34 @@ func OpenAPI2RefToObject(t openapi2.T, ref string) any {
 		return nil
 	}
 	val := obj.Value
-	for _, pro := range val.Properties {
-		if pro.Value == nil {
-			continue
-		}
-		pt := pro.Value
-		_ = pt
+	return schemaValue(t, val)
+}
+
+func schemaValue(data openapi2.T, i *openapi3.Schema) any {
+	if i == nil {
+		return nil
 	}
-	return nil
+
+	switch i.Type {
+	case "array":
+		m := omap.NewGeneralOrderedMap()
+		if i.Items.Ref != "" {
+			m.Add(OpenAPI2RefToObject(data, i.Items.Ref))
+			return m
+		}
+		m.Add(schemaValue(data, i.Items.Value))
+		return m
+	case "object":
+		m := omap.NewGeneralOrderedMap()
+		for field, pt := range i.Properties {
+			if pt.Ref != "" {
+				m.Set(field, OpenAPI2RefToObject(data, pt.Ref))
+			} else {
+				m.Set(field, schemaValue(data, pt.Value))
+			}
+		}
+		return m
+	default:
+		return OpenAPITypeToMockDataLiteral(i.Type, i.Default)
+	}
 }
