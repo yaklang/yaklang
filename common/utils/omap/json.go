be1f333b04cd68c8dc9a68dc9604dc1262678a4f
v1ll4n <v1ll4n.a5k@gmail.com>
2024-01-05 14:58:49 +0800

    add marshal json

---
+++ common/utils/omap/json.go
@@ -0,0 +1,62 @@
+package omap
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/yaklang/yaklang/common/utils"
+)
+
+func (v *OrderedMap[T, V]) MarshalJSON() ([]byte, error) {
+	if v.HaveLiteralValue() {
+		raw, err := json.Marshal(v.LiteralValue())
+		if err != nil {
+			return nil, utils.Errorf("cannot marshal literal val: %T", v.LiteralValue())
+		}
+		return raw, nil
+	}
+	if v.CanAsList() {
+		var buf bytes.Buffer
+		buf.WriteByte('[')
+		for index, val := range v.Values() {
+			if index != 0 {
+				buf.WriteByte(',')
+			}
+			raw, err := json.Marshal(val)
+			if err != nil {
+				buf.Write([]byte("null"))
+				continue
+			}
+			buf.Write(raw)
+		}
+		buf.WriteByte(']')
+		return buf.Bytes(), nil
+	}
+
+	var buf = new(bytes.Buffer)
+	buf.WriteByte('{')
+	first := true
+	v.ForEach(func(i T, v V) bool {
+		if first {
+			first = false
+		} else {
+			buf.WriteByte(',')
+		}
+		fieldName := fmt.Sprint(i)
+		_ = fieldName
+		raw, _ := json.Marshal(fieldName)
+		buf.Write(raw)
+		buf.WriteByte(':')
+
+		raw, err := json.Marshal(v)
+		if err != nil {
+			buf.Write([]byte("null"))
+			return true
+		}
+		buf.Write(raw)
+		return true
+	})
+	buf.WriteByte('}')
+
+	return buf.Bytes(), nil
+}
--- common/utils/omap/ordered_test.go
+++ common/utils/omap/ordered_test.go
@@ -29,3 +29,25 @@ func TestWalkSearch(t *testing.T) {
 	}
 	spew.Dump(vars)
 }
+
+func TestMarshalJSON(t *testing.T) {
+	m := NewGeneralOrderedMap()
+	m2 := NewGeneralOrderedMap()
+	m2.Set("B", 1)
+	m.Set("D", '1')
+
+	m.Set("A", m2)
+	m3 := NewGeneralOrderedMap()
+	m3.Add("111")
+	m3.Add("112")
+	m3.Add("113")
+	m.Set("C", m3)
+	raw, err := m.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(raw) != `{"D":49,"A":{"B":1},"C":["111","112","113"]}` {
+		t.Fatal(string(raw) + ": not right")
+	}
+	spew.Dump(raw)
+}
