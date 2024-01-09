eff850d2ccd9caacebb5d8d5a31d671bc846d4bb
z3 <z3r0ne@163.com>
2024-01-09 16:52:26 +0800

    mock了发包函数，增加了对traceroute执行逻辑的测试

--- common/utils/pingutil/traceroute_test.go
+++ common/utils/pingutil/traceroute_test.go
@@ -2,15 +2,30 @@ package pingutil
 
 import (
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/yaklang/yaklang/common/utils"
+	"math/rand"
 	"testing"
 )
 
-func _TestTraceroute(t *testing.T) {
-	rspChan, err := Traceroute("93.184.216.34")
+func TestTraceroute(t *testing.T) {
+	expectStr := ""
+	rspChan, err := Traceroute("8.8.8.8", WithSender(func(config *TracerouteConfig, host string, hop int) (*TracerouteResponse, error) {
+		response := &TracerouteResponse{
+			Hop:    hop,
+			IP:     utils.GetRandomIPAddress(),
+			RTT:    int64(rand.Intn(1000)),
+			Reason: utils.RandStringBytes(1),
+		}
+		expectStr += fmt.Sprintf("hop: %d, ip: %s, rtt: %dms, reason: %s\n", response.Hop, response.IP, response.RTT, response.Reason)
+		return response, nil
+	}))
 	if err != nil {
 		t.Fatal(err)
 	}
+	actualResp := ""
 	for response := range rspChan {
-		fmt.Printf("hop: %d, ip: %s, rtt: %dms, reason: %s\n", response.Hop, response.IP, response.RTT, response.Reason)
+		actualResp += fmt.Sprintf("hop: %d, ip: %s, rtt: %dms, reason: %s\n", response.Hop, response.IP, response.RTT, response.Reason)
 	}
+	assert.Equal(t, expectStr, actualResp)
 }
