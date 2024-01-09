d82f700dcc31ee1e15cd90ef2418bbbc3119075c
v1ll4n <v1ll4n.a5k@gmail.com>
2024-01-09 14:26:36 +0800

    fix response

--- common/openapi/request_test.go
+++ common/openapi/request_test.go
@@ -2,6 +2,8 @@ package openapi
 
 import (
 	_ "embed"
+	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
 	"testing"
 )
 
@@ -9,12 +11,25 @@ import (
 var openapi2demo string
 
 func TestRequest_V2(t *testing.T) {
-	v2Generator(openapi2demo, nil)
+	config := NewDefaultOpenAPIConfig()
+	db := consts.GetGormProjectDatabase()
+	config.FlowHandler = func(flow *yakit.HTTPFlow) {
+		flow.SourceType = "crawler"
+		yakit.SaveHTTPFlow(db, flow)
+	}
+	v2Generator(openapi2demo, config)
 }
 
 //go:embed openapi3/testdata/oai_v3_stoplight.json
 var openapi3demo string
 
 func TestRequest_V3(t *testing.T) {
-	v3Generator(openapi3demo, nil)
+	config := NewDefaultOpenAPIConfig()
+	db := consts.GetGormProjectDatabase()
+	_ = db
+	config.FlowHandler = func(flow *yakit.HTTPFlow) {
+		flow.SourceType = "crawler"
+		yakit.SaveHTTPFlow(db, flow)
+	}
+	v3Generator(openapi3demo, config)
 }
