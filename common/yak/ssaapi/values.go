31581df334aef625364eb624a8f4f4b751e37d82
wlz <lingze.wl@gmail.com>
2024-01-12 14:21:18 +0800

    fix(ssaapi): use ExternLib check IsExtern not Parameter

--- common/yak/ssaapi/values.go
+++ common/yak/ssaapi/values.go
@@ -388,7 +388,7 @@ func (v *Value) GetSelf() *Value {
 	return v
 }
 
-func (v *Value) IsExtern() bool       { return v.IsParameter() && v.node.IsExtern() }
+func (v *Value) IsExtern() bool       { return v.node.GetOpcode() == ssa.OpExternLib }
 func (v *Value) IsFunction() bool     { return v.node.GetOpcode() == ssa.OpFunction }
 func (v *Value) IsBasicBlock() bool   { return v.node.GetOpcode() == ssa.OpBasicBlock }
 func (v *Value) IsParameter() bool    { return v.node.GetOpcode() == ssa.OpParameter }
