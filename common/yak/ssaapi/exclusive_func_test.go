1670866886e50184cd02e5db4471330104270308
v1ll4n <v1ll4n.a5k@gmail.com>
2024-01-16 10:58:58 +0800

    add some testcase intro

--- common/yak/ssaapi/exclusive_func_test.go
+++ common/yak/ssaapi/exclusive_func_test.go
@@ -3,7 +3,7 @@ package ssaapi
 import "testing"
 
 func TestFunctionTrace(t *testing.T) {
-	Parse(`
+	prog, err := Parse(`
 a = 1
 b = (c, d) => {
 	a = c + d
@@ -13,5 +13,9 @@ e, f = b(2,3)
 g = e // 3
 h = f // 2
 i = a // 2 + 3
-`).Show()
+`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	prog.Show()
 }
