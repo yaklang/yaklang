cc2f0ea344b5e1571eb45fcd340bd61b314933a1
wlz <lingze.wl@gmail.com>
2024-01-16 16:54:40 +0800

    fix(grpc): language hover in loop

--- common/yakgrpc/grpc_language_suggestion_test.go
+++ common/yakgrpc/grpc_language_suggestion_test.go
@@ -2,6 +2,7 @@ package yakgrpc
 
 import (
 	"context"
+	"strings"
 	"testing"
 
 	"github.com/samber/lo"
@@ -73,28 +74,50 @@ prog.
 	})
 }
 
-func TestGRPCMUSTPASS_LANGUAGE_SuggestionHover(t *testing.T) {
-	local, err := NewLocalClient()
-	if err != nil {
-		t.Fatal(err)
+var local ypb.YakClient = nil
+
+func CheckHover(t *testing.T) func(t *testing.T, code, typ string, Range *ypb.Range, want string, subStr ...bool) {
+	if local == nil {
+		var err error
+		local, err = NewLocalClient()
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 
 	getHover := func(t *testing.T, code, typ string, Range *ypb.Range) *ypb.YaklangLanguageSuggestionResponse {
 		return GetSuggestion(local, "hover", typ, t, code, Range)
 	}
+	check := func(t *testing.T, code, typ string, Range *ypb.Range, want string, sub ...bool) {
+		subStr := false
+		for _, v := range sub {
+			if v {
+				subStr = true
+				break
+			}
+		}
 
-	check := func(t *testing.T, code, typ string, Range *ypb.Range, want string) {
 		req := getHover(t, code, typ, Range)
 		log.Info(req.SuggestionMessage)
 		if len(req.SuggestionMessage) != 1 {
 			t.Fatal("should get 1 suggestion")
 		}
 		got := req.SuggestionMessage[0].Label
-		if got != want {
-			t.Fatalf("want %s, but get %s", want, got)
+		if subStr {
+			if !strings.Contains(got, want) {
+				t.Fatalf("want %s, but get %s", want, got)
+			}
+		} else {
+			if got != want {
+				t.Fatalf("want %s, but get %s", want, got)
+			}
 		}
-
 	}
+	return check
+}
+
+func TestGRPCMUSTPASS_LANGUAGE_SuggestionHover_Basic(t *testing.T) {
+	check := CheckHover(t)
 
 	t.Run("check mitm hover argument", func(t *testing.T) {
 		check(t, `
@@ -114,55 +137,133 @@ func TestGRPCMUSTPASS_LANGUAGE_SuggestionHover(t *testing.T) {
 			"```go\nfunc modify(r1 yakit.HTTPFlow) null\n```",
 		)
 	})
+}
 
-	t.Run("check extern lib hover", func(t *testing.T) {
-		code := `
-ssa.Parse(
-    "var i = 0", 
+func TestGRPCMUSTPASS_LANGUAGE_SuggestionHover_ExternLib(t *testing.T) {
+	check := CheckHover(t)
+	code := `
+prog  = ssa.Parse(
+    "", 
     ssa.withLanguage(
         ssa.Javascript
     )
-)
+)~
 `
 
-		{
-			ssaRange := &ypb.Range{
-				Code:        "ssa",
-				StartLine:   2,
-				StartColumn: 0,
-				EndLine:     2,
-				EndColumn:   3,
-			}
-			want := getExternLibDesc("ssa", "any")
-			check(t, code, "yak", ssaRange, want)
+	t.Run("check extern lib hover", func(t *testing.T) {
+		ssaRange := &ypb.Range{
+			Code:        "ssa",
+			StartLine:   2,
+			StartColumn: 8,
+			EndLine:     2,
+			EndColumn:   11,
 		}
-		{
-			ssaParseRange := &ypb.Range{
-				Code:        "ssa.Parse",
-				StartLine:   2,
-				StartColumn: 0,
-				EndLine:     2,
-				EndColumn:   9,
-			}
-			// 标准库函数
-			funcDecl := getFuncDeclByName("ssa.Parse")
-			desc := getFuncDeclDesc(funcDecl, "Parse")
-			want := desc
-			check(t, code, "yak", ssaParseRange, want)
+		want := getExternLibDesc("ssa", "any")
+		check(t, code, "yak", ssaRange, want)
+	})
+
+	t.Run("check extern lib method hover", func(t *testing.T) {
+		ssaParseRange := &ypb.Range{
+			Code:        "ssa.Parse",
+			StartLine:   2,
+			StartColumn: 8,
+			EndLine:     2,
+			EndColumn:   17,
 		}
-		{
-			ssaParseRange := &ypb.Range{
-				Code:        "ssa.Javascript",
-				StartLine:   5,
-				StartColumn: 9,
-				EndLine:     5,
-				EndColumn:   22,
-			}
-			// 标准库变量
-			instance := getInstanceByName("ssa.Javascript")
-			desc := getInstanceDesc(instance)
-			want := desc
-			check(t, code, "yak", ssaParseRange, want)
+		// 标准库函数
+		funcDecl := getFuncDeclByName("ssa.Parse")
+		desc := getFuncDeclDesc(funcDecl, "Parse")
+		want := desc
+		check(t, code, "yak", ssaParseRange, want)
+	})
+
+	t.Run("check extern lib instance hover", func(t *testing.T) {
+		ssaParseRange := &ypb.Range{
+			Code:        "ssa.Javascript",
+			StartLine:   5,
+			StartColumn: 8,
+			EndLine:     5,
+			EndColumn:   22,
 		}
+		// 标准库变量
+		instance := getInstanceByName("ssa.Javascript")
+		desc := getInstanceDesc(instance)
+		want := desc
+		check(t, code, "yak", ssaParseRange, want)
+	})
+	t.Run("check extern lib method return hover", func(t *testing.T) {
+		progRange := &ypb.Range{
+			Code:        "prog",
+			StartLine:   2,
+			StartColumn: 0,
+			EndLine:     2,
+			EndColumn:   4,
+		}
+		want := `func (Program) Ref(name string) Value`
+		check(t, code, "yak", progRange, want, true)
+	})
+}
+
+func TestGRPCMUSTPASS_LANGUAGE_SuggestionHover_ExternLib_InLoop(t *testing.T) {
+	check := CheckHover(t)
+	code := `for a {
+prog = ssa.Parse(
+	"", 
+	ssa.withLanguage(
+		ssa.Javascript
+	)
+)~
+}`
+	t.Run("check extern lib hover", func(t *testing.T) {
+		ssaRange := &ypb.Range{
+			Code:        "ssa",
+			StartLine:   2,
+			StartColumn: 8,
+			EndLine:     2,
+			EndColumn:   11,
+		}
+		want := getExternLibDesc("ssa", "any")
+		check(t, code, "yak", ssaRange, want)
+	})
+
+	t.Run("check extern lib method hover", func(t *testing.T) {
+		ssaParseRange := &ypb.Range{
+			Code:        "ssa.Parse",
+			StartLine:   2,
+			StartColumn: 8,
+			EndLine:     2,
+			EndColumn:   17,
+		}
+		// 标准库函数
+		funcDecl := getFuncDeclByName("ssa.Parse")
+		desc := getFuncDeclDesc(funcDecl, "Parse")
+		want := desc
+		check(t, code, "yak", ssaParseRange, want)
+	})
+
+	t.Run("check extern lib instance hover", func(t *testing.T) {
+		ssaParseRange := &ypb.Range{
+			Code:        "ssa.Javascript",
+			StartLine:   5,
+			StartColumn: 8,
+			EndLine:     5,
+			EndColumn:   22,
+		}
+		// 标准库变量
+		instance := getInstanceByName("ssa.Javascript")
+		desc := getInstanceDesc(instance)
+		want := desc
+		check(t, code, "yak", ssaParseRange, want)
+	})
+	t.Run("check extern lib method return hover", func(t *testing.T) {
+		progRange := &ypb.Range{
+			Code:        "prog",
+			StartLine:   2,
+			StartColumn: 0,
+			EndLine:     2,
+			EndColumn:   4,
+		}
+		want := `func (Program) Ref(name string) Value`
+		check(t, code, "yak", progRange, want, true)
 	})
 }
