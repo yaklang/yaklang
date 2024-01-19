11dc62c909350b592c547a49cfd1ee6a4297193e
v1ll4n <v1ll4n.a5k@gmail.com>
2024-01-19 18:01:29 +0800

    hook every node

--- common/yak/ssaapi/exclusive_config.go
+++ common/yak/ssaapi/exclusive_config.go
@@ -4,6 +4,9 @@ type OperationConfig struct {
 	// 限制递归深度，每一次递归核心函数，计数器都会加一
 	// 上下文计数器受到这个限制
 	MaxDepth int
+
+	// Hook
+	HookEveryNode func(*Value) error
 }
 
 type OperationOption func(*OperationConfig)
@@ -11,6 +14,12 @@ type OperationOption func(*OperationConfig)
 func WithMaxDepth(maxDepth int) OperationOption {
 	return func(operationConfig *OperationConfig) {
 		operationConfig.MaxDepth = maxDepth
+	}
+}
+
+func WithHookEveryNode(hookNode func(*Value) error) OperationOption {
+	return func(operationConfig *OperationConfig) {
+		operationConfig.HookEveryNode = hookNode
 	}
 }
 
