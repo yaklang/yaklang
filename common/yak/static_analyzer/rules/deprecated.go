08c64aaf2702c3cf786a336250c531e39025ce7f
wlz <lingze.wl@gmail.com>
2024-01-24 17:28:46 +0800

    feat(static_analyze): support check deprecated function

---
+++ common/yak/static_analyzer/rules/deprecated.go
@@ -0,0 +1,28 @@
+package rules
+
+import (
+	"github.com/yaklang/yaklang/common/yak/ssaapi"
+	"github.com/yaklang/yaklang/common/yak/static_analyzer/plugin_type"
+	"github.com/yaklang/yaklang/common/yak/static_analyzer/result"
+	"github.com/yaklang/yaklang/common/yak/yakdoc/doc"
+)
+
+func init() {
+	plugin_type.RegisterCheckRuler(plugin_type.PluginTypeYak, Deprecated)
+}
+
+func Deprecated(prog *ssaapi.Program) *result.StaticAnalyzeResults {
+	res := result.NewStaticAnalyzeResults("deprecated check")
+
+	handler := func(funName, msg string) {
+		prog.Ref(funName).GetUsers().ForEach(func(v *ssaapi.Value) {
+			res.NewDeprecated(msg, v)
+		})
+	}
+
+	funcs := doc.DefaultDocumentHelper.DeprecatedFunctions
+	for _, fun := range funcs {
+		handler(fun.Name, fun.Msg)
+	}
+	return res
+}
