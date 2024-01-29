0add4e5a1b2c4b33a7dbe5dd898a506979926673
rookie <rookie_is@qq.com>
2024-01-29 17:18:35 +0800

    feat(hybridScan):添加接口供给渲染file fuzztag

---
+++ common/yakgrpc/grpc_fuzzer_render.go
@@ -0,0 +1,21 @@
+package yakgrpc
+
+import (
+	"context"
+	"github.com/yaklang/yaklang/common/mutate"
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/utils/lowhttp"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+)
+
+func (s *Server) RenderHTTPFuzzerPacket(ctx context.Context, req *ypb.RenderHTTPFuzzerPacketRequest) (*ypb.RenderHTTPFuzzerPacketResponse, error) {
+	packet := req.GetPacket()
+	res, err := mutate.FuzzTagExec(packet, mutate.Fuzz_WithEnableFiletag())
+	if err != nil || len(res) == 0 {
+		return nil, utils.Wrapf(err, "cannot render fuzztag: %v", packet)
+	}
+	newPacket := res[0]
+	return &ypb.RenderHTTPFuzzerPacketResponse{
+		Packet: lowhttp.ConvertHTTPRequestToFuzzTag([]byte(newPacket)),
+	}, nil
+}
