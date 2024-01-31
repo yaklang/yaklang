cfc71b56200a0ec04e555956e81afaffff9e0726
v1ll4n <v1ll4n.a5k@gmail.com>
2024-01-31 15:15:32 +0800

    generate without scheme

--- common/openapi/openapigen/path.go
+++ common/openapi/openapigen/path.go
@@ -98,22 +98,39 @@ func mergeParams(params ...[]*openapi3.Parameter) []*openapi3.Parameter {
 	return result.Values()
 }
 
-func pathToOpenAPIStruct(pathRaw string) (string, *openapi3.PathItem, []*openapi3.Parameter) {
+func mergeRefParams(params ...openapi3.Parameters) openapi3.Parameters {
+	var origin [][]*openapi3.Parameter
+	for _, pset := range params {
+		var ret []*openapi3.Parameter
+		for _, ref := range pset {
+			if ref.Value != nil {
+				ret = append(ret, ref.Value)
+			}
+		}
+		origin = append(origin, ret)
+	}
+	return paramListToParameters(mergeParams(origin...))
+}
+
+func pathToOpenAPIStruct(item *openapi3.PathItem, pathRaw string) (string, *openapi3.PathItem, []*openapi3.Parameter) {
 	pathWithoutQuery, query, haveQuery := strings.Cut(pathRaw, "?")
 	if !haveQuery {
 		query = ""
 	}
-	item := &openapi3.PathItem{
-		Connect: nil,
-		Delete:  nil,
-		Get:     nil,
-		Head:    nil,
-		Options: nil,
-		Patch:   nil,
-		Post:    nil,
-		Put:     nil,
-		Trace:   nil,
+	if item == nil {
+		item = &openapi3.PathItem{
+			Connect: nil,
+			Delete:  nil,
+			Get:     nil,
+			Head:    nil,
+			Options: nil,
+			Patch:   nil,
+			Post:    nil,
+			Put:     nil,
+			Trace:   nil,
+		}
 	}
+
 	if query != "" {
 		return pathWithoutQuery, item, extractQueryParams(query)
 	}
@@ -140,6 +157,10 @@ Content-Type: application/json
 
 	ins := &openapi3.Response{}
 	for key, value := range lowhttp.GetHTTPPacketHeaders(response) {
+		switch strings.ToLower(key) {
+		case "content-length", "content-type", "transfer-encoding", "content-encoding":
+			continue
+		}
 		if ins.Headers == nil {
 			ins.Headers = make(map[string]*openapi3.HeaderRef)
 		}
@@ -155,12 +176,39 @@ Content-Type: application/json
 	return ins
 }
 
-func requestToOpenAPIStruct(request []byte) (string, *openapi3.PathItem, *openapi3.Operation, error) {
+func mergeResponse(rsps *openapi3.Responses, code int, rsp *openapi3.Response) *openapi3.Responses {
+	if rsps == nil {
+		rsps = openapi3.NewResponses()
+	}
+	if rsps.Status(code) == nil {
+		rsps.Set(fmt.Sprint(code), &openapi3.ResponseRef{Value: rsp})
+	}
+	return rsps
+}
+
+func HttpFlowToOpenAPIStruct(before string, pathItem *openapi3.PathItem, req []byte, rsp []byte) (string, *openapi3.PathItem, error) {
+	if before == "" {
+		before = lowhttp.GetHTTPRequestPathWithoutQuery(req)
+	}
+	pathName, pathItem, operations, err := requestToOpenAPIStruct(before, pathItem, req)
+	if err != nil {
+		return pathName, pathItem, err
+	}
+
+	statusCode := lowhttp.ExtractStatusCodeFromResponse(rsp)
+	if statusCode <= 0 {
+		statusCode = 200
+	}
+	operations.Responses = mergeResponse(operations.Responses, statusCode, responseToOpenAPIStruct(rsp))
+	return pathName, pathItem, nil
+}
+
+func requestToOpenAPIStruct(beforePath string, item *openapi3.PathItem, request []byte) (string, *openapi3.PathItem, *openapi3.Operation, error) {
 	p := lowhttp.GetHTTPRequestPath(request)
 	if p == "" {
 		return "", nil, nil, utils.Error("path is empty")
 	}
-	pathWithoutQuery, ops, params := pathToOpenAPIStruct(p)
+	pathWithoutQuery, ops, params := pathToOpenAPIStruct(item, p)
 
 	operation := &openapi3.Operation{
 		Parameters: paramListToParameters(params),
@@ -170,25 +218,160 @@ func requestToOpenAPIStruct(request []byte) (string, *openapi3.PathItem, *openap
 	method := lowhttp.GetHTTPRequestMethod(request)
 	switch strings.TrimSpace(strings.ToUpper(method)) {
 	case "GET":
-		ops.Get = operation
+		if ops.Get == nil {
+			ops.Get = operation
+		} else {
+			after, parameters, isSame, _ := shrinkPath(beforePath, pathWithoutQuery)
+			if !isSame && len(parameters) > 0 {
+				ops.Get.Parameters = mergeRefParams(ops.Get.Parameters, paramListToParameters(parameters))
+				pathWithoutQuery = after
+			}
+			ops.Get.Parameters = mergeRefParams(ops.Get.Parameters, operation.Parameters)
+		}
 	case "POST":
-		ops.Post = operation
+		if ops.Post == nil {
+			ops.Post = operation
+		} else {
+			after, parameters, isSame, _ := shrinkPath(beforePath, pathWithoutQuery)
+			if !isSame && len(parameters) > 0 {
+				ops.Post.Parameters = mergeRefParams(ops.Post.Parameters, paramListToParameters(parameters))
+				pathWithoutQuery = after
+			}
+			ops.Post.Parameters = mergeRefParams(ops.Post.Parameters, operation.Parameters)
+		}
 	case "PUT":
-		ops.Put = operation
+		if ops.Put == nil {
+			ops.Put = operation
+		} else {
+			after, parameters, isSame, _ := shrinkPath(beforePath, pathWithoutQuery)
+			if !isSame && len(parameters) > 0 {
+				ops.Put.Parameters = mergeRefParams(ops.Put.Parameters, paramListToParameters(parameters))
+				pathWithoutQuery = after
+			}
+			ops.Put.Parameters = mergeRefParams(ops.Put.Parameters, operation.Parameters)
+		}
 	case "DELETE":
-		ops.Delete = operation
+		if ops.Delete == nil {
+			ops.Delete = operation
+		} else {
+			after, parameters, isSame, _ := shrinkPath(beforePath, pathWithoutQuery)
+			if !isSame && len(parameters) > 0 {
+				ops.Delete.Parameters = mergeRefParams(ops.Delete.Parameters, paramListToParameters(parameters))
+				pathWithoutQuery = after
+			}
+			ops.Delete.Parameters = mergeRefParams(ops.Delete.Parameters, operation.Parameters)
+		}
 	case "CONNECT":
-		ops.Connect = operation
+		if ops.Connect == nil {
+			ops.Connect = operation
+		} else {
+			after, parameters, isSame, _ := shrinkPath(beforePath, pathWithoutQuery)
+			if !isSame && len(parameters) > 0 {
+				ops.Connect.Parameters = mergeRefParams(ops.Connect.Parameters, paramListToParameters(parameters))
+				pathWithoutQuery = after
+			}
+			ops.Connect.Parameters = mergeRefParams(ops.Connect.Parameters, operation.Parameters)
+		}
 	case "OPTIONS":
-		ops.Options = operation
+		if ops.Options == nil {
+			ops.Options = operation
+		} else {
+			after, parameters, isSame, _ := shrinkPath(beforePath, pathWithoutQuery)
+			if !isSame && len(parameters) > 0 {
+				ops.Options.Parameters = mergeRefParams(ops.Options.Parameters, paramListToParameters(parameters))
+				pathWithoutQuery = after
+			}
+			ops.Options.Parameters = mergeRefParams(ops.Options.Parameters, operation.Parameters)
+		}
 	case "TRACE":
-		ops.Trace = operation
+		if ops.Trace == nil {
+			ops.Trace = operation
+		} else {
+			after, parameters, isSame, _ := shrinkPath(beforePath, pathWithoutQuery)
+			if !isSame && len(parameters) > 0 {
+				ops.Trace.Parameters = mergeRefParams(ops.Trace.Parameters, paramListToParameters(parameters))
+				pathWithoutQuery = after
+			}
+			ops.Trace.Parameters = mergeRefParams(ops.Trace.Parameters, operation.Parameters)
+		}
 	case "PATCH":
-		ops.Patch = operation
+		if ops.Patch == nil {
+			ops.Patch = operation
+		} else {
+			after, parameters, isSame, _ := shrinkPath(beforePath, pathWithoutQuery)
+			if !isSame && len(parameters) > 0 {
+				ops.Patch.Parameters = mergeRefParams(ops.Patch.Parameters, paramListToParameters(parameters))
+				pathWithoutQuery = after
+			}
+			ops.Patch.Parameters = mergeRefParams(ops.Patch.Parameters, operation.Parameters)
+		}
 	case "HEAD":
-		ops.Head = operation
+		if ops.Head == nil {
+			ops.Head = operation
+		} else {
+			after, parameters, isSame, _ := shrinkPath(beforePath, pathWithoutQuery)
+			if !isSame && len(parameters) > 0 {
+				ops.Head.Parameters = mergeRefParams(ops.Head.Parameters, paramListToParameters(parameters))
+				pathWithoutQuery = after
+			}
+			ops.Head.Parameters = mergeRefParams(ops.Head.Parameters, operation.Parameters)
+		}
 	default:
 		return "", nil, nil, utils.Errorf("method [%v] not supported", method)
 	}
 	return pathWithoutQuery, ops, operation, nil
+}
+
+type BasicHTTPFlow struct {
+	Request  []byte
+	Response []byte
+}
+
+func generate(c chan *BasicHTTPFlow) ([]byte, error) {
+	results := omap.NewOrderedMap(map[string]*openapi3.PathItem{})
+	for flow := range c {
+		pathWithoutQ := lowhttp.GetHTTPRequestPathWithoutQuery(flow.Request)
+		isNewPath := true
+		var existedPath string
+		for _, existed := range results.Keys() {
+			_, params, _, _ := shrinkPath(existed, pathWithoutQ)
+			if len(params) > 0 {
+				existedPath = existed
+				isNewPath = false
+				break
+			}
+		}
+		if !isNewPath {
+			pathWithoutQ = existedPath
+		}
+		pathItem, ok := results.Get(pathWithoutQ)
+		if !ok {
+			pathItem = nil
+		}
+		newPath, pathItem, err := HttpFlowToOpenAPIStruct(pathWithoutQ, pathItem, flow.Request, flow.Response)
+		if err != nil {
+			continue
+		}
+		if newPath != pathWithoutQ {
+			results.Delete(pathWithoutQ)
+			results.Set(newPath, pathItem)
+		} else {
+			results.Set(pathWithoutQ, pathItem)
+		}
+	}
+
+	var t openapi3.T
+	t.Info = &openapi3.Info{
+		Title:          "Yakit Generated API for www.example.com",
+		Description:    "",
+		TermsOfService: "",
+		Version:        "",
+	}
+
+	t.Paths = openapi3.NewPaths()
+	results.ForEach(func(i string, v *openapi3.PathItem) bool {
+		t.Paths.Set(i, v)
+		return true
+	})
+	return t.MarshalJSON()
 }
