577a645350a9f648b431a90cce238a010d0b0515
v1ll4n <v1ll4n.a5k@gmail.com>
2024-01-31 15:15:32 +0800

    generate without scheme

--- common/openapi/openapigen/path_test.go
+++ common/openapi/openapigen/path_test.go
@@ -1,8 +1,12 @@
 package openapigen
 
 import (
+	"fmt"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/assert"
+	"github.com/yaklang/yaklang/common/openapi/openapiyaml"
+	"github.com/yaklang/yaklang/common/utils"
+	"strings"
 	"testing"
 )
 
@@ -70,4 +74,81 @@ func TestShrinkPath2(t *testing.T) {
 	test := assert.New(t)
 	test.Equal(after, "/api/v1/brute/123")
 	test.Equal(len(params), 0)
+}
+
+func TestBasicUse(t *testing.T) {
+	path, item, err := HttpFlowToOpenAPIStruct("", nil, []byte(`GET /v1/abc/asdfasdf/1
+Host: www.example.com
+`), nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	path, item, err = HttpFlowToOpenAPIStruct(path, item, []byte(`GET /v1/abc/asdfasdf/2
+Host: www.example.com`), []byte(`HTTP/1.1 302 Per
+Content-Length: 1
+
+1`))
+	spew.Dump(path)
+	raw, err := item.MarshalJSON()
+	if err != nil {
+		t.Fatal(err)
+	}
+	raw, _ = openapiyaml.JSONToYAML(raw)
+	fmt.Println(string(raw))
+
+	if !strings.Contains(string(raw), `asdfasdfId`) {
+		t.Fatal("not contains asdfasdfId")
+	}
+}
+
+func TestGenerate(t *testing.T) {
+	input := make(chan *BasicHTTPFlow, 100)
+	input <- &BasicHTTPFlow{
+		Request: []byte(`GET /v1/abc/asdfasdf/1
+Host: www.example.com`),
+		Response: []byte("HTTP/1.1 302 Per\nContent-Length: 1\n\n1"),
+	}
+	input <- &BasicHTTPFlow{
+		Request: []byte(`GET /v1/abc/asdfasdf/2
+Host: www.example.com`),
+		Response: []byte("HTTP/1.1 302 Per\nContent-Length: 1\n\n1"),
+	}
+	input <- &BasicHTTPFlow{
+		Request: []byte(`GET /v1/abc/ddd/aaa21
+Host: www.example.com`),
+		Response: []byte("HTTP/1.1 302 Per\nContent-Length: 1\n\n1"),
+	}
+	input <- &BasicHTTPFlow{
+		Request: []byte(`GET /v1/abc/eee/2?id=2&ca=1
+Host: www.example.com`),
+		Response: []byte("HTTP/1.1 302 Per\nContent-Length: 1\n\n1"),
+	}
+	input <- &BasicHTTPFlow{
+		Request: []byte(`GET /v1/abc/eee/5
+Host: www.example.com`),
+		Response: []byte("HTTP/1.1 302 Per\nContent-Length: 1\n\n1"),
+	}
+	input <- &BasicHTTPFlow{
+		Request: []byte(`GET /v1/abc/eee/5?id=1
+Host: www.example.com`),
+		Response: []byte("HTTP/1.1 302 Per\nContent-Length: 1\n\n1"),
+	}
+	close(input)
+
+	raw, err := generate(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	raw, _ = openapiyaml.JSONToYAML(raw)
+	fmt.Println(string(raw))
+
+	if utils.MatchAllOfSubString(
+		string(raw),
+		`/v1/abc/eee/5:`,
+		`/v1/abc/ddd/{dddId}:`,
+		`/v1/abc/asdfasdf/{asdfasdfId}:`) {
+		t.Log("ok")
+	} else {
+		t.Fatal("not match")
+	}
 }
