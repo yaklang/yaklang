7dcbff138a48a4586f090d9904b4248fdb165c22
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-02-05 17:39:49 +0800

    fix(lowhttp): FixHTTPResponse charset error

--- common/utils/lowhttp/prescan.go
+++ common/utils/lowhttp/prescan.go
@@ -2,10 +2,11 @@ package lowhttp
 
 import (
 	"bytes"
+	"strings"
+
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/charset"
 	"golang.org/x/text/encoding"
-	"strings"
 )
 
 func charsetFromMetaElement(s string) string {
@@ -44,7 +45,7 @@ func charsetFromMetaElement(s string) string {
 
 func charsetPrescan(content []byte) (e encoding.Encoding, name string) {
 	z := html.NewTokenizer(bytes.NewReader(content))
-	var count = 0
+	count := 0
 	for {
 		count++
 		if count > 2000 {
@@ -59,7 +60,7 @@ func charsetPrescan(content []byte) (e encoding.Encoding, name string) {
 			if !bytes.Equal(tagName, []byte("meta")) {
 				continue
 			}
-			attrList := make(map[string]bool)
+			attrList := make(map[string]struct{})
 			gotPragma := false
 
 			const (
@@ -75,10 +76,10 @@ func charsetPrescan(content []byte) (e encoding.Encoding, name string) {
 				var key, val []byte
 				key, val, hasAttr = z.TagAttr()
 				ks := string(key)
-				if attrList[ks] {
+				if _, ok := attrList[ks]; ok {
 					continue
 				}
-				attrList[ks] = true
+				attrList[ks] = struct{}{}
 				for i, c := range val {
 					if 'A' <= c && c <= 'Z' {
 						val[i] = c + 0x20
