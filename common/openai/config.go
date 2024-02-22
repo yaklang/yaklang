e904bccbdaf46fa51b91d948b38ff39145dce2d5
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-02-22 18:02:05 +0800

    feat(lib-openai): function call

--- common/openai/config.go
+++ common/openai/config.go
@@ -1,11 +1,12 @@
 package openai
 
 import (
-	"github.com/yaklang/yaklang/common/consts"
-	"github.com/yaklang/yaklang/common/log"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/log"
 )
 
 type ConfigOption func(client *Client)
@@ -24,7 +25,7 @@ func WithAPIKey(i string) ConfigOption {
 
 func WithAPIKeyFromYakitHome() ConfigOption {
 	return func(client *Client) {
-		var raw, err = os.ReadFile(filepath.Join(consts.GetDefaultYakitBaseDir(), "openai-key.txt"))
+		raw, err := os.ReadFile(filepath.Join(consts.GetDefaultYakitBaseDir(), "openai-key.txt"))
 		if err != nil {
 			log.Warnf("cannot find openai-key.txt in %s", consts.GetDefaultYakitProjectsDir())
 			return
@@ -48,5 +49,32 @@ func WithDomain(i string) ConfigOption {
 func WithYakProxy() ConfigOption {
 	return func(client *Client) {
 		client.Domain = "api.ai.yaklang.com"
+	}
+}
+
+func WithFunctionParameterType(i string) ConfigOption {
+	return func(client *Client) {
+		client.Parameters.Type = i
+	}
+}
+
+func WithFunctionProperty(name, typ, description string, enum ...[]string) ConfigOption {
+	_enum := []string{}
+	if len(enum) > 0 {
+		_enum = enum[0]
+	}
+
+	return func(client *Client) {
+		client.Parameters.Properties[name] = Property{
+			Type:        typ,
+			Description: description,
+			Enum:        _enum,
+		}
+	}
+}
+
+func WithFunctionRequired(names ...string) ConfigOption {
+	return func(client *Client) {
+		client.Parameters.Required = names
 	}
 }
