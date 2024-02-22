8b10c441c7cfa653740924cfcf089c913d0d6d53
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-02-22 15:28:35 +0800

    feat(thirdparty): ExtraParams

--- common/consts/thirdparty.go
+++ common/consts/thirdparty.go
@@ -1,8 +1,9 @@
 package consts
 
 import (
-	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 	"sync"
+
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
 // thirdparty app config
@@ -16,20 +17,31 @@ type thirdPartyApplicationConfig struct {
 	Namespace      string
 	Domain         string
 	WebhookURL     string
+	ExtraParams    map[string]string
+}
+
+func (c *thirdPartyApplicationConfig) GetExtraParam(name string) string {
+	if c.ExtraParams == nil {
+		return ""
+	}
+	if v, ok := c.ExtraParams[name]; ok {
+		return v
+	}
+	return ""
 }
 
 func GetThirdPartyApplicationConfig(t string) *thirdPartyApplicationConfig {
 	if v, ok := thirdPartyConfig.Load(t); ok {
 		return v.(*thirdPartyApplicationConfig)
 	}
-	return &thirdPartyApplicationConfig{}
+	return &thirdPartyApplicationConfig{ExtraParams: make(map[string]string)}
 }
 
 func AllThirdPartyApplicationConfig() []*ypb.ThirdPartyApplicationConfig {
 	var configs []*ypb.ThirdPartyApplicationConfig
 	thirdPartyConfig.Range(func(key, value interface{}) bool {
 		rawConfig := value.(*thirdPartyApplicationConfig)
-		configs = append(configs, &ypb.ThirdPartyApplicationConfig{
+		config := &ypb.ThirdPartyApplicationConfig{
 			Type:           rawConfig.Type,
 			APIKey:         rawConfig.APIKey,
 			UserIdentifier: rawConfig.UserIdentifier,
@@ -37,17 +49,25 @@ func AllThirdPartyApplicationConfig() []*ypb.ThirdPartyApplicationConfig {
 			Namespace:      rawConfig.Namespace,
 			Domain:         rawConfig.Domain,
 			WebhookURL:     rawConfig.WebhookURL,
-		})
+			ExtraParams:    make([]*ypb.KVPair, 0, len(rawConfig.ExtraParams)),
+		}
+		for k, v := range rawConfig.ExtraParams {
+			config.ExtraParams = append(config.ExtraParams, &ypb.KVPair{Key: k, Value: v})
+		}
+
+		configs = append(configs, config)
 		return true
 	})
 	return configs
 }
+
 func ClearThirdPartyApplicationConfig() {
 	thirdPartyConfig.Range(func(key, value interface{}) bool {
 		thirdPartyConfig.Delete(key)
 		return true
 	})
 }
+
 func UpdateThirdPartyApplicationConfig(config *ypb.ThirdPartyApplicationConfig) {
 	if config.Type == "" {
 		return
@@ -61,6 +81,11 @@ func UpdateThirdPartyApplicationConfig(config *ypb.ThirdPartyApplicationConfig) 
 		Namespace:      config.Namespace,
 		Domain:         config.Domain,
 		WebhookURL:     config.WebhookURL,
+		ExtraParams:    make(map[string]string, len(config.ExtraParams)),
 	}
+	for _, kv := range config.ExtraParams {
+		c.ExtraParams[kv.Key] = kv.Value
+	}
+
 	thirdPartyConfig.Store(config.Type, c)
 }
