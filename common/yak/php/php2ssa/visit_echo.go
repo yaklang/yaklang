d480f86ad7eb080adf541362013a193cebd5c3cf
VillanCh <188228725@qq.com>
2024-02-27 00:09:03 +0800

    fix undefined

--- common/yak/php/php2ssa/visit_echo.go
+++ common/yak/php/php2ssa/visit_echo.go
@@ -1,6 +1,9 @@
 package php2ssa
 
-import phpparser "github.com/yaklang/yaklang/common/yak/php/parser"
+import (
+	phpparser "github.com/yaklang/yaklang/common/yak/php/parser"
+	"github.com/yaklang/yaklang/common/yak/ssa"
+)
 
 func (y *builder) VisitEchoStatement(raw phpparser.IEchoStatementContext) interface{} {
 	if y == nil || raw == nil {
@@ -12,5 +15,14 @@ func (y *builder) VisitEchoStatement(raw phpparser.IEchoStatementContext) interf
 		return nil
 	}
 
+	caller := y.ir.ReadOrCreateVariable("echo")
+	var args []ssa.Value
+	for _, expr := range i.ExpressionList().(*phpparser.ExpressionListContext).AllExpression() {
+		val := y.VisitExpression(expr)
+		args = append(args, val)
+	}
+
+	call := y.ir.NewCall(caller, args)
+	y.ir.EmitCall(call)
 	return nil
 }
