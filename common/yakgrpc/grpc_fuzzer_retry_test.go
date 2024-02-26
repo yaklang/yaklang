e519ecd368647256e60637f0f37b4f2dd4a0a5b6
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-02-26 14:42:22 +0800

    test(HTTPFuzzer_Retry):

---
+++ common/yakgrpc/grpc_fuzzer_retry_test.go
@@ -0,0 +1,120 @@
+package yakgrpc
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+)
+
+func TestGRPCMUSTPASS_HTTPFuzzer_Retry(t *testing.T) {
+	c, err := NewLocalClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	token := utils.RandStringBytes(16)
+	count := 0
+	targetHost, targetPort := utils.DebugMockTCPEx(func(ctx context.Context, lis net.Listener, conn net.Conn) {
+		count++
+		if count%2 == 0 {
+			conn.Close()
+			return
+		}
+		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nHello\r\n"))
+	})
+
+	client, err := c.HTTPFuzzer(context.Background(), &ypb.FuzzerRequest{
+		ForceFuzz: true,
+		Params: []*ypb.FuzzerParamItem{
+			{
+				Key:   "token",
+				Value: token,
+			},
+		},
+		Request: `GET /?token={{params(token)}}c={{int(1-10)}} HTTP/1.1
+Host: ` + utils.HostPort(targetHost, targetPort) + `
+`,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rspCount := 0
+	rspSuccessCount := 0
+	var taskID int64 = 0
+	for {
+		rsp, err := client.Recv()
+		if err != nil {
+			break
+		}
+		rspCount++
+		if rsp.Ok {
+			rspSuccessCount++
+		}
+		taskID = rsp.GetTaskId()
+	}
+	if taskID == 0 {
+		t.Fatal("TaskID not found in response")
+	}
+	if rspCount != 10 {
+		t.Fatalf("want 10 responses, but got %d", rspCount)
+	}
+	if rspSuccessCount != 5 {
+		t.Fatalf("want 5 success responses, but got %d", rspSuccessCount)
+	}
+
+	retryTestCases := []int{7, 9, 9, 10}
+	for i, wantSuccessCount := range retryTestCases {
+		client, err = c.HTTPFuzzer(context.Background(), &ypb.FuzzerRequest{
+			RetryTaskID: taskID,
+		})
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		rspCount, rspSuccessCount = 0, 0
+		for {
+			rsp, err := client.Recv()
+			if err != nil {
+				break
+			}
+			if len(rsp.RequestRaw) > 0 {
+				rspCount++
+			}
+			if rsp.Ok {
+				rspSuccessCount++
+			}
+			taskID = rsp.TaskId
+		}
+
+		if rspCount != 10 {
+			t.Fatalf("[retry %d] want 10 responses, but got %d", i+1, rspCount)
+		}
+		if rspSuccessCount != wantSuccessCount {
+			t.Fatalf("[retry %d] want %d success responses, but got %d", i+1, wantSuccessCount, rspSuccessCount)
+		}
+	}
+
+	taskRsp, err := c.QueryHistoryHTTPFuzzerTaskEx(context.Background(), &ypb.QueryHistoryHTTPFuzzerTaskExParams{
+		Keyword: token,
+		Pagination: &ypb.Paging{
+			Page:    1,
+			Limit:   10,
+			OrderBy: "id",
+			Order:   "asc",
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if taskRsp.Total != 1 {
+		t.Fatalf("want 1 task, but got %d", taskRsp.Total)
+	}
+	taskData := taskRsp.Data[0]
+	if taskData.BasicInfo.HTTPFlowTotal != 10 && taskData.BasicInfo.HTTPFlowSuccessCount != 10 {
+		t.Fatalf("task check failed: %#v", taskData.BasicInfo)
+	}
+}
