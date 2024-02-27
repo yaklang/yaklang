d27d0ea4e92f27623a2c25f8fee98674467800d0
wlz <lingze.wl@gmail.com>
2024-02-27 11:08:33 +0800

    refactor(crawler):modify js check rule

--- common/crawler/js_test.go
+++ common/crawler/js_test.go
@@ -4,35 +4,73 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
 func TestMUSTPASS_JSHandle(t *testing.T) {
-	var count = 0
-	code := `console.log('1.js'); var deepUrl = 'deep.js';;
+
+	check := func(t *testing.T, code string, want *requestNewTarget) {
+		test := assert.New(t)
+
+		handleJS(code, func(got *requestNewTarget) {
+			test.Equal(want.Method, got.Method)
+			test.Equal(want.Path, got.Path)
+			test.Equal(len(want.Header), len(got.Header))
+			test.Equal(want.Header, got.Header)
+		})
+	}
+
+	type checkData struct {
+		name string
+		code string
+		want *requestNewTarget
+	}
+
+	checkDatas := []checkData{
+		{
+			name: "featch post",
+			code: `
+console.log('1.js'); var deepUrl = 'deep.js';;
 console.log('2.js'); fetch(deepUrl, {
 	method: 'POST',
 	headers: { 'HackedJS': "AAA"},
 });
+			`,
+			want: &requestNewTarget{
+				Method: "POST",
+				Path:   "deep.js",
+				Header: []*ypb.KVPair{
+					{Key: "HackedJS", Value: "AAA"},
+				},
+			},
+		},
 
-
+		{
+			name: "fetch get",
+			code: `
 fetch('/misc/response/fetch/basic.action')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    return response.text();
-  })
-  .then(data => {
-    console.log(data); // 这里是你的页面内容
-  })
-  .catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
-
-
-;
-// 创建一个新的 XMLHttpRequest 对象
+.then(response => {
+  if (!response.ok) {
+	throw new Error('Network response was not ok ' + response.statusText);
+  }
+  return response.text();
+})
+.then(data => {
+  console.log(data); // 这里是你的页面内容
+})
+.catch(error => {
+  console.error('There has been a problem with your fetch operation:', error);
+});
+			`,
+			want: &requestNewTarget{
+				Method: "GET",
+				Path:   "/misc/response/fetch/basic.action",
+			},
+		},
+		{
+			name: "xhr post",
+			code: `
+	// 创建一个新的 XMLHttpRequest 对象
 var xhr = new XMLHttpRequest;
 
 // 配置请求类型为 POST，以及目标 URL
@@ -58,13 +96,20 @@ xhr.onreadystatechange = function() {
 
 // 发送请求，可以在此处发送任何需要的数据
 xhr.send();;
-`
-	HandleJS(false, []byte(`GET / HTTP/1.1
-Host: www.example.com
+			`,
+			want: &requestNewTarget{
+				Method: "POST",
+				Path:   "deep.js",
+				Header: []*ypb.KVPair{
+					{Key: "HackedJS", Value: "AAA"},
+				},
+			},
+		},
+	}
 
-`), code, func(b bool, bytes []byte) {
-		log.Infof("https: %v, bytes: %v", b, string(bytes))
-		count++
-	})
-	assert.Equal(t, 3, count)
+	for _, data := range checkDatas {
+		t.Run(data.name, func(t *testing.T) {
+			check(t, data.code, data.want)
+		})
+	}
 }
