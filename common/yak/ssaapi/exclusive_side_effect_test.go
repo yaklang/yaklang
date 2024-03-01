4ac66f5f827cbd9c372ed24a069020830da330b3
v1ll4n <v1ll4n@qq.com>
2024-03-01 10:48:07 +0800

    add testcase for sideeffect

---
+++ common/yak/ssaapi/exclusive_side_effect_test.go
@@ -0,0 +1,164 @@
+package ssaapi
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ParseMustForTestCase(code string) *Program {
+	prog, err := Parse(code)
+	if err != nil {
+		panic(err)
+	}
+	return prog
+}
+
+func TestSideEffect_Case1(t *testing.T) {
+	prog := ParseMustForTestCase(`
+a = 1
+b = () => {
+	a = 2
+}
+b()
+c = a;
+`)
+	cRef := prog.Ref("c")
+	cRefToFunc := false
+	cRefTo2 := false
+	results := cRef.GetTopDefs().Show().ForEach(func(value *Value) {
+		if value.IsFunction() {
+			cRefToFunc = true
+		}
+		if fmt.Sprint(value.GetConstValue()) == `2` {
+			cRefTo2 = true
+		}
+	})
+	if len(results) != 2 {
+		t.Fatalf("Expect 2 results, but got %d", len(results))
+	}
+	if !cRefToFunc {
+		t.Fatal("Expect c ref defsto to function, but not")
+	}
+
+	if !cRefTo2 {
+		t.Fatal("Expect c ref defsto to 2, but not")
+	}
+}
+
+func TestSideEffect_Obj(t *testing.T) {
+	prog := ParseMustForTestCase(`
+a = {}
+b = () => {
+	a.b = 2
+}
+b()
+c = a.b;
+`)
+	cRef := prog.Ref("c")
+	cRefToFunc := false
+	cRefTo2 := false
+	results := cRef.GetTopDefs().Show().ForEach(func(value *Value) {
+		if value.IsFunction() {
+			cRefToFunc = true
+		}
+		if fmt.Sprint(value.GetConstValue()) == `2` {
+			cRefTo2 = true
+		}
+	})
+	if len(results) != 2 {
+		t.Fatalf("Expect 2 results, but got %d", len(results))
+	}
+	if !cRefToFunc {
+		t.Fatal("Expect c ref defsto to function, but not")
+	}
+
+	if !cRefTo2 {
+		t.Fatal("Expect c ref defsto to 2, but not")
+	}
+}
+
+func TestSideEffect_Case2(t *testing.T) {
+	prog := ParseMustForTestCase(`
+a = 1
+b = () => {
+	a = 2
+}
+if e {b()}
+c = a;
+`)
+	cRef := prog.Ref("c")
+	cRefToFunc := false
+	cRefTo2 := false
+	cRefTo1 := false
+	results := cRef.GetTopDefs().Show().ForEach(func(value *Value) {
+		if value.IsFunction() {
+			cRefToFunc = true
+		}
+		if fmt.Sprint(value.GetConstValue()) == `2` {
+			cRefTo2 = true
+		}
+		if fmt.Sprint(value.GetConstValue()) == `1` {
+			cRefTo1 = true
+		}
+	})
+	if len(results) < 3 {
+		t.Fatalf("Expect >=3 results, but got %d", len(results))
+	}
+	if !cRefToFunc {
+		t.Fatal("Expect c ref defsto to function, but not")
+	}
+
+	if !cRefTo2 {
+		t.Fatal("Expect c ref defsto to 2, but not")
+	}
+	if !cRefTo1 {
+		t.Fatal("Expect c ref defsto to 1, but not")
+	}
+
+	if !cRef.Get(0).IsPhi() {
+		t.Fatal("c is phi must")
+	}
+}
+
+func TestSideEffect_Obj2(t *testing.T) {
+	prog := ParseMustForTestCase(`
+a = 1
+b = () => {
+	a = 2
+}
+if e {b()}
+c = a;
+`)
+	cRef := prog.Ref("c")
+	cRefToFunc := false
+	cRefTo2 := false
+	cRefTo1 := false
+	results := cRef.GetTopDefs().Show().ForEach(func(value *Value) {
+		if value.IsFunction() {
+			cRefToFunc = true
+		}
+		if fmt.Sprint(value.GetConstValue()) == `2` {
+			cRefTo2 = true
+		}
+		if fmt.Sprint(value.GetConstValue()) == `1` {
+			cRefTo1 = true
+		}
+	})
+	if len(results) < 3 {
+		t.Fatalf("Expect >=3 results, but got %d", len(results))
+	}
+	if !cRefToFunc {
+		t.Fatal("Expect c ref defsto to function, but not")
+	}
+
+	if !cRefTo2 {
+		t.Fatal("Expect c ref defsto to 2, but not")
+	}
+	if !cRefTo1 {
+		t.Fatal("Expect c ref defsto to 1, but not")
+	}
+
+	if !cRef.Get(0).IsPhi() {
+		t.Fatal("c is phi must")
+	}
+}
