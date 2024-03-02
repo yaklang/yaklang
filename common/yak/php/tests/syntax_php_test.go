29b95f9c2c9c1b7699921f255bbc8b15fb0dc11a
VillanCh <188228725@qq.com>
2024-03-02 12:13:57 +0800

    fix php grammar

---
+++ common/yak/php/tests/syntax_php_test.go
@@ -0,0 +1,44 @@
+package tests
+
+import (
+	"embed"
+	"fmt"
+	"github.com/antlr/antlr4/runtime/Go/antlr/v4"
+	phpparser "github.com/yaklang/yaklang/common/yak/php/parser"
+	"path/filepath"
+	"testing"
+)
+
+//go:embed syntax/***
+var syntaxFs embed.FS
+
+func validateSource(t *testing.T, filename string, src string) {
+	t.Run(fmt.Sprintf("syntax file: %v", filename), func(t *testing.T) {
+		lex := phpparser.NewPHPLexer(antlr.NewInputStream(src))
+		tokenStream := antlr.NewCommonTokenStream(lex, antlr.TokenDefaultChannel)
+		parser := phpparser.NewPHPParser(tokenStream)
+		parser.SetErrorHandler(antlr.NewBailErrorStrategy())
+		if parser.HtmlDocument() == nil {
+			t.Errorf("file: %v 's syntax parsing failed, no html document entry", filename)
+		}
+	})
+
+}
+
+func TestAllSyntaxForPHP_G4(t *testing.T) {
+	entry, err := syntaxFs.ReadDir("syntax")
+	if err != nil {
+		t.Fatalf("no embed syntax files found: %v", err)
+	}
+	for _, f := range entry {
+		if f.IsDir() {
+			continue
+		}
+		path := filepath.Join("syntax", f.Name())
+		raw, err := syntaxFs.ReadFile(path)
+		if err != nil {
+			t.Fatalf("cannot found syntax fs: %v", path)
+		}
+		validateSource(t, path, string(raw))
+	}
+}
