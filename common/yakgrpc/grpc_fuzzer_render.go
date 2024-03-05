1877dbc161e0efb9209fb1081f2293e8d8ceca3b
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-03-05 10:55:15 +0800

    fix(httpflow):
    will fix body first if different boundary

--- common/yakgrpc/grpc_fuzzer_render.go
+++ common/yakgrpc/grpc_fuzzer_render.go
@@ -2,12 +2,14 @@ package yakgrpc
 
 import (
 	"context"
+
 	"github.com/yaklang/yaklang/common/mutate"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/lowhttp"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
+// ! 已弃用
 func (s *Server) RenderHTTPFuzzerPacket(ctx context.Context, req *ypb.RenderHTTPFuzzerPacketRequest) (*ypb.RenderHTTPFuzzerPacketResponse, error) {
 	packet := req.GetPacket()
 	res, err := mutate.FuzzTagExec(packet, mutate.Fuzz_WithEnableFiletag())
