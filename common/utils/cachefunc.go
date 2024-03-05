bee945897cf2c9a41441ca5d07af373b825762eb
rookieBC <rookie_is@qq.com>
2024-03-05 22:04:01 +0800

    fix(cachefunc):修复处理不当的缓存方法

--- common/utils/cachefunc.go
+++ common/utils/cachefunc.go
@@ -8,15 +8,22 @@ func CacheFunc[T any](t time.Duration, f func() (T, error)) func() (T, error) {
 	timer := time.NewTimer(t)
 	var cache T
 	var err error
+	for i := 0; i < 3; i++ {
+		cache, err = f()
+		if err == nil {
+			break
+		}
+	}
 	return func() (T, error) {
 		select {
 		case <-timer.C:
 			for i := 0; i < 3; i++ {
 				cache, err = f()
 				if err == nil {
-					return cache, nil
+					break
 				}
 			}
+			timer.Reset(t)
 			return cache, err
 		default:
 			return cache, nil
