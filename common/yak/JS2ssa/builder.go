09ab743137e924a12d14010674ac6774563d9926
wlz <lingze.wl@gmail.com>
2024-03-07 15:17:01 +0800

    refactor(ssaapi):new build Function register
    just use one function register language in ssaapi

--- common/yak/JS2ssa/builder.go
+++ common/yak/JS2ssa/builder.go
@@ -1,30 +1,13 @@
 package js2ssa
 
 import (
-	"runtime/debug"
-
 	"github.com/antlr/antlr4/runtime/Go/antlr/v4"
 
 	"github.com/yaklang/yaklang/common/utils"
 	JS "github.com/yaklang/yaklang/common/yak/antlr4JS/parser"
+	"github.com/yaklang/yaklang/common/yak/antlr4util"
 	"github.com/yaklang/yaklang/common/yak/ssa"
-	"github.com/yaklang/yaklang/common/yak/ssa4analyze"
 )
-
-type Parser struct {
-}
-
-func NewParser() *Parser {
-	return &Parser{}
-}
-
-func (p *Parser) Parse(src string, must bool, callBack func(*ssa.FunctionBuilder)) (*ssa.Program, error) {
-	return parseSSA(src, must, nil, callBack)
-}
-
-func (p *Parser) Feed(src string, must bool, prog *ssa.Program) {
-	parseSSA(src, must, prog, nil)
-}
 
 type astbuilder struct {
 	*ssa.FunctionBuilder
@@ -40,75 +23,31 @@ func NewAstBuilder(functionBuilder *ssa.FunctionBuilder) *astbuilder {
 	}
 }
 
-func parseSSA(src string, force bool, prog *ssa.Program, callback func(*ssa.FunctionBuilder)) (ret *ssa.Program, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			debug.PrintStack()
-			ret = nil
-			err = utils.Errorf("parse error with panic : %v", r)
-		}
-	}()
-
-	if prog == nil {
-		prog = ssa.NewProgram()
+func Build(src string, force bool, builder *ssa.FunctionBuilder) error {
+	ast, err := frontend(src, force)
+	if err != nil {
+		return err
 	}
-	if err := frontend(src, force, func(ast *JS.ProgramContext) {
-		funcBuilder := prog.GetAndCreateMainFunctionBuilder()
-		if funcBuilder == nil {
-			return
-		}
-		if callback != nil {
-			callback(funcBuilder)
-		}
-		astbuilder := NewAstBuilder(funcBuilder)
-		astbuilder.build(ast)
-		astbuilder.Finish()
-	}); err != nil {
-		//TODO: merge code with yak2ssa/builder.go
-		return prog, nil
-	}
-
-	ssa4analyze.RunAnalyzer(prog)
-	return prog, nil
+	astBuilder := NewAstBuilder(builder)
+	astBuilder.build(ast)
+	return nil
 }
 
-// error listener for lexer and parser
-type ErrorListener struct {
-	err []string
-	*antlr.DefaultErrorListener
-}
-
-func (el *ErrorListener) SyntaxError(recognizer antlr.Recognizer, offendingSymbol interface{}, line, column int, msg string, e antlr.RecognitionException) {
-	el.err = append(el.err, msg)
-}
-
-func NewErrorListener() *ErrorListener {
-	return &ErrorListener{
-		err:                  []string{},
-		DefaultErrorListener: antlr.NewDefaultErrorListener(),
-	}
-}
-
-func frontend(src string, must bool, handler func(*JS.ProgramContext)) error {
-	errListener := NewErrorListener()
-	// start := time.Now()
+func frontend(src string, must bool) (*JS.ProgramContext, error) {
+	errListener := antlr4util.NewErrorListener()
 	lexer := JS.NewJavaScriptLexer(antlr.NewInputStream(src))
 	lexer.RemoveErrorListeners()
 	lexer.AddErrorListener(errListener)
 	tokenStream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
 	parser := JS.NewJavaScriptParser(tokenStream)
-	// log.Info(time.Since(start))
 	parser.RemoveErrorListeners()
 	parser.AddErrorListener(errListener)
 	parser.SetErrorHandler(antlr.NewDefaultErrorStrategy())
 	ast := parser.Program().(*JS.ProgramContext)
-	// log.Info("ast time ", time.Since(start))
-	if must || len(errListener.err) == 0 {
-		handler(ast)
-		return nil
-	} else {
-		return utils.Errorf("parse AST FrontEnd error : %v", errListener.err)
+	if must || len(errListener.GetErrors()) == 0 {
+		return ast, nil
 	}
+	return nil, utils.Errorf("parse AST FrontEnd error : %v", errListener.GetErrors())
 }
 
 func (b *astbuilder) AddToCmap(key string) {
---
+++ common/yak/ssaapi/language_parser.go
@@ -0,0 +1,62 @@
+package ssaapi
+
+import (
+	"github.com/yaklang/yaklang/common/utils"
+	js2ssa "github.com/yaklang/yaklang/common/yak/JS2ssa"
+	"github.com/yaklang/yaklang/common/yak/ssa"
+	"github.com/yaklang/yaklang/common/yak/ssa4analyze"
+	"github.com/yaklang/yaklang/common/yak/yak2ssa"
+)
+
+type Language string
+
+const (
+	Yak Language = "yak"
+	JS  Language = "js"
+)
+
+type Build func(string, bool, *ssa.FunctionBuilder) error
+
+var (
+	LanguageBuilders = map[Language]Build{
+		Yak: yak2ssa.Build,
+		JS:  js2ssa.Build,
+	}
+)
+
+func parse(c *config, prog *ssa.Program) (ret *ssa.Program, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			// debug.PrintStack()
+			ret = nil
+			err = utils.Errorf("parse error with panic : %v", r)
+		}
+	}()
+
+	if prog == nil {
+		prog = ssa.NewProgram()
+	}
+
+	builder := prog.GetAndCreateMainFunctionBuilder()
+	builder.WithExternLib(c.externLib)
+	builder.WithExternValue(c.externValue)
+	builder.WithExternMethod(c.externMethod)
+	builder.WithDefineFunction(c.defineFunc)
+
+	if err := c.Build(c.code, c.ignoreSyntaxErr, builder); err != nil {
+		return nil, err
+	}
+
+	builder.Finish()
+	ssa4analyze.RunAnalyzer(prog)
+	return prog, nil
+}
+
+func feed(c *config, prog *ssa.Program, code string) {
+	builder := prog.GetAndCreateMainFunctionBuilder()
+	if err := c.Build(code, c.ignoreSyntaxErr, builder); err != nil {
+		return
+	}
+	builder.Finish()
+	ssa4analyze.RunAnalyzer(prog)
+}
