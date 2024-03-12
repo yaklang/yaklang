b6e957165129f0b16bede56413324ad2add7d52a
go0p <gp827782797@qq.com>
2024-03-12 14:59:01 +0800

    feat(mitm): add test case

--- common/yakgrpc/grpc_mitm_websocket_test.go
+++ common/yakgrpc/grpc_mitm_websocket_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/yaklang/yaklang/common/utils/lowhttp"
 	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"strings"
 	"testing"
 	"time"
 )
@@ -63,4 +64,107 @@ Accept: */*
 			t.Errorf("search httpflow by token failed: yakit.QuickSearchMITMHTTPFlowCount(token)")
 		}
 	})
+}
+
+func TestGRPCMUSTPASS_MITM_WebSocket_Payload(t *testing.T) {
+	ctx, cancel := context.WithCancel(utils.TimeoutContextSeconds(20))
+	defer cancel()
+	token := utils.RandNumberStringBytes(20)
+
+	host, port := utils.DebugMockEchoWs([]byte(token))
+
+	client, err := NewLocalClient() // 新建一个 yakit client
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rPort := utils.GetRandomAvailableTCPPort()
+	var proxy = "http://" + utils.HostPort("127.0.0.1", rPort)
+
+	// 启动MITM服务器
+	stream, err := client.MITM(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+	stream.Send(&ypb.MITMRequest{
+		Host: "127.0.0.1",
+		Port: uint32(rPort),
+	})
+
+	stream.Send(&ypb.MITMRequest{SetAutoForward: true, AutoForwardValue: false})
+	tokenCount := 0
+	for {
+		rcpResponse, err := stream.Recv()
+		if err != nil {
+			break
+		}
+		rspMsg := string(rcpResponse.GetMessage().GetMessage())
+		if len(rcpResponse.GetRequest()) > 0 {
+			if len(rcpResponse.GetResponse()) > 0 {
+				stream.Send(&ypb.MITMRequest{
+					Response:   rcpResponse.GetResponse(),
+					Id:         rcpResponse.GetId(),
+					ResponseId: rcpResponse.GetResponseId(),
+				})
+			}
+			if len(rcpResponse.GetPayload()) > 0 {
+				if rcpResponse.GetRequest() == nil {
+					t.Fatalf("websocket rcpResponse.GetRequest() is nil")
+				}
+				if !strings.Contains(string(rcpResponse.GetPayload()), token) {
+					t.Fatalf("websocket rcpResponse.GetPayload() not contains token")
+				}
+				if strings.Contains(string(rcpResponse.GetPayload()), token) {
+					tokenCount++
+				}
+				stream.Send(&ypb.MITMRequest{
+					Id:         rcpResponse.GetId(),
+					ResponseId: rcpResponse.GetResponseId(),
+				})
+			}
+
+			stream.Send(&ypb.MITMRequest{
+				Id:             rcpResponse.GetId(),
+				HijackResponse: true,
+			})
+			stream.Send(&ypb.MITMRequest{
+				Id:      rcpResponse.GetId(),
+				Request: rcpResponse.GetRequest(),
+			})
+		}
+		if strings.Contains(rspMsg, `starting mitm serve`) {
+			go func() {
+				time.Sleep(1 * time.Second)
+				wsClient, err := lowhttp.NewWebsocketClient([]byte(fmt.Sprintf(`GET /echo HTTP/1.1
+Host: %s
+Accept-Encoding: gzip, deflate
+Sec-WebSocket-Key: 3o0bLKJzcaNwhJQs4wBw2g==
+Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
+Cache-Control: no-cache
+Pragma: no-cache
+Upgrade: websocket
+Sec-WebSocket-Version: 13
+Connection: keep-alive, Upgrade
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0
+Accept: */*
+`, utils.HostPort(host, port))), lowhttp.WithWebsocketProxy(proxy))
+				if err != nil {
+					t.Fatal(err)
+				}
+				time.Sleep(1 * time.Second)
+				//for i := 0; i < 3; i++ {
+				err = wsClient.Write([]byte(token))
+				//}
+				if err != nil {
+					t.Fatal(err)
+				}
+				time.Sleep(2 * time.Second)
+				cancel()
+			}()
+		}
+	}
+
+	if tokenCount != 2 {
+		t.Fatalf("tokenCount != 2")
+	}
 }
