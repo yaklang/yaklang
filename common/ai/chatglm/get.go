f4b6563fddc2220796dd267262fd7855ab76850c
v1ll4n <v1ll4n@qq.com>
2024-03-14 15:59:56 +0800

    add glm api code

---
+++ common/ai/chatglm/get.go
@@ -0,0 +1,56 @@
+package chatglm
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/utils/lowhttp"
+	"github.com/yaklang/yaklang/common/utils/lowhttp/poc"
+	"time"
+)
+
+func post(apiURL, token string, params map[string]interface{}, timeout time.Duration) (map[string]interface{}, error) {
+
+	rsp, req, err := poc.DoPOST(
+		apiURL,
+		poc.WithReplaceAllHttpPacketHeaders(map[string]string{
+			"Accept":        "application/json",
+			"Content-Type":  "application/json; charset=UTF-8",
+			"Authorization": token,
+		}),
+		poc.WithReplaceHttpPacketBody(utils.Jsonify(params), false),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("request post to %v：%v", apiURL, err)
+	}
+	_ = req
+
+	var result = make(map[string]any)
+	err = json.Unmarshal(rsp.GetBody(), &result)
+	if err != nil {
+		return nil, fmt.Errorf("JSON response failed：%w", err)
+	}
+	return result, nil
+}
+
+func get(apiURL, token string, timeout time.Duration) (map[string]interface{}, error) {
+	rsp, req, err := poc.DoGET(apiURL,
+		poc.WithReplaceAllHttpPacketHeaders(map[string]string{
+			"Accept":        "application/json",
+			"Content-Type":  "application/json; charset=UTF-8",
+			"Authorization": token,
+		}),
+		poc.WithTimeout(timeout.Seconds()),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("request %v failed: %s", apiURL, err)
+	}
+	_ = req
+	_, body := lowhttp.SplitHTTPPacketFast(rsp.RawPacket)
+	var result map[string]interface{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return nil, fmt.Errorf("JSON 解析异常：%w", err)
+	}
+	return result, nil
+}
