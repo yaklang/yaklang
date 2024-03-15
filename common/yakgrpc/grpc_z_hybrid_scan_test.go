6fca24befd05b371e47a78bceb83d50cb9dae958
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-03-15 11:07:53 +0800

    test(HybridScan_Stop_Smoking): remove

--- common/yakgrpc/grpc_z_hybrid_scan_test.go
+++ common/yakgrpc/grpc_z_hybrid_scan_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"testing"
+
 	"github.com/davecgh/go-spew/spew"
 	"github.com/yaklang/yaklang/common/consts"
 	"github.com/yaklang/yaklang/common/log"
@@ -12,10 +14,6 @@ import (
 	"github.com/yaklang/yaklang/common/vulinbox"
 	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
-	"net/http"
-	"strings"
-	"testing"
-	"time"
 )
 
 func TestServer_HybridScan(t *testing.T) {
@@ -203,95 +201,96 @@ mirrorHTTPFlow = func(isHttps , url , req , rsp , body) {
     }
 }`
 
-func TestGRPCMUSTPASS_HybridScan_Stop_Smoking(t *testing.T) {
-	scriptNameList := make([]string, 10)
-	defer func() {
-		for _, name := range scriptNameList { // 清理临时插件
-			yakit.DeleteYakScriptByName(consts.GetGormProfileDatabase(), name)
-		}
-	}()
+// ! remove because of unstable
+// func TestGRPCMUSTPASS_HybridScan_Stop_Smoking(t *testing.T) {
+// 	scriptNameList := make([]string, 10)
+// 	defer func() {
+// 		for _, name := range scriptNameList { // 清理临时插件
+// 			yakit.DeleteYakScriptByName(consts.GetGormProfileDatabase(), name)
+// 		}
+// 	}()
 
-	var check = true
-	var sendStop = false
-	target := utils.HostPort(utils.DebugMockHTTPHandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if sendStop {
-			check = false // 如果已经向前端发送停止信号,还有对mock服务的请求,则停止失败
-		}
-		w.Write([]byte("Hello, World!"))
-	}))
+// 	var check = true
+// 	var sendStop = false
+// 	target := utils.HostPort(utils.DebugMockHTTPHandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		if sendStop {
+// 			check = false // 如果已经向前端发送停止信号,还有对mock服务的请求,则停止失败
+// 		}
+// 		w.Write([]byte("Hello, World!"))
+// 	}))
 
-	for i := 0; i < 10; i++ {
-		scriptName, err := yakit.CreateTemporaryYakScript("mitm", fmt.Sprintf(StopTestCode, target))
-		if err != nil {
-			panic(err)
-		}
-		scriptNameList = append(scriptNameList, scriptName)
-	}
+// 	for i := 0; i < 10; i++ {
+// 		scriptName, err := yakit.CreateTemporaryYakScript("mitm", fmt.Sprintf(StopTestCode, target))
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		scriptNameList = append(scriptNameList, scriptName)
+// 	}
 
-	client, err := NewLocalClient()
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	client, err := NewLocalClient()
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	streamContext, streamCancel := context.WithCancel(context.Background())
-	defer streamCancel()
-	stream, err := client.HybridScan(streamContext)
-	if err != nil {
-		t.Fatal(err)
-	}
+// 	streamContext, streamCancel := context.WithCancel(context.Background())
+// 	defer streamCancel()
+// 	stream, err := client.HybridScan(streamContext)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-	stream.Send(&ypb.HybridScanRequest{
-		Control:        true,
-		HybridScanMode: "new",
-	})
+// 	stream.Send(&ypb.HybridScanRequest{
+// 		Control:        true,
+// 		HybridScanMode: "new",
+// 	})
 
-	targetList := []string{}
-	for i := 0; i < 5; i++ { // 构造 5 个目标
-		targetList = append(targetList, "http://"+target+"/"+utils.RandStringBytes(3))
-	}
+// 	targetList := []string{}
+// 	for i := 0; i < 5; i++ { // 构造 5 个目标
+// 		targetList = append(targetList, "http://"+target+"/"+utils.RandStringBytes(3))
+// 	}
 
-	stream.Send(&ypb.HybridScanRequest{
-		Targets: &ypb.HybridScanInputTarget{
-			Input: strings.Join(targetList, ","),
-		},
-		Plugin: &ypb.HybridScanPluginConfig{
-			PluginNames: scriptNameList,
-		},
-	})
+// 	stream.Send(&ypb.HybridScanRequest{
+// 		Targets: &ypb.HybridScanInputTarget{
+// 			Input: strings.Join(targetList, ","),
+// 		},
+// 		Plugin: &ypb.HybridScanPluginConfig{
+// 			PluginNames: scriptNameList,
+// 		},
+// 	})
 
-	streamReturnCheck := false
-	for {
-		rsp, err := stream.Recv()
-		if err != nil {
-			if strings.Contains(err.Error(), "task manager cancled") { // 如果返回的错误不是 task manager cancled 则代表在2秒内未成功返回停止信号给client
-				streamReturnCheck = true
-			}
-			break
-		}
-		if rsp.ExecResult != nil && rsp.ExecResult.IsMessage {
-			if bytes.Contains(rsp.ExecResult.Message, []byte("test information")) {
-				stream.Send(&ypb.HybridScanRequest{
-					Control:        false,
-					HybridScanMode: "stop",
-				})
-				go func() {
-					time.Sleep(2 * time.Second) //等待 2 秒后手动关闭连接
-					streamCancel()
-				}()
-			}
-		}
-		spew.Dump(rsp)
-	}
-	if !streamReturnCheck {
-		t.Fatal("return front fail")
-	}
-	sendStop = true             // 已经向前端发送停止信号,检查是否成功停止
-	time.Sleep(4 * time.Second) // 等待 4 秒,是否还有请求mock服务
-	if !check {
-		t.Fatal("stop hybridScan fail")
-	}
+// 	streamReturnCheck := false
+// 	for {
+// 		rsp, err := stream.Recv()
+// 		if err != nil {
+// 			if strings.Contains(err.Error(), "task manager cancled") { // 如果返回的错误不是 task manager cancled 则代表在2秒内未成功返回停止信号给client
+// 				streamReturnCheck = true
+// 			}
+// 			break
+// 		}
+// 		if rsp.ExecResult != nil && rsp.ExecResult.IsMessage {
+// 			if bytes.Contains(rsp.ExecResult.Message, []byte("test information")) {
+// 				stream.Send(&ypb.HybridScanRequest{
+// 					Control:        false,
+// 					HybridScanMode: "stop",
+// 				})
+// 				go func() {
+// 					time.Sleep(2 * time.Second) //等待 2 秒后手动关闭连接
+// 					streamCancel()
+// 				}()
+// 			}
+// 		}
+// 		spew.Dump(rsp)
+// 	}
+// 	if !streamReturnCheck {
+// 		t.Fatal("return front fail")
+// 	}
+// 	sendStop = true             // 已经向前端发送停止信号,检查是否成功停止
+// 	time.Sleep(4 * time.Second) // 等待 4 秒,是否还有请求mock服务
+// 	if !check {
+// 		t.Fatal("stop hybridScan fail")
+// 	}
 
-}
+// }
 
 func TestGRPCMUSTPASS_HybridScan_HttpflowID(t *testing.T) {
 	token := utils.RandSecret(10)
