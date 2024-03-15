8ac6c7ec911c05b3b123222058ade0b46796480b
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-03-15 12:01:08 +0800

    test(DuplexConnection):

--- common/yakgrpc/yakit/duplex_conn.go
+++ common/yakgrpc/yakit/duplex_conn.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/samber/lo"
+	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
@@ -16,8 +17,8 @@ type serverPushDescription struct {
 
 var (
 	serverPushMutex          = new(sync.Mutex)
+	broadcastWithTypeMutex   = new(sync.Mutex)
 	serverPushCallback       []serverPushDescription
-	broadcastWithTypeMu      = new(sync.Mutex)
 	broadcastTypeCallerTable = make(map[string]func(func()))
 )
 
@@ -25,6 +26,7 @@ func RegisterServerPushCallback(id string, stream ypb.Yak_DuplexConnectionServer
 	serverPushMutex.Lock()
 	defer serverPushMutex.Unlock()
 
+	log.Infof("Register server push callback: %v", id)
 	serverPushCallback = append(serverPushCallback, serverPushDescription{
 		Name: id,
 		Handle: func(response *ypb.DuplexConnectionResponse) {
@@ -37,22 +39,24 @@ func UnRegisterServerPushCallback(id string) {
 	serverPushMutex.Lock()
 	defer serverPushMutex.Unlock()
 
+	log.Infof("UnRegister server push callback: %v", id)
 	serverPushCallback = lo.Filter(serverPushCallback, func(item serverPushDescription, index int) bool {
 		return item.Name != id
 	})
 }
 
 func broadcastRaw(i any) {
-	serverPushMutex.Lock()
-	defer serverPushMutex.Unlock()
+	broadcastWithTypeMutex.Lock()
+	defer broadcastWithTypeMutex.Unlock()
+
 	for _, item := range serverPushCallback {
 		item.Handle(&ypb.DuplexConnectionResponse{Data: utils.Jsonify(i)})
 	}
 }
 
 func BroadcastData(typeString string, data any) {
-	broadcastWithTypeMu.Lock()
-	defer broadcastWithTypeMu.Unlock()
+	broadcastWithTypeMutex.Lock()
+	defer broadcastWithTypeMutex.Unlock()
 
 	buildData := func() map[string]any {
 		return map[string]any{
