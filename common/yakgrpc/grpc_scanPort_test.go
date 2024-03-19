d8449a68296591d51454b8086efc3042e552d9a7
wlz <lingze.wl@gmail.com>
2024-03-19 14:29:18 +0800

    fix(grpc): scanPort script fixup and mock test

--- common/yakgrpc/grpc_scanPort_test.go
+++ common/yakgrpc/grpc_scanPort_test.go
@@ -1,33 +1,47 @@
 package yakgrpc
 
-//func TestServer_PortScan(t *testing.T) {
-//	client, err := NewLocalClient()
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	r, err := client.PortScan(context.Background(), &ypb.PortScanRequest{
-//		Targets:     "47.52.100.1/24",
-//		Ports:       "22",
-//		Mode:        "fp",
-//		Proto:       []string{"tcp"},
-//		Concurrent:  50,
-//		Active:      false,
-//		ScriptNames: []string{"OpenSSH CVE 合规检查：2010-2021"},
-//	})
-//	_ = r
-//	if err != nil {
-//		panic(err)
-//	}
-//	for {
-//		result, err := r.Recv()
-//		if err != nil {
-//			break
-//		}
-//		spew.Dump(result)
-//	}
-//}
-//
+import (
+	"context"
+	"errors"
+	"io"
+	"strconv"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+)
+
+func TestServer_PortScan(t *testing.T) {
+	client, err := NewLocalClient()
+	require.Nil(t, err)
+
+	host, port := utils.DebugMockHTTP([]byte{})
+
+	r, err := client.PortScan(context.Background(), &ypb.PortScanRequest{
+		Targets:     host,
+		Ports:       strconv.Itoa(port),
+		Mode:        "fp",
+		Proto:       []string{"tcp"},
+		Concurrent:  50,
+		Active:      false,
+		ScriptNames: []string{},
+	})
+	_ = r
+	require.Nil(t, err)
+	for {
+		result, err := r.Recv()
+		if err != nil {
+			if !errors.Is(err, io.EOF) {
+				require.Nilf(t, err, "stream error: %v", err)
+			}
+			break
+		}
+		spew.Dump(result)
+	}
+}
+
 //func TestServer_PortScanUDP(t *testing.T) {
 //	client, err := NewLocalClient()
 //	if err != nil {
