4c3925a32ba1df4b14dcc3fd95d6644edab4535b
VillanCh <188228725@qq.com>
2024-03-21 08:00:26 +0800

    create new package for op ws

--- common/yak/wsc/cmd/main.go
+++ common/yak/wsc/cmd/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"github.com/urfave/cli"
-	"github.com/yaklang/yaklang/common/crep"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/yak/wsc"
 	"os"
 	"os/signal"
 	"strings"
 	"sync"
 	"syscall"
+	"time"
 )
 
 var (
@@ -36,15 +39,30 @@ func main() {
 
 	app.Commands = []cli.Command{}
 
-	app.Flags = []cli.Flag{}
+	app.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:  "port,start-port",
+			Usage: "websocket port, start from 11212 - 11222, default 11212, crease 1 after failed to listen util 11220",
+			Value: 11212,
+		},
+	}
 
 	app.Before = func(context *cli.Context) error {
 		return nil
 	}
 
 	app.Action = func(c *cli.Context) error {
-		// not implemented
-		return crep.NewWebsocketController("a", 8881).Run()
+		startPort := c.Int("port")
+		for i := startPort; i < startPort+10; i++ {
+			err := wsc.NewWebsocketController("a", i).Run()
+			if err != nil {
+				log.Errorf("start websocket controller failed: %v", err)
+				time.Sleep(time.Second)
+				continue
+			}
+			return nil
+		}
+		return utils.Error("start websocket controller failed")
 	}
 
 	err := app.Run(os.Args)
