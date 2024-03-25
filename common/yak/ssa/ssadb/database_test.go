c07e9f8f0616f1a7ac4d0812c1006ccfa8b48164
wlz <lingze.wl@gmail.com>
2024-03-25 17:17:54 +0800

    feat(ssa):save instruction variable in database

--- common/yak/ssa/ssadb/database_test.go
+++ common/yak/ssa/ssadb/database_test.go
@@ -1,12 +1,16 @@
 package ssadb_test
 
 import (
+	"sort"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/yak/ssa"
 	"github.com/yaklang/yaklang/common/yak/ssa/ssadb"
+	"github.com/yaklang/yaklang/common/yak/ssaapi"
 )
 
 func TestSqliteID(t *testing.T) {
@@ -17,4 +21,35 @@ func TestSqliteID(t *testing.T) {
 	defer ssadb.DeleteProgram(db, projectName)
 
 	require.Equal(t, id+1, id2)
+}
+
+func TestBuild(t *testing.T) {
+	db := consts.GetGormProjectDatabase().Debug()
+	programName := uuid.NewString()
+	code := `
+		a = 1
+		b = a
+		c = b
+		d = c + a
+		`
+
+	prog, err := ssaapi.Parse(
+		code,
+		ssaapi.WithLanguage(ssaapi.Yak),
+	)
+	defer ssadb.DeleteProgram(db, programName)
+
+	require.NoError(t, err)
+	prog.Program.ShowWithSource()
+
+	ircode := ssadb.GetIrByVariable(db, programName, "a")
+
+	require.NotNil(t, ircode)
+
+	spew.Dump(ircode)
+	require.Equal(t, ssa.SSAOpcode2Name[ssa.SSAOpcodeConstInst], ircode.OpcodeName)
+
+	v := ircode.Variable
+	sort.Strings(v)
+	require.Equal(t, ssadb.StringSlice{"a", "b", "c"}, v)
 }
