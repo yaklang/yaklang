6243680d2fceb80cecf8fd909a149679dbbc0a6c
VillanCh <188228725@qq.com>
2024-03-26 09:16:06 +0800

    add filesys

--- common/utils/filesys/fs_test.go
+++ common/utils/filesys/fs_test.go
@@ -7,17 +7,45 @@ import (
 	"testing"
 )
 
-//go:embed testdata
+//go:embed testdata/***
 var testfs embed.FS
 
-func TestFS(t *testing.T) {
+func TestFS_1(t *testing.T) {
+	f := testfs
+	_ = f
+	count := 0
 	err := Recursive(
-		"/Users/v1ll4n/Projects/yaklang",
-		WithDirMatch("ut*", WithFileStat(func(pathname string, info os.FileInfo) error {
+		"testdata",
+		WithEmbedFS(testfs),
+		WithDirMatch("ta", WithFileStat(func(pathname string, info os.FileInfo) error {
 			log.Infof("match: %v", pathname)
+			count++
 			return nil
 		})))
 	if err != nil {
 		t.Fatal(err)
+	}
+	if count != 4 {
+		t.Fatal("count != 4")
+	}
+}
+
+func TestFS_Chains(t *testing.T) {
+	count := 0
+	err := Recursive(
+		"testdata",
+		WithDirMatches([]string{
+			"cc", "dd",
+		}, WithFileStat(func(pathname string, info os.FileInfo) error {
+			count++
+			log.Infof("match: %v", pathname)
+			return nil
+		})), WithEmbedFS(testfs),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != 2 {
+		t.Fatal("count != 4")
 	}
 }
