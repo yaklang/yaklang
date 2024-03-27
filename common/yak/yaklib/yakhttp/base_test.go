92bdd5094d8be259a1ac7e50527869b008d8d60e
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-03-27 16:59:41 +0800

    feat&refactor(lib-http):
    add plugin context options
    refactor HTTPConfig, use PocConfigOption directly

--- common/yak/yaklib/yakhttp/base_test.go
+++ common/yak/yaklib/yakhttp/base_test.go
@@ -7,12 +7,12 @@ import (
 
 func TestHttpRequestWithSession(t *testing.T) {
 	// same session
-	_, err := httpRequest("GET", "https://pie.dev/cookies/set/name1/value1", Session("test"))
+	_, err := httpRequest("GET", "https://pie.dev/cookies/set/name1/value1", WithSession("test"))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	rsp, err := httpRequest("GET", "https://pie.dev/cookies", Session("test"))
+	rsp, err := httpRequest("GET", "https://pie.dev/cookies", WithSession("test"))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -25,12 +25,12 @@ func TestHttpRequestWithSession(t *testing.T) {
 	}
 
 	// not a same session
-	_, err = httpRequest("GET", "https://pie.dev/cookies/set/name1/value1", Session("test1"))
+	_, err = httpRequest("GET", "https://pie.dev/cookies/set/name1/value1", WithSession("test1"))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	rsp, err = httpRequest("GET", "https://pie.dev/cookies", Session("test2"))
+	rsp, err = httpRequest("GET", "https://pie.dev/cookies", WithSession("test2"))
 	if err != nil {
 		t.Fatal(err)
 	}
