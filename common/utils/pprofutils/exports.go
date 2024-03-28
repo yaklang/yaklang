4ba5be885ee0b474a0d0f6d6f21c9cc104146d19
v1ll4n <v1ll4n@qq.com>
2024-03-28 15:07:44 +0800

    add profile interface in yaklang

---
+++ common/utils/pprofutils/exports.go
@@ -0,0 +1,53 @@
+package pprofutils
+
+import (
+	"github.com/yaklang/yaklang/common/log"
+	"sync"
+)
+
+var Exports = map[string]any{
+	"StartCPUAndMemoryProfile": StartCPUAndMemoryProfile,
+	"StartCPUProfile":          StartCPUProfile,
+	"StartMemoryProfile":       StartMemoryProfile,
+
+	"cpuProfilePath":       WithCPUProfileFile,
+	"memProfilePath":       WithMemProfileFile,
+	"ctx":                  WithContext,
+	"onCPUProfileFinished": WithOnCPUProfileFinished,
+	"onCPUProfileStarted":  WithOnCPUProfileStarted,
+	"onMemProfileStarted":  WithOnMemProfileStarted,
+	"onMemProfileFinished": WithOnMemProfileFinished,
+	"timeout":              WithTimeout,
+}
+
+func StartCPUAndMemoryProfile(opts ...Option) error {
+	// 并行开始CPU profiling
+	wg := new(sync.WaitGroup)
+	wg.Add(2)
+
+	errs := make(chan error, 2)
+	go func() {
+		defer func() {
+			wg.Done()
+		}()
+		err := StartCPUProfile(opts...)
+		errs <- err
+		if err != nil {
+			log.Errorf("CPU Profile error: %v", err)
+		}
+	}()
+
+	go func() {
+		defer func() {
+			wg.Done()
+		}()
+		// 在CPU profiling结束后开始Memory profiling
+		err := StartMemoryProfile(opts...)
+		if err != nil {
+			log.Errorf("Memory Profile error: %v", err)
+		}
+	}()
+
+	wg.Wait()
+	return nil
+}
