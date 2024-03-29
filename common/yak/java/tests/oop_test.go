5f848ec38d7c120f4eec2fd5f7aacddebf8f4943
intSheep <1098204193@qq.com>
2024-03-29 18:03:27 +0800

    feat(java2ssa):add side effect

--- common/yak/java/tests/oop_test.go
+++ common/yak/java/tests/oop_test.go
@@ -1,6 +1,9 @@
 package tests
 
-import "testing"
+import (
+	"github.com/yaklang/yaklang/common/yak/ssaapi/ssatest"
+	"testing"
+)
 
 func TestJava_OOP_Var_member(t *testing.T) {
 
@@ -8,13 +11,34 @@ func TestJava_OOP_Var_member(t *testing.T) {
 		CheckAllJavaPrintlnValue(`
 class A {
 		int a = 0;
+
 }
 class Main{
 		public static void main(String[] args) {
 			A a = new A();
-			println(a.a);
+			println(a);
 		}
 }
 		`, []string{"0"}, t)
+	})
+
+	t.Run("side effect", func(t *testing.T) {
+		ssatest.CheckPrintlnValue(`
+	public class A {
+		private int a = 0; 
+	
+		public void Hello(int a) { 
+			println(a);
+		}
+}
+		class Main{
+		public static void main(String[] args) {
+			A a = new A();
+			println(a.Hello());
+		}
+}
+		`, []string{
+			"0", "side-effect(Parameter-$par, $this.a)",
+		}, t)
 	})
 }
