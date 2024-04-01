f14837d12b975fce85573e8f805a5bd6e9c8e093
intSheep <1098204193@qq.com>
2024-04-01 17:40:29 +0800

    test(java2ssa):rewrite frontend test

--- common/yak/java/tests/syntax_test.go
+++ common/yak/java/tests/syntax_test.go
@@ -2,17 +2,25 @@ package tests
 
 import (
 	"embed"
-	"path"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/yak/java/java2ssa"
+	"path/filepath"
 	"strings"
 	"testing"
-
-	test "github.com/yaklang/yaklang/common/yak/ssaapi/ssatest"
 )
 
 //go:embed code
 var codeFs embed.FS
 
-func TestJavaBasicParse_MockFile(t *testing.T) {
+func validateSource(t *testing.T, filename string, src string) {
+	t.Run(fmt.Sprintf("syntax file: %v", filename), func(t *testing.T) {
+		_, err := java2ssa.Frontend(src, false)
+		require.Nil(t, err, "parse AST FrontEnd error : %v", err)
+	})
+}
+
+func TestAllSyntaxForJava_G4(t *testing.T) {
 	entry, err := codeFs.ReadDir("code")
 	if err != nil {
 		t.Fatalf("no embed syntax files found: %v", err)
@@ -21,16 +29,38 @@ func TestJavaBasicParse_MockFile(t *testing.T) {
 		if f.IsDir() {
 			continue
 		}
-		path := path.Join("code", f.Name())
-		if !strings.HasSuffix(path, ".java") {
+		path := filepath.Join("syntax", f.Name())
+		if !strings.HasSuffix(path, ".php") {
 			continue
 		}
 		raw, err := codeFs.ReadFile(path)
 		if err != nil {
 			t.Fatalf("cannot found syntax fs: %v", path)
 		}
-		t.Run(path, func(t *testing.T) {
-			test.MockSSA(t, string(raw))
-		})
+		validateSource(t, path, string(raw))
 	}
 }
+
+//func TestJavaBasicParse_MockFile(t *testing.T) {
+//	entry, err := codeFs.ReadDir("code")
+//	if err != nil {
+//		t.Fatalf("no embed syntax files found: %v", err)
+//	}
+//	for _, f := range entry {
+//		if f.IsDir() {
+//			continue
+//		}
+//		path := path.Join("code", f.Name())
+//		if !strings.HasSuffix(path, ".java") {
+//			continue
+//		}
+//		raw, err := codeFs.ReadFile(path)
+//		if err != nil {
+//			t.Fatalf("cannot found syntax fs: %v", path)
+//		}
+//		t.Run(path, func(t *testing.T) {
+//			//test.MockSSA(t, string(raw))
+//
+//		})
+//	}
+//}
