13018418ce54f0709eeb8eaa6f4c1adfc9fc8b9f
intSheep <1098204193@qq.com>
2024-04-01 17:26:22 +0800

    feat(java2ssa):add setRange

---
+++ common/yak/java/java2ssa/range.go
@@ -0,0 +1,15 @@
+package java2ssa
+
+import (
+	"github.com/yaklang/yaklang/common/yak/antlr4util"
+)
+
+func (y *builder) SetRange(token antlr4util.CanStartStopToken) func() {
+	r := antlr4util.GetRange(token)
+	backup := y.CurrentRange
+	y.CurrentRange = r
+
+	return func() {
+		y.CurrentRange = backup
+	}
+}
