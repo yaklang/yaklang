4db4ec0088d5ff85f22aa67e16a698609b8f32c7
intSheep <1098204193@qq.com>
2024-04-01 17:41:25 +0800

    test(java2ssa):add oop test

--- common/yak/java/tests/oop_test.go
+++ common/yak/java/tests/oop_test.go
@@ -8,7 +8,7 @@ import (
 func TestJava_OOP_Var_member(t *testing.T) {
 
 	t.Run("test simple member call", func(t *testing.T) {
-		CheckAllJavaPrintlnValue(`
+		ssatest.CheckPrintlnValue(`
 class A {
 		int a = 0;
 
@@ -16,7 +16,7 @@ class A {
 class Main{
 		public static void main(String[] args) {
 			A a = new A();
-			println(a);
+			println(a.a);
 		}
 }
 		`, []string{"0"}, t)
@@ -24,21 +24,189 @@ class Main{
 
 	t.Run("side effect", func(t *testing.T) {
 		ssatest.CheckPrintlnValue(`
-	public class A {
+	 class A {
 		private int a = 0; 
 	
-		public void Hello(int a) { 
-			println(a);
+		public void setA(int par) { 
+			this.a = par;
 		}
 }
 		class Main{
 		public static void main(String[] args) {
 			A a = new A();
-			println(a.Hello());
+			println(a.a);
+			a.setA(1);
+			println(a.a);
 		}
 }
 		`, []string{
-			"0", "side-effect(Parameter-$par, $this.a)",
+			"0", "side-effect(Parameter-par, this.a)",
+		}, t)
+	})
+
+	t.Run("free-value", func(t *testing.T) {
+		ssatest.CheckPrintlnValue(`
+		class A {
+			int a = 0; 
+			public void getA() {
+				return this.a;
+			}
+		}
+
+		class Main{
+		public static void main(String[] args) {
+		A a = new A(); 
+		println(a.getA());
+		a.a=1;
+		println(a.getA());
+		}
+}
+		`, []string{
+			"Function-.getA(make(object{}),0)",
+			"Function-.getA(make(object{}),1)",
+		}, t)
+	})
+
+	t.Run("just use method", func(t *testing.T) {
+		ssatest.CheckPrintlnValue(`
+	public 	class A {
+			int a = 0; 
+			public void getA(){
+			return this.a;
+			}
+			
+			public void setA(int par){
+			this.a=par;
+			}
+		}
+	public class Main{
+		public static void main(String[] args) {
+		A a = new A(); 
+		println(a.getA());
+		a.setA(1);
+		println(a.getA());
+		}
+}
+		`, []string{
+			"Function-.getA(make(object{}),0)",
+			"Function-.getA(make(object{}),side-effect(Parameter-par, this.a))",
+		}, t)
+	})
+}
+
+func TestJava_Extend_Class(t *testing.T) {
+
+	t.Run("test extend constant ", func(t *testing.T) {
+		ssatest.CheckPrintlnValue(`
+		class A {
+			int a = 0; 
+		}
+	public class B extends A{}
+	public class C extends B{}
+	public class Main{
+		public static void main(String[] args) {
+		C C = new C();
+		println(C.a); // 0
+}}
+		`, []string{
+			"0",
+		}, t)
+	})
+
+	t.Run("free-value", func(t *testing.T) {
+		ssatest.CheckPrintlnValue(`
+	public 	class Q {
+			int a = 0; 
+			public void getA() {
+				return this.a;
+			}
+		}
+		class A extends Q{}
+		public class Main{
+		public static void main(String[] args) {
+			
+		A a = new A(); 
+		println(a.getA());
+		a.a=1;
+		println(a.getA());
+		}
+}
+		`, []string{
+			"Function-.getA(make(object{}),0)",
+			"Function-.getA(make(object{}),1)",
+		}, t)
+	})
+
+	t.Run("just use method", func(t *testing.T) {
+		ssatest.CheckPrintlnValue(`
+		public class Q {
+			int a = 0; 
+			public void getA(){
+			return this.a;
+			}
+			
+			public void setA(int par){
+			this.a=par;
+			}
+		}
+		class A extends Q{}
+		public class Main{
+		public static void main(String[] args) {
+		A a = new A(); 
+		println(a.getA());
+		a.setA(1);
+		println(a.getA());
+		}
+}
+		`, []string{
+			"Function-.getA(make(object{}),0)",
+			"Function-.getA(make(object{}),side-effect(Parameter-par, this.a))",
+		}, t)
+	})
+}
+
+func TestJava_Construct(t *testing.T) {
+	t.Run("no construct", func(t *testing.T) {
+		code := `
+	public	class A {
+			int num = 0;
+			public int getNum() {
+				super();
+				return this.num;
+			}
+		}
+public class Main{
+		public static void main(String[] args) {
+		A a = new A(); 
+		println(a.getNum());
+		}
+}
+		`
+		ssatest.CheckPrintlnValue(code, []string{
+			"Function-.getNum(make(object{}),0)",
+		}, t)
+	})
+
+	t.Run("normal construct", func(t *testing.T) {
+		code := `
+public class A {
+	private int  num = 0;
+	public A(int num) {
+		this.num = num;
+	}
+	public int getNum() {
+		return this.num;
+	}
+}
+public class Main{
+		public static void main(String[] args) {
+		A a = new A(1);
+		println(a.getNum());
+		}
+}
+`
+		ssatest.CheckPrintlnValue(code, []string{
+			"Function-.getNum(make(object{}),side-effect(Parameter-num, this.num))",
 		}, t)
 	})
 }
