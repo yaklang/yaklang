2af46c7b52d0e181a4653f533cda63c11b9894ab
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-04-07 16:03:33 +0800

    feat(lib-js):
    - refactor js.Run, add options to import third-party libs
    - add some comments

--- common/yak/yaklib/js_test.go
+++ common/yak/yaklib/js_test.go
@@ -2,9 +2,10 @@ package yaklib
 
 import (
 	"fmt"
-	"github.com/yaklang/yaklang/common/javascript/otto/parser"
 	"strconv"
 	"testing"
+
+	"github.com/yaklang/yaklang/common/javascript/otto/parser"
 )
 
 func TestWalk(t *testing.T) {
@@ -29,4 +30,33 @@ if (a == b){
 	if err == nil {
 		fmt.Printf("%v", res)
 	}
+}
+
+func TestRunWithCryptoJS(t *testing.T) {
+	code := `CryptoJS.HmacSHA256("Message", "secret").toString();`
+	_, value, err := _run(code, _libCryptoJSV3())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(value.String())
+}
+
+func TestRunWithJSRSASign(t *testing.T) {
+	code := `pemPublicKey = "-----BEGIN PUBLIC KEY-----\
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtnbrr63e/UbC8j7dXL4I\
+KaCAswHJrIWeY59Dcj5Og+W5Cgt7X+qrpOm7/ojpW+IdPVAYXdFPeZUEVe1p3j/X\
+7lsrIBg/iJ6lFDZb1TMTyF6LOFKQmz9ElMnZ1JQxwaKoP5CouYQ7ZJwtSIadUGKD\
+0zBy/b6yZ5KO4TIGmK7116BCp6GLU5PEYBPupGTULa6LZbqY3P4f9+ptgSjRKszJ\
+2MDmQwnhNu87eAwM3k8BEEaNBw7MviWTJp/hwr63MS6rhAzul6I/p5cDwMZf+UXW\
+14Q8PF3DXNJ1il44ihV6dW54Ynt77BC9ULmkAOrdMkXMp0830vK4bs1T3oGJlJdv\
+owIDAQAB\
+-----END PUBLIC KEY-----";
+publicKey = KEYUTIL.getKey(pemPublicKey);
+publicKey.encrypt("yaklang");
+`
+	_, value, err := _run(code, _libJSRSASign())
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(value.String())
 }
