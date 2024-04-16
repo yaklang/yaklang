a42317acab71e9455ddb054b15dcc0b3d2e75b28
intSheep <1098204193@qq.com>
2024-04-16 16:42:37 +0800

    feat(utils/filesys): add virtual filesys and testcase

---
+++ common/utils/filesys/interface.go
@@ -0,0 +1,27 @@
+package filesys
+
+import (
+	"io/fs"
+	"os"
+)
+
+// FileSystem defines the methods of an abstract filesystem.
+type FileSystem interface {
+	fs.FS
+
+	// ReadDir reads the directory named by dirname and returns a
+	// list of directory entries.
+	ReadDir(dirname string) ([]os.FileInfo, error)
+
+	// Lstat returns a FileInfo describing the named file. If the file is a
+	// symbolic link, the returned FileInfo describes the symbolic link. Lstat
+	// makes no attempt to follow the link.
+	Lstat(name string) (os.FileInfo, error)
+
+	// Join joins any number of path elements into a single path, adding a
+	// separator if necessary. The result is Cleaned; in particular, all
+	// empty strings are ignored.
+	//
+	// The separator is FileSystem specific.
+	Join(elem ...string) string
+}
