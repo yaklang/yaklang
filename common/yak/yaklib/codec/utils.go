a59774f4257b8a4959253a181f5ae9f1d7923c43
v1ll4n <v1ll4n@qq.com>
2024-04-19 17:11:57 +0800

    resolve re-cycle include

--- common/yak/yaklib/codec/utils.go
+++ common/yak/yaklib/codec/utils.go
@@ -6,3 +6,11 @@ import (
 
 var PKCS7Padding = sm4.PKCS7Padding
 var PKCS7UnPadding = sm4.PKCS7UnPadding
+
+func PKCS7PaddingFor8ByteBlock(src []byte) []byte {
+	return PKCS5Padding(src, 8)
+}
+
+func PKCS7UnPaddingFor8ByteBlock(src []byte) []byte {
+	return PKCS5UnPadding(src)
+}
