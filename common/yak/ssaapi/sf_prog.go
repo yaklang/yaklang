e13954d9d05a28ed7d2481a32e55bba0d44bfdc0
v1ll4n <v1ll4n@qq.com>
2024-04-23 17:44:43 +0800

    add sf iface for ssaapi

--- common/yak/ssaapi/sf_prog.go
+++ common/yak/ssaapi/sf_prog.go
@@ -1,1 +1,57 @@
 package ssaapi
+
+import (
+	"github.com/gobwas/glob"
+	"github.com/yaklang/yaklang/common/syntaxflow/sfvm"
+	"github.com/yaklang/yaklang/common/utils"
+	"regexp"
+)
+
+var _ sfvm.ValueOperator = &Program{}
+
+func (p *Program) GetName() string {
+	return p.Program.GetProgramName()
+}
+
+func (p *Program) IsMap() bool { return false }
+
+func (p *Program) IsList() bool {
+	//TODO implement me
+	return false
+}
+
+func (p *Program) ExactMatch(s string) (bool, sfvm.ValueOperator, error) {
+	vals := p.Ref(s)
+	if len(vals) > 0 {
+		return true, nil, nil
+	}
+	return false, nil, nil
+}
+
+func (p *Program) GlobMatch(glob glob.Glob) (bool, sfvm.ValueOperator, error) {
+	return false, nil, utils.Error("ssa.Program is not supported glob match")
+}
+
+func (p *Program) RegexpMatch(regexp *regexp.Regexp) (bool, sfvm.ValueOperator, error) {
+	return false, nil, utils.Error("ssa.Program is not supported regexp match")
+}
+
+func (p *Program) GetMembers() (sfvm.ValueOperator, error) {
+	return nil, utils.Error("ssa.Program is not supported get members")
+}
+
+func (p *Program) ListIndex(i int) (sfvm.ValueOperator, error) {
+	return nil, utils.Error("ssa.Program is not supported list index")
+}
+
+func (p *Program) GetCallActualParams() (sfvm.ValueOperator, error) {
+	return nil, utils.Error("ssa.Program is not supported call actual params")
+}
+
+func (p *Program) GetSyntaxFlowTopDef() (sfvm.ValueOperator, error) {
+	return nil, utils.Error("ssa.Program is not supported syntax flow top def")
+}
+
+func (p *Program) GetSyntaxFlowBottomUse() (sfvm.ValueOperator, error) {
+	return nil, utils.Error("ssa.Program is not supported syntax flow bottom use")
+}
--- common/yak/ssaapi/sf_values.go
+++ common/yak/ssaapi/sf_values.go
@@ -1,1 +1,69 @@
 package ssaapi
+
+import (
+	"github.com/gobwas/glob"
+	"github.com/yaklang/yaklang/common/syntaxflow/sfvm"
+	"github.com/yaklang/yaklang/common/utils"
+	"regexp"
+)
+
+var _ sfvm.ValueOperator = new(Values)
+
+func (value Values) GetName() string {
+	return value.String()
+}
+
+func (value Values) IsMap() bool {
+	return false
+}
+
+func (value Values) IsList() bool {
+	return true
+}
+
+func (value Values) ExactMatch(s string) (bool, sfvm.ValueOperator, error) {
+	vals := value.Ref(s)
+	if len(vals) > 0 {
+		return true, vals, nil
+	}
+	return false, nil, nil
+}
+
+func (value Values) GlobMatch(glob glob.Glob) (bool, sfvm.ValueOperator, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (value Values) RegexpMatch(regexp *regexp.Regexp) (bool, sfvm.ValueOperator, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (value Values) GetCallActualParams() (sfvm.ValueOperator, error) {
+	return nil, utils.Error("ssa.Values is not supported call actual params")
+}
+
+func (value Values) GetMembers() (sfvm.ValueOperator, error) {
+	var vals Values
+	for _, v := range value {
+		if v.IsObject() {
+			vals = append(vals, v.GetAllMember()...)
+		}
+	}
+	return vals, nil
+}
+
+func (value Values) GetSyntaxFlowTopDef() (sfvm.ValueOperator, error) {
+	return value.GetTopDefs(), nil
+}
+
+func (value Values) GetSyntaxFlowBottomUse() (sfvm.ValueOperator, error) {
+	return value.GetBottomUses(), nil
+}
+
+func (value Values) ListIndex(i int) (sfvm.ValueOperator, error) {
+	if i < 0 || i >= len(value) {
+		return nil, utils.Error("index out of range")
+	}
+	return value[i], nil
+}
