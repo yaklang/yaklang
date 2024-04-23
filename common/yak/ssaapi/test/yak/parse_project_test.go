766a4513fed3dda378c1f68c580820d34a1983f7
wlz <lingze.wl@gmail.com>
2024-04-23 10:38:46 +0800

    refactor(ssaapi):use reducer when build project

--- common/yak/ssaapi/test/yak/parse_project_test.go
+++ common/yak/ssaapi/test/yak/parse_project_test.go
@@ -1,34 +1,42 @@
 package ssaapi
 
 import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils/filesys"
 	"github.com/yaklang/yaklang/common/yak/ssaapi"
-	"testing"
 )
 
 func TestParseProject(t *testing.T) {
 	vfs := filesys.NewVirtualFs()
-	err := vfs.AddDirByString("a", "b", "c")
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = vfs.AddFileToDir("a", "a.yak", `include "b/b.yak"; dump(b)`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	vfs.AddFileToDir("b", "b.yak", `dump("in b.yak"); b = 3`)
-	vfs.AddFileToDir("c", "c.yak", `include "b/b.yak"; dump(b + 1)`)
+	vfs.AddFile("a/b", "c")
+	vfs.AddFile("a/a.yak", `include "b/b.yak"; dump(b)`)
+	vfs.AddFile("b/b.yak", `dump("in b.yak"); b = 3`)
+	vfs.AddFile("c/c.yak", `include "b/b.yak"; dump(b + 1)`)
 
-	prog, err := ssaapi.ParseProject(
-		vfs,
-		//ssaapi.WithFileSystemEntry("a/a.yak"),
-		ssaapi.WithDatabaseProgramName("test"),
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-	prog.Show()
-	if len(prog.Ref("b")) <= 0 {
-		t.Fatal("not found b")
-	}
+	t.Run("parse project with entry", func(t *testing.T) {
+		progs, err := ssaapi.ParseProject(
+			vfs,
+			ssaapi.WithFileSystemEntry("a/a.yak"),
+			// ssaapi.WithDatabaseProgramName("test"),
+		)
+		for index, prog := range progs {
+			log.Infof("prog[%d]:", index)
+			prog.Show()
+		}
+
+		require.NoError(t, err, "parse project failed")
+
+		require.Len(t, progs, 2, "progs should be 2")
+		prog := progs[0]
+
+		valuesB := prog.Ref("b")
+		require.Len(t, valuesB, 1, "valuesB should be 1")
+
+		valueB := valuesB[0]
+		require.Equal(t, "3", valueB.String(), "valueB should be 3")
+	})
+
 }
