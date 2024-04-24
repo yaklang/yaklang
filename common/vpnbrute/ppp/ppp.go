87d58596091173f0e167bb68f59066af23653a14
rookie <rookie_is@qq.com>
2024-04-24 10:22:07 +0800

    refactor(pptp):refactor pptp brute

--- common/vpnbrute/ppp/ppp.go
+++ common/vpnbrute/ppp/ppp.go
@@ -2,7 +2,6 @@ package ppp
 
 import (
 	"bytes"
-	"github.com/davecgh/go-spew/spew"
 	binparser "github.com/yaklang/yaklang/common/bin-parser"
 	"github.com/yaklang/yaklang/common/bin-parser/parser/base"
 	"github.com/yaklang/yaklang/common/go-funk"
@@ -10,14 +9,29 @@ import (
 )
 
 var (
-	LCPTypeReq         uint8 = 0x1
-	LCPTypeAck         uint8 = 0x2
-	LCPTypeNak         uint8 = 0x3
-	LCPTypeRej         uint8 = 0x4
-	CHAP_MD5                 = []byte{0xc2, 0x23, 0x05}
-	MS_CHAP_V2               = []byte{0xc2, 0x23, 0x81}
-	PAP                      = []byte{0xc0, 0x23}
-	SupportAuthTypeMap       = map[string][]byte{
+	PPP_LCP  uint16 = 0xc021
+	PPP_PAP  uint16 = 0xc023
+	PPP_CHAP uint16 = 0xc223
+
+	LCP_REQ uint8 = 0x1
+	LCP_ACK uint8 = 0x2
+	LCP_NCK uint8 = 0x3
+	LCP_REJ uint8 = 0x4
+
+	LCP_OPTION_AUTH uint8 = 0x3
+
+	CHAP_MD5   = []byte{0xc2, 0x23, 0x05}
+	MS_CHAP_V2 = []byte{0xc2, 0x23, 0x81}
+	PAP        = []byte{0xc0, 0x23}
+
+	CHAP_CHALLENGE uint8 = 0x1
+	CHAP_SUCCESS   uint8 = 0x3
+	CHAP_FAILURE   uint8 = 0x4
+
+	PAP_ACK uint8 = 0x2
+	PAP_NAK uint8 = 0x3
+
+	SupportAuthTypeMap = map[string][]byte{
 		"CHAP":     CHAP_MD5,
 		"MSCHAPV2": MS_CHAP_V2,
 		"PAP":      PAP,
@@ -31,7 +45,12 @@ type PPPAuth struct {
 	AuthTypeName       string
 	CanUseAuthTypeList map[string][]byte
 	AuthOk             chan bool
+	NegotiateOk        chan struct{}
 	MagicNumber        []byte
+
+	recvAck bool
+	sendAck bool // has send and recv ack ,High probability of successful negotiation
+
 }
 
 func GetDefaultPPPAuth() *PPPAuth {
@@ -40,6 +59,7 @@ func GetDefaultPPPAuth() *PPPAuth {
 		CanUseAuthTypeList: SupportAuthTypeMap,
 		AuthTypeName:       "CHAP",
 		AuthOk:             make(chan bool, 2),
+		NegotiateOk:        make(chan struct{}, 2),
 		MagicNumber:        []byte(utils.RandSecret(4)),
 	}
 }
@@ -81,12 +101,11 @@ func (p *PPPAuth) ChangeAuthType(rejectType []byte) error {
 	return nil
 }
 
-func (p *PPPAuth) GetPPPReqParams() map[string]any {
-	//lcpByte, _ := codec.DecodeHex("0100001501040578050626a73d32070208020d0306")
+func (p *PPPAuth) GetPPPStartReqParams() map[string]any {
 	return map[string]any{
 		"Address":  0xff,
 		"Control":  0x03,
-		"Protocol": 0xc021,
+		"Protocol": PPP_LCP,
 		//"Information": map[string]any{
 		"LCP": p.GetLCPConfigReqParams(0),
 		//"LCP": lcpByte,
@@ -94,19 +113,32 @@ func (p *PPPAuth) GetPPPReqParams() map[string]any {
 	}
 }
 
+func (p *PPPAuth) GetPAPReqParams() map[string]any {
+	return map[string]any{
+		"Address":  0xff,
+		"Control":  0x03,
+		"Protocol": PPP_PAP,
+		"PAP": map[string]any{
+			"Code":       1,
+			"Identifier": 0,
+			"Length":     len(p.Username) + len(p.Password) + 6,
+			"Request": map[string]any{
+				"Peer ID Length":  len(p.Username),
+				"Peer ID":         p.Username,
+				"Password Length": len(p.Password),
+				"Password":        p.Password,
+			},
+		},
+	}
+}
+
 func (p *PPPAuth) GetLCPConfigReqParams(id int) map[string]any {
 	return map[string]any{ // just negotiate Auth Type
 		"Code":       1,
 		"Identifier": id,
-		//"Length":     12 + len(p.AuthTypeCode),
-		"Length": 10,
+		"Length":     10,
 		"Info": map[string]any{
 			"Options": []map[string]any{
-				//{
-				//	"Type":   3,
-				//	"Length": len(p.AuthTypeCode) + 2,
-				//	"Data":   p.AuthTypeCode,
-				//},
 				{
 					"Type":   5,
 					"Length": 6,
@@ -129,13 +161,13 @@ func (p *PPPAuth) ProcessMessage(messageNode *base.Node) (map[string]any, error)
 	var err error
 	var res any
 	switch pppType {
-	case 0xc021:
+	case PPP_LCP:
 		res, err = p.ProcessLCPMessage(base.GetNodeByPath(messageNode, "Information.LCP"))
 		resultParams["LCP"] = res
-	//case 0xc023:
-	//	res, err = p.ProcessPAPMessage(base.GetNodeByPath(messageNode, "@PPP.PAP"))
-	//	resultParams["PAP"] = res
-	case 0xc223:
+	case PPP_PAP:
+		res, err = p.ProcessPAPMessage(base.GetNodeByPath(messageNode, "@PPP.PAP"))
+		resultParams["PAP"] = res
+	case PPP_CHAP:
 		res, err = p.ProcessCHAPMessage(base.GetNodeByPath(messageNode, "Information.CHAP"))
 		resultParams["CHAP"] = res
 	}
@@ -152,30 +184,55 @@ func (p *PPPAuth) ProcessLCPMessage(messageNode *base.Node) (map[string]any, err
 	if messageNode.Name != "LCP" {
 		return nil, utils.Errorf("not LCP message")
 	}
-	messageMap := binparser.NodeToMap(messageNode).(map[string]any)
-	lcpType := messageMap["Code"].(uint8)
+	messageMap, ok := binparser.NodeToMap(messageNode).(map[string]any)
+	if !ok {
+		return nil, utils.Error("process lcp message convert Node to map failed")
+	}
+
+	var lcpType, lcpId uint8
+	err := base.UnmarshalSubData(messageMap, "Code", &lcpType)
+	if err != nil {
+		return nil, utils.Wrap(err, "lcp get lcp code failed")
+	}
+	err = base.UnmarshalSubData(messageMap, "Identifier", &lcpId)
+	if err != nil {
+		return nil, utils.Wrap(err, "lcp get lcp Identifier code failed")
+	}
+
 	if lcpType > 0x4 {
 		return nil, nil
 	}
-	if lcpType == LCPTypeAck { // ack
+
+	if lcpType == LCP_ACK { // ack not need send resp message
+		p.recvAck = true
+		if p.sendAck {
+			p.NegotiateOk <- struct{}{}
+		}
 		return nil, nil
 	}
-	lcpId := messageMap["Identifier"].(uint8)
 
-	spew.Dump(messageMap)
-
-	options := messageMap["Info"].(map[string]any)["Options"].([]any)
+	var options []any
+	err = base.UnmarshalSubData(messageMap, "Info.Options", &options)
+	if err != nil {
+		return nil, utils.Wrap(err, "lcp get lcp options failed")
+	}
 
 	var authCode []byte
 	var hasAuthCode bool
 	for _, option := range options {
-		option := option.(map[string]any)
-		if option["Type"] == uint8(3) {
+		option, ok := option.(map[string]any)
+		if !ok {
+			return nil, utils.Error("convert lcp option to map failed")
+		}
+		if option["Type"] == LCP_OPTION_AUTH {
 			hasAuthCode = true
-			authCode = option["Data"].([]byte)
+			err = base.UnmarshalSubData(option, "Data", &authCode)
+			if err != nil {
+				return nil, utils.Wrap(err, "lcp get auth code failed")
+			}
 		}
 	}
-	if lcpType == LCPTypeRej {
+	if lcpType == LCP_REJ {
 		err := p.ChangeAuthType(authCode)
 		if err != nil {
 			return nil, err
@@ -191,39 +248,77 @@ func (p *PPPAuth) ProcessLCPMessage(messageNode *base.Node) (map[string]any, err
 	}
 
 	switch lcpType {
-	case LCPTypeReq: // req
-		messageMap["Code"] = LCPTypeAck
+	case LCP_REQ: // req
+		p.sendAck = true
+		if p.recvAck {
+			p.NegotiateOk <- struct{}{}
+		}
+		messageMap["Code"] = LCP_ACK
 		return messageMap, nil
-	case LCPTypeNak: // nak
-		messageMap["Code"] = LCPTypeReq
+	case LCP_NCK: // nak
+		messageMap["Code"] = LCP_REQ
 		return messageMap, nil
 	}
 	return nil, nil // not process no support message
 }
 
-func (p *PPPAuth) ProcessPAPMessage(messageNode *base.Node) {
+func (p *PPPAuth) ProcessPAPMessage(messageNode *base.Node) (map[string]any, error) {
+	if messageNode.Name != "PAP" {
+		return nil, utils.Errorf("not PAP message")
+	}
 
+	messageMap, ok := binparser.NodeToMap(messageNode).(map[string]any)
+	if !ok {
+		return nil, utils.Error("convert PAP message to map failed")
+	}
+
+	var papType uint8
+	err := base.UnmarshalSubData(messageMap, "Code", &papType)
+	if err != nil {
+		return nil, utils.Wrap(err, "CHAP get CHAP AUTH code failed")
+	}
+
+	switch papType {
+	case PAP_ACK:
+		p.AuthOk <- true
+		return nil, nil
+	case PAP_NAK:
+		p.AuthOk <- false
+		return nil, nil
+	}
+	return nil, nil
 }
 
 func (p *PPPAuth) ProcessCHAPMessage(messageNode *base.Node) (map[string]any, error) {
 	if messageNode.Name != "CHAP" {
-		return nil, utils.Errorf("not LCP message")
+		return nil, utils.Errorf("not CHAP message")
 	}
 
-	messageMap := binparser.NodeToMap(messageNode).(map[string]any)
+	messageMap, ok := binparser.NodeToMap(messageNode).(map[string]any)
+	if !ok {
+		return nil, utils.Error("convert CHAP message to map failed")
+	}
 
-	chapType := messageMap["Code"].(uint8)
-	id := messageMap["Identifier"].(uint8)
+	var chapType, id uint8
+	err := base.UnmarshalSubData(messageMap, "Code", &chapType)
+	if err != nil {
+		return nil, utils.Wrap(err, "CHAP get CHAP AUTH code failed")
+	}
+	err = base.UnmarshalSubData(messageMap, "Identifier", &id)
+	if err != nil {
+		return nil, utils.Wrap(err, "CHAP get CHAP Identifier code failed")
+	}
+
 	switch chapType {
-	case 1: // req - challenge
-		DataMap := messageMap["Info"].(map[string]any)["Data"]
-		if DataMap == nil {
-			return nil, nil
+	case CHAP_CHALLENGE: // req - challenge
+		var challenge []byte
+		err = base.UnmarshalSubData(messageMap, "Info.Data.Value", &challenge)
+		if err != nil {
+			return nil, utils.Wrap(err, "CHAP get CHAP challenge failed")
 		}
-		challenge := DataMap.(map[string]any)["Value"].([]byte)
 		response, err := GenerateCHAPResponse([]byte{id}, challenge, []byte(p.Username), []byte(p.Password), p.AuthTypeCode)
 		if err != nil {
-			return nil, err
+			return nil, utils.Wrap(err, "CHAP generate CHAP response failed")
 		}
 		return map[string]any{
 			"Code":       2,
@@ -235,10 +330,10 @@ func (p *PPPAuth) ProcessCHAPMessage(messageNode *base.Node) (map[string]any, er
 				"Name":       []byte(p.Username),
 			},
 		}, nil
-	case 3: // success
+	case CHAP_SUCCESS: // success
 		p.AuthOk <- true
 		return nil, nil
-	case 4: // failure
+	case CHAP_FAILURE: // failure
 		p.AuthOk <- false
 		return nil, nil
 	}
