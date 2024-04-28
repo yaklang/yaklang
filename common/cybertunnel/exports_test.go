a80097a1b3f5210b4aa20196be0fe7b96835c52a
v1ll4n <v1ll4n@qq.com>
2024-04-28 17:24:22 +0800

    add cybertunnel testcase into ci

--- common/cybertunnel/exports_test.go
+++
@@ -1,9 +0,0 @@
-package cybertunnel
-
-import "testing"
-
-func TestGetTunnelServerExternalIP(t *testing.T) {
-	rsp, err := QueryExistedDNSLogEvents("127.0.0.1:64333", "abc")
-	_ = err
-	println(rsp)
-}
