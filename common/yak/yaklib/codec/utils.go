b1010560dcf284f8c89662d7c6e7ca7198116245
rookie <rookie_is@qq.com>
2024-04-30 14:54:35 +0800

    refactor(codec):add symmetric crypt mode function

--- common/yak/yaklib/codec/utils.go
+++ common/yak/yaklib/codec/utils.go
@@ -1,7 +1,19 @@
 package codec
 
 import (
+	"crypto/cipher"
+	"errors"
+	"fmt"
 	"github.com/yaklang/yaklang/common/gmsm/sm4"
+	"strconv"
+)
+
+var (
+	CBC = "CBC"
+	ECB = "ECB"
+	CFB = "CFB"
+	OFB = "OFB"
+	CTR = "CTR"
 )
 
 var PKCS7Padding = sm4.PKCS7Padding
@@ -13,6 +25,138 @@ func PKCS7PaddingFor8ByteBlock(src []byte) []byte {
 
 func PKCS7UnPaddingFor8ByteBlock(src []byte) []byte {
 	return PKCS5UnPadding(src)
+}
+
+func FixIV(iv, key []byte, blockSize int) []byte {
+	if iv == nil && len(key) >= blockSize { // iv is nil, use key as iv
+		iv = key[:blockSize]
+	}
+	if len(iv) > blockSize { // iv is too long, truncate it
+		iv = iv[:blockSize]
+	}
+	return ZeroPadding(iv, blockSize)
+}
+
+func BlockCheck(iv, data []byte, blockSize int) error {
+	if len(iv) != blockSize {
+		return errors.New("check iv length: invalid iv size " + strconv.Itoa(len(iv)))
+	}
+
+	if len(data)%blockSize != 0 {
+		return errors.New("check data length: invalid data size " + strconv.Itoa(len(data)))
+	}
+
+	return nil
+}
+
+func CBCEncode(c cipher.Block, iv, data []byte) ([]byte, error) {
+	if err := BlockCheck(iv, data, c.BlockSize()); err != nil {
+		return nil, err
+	}
+	out := make([]byte, len(data))
+	encrypter := cipher.NewCBCEncrypter(c, iv)
+	encrypter.CryptBlocks(out, data)
+	return out, nil
+}
+
+func CBCDecode(c cipher.Block, iv, data []byte) ([]byte, error) {
+	if err := BlockCheck(iv, data, c.BlockSize()); err != nil {
+		return nil, err
+	}
+	out := make([]byte, len(data))
+	decrypter := cipher.NewCBCDecrypter(c, iv)
+	decrypter.CryptBlocks(out, data)
+	return out, nil
+}
+
+func CFBEncode(c cipher.Block, iv, data []byte) ([]byte, error) {
+	if err := BlockCheck(iv, data, c.BlockSize()); err != nil {
+		return nil, err
+	}
+	out := make([]byte, len(data))
+	encrypter := cipher.NewCFBEncrypter(c, iv)
+	encrypter.XORKeyStream(out, data)
+	return out, nil
+}
+
+func CFBDecode(c cipher.Block, iv, data []byte) ([]byte, error) {
+	if err := BlockCheck(iv, data, c.BlockSize()); err != nil {
+		return nil, err
+	}
+	out := make([]byte, len(data))
+	decrypter := cipher.NewCFBDecrypter(c, iv)
+	decrypter.XORKeyStream(out, data)
+	return out, nil
+}
+
+func ECBEncode(c cipher.Block, data []byte) ([]byte, error) {
+	blockSize := c.BlockSize()
+	if len(data)%blockSize != 0 {
+		return nil, fmt.Errorf("check data length: invalid data size %d, need %d", len(data), blockSize)
+	}
+	out := make([]byte, len(data))
+	for i := 0; i < len(data)/blockSize; i++ {
+		in_tmp := data[i*blockSize : i*blockSize+blockSize]
+		out_tmp := make([]byte, blockSize)
+		c.Encrypt(out_tmp, in_tmp)
+		copy(out[i*blockSize:i*blockSize+blockSize], out_tmp)
+	}
+	return out, nil
+}
+
+func ECBDecode(c cipher.Block, data []byte) ([]byte, error) {
+	blockSize := c.BlockSize()
+	if len(data)%blockSize != 0 {
+		return nil, fmt.Errorf("check data length: invalid data size %d, need %d", len(data), blockSize)
+	}
+	out := make([]byte, len(data))
+	for i := 0; i < len(data)/blockSize; i++ {
+		in_tmp := data[i*blockSize : i*blockSize+blockSize]
+		out_tmp := make([]byte, blockSize)
+		c.Decrypt(out_tmp, in_tmp)
+		copy(out[i*blockSize:i*blockSize+blockSize], out_tmp)
+	}
+	return out, nil
+}
+
+func OFBEncode(c cipher.Block, iv, data []byte) ([]byte, error) {
+	if err := BlockCheck(iv, data, c.BlockSize()); err != nil {
+		return nil, err
+	}
+	out := make([]byte, len(data))
+	encrypter := cipher.NewOFB(c, iv)
+	encrypter.XORKeyStream(out, data)
+	return out, nil
+}
+
+func OFBDecode(c cipher.Block, iv, data []byte) ([]byte, error) {
+	if err := BlockCheck(iv, data, c.BlockSize()); err != nil {
+		return nil, err
+	}
+	out := make([]byte, len(data))
+	decrypter := cipher.NewOFB(c, iv)
+	decrypter.XORKeyStream(out, data)
+	return out, nil
+}
+
+func CTREncode(c cipher.Block, iv, data []byte) ([]byte, error) {
+	if err := BlockCheck(iv, data, c.BlockSize()); err != nil {
+		return nil, err
+	}
+	out := make([]byte, len(data))
+	encrypter := cipher.NewCTR(c, iv)
+	encrypter.XORKeyStream(out, data)
+	return out, nil
+}
+
+func CTRDecode(c cipher.Block, iv, data []byte) ([]byte, error) {
+	if err := BlockCheck(iv, data, c.BlockSize()); err != nil {
+		return nil, err
+	}
+	out := make([]byte, len(data))
+	decrypter := cipher.NewCTR(c, iv)
+	decrypter.XORKeyStream(out, data)
+	return out, nil
 }
 
 type EncodedFunc func(any) string
