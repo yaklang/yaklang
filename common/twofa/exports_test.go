30c8b9e2cb507a772c54667bd9c7988f77b2f2af
VillanCh <188228725@qq.com>
2024-05-04 14:30:59 +0800

    add exp cross-build for docker

--- common/twofa/exports_test.go
+++ common/twofa/exports_test.go
@@ -1,11 +1,15 @@
 package twofa
 
 import (
+	"context"
 	"encoding/base32"
 	"fmt"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/utils/lowhttp/poc"
+	"strings"
 	"testing"
 )
 
@@ -17,6 +21,40 @@ func TestNewTOTPConfig(t *testing.T) {
 	config.GetToptUTCCode()
 	spew.Dump("TOTP", config.GetToptUTCCode())
 	assert.Equal(t, NewTOTPConfig(id).GetToptUTCCode(), config.GetToptUTCCode())
+}
+
+func TestNewTOTPServer(t *testing.T) {
+	responseId := uuid.New().String()
+	target, to := utils.DebugMockHTTPEx(func(req []byte) []byte {
+		return []byte(`HTTP/1.1 200 OK
+Content-Length: ` + fmt.Sprint(len(responseId)) + `
+
+` + responseId)
+	})
+	target = utils.HostPort(target, to)
+
+	pPort := utils.GetRandomAvailableTCPPort()
+
+	secret := uuid.New().String()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		NewOTPServer(secret, pPort, target).ServeContext(ctx)
+	}()
+
+	code := NewTOTPConfig(secret).GetToptUTCCode()
+	rsp, req, err := poc.HTTP(`GET / HTTP/1.1
+Host: 127.0.0.1:`+fmt.Sprint(pPort)+`
+`, poc.WithAppendHeader("Y-T-Verify-Code", fmt.Sprint(code)))
+	if err != nil {
+		panic(err)
+	}
+	_ = req
+	fmt.Println(string(rsp))
+	if !strings.Contains(string(rsp), responseId) {
+		t.Fatal("failed for verify proxy for in totp")
+	}
 }
 
 func TestComputeCode(t *testing.T) {
