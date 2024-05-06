f227440e0d47ac3245f95f275cdd14b219337e3e
intSheep <1098204193@qq.com>
2024-05-06 15:34:42 +0800

    fix(syntaxflow/sfvm):修复syntaxflow生成指令时多余pop

--- common/syntaxflow/sfvm/visitor_filter_expr.go
+++ common/syntaxflow/sfvm/visitor_filter_expr.go
@@ -71,7 +71,7 @@ func (y *SyntaxFlowVisitor) VisitFilterExpr(raw sf.IFilterExprContext) error {
 				}
 			}
 		}
-		y.EmitPop()
+		//y.EmitPop()
 	case *sf.FieldIndexFilterContext:
 		err := y.VisitFilterExpr(ret.FilterExpr())
 		if err != nil {
--- common/yak/ssaapi/syntaxflow_test.go
+++ common/yak/ssaapi/syntaxflow_test.go
@@ -6,37 +6,60 @@ import (
 )
 
 func TestProgramSyntaxFlow_Match(t *testing.T) {
-	prog, err := Parse(`
+	t.Run("Test Match", func(t *testing.T) {
+		prog, err := Parse(`
 a = Runtime.getRuntime()
 a.exec("bash attack")
 `)
-	if err != nil {
-		t.Fatal(err)
-	}
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	results, err := prog.SyntaxFlowWithError(`.getRuntime`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, 1, len(results))
-	results.Show()
-}
+		results, err := prog.SyntaxFlowWithError(`.getRuntime`)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, 1, len(results))
+		results.Show()
+	})
 
-func TestProgramSyntaxFlow_Match_MemberCallMember(t *testing.T) {
-	prog, err := Parse(`
+	t.Run("Match MemberCallMember", func(t *testing.T) {
+		prog, err := Parse(`
 a = Runtime.getRuntime()
 a.exec("bash attack")
 `)
-	if err != nil {
-		t.Fatal(err)
-	}
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	results, err := prog.SyntaxFlowWithError(`.getRuntime().exec()`)
-	if err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, 1, len(results))
-	if !results[0].IsCall() {
-		t.Fatal("expect call")
-	}
+		results, err := prog.SyntaxFlowWithError(`.getRuntime().exec()`)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, 1, len(results))
+		if !results[0].IsCall() {
+			t.Fatal("expect call")
+		}
+	})
+
+	t.Run("Match MemberCallMember", func(t *testing.T) {
+		prog, err := Parse(`
+a = Runtime.getRuntime()
+a.exec("bash attack")
+`)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		results, err := prog.SyntaxFlowWithError(`Runtime.getRuntime().exec()`)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Contains(t, results.String(), "Undefined-a.exec(valid)(\"bash attack\")")
+		if !results[0].IsCall() {
+			t.Fatal("expect call")
+		}
+
+	})
+
 }
