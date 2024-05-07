3c527338aa1e8664f26eebbb08ab15ac15db9b19
v1ll4n <v1ll4n@qq.com>
2024-05-07 12:48:24 +0800

    checkout ref

--- scripts/generate-commit.sh
+++ scripts/generate-commit.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 echo "Start to generate commit message"
 
-previous_commit=$(git for-each-ref --sort=-creatordate --format '%(refname:short)' refs/tags | grep -v '\-alpha' | grep -v '\-beta' | head -n 1)
-echo "Previous Commit: $previous_commit"
+# 获取当前 commit 的哈希
 current_commit=$(git rev-parse HEAD)
 echo "Current Commit: $current_commit"
-git log --format="%s" "$previous_commit".. "$current_commit"> /tmp/raw_commit_message.txt && cat /tmp/raw_commit_message.txt
+
+# 获取不包含 'alpha' 和 'beta'，且不是当前 commit 的最近一个 tag
+previous_tag=$(git for-each-ref --sort=-creatordate --format '%(refname:short)' refs/tags | grep -v '\-alpha' | grep -v '\-beta' | while read tag; do
+    tag_commit=$(git rev-list -n 1 $tag)
+    if [ "$tag_commit" != "$current_commit" ]; then
+        echo $tag
+        break
+    fi
+done | head -n 1)
+
+echo "Previous Tag: $previous_tag"
+
+# 检查 previous_tag 是否为空
+if [ -z "$previous_tag" ]; then
+    echo "No valid previous tag found that is not the current commit."
+    exit 1
+fi
 
 echo "Start to check commit message"
 # 检查/tmp/raw_commit_message.txt的行数
