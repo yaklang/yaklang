6f7723584a96b7d853ec79dcc1c205bde5c0e8a8
v1ll4n <v1ll4n@qq.com>
2024-05-07 12:01:12 +0800

    fix crossbuild ci

--- scripts/generate-commit.sh
+++ scripts/generate-commit.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-git log --format="%s" $(git for-each-ref --sort=-creatordate --format '%(refname:short)' refs/tags | grep -v '\-alpha' | grep -v '\-beta' | head -n 1)..$(git rev-parse HEAD) > /tmp/msg.txt && cat /tmp/msg.txt
+previous_commit=$(git for-each-ref --sort=-creatordate --format '%(refname:short)' refs/tags | grep -v '\-alpha' | grep -v '\-beta' | head -n 1)
+echo "Previous Commit: $previous_commit"
+current_commit=$(git rev-parse HEAD)
+echo "Current Commit: $current_commit"
+git log --format="%s" $previous_commit.. $current_commit> /tmp/msg.txt && cat /tmp/msg.txt
+
+# 检查/tmp/raw_commit_message.txt的行数
+line_count=$(wc -l < /tmp/raw_commit_message.txt)
+
+if [ "$line_count" -le 3 ]; then
+    echo "Fewer than 4 commit messages found. Fetching the last 30 commits:"
+    git log -n 30 --format="%s" > /tmp/raw_commit_message.txt
+fi
+
+cat /tmp/raw_commit_message.txt
