def6faa0127ea1e70ae5da1ee83adaef6daeeb21
v1ll4n <v1ll4n@qq.com>
2024-05-07 17:52:06 +0800

    add cli utils for ssa

--- common/yak/cmd/yak.go
+++ common/yak/cmd/yak.go
@@ -740,6 +740,7 @@ func main() {
 	app.Commands = append(app.Commands, cliGroup("Java Serialization Utils", yakcmds.JavaUtils...)...)
 	app.Commands = append(app.Commands, cliGroup("Project Management", yakcmds.ProjectCommands...)...)
 	app.Commands = append(app.Commands, cliGroup("Traffic Utils", yakcmds.TrafficUtilCommands...)...)
+	app.Commands = append(app.Commands, cliGroup("SSA Compiler", yakcmds.SSACompilerCommands...)...)
 	app.Commands = append(app.Commands, cliGroup("Utils", yakcmds.UtilsCommands...)...)
 	app.Commands = append(app.Commands, cliGroup("Network Distribution Utils", yakcmds.DistributionCommands...)...)
 	app.Commands = append(app.Commands, cliGroup("Vuln & Network Scanner", yakcmds.ScanCommands...)...)
