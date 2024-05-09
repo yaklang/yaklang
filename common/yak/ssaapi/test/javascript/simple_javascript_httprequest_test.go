4e5f995bac92417130cf968f5d391e4bee0531c1
Q16G <760046475@qq.com>
2024-05-09 15:03:43 +0800

    fix(js2ssa): 增加ajax、fetch测试

--- common/yak/ssaapi/test/javascript/simple_javascript_httprequest_test.go
+++ common/yak/ssaapi/test/javascript/simple_javascript_httprequest_test.go
@@ -1,6 +1,8 @@
 package javascript
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"github.com/yaklang/yaklang/common/yak/ssaapi"
 	"testing"
 )
@@ -71,7 +73,112 @@ func Test_JS_XMLHttpRequest(t *testing.T) {
 
 }
 
-func Test_JS_Fetch(t *testing.T) {}
+func TestJs_Ajax(t *testing.T) {
+	code := `$.ajax({ //统计访问量
+    url:'/foot_action!getCount.action',
+    type: 'POST',
+    dataType: 'json',
+    cache:false,
+    data: {url:window.location.href},
+    timeout: 5000,
+    error: function(){
+    },
+    success: function(result){
+     $("#fwls").html(result.count);
+        $("#fwl").html(result.count1);
+    }
+ });`
+	prog, err := ssaapi.Parse(code, ssaapi.WithLanguage(ssaapi.JS))
+	if err != nil {
+		panic(err)
+	}
+	withError, err := prog.SyntaxFlowWithError("$.ajax()")
+	if err != nil {
+		panic(err)
+	}
+	for _, value := range withError {
+		operator, err := value.GetCallActualParams()
+		if err != nil {
+			panic(err)
+		}
+		members, err := operator.GetMembers()
+		if err != nil {
+			panic(err)
+		}
+		match, valueOperator, err := members.ExactMatch("url")
+		if err != nil {
+			panic(err)
+		}
+		assert.Equal(t, match, true, "match is false, except true")
+		assert.Equal(t, len(valueOperator.GetNames()), 4, "valueOperator number not match")
+	}
+}
+func TestAjax_post(t *testing.T) {
+	code := `$.post({
+  url: 'https://jsonplaceholder.typicode.com/posts',
+  contentType: 'application/json',
+  data: JSON.stringify(formData),
+  success: function(response) {
+    // ...
+  },
+  error: function(xhr, status, error) {
+    // ...
+  }
+});
+`
+	prog, err := ssaapi.Parse(code, ssaapi.WithLanguage(ssaapi.JS))
+	if err != nil {
+		panic(err)
+	}
+	withError, err := prog.SyntaxFlowWithError("$.post()")
+	if err != nil {
+		panic(err)
+	}
+	params, err := withError.GetCallActualParams()
+	if err != nil {
+		panic(err)
+	}
+	members, err := params.GetMembers()
+	if err != nil {
+		panic(err)
+	}
+	match, operator, err := members.ExactMatch("url")
+	if err != nil {
+		panic(err)
+	}
+	assert.Equal(t, match, true, "match is false, except true")
+	assert.Equal(t, len(operator.GetNames()), 4, "valueOperator number not match")
+}
+
+func Test_JS_Fetch(t *testing.T) {
+	code := `fetch('url')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json(); 
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
+`
+	prog, err := ssaapi.Parse(code, ssaapi.WithLanguage(ssaapi.JS))
+	if err != nil {
+		panic(err)
+	}
+	withError, err := prog.SyntaxFlowWithError("fetch()")
+	if err != nil {
+		panic(err)
+	}
+	params, err := withError.GetCallActualParams()
+	if err != nil {
+		panic(err)
+	}
+	assert.Equal(t, len(params.GetNames()), 2, fmt.Sprintf("not match,except 1,match %v", len(params.GetNames())))
+}
 
 func Test_JS_JQuery(t *testing.T) {}
 
