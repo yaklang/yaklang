6e4219422b5239d5292566c9a5ea39b1970dcb32
VillanCh <188228725@qq.com>
2024-05-18 11:51:57 +0800

    Updated and revamped the Git Diff function in multiple files

--- common/yak/cmd/yakcmds/gitcmds.go
+++ common/yak/cmd/yakcmds/gitcmds.go
@@ -1,20 +1,180 @@
 package yakcmds
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/format/diff"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/urfave/cli"
+	"github.com/yaklang/yaklang/common/ai"
+	"github.com/yaklang/yaklang/common/ai/aispec"
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/utils/memedit"
 	"github.com/yaklang/yaklang/common/utils/yakgit/yakdiff"
+	"github.com/yaklang/yaklang/common/yak/yaklib/codec"
+	"io"
+	"os"
 	"path/filepath"
 	"strings"
 )
 
 var GitCommands = []*cli.Command{
+	{
+		Name: "git-ai-commit",
+		Action: func(c *cli.Context) error {
+			dir, err := os.Getwd()
+			if err != nil {
+				return err
+			}
+			repo, err := git.PlainOpen(dir)
+			if err != nil {
+				return err
+			}
+
+			worktree, err := repo.Worktree()
+			if err != nil {
+				return utils.Wrap(err, "plain worktree failed")
+			}
+
+			headRef, err := repo.Head()
+			if err != nil {
+				return err
+			}
+			headCommit, err := repo.CommitObject(headRef.Hash())
+			if err != nil {
+				return utils.Wrap(err, `wtBase, err := repo.CommitObject(ref.Hash())`)
+			}
+
+			stats, _ := worktree.Status()
+			if len(stats) > 0 {
+				// submit and amend
+				_, err = worktree.Add(".")
+				if err != nil {
+					return utils.Wrap(err, "worktree.Add failed")
+				}
+				commit, err := worktree.Commit("Auto Commit (Wait for amend)", &git.CommitOptions{
+					All:       true,
+					Author:    &headCommit.Author,
+					Committer: &headCommit.Committer,
+					Parents:   []plumbing.Hash{headCommit.ParentHashes[0]},
+				})
+				if err != nil {
+					return utils.Wrap(err, "worktree.Commit failed")
+				}
+				headCommit, err = repo.CommitObject(commit)
+				if err != nil {
+					return utils.Wrap(err, `auto-commit changes failed`)
+				}
+				headRef, err = repo.Head()
+				log.Infof("use head ref after commit")
+			}
+
+			// amend HEAD~1...HEAD
+			headTree, err := headCommit.Tree()
+			if err != nil {
+				return utils.Wrap(err, `baseTree, err := wtBase.Tree()`)
+			}
+
+			parentBase, err := headCommit.Parent(0)
+			if err != nil {
+				return utils.Wrap(err, `wtBase.Parent(0) failed`)
+			}
+			parentTree, err := parentBase.Tree()
+			if err != nil {
+				return utils.Wrap(err, `fetch parent commit tree`)
+			}
+
+			changes, err := parentTree.Diff(headTree)
+			if err != nil {
+				return err
+			}
+
+			result := bytes.NewBuffer(nil)
+			result.WriteString(`以下是 Git Diff` + "\r\n\r\n")
+			for _, change := range changes {
+				patch, err := change.Patch()
+				if err != nil {
+					continue
+				}
+				var buf bytes.Buffer
+				buf.WriteString(change.String())
+				buf.WriteByte('\n')
+				raw := patch.String()
+				if len(raw) > 409600 {
+					raw = utils.ShrinkString(raw, 409600)
+				}
+				buf.WriteString(raw)
+				fmt.Println(buf.String())
+				io.Copy(result, &buf)
+			}
+
+			var finalMessage string
+		GENERATE:
+			for {
+				log.Info("AI start to generate commit message(current)...")
+				results, err := ai.FunctionCall(result.String(), map[string]any{
+					"message":    "英文：适用于上述 git diff 的 commit message浓缩成一句话，标注 feature/bugfix/doc 之类的",
+					"message_zh": `中文：要求含义同 message`,
+				}, aispec.WithDebugStream(), aispec.WithType("openai"))
+				if err != nil {
+					log.Warnf("AI failed: %v", err)
+					_, err := fmt.Scanf("start to retry? (enter to continue)")
+					if err != nil {
+						log.Warnf("retry failed: %v", err)
+					}
+					continue
+				}
+
+				commitMessage, ok := results["message"]
+				if !ok {
+					log.Infof("cannot generate ai commit message: ")
+					_, err := fmt.Scanf("start to retry? (enter to continue)")
+					if err != nil {
+						log.Warnf("retry failed: %v", err)
+					}
+					continue
+				}
+
+				log.Infof("AI commit message: %v", commitMessage)
+				commitMessageZh, _ := results["message_zh"]
+				fmt.Println(commitMessageZh)
+				// yes or retry?
+				var result string
+				fmt.Println()
+				fmt.Println(`start to commit? (yes/y/t to commit, retry/r to retry, enter to continue): `)
+				_, err = fmt.Scanf("%s", &result)
+				if err != nil {
+					return err
+				}
+				switch strings.TrimSpace(strings.ToLower(result)) {
+				case "retry", "r":
+					continue
+				case "yes", "y", "t":
+					finalMessage = codec.AnyToString(commitMessage)
+					break GENERATE
+				}
+			}
+			fmt.Println("start to commit...")
+			commit, err := worktree.Commit(finalMessage, &git.CommitOptions{
+				All:       true,
+				Author:    &headCommit.Author,
+				Committer: &headCommit.Committer,
+				Parents:   []plumbing.Hash{headCommit.ParentHashes[0]},
+			})
+			if err != nil {
+				return utils.Wrapf(err, "amend commit failed: %v", finalMessage)
+			}
+			_, err = repo.CommitObject(commit)
+			if err != nil {
+				return utils.Wrap(err, `auto-commit changes failed`)
+			}
+			return nil
+		},
+	},
 	{
 		Name: "git-diff",
 		Flags: []cli.Flag{
@@ -154,20 +314,45 @@ var GitCommands = []*cli.Command{
 						} else {
 							fmt.Printf("%v: | %v\n", action, filename)
 						}
-						for _, chunked := range fp.Chunks() {
+
+						chunks := fp.Chunks()
+						if len(chunks) <= 0 {
+							continue
+						}
+
+						dstFile, err := os.ReadFile(filename)
+						if err != nil {
+							continue
+						}
+						editor := memedit.NewMemEditor(string(dstFile))
+
+						editorOffset := 0
+						for _, chunked := range chunks {
 							verbose := chunked.Content()
 							verbose = utils.ShrinkString(verbose, 64)
 
 							var action string = " "
+							var suffix string
 							switch chunked.Type() {
 							case diff.Equal: // eq
 								action = " "
+								editor.FindStringRangeIndexFirst(editorOffset, chunked.Content(), func(rangeIf memedit.RangeIf) {
+									editorOffset = editor.GetOffsetByPosition(rangeIf.GetEnd())
+								})
 							case diff.Add: //
 								action = "+"
+								editor.FindStringRangeIndexFirst(editorOffset, chunked.Content(), func(rangeIf memedit.RangeIf) {
+									editorOffset = editor.GetOffsetByPosition(rangeIf.GetEnd())
+									suffix = ` (` + fmt.Sprintf(
+										"%v:%v-%v:%v",
+										rangeIf.GetStart().GetLine(), rangeIf.GetStart().GetColumn(),
+										rangeIf.GetEnd().GetLine(), rangeIf.GetEnd().GetColumn(),
+									) + `)`
+								})
 							case diff.Delete:
 								action = "-"
 							}
-							fmt.Printf("  |%v%-67s |\n", action, verbose)
+							fmt.Printf("  |%v%-67s |%v\n", action, verbose, suffix)
 						}
 					}
 
