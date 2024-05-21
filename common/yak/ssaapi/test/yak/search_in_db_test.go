6fb5db63d5203cfd325383f221d916facff28b2d
VillanCh <188228725@qq.com>
2024-05-21 23:13:30 +0800

    add FindFlexibleDependsIntersection

--- common/yak/ssaapi/test/yak/search_in_db_test.go
+++ common/yak/ssaapi/test/yak/search_in_db_test.go
@@ -1,6 +1,8 @@
 package ssaapi
 
 import (
+	"fmt"
+	"github.com/yaklang/yaklang/common/yak/ssaapi"
 	"testing"
 
 	"github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
@@ -23,4 +25,32 @@ register("/route1", handler)
 		map[string][]string{
 			"target": {"Parameter-request"},
 		})
+}
+
+func TestSearchAndFind(t *testing.T) {
+	code := `
+cmd = request.GetParam("cmd")
+cmd.ReplaceAll("hacked", "foo-bar")
+if cmd.Contains("ca") {
+	cmd = str.Replace("foo", "bar")
+}
+os.System(cmd)
+`
+	prog, err := ssaapi.Parse(code)
+	if err != nil {
+		t.Fatal(err)
+	}
+	params := prog.SyntaxFlowChain(`req*.Get*() as $params`).Show()
+	upSource := prog.SyntaxFlowChain("os.System(* as $sink)").Show()
+	r := ssaapi.FindFlexibleDependsIntersection(upSource, params).Show().Len()
+	if r <= 0 {
+		t.Fatal("cannot find flexible depends intersection")
+	}
+
+	fmt.Println("-------------------------------------------------------")
+
+	r = ssaapi.FindFlexibleCommonDepends(append(upSource, params...)).Show().Len()
+	if r != 1 {
+		t.Fatal("cannot find depends in common")
+	}
 }
