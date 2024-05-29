8632fc5002907e4966ebb27b4d9cef48154d21d9
wlz <lingze.wl@gmail.com>
2024-05-29 14:26:46 +0800

    test(coreplugin):use buildInPlugin static analyze

--- common/coreplugin/static_analyze_test.go
+++ common/coreplugin/static_analyze_test.go
@@ -2,23 +2,22 @@ package coreplugin
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/samber/lo"
-	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/yak"
 	"github.com/yaklang/yaklang/common/yak/ssaapi"
 	"github.com/yaklang/yaklang/common/yak/static_analyzer"
 	"github.com/yaklang/yaklang/common/yak/static_analyzer/result"
+	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
 )
 
-func MITMCheck(code string, t *testing.T) {
-	_, err := ssaapi.Parse(code, static_analyzer.GetPluginSSAOpt("mitm")...)
+func staticCheck(code, pluginType string, t *testing.T) {
+	_, err := ssaapi.Parse(code, static_analyzer.GetPluginSSAOpt(pluginType)...)
 	if err != nil {
 		t.Fatal("Failed to parse code: ", err)
 	}
-	if res := yak.StaticAnalyzeYaklang(string(code), "mitm"); len(lo.Filter(res, func(item *result.StaticAnalyzeResult, index int) bool {
+	if res := yak.StaticAnalyzeYaklang(string(code), pluginType); len(lo.Filter(res, func(item *result.StaticAnalyzeResult, index int) bool {
 		return item.Severity == result.Error
 	})) != 0 {
 		t.Fatalf("plugin : static analyzer failed: \n%s", res)
@@ -26,27 +25,10 @@ func MITMCheck(code string, t *testing.T) {
 }
 
 func TestAnalyzeMustPASS_CorePlugin(t *testing.T) {
-	files, err := basePlugin.ReadDir("base-yak-plugin")
-	if err != nil {
-		log.Error("Failed to read directory:", err)
-		return
-	}
-
-	for _, file := range files {
-		if strings.Contains(file.Name(), "核心引擎性能采样") {
-			continue
-		}
-		if !file.IsDir() {
-			filePath := fmt.Sprintf("base-yak-plugin/%s", file.Name())
-			codeBytes, err := basePlugin.ReadFile(filePath)
-			if err != nil {
-				log.Error("Failed to read file:", err)
-				continue
-			}
-
-			t.Run(fmt.Sprintf("plugin %s", file.Name()), func(t *testing.T) {
-				MITMCheck(string(codeBytes), t)
-			})
-		}
+	yakit.CallPostInitDatabase()
+	for _, plugin := range buildInPlugin {
+		t.Run(fmt.Sprintf("plugin(%s) %s", plugin.Type, plugin.ScriptName), func(t *testing.T) {
+			staticCheck(plugin.Content, plugin.Type, t)
+		})
 	}
 }
