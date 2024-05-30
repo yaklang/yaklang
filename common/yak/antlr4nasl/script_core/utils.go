a6a888532320d29cad930df75ae975a437172a90
z3 <z3r0ne@163.com>
2024-05-30 14:48:55 +0800

    support all ACT_SETTINGS category script

---
+++ common/yak/antlr4nasl/script_core/utils.go
@@ -0,0 +1,35 @@
+package script_core
+
+import (
+	"github.com/yaklang/yaklang/common/netx"
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/utils/pingutil"
+	"time"
+)
+
+func Ping(ctx *ExecContext) {
+	ok := false
+	defer func() {
+		if ok {
+			ctx.Kbs.SetKB("Host/dead", 0)
+			ctx.Kbs.SetKB("Host/State", "up")
+		} else {
+			//ping检测不存活 或排除打印机设备时会标注为dead
+			ctx.Kbs.SetKB("Host/dead", 1)
+			ctx.Kbs.SetKB("Host/State", "down")
+		}
+	}()
+	target := ctx.Host
+	tcpPingPort := "80,443,22,3389"
+	timeout := 3 * time.Second
+	dnsTimeout := 3 * time.Second
+	proxies := ctx.Proxies
+	if utils.IsIPv4(target) || utils.IsIPv6(target) {
+		ok = pingutil.PingAuto(target, tcpPingPort, timeout, proxies...).Ok
+	} else {
+		result := netx.LookupFirst(target, netx.WithTimeout(dnsTimeout))
+		if result != "" && (utils.IsIPv4(result) || utils.IsIPv6(result)) {
+			ok = pingutil.PingAuto(result, tcpPingPort, timeout, proxies...).Ok
+		}
+	}
+}
