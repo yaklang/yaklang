51886f8c7ebabd1d955db82bf9086092e1635da5
v1ll4n <v1ll4n@qq.com>
2024-06-04 14:02:50 +0800

    remove big lib, use openai-style

--- common/ai/tongyi/gateway.go
+++ common/ai/tongyi/gateway.go
@@ -1,14 +1,16 @@
-package openai
+package dashscopego
 
 import (
 	"errors"
 	"github.com/yaklang/yaklang/common/ai/aispec"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/lowhttp/poc"
 	"io"
 )
 
 func init() {
-	aispec.Register("openai", func() aispec.AIGateway {
+	aispec.Register("tongyi", func() aispec.AIGateway {
 		return &GetawayClient{}
 	})
 }
@@ -25,48 +27,37 @@ func (g *GetawayClient) Chat(s string, function ...aispec.Function) (string, err
 
 func (g *GetawayClient) ChatEx(details []aispec.ChatDetail, function ...aispec.Function) ([]aispec.ChatChoice, error) {
 	return aispec.ChatExBase(g.targetUrl, g.config.Model, details, function, g.BuildHTTPOptions, g.config.StreamHandler)
-
-}
-
-func (g *GetawayClient) ExtractData(msg string, desc string, fields map[string]any) (map[string]any, error) {
-	return aispec.ChatBasedExtractData(g.targetUrl, g.config.Model, msg, fields, g.BuildHTTPOptions, g.config.StreamHandler)
 }
 
 func (g *GetawayClient) ChatStream(s string) (io.Reader, error) {
 	return aispec.ChatWithStream(g.targetUrl, g.config.Model, s, g.BuildHTTPOptions)
 }
 
+func (g *GetawayClient) ExtractData(data string, desc string, fields map[string]any) (map[string]any, error) {
+	return aispec.ChatBasedExtractData(g.targetUrl, g.config.Model, data, fields, g.BuildHTTPOptions, g.config.StreamHandler)
+}
+
 func (g *GetawayClient) LoadOption(opt ...aispec.AIConfigOption) {
 	config := aispec.NewDefaultAIConfig(opt...)
+
+	log.Info("load option for tongyi ai")
 	g.config = config
 
 	if g.config.Model == "" {
-		g.config.Model = "gpt-3.5-turbo"
+		g.config.Model = "qwen-turbo"
 	}
 
 	if config.BaseURL != "" {
 		g.targetUrl = config.BaseURL
-	} else if config.Domain != "" {
-		if config.NoHttps {
-			g.targetUrl = "http://" + config.Domain + "/v1/chat/completions"
-		} else {
-			g.targetUrl = "https://" + config.Domain + "/v1/chat/completions"
-		}
 	} else {
-		g.targetUrl = "https://api.openai.com/v1/chat/completions"
+		g.targetUrl = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions"
 	}
 }
 
-func (g *GetawayClient) CheckValid() error {
-	if g.config.APIKey == "" {
-		return errors.New("APIKey is required")
-	}
-	return nil
-}
 func (g *GetawayClient) BuildHTTPOptions() ([]poc.PocConfigOption, error) {
 	opts := []poc.PocConfigOption{
 		poc.WithReplaceAllHttpPacketHeaders(map[string]string{
-			"Content-Type":  "application/json; charset=UTF-8",
+			"Content-Type":  "application/json",
 			"Accept":        "application/json",
 			"Authorization": "Bearer " + g.config.APIKey,
 		}),
@@ -76,4 +67,14 @@ func (g *GetawayClient) BuildHTTPOptions() ([]poc.PocConfigOption, error) {
 		opts = append(opts, poc.WithProxy(g.config.Proxy))
 	}
 	return opts, nil
+}
+
+func (g *GetawayClient) CheckValid() error {
+	if g.config == nil {
+		return utils.Error("bad config (empty)")
+	}
+	if g.config.APIKey == "" {
+		return errors.New("APIKey is required")
+	}
+	return nil
 }
