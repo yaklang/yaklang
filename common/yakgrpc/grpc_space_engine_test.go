9275b5c1ad21efeb760bc1a7badcbc9954ad5955
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-06-04 10:47:27 +0800

    test(space-engine): custom domain test

--- common/yakgrpc/grpc_space_engine_test.go
+++ common/yakgrpc/grpc_space_engine_test.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/yak"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
@@ -41,6 +45,46 @@ func TestServer_GetSpaceEngineStatus(t *testing.T) {
 			fmt.Println(resp)
 		})
 	}
+}
+
+func TestGRPCMUSTPASS_SpaceEngineCustomDomain(t *testing.T) {
+	client, err := NewLocalClient(true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	passed := false
+
+	host, port := utils.DebugMockHTTPEx(func(req []byte) []byte {
+		passed = true
+		spew.Dump(req)
+		return []byte(`{}`)
+	})
+
+	defer client.ResetGlobalNetworkConfig(context.Background(), &ypb.ResetGlobalNetworkConfigRequest{})
+
+	config, err := client.GetGlobalNetworkConfig(context.Background(), &ypb.GetGlobalNetworkConfigRequest{})
+	require.NoError(t, err)
+	token := utils.RandSecret(10)
+	config.AppConfigs = []*ypb.ThirdPartyApplicationConfig{
+		{
+			Type:           "fofa",
+			APIKey:         token,
+			UserIdentifier: "user",
+			ExtraParams: []*ypb.KVPair{
+				{Key: "domain", Value: utils.HostPort(host, port)},
+			},
+		},
+	}
+	_, err = client.SetGlobalNetworkConfig(context.Background(), config)
+	require.NoError(t, err)
+	yak.Execute(`
+ch, err = spacengine.Query("port:8080", spacengine.fofa(str.RandSecret(10)))
+for r in ch {
+	dump(r)
+}
+`)
+	require.NoError(t, err)
+	require.True(t, passed, `space engine set custom domain failed`)
 }
 
 // func TestServer_GetSpaceEngineAccountStatus(t *testing.T) {
