15cf4673f5e0314a7807414163c3ec6955558ef6
wlz <lingze.wl@gmail.com>
2024-06-05 16:32:39 +0800

    fix(ssa):param-member can be object

--- common/yak/java/tests/member_test.go
+++ common/yak/java/tests/member_test.go
@@ -1,1 +1,31 @@
 package tests
+
+import (
+	"testing"
+
+	"github.com/yaklang/yaklang/common/yak/ssaapi"
+	"github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
+)
+
+func TestMemberThis(t *testing.T) {
+	t.Run("test simple", func(t *testing.T) {
+		code := `
+package foo.bar;
+
+class A {
+	public  int key;
+
+	public void foo() {
+		print(this.key.String());
+	}
+}
+		`
+		ssatest.CheckSyntaxFlow(t, code,
+			`key.String() as $target`,
+			map[string][]string{
+				"target": {"Undefined-this.key.String(valid)()"},
+			},
+			ssaapi.WithLanguage(ssaapi.JAVA),
+		)
+	})
+}
