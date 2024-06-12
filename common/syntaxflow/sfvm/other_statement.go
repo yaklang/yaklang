f0ad6a8cddcc1ffaa36cc61cf399c59d8c32da9c
v1ll4n <v1ll4n@qq.com>
2024-06-12 15:51:25 +0800

    impl opcode for desc and check

---
+++ common/syntaxflow/sfvm/other_statement.go
@@ -0,0 +1,79 @@
+package sfvm
+
+import (
+	"github.com/yaklang/yaklang/common/syntaxflow/sf"
+	"github.com/yaklang/yaklang/common/utils/yakunquote"
+	"strings"
+)
+
+func mustUnquoteSyntaxFlowString(text string) string {
+	if strings.HasPrefix(text, "\"") || strings.HasPrefix(text, "'") {
+		afterText, err := yakunquote.Unquote(text)
+		if err != nil {
+			text = text[1 : len(text)-1]
+		} else {
+			text = afterText
+		}
+	}
+	return text
+}
+
+func (y *SyntaxFlowVisitor) VisitCheckStatement(raw sf.ICheckStatementContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*sf.CheckStatementContext)
+	if i == nil {
+		return nil
+	}
+
+	var thenString string
+	var elseString string
+
+	if i.ThenExpr() != nil {
+		text := i.ThenExpr().(*sf.ThenExprContext).StringLiteral().GetText()
+		thenString = mustUnquoteSyntaxFlowString(text)
+	}
+
+	if i.ElseExpr() != nil {
+		text := i.ElseExpr().(*sf.ElseExprContext).StringLiteral().GetText()
+		elseString = mustUnquoteSyntaxFlowString(text)
+	}
+
+	ref := i.RefVariable().GetText()
+	ref = strings.TrimLeft(ref, "$")
+	y.EmitCheckParam(ref, thenString, elseString)
+	return nil
+}
+
+func (y *SyntaxFlowVisitor) VisitDescriptionStatement(raw sf.IDescriptionStatementContext) interface{} {
+	if y == nil || raw == nil {
+		return nil
+	}
+
+	i, _ := raw.(*sf.DescriptionStatementContext)
+	if i == nil {
+		return nil
+	}
+
+	if i.DescriptionItems() == nil {
+		return nil
+	}
+
+	for _, item := range i.DescriptionItems().(*sf.DescriptionItemsContext).AllDescriptionItem() {
+		if ret, ok := item.(*sf.DescriptionItemContext); ok {
+			results := ret.AllStringLiteral()
+			if len(results) >= 2 {
+				key := mustUnquoteSyntaxFlowString(results[0].GetText())
+				value := mustUnquoteSyntaxFlowString(results[1].GetText())
+				y.EmitAddDescription(key, value)
+			} else {
+				key := mustUnquoteSyntaxFlowString(results[0].GetText())
+				y.EmitAddDescription(key, "")
+			}
+		}
+	}
+
+	return nil
+}
