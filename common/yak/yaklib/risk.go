912c4302e1b5e366def357f8e4ca85cf7285d7b4
rookie <rookie_is@qq.com>
2024-06-13 16:53:24 +0800

    fix(risk-counter):increment risk counter when output risk

--- common/yak/yaklib/risk.go
+++ common/yak/yaklib/risk.go
@@ -37,9 +37,6 @@ func YakitNewRiskBuilder(client *YakitClient) func(target string, opts ...yakit.
 
 `, title, risk.IP))
 			}
-			client.Output(&YakitStatusCard{
-				Id: "漏洞/风险/指纹", Data: fmt.Sprint(client.AddCounter()), Tags: nil,
-			})
 			client.Output(risk)
 		}
 	}
