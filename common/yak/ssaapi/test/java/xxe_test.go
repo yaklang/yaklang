6553556fd081a9fb7928f626d19739bd650740b5
VillanCh <188228725@qq.com>
2024-06-19 08:56:26 +0800

    improve condition via example(xxe)

--- common/yak/ssaapi/test/java/xxe_test.go
+++ common/yak/ssaapi/test/java/xxe_test.go
@@ -2,6 +2,7 @@ package java
 
 import (
 	_ "embed"
+	"github.com/yaklang/yaklang/common/syntaxflow/sfvm"
 	"github.com/yaklang/yaklang/common/yak/ssaapi"
 	"github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
 	"testing"
@@ -17,6 +18,23 @@ func TestXXE(t *testing.T) {
 		}
 		if prog.SyntaxFlowChain(".parse().getDocumentElement()").Show().Len() != 2 {
 			t.Fatal("parse().getDocumentElement() not found (not right)")
+		}
+		return nil
+	}, ssaapi.WithLanguage(ssaapi.JAVA))
+}
+
+func TestXXE_WithConditionExpr(t *testing.T) {
+	ssatest.Check(t, XXE_Code, func(prog *ssaapi.Program) error {
+		if prog.SyntaxFlowChain(`
+desc("Description": 'checking setFeature/setXIncludeAware/setExpandEntityReferences in DocumentBuilderFactory.newInstance()')
+
+DocumentBuilderFactory.newInstance()?{any: 'setFeature', 'setXIncludeAware', 'setExpandEntityReferences'} as $entry;
+$entry.*Builder().parse() as $result;
+
+check $result then "dangerous xml doc builder" else "safe xml doc builder";
+
+`, sfvm.WithEnableDebug(true)).Show().Len() <= 0 {
+			t.Fatal("setFeature(*) not found")
 		}
 		return nil
 	}, ssaapi.WithLanguage(ssaapi.JAVA))
