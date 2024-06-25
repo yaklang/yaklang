fb81c9462f0f303ad8bc1afad2dd024070c42a63
z3 <z3r0ne@163.com>
2024-06-25 17:43:53 +0800

    support parse not grammatically correct expression

--- common/fp/fingerprint/parsers/exp_parser.go
+++ common/fp/fingerprint/parsers/exp_parser.go
@@ -21,9 +21,11 @@ func ParseExpRule(rules [][2]string) ([]*rule.FingerPrintRule, error) {
 		}
 		r, err := compileExp(exp)
 		if err != nil {
-			log.Errorf("parse exp %s error: %v", exp, err)
-			continue
-			//return nil, err
+			r, err = compatibleSyntaxCompileExp(exp)
+			if err != nil {
+				log.Errorf("parse exp %s error: %v", exp, err)
+				continue
+			}
 		}
 		r.MatchParam.Info = &rule.FingerprintInfo{
 			Info: info,
@@ -33,6 +35,131 @@ func ParseExpRule(rules [][2]string) ([]*rule.FingerPrintRule, error) {
 	return res, nil
 }
 
+func compatibleSyntaxCompileExp(exp string) (*rule.FingerPrintRule, error) {
+	splitTokens := []string{"||", "&&"}
+	res := utils.IndexAllSubstrings(exp, splitTokens...)
+	tokens := []string{}
+	pre := 0
+	cut := func(i int) {
+		defer func() {
+			pre = i
+		}()
+		if len(exp[pre:i]) == 0 {
+			return
+		}
+		tokens = append(tokens, exp[pre:i])
+	}
+	for _, re := range res {
+		cut(re[1])
+		matchedStr := splitTokens[re[0]]
+		cut(re[1] + len(matchedStr))
+	}
+	cut(len(exp))
+	tmpItems := []any{}
+	for _, token := range tokens {
+		token = strings.TrimSpace(token)
+		if token == "" {
+			continue
+		}
+		if strings.HasPrefix(token, "(") {
+			tmpItems = append(tmpItems, "(")
+			token = token[1:]
+		}
+		if strings.HasSuffix(token, ")") {
+			tmpItems = append(tmpItems, ")")
+			token = token[:len(token)-1]
+		}
+		if utils.StringArrayContains(splitTokens, token) {
+			tmpItems = append(tmpItems, token)
+			continue
+		}
+		left, rightStr, ok := strings.Cut(token, "=")
+		if !ok {
+			return nil, fmt.Errorf("parse exp failed: %v", token)
+		}
+		var right any
+		if len(rightStr) > 1 && rightStr[0] == '"' && rightStr[len(rightStr)-1] == '"' {
+			right = rightStr[1 : len(rightStr)-1]
+		} else {
+			if token == "true" {
+				right = true
+			} else if token == "false" {
+				right = false
+			} else {
+				v, err := strconv.Atoi(token)
+				if err != nil {
+					right = rightStr
+				} else {
+					right = v
+				}
+			}
+		}
+		currentRule := &rule.FingerPrintRule{
+			Method: "exp",
+			MatchParam: &rule.MatchMethodParam{
+				Op:     "=",
+				Params: []any{left, right},
+			},
+		}
+		tmpItems = append(tmpItems, currentRule)
+	}
+	resStack := utils.NewStack[any]()
+	opStack := utils.NewStack[string]()
+	for i := 0; i < len(tmpItems); i++ {
+		item := tmpItems[i]
+		switch v := item.(type) {
+		case *rule.FingerPrintRule:
+			resStack.Push(v)
+		case string:
+			switch v {
+			case "(":
+				opStack.Push(v)
+			case ")":
+				op := opStack.Pop()
+				for op != "(" {
+					resStack.Push(op)
+					op = opStack.Pop()
+				}
+			default:
+				for opStack.Peek() == "&&" {
+					resStack.Push(opStack.Pop())
+				}
+				opStack.Push(v)
+			}
+		}
+	}
+	for !opStack.IsEmpty() {
+		resStack.Push(opStack.Pop())
+	}
+	reverseResStack := utils.NewStack[any]()
+	for !resStack.IsEmpty() {
+		reverseResStack.Push(resStack.Pop())
+	}
+	resStack = reverseResStack
+	newComplex := func(op string, rs []*rule.FingerPrintRule) *rule.FingerPrintRule {
+		return &rule.FingerPrintRule{
+			Method: "complex",
+			MatchParam: &rule.MatchMethodParam{
+				Condition: op,
+				SubRules:  rs,
+			},
+		}
+	}
+	mergeStack := utils.NewStack[*rule.FingerPrintRule]()
+	for !resStack.IsEmpty() {
+		switch v := resStack.Pop().(type) {
+		case *rule.FingerPrintRule:
+			mergeStack.Push(v)
+		case string:
+			if v == "||" {
+				mergeStack.Push(newComplex("or", mergeStack.PopN(2)))
+			} else {
+				mergeStack.Push(newComplex("and", mergeStack.PopN(2)))
+			}
+		}
+	}
+	return mergeStack.Pop(), nil
+}
 func compileExp(exp string) (*rule.FingerPrintRule, error) {
 	res := utils.IndexAllSubstrings(exp, buildinTokens...)
 	tokens := []string{}
@@ -83,18 +210,6 @@ func compileExp(exp string) (*rule.FingerPrintRule, error) {
 				r.MatchParam = &rule.MatchMethodParam{}
 				r.MatchParam.Params = append(r.MatchParam.Params, data)
 				currentStatus = "op"
-				//if _, ok := vars[token]; ok {
-				//	data := vars[token]
-				//	currentRule = &rule.FingerPrintRule{}
-				//	tmpItems = append(tmpItems, currentRule)
-				//	r := currentRule
-				//	r.Method = "exp"
-				//	r.MatchParam = &rule.MatchMethodParam{}
-				//	r.MatchParam.Params = append(r.MatchParam.Params, data)
-				//	currentStatus = "op"
-				//} else {
-				//	return nil, fmt.Errorf("unsupported var %s", token)
-				//}
 			}
 		case "op":
 			if !utils.StringArrayContains([]string{"="}, token) {
@@ -123,6 +238,7 @@ func compileExp(exp string) (*rule.FingerPrintRule, error) {
 			if escape {
 				strBuf += originToken
 				escape = false
+				index++
 				continue
 			} else {
 				switch token {
