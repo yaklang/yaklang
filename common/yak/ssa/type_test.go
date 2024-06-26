82f6115943c126d18c2a3cd51c7f92b48c4d24ae
wlz <lingze.wl@gmail.com>
2024-06-26 11:05:07 +0800

    fix(ssa):function type String-RawString recursive

---
+++ common/yak/ssa/type_test.go
@@ -0,0 +1,23 @@
+package ssa_test
+
+import (
+	"testing"
+
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/yak/ssa"
+)
+
+func Test_Type_ContainSelf(t *testing.T) {
+	t.Run("function return self", func(t *testing.T) {
+		fType := ssa.NewFunctionType("", nil, nil, false)
+		fType.ReturnType = fType
+		log.Infof("fType: %s", fType.String())
+	})
+
+	t.Run("object type contain self", func(t *testing.T) {
+		objType := ssa.NewMapType(ssa.GetStringType(), ssa.GetAnyType())
+		objType.FieldType = objType
+		objType.Finish()
+		log.Infof("objType: %s", objType.String())
+	})
+}
