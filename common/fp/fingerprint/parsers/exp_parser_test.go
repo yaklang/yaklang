1fddff75c9b6b1ce992afd2269b7c95dc7bdd10c
z3 <z3r0ne@163.com>
2024-06-27 15:12:33 +0800

    remove invalid test

--- common/fp/fingerprint/parsers/exp_parser_test.go
+++ common/fp/fingerprint/parsers/exp_parser_test.go
@@ -1,11 +1,7 @@
 package parsers
 
 import (
-	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
-	"github.com/yaklang/yaklang/common/go-funk"
-	"os"
-	"strings"
 	"testing"
 )
 
@@ -53,55 +49,4 @@ func TestCompiler(t *testing.T) {
 	assert.Equal(t, "complex", rules[0].Method)
 	assert.Equal(t, "or", rules[0].MatchParam.Condition)
 	assert.Equal(t, 2, len(rules[0].MatchParam.SubRules))
-}
-
-func _TestExportExpRules(t *testing.T) {
-	db, err := gorm.Open("sqlite3", "/Users/z3/Downloads/TideFinger/python3/cms_finger.db")
-	if err != nil {
-		t.Fatal(err)
-	}
-	db = db.Debug()
-	raws, err := db.Table("tide").Rows()
-	if err != nil {
-		t.Fatal(err)
-	}
-	ruleMap := map[string]struct{}{}
-	ruleList := [][]string{}
-	for raws.Next() {
-		var id int
-		var name, keys string
-		err := raws.Scan(&id, &name, &keys)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if _, ok := ruleMap[name]; ok {
-			continue
-		}
-		ruleMap[name] = struct{}{}
-		ruleList = append(ruleList, []string{name, keys})
-	}
-	raws, err = db.Table("fofa_back").Rows()
-	if err != nil {
-		t.Fatal(err)
-	}
-	for raws.Next() {
-		var id int
-		var name, keys string
-		err := raws.Scan(&id, &name, &keys)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if _, ok := ruleMap[name]; ok {
-			continue
-		}
-		ruleMap[name] = struct{}{}
-		ruleList = append(ruleList, []string{name, keys})
-	}
-
-	res1 := funk.Map(ruleList, func(d []string) string {
-		return strings.Join(d, "\x00")
-	})
-	res := strings.Join(res1.([]string), "\n")
-	os.WriteFile("/Users/z3/Downloads/rule.txt", []byte(res), 0644)
-	//println(res)
 }
