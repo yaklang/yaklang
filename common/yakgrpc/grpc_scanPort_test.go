c2addd0f0a7feecccab5712969b36b66c3454c49
z3 <z3r0ne@163.com>
2024-06-27 15:58:39 +0800

    add user-fingerprint option for PortScan interface

--- common/yakgrpc/grpc_scanPort_test.go
+++ common/yakgrpc/grpc_scanPort_test.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"errors"
 	"io"
+	"os"
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
@@ -39,6 +41,52 @@ func TestServer_PortScan(t *testing.T) {
 			break
 		}
 		spew.Dump(result)
+	}
+}
+func TestServer_CustomFingerprint(t *testing.T) {
+	client, err := NewLocalClient()
+	require.Nil(t, err)
+
+	host, port := utils.DebugMockHTTP([]byte("test CustomFingerprint"))
+
+	f, err := os.CreateTemp(os.TempDir(), "yakit-test-fingerprint-*.yaml")
+	if err != nil {
+		t.Fatal(err)
+	}
+	f.WriteString(`- methods:
+    - keywords:
+        - product: 测试
+          regexp: "test CustomFingerprint"`)
+	f.Close()
+
+	r, err := client.PortScan(context.Background(), &ypb.PortScanRequest{
+		UserFingerprintFiles: []string{f.Name()},
+		Targets:              host,
+		Ports:                strconv.Itoa(port),
+		Mode:                 "fingerprint",
+		Proto:                []string{"tcp"},
+		Concurrent:           50,
+		Active:               false,
+		ScriptNames:          []string{},
+	})
+	_ = r
+	require.Nil(t, err)
+	ok := false
+	for {
+		result, err := r.Recv()
+		if err != nil {
+			if !errors.Is(err, io.EOF) {
+				require.Nilf(t, err, "stream error: %v", err)
+			}
+			break
+		}
+		if strings.Contains(string(result.Message), "http/测试") {
+			ok = true
+		}
+		spew.Dump(result)
+	}
+	if !ok {
+		t.FailNow()
 	}
 }
 
