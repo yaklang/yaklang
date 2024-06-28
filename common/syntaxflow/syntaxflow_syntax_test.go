fbfd3504cb35e59c2fb845a1d5b6199fa45f4cde
v1ll4n <v1ll4n@qq.com>
2024-06-28 15:26:12 +0800

    fix syntax example\

--- common/syntaxflow/syntaxflow_syntax_test.go
+++ common/syntaxflow/syntaxflow_syntax_test.go
@@ -128,7 +128,7 @@ func TestSyntaxInOne(t *testing.T) {
 		"exec(* #-> ?{(any: 'example',abc,) && (opcode: call,phi)})",
 		"exec(* #-> ?{((any: 'example',abc,) && (opcode: phi))})",
 		"a...b",
-		"a..b.ccc()...e",
+		"a...b.ccc()...e",
 	} {
 		vm := sfvm.NewSyntaxFlowVirtualMachine().Debug(true)
 		_, err := vm.Compile(i)
