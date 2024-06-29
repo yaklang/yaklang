30c2d9788f9e1778c3fdaca6150ad06a8583be99
VillanCh <188228725@qq.com>
2024-06-29 20:42:08 +0800

    add sarif exports

---
+++ common/yak/java/tests/demo_test.go
@@ -0,0 +1,31 @@
+package tests
+
+import (
+	"fmt"
+	"github.com/yaklang/yaklang/common/yak/ssaapi"
+	"github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
+	"testing"
+)
+
+func TestCaseForSimple(t *testing.T) {
+	ssatest.Check(t, `
+public class XXEController {
+
+    @RequestMapping(value = "/one")
+	public List<MetricBean> getMetricList(String domain) throws SQLException {
+		String sql = String.format("select role from metrics.metric where domain_name = '%s'", domain);
+		Connection connection = MetricDataSource.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+		List<MetricBean> MetricList = new ArrayList<>();
+		ResultSet resultSet = preparedStatement.executeQuery();
+		preparedStatement.close();
+		connection.close();
+		return tdsqlMetricList;
+	}
+}
+`, func(prog *ssaapi.Program) error {
+		a := prog.SyntaxFlowChain("MetricDataSource.getConnection().prepareState*(* #-> * as $source)").Show().DotGraph()
+		fmt.Println(string(a))
+		return nil
+	}, ssaapi.WithLanguage(ssaapi.JAVA))
+}
