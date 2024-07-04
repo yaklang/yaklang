50b3dd683c60e7561d3007e78941c9fad3f94ac5
wlz <lingze.wl@gmail.com>
2024-07-04 16:19:06 +0800

    fix(grpc):use cmd.Dir set directory path

--- common/yakgrpc/grpc_terminal_test.go
+++ common/yakgrpc/grpc_terminal_test.go
@@ -118,3 +118,68 @@ func TestTerminalControlChar(t *testing.T) {
 		t.Fatalf("failed to read expect control char output from terminal")
 	}
 }
+
+func TestTerminalPath(t *testing.T) {
+	testText := uuid.NewString()
+	testBinaryPath := "cat"
+	if runtime.GOOS == "windows" {
+		testBinaryPath = "type"
+	}
+
+	// os.TempDir()
+	filename := "testfile"
+	path := os.TempDir()
+	temp, err := os.CreateTemp(path, filename)
+	assert.NilError(t, err)
+	temp.WriteString(testText)
+	defer temp.Close()
+
+	client, err := NewLocalClient()
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	stream, err := client.YaklangTerminal(ctx)
+	require.NoError(t, err)
+
+	stream.Send(&ypb.Input{
+		Path: path,
+	})
+
+	passed := false
+	firstMsgRecv := false
+
+	for {
+		output, err := stream.Recv()
+		if err != nil {
+			spew.Dump(err)
+			break
+		}
+		outputStr := strings.TrimSpace(string(output.Raw))
+		if !firstMsgRecv {
+			firstMsgRecv = true
+			stream.Send(&ypb.Input{
+				Raw: []byte(fmt.Sprintf("%s %s", testBinaryPath, temp.Name())),
+			})
+			if runtime.GOOS == "windows" {
+				stream.Send(&ypb.Input{
+					Raw: []byte("\r\n"),
+				})
+			} else {
+				stream.Send(&ypb.Input{
+					Raw: []byte("\n"),
+				})
+			}
+		}
+		if strings.Contains(outputStr, testText) {
+			passed = true
+			cancel()
+		}
+		t.Logf("%s", spew.Sdump(output.Raw))
+	}
+
+	if !passed {
+		t.Fatalf("failed to read expect output from terminal")
+	}
+
+}
