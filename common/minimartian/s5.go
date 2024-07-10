1edc6c53fa0c8c2b4e115e01caef8859aaf8ed4b
rookieBC <rookie_is@qq.com>
2024-07-11 00:19:51 +0800

    refactor(s5):refactor s5 proxy,support bind mode

--- common/minimartian/s5.go
+++ common/minimartian/s5.go
@@ -3,6 +3,8 @@ package minimartian
 import (
 	"context"
 	"encoding/binary"
+	"github.com/pkg/errors"
+	"github.com/yaklang/yaklang/common/utils/netutil"
 	"io"
 	"net"
 	"os"
@@ -18,6 +20,8 @@ import (
 
 type S5Config struct {
 	HandshakeTimeout    time.Duration
+	S5RequestTimeout    time.Duration
+	DialDstTimeout      time.Duration
 	Debug               bool
 	DownstreamHTTPProxy string
 	ProxyUsername       string
@@ -27,6 +31,8 @@ type S5Config struct {
 func NewSocks5Config() *S5Config {
 	return &S5Config{
 		HandshakeTimeout: 30 * time.Second,
+		S5RequestTimeout: 30 * time.Second,
+		DialDstTimeout:   30 * time.Second,
 	}
 }
 
@@ -36,6 +42,10 @@ const (
 	authNone                = 0x00
 	authWithUsernameAndPass = 0x02
 	authNoAcceptable        = 0xFF
+
+	UsernameAndPassVersion     = 0x01
+	UsernameAndPassAuthSuccess = 0x00
+	UsernameAndPassAuthFail    = 0x01
 
 	commandConnect = 0x01
 	commandBind    = 0x02
@@ -71,203 +81,249 @@ func IsSocks5HandleShake(conn net.Conn) (fConn net.Conn, _ bool, _ byte, _ error
 	return peekable, raw[0] == socks5Version && raw[1] > 0, raw[0], nil
 }
 
-func (c *S5Config) Handshake(conn net.Conn) (net.Conn, error) {
-	err := c.handshakeHandler(conn)
-	if err != nil {
-		return nil, utils.Errorf("s5 handshake failed: %s", err)
-	}
-	return conn, nil
-}
-
-//func (c *S5Config) Negotiation(conn net.Conn) error {
-//	res, err := parser.ParseBinary(conn, "application-layer.socks5", "ClientNegotiation")
-//	if err != nil {
-//		return utils.Errorf("parse s5 negotiation failed: %s", err)
-//	}
-//	if clientNegotiation,ok := binparser.NodeToMap(res).(map[string]any); ok {
-//
-//	}else {
-//		return utils.Errorf("parse s5 negotiation failed: %s", err)
-//	}
-//
-//}
-
-func (c *S5Config) handshakeHandler(conn net.Conn) error {
-	// handle shake
+func (c *S5Config) Handshake(conn net.Conn) error {
 	conn.SetReadDeadline(time.Now().Add(c.HandshakeTimeout))
+	defer conn.SetReadDeadline(time.Time{})
 
-	var buf = make([]byte, 2)
-
-	if _, err := io.ReadFull(conn, buf); err != nil {
-		return utils.Errorf("handle shake header failed: %s", err)
+	meta, err := utils.ReadN(conn, 2)
+	if err != nil || len(meta) < 2 {
+		return utils.Errorf("read negotiation header failed: %s", err)
 	}
 
-	if buf[0] != socks5Version {
-		return utils.Errorf("handle socks5 version failed: %v", strconv.Quote(string(buf)))
+	version := meta[0]
+	nMethods := meta[1]
+	if version != socks5Version {
+		return utils.Errorf("negotiation socks5 version failed: %v", strconv.Quote(string(meta)))
 	}
 
-	methods := make([]byte, buf[1])
+	var methods = make([]byte, nMethods)
 	if _, err := io.ReadFull(conn, methods); err != nil {
-		return utils.Errorf("handle shake auth methods failed: %s", err)
+		return utils.Errorf("negotiation shake auth methods failed: %s", err)
 	}
 
-	finishedAuth := len(methods) > 0
-	needpassword := c.ProxyPassword != "" || c.ProxyUsername != ""
-
-	if !finishedAuth {
-		conn.Write([]byte{socks5Version, authNoAcceptable})
-		return utils.Errorf("handle shake auth methods failed: %v", strconv.Quote(string(methods)))
+	supportMethods := []byte{authWithUsernameAndPass}
+	needAuth := c.ProxyPassword != "" || c.ProxyUsername != ""
+	if !needAuth {
+		supportMethods = append(supportMethods, authNone)
 	}
 
-	var authFlag byte = authNone
-	if needpassword {
-		authFlag = authWithUsernameAndPass
-		conn.Write([]byte{socks5Version, authFlag})
+	authFlag := byte(authNoAcceptable)
+	for _, method := range methods {
+		if utils.ContainsAny(supportMethods, method) {
+			authFlag = method
+		}
+	}
 
-		bytes, err := utils.ReadN(conn, 2)
-		if err != nil {
-			return err
-		}
-		if bytes[0] != 0x01 {
-			return utils.Errorf("auth version failed: %v", strconv.Quote(string(bytes)))
-		}
-		var username string
-		var password string
-		ulen := int(bytes[1])
-		if ulen == 0 {
-			username = ""
-		} else {
-			usernameBytes, err := utils.ReadN(conn, ulen)
-			if err != nil {
-				return utils.Errorf("read username failed: %s, err")
-			}
-			username = string(usernameBytes)
-		}
+	_, err = conn.Write([]byte{socks5Version, authFlag})
+	if err != nil {
+		return err
+	}
 
-		bytes, err = utils.ReadN(conn, 1)
+	if needAuth {
+		authVersion, err := utils.ReadN(conn, 1)
 		if err != nil {
 			return err
 		}
 
-		plen := int(bytes[0])
-		if plen == 0 {
-			password = ""
-		} else {
-			passwordBytes, err := utils.ReadN(conn, plen)
+		if len(authVersion) <= 0 {
+			return utils.Errorf("read auth version get empty")
+		}
+
+		if authVersion[0] != byte(UsernameAndPassVersion) {
+			return utils.Errorf("auth version failed: %v", strconv.Quote(string(authVersion[0])))
+		}
+
+		getDate := func() (string, error) {
+			lengthByte, err := utils.ReadN(conn, 1)
 			if err != nil {
-				return utils.Errorf("read password failed: %s, err")
+				return "", err
 			}
-			password = string(passwordBytes)
+			if len(lengthByte) <= 0 {
+				return "", utils.Errorf("read data length get empty")
+			}
+			data, err := utils.ReadN(conn, int(lengthByte[0]))
+			if err != nil {
+				return "", err
+			}
+			return string(data), nil
+		}
+
+		username, err := getDate()
+		if err != nil {
+			return err
+		}
+		password, err := getDate()
+		if err != nil {
+			return err
 		}
 
 		if !(c.ProxyUsername == username && c.ProxyPassword == password) {
+			conn.Write([]byte{UsernameAndPassVersion, UsernameAndPassAuthFail})
 			return utils.Errorf("auth failed")
 		}
-		conn.Write([]byte{0x01, 0x00})
-	} else {
-		conn.Write([]byte{socks5Version, authFlag})
+		conn.Write([]byte{UsernameAndPassVersion, UsernameAndPassAuthSuccess})
 	}
 
 	return nil
 }
 
-func (c *S5Config) HandleS5Request(conn net.Conn) (net.Conn, error) {
-	buf := make([]byte, 4)
-
-	s5RequestTimeout := 30 * time.Second
-
-	conn.SetDeadline(time.Now().Add(s5RequestTimeout))
-	if _, err := io.ReadFull(conn, buf); err != nil {
-		return nil, utils.Errorf("cannot read s5 req header: %s", err)
+func (c *S5Config) HandleS5RequestHeader(conn net.Conn) (version byte, cmd byte, AType byte, target string, err error) {
+	headerBuf, err := utils.ReadN(conn, 4)
+	if err != nil || len(headerBuf) < 4 {
+		return
 	}
-
-	version, cmd, reserved := buf[0], buf[1], buf[2]
-
-	if version != socks5Version {
-		return nil, utils.Errorf("invalid s5 version: %v", strconv.Quote(string(buf)))
-	}
-
-	if cmd != commandConnect {
-		return nil, utils.Errorf("invalid s5 command: %v", strconv.Quote(string(buf)))
-	}
+	version, cmd, reserved, AType := headerBuf[0], headerBuf[1], headerBuf[2], headerBuf[3]
 	_ = reserved
 
-	// 要连接的具体目标
-	var targetHost string
+	if version != socks5Version {
+		err = utils.Errorf("invalid s5 version: %v", strconv.Quote(string(version)))
+		return
+	}
 
-	var addrBytesRaw []byte
-	addrType := buf[3]
-	switch addrType {
+	var targetHost string
+	switch AType {
 	case addrTypeIPv4:
 		v4 := make(net.IP, net.IPv4len)
-		if _, err := io.ReadFull(conn, v4); err != nil {
-			return nil, err
+		if _, err = io.ReadFull(conn, v4); err != nil {
+			return
 		}
-		addrBytesRaw = append(addrBytesRaw, addrTypeIPv4)
-		addrBytesRaw = append(addrBytesRaw, v4...)
 		targetHost = v4.String()
 	case addrTypeIPv6:
 		v6 := make(net.IP, net.IPv6len)
-		if _, err := io.ReadFull(conn, v6); err != nil {
-			return nil, err
+		if _, err = io.ReadFull(conn, v6); err != nil {
+			return
 		}
-		addrBytesRaw = append(addrBytesRaw, addrTypeIPv6)
-		addrBytesRaw = append(addrBytesRaw, v6...)
 		targetHost = v6.String()
 	case addrTypeFQDN:
-		if _, err := io.ReadFull(conn, buf[:1]); err != nil {
-			return nil, err
+		var buf []byte
+		buf, err = utils.ReadN(conn, 1)
+		if err != nil || len(buf) < 1 {
+			return
 		}
 		fqdnLen := buf[0]
 		fqdn := make([]byte, fqdnLen)
-		if _, err := io.ReadFull(conn, fqdn); err != nil {
-			return nil, err
+		if _, err = io.ReadFull(conn, fqdn); err != nil {
+			return
 		}
-		addrBytesRaw = append(addrBytesRaw, addrTypeFQDN, buf[0])
-		addrBytesRaw = append(addrBytesRaw, fqdn...)
 		targetHost = string(fqdn)
 	default:
-		return nil, utils.Errorf("read s5 req addr type failed: %v", strconv.Quote(string(buf)))
+		err = utils.Errorf("read s5 req addr type failed: %v", strconv.Quote(string(AType)))
+		return
 	}
 
 	var targetPort uint16 // 0-65535 (0xffff f=(0b1111))
-	if err := binary.Read(conn, binary.BigEndian, &targetPort); err != nil {
-		return nil, utils.Errorf("read s5 req port failed: %s", err)
+	if err = binary.Read(conn, binary.BigEndian, &targetPort); err != nil {
+		return
 	}
+
+	return version, cmd, AType, utils.HostPort(targetHost, targetPort), nil
+}
+
+func BuildReply(host net.IP, port int) []byte {
 	var portsBytes = make([]byte, 2)
-	binary.BigEndian.PutUint16(portsBytes, targetPort)
-	addrBytesRaw = append(addrBytesRaw, portsBytes...)
-
-	targetAddr := utils.HostPort(targetHost, targetPort)
-	log.Debugf("socks5 connection recv from[%v] to [%v]", conn.RemoteAddr(), targetAddr)
-
-	downstreamProxy := c.DownstreamHTTPProxy
-	var proxyConnectionTimeout = 30 * time.Second
-	var actConn net.Conn
-	var err error
-	actConn, err = netx.DialTCPTimeout(proxyConnectionTimeout, targetAddr, downstreamProxy)
-	if err != nil {
-		return nil, utils.Errorf("dial target failed: %s", err)
-	}
-	if actConn == nil {
-		return nil, utils.Error("BUG: act conn is nil")
-	}
-
-	// 告诉客户端已成功连接到目标服务器
+	binary.BigEndian.PutUint16(portsBytes, uint16(port))
 	reply := []byte{socks5Version, replySuccess, 0x00}
-	host, _, _ := utils.ParseStringToHostPort(actConn.RemoteAddr().String())
-	if utils.IsIPv4(host) {
+	if ipIns := host.To4(); ipIns != nil {
 		reply = append(reply, addrTypeIPv4)
-		reply = append(reply, net.ParseIP(host).To4()...)
+		reply = append(reply, ipIns...)
 		reply = append(reply, portsBytes...)
 	} else {
 		reply = append(reply, addrTypeIPv4)
 		reply = append(reply, 0x00, 0x00, 0x00, 0x00)
 		reply = append(reply, portsBytes...)
 	}
+	return reply
+}
 
-	conn.Write(reply)
-	return actConn, nil
+func (c *S5Config) HandleConnect(conn net.Conn, target string) error {
+	dstConn, err := netx.DialTCPTimeout(c.DialDstTimeout, target)
+	if err != nil {
+		return utils.Errorf("dial target failed: %s", err)
+	}
+	if dstConn == nil {
+		return utils.Error("BUG: act conn is nil")
+	}
+
+	// 告诉客户端已成功连接到目标服务器
+	host, port, _ := utils.ParseStringToHostPort(dstConn.LocalAddr().String())
+	conn.Write(BuildReply(net.ParseIP(host), port))
+	err = c.ConnectionFallback(dstConn, conn)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *S5Config) HandleBind(conn net.Conn, target string) error {
+	bindPort := utils.GetRandomAvailableTCPPort()
+	targetHost, targetPort, _ := utils.ParseStringToHostPort(target)
+	if !utils.IsIPv4(target) && !utils.IsIPv6(target) {
+		targetHost = netx.LookupFirst(targetHost, netx.WithTimeout(3*time.Second))
+		if targetHost == "" {
+			return errors.Errorf("cannot found domain[%s]'s ip address", target)
+		}
+	}
+	_, _, bindHost, err := netutil.Route(time.Second*5, targetHost)
+	if err != nil {
+		return err
+	}
+
+	listener, err := net.Listen("tcp", utils.HostPort(bindHost.String(), bindPort))
+	if err != nil {
+		return err
+	}
+
+	// 告诉客户端已开始监听
+	_, err = conn.Write(BuildReply(bindHost, bindPort))
+	if err != nil {
+		return err
+	}
+
+	defer listener.Close()
+	for {
+		dstConn, err := listener.Accept()
+		if err != nil {
+			return utils.Errorf("socks5 server bind mode accept err %v", err)
+		}
+		if dstConn.RemoteAddr().String() == utils.HostPort(targetHost, targetPort) {
+			// 第二次 reply
+			_, err = conn.Write(BuildReply(net.ParseIP(targetHost), targetPort))
+			if err != nil {
+				return err
+			}
+			err := c.ConnectionFallback(dstConn, conn)
+			if err != nil {
+				return utils.Errorf("socks5 server bind mode connectFallback err %v", err)
+			}
+			break
+		}
+	}
+
+	return nil
+}
+
+func (c *S5Config) HandleS5Request(conn net.Conn) (net.Conn, error) {
+	conn.SetDeadline(time.Now().Add(c.S5RequestTimeout))
+	defer conn.SetDeadline(time.Time{})
+
+	_, cmd, _, target, err := c.HandleS5RequestHeader(conn)
+	if err != nil {
+		return conn, err
+	}
+
+	switch cmd {
+	case commandConnect:
+		err := c.HandleConnect(conn, target)
+		if err != nil {
+			return nil, err
+		}
+	case commandBind:
+
+	case commandUDP:
+		//TODO
+	}
+
+	return conn, nil
 }
 
 func (c *S5Config) ServeConn(conn net.Conn) error {
@@ -279,8 +335,8 @@ func (c *S5Config) ServeConn(conn net.Conn) error {
 			log.Errorf("serve socks5 connection panic: %v", err)
 		}
 	}()
-	var err error
-	conn, err = c.Handshake(conn)
+
+	err := c.Handshake(conn)
 	if err != nil {
 		return err
 	}
