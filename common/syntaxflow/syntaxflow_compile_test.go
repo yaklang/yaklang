8bb6c0df6141d3a9ca308985e760b77f7aab7dbf
zpy <1098204193@qq.com>
2024-07-10 14:29:38 +0800

    test(syntaxflow):merge two compilation tests into one

--- common/syntaxflow/syntaxflow_compile_test.go
+++ common/syntaxflow/syntaxflow_compile_test.go
@@ -1,10 +1,49 @@
 package syntaxflow
 
 import (
+	"github.com/davecgh/go-spew/spew"
+	"strings"
 	"testing"
 
 	"github.com/yaklang/yaklang/common/syntaxflow/sfvm"
 )
+
+func TestSyntaxFlowFilter_Search(t *testing.T) {
+	for _, i := range []map[string]string{
+		{
+			"code":    "exec", // Ref
+			"keyword": "push$exact",
+		},
+		{
+			"code":    "ex*",
+			"keyword": "push$glob",
+		},
+		{
+			"code":    "/abc/",
+			"keyword": "push$regex",
+		},
+	} {
+		vm := sfvm.NewSyntaxFlowVirtualMachine()
+		vm.Debug()
+		frame, err := vm.Compile(i["code"])
+		if err != nil {
+			t.Fatal(err)
+		}
+		vm.Show()
+		count := 0
+		checked := false
+		count += len(frame.Codes)
+		for _, c := range frame.Codes {
+			if strings.Contains(c.String(), i["keyword"]) {
+				checked = true
+			}
+		}
+		if !checked {
+			t.Fatalf("SyntaxFlowVirtualMachine.Compile failed: %v", spew.Sdump(i))
+		}
+	}
+
+}
 
 func TestCompile(t *testing.T) {
 	vm := sfvm.NewSyntaxFlowVirtualMachine()
