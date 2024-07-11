0d58598f8e998b920d3495e0b1c0b7df2b2b6f84
YWHKKX <2076064543@qq.com>
2024-07-11 14:31:14 +0800

    add test

--- common/yak/JS2ssa/test/TemplateString_test.go
+++ common/yak/JS2ssa/test/TemplateString_test.go
@@ -2,25 +2,27 @@ package test
 
 import (
 	_ "embed"
-	"fmt"
 	_ "net/http/pprof"
 	"testing"
+
+	test "github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
 )
 
+func TestTemplateString1(t *testing.T) {
+	t.Run("normal", func(t *testing.T) {
+		test.CheckPrintlnValue("var a = `hello world`; println(a)", []string{`"hello world"`}, t)
+	})
+}
+
 func TestTemplateString2(t *testing.T) {
-	prog, err := ParseSSA("var a = `hello ${5 + 10} world`;")
-	if err != nil {
-		t.Fatal("prog parse error", err)
-	}
-	prog.Show()
-	fmt.Println(prog.GetErrors())
+
+	t.Run("expr", func(t *testing.T) {
+		test.CheckPrintlnValue("var b = 123; var a = `b = ${b}`; println(a)", []string{`add("b = ", castType(string, 123))`}, t)
+	})
 }
 
 func TestTemplateString3(t *testing.T) {
-	prog, err := ParseSSA("var b = 123; var a = `b = ${b}`;")
-	if err != nil {
-		t.Fatal("prog parse error", err)
-	}
-	prog.Show()
-	fmt.Println(prog.GetErrors())
+	t.Run("expr add", func(t *testing.T) {
+		test.CheckPrintlnValue("var a = `hello ${5 + 10} world`; println(a)", []string{`add(add("hello ", castType(string, 15)), " world")`}, t)
+	})
 }
