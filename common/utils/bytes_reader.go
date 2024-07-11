200d073416e53afb1c3bd225a94baa907e7e0683
rookie <rookie_is@qq.com>
2024-07-11 17:22:10 +0800

    fix(mitm):big resp io copy

--- common/utils/bytes_reader.go
+++ common/utils/bytes_reader.go
@@ -19,6 +19,48 @@ import (
 	"github.com/yaklang/yaklang/common/yak/yaklib/codec"
 )
 
+func IOCopy(dst io.Writer, src io.Reader, buf []byte) (written int64, err error) {
+	if buf == nil {
+		size := 32 * 1024
+		if l, ok := src.(*io.LimitedReader); ok && int64(size) > l.N {
+			if l.N < 1 {
+				size = 1
+			} else {
+				size = int(l.N)
+			}
+		}
+		buf = make([]byte, size)
+	}
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			nw, ew := dst.Write(buf[0:nr])
+			if nw < 0 || nr < nw {
+				nw = 0
+				if ew == nil {
+					ew = Errorf("short write")
+				}
+			}
+			written += int64(nw)
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if er != io.EOF {
+				err = er
+			}
+			break
+		}
+	}
+	return written, err
+}
+
 func ReadWithContextTickCallback(ctx context.Context, rc io.Reader, callback func([]byte) bool, interval time.Duration) {
 	scanner := bufio.NewScanner(rc)
 	scanner.Split(bufio.ScanBytes)
