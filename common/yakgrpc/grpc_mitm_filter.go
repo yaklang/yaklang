f8b11d04129f8f4552bf828dbf43f6447e012d36
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-07-11 17:16:58 +0800

    feat(grpc-filter): project side filter, save to project db first

--- common/yakgrpc/grpc_mitm_filter.go
+++ common/yakgrpc/grpc_mitm_filter.go
@@ -2,11 +2,13 @@ package yakgrpc
 
 import (
 	"context"
+
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
 func (s *Server) SetMITMFilter(ctx context.Context, req *ypb.SetMITMFilterRequest) (*ypb.SetMITMFilterResponse, error) {
-	var filterManager = NewMITMFilterManager(s.GetProfileDatabase())
+	projectDB, profileDB := s.GetProjectDatabase(), s.GetProfileDatabase()
+	filterManager := GetMITMFilterManager(projectDB, profileDB)
 	filterManager.IncludeHostnames = req.GetIncludeHostname()
 	filterManager.ExcludeHostnames = req.GetExcludeHostname()
 	filterManager.ExcludeMethods = req.GetExcludeMethod()
@@ -15,12 +17,14 @@ func (s *Server) SetMITMFilter(ctx context.Context, req *ypb.SetMITMFilterReques
 	filterManager.ExcludeMIME = req.GetExcludeContentTypes()
 	filterManager.ExcludeUri = req.GetExcludeUri()
 	filterManager.IncludeUri = req.GetIncludeUri()
+	// force save to project DB
+	filterManager.db = projectDB
 	filterManager.Save()
 	return &ypb.SetMITMFilterResponse{}, nil
 }
 
 func (s *Server) GetMITMFilter(ctx context.Context, req *ypb.Empty) (*ypb.SetMITMFilterRequest, error) {
-	var filterManager = NewMITMFilterManager(s.GetProfileDatabase())
+	filterManager := GetMITMFilterManager(s.GetProjectDatabase(), s.GetProfileDatabase())
 	return &ypb.SetMITMFilterRequest{
 		IncludeHostname:     filterManager.IncludeHostnames,
 		ExcludeHostname:     filterManager.ExcludeHostnames,
