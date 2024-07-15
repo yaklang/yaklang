3fa545180224743bce72d7ecdfa3793dd2b1b1a4
rookie <rookie_is@qq.com>
2024-07-15 12:17:02 +0800

    refactor(s5):add udp mod

--- common/minimartian/s5.go
+++ common/minimartian/s5.go
@@ -26,6 +26,10 @@ type S5Config struct {
 	DownstreamHTTPProxy string
 	ProxyUsername       string
 	ProxyPassword       string
+	Addr                string
+	UdpChangeCache      *utils.Cache[*UDPExchange]
+	UdpSrcCache         *utils.Cache[struct{}]
+	UdpConn             *net.UDPConn
 }
 
 func NewSocks5Config() *S5Config {
@@ -33,8 +37,15 @@ func NewSocks5Config() *S5Config {
 		HandshakeTimeout: 30 * time.Second,
 		S5RequestTimeout: 30 * time.Second,
 		DialDstTimeout:   30 * time.Second,
+		UdpChangeCache:   utils.NewTTLCache[*UDPExchange](time.Minute * 5),
+		UdpSrcCache:      utils.NewTTLCache[struct{}](time.Minute * 5),
 	}
 }
+
+var (
+	ErrS5Version  = errors.New("invalid s5 version")
+	ErrBadRequest = errors.New("bad s5 request")
+)
 
 const (
 	socks5Version = 0x05
@@ -47,9 +58,9 @@ const (
 	UsernameAndPassAuthSuccess = 0x00
 	UsernameAndPassAuthFail    = 0x01
 
-	commandConnect = 0x01
-	commandBind    = 0x02
-	commandUDP     = 0x03
+	commandConnect      = 0x01
+	commandBind         = 0x02
+	commandUDPAssociate = 0x03
 
 	addrTypeIPv4 = 0x01
 	addrTypeFQDN = 0x03
@@ -167,59 +178,92 @@ func (c *S5Config) Handshake(conn net.Conn) error {
 	return nil
 }
 
-func (c *S5Config) HandleS5RequestHeader(conn net.Conn) (version byte, cmd byte, AType byte, target string, err error) {
-	headerBuf, err := utils.ReadN(conn, 4)
-	if err != nil || len(headerBuf) < 4 {
+func ParseAddress(address string) (a byte, addr []byte, port []byte, err error) {
+	var h, p string
+	h, p, err = net.SplitHostPort(address)
+	if err != nil {
 		return
 	}
-	version, cmd, reserved, AType := headerBuf[0], headerBuf[1], headerBuf[2], headerBuf[3]
-	_ = reserved
-
-	if version != socks5Version {
-		err = utils.Errorf("invalid s5 version: %v", strconv.Quote(string(version)))
-		return
+	ip := net.ParseIP(h)
+	if ip4 := ip.To4(); ip4 != nil {
+		a = addrTypeIPv4
+		addr = ip4
+	} else if ip6 := ip.To16(); ip6 != nil {
+		a = addrTypeFQDN
+		addr = ip6
+	} else {
+		a = addrTypeFQDN
+		addr = []byte{byte(len(h))}
+		addr = append(addr, []byte(h)...)
 	}
-
-	var targetHost string
-	switch AType {
-	case addrTypeIPv4:
-		v4 := make(net.IP, net.IPv4len)
-		if _, err = io.ReadFull(conn, v4); err != nil {
-			return
-		}
-		targetHost = v4.String()
-	case addrTypeIPv6:
-		v6 := make(net.IP, net.IPv6len)
-		if _, err = io.ReadFull(conn, v6); err != nil {
-			return
-		}
-		targetHost = v6.String()
-	case addrTypeFQDN:
-		var buf []byte
-		buf, err = utils.ReadN(conn, 1)
-		if err != nil || len(buf) < 1 {
-			return
-		}
-		fqdnLen := buf[0]
-		fqdn := make([]byte, fqdnLen)
-		if _, err = io.ReadFull(conn, fqdn); err != nil {
-			return
-		}
-		targetHost = string(fqdn)
-	default:
-		err = utils.Errorf("read s5 req addr type failed: %v", strconv.Quote(string(AType)))
-		return
-	}
-
-	var targetPort uint16 // 0-65535 (0xffff f=(0b1111))
-	if err = binary.Read(conn, binary.BigEndian, &targetPort); err != nil {
-		return
-	}
-
-	return version, cmd, AType, utils.HostPort(targetHost, targetPort), nil
+	i, _ := strconv.Atoi(p)
+	port = make([]byte, 2)
+	binary.BigEndian.PutUint16(port, uint16(i))
+	return
 }
 
-func BuildReply(host net.IP, port int) []byte {
+type S5Request struct {
+	Ver     byte
+	Cmd     byte
+	Rsv     byte // 0x00
+	Atyp    byte
+	DstHost []byte
+	DstPort []byte // 2 bytes
+}
+
+func (c *S5Config) HandleS5RequestHeader(conn net.Conn) (*S5Request, error) {
+	conn.SetDeadline(time.Now().Add(c.S5RequestTimeout))
+	defer conn.SetDeadline(time.Time{})
+	bb := make([]byte, 4)
+	if _, err := io.ReadFull(conn, bb); err != nil {
+		return nil, err
+	}
+	if bb[0] != socks5Version {
+		return nil, ErrS5Version
+	}
+	var addr []byte
+	addrType := bb[3]
+	switch addrType {
+	case addrTypeIPv4:
+		addr = make(net.IP, net.IPv4len)
+		if _, err := io.ReadFull(conn, addr); err != nil {
+			return nil, err
+		}
+	case addrTypeIPv6:
+		addr = make(net.IP, net.IPv6len)
+		if _, err := io.ReadFull(conn, addr); err != nil {
+			return nil, err
+		}
+	case addrTypeFQDN:
+		var buf []byte
+		buf, err := utils.ReadN(conn, 1)
+		if err != nil || len(buf) < 1 {
+			return nil, utils.Errorf("read fqdn length failed: %s", err)
+		}
+		fqdnLen := buf[0]
+		addr = make([]byte, fqdnLen)
+		if _, err = io.ReadFull(conn, addr); err != nil {
+			return nil, err
+		}
+	default:
+		return nil, ErrBadRequest
+	}
+
+	port := make([]byte, 2)
+	if _, err := io.ReadFull(conn, port); err != nil {
+		return nil, err
+	}
+	return &S5Request{
+		Ver:     bb[0],
+		Cmd:     bb[1],
+		Rsv:     bb[2],
+		Atyp:    bb[3],
+		DstHost: addr,
+		DstPort: port,
+	}, nil
+}
+
+func NewReply(host net.IP, port int) []byte {
 	var portsBytes = make([]byte, 2)
 	binary.BigEndian.PutUint16(portsBytes, uint16(port))
 	reply := []byte{socks5Version, replySuccess, 0x00}
@@ -235,8 +279,8 @@ func BuildReply(host net.IP, port int) []byte {
 	return reply
 }
 
-func (c *S5Config) HandleConnect(conn net.Conn, target string) error {
-	dstConn, err := netx.DialTCPTimeout(c.DialDstTimeout, target)
+func (c *S5Config) HandleConnect(conn net.Conn, req *S5Request) error {
+	dstConn, err := netx.DialTCPTimeout(c.DialDstTimeout, utils.HostPort(string(req.DstHost), binary.BigEndian.Uint16(req.DstPort)))
 	if err != nil {
 		return utils.Errorf("dial target failed: %s", err)
 	}
@@ -246,7 +290,7 @@ func (c *S5Config) HandleConnect(conn net.Conn, target string) error {
 
 	// 告诉客户端已成功连接到目标服务器
 	host, port, _ := utils.ParseStringToHostPort(dstConn.LocalAddr().String())
-	conn.Write(BuildReply(net.ParseIP(host), port))
+	conn.Write(NewReply(net.ParseIP(host), port))
 	err = c.ConnectionFallback(dstConn, conn)
 	if err != nil {
 		return err
@@ -254,10 +298,11 @@ func (c *S5Config) HandleConnect(conn net.Conn, target string) error {
 	return nil
 }
 
-func (c *S5Config) HandleBind(conn net.Conn, target string) error {
+func (c *S5Config) HandleBind(conn net.Conn, req *S5Request) error {
+	target := utils.HostPort(string(req.DstHost), binary.BigEndian.Uint16(req.DstPort))
 	bindPort := utils.GetRandomAvailableTCPPort()
-	targetHost, targetPort, _ := utils.ParseStringToHostPort(target)
-	if !utils.IsIPv4(target) && !utils.IsIPv6(target) {
+	targetHost, targetPort := string(req.DstHost), binary.BigEndian.Uint16(req.DstPort)
+	if !utils.IsIPv4(targetHost) && !utils.IsIPv6(targetHost) {
 		targetHost = netx.LookupFirst(targetHost, netx.WithTimeout(3*time.Second))
 		if targetHost == "" {
 			return errors.Errorf("cannot found domain[%s]'s ip address", target)
@@ -274,7 +319,7 @@ func (c *S5Config) HandleBind(conn net.Conn, target string) error {
 	}
 
 	// 告诉客户端已开始监听
-	_, err = conn.Write(BuildReply(bindHost, bindPort))
+	_, err = conn.Write(NewReply(bindHost, bindPort))
 	if err != nil {
 		return err
 	}
@@ -287,7 +332,7 @@ func (c *S5Config) HandleBind(conn net.Conn, target string) error {
 		}
 		if dstConn.RemoteAddr().String() == utils.HostPort(targetHost, targetPort) {
 			// 第二次 reply
-			_, err = conn.Write(BuildReply(net.ParseIP(targetHost), targetPort))
+			_, err = conn.Write(NewReply(net.ParseIP(targetHost), int(targetPort)))
 			if err != nil {
 				return err
 			}
@@ -302,31 +347,260 @@ func (c *S5Config) HandleBind(conn net.Conn, target string) error {
 	return nil
 }
 
-func (c *S5Config) HandleS5Request(conn net.Conn) (net.Conn, error) {
-	conn.SetDeadline(time.Now().Add(c.S5RequestTimeout))
-	defer conn.SetDeadline(time.Time{})
+func (c *S5Config) HandleUDPAssociate(conn net.Conn, req *S5Request) error {
+	srcAddr := conn.RemoteAddr().String()
+	clientHost, _, _ := utils.ParseStringToHostPort(srcAddr)
+	if clientHost == string(req.DstHost) {
+		srcAddr = utils.HostPort(string(req.DstHost), binary.BigEndian.Uint16(req.DstPort))
+	}
+	host, port, _ := utils.ParseStringToHostPort(c.Addr)
+	if _, err := conn.Write(NewReply(net.ParseIP(host), port)); err != nil {
+		return err
+	}
+	c.UdpSrcCache.Set(srcAddr, struct{}{})
+	return nil
+}
 
-	_, cmd, _, target, err := c.HandleS5RequestHeader(conn)
+type Datagram struct {
+	Rsv     []byte // 0x00 0x00
+	Frag    byte
+	Atyp    byte
+	DstHost []byte
+	DstPort []byte // 2 bytes
+	Data    []byte
+}
+
+func NewDatagramFromBytes(bb []byte) (*Datagram, error) {
+	n := len(bb)
+	minl := 4
+	if n < minl {
+		return nil, ErrBadRequest
+	}
+	var host []byte
+	addrType := bb[3]
+	switch addrType {
+	case addrTypeIPv4:
+		minl += 4
+		if n < minl {
+			return nil, ErrBadRequest
+		}
+		host = bb[minl-4 : minl]
+	case addrTypeIPv6:
+		minl += 16
+		if n < minl {
+			return nil, ErrBadRequest
+		}
+		host = bb[minl-16 : minl]
+	case addrTypeFQDN:
+		minl += 1
+		if n < minl {
+			return nil, ErrBadRequest
+		}
+		l := bb[4]
+		if l == 0 {
+			return nil, ErrBadRequest
+		}
+		minl += int(l)
+		if n < minl {
+			return nil, ErrBadRequest
+		}
+		host = bb[minl-int(l) : minl]
+		host = append([]byte{l}, host...)
+	default:
+		return nil, ErrBadRequest
+	}
+	minl += 2
+	if n <= minl {
+		return nil, ErrBadRequest
+	}
+	port := bb[minl-2 : minl]
+	data := bb[minl:]
+	d := &Datagram{
+		Rsv:     bb[0:2],
+		Frag:    bb[2],
+		Atyp:    bb[3],
+		DstHost: host,
+		DstPort: port,
+		Data:    data,
+	}
+	return d, nil
+}
+
+func NewDatagram(atyp byte, dstaHost []byte, dstport []byte, data []byte) *Datagram {
+	if atyp == addrTypeFQDN {
+		dstaHost = append([]byte{byte(len(dstaHost))}, dstaHost...)
+	}
+	return &Datagram{
+		Rsv:     []byte{0x00, 0x00},
+		Frag:    0x00,
+		Atyp:    atyp,
+		DstHost: dstaHost,
+		DstPort: dstport,
+		Data:    data,
+	}
+}
+
+func (d *Datagram) Bytes() []byte {
+	b := make([]byte, 0)
+	b = append(b, d.Rsv...)
+	b = append(b, d.Frag)
+	b = append(b, d.Atyp)
+	b = append(b, d.DstHost...)
+	b = append(b, d.DstPort...)
+	b = append(b, d.Data...)
+	return b
+}
+
+type UDPExchange struct {
+	ClientAddr *net.UDPAddr
+	RemoteConn net.Conn
+}
+
+func (c *S5Config) UDPHandle(addr *net.UDPAddr, d *Datagram) error {
+	src := addr.String()
+	send := func(ue *UDPExchange, data []byte) error {
+		_, err := ue.RemoteConn.Write(data)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	dst := utils.HostPort(string(d.DstHost), d.DstPort)
+	var ue *UDPExchange
+	ue, ok := c.UdpChangeCache.Get(src + dst)
+	if ok {
+		return send(ue, d.Data)
+	}
+
+	_, ok = c.UdpSrcCache.Get(src)
+	if !ok {
+		log.Warnf("UDP src not found: %s", src)
+		return nil
+	}
+
+	var err error
+	remoteAddr, err := net.ResolveUDPAddr("udp", utils.HostPort(string(d.DstHost), d.DstPort))
 	if err != nil {
-		return conn, err
+		return err
 	}
-
-	switch cmd {
-	case commandConnect:
-		err := c.HandleConnect(conn, target)
-		if err != nil {
-			return nil, err
-		}
-	case commandBind:
-		err := c.HandleBind(conn, target)
-		if err != nil {
-			return nil, err
-		}
-	case commandUDP:
-		//TODO
+	localAddr, err := net.ResolveUDPAddr("udp", c.UdpConn.LocalAddr().String())
+	rc, err := net.DialUDP("udp", localAddr, remoteAddr)
+	ue = &UDPExchange{
+		ClientAddr: addr,
+		RemoteConn: rc,
 	}
+	if err := send(ue, d.Data); err != nil {
+		ue.RemoteConn.Close()
+		return err
+	}
+	c.UdpChangeCache.Set(src+remoteAddr.String(), ue)
+	go func(ue *UDPExchange, dst string) {
+		defer func() {
+			ue.RemoteConn.Close()
+			c.UdpChangeCache.Remove(ue.ClientAddr.String() + dst)
+		}()
+		var b [65507]byte
+		for {
+			n, err := ue.RemoteConn.Read(b[:])
+			if err != nil {
+				return
+			}
+			a, host, port, err := ParseAddress(dst)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			if a == addrTypeFQDN {
+				host = host[1:]
+			}
+			d1 := NewDatagram(a, host, port, b[0:n])
+			if _, err := c.UdpConn.WriteToUDP(d1.Bytes(), ue.ClientAddr); err != nil {
+				return
+			}
+		}
+	}(ue, dst)
+	return nil
+}
 
-	return conn, nil
+func (c *S5Config) Serve(Addr string) error {
+	listener, err := net.Listen("tcp", Addr)
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	c.Addr = Addr
+	udpAddr, err := net.ResolveUDPAddr("udp", Addr)
+	if err != nil {
+		return utils.Errorf("resolve udp addr failed: %s", err)
+	}
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		return utils.Errorf("listen udp failed: %s", err)
+	}
+	c.UdpConn = udpConn
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				b := make([]byte, 65507)
+				n, addr, err := udpConn.ReadFromUDP(b)
+				if err != nil {
+					log.Errorf("read udp failed: %v", err)
+					cancel()
+					return
+				}
+				go func(addr *net.UDPAddr, b []byte) {
+					d, err := NewDatagramFromBytes(b)
+					if err != nil {
+						log.Println(err)
+						return
+					}
+					if d.Frag != 0x00 {
+						log.Println("Ignore frag", d.Frag)
+						return
+					}
+					if err := c.UDPHandle(addr, d); err != nil {
+						log.Println(err)
+						return
+					}
+				}(addr, b[0:n])
+			}
+		}
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			break
+		default:
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Errorf("accept failed: %v", err)
+				continue
+			}
+			go func() {
+				defer func() {
+					if conn != nil {
+						conn.Close()
+					}
+					if err := recover(); err != nil {
+						log.Errorf("serve socks5 connection panic: %v", err)
+					}
+				}()
+				err := c.ServeConn(conn)
+				if err != nil {
+					log.Errorf("serve socks5 connection failed: %v", err)
+				}
+			}()
+		}
+	}
 }
 
 func (c *S5Config) ServeConn(conn net.Conn) error {
@@ -344,21 +618,30 @@ func (c *S5Config) ServeConn(conn net.Conn) error {
 		return err
 	}
 
-	dstConn, err := c.HandleS5Request(conn)
+	req, err := c.HandleS5RequestHeader(conn)
 	if err != nil {
 		return err
 	}
-	var isHttp bool
-	conn, dstConn, isHttp, err = c.HijackSource(conn, dstConn)
-	if err != nil {
-		return err
+
+	switch req.Cmd {
+	case commandConnect:
+		err := c.HandleConnect(conn, req)
+		if err != nil {
+			return err
+		}
+	case commandBind:
+		err := c.HandleBind(conn, req)
+		if err != nil {
+			return err
+		}
+	case commandUDPAssociate:
+		err := c.HandleUDPAssociate(conn, req)
+		if err != nil {
+			return err
+		}
 	}
-	_ = isHttp
-	return c.ConnectionFallback(conn, dstConn)
-	//if !isHttp {
-	//	return c.ConnectionFallback(conn, dstConn)
-	//}
-	//return nil
+
+	return nil
 }
 
 func (c *S5Config) ConnectionFallback(src, proxiedConn net.Conn) error {
