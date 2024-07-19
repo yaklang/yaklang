687f12cc883a4c0a5f30699a83f11778a857f25f
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-07-19 16:04:42 +0800

    refactor(yakurl): fuzzy search document

---
+++ common/utils/fuzzy/fuzzy_test.go
@@ -0,0 +1,415 @@
+package fuzzy
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+	"testing"
+)
+
+const deBelloGallico = `All Gaul is divided into three parts, one of which the Belgae inhabit,
+the Aquitani another, those who in their own language are called Celts, in our Gauls, the third.
+All these differ from each other in language, customs and laws. The river Garonne separates the
+Gauls from the Aquitani; the Marne and the Seine separate them from the Belgae. Of all these,
+the Belgae are the bravest, because they are furthest from the civilization and refinement of
+[our] Province, and merchants least frequently resort to them, and import those things which tend
+to effeminate the mind; and they are the nearest to the Germans, who dwell beyond the Rhine,
+with whom they are continually waging war; for which reason the Helvetii also surpass the rest
+of the Gauls in valor, as they contend with the Germans in almost daily battles, when they either
+repel them from their own territories, or themselves wage war on their frontiers. One part of
+these, which it has been said that the Gauls occupy, takes its beginning at the river Rhone;
+it is bounded by the river Garonne, the ocean, and the territories of the Belgae; it borders,
+too, on the side of the Sequani and the Helvetii, upon the river Rhine, and stretches toward the
+north. The Belgae rises from the extreme frontier of Gaul, extend to the lower part of the river
+Rhine; and look toward the north and the rising sun. Aquitania extends from the river Garonne to
+the Pyrenaean mountains and to that part of the ocean which is near Spain: it looks between the
+setting of the sun, and the north star.`
+
+type fuzzyTest struct {
+	source string
+	target string
+	wanted bool
+	rank   int
+}
+
+var fuzzyTests = []fuzzyTest{
+	{"zazz", deBelloGallico + " zazz", true, 1544},
+	{"zazz", "zazz " + deBelloGallico, true, 1544},
+	{"twl", "cartwheel", true, 6},
+	{"cart", "cartwheel", true, 5},
+	{"cw", "cartwheel", true, 7},
+	{"ee", "cartwheel", true, 7},
+	{"art", "cartwheel", true, 6},
+	{"eeel", "cartwheel", false, -1},
+	{"dog", "cartwheel", false, -1},
+	{"ёлка", "ёлочка", true, 2},
+	{"ветер", "ёлочка", false, -1},
+	{"中国", "中华人民共和国", true, 5},
+	{"日本", "中华人民共和国", false, -1},
+	{"イ", "イカ", true, 1},
+	{"limón", "limon", false, -1},
+	{"kitten", "setting", false, -1},
+	{"\xffinvalid UTF-8\xff", "", false, -1}, // invalid UTF-8
+	{"Ⱦ", "", false, -1},                     // uppercase and lowercase runes have different UTF-8 encoding lengths
+}
+
+func TestFuzzyMatch(t *testing.T) {
+	for _, val := range fuzzyTests {
+		match := Match(val.source, val.target)
+		if match != val.wanted {
+			t.Errorf("%s in %s expected match to be %t, got %t",
+				val.source, val.target, val.wanted, match)
+		}
+	}
+}
+
+func TestFuzzyMatchFold(t *testing.T) {
+	for _, val := range fuzzyTests {
+		match := MatchFold(val.source, strings.ToUpper(val.target))
+		if match != val.wanted {
+			t.Errorf("%s in %s expected match to be %t, got %t",
+				val.source, strings.ToUpper(val.target), val.wanted, match)
+		}
+	}
+}
+
+func TestFuzzyMatchNormalized(t *testing.T) {
+	normalizedTests := []struct {
+		source string
+		target string
+		wanted bool
+	}{
+		{"limon", "limón", true},
+		{"limón", "limon tart", true},
+		{"limón", "LiMóN tArT", false},
+		{"limón", "LeMoN tArT", false},
+	}
+
+	for _, val := range normalizedTests {
+		match := MatchNormalized(val.source, val.target)
+		if match != val.wanted {
+			t.Errorf("%s in %s expected match to be %t, got %t",
+				val.source, val.target, val.wanted, match)
+		}
+	}
+}
+
+func TestFuzzyMatchNormalizedFold(t *testing.T) {
+	normalizedTests := []struct {
+		source string
+		target string
+		wanted bool
+	}{
+		{"limon", "limón", true},
+		{"limón", "limon tart", true},
+		{"limón", "LiMóN tArT", true},
+		{"limón", "LeMoN tArT", false},
+	}
+
+	for _, val := range normalizedTests {
+		match := MatchNormalizedFold(val.source, val.target)
+		if match != val.wanted {
+			t.Errorf("%s in %s expected match to be %t, got %t",
+				val.source, val.target, val.wanted, match)
+		}
+	}
+}
+
+func TestFuzzyFind(t *testing.T) {
+	target := []string{"cartwheel", "foobar", "wheel", "baz", "cartwhéél"}
+	wanted := []string{"cartwheel", "wheel"}
+
+	matches := Find("whel", target)
+
+	if len(matches) != len(wanted) {
+		t.Errorf("expected %s, got %s", wanted, matches)
+	}
+
+	for i := range wanted {
+		if wanted[i] != matches[i] {
+			t.Errorf("expected %s, got %s", wanted, matches)
+		}
+	}
+}
+
+func TestFuzzyFindNormalized(t *testing.T) {
+	target := []string{"cartwheel", "foobar", "wheel", "baz", "cartwhéél", "WHEEL"}
+	wanted := []string{"cartwheel", "wheel", "cartwhéél"}
+
+	matches := FindNormalized("whél", target)
+
+	if len(matches) != len(wanted) {
+		t.Errorf("expected %s, got %s", wanted, matches)
+	}
+
+	for i := range wanted {
+		if wanted[i] != matches[i] {
+			t.Errorf("expected %s, got %s", wanted, matches)
+		}
+	}
+}
+
+func TestFuzzyFindNormalizedFold(t *testing.T) {
+	target := []string{"cartwheel", "foobar", "wheel", "baz", "cartwhéél", "WHEEL"}
+	wanted := []string{"cartwheel", "wheel", "cartwhéél", "WHEEL"}
+
+	matches := FindNormalizedFold("whél", target)
+
+	if len(matches) != len(wanted) {
+		t.Errorf("expected %s, got %s", wanted, matches)
+	}
+
+	for i := range wanted {
+		if wanted[i] != matches[i] {
+			t.Errorf("expected %s, got %s", wanted, matches)
+		}
+	}
+}
+
+func TestRankMatch(t *testing.T) {
+	for _, val := range fuzzyTests {
+		rank := RankMatch(val.source, val.target)
+		if rank != val.rank {
+			t.Errorf("expected ranking %d, got %d for %s in %s",
+				val.rank, rank, val.source, val.target)
+		}
+	}
+}
+
+func TestRankMatchNormalized(t *testing.T) {
+	fuzzyTests := []struct {
+		source string
+		target string
+		rank   int
+	}{
+		{"limó", "limon", 1},
+		{"limó", "limon", 1},
+		{"limó", "LIMON", -1},
+	}
+
+	for _, val := range fuzzyTests {
+		rank := RankMatchNormalized(val.source, val.target)
+		if rank != val.rank {
+			t.Errorf("expected ranking %d, got %d for %s in %s",
+				val.rank, rank, val.source, val.target)
+		}
+	}
+}
+
+func TestRankMatchNormalizedFold(t *testing.T) {
+	fuzzyTests := []struct {
+		source string
+		target string
+		rank   int
+	}{
+		{"limó", "limon", 1},
+		{"limó", "limon", 1},
+		{"limó", "LIMON", 1},
+		{"limó", "LIMON TART", 6},
+	}
+
+	for _, val := range fuzzyTests {
+		rank := RankMatchNormalizedFold(val.source, val.target)
+		if rank != val.rank {
+			t.Errorf("expected ranking %d, got %d for %s in %s",
+				val.rank, rank, val.source, val.target)
+		}
+	}
+}
+
+func TestRankMatchNormalizedFoldConcurrent(t *testing.T) {
+	target := strings.Split("Lorem ipsum dolor sit amet, consectetur adipiscing elit", " ")
+	source := "ips"
+	procs := 10
+	iter := 10
+	type empty struct{}
+	done := make(chan empty)
+	for i := 0; i <= procs; i++ {
+		go func() {
+			for n := 0; n < iter; n++ {
+				_ = RankFindNormalizedFold(source, target)
+			}
+			done <- empty{}
+		}()
+	}
+	cnt := 0
+	for i := 0; i < procs; i++ {
+		<-done
+		cnt++
+	}
+}
+
+func TestRankFind(t *testing.T) {
+	target := []string{"cartwheel", "foobar", "wheel", "baz"}
+	wanted := []Rank{
+		{"whl", "cartwheel", 6, 0},
+		{"whl", "wheel", 2, 2},
+	}
+
+	ranks := RankFind("whl", target)
+
+	if len(ranks) != len(wanted) {
+		t.Errorf("expected %+v, got %+v", wanted, ranks)
+	}
+
+	for i := range wanted {
+		if wanted[i] != ranks[i] {
+			t.Errorf("expected %+v, got %+v", wanted, ranks)
+		}
+	}
+}
+
+func TestRankFindNormalized(t *testing.T) {
+	target := []string{"limón", "limon", "lemon", "LIMON"}
+	wanted := []Rank{
+		{"limó", "limón", 1, 0},
+		{"limó", "limon", 2, 1},
+	}
+
+	ranks := RankFindNormalized("limó", target)
+
+	if len(ranks) != len(wanted) {
+		t.Errorf("expected %+v, got %+v", wanted, ranks)
+	}
+
+	for i := range wanted {
+		if wanted[i] != ranks[i] {
+			t.Errorf("expected %+v, got %+v", wanted, ranks)
+		}
+	}
+}
+
+func TestRankFindNormalizedFold(t *testing.T) {
+	target := []string{"limón", "limon", "lemon", "LIMON"}
+	wanted := []Rank{
+		{"limó", "limón", 1, 0},
+		{"limó", "limon", 2, 1},
+		{"limó", "LIMON", 5, 3},
+	}
+
+	ranks := RankFindNormalizedFold("limó", target)
+
+	if len(ranks) != len(wanted) {
+		t.Errorf("expected %+v, got %+v", wanted, ranks)
+	}
+
+	for i := range wanted {
+		if wanted[i] != ranks[i] {
+			t.Errorf("expected %+v, got %+v", wanted, ranks)
+		}
+	}
+}
+
+func TestSortingRanks(t *testing.T) {
+	rs := Ranks{{"a", "b", 1, 0}, {"a", "cc", 2, 1}, {"a", "a", 0, 2}}
+	wanted := Ranks{rs[2], rs[0], rs[1]}
+
+	sort.Sort(rs)
+
+	for i := range wanted {
+		if wanted[i] != rs[i] {
+			t.Errorf("expected %+v, got %+v", wanted, rs)
+		}
+	}
+}
+
+func BenchmarkMatch(b *testing.B) {
+	ft := fuzzyTests[2]
+	for i := 0; i < b.N; i++ {
+		Match(ft.source, ft.target)
+	}
+}
+
+func BenchmarkMatchBigLate(b *testing.B) {
+	ft := fuzzyTests[0]
+	for i := 0; i < b.N; i++ {
+		Match(ft.source, ft.target)
+	}
+}
+
+func BenchmarkMatchBigEarly(b *testing.B) {
+	ft := fuzzyTests[1]
+	for i := 0; i < b.N; i++ {
+		Match(ft.source, ft.target)
+	}
+}
+
+func BenchmarkMatchFold(b *testing.B) {
+	ft := fuzzyTests[2]
+	for i := 0; i < b.N; i++ {
+		MatchFold(ft.source, ft.target)
+	}
+}
+
+func BenchmarkMatchFoldBigLate(b *testing.B) {
+	ft := fuzzyTests[0]
+	for i := 0; i < b.N; i++ {
+		MatchFold(ft.source, ft.target)
+	}
+}
+
+func BenchmarkMatchFoldBigEarly(b *testing.B) {
+	ft := fuzzyTests[1]
+	for i := 0; i < b.N; i++ {
+		MatchFold(ft.source, ft.target)
+	}
+}
+
+func BenchmarkFindFold(b *testing.B) {
+	b.Run("Plain", func(b *testing.B) { benchmarkFindFold(b, fuzzyTests[2]) })
+	b.Run("BigLate", func(b *testing.B) { benchmarkFindFold(b, fuzzyTests[0]) })
+	b.Run("BigEarly", func(b *testing.B) { benchmarkFindFold(b, fuzzyTests[1]) })
+}
+
+func benchmarkFindFold(b *testing.B, ft fuzzyTest) {
+	src := ft.source
+	var tgts []string
+	for i := 0; i < 128; i++ {
+		tgts = append(tgts, ft.target)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		FindFold(src, tgts)
+	}
+}
+
+func BenchmarkRankMatch(b *testing.B) {
+	ft := fuzzyTests[2]
+	for i := 0; i < b.N; i++ {
+		RankMatch(ft.source, ft.target)
+	}
+}
+
+func BenchmarkRankMatchBigLate(b *testing.B) {
+	ft := fuzzyTests[0]
+	for i := 0; i < b.N; i++ {
+		RankMatch(ft.source, ft.target)
+	}
+}
+
+func BenchmarkRankMatchBigEarly(b *testing.B) {
+	ft := fuzzyTests[1]
+	for i := 0; i < b.N; i++ {
+		RankMatch(ft.source, ft.target)
+	}
+}
+
+func ExampleMatch() {
+	fmt.Print(Match("twl", "cartwheel"))
+	// Output: true
+}
+
+func ExampleFind() {
+	fmt.Print(Find("whl", []string{"cartwheel", "foobar", "wheel", "baz"}))
+	// Output: [cartwheel wheel]
+}
+
+func ExampleRankMatch() {
+	fmt.Print(RankMatch("twl", "cartwheel"))
+	// Output: 6
+}
+
+func ExampleRankFind() {
+	fmt.Printf("%+v", RankFind("whl", []string{"cartwheel", "foobar", "wheel", "baz"}))
+	// Output: [{Source:whl Target:cartwheel Distance:6 OriginalIndex:0} {Source:whl Target:wheel Distance:2 OriginalIndex:2}]
+}
--- common/yak/yakurl/document.go
+++ common/yak/yakurl/document.go
@@ -2,12 +2,14 @@ package yakurl
 
 import (
 	"fmt"
+	"net/url"
 	"sort"
 	"strings"
 	"time"
 
+	"github.com/samber/lo"
 	"github.com/yaklang/yaklang/common/utils"
-	"github.com/yaklang/yaklang/common/utils/omap"
+	"github.com/yaklang/yaklang/common/utils/fuzzy"
 	"github.com/yaklang/yaklang/common/yak/yakdoc"
 	"github.com/yaklang/yaklang/common/yak/yakdoc/doc"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
@@ -16,117 +18,180 @@ import (
 type documentAction struct{}
 
 func (d *documentAction) Post(params *ypb.RequestYakURLParams) (*ypb.RequestYakURLResponse, error) {
-	//TODO implement me
+	// TODO implement me
 	return nil, utils.Error("not implemented")
 }
 
 func (d *documentAction) Put(params *ypb.RequestYakURLParams) (*ypb.RequestYakURLResponse, error) {
-	//TODO implement me
+	// TODO implement me
 	return nil, utils.Error("not implemented")
 }
 
 func (d *documentAction) Delete(params *ypb.RequestYakURLParams) (*ypb.RequestYakURLResponse, error) {
-	//TODO implement me
+	// TODO implement me
 	return nil, utils.Error("not implemented")
 }
 
 func (d *documentAction) Head(params *ypb.RequestYakURLParams) (*ypb.RequestYakURLResponse, error) {
-	//TODO implement me
+	// TODO implement me
 	return nil, utils.Error("not implemented")
 }
 
 func (d *documentAction) Do(params *ypb.RequestYakURLParams) (*ypb.RequestYakURLResponse, error) {
-	//TODO implement me
+	// TODO implement me
 	return nil, utils.Error("not implemented")
 }
 
 var (
-	yakDocumentExistedCoolDown = utils.NewCoolDown(10 * time.Second)
-	yakDocumentExisted         = omap.NewOrderedMap(make(map[string]*yakdoc.ScriptLib))
+	docMapOnce      = utils.NewCoolDown(10 * time.Second)
+	docMap          = make(map[string][]*ypb.YakURLResource)
+	fuzzySearchMap  = make(map[string]*ypb.YakURLResource)
+	fuzzySearchKeys = make([]string, 0)
+	maxFuzzyLength  = 64
 )
 
 func (d *documentAction) Get(params *ypb.RequestYakURLParams) (*ypb.RequestYakURLResponse, error) {
+	// available query:
+	// u.Path=xxx # search keyword
 	u := params.GetUrl()
 	if u == nil {
 		return nil, utils.Error("url is nil")
 	}
 
-	if yakDocumentExisted.Len() <= 0 {
-		yakDocumentExistedCoolDown.Do(func() {
-			result := doc.DefaultDocumentHelper
-			for _, i := range result.Libs {
-				yakDocumentExisted.Set(i.Name, i)
+	query := make(url.Values)
+	for _, v := range u.GetQuery() {
+		query.Add(v.GetKey(), v.GetValue())
+	}
+
+	if len(docMap) <= 0 {
+		docMapOnce.Do(func() {
+			// all lib
+			resources := make([]*ypb.YakURLResource, 0, len(doc.DefaultDocumentHelper.Libs))
+			keys := lo.Keys(doc.DefaultDocumentHelper.Libs)
+			sort.Strings(keys)
+			for _, key := range keys {
+				lib := doc.DefaultDocumentHelper.Libs[key]
+				resources = append(resources, &ypb.YakURLResource{
+					ResourceType:      "document",
+					VerboseType:       "Document",
+					ResourceName:      lib.Name,
+					VerboseName:       lib.Name,
+					Path:              "/",
+					Url:               &ypb.YakURL{Schema: "yakdocument", Location: lib.Name},
+					HaveChildrenNodes: true,
+				})
+			}
+			docMap[""] = resources
+
+			// each lib
+			for _, lib := range doc.DefaultDocumentHelper.Libs {
+				resources = generateResourceFromLib(lib)
+				sort.SliceStable(resources, func(i, j int) bool {
+					if resources[i].ResourceType != resources[j].ResourceType {
+						return resources[i].ResourceType < resources[j].ResourceType
+					}
+					return resources[i].ResourceName < resources[j].ResourceName
+				})
+
+				for _, rsc := range resources {
+					extrasStr := strings.Join(lo.Map(rsc.Extra, func(v *ypb.KVPair, i int) string {
+						return v.Value
+					}), "\n")
+					totalName := fmt.Sprintf("%s.%s", lib.Name, rsc.ResourceName)
+					docMap[totalName] = []*ypb.YakURLResource{rsc}
+
+					_ = extrasStr
+					fuzzyKey := fmt.Sprintf("%s.%s|%s", lib.Name, rsc.ResourceName, extrasStr)
+					cloned := cloneResource(rsc)
+					cloned.ResourceName = totalName
+					fuzzySearchMap[fuzzyKey] = cloned
+					fuzzySearchKeys = append(fuzzySearchKeys, fuzzyKey)
+				}
+				docMap[lib.Name] = resources
 			}
 		})
 	}
 
-	docMap := yakDocumentExisted
-	libName := strings.TrimSpace(u.GetLocation())
-	if libName == "" {
-		var rsc []*ypb.YakURLResource
-		docMap.ForEach(func(i string, v *yakdoc.ScriptLib) bool {
-			rsc = append(rsc, &ypb.YakURLResource{
-				ResourceType:      "document",
-				VerboseType:       "Document",
-				ResourceName:      v.Name,
-				VerboseName:       v.Name,
-				Path:              "/",
-				Url:               &ypb.YakURL{Schema: "yakdocument", Location: v.Name},
-				HaveChildrenNodes: true,
-			})
-			return true
-		})
-		sort.SliceStable(rsc, func(i, j int) bool {
-			return rsc[i].VerboseName < rsc[j].VerboseName
-		})
-		return &ypb.RequestYakURLResponse{Resources: rsc}, nil
-	} else {
-		result, ok := docMap.Get(libName)
-		if !ok {
-			return nil, utils.Errorf("lib[%v] is not existed", libName)
+	// exact word
+	word := u.GetLocation()
+	exactWord := strings.TrimSpace(word)
+	resources, ok := docMap[exactWord]
+	if !ok {
+		// fuzzy search
+		fuzzyResults := fuzzy.RankFind(word, fuzzySearchKeys)
+		sort.Sort(fuzzyResults)
+		maxLen := len(fuzzyResults)
+		if maxLen > maxFuzzyLength {
+			maxLen = maxFuzzyLength
 		}
-		return getResponseFromPath(result, u)
+		for i := 0; i < maxLen; i++ {
+			resources = append(resources, fuzzySearchMap[fuzzyResults[i].Target])
+		}
 	}
-	return &ypb.RequestYakURLResponse{}, nil
+	return &ypb.RequestYakURLResponse{Resources: resources}, nil
 }
 
-func getResponseFromPath(document *yakdoc.ScriptLib, u *ypb.YakURL) (*ypb.RequestYakURLResponse, error) {
-	ret := strings.ToLower(strings.Trim(u.GetPath(), "/"))
-	var rsc []*ypb.YakURLResource
-	for _, i := range document.Functions {
-		name := fmt.Sprintf("%v.%v", i.LibName, i.MethodName)
-		if ret != "" && !utils.IContains(i.MethodName, ret) {
-			continue
+func cloneResource(rsc *ypb.YakURLResource) *ypb.YakURLResource {
+	if rsc == nil {
+		return nil
+	}
+	// copy extras
+	extras := lo.Map(rsc.Extra, func(v *ypb.KVPair, i int) *ypb.KVPair {
+		return &ypb.KVPair{Key: v.Key, Value: v.Value}
+	})
+
+	res := &ypb.YakURLResource{
+		ResourceType:      rsc.ResourceType,
+		VerboseType:       rsc.VerboseType,
+		ResourceName:      rsc.ResourceName,
+		VerboseName:       rsc.VerboseName,
+		Path:              rsc.Path,
+		Url:               rsc.Url,
+		HaveChildrenNodes: rsc.HaveChildrenNodes,
+		Extra:             extras,
+	}
+	return res
+}
+
+func generateResourceFromLib(lib *yakdoc.ScriptLib) []*ypb.YakURLResource {
+	resources := make([]*ypb.YakURLResource, 0, len(lib.Functions)+len(lib.Instances))
+	for _, i := range lib.Functions {
+		name := i.MethodName
+		decl, doc := i.Decl, i.Document
+		if doc != "" {
+			doc = "\n\n" + strings.Replace(doc, "```\n", "```yak\n", 1)
 		}
-		rsc = append(rsc, &ypb.YakURLResource{
+		if decl != "" {
+			decl = "func " + decl
+		}
+		desc := fmt.Sprintf("```go\n%s\n```%s", decl, doc)
+		rsc := &ypb.YakURLResource{
 			ResourceType: "function",
 			VerboseType:  "Function",
 			ResourceName: name,
-			VerboseName:  name,
+			VerboseName:  fmt.Sprintf("%s.%s", lib.Name, name),
 			Path:         "/" + name,
-			Url:          &ypb.YakURL{Schema: "yakdocument", Location: document.Name, Path: name},
+			Url:          &ypb.YakURL{Schema: "yakdocument", Location: lib.Name, Path: name},
 			Extra: []*ypb.KVPair{
-				{Key: "Content", Value: i.String()},
+				{Key: "Content", Value: desc},
 			},
-		})
-	}
-	for _, i := range document.Instances {
-		name := fmt.Sprintf("%v.%v", i.LibName, i.InstanceName)
-		if ret != "" && !utils.IContains(i.InstanceName, ret) {
-			continue
 		}
+		resources = append(resources, rsc)
+	}
 
-		rsc = append(rsc, &ypb.YakURLResource{
+	for _, i := range lib.Instances {
+		name := i.InstanceName
+		resources = append(resources, &ypb.YakURLResource{
 			ResourceType: "variable",
 			VerboseType:  "variable",
 			ResourceName: name,
-			VerboseName:  "func " + name,
+			VerboseName:  fmt.Sprintf("variable %s.%s", lib.Name, name),
 			Path:         "/" + name,
-			Url:          &ypb.YakURL{Schema: "yakdocument", Location: document.Name, Path: name},
+			Url:          &ypb.YakURL{Schema: "yakdocument", Location: lib.Name, Path: name},
 		})
 	}
-	sort.SliceStable(rsc, func(i, j int) bool {
-		return rsc[i].VerboseName < rsc[j].VerboseName
+	sort.SliceStable(resources, func(i, j int) bool {
+		return resources[i].VerboseName < resources[j].VerboseName
 	})
-	return &ypb.RequestYakURLResponse{Resources: rsc}, nil
+	return resources
 }
