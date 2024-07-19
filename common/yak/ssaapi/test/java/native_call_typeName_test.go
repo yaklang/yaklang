1c62456858385bbca3b6ba35ea0c9a884eb1006b
intSheep <1098204193@qq.com>
2024-07-19 17:56:25 +0800

    feat(java2ssa):add get fullTypeName version message

--- common/yak/ssaapi/test/java/native_call_typeName_test.go
+++ common/yak/ssaapi/test/java/native_call_typeName_test.go
@@ -1,7 +1,10 @@
 package java
 
 import (
+	_ "embed"
 	"github.com/stretchr/testify/assert"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/syntaxflow/sfvm"
 	"github.com/yaklang/yaklang/common/yak/ssaapi"
 	"github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
 	"testing"
@@ -15,4 +18,26 @@ func TestNativeCallTypeName(t *testing.T) {
 		assert.Contains(t, typeName.String(), "javax.xml.parsers.DocumentBuilder")
 		return nil
 	}, ssaapi.WithLanguage(ssaapi.JAVA))
+}
+
+func TestSimpleNativeCallTypeName(t *testing.T) {
+	ssatest.Check(t, XXE_Code, func(prog *ssaapi.Program) error {
+		typeName := prog.SyntaxFlowChain(`documentBuilder<typeName> as $id;`)[0]
+		assert.Contains(t, typeName.String(), "DocumentBuilder")
+		typeName = prog.SyntaxFlowChain(`documentBuilder<fullTypeName> as $id;`)[0]
+		assert.Contains(t, typeName.String(), "javax.xml.parsers.DocumentBuilder")
+		return nil
+	}, ssaapi.WithLanguage(ssaapi.JAVA))
+}
+
+func TestNativeCallTypeNameWithSCA(t *testing.T) {
+	progs, err := ssaapi.ParseProjectFromPath("D:\\goProject\\syntaxflow-zero-to-hero\\java-realworld\\sample", ssaapi.WithLanguage(ssaapi.JAVA))
+	if err != nil {
+		return
+	}
+	prog := progs[0]
+	typeName := prog.SyntaxFlowChain("a<fullTypeName> as $id;", sfvm.WithEnableDebug(true))[0]
+	assert.Contains(t, typeName.String(), "com.alibaba.fastjson.JSON\n")
+
+	log.Infof("typeName fastjson: %s", typeName.String())
 }
