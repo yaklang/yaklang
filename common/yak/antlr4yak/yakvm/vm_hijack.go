fbfa87ecccac044ae7e3ae5f2513b40503418711
v1ll4n <v1ll4n@qq.com>
2024-07-24 10:18:07 +0800

    remove useless

--- common/yak/antlr4yak/yakvm/vm_hijack.go
+++ common/yak/antlr4yak/yakvm/vm_hijack.go
@@ -2,10 +2,6 @@ package yakvm
 
 import "github.com/yaklang/yaklang/common/utils"
 
-func (v *Frame) hijackMapMemberCall(caller, callee string, hijacker func(i interface{}) interface{}) {
-	v.hijackMapMemberCallHandlers.Store(utils.CalcSha1(caller, callee), hijacker)
-}
-
 func (v *Frame) execHijackMapMemberCallHandler(caller, callee string, origin interface{}) interface{} {
 	val, ok := v.hijackMapMemberCallHandlers.Load(utils.CalcSha1(caller, callee))
 	if !ok {
