cd9e32bf26af41d06c5775453dbda37b07131190
v1ll4n <v1ll4n@qq.com>
2024-07-31 11:55:24 +0800

    add servlet params as lib

---
+++ common/yak/ssaapi/test/syntaxflow/include_test.go
@@ -0,0 +1,143 @@
+package syntaxflow
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/yaklang/yaklang/common/utils/filesys"
+	"github.com/yaklang/yaklang/common/yak/ssaapi"
+	"github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
+	"testing"
+)
+
+func TestLib_ServletParam(t *testing.T) {
+
+	vfs := filesys.NewVirtualFs()
+	vfs.AddFile("A.java", `package net.javaguides.usermanagement.web;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.javaguides.usermanagement.dao.UserDAO;
+import net.javaguides.usermanagement.model.User;
+
+/**
+ * ControllerServlet.java
+ * This servlet acts as a page controller for the application, handling all
+ * requests from the user.
+ * @email Ramesh Fadatare
+ */
+
+@WebServlet("/")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserDAO userDAO;
+	
+	public void init() {
+		userDAO = new UserDAO();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+
+		try {
+			switch (action) {
+			case "/new":
+				showNewForm(request, response);
+				break;
+			case "/insert":
+				insertUser(request, response);
+				break;
+			case "/delete":
+				deleteUser(request, response);
+				break;
+			case "/edit":
+				showEditForm(request, response);
+				break;
+			case "/update":
+				updateUser(request, response);
+				break;
+			default:
+				listUser(request, response);
+				break;
+			}
+		} catch (SQLException ex) {
+			throw new ServletException(ex);
+		}
+	}
+
+	private void listUser(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+		List<User> listUser = userDAO.selectAllUsers();
+		request.setAttribute("listUser", listUser);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-list.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		User existingUser = userDAO.selectUser(id);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+		request.setAttribute("user", existingUser);
+		dispatcher.forward(request, response);
+
+	}
+
+	private void insertUser(HttpServletRequest request, HttpServletResponse response) 
+			throws SQLException, IOException {
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String country = request.getParameter("country");
+		User newUser = new User(name, email, country);
+		userDAO.insertUser(newUser);
+		response.sendRedirect("list");
+	}
+
+	private void updateUser(HttpServletRequest request, HttpServletResponse response) 
+			throws SQLException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String country = request.getParameter("country");
+
+		User book = new User(id, name, email, country);
+		userDAO.updateUser(book);
+		response.sendRedirect("list");
+	}
+
+	private void deleteUser(HttpServletRequest request, HttpServletResponse response) 
+			throws SQLException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		userDAO.deleteUser(id);
+		response.sendRedirect("list");
+
+	}
+
+}`)
+	ssatest.CheckWithFS(vfs, t, func(programs ssaapi.Programs) error {
+		prog := programs[0]
+		results := prog.SyntaxFlowChain(`<include('servlet-param')> as $params`)
+		results.Show()
+		assert.Greater(t, len(results), 7)
+		return nil
+	}, ssaapi.WithLanguage(ssaapi.JAVA))
+}
