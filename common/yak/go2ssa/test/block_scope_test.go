e30450849a081e4d5195efeda8708b15ad306699
YWHKKX <2076064543@qq.com>
2024-08-02 14:21:23 +0800

    add switch condition baiscblock

--- common/yak/go2ssa/test/block_scope_test.go
+++ common/yak/go2ssa/test/block_scope_test.go
@@ -7,7 +7,6 @@ import (
 )
 
 func TestBlock_Value(t *testing.T) {
-
 	t.Run("if stmt;exp", func(t *testing.T) {
 		test.CheckPrintlnValue(`package main
 		func main(){
@@ -43,20 +42,67 @@ func TestBlock_Value(t *testing.T) {
 		}, t)
 	})
 
-	// TODO
-	/*t.Run("switch stmt;exp", func(t *testing.T) {
+	t.Run("if-else stmt;exp", func(t *testing.T) {
+		test.CheckPrintlnValue(`package main
+
+		func main(){
+			if i:=1; i==1 {
+				println(i) // 1
+			}else if a:=2; a==2{ 
+				println(i) // 1
+			}else{
+				println(a) // 2
+				println(i) // 1
+			}
+		}
+		`, []string{
+			"1","1","2","1",
+		}, t)
+	})
+
+	t.Run("if-else stmt;exp EX", func(t *testing.T) {
+		test.CheckPrintlnValue(`package main
+
+		func main(){
+			if i:=1; i==1 {
+				println(i)
+			}else if i:=2; i==2{ 
+				println(i)
+			}else{
+				println(i)
+			}
+		}
+		`, []string{
+			"1","2","2",
+		}, t)
+	})
+
+	t.Run("switch stmt;exp", func(t *testing.T) {
 		test.CheckPrintlnValue( `package main
 		
 		func main(){
 			switch a := 2; a {
 			default:
 				println(a) // 2
-				a = 3
 			}
 			println(a) // Undefined-a
 		}
 		`, []string{"2","Undefined-a"}, t)
-	})*/
+	})
+
+	t.Run("switch stmt;exp EX", func(t *testing.T) {
+		test.CheckPrintlnValue( `package main
+		
+		func main(){
+			a := 1
+			switch a := 2; a {
+			default:
+				println(a) // 2
+			}
+			println(a) // 1
+		}
+		`, []string{"2","1"}, t)
+	})
 
 	t.Run("for stmt;exp;stmt", func(t *testing.T) {
 		test.CheckPrintlnValue( `package main
