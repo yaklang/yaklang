a66750ae841bd6fec8e3f1239683721a2aee5a8a
go0p <gp827782797@qq.com>
2024-08-05 22:30:27 +0800

    add domain && fromPing && doc

--- common/synscanx/filter.go
+++ common/synscanx/filter.go
@@ -1,13 +1,15 @@
 package synscanx
 
 import (
+	"github.com/yaklang/yaklang/common/netx"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/hostsparser"
 	"strconv"
 	"strings"
+	"time"
 )
 
-func (s *Scannerx) nonExcludedHost(host string) bool {
+func (s *Scannerx) excludedHost(host string) bool {
 	if s.config.excludeHosts != nil {
 		if s.config.excludeHosts.Contains(host) {
 			return true
@@ -17,7 +19,7 @@ func (s *Scannerx) nonExcludedHost(host string) bool {
 	return false
 }
 
-func (s *Scannerx) nonExcludedPort(port int) bool {
+func (s *Scannerx) excludedPort(port int) bool {
 	if s.config.excludePorts != nil {
 		if s.config.excludePorts.Contains(port) {
 			return true
@@ -31,7 +33,7 @@ func (s *Scannerx) GetNonExcludedPorts(ports string) []int {
 	var nonExcludedPorts []int
 	var fp string
 	for _, port := range utils.ParseStringToPorts(ports) {
-		if s.nonExcludedPort(port) {
+		if s.excludedPort(port) {
 			continue
 		}
 		fp += strconv.Itoa(port) + ","
@@ -48,10 +50,22 @@ func (s *Scannerx) GetNonExcludedHosts(targets string) []string {
 	var nonExcludedHosts []string
 
 	for _, host := range utils.ParseStringToHosts(targets) {
-		if s.nonExcludedHost(host) {
+		if !utils.IsIPv4(host) && !utils.IsIPv6(host) {
+			for _, _host := range netx.LookupAll(host, netx.WithTimeout(3*time.Second)) {
+				if s.excludedHost(_host) {
+					continue
+				}
+				if utils.IsIPv4(_host) || utils.IsIPv6(_host) {
+					nonExcludedHosts = append(nonExcludedHosts, _host)
+				}
+			}
+		}
+		if s.excludedHost(host) {
 			continue
 		}
-		nonExcludedHosts = append(nonExcludedHosts, host)
+		if utils.IsIPv4(host) || utils.IsIPv6(host) {
+			nonExcludedHosts = append(nonExcludedHosts, host)
+		}
 	}
 	s.hosts = hostsparser.NewHostsParser(s.ctx, strings.Join(nonExcludedHosts, ","))
 	if s.config.shuffle {
--- common/yak/yaklib/tools/synscanx.go
+++ common/yak/yaklib/tools/synscanx.go
@@ -7,10 +7,26 @@ import (
 	"github.com/yaklang/yaklang/common/synscanx"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/pcapfix"
+	"github.com/yaklang/yaklang/common/utils/pingutil"
 	"sync"
 	"time"
 )
 
+// Scan 使用 SYN 扫描技术进行端口扫描，它不必打开一个完整的TCP连接，只发送一个SYN包，就能做到打开连接的效果，然后等待对端的反应
+// @param {string} target 目标地址，支持 CIDR 格式
+// @param {string} port 端口，支持 1-65535、1,2,3、1-100,200-300 格式
+// @param {scanOpt} [opts] synscan 扫描参数
+// @return {chan *synscan.SynScanResult} 返回结果
+// Example:
+// ```
+// res, err := synscan.Scan("127.0.0.1", "1-65535") //
+// die(err)
+//
+//	for result := range res {
+//	  result.Show()
+//	}
+//
+// ```
 func _scanx(targets string, ports string, opts ...synscanx.SynxConfigOption) (chan *synscan.SynScanResult, error) {
 	config := synscanx.NewDefaultConfig()
 
@@ -21,71 +37,88 @@ func _scanx(targets string, ports string, opts ...synscanx.SynxConfigOption) (ch
 	return do(targets, ports, config)
 }
 
-//func _scanxFromPingUtils(res chan *pingutil.PingResult, ports string, opts ...synscanx.SynxConfigOption) (chan *synscan.SynScanResult, error) {
-//	config := synscanx.NewDefaultConfig()
+// ScanFromPing 对使用 ping.Scan 探测出的存活结果进行端口扫描，需要配合 ping.Scan 使用
+// @param {chan *PingResult} res ping.Scan 的扫描结果
+// @param {string} ports 端口，支持 1-65535、1,2,3、1-100,200-300 格式
+// @param {scanOpt} [opts] synscan 扫描参数
+// @return {chan *synscan.SynScanResult} 返回结果
+// Example:
+// ```
+// pingResult, err = ping.Scan("192.168.1.1/24") // 先进行存活探测
+// die(err)
+// res, err = synscan.ScanFromPing(pingResult, "1-65535") // 对存活结果进行端口扫描
+// die(err)
 //
-//	for _, opt := range opts {
-//		opt(config)
+//	for r := range res {
+//	  r.Show()
 //	}
 //
-//	return doFromPingUtils(pingutilsToChan(res), ports, config)
-//
-//}
+// ```
+func _scanxFromPingUtils(res chan *pingutil.PingResult, ports string, opts ...synscanx.SynxConfigOption) (chan *synscan.SynScanResult, error) {
+	config := synscanx.NewDefaultConfig()
 
-//func doFromPingUtils(res chan string, ports string, config *synscanx.SynxConfig) (chan *synscan.SynScanResult, error) {
-//	var sample string
-//	select {
-//	case pingResult := <-res:
-//		sample = pingResult
-//		go func() {
-//			res <- pingResult
-//		}()
-//	case <-time.After(15 * time.Second):
-//		return nil, utils.Errorf("ping timeout")
-//	}
-//	ctx, cancel := context.WithCancel(context.Background())
-//	scanner, err := synscanx.NewScannerx(ctx, sample, config)
-//	if err != nil {
-//		cancel()
-//		return nil, err
-//	}
-//	scanner.FromPing = true
-//	sendDoneSignal := make(chan struct{})
-//
-//	targetCh := make(chan *synscanx.SynxTarget, 16)
-//	resultCh := make(chan *synscan.SynScanResult, 1024)
-//
-//	var wg sync.WaitGroup
-//	wg.Add(2)
-//
-//	go func() {
-//		defer wg.Done()
-//		scanner.SubmitTargetFromPing(res, ports, targetCh)
-//		close(targetCh)
-//		<-sendDoneSignal
-//		close(resultCh)
-//		log.Infof("send done signal")
-//	}()
-//
-//	time.Sleep(100 * time.Millisecond)
-//
-//	go func() {
-//		defer wg.Done()
-//
-//		err := scanner.Scan(sendDoneSignal, targetCh, resultCh)
-//		if err != nil {
-//			close(resultCh)
-//			log.Errorf("scan failed: %s", err)
-//		}
-//	}()
-//
-//	go func() {
-//		wg.Wait()
-//		cancel()
-//	}()
-//
-//	return resultCh, nil
-//}
+	for _, opt := range opts {
+		opt(config)
+	}
+
+	return doFromPingUtils(pingutilsToChan(res), ports, config)
+
+}
+
+func doFromPingUtils(res chan string, ports string, config *synscanx.SynxConfig) (chan *synscan.SynScanResult, error) {
+	var sample string
+	select {
+	case pingResult := <-res:
+		sample = pingResult
+		go func() {
+			res <- pingResult
+		}()
+	case <-time.After(15 * time.Second):
+		return nil, utils.Errorf("ping timeout")
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	scanner, err := synscanx.NewScannerx(ctx, sample, config)
+	if err != nil {
+		cancel()
+		return nil, err
+	}
+	scanner.FromPing = true
+	sendDoneSignal := make(chan struct{})
+
+	targetCh := make(chan *synscanx.SynxTarget, 16)
+	resultCh := make(chan *synscan.SynScanResult, 1024)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		scanner.SubmitTargetFromPing(res, ports, targetCh)
+		close(targetCh)
+		<-sendDoneSignal
+		close(resultCh)
+		log.Infof("send done signal")
+	}()
+
+	time.Sleep(100 * time.Millisecond)
+
+	go func() {
+		defer wg.Done()
+
+		err := scanner.Scan(sendDoneSignal, targetCh, resultCh)
+		if err != nil {
+			close(resultCh)
+			log.Errorf("scan failed: %s", err)
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		cancel()
+	}()
+
+	return resultCh, nil
+}
 
 func do(targets, ports string, config *synscanx.SynxConfig) (chan *synscan.SynScanResult, error) {
 	ctx, cancel := context.WithCancel(context.Background())
@@ -136,8 +169,8 @@ func do(targets, ports string, config *synscanx.SynxConfig) (chan *synscan.SynSc
 var SynxPortScanExports = map[string]interface{}{
 	"FixPermission": pcapfix.Fix,
 
-	"Scan": _scanx,
-	//"ScanFromPing": _scanxFromPingUtils,
+	"Scan":         _scanx,
+	"ScanFromPing": _scanxFromPingUtils,
 
 	"callback":           synscanx.WithCallback,
 	"submitTaskCallback": synscanx.WithSubmitTaskCallback,
