c967da3708fe786b2ccf96ed61a7c7701335196d
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-08-06 10:46:39 +0800

    test(dnslog): fix test

--- common/yak/yaklib/dnslog_test.go
+++ common/yak/yaklib/dnslog_test.go
@@ -1,15 +1,22 @@
-package yaklib
+package yaklib_test
 
 import (
+	"testing"
+
 	"github.com/davecgh/go-spew/spew"
 	"github.com/yaklang/yaklang/common/consts"
 	"github.com/yaklang/yaklang/common/netx"
-	"testing"
+	"github.com/yaklang/yaklang/common/yak"
+	"github.com/yaklang/yaklang/common/yak/yaklib"
 )
+
+func init() {
+	yaklib.SetEngineInterface(yak.NewScriptEngine(1000))
+}
 
 func TestNewCustomDNSLog(t *testing.T) {
 	consts.GetGormProjectDatabase()
-	cd := NewCustomDNSLog(setScript("Goby DnsLog"))
+	cd := yaklib.NewCustomDNSLog(yaklib.WithDNSLog_SetScript("Goby DnsLog"))
 
 	domain, _, err := cd.GetSubDomainAndToken()
 	if err != nil {
