60b3e07a8e68540c563415ad7d91c1d87b09eea9
VillanCh <188228725@qq.com>
2024-08-06 23:50:41 +0800

    export group matcher

--- common/yak/yaktest/mustpass/z_debug_test.go
+++ common/yak/yaktest/mustpass/z_debug_test.go
@@ -17,7 +17,7 @@ func TestMustPassDebug(t *testing.T) {
 
 	yakit.RegisterLowHTTPSaveCallback()
 
-	debugName := "fuzz_param_array.yak"
+	debugName := "suricata_match_group.yak"
 	var debugCases [][]string
 	for k, v := range files {
 		if k == debugName {
