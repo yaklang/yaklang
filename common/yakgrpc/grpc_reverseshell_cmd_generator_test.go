0ecc9ab64a0ebb6d4927055cf2fb527759327aeb
rookie <rookie_is@qq.com>
2024-08-07 16:43:26 +0800

    fix(GenerateReverseShellCommand):fix test case

--- common/yakgrpc/grpc_reverseshell_cmd_generator_test.go
+++ common/yakgrpc/grpc_reverseshell_cmd_generator_test.go
@@ -46,6 +46,6 @@ func TestServer_GenerateReverseShellCommand(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	assert.Equal(t, "Bash -i,Bash 196,Bash read line,Bash 5,Bash udp,nc mkfifo,nc -e,BusyBox nc -e,nc -c,ncat -e,ncat udp,curl,rustcat,PHP cmd 2,PHP cmd small,socat #1,socat #2 (TTY),sqlite3 nc mkfifo,node.js,telnet,zsh", strings.Join(programListRes.GetProgramList(), ","))
+	assert.Equal(t, "Bash -i,Bash 196,Bash read line,Bash 5,Bash udp,nc mkfifo,nc -e,BusyBox nc -e,nc -c,ncat -e,ncat udp,curl,rustcat,C,C# TCP Client,C# Bash -i,Haskell #1,Perl,Perl no sh,Perl PentestMonkey,PHP PentestMonkey,PHP Ivan Sincek,PHP cmd,PHP cmd 2,PHP cmd small,PHP exec,PHP shell_exec,PHP system,PHP passthru,PHP `,PHP popen,PHP proc_open,Python #1,Python #2,Python3 #1,Python3 #2,Python3 shortest,Ruby #1,Ruby no sh,socat #1,socat #2 (TTY),sqlite3 nc mkfifo,node.js,node.js #2,Java #1,Java #2,Java #3,Java Web,Java Two Way,Javascript,telnet,zsh,Lua #1,Lua #2,Golang,Vlang,Awk,Dart,Crystal (system),Crystal (code)", strings.Join(programListRes.GetProgramList(), ","))
 	assert.Equal(t, "sh,/bin/sh,bash,/bin/bash,cmd,powershell,pwsh,ash,bsh,csh,ksh,zsh,pdksh,tcsh,mksh,dash", strings.Join(programListRes.GetShellList(), ","))
 }
