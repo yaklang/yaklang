1017c3d86034032f18c9ade9eb8323b5b98b6856
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-08-10 09:34:00 +0800

    test(ssa): remove useless defer test

--- common/yak/yak2ssa/test/error_test.go
+++ common/yak/yak2ssa/test/error_test.go
@@ -1544,17 +1544,3 @@ func TestParameterMember(t *testing.T) {
 		})
 	})
 }
-
-func TestDefer(t *testing.T) {
-	t.Run("test unexpected expression", func(t *testing.T) {
-		test.CheckError(t, test.TestCase{
-			Code: ` 
-			a = 1
-			defer a
-		`,
-			Want: []string{
-				yak2ssa.UnexpectedDeferStmt(),
-			},
-		})
-	})
-}
