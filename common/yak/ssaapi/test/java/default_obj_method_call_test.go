ff6a6114be5d834d216d433eaad4e739947f7471
intSheep <1098204193@qq.com>
2024-08-12 15:57:17 +0800

    fix(ssa):fix  memberCall method default  type

--- common/yak/ssaapi/test/java/default_obj_method_call_test.go
+++ common/yak/ssaapi/test/java/default_obj_method_call_test.go
@@ -364,3 +364,44 @@ public class B{
 		return nil
 	}, ssaapi.WithLanguage(consts.JAVA))
 }
+
+
+func TestDefaultObj_XX(t *testing.T) {
+	code :=`package com.example.filedownload;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+
+@RestController
+public class FileDownloadController {
+
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<FileSystemResource> downloadFile(@PathVariable String filename) {
+        File file = new File("path/to/your/files/" + filename);
+
+        if (!file.exists()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+		HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+
+}
+     
+}`
+	ssatest.Check(t, code, func(prog *ssaapi.Program) error {
+		prog.Show()
+		target:= prog.SyntaxFlowChain(`file.getName(* as $param)`).Show()
+		assert.Equal(t,1, target.Len())
+
+		target= prog.SyntaxFlowChain(`headers.add(* as $param)`).Show()
+		assert.Equal(t,3, target.Len())
+		return nil
+	}, ssaapi.WithLanguage(consts.JAVA))	
+}
