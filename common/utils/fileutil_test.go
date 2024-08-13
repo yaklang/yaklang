d70017c3db017546ccfabe955d49e370f9239071
wlz <lingze.wl@gmail.com>
2024-08-13 18:03:53 +0800

    fix(consts):delete dir only in yak-home dir

--- common/utils/fileutil_test.go
+++ common/utils/fileutil_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/filesys"
@@ -75,4 +76,54 @@ func TestCopyDirectory(t *testing.T) {
 			return utils.ConcurrentCopyDirectory(src, dst, 10, false)
 		})
 	})
+}
+
+func TestIsSubPath(t *testing.T) {
+	testCases := []struct {
+		name     string
+		sub      string
+		parent   string
+		expected bool
+	}{
+		{"SubPath is a direct child",
+			"/a/b/c", "/a/b", true},
+		{"SubPath is the same as parent",
+			"/a/b/c", "/a/b/c", false},
+		{"SubPath is not a child",
+			"/a/b/c", "/a/b/c/d", false},
+		{"Parent path with ..",
+			"/a/b/c", "/a/b/..", true},
+		{"Parent path with .. and same sub",
+			"/a/b/c", "/a/b/../b", true},
+		{"Parent path with .. and same sub with child",
+			"/a/b/c", "/a/b/../b/c", false},
+		{"Parent path with .. and different sub",
+			"/a/b/c", "/a/b/../d", false},
+		{"Parent path with .. and different sub with child",
+			"/a/b/c", "/a/b/../d/e", false},
+		{"Relative path sub is a direct child",
+			"./a/b/c", "./a/b", true},
+		{"Relative path sub is the same as parent",
+			"./a/b/c", "./a/b/c", false},
+		{"Relative path sub is not a child",
+			"./a/b/c", "./a/b/c/d", false},
+		{"Relative path parent with ..",
+			"./a/b/c", "./a/b/..", true},
+		{"Relative path parent with .. and same sub",
+			"./a/b/c", "./a/b/../b", true},
+		{"Relative path parent with .. and same sub with child",
+			"./a/b/c", "./a/b/../b/c", false},
+		{"Relative path parent with .. and different sub",
+			"./a/b/c", "./a/b/../d", false},
+		{"Relative path parent with .. and different sub with child",
+			"./a/b/c", "./a/b/../d/e", false},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Logf("Testing sub: %s, parent: %s, expected: %v", tc.sub, tc.parent, tc.expected)
+			result := utils.IsSubPath(tc.sub, tc.parent)
+			assert.Equal(t, tc.expected, result)
+		})
+	}
 }
