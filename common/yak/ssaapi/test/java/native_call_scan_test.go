eea4c52ad00b75ccd2ed18057c0c15c0fe52811f
intSheep <1098204193@qq.com>
2024-08-14 18:02:40 +0800

    test(ssaapi):add scan test with param

--- common/yak/ssaapi/test/java/native_call_scan_test.go
+++ common/yak/ssaapi/test/java/native_call_scan_test.go
@@ -9,24 +9,59 @@ import (
 	"github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
 )
 
-func TestScanPreviousSimple(t *testing.T) {
+func TestScanSimple(t *testing.T) {
 	code := `package com.example.A;
 	public class A {
 		public static void main(String[] args) {
-			a = 1+1;
+			bb1;
+			a = 1;
 			if (c){
-				a = 2+2;
+				a = 2;
 			}else {
-				a = 3 + 3;
+				a = 3;
 			}
-			bb;
+			bb2;
 		}
 	}
 	`
 	ssatest.Check(t, code, func(prog *ssaapi.Program) error {
 		prog.Show()
-		result := prog.SyntaxFlowChain("bb<scanPrevious> as $result",sfvm.WithEnableDebug(true)).Show()
-		assert.Equal(t, 5, result.Len())
+		result := prog.SyntaxFlowChain("bb2<scanPrevious> as $result").Show(false)
+		assert.Equal(t, 6, result.Len())
+		result = prog.SyntaxFlowChain("bb1<scanNext> as $result").Show(false)
+		assert.Equal(t, 6, result.Len())
+		return nil
+	}, ssaapi.WithLanguage(ssaapi.JAVA))
+}
+
+func TestScanPreviousWithParam(t *testing.T) {
+	code := `package com.example.A;
+	public class A {
+		public static void main(String[] args) {
+			bb1;
+			a = 1;
+			if (c){
+				a = 2;
+			}else {
+				a = 3;
+			}
+			bb2;
+		}
+	}
+	`
+	
+
+
+	ssatest.Check(t, code, func(prog *ssaapi.Program) error {
+		prog.Show()
+		result := prog.SyntaxFlowChain("bb2<scanPrevious(until=`*?{opcode: const}`)> as $result").Show(false)
+		assert.Equal(t, 2, result.Len())
+
+		result = prog.SyntaxFlowChain("bb2<scanPrevious(hook=`*?{opcode: const}`)> as $result").Show(false)
+		assert.Equal(t, 3, result.Len())
+
+		result = prog.SyntaxFlowChain("bb2<scanPrevious(exclude=`*?{opcode: const}`)> as $result",sfvm.WithEnableDebug(true)).Show(true)
+		assert.Equal(t, 3, result.Len())
 		return nil
 	}, ssaapi.WithLanguage(ssaapi.JAVA))
 }
