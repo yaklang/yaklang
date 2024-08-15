f986d66d18c9b8bce8a746f76e472aad2c814649
intSheep <1098204193@qq.com>
2024-08-15 17:18:50 +0800

    feat(ssaapi):add scan previoud and scan next

--- common/yak/ssaapi/sf_native_call_scan.go
+++ common/yak/ssaapi/sf_native_call_scan.go
@@ -11,13 +11,12 @@ import (
 
 type basicBlockap map[int64]struct{}
 
-type direction int
+type direction string
 
 const (
-    Previous direction = iota 
-    Next                      
+	Previous direction = "Previous"
+	Next	direction="Next"
 )
-
 
 var nativeCallScanPrevious = func(v sfvm.ValueOperator, frame *sfvm.SFFrame, params *sfvm.NativeCallActualParams) (bool, sfvm.ValueOperator, error) {
 	var vals []sfvm.ValueOperator
@@ -32,7 +31,7 @@ var nativeCallScanPrevious = func(v sfvm.ValueOperator, frame *sfvm.SFFrame, par
 		if !ok {
 			return nil
 		}
-		results := searchAlongBasicBlock(val.node,prog,frame,params, basicBlockMap,Previous)
+		results, _ := searchAlongBasicBlock(val.node, prog, frame, params, basicBlockMap, Previous)
 		if !ok {
 			return nil
 		}
@@ -55,7 +54,7 @@ var nativeCallScanNext = func(v sfvm.ValueOperator, frame *sfvm.SFFrame, params 
 		if !ok {
 			return nil
 		}
-		results := searchAlongBasicBlock(val.node, prog,frame,params,basicBlockMap,Next)
+		results, _ := searchAlongBasicBlock(val.node, prog, frame, params, basicBlockMap, Next)
 		if !ok {
 			return nil
 		}
@@ -65,9 +64,16 @@ var nativeCallScanNext = func(v sfvm.ValueOperator, frame *sfvm.SFFrame, params 
 	return true, sfvm.NewValues(vals), nil
 }
 
-func searchAlongBasicBlock(value ssa.Value, prog *Program,frame *sfvm.SFFrame,params *sfvm.NativeCallActualParams ,basicBlockMap basicBlockap,direction direction) []sfvm.ValueOperator {
+func searchAlongBasicBlock(
+		value ssa.Value,
+		prog *Program,
+		frame *sfvm.SFFrame,
+		params *sfvm.NativeCallActualParams,
+		basicBlockMap basicBlockap,
+		direction direction,
+	) ([]sfvm.ValueOperator, bool) {
 	if value == nil {
-		return nil
+		return nil, true
 	}
 
 	var results []sfvm.ValueOperator
@@ -76,72 +82,93 @@ func searchAlongBasicBlock(value ssa.Value, prog *Program,frame *sfvm.SFFrame,pa
 		block = value.GetBlock()
 	}
 	if block == nil {
-		return nil
+		return nil, true
 	}
 
 	if _, ok := basicBlockMap[block.GetId()]; ok {
-		return nil
+		return nil, true
 	}
 	basicBlockMap[block.GetId()] = struct{}{}
 
-	
 	for _, inst := range block.Insts {
-		if lz,ok := ssa.ToLazyInstruction(inst); ok {
+		if lz, ok := ssa.ToLazyInstruction(inst); ok {
 			inst = lz.Self()
 		}
 		if v, ok := ssa.ToValue(inst); ok {
 			token := utils.RandStringBytes(16)
 			token = "a" + token
-
-			if sfRule:=params.GetString("until");sfRule!=""{
-				sfRule =fmt.Sprintf("%s as $%s;",sfRule,token)
+			var sfRule string
+			if rule := params.GetString("until"); rule != "" {
+				sfRule = fmt.Sprintf("%s as $%s;", rule, token)
 				val := prog.NewValue(v)
-				res, err :=SyntaxFlowWithError(val,sfRule)
+				res, err :=SyntaxFlowWithError(val, sfRule,sfvm.WithEnableDebug(true))
 				if err != nil {
 					log.Warnf("scanPrevious/scanNext error: %v", err)
 				}
-				results = append(results, res.GetAllValuesChain())
-				if len(res.GetAllValuesChain()) > 0 {
-					return results
+				allValues := res.GetAllValuesChain()
+				for  _,val := range allValues{
+					_=val.AppendPredecessor(val,frame.WithPredecessorContext(fmt.Sprintf("scan%s",direction)))
+				}
+				results = append(results, allValues)
+				if len(allValues) > 0 {
+					return results, false
 				}
 			}
-			if sfRule := params.GetString("hook");sfRule!=""{
-				sfRule =fmt.Sprintf("%s as $%s;",sfRule,token)
+			if rule := params.GetString("hook"); rule != "" {
+				sfRule = fmt.Sprintf("%s as $%s;", rule, token)
 				val := prog.NewValue(v)
-				res, err :=SyntaxFlowWithError(val,sfRule)
+				res, err := SyntaxFlowWithError(val, sfRule)
 				if err != nil {
 					log.Warnf("scanPrevious/scanNext error: %v", err)
 				}
-				results = append(results, res.GetAllValuesChain())
+				allValues := res.GetAllValuesChain()
+				for  _,val := range allValues{
+					_=val.AppendPredecessor(val,frame.WithPredecessorContext(fmt.Sprintf("scan%s",direction)))
+				}
+				results = append(results, allValues)
 			}
-			if sfRule := params.GetString("exclude");sfRule!=""{
-				sfRule =fmt.Sprintf("%s as $%s;",sfRule,token)
+			if rule := params.GetString("exclude"); rule != "" {
+				sfRule = fmt.Sprintf("%s as $%s;", rule, token)
 				val := prog.NewValue(v)
-				res, err :=SyntaxFlowWithError(val,sfRule,sfvm.WithEnableDebug(true))
+				res, err := SyntaxFlowWithError(val, sfRule)
 				if err != nil {
 					log.Warnf("scanPrevious/scanNext error: %v", err)
 				}
-				if len(res.GetAllValuesChain()) == 0 {
-					results = append(results,val)
+				allValues := res.GetAllValuesChain()
+				for  _,val := range allValues{
+					_=val.AppendPredecessor(val,frame.WithPredecessorContext(fmt.Sprintf("scan%s",direction)))
 				}
+				if len(allValues) == 0 {
+					results = append(results, val)
+				}
+			}
+			if sfRule == "" {
+				val := prog.NewValue(v)
+				results = append(results, val)
 			}
 		}
 	}
 
 	// 向前寻找
-	if direction == Previous{
+	if direction == Previous {
 		for _, pred := range block.Preds {
-			res := searchAlongBasicBlock(pred, prog, frame,params,basicBlockMap,direction)
+			res, needContinue := searchAlongBasicBlock(pred, prog, frame, params, basicBlockMap, direction)
 			results = append(results, res...)
+			if !needContinue {
+				break
+			}
 		}
 	}
 	// 向后寻找
-	if direction ==Next{
-		for _,next := range block.Succs{
-			res := searchAlongBasicBlock(next, prog,frame, params,basicBlockMap,direction)
+	if direction == Next {
+		for _, next := range block.Succs {
+			res, isContinue := searchAlongBasicBlock(next, prog, frame, params, basicBlockMap, direction)
 			results = append(results, res...)
+			if !isContinue {
+				break
+			}
 		}
 	}
-	
-	return results
+
+	return results, true
 }
