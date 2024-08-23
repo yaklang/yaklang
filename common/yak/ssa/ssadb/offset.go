f273ffcbcec7a7acc31f02f1e9ba3199ecec8fa3
intSheep <1098204193@qq.com>
2024-08-23 16:38:32 +0800

    feat(ssadb):add save value and variable offset

---
+++ common/yak/ssa/ssadb/offset.go
@@ -0,0 +1,33 @@
+package ssadb
+
+import (
+	"sync/atomic"
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+type IrOffset struct {
+	gorm.Model
+
+	ProgramName string `json:"program_name" gorm:"index"`
+	// offset
+	Offset int64 `json:"offset" gorm:"index"`
+	//variable name
+	Variable string `json:"variable" gorm:"index"`
+	// value
+	ValueID int64 `json:"value_id" gorm:"index"`
+}
+
+func CreateOffset() *IrOffset {
+	ret := &IrOffset{}
+	return ret
+}
+func SaveIrOffset(idx *IrOffset) {
+	start := time.Now()
+	defer func() {
+		atomic.AddUint64(&_SSAIndexCost, uint64(time.Now().Sub(start).Nanoseconds()))
+	}()
+	db := GetDB()
+	db.Save(idx)
+}
