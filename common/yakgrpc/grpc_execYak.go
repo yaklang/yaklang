352fa38ecffef3627756c07219b041f50a1d0f0c
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-08-26 17:17:33 +0800

    feat(grpc-exec):use scriptPath first, set workdir

--- common/yakgrpc/grpc_execYak.go
+++ common/yakgrpc/grpc_execYak.go
@@ -5,24 +5,22 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+
 	"github.com/google/shlex"
 	uuid "github.com/google/uuid"
 	"github.com/yaklang/yaklang/common/consts"
 	"github.com/yaklang/yaklang/common/log"
-	"github.com/yaklang/yaklang/common/schema"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/yak"
 	"github.com/yaklang/yaklang/common/yak/antlr4yak"
 	"github.com/yaklang/yaklang/common/yak/yaklib"
 	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
-	"io"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"strconv"
-	"strings"
-	"time"
 )
 
 func status(verbose string, desc string) string {
@@ -38,7 +36,8 @@ func status(verbose string, desc string) string {
 
 func (s *Server) execRequest(req *ypb.ExecRequest, moduleName string, ctx context.Context, handler func(
 	result *ypb.ExecResult, logInfo *yaklib.YakitLog,
-) error, writer io.Writer) error {
+) error, writer io.Writer,
+) error {
 	runtimeId := uuid.New().String()
 
 	log.Infof("start to call exec-yak: %p", req)
@@ -47,7 +46,7 @@ func (s *Server) execRequest(req *ypb.ExecRequest, moduleName string, ctx contex
 		return utils.Errorf("cannot found yak engine binary...: %s", err)
 	}
 
-	//var messages = &[]string{}
+	// var messages = &[]string{}
 	yakitServer := yaklib.NewYakitServer(0,
 		yaklib.SetYakitServer_ProgressHandler(func(id string, progress float64) {
 			raw, _ := yaklib.YakitMessageGenerator(&yaklib.YakitProgress{
@@ -95,13 +94,13 @@ func (s *Server) execRequest(req *ypb.ExecRequest, moduleName string, ctx contex
 	defer yakitServer.Shutdown()
 
 	log.Info("start to handing params")
-	var params = []string{
+	params := []string{
 		"--yakit-webhook", yakitServer.Addr(),
 	}
 	for _, p := range req.GetParams() {
 		switch p.Key {
 		case "__yakit_plugin_names__":
-			var fp, err = os.CreateTemp(os.TempDir(), "yakit-plugin-selector-*.txt")
+			fp, err := os.CreateTemp(os.TempDir(), "yakit-plugin-selector-*.txt")
 			if err != nil {
 				return utils.Errorf("create yakit plugin selector failed: %s", err)
 			}
@@ -123,7 +122,7 @@ func (s *Server) execRequest(req *ypb.ExecRequest, moduleName string, ctx contex
 	}
 
 	if req.GetRunnerParamRaw() != "" {
-		var paramsExisted = strings.Join(params, " ")
+		paramsExisted := strings.Join(params, " ")
 		paramsRaw := fmt.Sprintf("%v %v", paramsExisted, req.GetRunnerParamRaw())
 		finalParams, err := shlex.Split(paramsRaw)
 		if err != nil {
@@ -133,41 +132,57 @@ func (s *Server) execRequest(req *ypb.ExecRequest, moduleName string, ctx contex
 		}
 	}
 
-	log.Info("start to fetch/handling yak code...")
-	var code = req.GetScript()
-	var scriptId = req.GetScriptId()
-	if code == "" {
-		if scriptId == "" {
-			return utils.Errorf("fetch yak code failed: %s", "empty code and scriptId")
+	log.Info("start to run yak code/script...")
+	var (
+		scriptPath = req.GetScriptPath()
+		scriptCode = req.GetScript()
+		scriptId   = req.GetScriptId()
+	)
+	createTempYakScript := func(code string) (string, error) {
+		f, err := os.CreateTemp("", "yakit-code-*.yak")
+		if err != nil {
+			return "", utils.Errorf("create temp file(for saving yak code) failed: %s", err)
 		}
+		_, err = f.WriteString(code)
+		if err != nil {
+			return "", utils.Errorf("save yak code to %v failed: %s", f.Name(), err)
+		}
+		f.Close()
+
+		return f.Name(), nil
+	}
+
+	// scriptPath > scriptCode > scriptId
+	if scriptPath != "" {
+		raw, err := os.ReadFile(scriptPath)
+		if err != nil {
+			return utils.Errorf("read script file failed: %s", err)
+		}
+		scriptCode = string(raw)
+	} else if scriptCode != "" {
+		scriptPath, err = createTempYakScript(scriptCode)
+		defer os.Remove(scriptPath)
+	} else {
 		script, err := yakit.GetYakScriptByName(s.GetProfileDatabase(), scriptId)
 		if err != nil {
 			return utils.Errorf("cannot find script yak code by scriptId:[%s]", scriptId)
 		}
-		code = script.Content
-	}
-	f, err := ioutil.TempFile("", "yaki-code-*.yak")
-	if err != nil {
-		return utils.Errorf("create temp file(for saving yak code) failed: %s", err)
-	}
-	_, err = f.WriteString(code)
-	if err != nil {
-		return utils.Errorf("save yak code to %v failed: %s", f.Name(), err)
-	}
-	f.Close()
-	defer os.RemoveAll(f.Name())
 
-	utils.Debug(func() {
-		raw, err := ioutil.ReadFile(f.Name())
-		if err != nil {
-			log.Errorf("verify %v failed: %s", f.Name(), err)
-			return
-		}
-		fmt.Println(string(raw))
-	})
+		scriptPath, err = createTempYakScript(script.Content)
+		defer os.Remove(scriptPath)
+	}
+
+	if scriptPath == "" {
+		return utils.Errorf("invalid exec request")
+	}
+
 	cmd := exec.CommandContext(
 		ctx, yakEnginePath,
-		append([]string{f.Name()}, params...)...)
+		append([]string{scriptPath}, params...)...)
+
+	if workdir := req.GetWorkDir(); workdir != "" {
+		cmd.Dir = workdir
+	}
 
 	cmd.Env = append(cmd.Env, os.Environ()...) // 继承主进程环境变量？
 
@@ -214,46 +229,48 @@ func (s *Server) execRequest(req *ypb.ExecRequest, moduleName string, ctx contex
 	cmd.Stdout = io.MultiWriter(writer, &stdoutBuffer, os.Stdout)
 	cmd.Stderr = io.MultiWriter(writer, &stderrBuffer, os.Stderr)
 
-	start := time.Now()
+	// start := time.Now()
 	err = cmd.Run()
-	history := &schema.ExecHistory{
-		Script:        code,
-		RuntimeId:     runtimeId,
-		ScriptId:      scriptId,
-		FromYakModule: moduleName,
-		TimestampNano: start.UnixNano(),
-		DurationMs:    time.Now().Sub(start).Milliseconds(),
-		Params:        strings.Join(params, " "),
-		Stdout:        strconv.Quote(stdoutBuffer.String()),
-		Stderr:        strconv.Quote(stderrBuffer.String()),
-	}
-	//defer func() {
-	//	raw, _ := json.Marshal(*messages)
-	//	if raw != nil {
-	//		history.Messages = strconv.Quote(string(raw))
-	//	}
-	//	history.RuntimeId = runtimeId
-	//	err := yakit.CreateOrUpdateExecHistory(s.db, history.CalcHash(), history)
-	//	if err != nil {
-	//		log.Errorf("save exec history failed: %s", err)
-	//		return
-	//	}
-	//}()
+	// history := &schema.ExecHistory{
+	// 	Script:        scriptCode,
+	// 	RuntimeId:     runtimeId,
+	// 	ScriptId:      scriptId,
+	// 	FromYakModule: moduleName,
+	// 	TimestampNano: start.UnixNano(),
+	// 	DurationMs:    time.Now().Sub(start).Milliseconds(),
+	// 	Params:        strings.Join(params, " "),
+	// 	Stdout:        strconv.Quote(stdoutBuffer.String()),
+	// 	Stderr:        strconv.Quote(stderrBuffer.String()),
+	// }
+	// //defer func() {
+	// //	raw, _ := json.Marshal(*messages)
+	// //	if raw != nil {
+	// //		history.Messages = strconv.Quote(string(raw))
+	// //	}
+	// //	history.RuntimeId = runtimeId
+	// //	err := yakit.CreateOrUpdateExecHistory(s.db, history.CalcHash(), history)
+	// //	if err != nil {
+	// //		log.Errorf("save exec history failed: %s", err)
+	// //		return
+	// //	}
+	// //}()
 
-	if err != nil {
-		history.Ok = false
-		history.Reason = fmt.Sprintf("execute yak code failed: %s", err)
-		log.Errorf("execute yak code failed: %s", err)
-		return err
-	}
+	// if err != nil {
+	// 	history.Ok = false
+	// 	history.Reason = fmt.Sprintf("execute yak code failed: %s", err)
+	// 	log.Errorf("execute yak code failed: %s", err)
+	// 	return err
+	// }
 
-	history.Ok = true
+	// history.Ok = true
 	return nil
 }
+
 func (s *Server) execRequestInCurrentServerEngine(req *ypb.ExecRequest, moduleName string, ctx context.Context, handler func(
 	result *ypb.ExecResult, logInfo *yaklib.YakitLog,
-) error, writer io.Writer) error {
-	var feedbackClient = yaklib.NewVirtualYakitClient(func(result *ypb.ExecResult) error {
+) error, writer io.Writer,
+) error {
+	feedbackClient := yaklib.NewVirtualYakitClient(func(result *ypb.ExecResult) error {
 		return handler(result, nil)
 	})
 	vAttach := NewVAttachCombinedOutputServer(func(msg *ypb.ExecResult) error {
@@ -263,7 +280,7 @@ func (s *Server) execRequestInCurrentServerEngine(req *ypb.ExecRequest, moduleNa
 		s.AttachCombinedOutput(nil, vAttach)
 	}()
 	engine := yak.NewYakitVirtualClientScriptEngine(feedbackClient)
-	var fp, err = os.CreateTemp(os.TempDir(), "yakit-plugin-selector-*.txt")
+	fp, err := os.CreateTemp(os.TempDir(), "yakit-plugin-selector-*.txt")
 	if err != nil {
 		return utils.Errorf("create yakit plugin selector failed: %s", err)
 	}
@@ -271,6 +288,7 @@ func (s *Server) execRequestInCurrentServerEngine(req *ypb.ExecRequest, moduleNa
 	fp.Close()
 	return engine.ExecuteMain(req.GetScript(), fp.Name())
 }
+
 func (s *Server) Exec(req *ypb.ExecRequest, stream ypb.Yak_ExecServer) error {
 	return s.ExecWithContext(stream.Context(), req, stream)
 }
