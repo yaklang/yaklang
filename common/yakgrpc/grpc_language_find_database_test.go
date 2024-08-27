847ecede2d4af52ccedac39a1351cb0117a562b6
wlz <lingze.wl@gmail.com>
2024-08-27 16:20:37 +0800

    fix(java2ssa):add code-range and fixup test-case

--- common/yakgrpc/grpc_language_find_database_test.go
+++ common/yakgrpc/grpc_language_find_database_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/yaklang/yaklang/common/utils/filesys"
 	"github.com/yaklang/yaklang/common/utils/filesys/filesys_interface"
 	"github.com/yaklang/yaklang/common/utils/memedit"
@@ -37,9 +38,9 @@ func check(
 		FileName:    frontEndFileName,
 		ProgramName: programName,
 	})
-	assert.NoErrorf(t, err, "YaklangLanguageFind failed: %v", err)
-	assert.NotNil(t, rsp)
-	assert.Len(t, rsp.Ranges, len(wantRanges))
+	require.NoErrorf(t, err, "YaklangLanguageFind failed: %v", err)
+	require.NotNil(t, rsp)
+	require.Len(t, rsp.Ranges, len(wantRanges))
 	for i, wantRange := range wantRanges {
 		assert.Equal(t, wantRange, GrpcRangeToRangeIf(rsp.Ranges[i]))
 	}
@@ -56,65 +57,71 @@ class A {
     public void a() {
         int a = 1; // 1, println1, println2
         println1(a); // println1, 1
-        if (a == 1) {
+        if (c == 1) {
             a = 2; // 2, println2
         }
-            println2(a); // println2, 1, 2
+        println2(a); // println2, 1, 2
     }
 }
     `)
 	a1 := newRangeFromText("5:13 5:14")
-	num1 := newRangeFromText("5:21 5:22")
-	println1 := newRangeFromText("6:22 6:23")
-	a2 := newRangeFromText("8:17 8:18")
-	num2 := newRangeFromText("8:21 8:22")
-	println2 := newRangeFromText("10:22 10:23")
+	num1 := newRangeFromText("5:17 5:18")
+	println1 := newRangeFromText("6:18 6:19")
+	a2 := newRangeFromText("8:13 8:14")
+	num2 := newRangeFromText("8:17 8:18")
+	println2 := newRangeFromText("10:18 10:19")
 	// only test reference
 	programID, fun := initProgram(t, vf, ssaapi.WithLanguage(ssaapi.JAVA))
 	_ = fun
 	defer fun()
-	t.Run("find from assign by variable", func(t *testing.T) {
+	t.Run("find from assign by variable: a1", func(t *testing.T) {
 		t.Log("find by variable a1")
 		check(local, t, programID,
 			"src/main/java/A.java",
 			a1,
-			[]memedit.RangeIf{println1, num1, println2},
+			[]memedit.RangeIf{a1, println1, println2},
 		)
+	})
+	t.Run("find from assign by variable: a2", func(t *testing.T) {
 		t.Log("find by variable a2")
 		check(local, t, programID,
 			"src/main/java/A.java",
 			a2,
-			[]memedit.RangeIf{num2, println2},
+			[]memedit.RangeIf{a2, println2},
 		)
 	})
 
-	t.Run("find from assign by value", func(t *testing.T) {
+	t.Run("find from assign by value: num1", func(t *testing.T) {
 		t.Log("find by value 1")
 		check(local, t, programID,
 			"src/main/java/A.java",
 			num1,
-			[]memedit.RangeIf{println1, num2, println2},
+			[]memedit.RangeIf{},
 		)
+	})
+	t.Run("find from assign by value: num2", func(t *testing.T) {
 		t.Log("find by value 2")
 		check(local, t, programID,
 			"src/main/java/A.java",
 			num2,
-			[]memedit.RangeIf{println2, num2},
+			[]memedit.RangeIf{},
 		)
 	})
 
-	t.Run("find from user", func(t *testing.T) {
+	t.Run("find from user: println1", func(t *testing.T) {
 		t.Log("find by println1")
 		check(local, t, programID,
 			"src/main/java/A.java",
 			println1,
-			[]memedit.RangeIf{println1, num1},
+			[]memedit.RangeIf{a1, println1, println2},
 		)
+	})
+	t.Run("find from user: println2", func(t *testing.T) {
 		t.Log("find by println2")
 		check(local, t, programID,
 			"src/main/java/A.java",
 			println2,
-			[]memedit.RangeIf{println2, num1, num2},
+			[]memedit.RangeIf{a1, println1, a2, println2},
 		)
 	})
 
