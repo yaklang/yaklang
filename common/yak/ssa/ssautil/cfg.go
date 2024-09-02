794fdfc7ef776ecd2ae07f7588ed1dea9f5ee8e8
YWHKKX <2076064543@qq.com>
2024-09-02 16:01:06 +0800

    fix switch scope phi bug

--- common/yak/ssa/ssautil/cfg.go
+++ common/yak/ssa/ssautil/cfg.go
@@ -351,7 +351,7 @@ func (s *SwitchStmt[T]) BuildBody(
 		s.mergeToNextBody = nil
 	}
 	ret := body(sub)
-	end := s.condition.CreateShadowScope()
+	end := s.condition.CreateSubScope()
 	end.CoverBy(ret)
 
 	if s.AutoBreak { // if this switch fall through, then merge to next body
