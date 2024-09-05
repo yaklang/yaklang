22fdef46c1a7f57a2cf343c82e2ab7e727367a68
YWHKKX <2076064543@qq.com>
2024-09-05 17:31:09 +0800

    fix golang library

---
+++ common/yak/go2ssa/test/import_test.go
@@ -0,0 +1,75 @@
+package test
+
+import (
+	"testing"
+
+	test "github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
+)
+
+func TestImport(t *testing.T) {
+	t.Run("function", func(t *testing.T) {
+		test.CheckPrintlnValue(`package main
+
+		import (
+			"gorm.io/driver/sqlite"
+			"gorm.io/gorm"
+			"log"
+		)
+
+		func main() {
+			db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+			if err != nil {
+				log.Fatal("failed to connect to database:", err)
+			}
+			println(db)
+			println(err)
+		}
+
+		`, []string{"Undefined-db(valid)", "Undefined-err(valid)"}, t)
+	})
+
+	t.Run("struct", func(t *testing.T) {
+		test.CheckPrintlnValue(`package main
+
+		import (
+			"fmt"
+			"net/url"
+		)
+
+		func main() {
+			rawURL := "https://www.example.com:8080/path?query=123#fragment"
+			parsedURL, err := url.Parse(rawURL)
+			if err != nil {
+				fmt.Println("Error parsing URL:", err)
+				return
+			}
+
+			println(parsedURL.Scheme)
+		}
+
+		`, []string{"Undefined-parsedURL.Scheme(valid)"}, t)
+	})
+
+	/*t.Run("struct value", func(t *testing.T) {
+		test.CheckPrintlnValue(`package main
+
+		import (
+			"fmt"
+			"net/url"
+		)
+
+		func main() {
+			u := url.URL{
+				Scheme: "https",
+				Host:   "www.example.com",
+				Path:   "/path",
+				RawQuery: "query=123",
+			}
+
+			println(u.Scheme)
+			println(u.Host)
+		}
+
+		`, []string{"\"https\"", "\"www.example.com\""}, t)
+	})*/
+}
