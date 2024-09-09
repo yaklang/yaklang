9edfddb119f798ed72f7d206730c919cfe4ddc11
wlz <lingze.wl@gmail.com>
2024-09-09 14:20:45 +0800

    test(php2ssa):add compile process test for php

--- common/yak/ssaapi/test/ssatest/process_test.go
+++ common/yak/ssaapi/test/ssatest/process_test.go
@@ -7,12 +7,41 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils/filesys"
+	"github.com/yaklang/yaklang/common/utils/filesys/filesys_interface"
 	"github.com/yaklang/yaklang/common/yak/ssa/ssadb"
 	"github.com/yaklang/yaklang/common/yak/ssaapi"
 )
 
-func TestParseProject(t *testing.T) {
+func checkProcess(vf filesys_interface.FileSystem, t *testing.T, opt ...ssaapi.Option) {
+	type message struct {
+		msg     string
+		process float64
+	}
 
+	matchRightProcess := false
+	msgs := make([]message, 0)
+	programID := uuid.NewString()
+	opt = append(opt,
+		ssaapi.WithProgramName(programID),
+		ssaapi.WithProcess(func(msg string, process float64) {
+			if 0 < process && process < 1 {
+				matchRightProcess = true
+			}
+			msgs = append(msgs, message{msg, process})
+		}),
+	)
+	prog, err := ssaapi.ParseProject(vf, opt...)
+	defer ssadb.DeleteProgram(ssadb.GetDB(), programID)
+	assert.NoError(t, err)
+	assert.NotNil(t, prog)
+	assert.True(t, matchRightProcess)
+	log.Infof("message: %v", msgs)
+	assert.Greater(t, len(msgs), 0)
+	end := msgs[len(msgs)-1]
+	assert.Equal(t, end.process, float64(1))
+}
+
+func TestParseProject_JAVA(t *testing.T) {
 	vf := filesys.NewVirtualFs()
 	vf.AddFile("example/src/main/java/com/example/apackage/a.java", `
 		package com.example.apackage; 
@@ -51,30 +80,38 @@ func TestParseProject(t *testing.T) {
 	}
 	`)
 
-	type message struct {
-		msg     string
-		process float64
-	}
+	checkProcess(vf, t, ssaapi.WithLanguage(ssaapi.JAVA))
+}
 
-	matchRightProcess := false
-	msgs := make([]message, 0)
-	programID := uuid.NewString()
-	prog, err := ssaapi.ParseProject(vf,
-		ssaapi.WithProgramName(programID),
-		ssaapi.WithLanguage(ssaapi.JAVA),
-		ssaapi.WithProcess(func(msg string, process float64) {
-			if 0 < process && process < 1 {
-				matchRightProcess = true
+func TestParseProject_PHP(t *testing.T) {
+	vf := filesys.NewVirtualFs()
+	vf.AddFile("example/src/main/php/a.php", `
+		<?php
+		require_once("b.php");
+		class A {
+			public function main() {
+				$b = new B();
+				// for test 1: A->B
+				target1($b->get());
+				// for test 2: B->A
+				$b->show(1);
 			}
-			msgs = append(msgs, message{msg, process})
-		}),
-	)
-	defer ssadb.DeleteProgram(ssadb.GetDB(), programID)
-	assert.NoError(t, err)
-	assert.NotNil(t, prog)
-	assert.True(t, matchRightProcess)
-	log.Infof("message: %v", msgs)
-	assert.Greater(t, len(msgs), 0)
-	end := msgs[len(msgs)-1]
-	assert.Equal(t, end.process, float64(1))
+		}`)
+	vf.AddFile("example/src/main/php/b.php", `
+		<?php
+		require_once("c.php");
+		class B {
+			public function get() {
+				return 1;
+			}
+		}`)
+	vf.AddFile("example/src/main/php/c.php", `
+		<?php
+		class C {
+			public function CFunc() {
+				echo "Hello, World";	
+			}
+		}`)
+
+	checkProcess(vf, t, ssaapi.WithLanguage(ssaapi.PHP))
 }
