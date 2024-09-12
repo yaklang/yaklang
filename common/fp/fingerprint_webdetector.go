219b9d3e2e8b11fe827781640bf6660883a3cca7
go0p <gp827782797@qq.com>
2024-09-12 16:55:23 +0800

    patch handle close

--- common/fp/fingerprint_webdetector.go
+++ common/fp/fingerprint_webdetector.go
@@ -90,121 +90,120 @@ func (f *Matcher) webDetector(result *MatchResult, ctx context.Context, config *
 		cpeAnalyzer = utils.NewCPEAnalyzer()
 		httpflows   []*HTTPFlow
 	)
-	if redirectInfos != nil {
-		log.Debugf("finished to check iotdevfp: %v fetch response[%v]", utils2.HostPort(ip.String(), port), len(redirectInfos))
-		result.State = OPEN
-		result.Fingerprint.ServiceName = "http"
-		if len(redirectInfos) > 1 {
-			redirectInfos = append([]*lowhttp.RedirectFlow{utils2.GetLastElement(redirectInfos)}, redirectInfos[1:]...)
+
+	log.Debugf("finished to check iotdevfp: %v fetch response[%v]", utils2.HostPort(ip.String(), port), len(redirectInfos))
+	result.State = OPEN
+	result.Fingerprint.ServiceName = "http"
+	if len(redirectInfos) > 1 {
+		redirectInfos = append([]*lowhttp.RedirectFlow{utils2.GetLastElement(redirectInfos)}, redirectInfos[1:]...)
+	}
+	for _, i := range redirectInfos {
+
+		var currentCPE []*rule.CPE
+		if !f.Config.DisableDefaultIotFingerprint {
+			iotdevResults := iotdevfp.MatchAll(i.Response)
+			for _, iotdevResult := range iotdevResults {
+				result.Fingerprint.CPEs = append(result.Fingerprint.CPEs, iotdevResult.GetCPE())
+				cpeIns, _ := webfingerprint.ParseToCPE(iotdevResult.GetCPE())
+
+				if cpeIns != nil {
+					currentCPE = append(currentCPE, fingerprint.LoadCPEFromWebfingerrintCPE(cpeIns))
+				}
+			}
 		}
-		for _, i := range redirectInfos {
 
-			var currentCPE []*rule.CPE
-			if !f.Config.DisableDefaultIotFingerprint {
-				iotdevResults := iotdevfp.MatchAll(i.Response)
-				for _, iotdevResult := range iotdevResults {
-					result.Fingerprint.CPEs = append(result.Fingerprint.CPEs, iotdevResult.GetCPE())
-					cpeIns, _ := webfingerprint.ParseToCPE(iotdevResult.GetCPE())
+		if result.Fingerprint == nil {
+			result.Fingerprint = &FingerprintInfo{
+				IP:          ip.String(),
+				Port:        port,
+				Proto:       TCP,
+				ServiceName: "http",
+				Banner:      strconv.Quote(string(i.Response)),
+			}
+		}
 
-					if cpeIns != nil {
-						currentCPE = append(currentCPE, fingerprint.LoadCPEFromWebfingerrintCPE(cpeIns))
-					}
-				}
+		if i.IsHttps {
+			name := strings.ToLower(result.Fingerprint.ServiceName)
+			if !strings.Contains(name, "https") &&
+				strings.Contains(name, "http") {
+				// 不包含 https 但是包含 http
+				result.Fingerprint.ServiceName = strings.ReplaceAll(name, "http", "https")
 			}
 
-			if result.Fingerprint == nil {
-				result.Fingerprint = &FingerprintInfo{
-					IP:          ip.String(),
-					Port:        port,
-					Proto:       TCP,
-					ServiceName: "http",
-					Banner:      strconv.Quote(string(i.Response)),
-				}
+			if name == "" {
+				result.Fingerprint.ServiceName = "https"
 			}
 
-			if i.IsHttps {
-				name := strings.ToLower(result.Fingerprint.ServiceName)
-				if !strings.Contains(name, "https") &&
-					strings.Contains(name, "http") {
-					// 不包含 https 但是包含 http
-					result.Fingerprint.ServiceName = strings.ReplaceAll(name, "http", "https")
-				}
+		}
 
-				if name == "" {
-					result.Fingerprint.ServiceName = "https"
-				}
-
-			}
-
-			info := i
-			requestHeader, requestBody := lowhttp.SplitHTTPHeadersAndBodyFromPacket(info.Request)
-			responseHeader, responseBody := lowhttp.SplitHTTPHeadersAndBodyFromPacket(info.Response)
-			flow := &HTTPFlow{
-				StatusCode:     lowhttp.GetStatusCodeFromResponse(info.Response),
-				IsHTTPS:        info.IsHttps,
-				RequestHeader:  []byte(requestHeader),
-				RequestBody:    requestBody,
-				ResponseHeader: []byte(responseHeader),
-				ResponseBody:   responseBody,
-				CPEs:           currentCPE,
-			}
-			httpflows = append(httpflows, flow)
-			f.matcher.Route = func(ctx context.Context, webPath string) ([]byte, error) {
-				target := utils2.HostPort(host, port)
-				packet := []byte(fmt.Sprintf(`GET %s HTTP/1.1
+		info := i
+		requestHeader, requestBody := lowhttp.SplitHTTPHeadersAndBodyFromPacket(info.Request)
+		responseHeader, responseBody := lowhttp.SplitHTTPHeadersAndBodyFromPacket(info.Response)
+		flow := &HTTPFlow{
+			StatusCode:     lowhttp.GetStatusCodeFromResponse(info.Response),
+			IsHTTPS:        info.IsHttps,
+			RequestHeader:  []byte(requestHeader),
+			RequestBody:    requestBody,
+			ResponseHeader: []byte(responseHeader),
+			ResponseBody:   responseBody,
+			CPEs:           currentCPE,
+		}
+		httpflows = append(httpflows, flow)
+		f.matcher.Route = func(ctx context.Context, webPath string) ([]byte, error) {
+			target := utils2.HostPort(host, port)
+			packet := []byte(fmt.Sprintf(`GET %s HTTP/1.1
 Host: %v
 User-Agent: Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7
 `, target, webPath))
-				ok, flow, err := FetchBannerFromHostPort(iotDetectCtx, packet, h, port, int64(config.FingerprintDataSize), config.RuntimeId, config.Proxies...)
-				if err != nil {
-					return nil, err
-				}
-				if !ok {
-					return nil, fmt.Errorf("fetch path %s banner failed", webPath)
-				}
-				f := utils2.GetLastElement(flow)
-				return f.Response, nil
+			ok, flow, err := FetchBannerFromHostPort(iotDetectCtx, packet, h, port, int64(config.FingerprintDataSize), config.RuntimeId, config.Proxies...)
+			if err != nil {
+				return nil, err
 			}
-			cpes := f.matcher.MatchResource(iotDetectCtx, f.Config.GetWebFingerprintRules(), func(path string) (*rule.MatchResource, error) {
-				res := &rule.MatchResource{
-					Protocol: "http",
-					Port:     port,
-					Path:     path,
-				}
-				cached := map[string][]byte{}
-				if path == "" || path == "/" {
-					res.Data = info.Response
-					return res, nil
-				}
-				if v, ok := cached[path]; ok {
-					res.Data = v
-					return res, nil
-				}
-				data, err := f.matcher.Route(ctx, path)
-				if err != nil {
-					return nil, err
-				}
-				cached[path] = data
-				return rule.NewHttpResource(data), nil
-			})
-			//cpes, err := f.wfMatcher.MatchWithOptions(info, config.GenerateWebFingerprintConfigOptions()...)
-			//if err != nil {
-			//	if !strings.Contains(err.Error(), "no rules matched") {
-			//		continue
-			//	}
-			//}
-			// 如果检测到指纹信息
-			if len(cpes) > 0 {
-				currentCPE = append(currentCPE, cpes...)
-				urlStr := info.RespRecord.Url
-				cpeAnalyzer.Feed(urlStr, cpes...)
-				results.Store(urlStr, cpes)
+			if !ok {
+				return nil, fmt.Errorf("fetch path %s banner failed", webPath)
 			}
+			f := utils2.GetLastElement(flow)
+			return f.Response, nil
+		}
+		cpes := f.matcher.MatchResource(iotDetectCtx, f.Config.GetWebFingerprintRules(), func(path string) (*rule.MatchResource, error) {
+			res := &rule.MatchResource{
+				Protocol: "http",
+				Port:     port,
+				Path:     path,
+			}
+			cached := map[string][]byte{}
+			if path == "" || path == "/" {
+				res.Data = info.Response
+				return res, nil
+			}
+			if v, ok := cached[path]; ok {
+				res.Data = v
+				return res, nil
+			}
+			data, err := f.matcher.Route(ctx, path)
+			if err != nil {
+				return nil, err
+			}
+			cached[path] = data
+			return rule.NewHttpResource(data), nil
+		})
+		//cpes, err := f.wfMatcher.MatchWithOptions(info, config.GenerateWebFingerprintConfigOptions()...)
+		//if err != nil {
+		//	if !strings.Contains(err.Error(), "no rules matched") {
+		//		continue
+		//	}
+		//}
+		// 如果检测到指纹信息
+		if len(cpes) > 0 {
+			currentCPE = append(currentCPE, cpes...)
+			urlStr := info.RespRecord.Url
+			cpeAnalyzer.Feed(urlStr, cpes...)
+			results.Store(urlStr, cpes)
+		}
 
-			if len(flow.CPEs) < len(currentCPE) {
-				flow.CPEs = currentCPE
-			}
+		if len(flow.CPEs) < len(currentCPE) {
+			flow.CPEs = currentCPE
 		}
 	}
 	urlCpe := map[string][]*rule.CPE{}
@@ -278,7 +277,7 @@ Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/w
 	if packet2 != nil {
 		packet = packet2
 	}
-
+	var isOpen bool
 	rspDetail, err := lowhttp.HTTP(
 		lowhttp.WithRuntimeId(runtimeId),
 		lowhttp.WithHttps(isTls),
@@ -289,9 +288,9 @@ Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/w
 		lowhttp.WithJsRedirect(true),
 		lowhttp.WithProxy(proxy...),
 	)
-	isOpen := rspDetail.PortIsOpen
 	if err != nil {
 		return isOpen, nil, utils2.Errorf("lowhttp.HTTP failed: %s", err)
 	}
+	isOpen = rspDetail.PortIsOpen
 	return isOpen, rspDetail.RedirectRawPackets, nil
 }
