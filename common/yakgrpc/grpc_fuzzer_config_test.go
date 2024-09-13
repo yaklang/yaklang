c76866bb2f6133a393164527227e46bab52d1d5b
intSheep <1098204193@qq.com>
2024-09-13 13:50:53 +0800

    feat(grpc-fuzzerconfig):add DbOperateMessage as response

--- common/yakgrpc/grpc_fuzzer_config_test.go
+++ common/yakgrpc/grpc_fuzzer_config_test.go
@@ -33,15 +33,17 @@ func TestGRPCMUSTPASS_FuzzerConfig(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, 20, len(result.GetData()))
 
-	_, err = client.DeleteFuzzerConfig(context.Background(), &ypb.DeleteFuzzerConfigRequest{PageId: "0", DeleteAll: true})
+	msg, err := client.DeleteFuzzerConfig(context.Background(), &ypb.DeleteFuzzerConfigRequest{DeleteAll: true})
+	require.Contains(t, msg.ExtraMessage, "Delete all webFuzzerConfig")
 	result, err = client.QueryFuzzerConfig(context.Background(), &ypb.QueryFuzzerConfigRequest{Limit: 20})
 	require.NoError(t, err)
 	require.Equal(t, 0, len(result.GetData()))
 
 	saveFuzzerConfig(20)
-	_, err = client.DeleteFuzzerConfig(context.Background(), &ypb.DeleteFuzzerConfigRequest{PageId: "1"})
+	msg, err = client.DeleteFuzzerConfig(context.Background(), &ypb.DeleteFuzzerConfigRequest{PageId: []string{"0", "1"}, DeleteAll: false})
+	require.Contains(t, msg.ExtraMessage, "Delete webFuzzerConfig with pageId")
 	require.NoError(t, err)
 	result, err = client.QueryFuzzerConfig(context.Background(), &ypb.QueryFuzzerConfigRequest{Limit: -1})
 	require.NoError(t, err)
-	require.Equal(t, 19, len(result.GetData()))
+	require.Equal(t, 18, len(result.GetData()))
 }
