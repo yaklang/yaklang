bea3ec1419e7821c66569393bd00591efb510f83
intSheep <1098204193@qq.com>
2024-09-13 15:03:57 +0800

    fix some error and refract test

--- common/yakgrpc/grpc_fuzzer_config_test.go
+++ common/yakgrpc/grpc_fuzzer_config_test.go
@@ -2,9 +2,9 @@ package yakgrpc
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
-	"strconv"
 	"testing"
 )
 
@@ -12,11 +12,14 @@ func TestGRPCMUSTPASS_FuzzerConfig(t *testing.T) {
 	client, err := NewLocalClient()
 	require.NoError(t, err)
 
+	var pageIds []string
 	saveFuzzerConfig := func(num int) {
 		var Data []*ypb.FuzzerConfig
 		for i := 0; i < num; i++ {
+			pageId := uuid.New().String()
+			pageIds = append(pageIds, pageId)
 			Data = append(Data, &ypb.FuzzerConfig{
-				PageId: strconv.Itoa(i),
+				PageId: pageId,
 				Type:   "group",
 				Config: "{\"isHTTPS\":true",
 			})
@@ -27,23 +30,21 @@ func TestGRPCMUSTPASS_FuzzerConfig(t *testing.T) {
 			require.NoError(t, err)
 		}
 	}
-
+	queryAll := &ypb.QueryFuzzerConfigRequest{Pagination: &ypb.Paging{Limit: -1}}
+	originResult, err := client.QueryFuzzerConfig(context.Background(), queryAll)
+	require.NoError(t, err)
 	saveFuzzerConfig(100)
-	result, err := client.QueryFuzzerConfig(context.Background(), &ypb.QueryFuzzerConfigRequest{Limit: 20})
-	require.NoError(t, err)
-	require.Equal(t, 20, len(result.GetData()))
 
-	msg, err := client.DeleteFuzzerConfig(context.Background(), &ypb.DeleteFuzzerConfigRequest{DeleteAll: true})
-	require.Contains(t, msg.ExtraMessage, "Delete all webFuzzerConfig")
-	result, err = client.QueryFuzzerConfig(context.Background(), &ypb.QueryFuzzerConfigRequest{Limit: 20})
+	newResult, err := client.QueryFuzzerConfig(context.Background(), queryAll)
 	require.NoError(t, err)
-	require.Equal(t, 0, len(result.GetData()))
+	require.Equal(t, len(originResult.GetData())+100, len(newResult.GetData()))
 
-	saveFuzzerConfig(20)
-	msg, err = client.DeleteFuzzerConfig(context.Background(), &ypb.DeleteFuzzerConfigRequest{PageId: []string{"0", "1"}, DeleteAll: false})
-	require.Contains(t, msg.ExtraMessage, "Delete webFuzzerConfig with pageId")
+	res, err := client.QueryFuzzerConfig(context.Background(), &ypb.QueryFuzzerConfigRequest{Pagination: &ypb.Paging{Limit: 10}})
 	require.NoError(t, err)
-	result, err = client.QueryFuzzerConfig(context.Background(), &ypb.QueryFuzzerConfigRequest{Limit: -1})
+	require.Equal(t, len(res.Data), 10)
+
+	_, err = client.DeleteFuzzerConfig(context.Background(), &ypb.DeleteFuzzerConfigRequest{PageId: pageIds})
 	require.NoError(t, err)
-	require.Equal(t, 18, len(result.GetData()))
+	result, err := client.QueryFuzzerConfig(context.Background(), queryAll)
+	require.Equal(t, len(originResult.GetData()), len(result.GetData()))
 }
