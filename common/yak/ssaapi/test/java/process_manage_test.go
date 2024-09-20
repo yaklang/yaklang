0604ae8b65962bc6d0b5e934d9bdc9ce80f4c6c6
wlz <lingze.wl@gmail.com>
2024-09-20 11:32:41 +0800

    test(ssatest):remove time base testcase

--- common/yak/ssaapi/test/java/process_manage_test.go
+++ common/yak/ssaapi/test/java/process_manage_test.go
@@ -4,52 +4,20 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
-	"github.com/yaklang/yaklang/common/yak/ssa/ssadb"
 	"io/fs"
 	"path/filepath"
 	"strings"
 	"testing"
-	"time"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/yaklang/yaklang/common/yak/ssa/ssadb"
 
 	"github.com/stretchr/testify/require"
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils/filesys"
 	"github.com/yaklang/yaklang/common/yak/ssaapi"
-	"github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
 )
-
-func TestJava_ProcessManage(t *testing.T) {
-	var originAstCost time.Duration
-	var cancelAstCost time.Duration
-
-	fs := filesys.NewRelLocalFs(`./badcase/`)
-	ssatest.CheckProfileWithFS(fs, t, func(p ssatest.ParseStage, prog ssaapi.Programs, start time.Time) error {
-		if p == ssatest.OnlyMemory {
-			originAstCost = time.Since(start)
-		}
-		return nil
-	}, ssaapi.WithLanguage(ssaapi.JAVA))
-
-	ctx, cancel := context.WithCancel(context.Background())
-	timer := time.NewTimer(originAstCost / 10)
-	go func() {
-		select {
-		case <-timer.C:
-			cancel()
-		}
-	}()
-	ssatest.CheckProfileWithFS(fs, t, func(p ssatest.ParseStage, prog ssaapi.Programs, start time.Time) error {
-		if p == ssatest.OnlyMemory {
-			cancelAstCost = time.Since(start)
-		}
-		return nil
-	}, ssaapi.WithLanguage(ssaapi.JAVA), ssaapi.WithContext(ctx))
-	require.Greater(t, originAstCost, cancelAstCost*2)
-	log.Info("origin ast cost: ", originAstCost)
-	log.Info("Proactively stop ast cost: ", cancelAstCost)
-}
 
 func TestJava_ProcessManage_Mutli_Files(t *testing.T) {
 	toCreate := []string{
