0e33ece58213f97c3a32074ffe9a84d716c7ad19
intSheep <1098204193@qq.com>
2024-09-29 10:49:39 +0800

    fix import cycle

--- common/yakgrpc/yakit/syntaxflow_rule.go
+++ common/yakgrpc/yakit/syntaxflow_rule.go
@@ -3,8 +3,6 @@ package yakit
 import (
 	"github.com/jinzhu/gorm"
 	"github.com/yaklang/yaklang/common/schema"
-	"github.com/yaklang/yaklang/common/syntaxflow/sfdb"
-	"github.com/yaklang/yaklang/common/syntaxflow/sfvm"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/bizhelper"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
@@ -82,64 +80,4 @@ func DeleteSyntaxFlowRuleByFilter(db *gorm.DB, filter *ypb.SyntaxFlowRuleFilter)
 		return 0, utils.Errorf("delete syntax flow rule failed: %s", db.Error)
 	}
 	return db.RowsAffected, nil
-}
-
-func SaveSyntaxFlowRule(db *gorm.DB, param *ypb.SaveSyntaxFlowRuleRequest) error {
-	language, err := sfdb.CheckSyntaxFlowLanguage(param.GetLanguage())
-	if err != nil {
-		return err
-	}
-	ruleType, err := sfdb.CheckSyntaxFlowRuleType(param.GetRuleName())
-	if err != nil {
-		return err
-	}
-	frame, err := sfvm.NewSyntaxFlowVirtualMachine().Compile(param.GetContent())
-	if err != nil {
-		return err
-	}
-	rule := frame.GetRule()
-	rule.Type = ruleType
-	rule.RuleName = param.GetRuleName()
-	rule.Language = string(language)
-	rule.Tag = strings.Join(param.GetTags(), "|")
-	err = createOrUpdateSyntaxFlow(db, rule.CalcHash(), rule)
-	if err != nil {
-		return utils.Wrap(err, "ImportRuleWithoutValid create or update syntax flow rule error")
-	}
-	return nil
-}
-
-func createOrUpdateSyntaxFlow(db *gorm.DB, hash string, i *schema.SyntaxFlowRule) error {
-	var rules []*schema.SyntaxFlowRule
-	if hash == "" {
-		hash = i.CalcHash()
-	}
-
-	var sameHashRule schema.SyntaxFlowRule
-	if db.Where("hash = ?", hash).First(&sameHashRule); sameHashRule.ID > 0 {
-		return nil
-	}
-
-	if err := db.Where("rule_name = ?", i.RuleName).Find(&rules).Error; err != nil {
-		if gorm.IsRecordNotFoundError(err) {
-			return db.Create(i).Error
-		}
-		return err
-	}
-	if len(rules) == 1 {
-		i.Hash = i.CalcHash()
-		// only one rule, check and update
-		rule := rules[0]
-		if rule.Hash != hash {
-			// if same name, but different content, update
-			return db.Model(&rule).Updates(i).Error
-		}
-	} else {
-		// multiple rule in same name, delete all and create new
-		if err := db.Where("rule_name = ?", i.RuleName).Unscoped().Delete(&schema.SyntaxFlowRule{}).Error; err != nil {
-			return err
-		}
-		return db.Create(i).Error
-	}
-	return nil
 }
