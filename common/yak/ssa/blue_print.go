1be10beff37b744cea2d1829cad1024dd164dc17
Q16G <760046475@qq.com>
2024-09-29 15:51:52 +0800

    refactor(cls): refactor cls api name

--- common/yak/ssa/blue_print.go
+++ common/yak/ssa/blue_print.go
@@ -5,38 +5,61 @@ import (
 
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils"
+	"strings"
 )
 
-type method struct {
-	function *Function
-	index    int
-}
+type BluePrintFieldKind int
 
-type BluePrintMember struct {
-	Value Value
-	Type  Type
-}
+const (
+	// method: static normal magic
+	BluePrintStaticMethod BluePrintFieldKind = iota
+	BluePrintNormalMethod
+	BluePrintMagicMethod
+
+	// member: normal const static
+	BluePrintNormalMember
+	BluePrintConstMember
+	BluePrintStaticMember
+)
+
+type ClassModifier int
+
+const (
+	NoneModifier ClassModifier = 1 << iota
+	Static
+	Public
+	Protected
+	Private
+	Abstract
+	Final
+	Readonly
+)
 
 // ClassBluePrint is a class blue print, it is used to create a new class
 type ClassBluePrint struct {
 	Name string
 
-	Method       map[string]*Function
+	NormalMethod map[string]*Function
 	StaticMethod map[string]*Function
+	MagicMethod  map[BluePrintMagicMethodKind]*Function
 
-	NormalMember map[string]*BluePrintMember
-	StaticMember map[string]Value
+	NormalMember map[string]Value
+	StaticMember map[string]*Phi
+	ConstValue   map[string]Value
 
 	CallBack []func()
 
 	// magic method
-	Copy        Value
 	Constructor Value
 	Destructor  Value
 
 	// _container is an inner ssa.Value
 	// the container can ref to the class member
+	// _container in this scope
 	_container Value
+
+	GeneralPhi       func(string) *Phi
+	GeneralUndefined func(string) *Undefined
 
 	ParentClass []*ClassBluePrint
 	// full Type Name
@@ -46,6 +69,89 @@ type ClassBluePrint struct {
 	lazyBuilder
 }
 
+func NewClassBluePrint(name string) *ClassBluePrint {
+	class := &ClassBluePrint{
+		Name:         name,
+		NormalMember: make(map[string]Value),
+		StaticMember: make(map[string]*Phi),
+		ConstValue:   make(map[string]Value),
+
+		NormalMethod: make(map[string]*Function),
+		StaticMethod: make(map[string]*Function),
+		MagicMethod:  make(map[BluePrintMagicMethodKind]*Function),
+
+		fullTypeName: make([]string, 0),
+	}
+	return class
+}
+
+// ======================= class blue print
+// AddParentClass is used to add a parent class to the class,
+func (c *ClassBluePrint) AddParentClass(parent *ClassBluePrint) {
+	if parent == nil {
+		return
+	}
+	c.ParentClass = append(c.ParentClass, parent)
+	for name, f := range parent.NormalMethod {
+		c.RegisterNormalMethod(name, f)
+	}
+	for name, f := range parent.StaticMethod {
+		c.RegisterStaticMethod(name, f)
+	}
+	for name, f := range parent.MagicMethod {
+		c.RegisterMagicMethod(name, f)
+	}
+	for name, value := range parent.NormalMember {
+		c.RegisterNormalMember(name, value)
+	}
+	for name, value := range parent.StaticMember {
+		c.RegisterStaticMember(name, value)
+	}
+	for name, value := range parent.ConstValue {
+		c.RegisterConstMember(name, value)
+	}
+}
+func (c *ClassBluePrint) CheckExtendBy(kls string) bool {
+	for _, class := range c.ParentClass {
+		if strings.EqualFold(class.Name, kls) {
+			return true
+		}
+	}
+	return false
+}
+
+func (c *ClassBluePrint) getFieldWithParent(get func(bluePrint *ClassBluePrint) bool) bool {
+	// if current class can get this field, just return true
+	if ok := get(c); ok {
+		return true
+	} else {
+		// if current class can't get this field, then check the parent class
+		for _, class := range c.ParentClass {
+			// if parent class can get this field, just return true
+			if ex := class.getFieldWithParent(get); ex {
+				return true
+			}
+		}
+	}
+	// not found this field
+	return false
+}
+
+// storeInContainer store static in global container
+func (c *ClassBluePrint) storeInContainer(name string, val Value, _type BluePrintFieldKind) {
+	if utils.IsNil(c._container) || utils.IsNil(c._container.GetFunc()) {
+		return
+	}
+	createVariable := func(builder *FunctionBuilder, variable *Variable) {
+		builder.AssignVariable(variable, val)
+	}
+	switch _type {
+	case BluePrintStaticMethod, BluePrintStaticMember:
+	default:
+		builder := c._container.GetFunc().builder
+		createVariable(builder, builder.CreateMemberCallVariable(c._container, builder.EmitConstInst(name)))
+	}
+}
 func (b *ClassBluePrint) InitializeWithContainer(con *Make) error {
 	if b._container != nil {
 		return utils.Errorf("the container is already initialized id:(%v)", b._container.GetId())
@@ -53,102 +159,8 @@ func (b *ClassBluePrint) InitializeWithContainer(con *Make) error {
 	b._container = con
 	return nil
 }
-
 func (b *ClassBluePrint) GetClassContainer() Value {
 	return b._container
-}
-
-func NewClassBluePrint() *ClassBluePrint {
-	class := &ClassBluePrint{
-		NormalMember: make(map[string]*BluePrintMember),
-		StaticMember: make(map[string]Value),
-
-		Method:       make(map[string]*Function),
-		StaticMethod: make(map[string]*Function),
-		fullTypeName: make([]string, 0),
-	}
-	return class
-}
-
-var _ Type = (*ClassBluePrint)(nil)
-
-/// ============= implement type interface
-
-func (c *ClassBluePrint) String() string {
-	str := fmt.Sprintf("ClassBluePrint: %s", c.Name)
-	return str
-}
-
-func (c *ClassBluePrint) PkgPathString() string {
-	return ""
-}
-
-func (c *ClassBluePrint) RawString() string {
-	return ""
-}
-
-func (c *ClassBluePrint) GetTypeKind() TypeKind {
-	return ClassBluePrintTypeKind
-}
-
-func (c *ClassBluePrint) SetMethod(m map[string]*Function) {
-	c.Method = m
-}
-
-func (c *ClassBluePrint) AddMethod(key string, fun *Function) {
-	if c._container != nil {
-		// set the container ref key to the method
-		log.Infof("bind %v.%v to function: %v", c.Name, key, fun.name)
-		funcContainsklass := c._container.GetFunc()
-		if funcContainsklass != nil && funcContainsklass.builder != nil {
-			builder := funcContainsklass.builder
-			variable := builder.CreateMemberCallVariable(c._container, builder.EmitConstInst(key))
-			builder.AssignVariable(variable, fun)
-		} else {
-			log.Warnf("bind %v.%v failed, reason: class's builder (from source is missed)", c.Name, key)
-		}
-	} else {
-		log.Warnf("class %v's ref container is nil", c.Name)
-	}
-	fun.SetMethod(true, c)
-	if f, ok := c.Method[key]; ok {
-		Point(fun, f)
-		// log.Infof("method %v is already exist, replace it", key)
-	}
-	c.Method[key] = fun
-}
-
-func (c *ClassBluePrint) GetMethod() map[string]*Function {
-	return c.Method
-}
-
-func (c *ClassBluePrint) SetMethodGetter(f func() map[string]*Function) {
-}
-
-func (c *ClassBluePrint) AddFullTypeName(name string) {
-	if c == nil {
-		return
-	}
-
-	c.fullTypeName = append(c.fullTypeName, name)
-}
-
-func (c *ClassBluePrint) GetFullTypeNames() []string {
-	if c == nil {
-		return nil
-	}
-	return c.fullTypeName
-}
-
-func (c *ClassBluePrint) SetFullTypeNames(names []string) {
-	if c == nil {
-		return
-	}
-	c.fullTypeName = names
-}
-
-func (c *ClassBluePrint) GetNormalMember(name string) *BluePrintMember {
-	return c.NormalMember[name]
 }
 
 func (c *ClassBluePrint) BuildConstructorAndDestructor() {
