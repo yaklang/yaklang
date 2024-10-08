50c94682263846cfa2563eb4345434ab8b3117aa
rookie <rookie_is@qq.com>
2024-10-08 17:49:00 +0800

    fix(getProxyFromEnv): darwin import cycle

--- common/utils/systemproxy/systemproxy.go
+++ common/utils/systemproxy/systemproxy.go
@@ -3,7 +3,10 @@ package systemproxy
 import (
 	"errors"
 	"fmt"
+	"github.com/yaklang/yaklang/common/utils"
+	"os"
 	"runtime"
+	"strings"
 )
 
 // Settings represents systemwide proxy settings.
@@ -17,3 +20,36 @@ type Settings struct {
 
 // ErrNotImpl error is returned when the current platform isn't supported yet.
 var ErrNotImpl = errors.New(fmt.Sprintf("systemproxy not implemented on this platform: %v", runtime.GOOS))
+
+func GetProxyFromEnv() string {
+	if setting, err := Get(); err == nil && setting.Enabled && setting.DefaultServer != "" {
+		return setting.DefaultServer
+	}
+	for _, k := range []string{
+		"YAK_PROXY", "yak_proxy",
+		"HTTP_PROXY", "http_proxy",
+		"HTTPS_PROXY", "https_proxy",
+		"all_proxy", "all_proxy",
+		"proxy", "proxy",
+	} {
+		if p := strings.Trim(os.Getenv(k), `"`); p != "" {
+			return FixProxy(p)
+		}
+	}
+	return ""
+}
+
+func FixProxy(i string) string {
+	if i == "" {
+		return ""
+	}
+
+	if !strings.Contains(i, "://") {
+		host, port, _ := utils.ParseStringToHostPort(i)
+		host = strings.Trim(host, `"' \r\n:`)
+		if host != "" && port > 0 {
+			return fmt.Sprintf("http://%v:%v", host, port)
+		}
+	}
+	return i
+}
