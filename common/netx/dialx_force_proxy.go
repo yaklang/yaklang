34b7b4df7193ebbbf5cceadc731dad7290da28e7
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-10-08 17:36:57 +0800

    fix(getProxyFromEnv): darwin import cycle

--- common/netx/dialx_force_proxy.go
+++ common/netx/dialx_force_proxy.go
@@ -3,7 +3,6 @@ package netx
 import (
 	"context"
 	"fmt"
-	"github.com/yaklang/yaklang/common/utils/systemproxy"
 	"net"
 	"net/url"
 	"os"
@@ -19,39 +18,6 @@ var ErrorProxyAuthFailed = utils.Error("invalid proxy username or password")
 
 func DialTCPTimeoutForceProxy(timeout time.Duration, target string, proxy string) (net.Conn, error) {
 	return connectForceProxy(nil, target, proxy, timeout)
-}
-
-func FixProxy(i string) string {
-	if i == "" {
-		return ""
-	}
-
-	if !strings.Contains(i, "://") {
-		host, port, _ := utils.ParseStringToHostPort(i)
-		host = strings.Trim(host, `"' \r\n:`)
-		if host != "" && port > 0 {
-			return fmt.Sprintf("http://%v:%v", host, port)
-		}
-	}
-	return i
-}
-
-func getProxyFromEnv() string {
-	if setting, err := systemproxy.Get(); err == nil && setting.Enabled && setting.DefaultServer != "" {
-		return setting.DefaultServer
-	}
-	for _, k := range []string{
-		"YAK_PROXY", "yak_proxy",
-		"HTTP_PROXY", "http_proxy",
-		"HTTPS_PROXY", "https_proxy",
-		"all_proxy", "all_proxy",
-		"proxy", "proxy",
-	} {
-		if p := strings.Trim(os.Getenv(k), `"`); p != "" {
-			return FixProxy(p)
-		}
-	}
-	return ""
 }
 
 func UnsetProxyFromEnv() {
