6b9da4ef00010ea3dde383a0e72dd817071bcff9
Q16G <760046475@qq.com>
2024-10-09 15:56:51 +0800

    test: add test

--- common/yak/ssaapi/test/php/cmdinj_test.go
+++ common/yak/ssaapi/test/php/cmdinj_test.go
@@ -106,3 +106,36 @@ test($_GET[1]);`
 		map[string][]string{"param": {"Undefined-_GET", "Undefined-_GET.1(valid)"}},
 		ssaapi.WithLanguage(ssaapi.PHP))
 }
+func TestDataflow(t *testing.T) {
+	/*
+		<include('php-param')> as $start;
+		<include('php-filter-function')> as $filter;
+		mysql_query(* as $param);
+		$param #{
+		include: `<self> & $start`,
+		exclude: `<self> & $filter`
+		}->  as $output
+	*/
+	code := `<?php
+$a = $_GET[1];
+
+if($c){
+    $a = filter($a);
+}else{
+    $a = unsafe($a);
+}
+eval($a);`
+	ssatest.CheckSyntaxFlow(t, code, `
+<include('php-param')> as $start;
+<include('php-filter-function')> as $filter;
+eval(* as $param);
+$param #{
+		include: <<<CODE
+<self> & $start
+CODE,
+		exclude: <<<CODE
+<self>?{opcode: call && <getCaller> & $filter}
+CODE
+}->  as $output
+`, map[string][]string{}, ssaapi.WithLanguage(ssaapi.PHP))
+}
