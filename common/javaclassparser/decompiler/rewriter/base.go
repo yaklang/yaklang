b89ed9a026d9e8cb0ed346f9cbffaf0b587aa555
z3 <z3r0ne@163.com>
2024-10-09 11:40:18 +0800

    support while do_while for for_each

--- common/javaclassparser/decompiler/rewriter/base.go
+++ common/javaclassparser/decompiler/rewriter/base.go
@@ -5,14 +5,16 @@ import (
 )
 
 const (
-	IfWriter = 1 << iota
-	ForWriter
-	SynchronizedWriter
-	WhileWriter
-	DoWhileWriter
-	TryWriter
-	SwitchWriter
-	TernaryWriter
+	IfRewriterFlag = 1 << iota
+	ForRewriterFlag
+	SynchronizedRewriterFlag
+	LoopRewriterFlag
+	WhileRewriterFlag
+	DoWhileReWriterFlag
+	TryRewriterFlag
+	SwitchRewriterFlag
+	TernaryRewriterFlag
+	BreakRewriterFlag
 )
 
 type rewriter struct {
@@ -29,12 +31,33 @@ func RegisterRewriter(writerType int, rewriterFunc rewriterFunc, checkStartNode 
 	}
 }
 func init() {
-	RegisterRewriter(IfWriter, RewriteIf, func(node *core.Node, manager *StatementManager) bool {
+	RegisterRewriter(IfRewriterFlag, RewriteIf, func(node *core.Node, manager *StatementManager) bool {
 		_, ok := node.Statement.(*core.ConditionStatement)
 		return ok
 	})
-	RegisterRewriter(SwitchWriter, SwitchRewriter, func(node *core.Node, manager *StatementManager) bool {
+	RegisterRewriter(SwitchRewriterFlag, SwitchRewriter, func(node *core.Node, manager *StatementManager) bool {
 		if v, ok := node.Statement.(*core.MiddleStatement); ok && v.Flag == core.MiddleSwitch {
+			return true
+		} else {
+			return false
+		}
+	})
+	RegisterRewriter(LoopRewriterFlag, LoopRewriter, func(node *core.Node, manager *StatementManager) bool {
+		if _, ok := node.Statement.(*core.GOTOStatement); ok {
+			return true
+		} else {
+			return false
+		}
+	})
+	RegisterRewriter(DoWhileReWriterFlag, DoWhileRewriter, func(node *core.Node, manager *StatementManager) bool {
+		if _, ok := node.Statement.(*core.ConditionStatement); ok {
+			return true
+		} else {
+			return false
+		}
+	})
+	RegisterRewriter(BreakRewriterFlag, BreakRewriter, func(node *core.Node, manager *StatementManager) bool {
+		if _, ok := node.Statement.(*core.GOTOStatement); ok {
 			return true
 		} else {
 			return false
