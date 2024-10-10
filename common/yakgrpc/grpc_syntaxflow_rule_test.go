2d290f6266bfeb3f90b97b18ee87aaec9f0e3463
intSheep <1098204193@qq.com>
2024-10-10 12:04:02 +0800

    refract(grpc-sf):refract sf rule and group CRUD

---
+++ common/yakgrpc/grpc_syntaxflow_rule_test.go
@@ -0,0 +1,129 @@
+package yakgrpc
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"testing"
+)
+
+func TestGRPCMUSTPASS_SyntaxFlow_Rule(t *testing.T) {
+	client, err := NewLocalClient()
+	require.NoError(t, err)
+
+	createRule := func(ruleName string) {
+		rule := &ypb.CreateSyntaxFlowRuleRequest{
+			SyntaxFlowInput: &ypb.SyntaxFlowRuleInput{
+				RuleName: ruleName,
+				Language: "java",
+			},
+		}
+		_, err := client.CreateSyntaxFlowRule(context.Background(), rule)
+		require.NoError(t, err)
+	}
+
+	queryRulesCount := func() int {
+		req := &ypb.QuerySyntaxFlowRuleRequest{
+			Pagination: &ypb.Paging{Limit: -1},
+		}
+		rsp, err := client.QuerySyntaxFlowRule(context.Background(), req)
+		require.NoError(t, err)
+		return len(rsp.GetRule())
+	}
+
+	deleteRuleByNames := func(names []string) {
+		for _, name := range names {
+			req := &ypb.DeleteSyntaxFlowRuleRequest{
+				Filter: &ypb.SyntaxFlowRuleFilter{
+					Rule: &ypb.SyntaxFlowRule{RuleName: name},
+				},
+			}
+			_, err = client.DeleteSyntaxFlowRule(context.Background(), req)
+			require.NoError(t, err)
+		}
+	}
+
+	updateRuleByNames := func(names []string) {
+		for _, name := range names {
+			req := &ypb.UpdateSyntaxFlowRuleRequest{
+				SyntaxFlowInput: &ypb.SyntaxFlowRuleInput{
+					RuleName: name,
+					Language: "php",
+				},
+			}
+			_, err = client.UpdateSyntaxFlowRule(context.Background(), req)
+			require.NoError(t, err)
+		}
+	}
+
+	t.Run("test create and delete syntax flow rule", func(t *testing.T) {
+		var ruleNames []string
+
+		beforeCreateCount := queryRulesCount()
+		for i := 0; i < 100; i++ {
+			ruleName := fmt.Sprintf("test_%s.sf", uuid.NewString())
+			createRule(ruleName)
+			ruleNames = append(ruleNames, ruleName)
+		}
+		afterCreateCount := queryRulesCount()
+		require.Equal(t, afterCreateCount-beforeCreateCount, 100)
+
+		deleteRuleByNames(ruleNames)
+		afterDeleteCount := queryRulesCount()
+		require.Equal(t, afterDeleteCount-beforeCreateCount, 0)
+	})
+
+	t.Run("test create and update  syntax flow rule", func(t *testing.T) {
+		var ruleNames []string
+
+		beforeCreateCount := queryRulesCount()
+		for i := 0; i < 100; i++ {
+			ruleName := fmt.Sprintf("test_%s.sf", uuid.NewString())
+			createRule(ruleName)
+			ruleNames = append(ruleNames, ruleName)
+		}
+		afterCreateCount := queryRulesCount()
+		require.Equal(t, afterCreateCount-beforeCreateCount, 100)
+
+		updateRuleByNames(ruleNames)
+		afterUpdateCount := queryRulesCount()
+		require.Equal(t, afterUpdateCount-afterCreateCount, 0)
+
+		deleteRuleByNames(ruleNames)
+		afterDeleteCount := queryRulesCount()
+		require.Equal(t, afterDeleteCount-beforeCreateCount, 0)
+	})
+
+	t.Run("test query syntax rule by key word", func(t *testing.T) {
+		ruleName := uuid.NewString()
+		createReq := &ypb.CreateSyntaxFlowRuleRequest{
+			SyntaxFlowInput: &ypb.SyntaxFlowRuleInput{
+				RuleName: ruleName,
+				Content: `desc(
+  title: '这是一个测试文件',
+  type: audit,
+  level: warning,
+)`,
+				Language: "java",
+			},
+		}
+		_, err := client.CreateSyntaxFlowRule(context.Background(), createReq)
+		require.NoError(t, err)
+
+		queryReq := &ypb.QuerySyntaxFlowRuleRequest{
+			Filter: &ypb.SyntaxFlowRuleFilter{
+				KeyWord: "这是一个测试文件",
+			},
+		}
+
+		rsp, err := client.QuerySyntaxFlowRule(context.Background(), queryReq)
+		require.NoError(t, err)
+		require.Equal(t, 1, len(rsp.GetRule()))
+		require.Equal(t, ruleName, rsp.GetRule()[0].RuleName)
+
+		deleteRuleByNames([]string{ruleName})
+	})
+
+}
--- common/yakgrpc/yakit/syntaxflow_rule.go
+++ common/yakgrpc/yakit/syntaxflow_rule.go
@@ -1,16 +1,14 @@
 package yakit
 
 import (
+	"errors"
 	"github.com/jinzhu/gorm"
 	"github.com/yaklang/yaklang/common/schema"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/bizhelper"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 	"strings"
-	"sync"
 )
-
-var syntaxFlowOpLock = new(sync.Mutex)
 
 func QuerySyntaxFlowRule(db *gorm.DB, params *ypb.QuerySyntaxFlowRuleRequest) (*bizhelper.Paginator, []*schema.SyntaxFlowRule, error) {
 	if params == nil {
@@ -40,44 +38,102 @@ func FilterSyntaxFlowRule(db *gorm.DB, params *ypb.SyntaxFlowRuleFilter) *gorm.D
 	if params == nil {
 		return db
 	}
-	if params.RuleName != "" {
-		db = db.Where("rule_name = ?", params.RuleName)
+	if params.GetAll() {
+		return db
 	}
-	if len(params.Language) > 0 {
-		db = bizhelper.ExactQueryStringArrayOr(db, "language", params.Language)
+	if params.GetUnsetGroup() {
+		db = db.Joins("LEFT JOIN syntax_flow_rule_groups P ON syntax_flow_rules.rule_name = P.rule_name")
+		db = db.Where("P.rule_name IS NULL")
+	} else if params.GetGroupName() != nil {
+		db = db.Joins("LEFT JOIN syntax_flow_rule_groups P ON syntax_flow_rules.rule_name = P.rule_name")
+		db = bizhelper.ExactQueryStringArrayOr(db, "`group_name`", params.GetGroupName())
 	}
-	if len(params.Purpose) > 0 {
-		db = bizhelper.ExactQueryStringArrayOr(db, "purpose", params.Purpose)
+	rule := params.GetRule()
+	keyWord := params.GetKeyWord()
+
+	if rule.GetRuleName() != "" {
+		db = db.Where("rule_name = ?", rule.GetRuleName())
 	}
-	if len(params.Severity) > 0 {
-		db = bizhelper.ExactQueryStringArrayOr(db, "severity", params.Severity)
+	if len(rule.GetLanguage()) > 0 {
+		db = db.Where("language = ?", rule.GetLanguage())
+	}
+	if len(rule.GetPurpose()) > 0 {
+		db = db.Where("purpose = ?", rule.GetPurpose())
+	}
+	if len(rule.GetSeverity()) > 0 {
+		db = db.Where("severity = ?", rule.GetSeverity())
 	}
 
-	tags := utils.StringArrayFilterEmpty(params.GetTag())
-	if len(tags) > 0 {
-		db = bizhelper.FuzzQueryStringArrayOrLike(db, "tag", tags)
+	if rule.GetTag() != "" {
+		db = bizhelper.FuzzQuery(db, "tag", rule.GetTag())
 	}
-	if params.Verified {
-		db = bizhelper.QueryByBool(db, "is_general_module", true)
+	if rule.GetVerified() {
+		db = bizhelper.QueryByBool(db, "verified", true)
 	}
-	if params.IsBuildInRule {
+	if rule.GetIsBuildInRule() {
 		db = bizhelper.QueryByBool(db, "is_build_in", true)
 	}
-	if params.AllowIncluded {
+	if rule.GetAllowIncluded() {
 		db = bizhelper.QueryByBool(db, "allow_included", true)
 	}
-	if params.GetKeyword() != "" {
+	if keyWord != "" {
 		db = bizhelper.FuzzSearchWithStringArrayOrEx(db, []string{
 			"rule_name", "title", "title_zh", "description", "content", "tag",
-		}, strings.Split(params.GetKeyword(), ","), false)
+		}, strings.Split(keyWord, ","), false)
 	}
+
 	return db
 }
 
-func DeleteSyntaxFlowRuleByFilter(db *gorm.DB, filter *ypb.SyntaxFlowRuleFilter) (int64, error) {
-	db = FilterSyntaxFlowRule(db, filter)
-	if db = db.Unscoped().Delete(&schema.SyntaxFlowRule{}); db.Error != nil {
-		return 0, utils.Errorf("delete syntax flow rule failed: %s", db.Error)
+func CreateSyntaxFlowRule(db *gorm.DB, rule *schema.SyntaxFlowRule) error {
+	if rule == nil {
+		return utils.Errorf("create syntaxFlow rule failed: rule is nil")
 	}
-	return db.RowsAffected, nil
+	if rule.RuleName == "" {
+		return utils.Errorf("create syntaxFlow rule failed: rule name is empty")
+	}
+
+	var findRule schema.SyntaxFlowRule
+	db = db.Model(&schema.SyntaxFlowRule{})
+	if err := db.First(&findRule, "rule_name = ?", rule.RuleName).Error; err == nil {
+		return utils.Errorf("create syntaxFlow rule failed: rule name %s already exists", rule.RuleName)
+	}
+
+	if err := db.Create(rule).Error; err != nil {
+		return utils.Errorf("create syntaxFlow rule failed: %s", err)
+	}
+	return nil
+}
+func UpdateSyntaxFlowRule(db *gorm.DB, rule *schema.SyntaxFlowRule) error {
+	if rule == nil {
+		return utils.Errorf("update syntaxFlow rule failed: rule is nil")
+	}
+	if rule.RuleName == "" {
+		return utils.Errorf("update syntaxFlow rule failed: rule name is empty")
+	}
+
+	findRule := schema.SyntaxFlowRule{}
+	db = db.Model(&schema.SyntaxFlowRule{})
+	db = db.First(&findRule, "rule_name = ?", rule.RuleName)
+	if err := db.Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return utils.Errorf("update syntaxFlow rule failed: rule name %s not found", rule.RuleName)
+		} else {
+			return utils.Errorf("update syntaxFlow rule failed: %s", err)
+		}
+	}
+	if err := db.Model(findRule).Update(rule).Error; err != nil {
+		return utils.Errorf("update syntaxFlow rule failed: %s", err)
+	}
+	return nil
+}
+
+func DeleteSyntaxFlowRule(db *gorm.DB, params *ypb.DeleteSyntaxFlowRuleRequest) (int64, error) {
+	db = db.Model(&schema.SyntaxFlowRule{})
+	if params == nil || params.Filter == nil {
+		return 0, utils.Errorf("delete syntaxFlow rule failed: synatx flow filter is nil")
+	}
+	db = FilterSyntaxFlowRule(db, params.Filter)
+	db = db.Unscoped().Delete(&schema.SyntaxFlowRule{})
+	return db.RowsAffected, db.Error
 }
