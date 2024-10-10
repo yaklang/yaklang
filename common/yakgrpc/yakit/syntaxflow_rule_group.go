8f1d875ab1275e1e4928fd0166f1cd333b0aa914
intSheep <1098204193@qq.com>
2024-10-10 12:04:02 +0800

    refract(grpc-sf):refract sf rule and group CRUD

---
+++ common/yakgrpc/yakit/syntaxflow_rule_group.go
@@ -0,0 +1,113 @@
+package yakit
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
+	"github.com/yaklang/yaklang/common/schema"
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/utils/bizhelper"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"strings"
+	"sync"
+)
+
+var syntaxFlowOpLock = new(sync.Mutex)
+
+type SyntaxFlowGroupResult struct {
+	RuleName      string
+	GroupName     string
+	IsBuildInRule bool
+	Count         int
+}
+
+func QuerySyntaxFlowRuleGroup(db *gorm.DB, params *ypb.QuerySyntaxFlowRuleGroupRequest) (result []*SyntaxFlowGroupResult, err error) {
+	if params == nil {
+		return nil, utils.Error("query syntax flow rule group failed: query params is nil")
+	}
+	db = db.Model(&schema.SyntaxFlowRuleGroup{})
+	db = FilterSyntaxFlowGroup(db, params.GetFilter())
+	db = db.Select("group_name, count(*) as count").Group("group_name").Order("count desc")
+	db = db.Scan(&result)
+	err = db.Error
+	return
+}
+
+func FilterSyntaxFlowGroup(db *gorm.DB, filter *ypb.SyntaxFlowRuleGroupFilter) *gorm.DB {
+	if filter == nil {
+		return db
+	}
+	if filter.GetAll() {
+		return db
+	}
+	if filter.GetIsBuiltinRule() {
+		db = bizhelper.QueryByBool(db, "is_build_in", true)
+	}
+	if filter.GetKeyWord() != "" {
+		db = bizhelper.FuzzSearchWithStringArrayOrEx(db, []string{
+			"rule_name", "group_name",
+		}, strings.Split(filter.GetKeyWord(), ","), false)
+	}
+	return db
+}
+
+func AddSyntaxFlowRulesGroup(db *gorm.DB, i *schema.SyntaxFlowRuleGroup) error {
+	syntaxFlowOpLock.Lock()
+	defer syntaxFlowOpLock.Unlock()
+	db = db.Model(&schema.SyntaxFlowRuleGroup{})
+	if i.RuleName == "" {
+		return utils.Error("add syntax flow rule group failed:rule name is empty")
+	}
+	if i.GroupName == "" {
+		return utils.Error("add syntax flow rule group failed:group name is empty")
+	}
+	hash := i.CalcHash()
+	var data schema.SyntaxFlowRuleGroup
+	if err := db.Where("hash = ?", hash).Find(&data).Error; err == nil {
+		return utils.Error("add syntax flow rule group failed: this rule already belongs to this group")
+	}
+	if db := db.Create(i); db.Error != nil {
+		return utils.Errorf("create SyntaxFlowGroup failed: %s", db.Error)
+	}
+	return nil
+}
+
+func UpdateSyntaxFlowRulesGroup(db *gorm.DB, i *schema.SyntaxFlowRuleGroup) error {
+	syntaxFlowOpLock.Lock()
+	defer syntaxFlowOpLock.Unlock()
+	db = db.Model(&schema.SyntaxFlowRuleGroup{})
+	if i.RuleName == "" {
+		return utils.Error("add syntax flow rule group failed:rule name is empty")
+	}
+	if i.GroupName == "" {
+		return utils.Error("add syntax flow rule group failed:group name is empty")
+	}
+	if err := db.Where("rule_name = ?", i.RuleName).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return utils.Errorf("update syntax flow rule group failed:the rule name %s not found", i.RuleName)
+		} else {
+			return utils.Errorf("update syntax flow rule group failed: %s", err)
+		}
+	}
+	if db := db.Model(&schema.SyntaxFlowRuleGroup{}).Where("rule_name = ?", i.RuleName).Update(i); db.Error != nil {
+		return utils.Errorf("update SyntaxFlowGroup failed: %s", db.Error)
+	}
+	return nil
+}
+
+func DeleteSyntaxFlowRuleGroup(db *gorm.DB, i *schema.SyntaxFlowRuleGroup) error {
+	syntaxFlowOpLock.Lock()
+	defer syntaxFlowOpLock.Unlock()
+	db = db.Model(&schema.SyntaxFlowRuleGroup{})
+	hash := i.CalcHash()
+	if err := db.Where("hash = ?", hash).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return utils.Errorf("delete syntax flow rule group failed: the rule group not found")
+		} else {
+			return utils.Errorf("delete syntax flow rule group failed: %s", err)
+		}
+	}
+	if db := db.Where("hash = ?", hash).Unscoped().Delete(&schema.SyntaxFlowRuleGroup{}); db.Error != nil {
+		return utils.Errorf("delete SyntaxFlowGroup failed: %s", db.Error)
+	}
+	return nil
+}
