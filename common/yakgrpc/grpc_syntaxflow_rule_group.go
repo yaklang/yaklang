4a01c4b9a5b3dbe9f11c972ef08ae9bec96246c1
intSheep <1098204193@qq.com>
2024-10-11 15:33:52 +0800

    feat(grpc-sf):The group name of query group cannot be empty

--- common/yakgrpc/grpc_syntaxflow_rule_group.go
+++ common/yakgrpc/grpc_syntaxflow_rule_group.go
@@ -2,29 +2,15 @@ package yakgrpc
 
 import (
 	"context"
-	"github.com/yaklang/yaklang/common/filter"
 	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
 func (s *Server) QuerySyntaxFlowRuleGroup(ctx context.Context, req *ypb.QuerySyntaxFlowRuleGroupRequest) (*ypb.QuerySyntaxFlowRuleGroupResponse, error) {
-	var rsps []*ypb.SyntaxFlowGroup
 	result, err := yakit.QuerySyntaxFlowRuleGroup(s.GetProfileDatabase(), req)
 	if err != nil {
 		return nil, err
 	}
-	filterGroup := filter.NewFilter()
-	for _, r := range result {
-		if filterGroup.Exist(r.GroupName) {
-			continue
-		}
-		rsp := &ypb.SyntaxFlowGroup{
-			GroupName: r.GroupName,
-			Count:     int32(r.Count),
-		}
-		rsps = append(rsps, rsp)
-		filterGroup.Insert(r.GroupName)
-	}
-	return &ypb.QuerySyntaxFlowRuleGroupResponse{Group: rsps}, nil
+	return &ypb.QuerySyntaxFlowRuleGroupResponse{Group: result}, nil
 }
 
