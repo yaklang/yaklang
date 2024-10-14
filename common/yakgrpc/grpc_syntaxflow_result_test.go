9bb0b6d68637aa627307718869c5674e77147d2b
wlz <lingze.wl@gmail.com>
2024-10-14 17:34:11 +0800

    feat(ssadb):support duplexConnection notify

--- common/yakgrpc/grpc_syntaxflow_result_test.go
+++ common/yakgrpc/grpc_syntaxflow_result_test.go
@@ -2,13 +2,18 @@ package yakgrpc
 
 import (
 	"context"
+	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/yak/ssa/ssadb"
 	"github.com/yaklang/yaklang/common/yak/ssaapi"
+	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
@@ -84,4 +89,65 @@ func TestGRPCMUSTPASS_SyntaxFlow_Result(t *testing.T) {
 		require.Equal(t, resultID3, uint(rsp.GetResults()[2].GetResultID()))
 	})
 
+}
+
+func TestGRPCMUSTPASS_Syntaxflow_Notify(t *testing.T) {
+	yakit.InitialDatabase()
+
+	local, err := NewLocalClient(true)
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	stream, err := local.DuplexConnection(ctx)
+	require.NoError(t, err)
+
+	taskID1 := ""
+	{
+		progName := uuid.NewString()
+		prog, err := ssaapi.Parse(`println("araa")`,
+			ssaapi.WithProgramName(progName), ssaapi.WithLanguage(ssaapi.Yak),
+		)
+		defer ssadb.DeleteProgram(ssadb.GetDB(), progName)
+		require.NoError(t, err)
+
+		taskID1 = uuid.NewString()
+		res := prog.SyntaxFlow(`println(* as $para); alert $para`)
+		resultID1, err := res.Save(taskID1)
+		defer ssadb.DeleteResultByID(resultID1)
+		defer yakit.DeleteRisk(consts.GetGormProjectDatabase(), &ypb.QueryRisksRequest{
+			RuntimeId: taskID1,
+		})
+		require.NoError(t, err)
+		_ = resultID1
+	}
+
+	passCheck := 0
+
+	var res *ypb.DuplexConnectionResponse
+	for {
+		res, err = stream.Recv()
+		log.Info(res)
+		log.Info(err)
+		if res.MessageType == "syntaxflow_result" {
+			var tmp map[string]string
+			err = json.Unmarshal(res.GetData(), &tmp)
+			require.NoError(t, err)
+			require.Equal(t, tmp["task_id"], taskID1)
+			passCheck++
+		}
+		if res.MessageType == "risk" {
+			_, risks, err := yakit.QueryRisks(consts.GetGormProjectDatabase(), &ypb.QueryRisksRequest{
+				RuntimeId: taskID1,
+			})
+			require.NoError(t, err)
+			require.Equal(t, 1, len(risks))
+			require.Equal(t, taskID1, risks[0].RuntimeId)
+			passCheck++
+		}
+
+		if passCheck == 2 {
+			break
+		}
+	}
+	cancel()
 }
