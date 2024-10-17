827ad9dc7303bf4c043077f94dd892760f4e35b4
rookie <rookie_is@qq.com>
2024-10-17 15:31:39 +0800

    fix(func_helper):fix Throttle ,remove Debounce use lo

--- common/utils/func_helper.go
+++ common/utils/func_helper.go
@@ -5,21 +5,21 @@ import (
 	"time"
 )
 
-func NewDebounce(wait float64) func(f func()) {
-	var timer *time.Timer
-	var mu sync.Mutex
-
-	return func(f func()) {
-		mu.Lock()
-		defer mu.Unlock()
-		if timer != nil {
-			timer.Stop()
-		}
-		timer = time.AfterFunc(FloatSecondDuration(wait), func() {
-			f()
-		})
-	}
-}
+//func NewDebounce(wait float64) func(f func()) {
+//	var timer *time.Timer
+//	var mu sync.Mutex
+//
+//	return func(f func()) {
+//		mu.Lock()
+//		defer mu.Unlock()
+//		if timer != nil {
+//			timer.Stop()
+//		}
+//		timer = time.AfterFunc(FloatSecondDuration(wait), func() {
+//			f()
+//		})
+//	}
+//}
 
 func NewThrottle(wait float64) func(f func()) {
 	var timer *time.Timer
@@ -29,8 +29,8 @@ func NewThrottle(wait float64) func(f func()) {
 		defer mu.Unlock()
 
 		if timer == nil {
+			f()
 			timer = time.AfterFunc(FloatSecondDuration(wait), func() {
-				f()
 				timer = nil
 			})
 		}
