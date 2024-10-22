2c9455310e28fa201abf2aefb2bea2a7084af3b1
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-10-22 11:03:45 +0800

    feat(sfweb): websocket allow any origin

--- common/sfweb/route_scan.go
+++ common/sfweb/route_scan.go
@@ -21,6 +21,9 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 type SyntaxFlowScanRequest struct {
