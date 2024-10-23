eb00d01948cd6531686f0fb72c5f8adc0790d8f1
Q16G <760046475@qq.com>
2024-10-23 17:30:38 +0800

    refactor(member-call): refactor ssa member call,split member-call to more file and modify and add  strategy

---
+++ common/yak/ssa/object.go
@@ -0,0 +1,44 @@
+package ssa
+
+import "github.com/yaklang/yaklang/common/utils"
+
+func (b *FunctionBuilder) CreateObjectWithSlice(vs []Value) *Make {
+	return b.BuildObjectAddFieldBuild(len(vs),
+		func(i int) Value { return NewConst(i) },
+		func(i int) Value { return vs[i] },
+	)
+}
+
+func (b *FunctionBuilder) CreateObjectWithMap(keys []Value, vs []Value) *Make {
+	return b.BuildObjectAddFieldBuild(len(vs),
+		func(i int) Value { return keys[i] },
+		func(i int) Value { return vs[i] },
+	)
+}
+
+func (b *FunctionBuilder) BuildObjectAddFieldBuild(size int, keys func(int) Value, value func(int) Value) *Make {
+	// lValueLen := NewConst(size)
+	var lValueLen Value = nil
+	itf := b.EmitMakeWithoutType(lValueLen, lValueLen)
+	if utils.IsNil(itf) {
+		return nil
+	}
+	if b.MarkedVariable != nil {
+		itf.SetName(b.MarkedVariable.GetName())
+		b.MarkedThisObject = itf
+		defer func() {
+			b.MarkedThisObject = nil
+		}()
+	}
+	ityp := NewObjectType()
+	itf.SetType(ityp)
+	for i := 0; i < size; i++ {
+		key := keys(i)
+		value := value(i)
+		v := b.CreateMemberCallVariable(itf, key)
+		b.AssignVariable(v, value)
+	}
+	ityp.Finish()
+	// ityp.Len = len(vs)
+	return itf
+}
