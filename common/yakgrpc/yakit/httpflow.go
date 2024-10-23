301474676491dfb00b7678d25bc08bd430e651dc
rookie <rookie_is@qq.com>
2024-10-23 15:42:48 +0800

    refactor(extracted):trace id use http flow hidden index.

--- common/yakgrpc/yakit/httpflow.go
+++ common/yakgrpc/yakit/httpflow.go
@@ -476,17 +476,19 @@ func UpdateHTTPFlowTags(db *gorm.DB, i *schema.HTTPFlow) (finErr error) {
 	}()
 
 	if i.ID > 0 {
-		if db = db.Where("id = ?", i.ID).UpdateColumn("tags", i.Tags); db.Error != nil {
+		i.Hash = i.CalcHash()
+		if db = db.Where("id = ?", i.ID).UpdateColumns(schema.HTTPFlow{Hash: i.Hash, Tags: tags}); db.Error != nil {
 			log.Errorf("update tags(by id) failed: %s", db.Error)
 			return db.Error
 		}
 	} else if i.HiddenIndex != "" {
-		if db = db.Where("hidden_index = ?", i.HiddenIndex).UpdateColumn("tags", i.Tags); db.Error != nil {
+		i.Hash = i.CalcHash()
+		if db = db.Where("hidden_index = ?", i.HiddenIndex).UpdateColumn(schema.HTTPFlow{Hash: i.Hash, Tags: tags}); db.Error != nil {
 			log.Errorf("update tags(by hidden_index) failed: %s", db.Error)
 			return db.Error
 		}
 	} else if i.Hash != "" {
-		if db = db.Where("hash = ?", i.HiddenIndex).UpdateColumn("tags", i.Tags); db.Error != nil {
+		if db = db.Where("hash = ?", i.Hash).UpdateColumn("tags", i.Tags); db.Error != nil {
 			log.Errorf("update tags(by hash) failed: %s", db.Error)
 			return db.Error
 		}
