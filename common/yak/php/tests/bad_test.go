aca3f57588a5dd8e0e6cb7745ad8ad21533c1445
rookie <rookie_is@qq.com>
2024-10-23 14:38:07 +0800

    test(php-lexer): add test case

--- common/yak/php/tests/bad_test.go
+++ common/yak/php/tests/bad_test.go
@@ -2,6 +2,9 @@ package tests
 
 import (
 	_ "embed"
+	"github.com/antlr/antlr4/runtime/Go/antlr/v4"
+	"github.com/stretchr/testify/require"
+	phpparser "github.com/yaklang/yaklang/common/yak/php/parser"
 	"github.com/yaklang/yaklang/common/yak/ssaapi"
 	"github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
 	"testing"
@@ -50,9 +53,42 @@ var bad_panic3 string
 
 func TestBadPanic3(t *testing.T) {
 	ssatest.NonStrictMockSSA(t, bad_panic3)
+	//lexer := phpparser.NewPHPLexer(antlr.NewInputStream(bad_panic3))
+	//lexer.RemoveErrorListeners()
+	//tokenStream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	//source := tokenStream.GetTokenSource()
+	//for {
+	//	token := source.NextToken()
+	//	fmt.Printf("%d:%s\n", token.GetTokenType(), token.GetText())
+	//	if token == nil || token.GetTokenType() == antlr.TokenEOF {
+	//		break
+	//	}
+	//}
 }
 
-func TestBaadCode(t *testing.T) {
+func TestPHPHtmlComment(t *testing.T) {
+	testCode := `/*
+<?php die(); ?>
+*/`
+	lexer := phpparser.NewPHPLexer(antlr.NewInputStream(testCode))
+	lexer.RemoveErrorListeners()
+	tokenStream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	source := tokenStream.GetTokenSource()
+	hasPHPstartToken := false
+	for {
+		token := source.NextToken()
+		if token == nil || token.GetTokenType() == antlr.TokenEOF {
+			break
+		}
+		if token.GetTokenType() == phpparser.PHPLexerPHPStart {
+			hasPHPstartToken = true
+		}
+	}
+	require.True(t, hasPHPstartToken, "PHPStart token not found")
+
+}
+
+func TestBadCode(t *testing.T) {
 	code := `/*
 /*
 
