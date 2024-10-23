d4b4666b288e0ef19adc23a1cc0d5daf386d9a87
Q16G <760046475@qq.com>
2024-10-23 17:30:38 +0800

    refactor(member-call): refactor ssa member call,split member-call to more file and modify and add  strategy

---
+++ common/yak/ssa/member_call_api.go
@@ -0,0 +1,55 @@
+package ssa
+
+import "github.com/yaklang/yaklang/common/log"
+
+// read member call variable, want method
+func (b *FunctionBuilder) ReadMemberCallMethod(object, key Value) Value {
+	return b.readMemberCallValueEx(object, key, true)
+}
+
+// read member call variable, want variable
+func (b *FunctionBuilder) ReadMemberCallValue(object, key Value) Value {
+	return b.readMemberCallValueEx(object, key, false)
+}
+
+func (b *FunctionBuilder) readMemberCallValueEx(object, key Value, wantFunction bool) Value {
+	if res := b.CheckMemberCallNilValue(object, key, "readMemberCallVariableEx"); res != nil {
+		return res
+	}
+
+	// to extern lib
+	if extern, ok := ToExternLib(object); ok {
+		return b.TryBuildExternLibValue(extern, key)
+	}
+
+	// normal member call
+	return b.getFieldValue(object, key, wantFunction)
+}
+
+// create member call variable
+func (b *FunctionBuilder) CreateMemberCallVariable(object, key Value) *Variable {
+	// check
+	if object.GetId() == -1 || key.GetId() == -1 {
+		log.Infof("CreateMemberCallVariable: %v, %v", object.GetName(), key)
+	}
+	// extern lib
+	if extern, ok := ToExternLib(object); ok {
+		name := getExternLibMemberCall(object, key)
+		ret := b.CreateVariable(name)
+		ret.SetMemberCall(extern, key)
+		return ret
+	}
+
+	// normal member call
+	// name := b.getFieldName(object, key)
+	res := checkCanMemberCallExist(object, key)
+	name := res.name
+	if object.GetOpcode() != SSAOpcodeParameter {
+		// if member not exist, create undefine member in object position
+		b.checkAndCreatDefaultMember(res, object, key)
+	}
+	// log.Infof("CreateMemberCallVariable: %v, %v", retValue.GetName(), key)
+	ret := b.CreateVariable(name)
+	ret.SetMemberCall(object, key)
+	return ret
+}
