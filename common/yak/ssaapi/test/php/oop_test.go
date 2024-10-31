ebe1d43f1ab4b1498ec95e7e91b5ffa0f6c9935f
Q16G <760046475@qq.com>
2024-10-31 22:36:24 +0800

    add(test)

--- common/yak/ssaapi/test/php/oop_test.go
+++ common/yak/ssaapi/test/php/oop_test.go
@@ -28,3 +28,63 @@ println($a->a);
 		"param": {"Undefined-AA-constructor", "Undefined-AA", "1"},
 	}, ssaapi.WithLanguage(ssaapi.PHP))
 }
+func TestCode(t *testing.T) {
+	code :=
+	code := `<?php
+
+namespace lib\common\fileOperator;
+class FileHandler{
+    public $file;
+    public $path = "C:\\temp\\store\\";
+    public function __construct(){
+
+    }
+    public function __read($file){
+        $this->file = $this->path.$file;
+        $this->file = $this->filter($this->file);
+        $fd = fopen($this->file);
+        $content = fread($fd);
+        fclose($fd);
+        return $content;
+    }
+    public function __write($file,$content){
+        $file = $this->filter($file);
+        file_put_contents($file,$content);
+    }
+    public function filter($filename){
+        return str_ireplace("../","",$filename);
+    }
+}
+
+namespace base\user\Constroller;
+
+use lib\common\fileOperator\FileHandler;
+
+class UserController extends Controller{
+    private $user= "";
+    private $pass = "";
+    public function Login(){
+        if(isset($_POST["user"])){
+            $this->user = $_POST["user"];
+        }
+        if(isset($_POST["pass"])){
+            $this->pass = $_POST["pass"];
+        }
+        $result = $mysql->query($this->user,$this->pass);
+        if($result){
+            $this->Cache();
+        }
+    }
+    public function Cache(){
+        $file = $this->user.$this->pass.".php";
+        $fd = new FileHandler();
+        $content = $fd->__read($file);
+        if($content==""){
+            $loginfo[0]=$this->user;
+            $loginfo[1]=$this->pass;
+            $fd->__write($file,json_encode($this->user));
+        }
+    }
+}`
+	ssatest.CheckSyntaxFlow(t, code, ``, map[string][]string{}, ssaapi.WithLanguage(ssaapi.PHP))
+}
