72868535e6f505fcb92b9327f751901cddb1ac99
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-11-01 16:30:59 +0800

    fix(sfweb): init call post init database

--- common/sfwebrunner/main.go
+++ common/sfwebrunner/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/sfweb"
 	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
 )
 
 var (
@@ -81,6 +82,8 @@ func main() {
 	}
 
 	app.Action = func(c *cli.Context) error {
+		yakit.InitialDatabase()
+
 		debug := c.Bool("debug")
 		port := c.Int("port")
 		opts := []sfweb.ServerOpt{
