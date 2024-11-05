399cf1bc25d0c85215456302847bf86177802208
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-11-05 11:29:15 +0800

    refactor(fs): interface

--- common/utils/filesys/filesys_interface/interface.go
+++ common/utils/filesys/filesys_interface/interface.go
@@ -5,45 +5,52 @@ import (
 	"os"
 )
 
-// FileSystem defines the methods of an abstract filesystem.
-type FileSystem interface {
-	ReadFile(name string) ([]byte, error)
-
-	Open(name string) (fs.File, error)
+type ReadOnlyFileSystem interface {
+	fs.ReadDirFS
+	fs.ReadFileFS
+	// OpenFile like Open but opens the named file with specified flag and perm.
 	OpenFile(name string, flag int, perm os.FileMode) (fs.File, error)
-	// RelOpen(name string) (fs.File, error)
-
 	// Stat returns a FileInfo describing the file.
 	// If there is an error, it should be of type *PathError.
 	Stat(name string) (fs.FileInfo, error)
-	// RelStat(name string) (fs.FileInfo, error)
-	// ReadDir reads the named directory
-	// and returns a list of directory entries sorted by filename.
-	ReadDir(name string) ([]fs.DirEntry, error)
 
-	Join(elem ...string) string
-	// Rel(targpath string) (string, error)
-
+	// ExtraInfo returns extra information about the fs.
+	ExtraInfo(string) map[string]any
+}
+type PathFileSystem interface {
 	GetSeparators() rune
-
+	Join(elem ...string) string
 	Base(string) string
 	PathSplit(string) (string, string)
 	Ext(string) string
 	IsAbs(string) bool
 	Getwd() (string, error)
 	Exists(string) (bool, error)
-	Rename(string, string) error
 	Rel(string, string) (string, error)
+}
+
+type WriteFileSystem interface {
+	Rename(string, string) error
 	WriteFile(string, []byte, os.FileMode) error
 	Delete(string) error
 	MkdirAll(string, os.FileMode) error
-	ExtraInfo(string) map[string]any
 }
 
-type TrashFS interface {
+// TrashFileSystem defines the methods of an abstract filesystem that can throw files away.
+// It's optional to implement this interface.
+type TrashFileSystem interface {
 	Throw(filenames ...string) error
 }
 
-type SyncFile interface {
+// SyncFileSystem defines the methods of an abstract filesystem that can sync.
+// It's optional to implement this interface.
+type SyncFileSystem interface {
 	Sync() error
+}
+
+// FileSystem defines the methods of an abstract filesystem.
+type FileSystem interface {
+	ReadOnlyFileSystem
+	PathFileSystem
+	WriteFileSystem
 }
