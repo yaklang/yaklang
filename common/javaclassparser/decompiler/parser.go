3a14af1f3b6e25bcc87f9693452af8da63ef5532
z3 <z3r0ne@163.com>
2024-11-07 15:43:24 +0800

    fix if rewriter

--- common/javaclassparser/decompiler/parser.go
+++ common/javaclassparser/decompiler/parser.go
@@ -5,7 +5,6 @@ import (
 	"github.com/yaklang/yaklang/common/javaclassparser/decompiler/core"
 	"github.com/yaklang/yaklang/common/javaclassparser/decompiler/core/statements"
 	"github.com/yaklang/yaklang/common/javaclassparser/decompiler/rewriter"
-	utils2 "github.com/yaklang/yaklang/common/javaclassparser/decompiler/utils"
 	"github.com/yaklang/yaklang/common/utils"
 )
 
@@ -27,7 +26,6 @@ func ParseBytesCode(decompiler *core.Decompiler) (res []statements.Statement, er
 	//core.GenerateDominatorTree(decompiler.RootNode)
 	statementManager := rewriter.NewRootStatementManager(decompiler.RootNode)
 	statementManager.SetId(decompiler.CurrentId)
-	utils2.DumpNodesToDotExp(decompiler.RootNode)
 	err = statementManager.Rewrite()
 	if err != nil {
 		return nil, err
