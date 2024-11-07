d17380549a6029a3afd2038b20ac2180317037d3
z3 <z3r0ne@163.com>
2024-11-07 11:40:47 +0800

    optimize loop jmp rewriter

--- common/javaclassparser/decompiler/parser.go
+++ common/javaclassparser/decompiler/parser.go
@@ -5,6 +5,7 @@ import (
 	"github.com/yaklang/yaklang/common/javaclassparser/decompiler/core"
 	"github.com/yaklang/yaklang/common/javaclassparser/decompiler/core/statements"
 	"github.com/yaklang/yaklang/common/javaclassparser/decompiler/rewriter"
+	utils2 "github.com/yaklang/yaklang/common/javaclassparser/decompiler/utils"
 	"github.com/yaklang/yaklang/common/utils"
 )
 
@@ -26,7 +27,7 @@ func ParseBytesCode(decompiler *core.Decompiler) (res []statements.Statement, er
 	//core.GenerateDominatorTree(decompiler.RootNode)
 	statementManager := rewriter.NewRootStatementManager(decompiler.RootNode)
 	statementManager.SetId(decompiler.CurrentId)
-	//utils2.DumpNodesToDotExp(decompiler.RootNode)
+	utils2.DumpNodesToDotExp(decompiler.RootNode)
 	err = statementManager.Rewrite()
 	if err != nil {
 		return nil, err
