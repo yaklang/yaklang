db7af2dd475d8f40cf33d87c80fd57fd52a58494
rookie <rookie_is@qq.com>
2024-11-08 11:17:11 +0800

    test(grpc-project): add export and import project test

--- common/yakgrpc/grpc_project_test.go
+++ common/yakgrpc/grpc_project_test.go
@@ -2,11 +2,19 @@ package yakgrpc
 
 import (
 	"context"
+	"errors"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/google/uuid"
+	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"io"
 	"testing"
+	"time"
 )
 
 func TestServer_UpdateProject(t *testing.T) {
@@ -45,4 +53,74 @@ func TestServer_UpdateProject(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func TestServer_Project_ExportAndImportProject(t *testing.T) {
+	client, err := NewLocalClient()
+	require.NoError(t, err)
+	ctx := utils.TimeoutContext(20 * time.Second) // set timeout
+	token := utils.RandStringBytes(10)
+	newProjectResp, err := client.NewProject(ctx, &ypb.NewProjectRequest{
+		ProjectName:   token,
+		Description:   "hello",
+		Type:          yakit.TypeProject,
+		ChildFolderId: 0,
+		FolderId:      0,
+	})
+	require.NoError(t, err)
+	defer func() {
+		yakit.DeleteProjectByProjectName(consts.GetGormProfileDatabase(), token)
+	}()
+
+	passwd := utils.RandStringBytes(10)
+	exportStream, err := client.ExportProject(ctx, &ypb.ExportProjectRequest{
+		ProjectName: token,
+		Password:    passwd,
+		Id:          newProjectResp.Id,
+	})
+	require.NoError(t, err)
+
+	exportPath := ""
+	for {
+		rsp, err := exportStream.Recv()
+		if err != nil {
+			if !errors.Is(err, io.EOF) {
+				t.Fatal(err)
+			}
+			break
+		}
+		if rsp.TargetPath != "" {
+			exportPath = rsp.TargetPath
+		}
+		spew.Dump(rsp)
+	}
+
+	token2 := utils.RandStringBytes(10)
+	importStream, err := client.ImportProject(ctx, &ypb.ImportProjectRequest{
+		ProjectFilePath:  exportPath,
+		Password:         passwd,
+		LocalProjectName: token2,
+	})
+	require.NoError(t, err)
+
+	for {
+		rsp, err := importStream.Recv()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			t.Fatal(err)
+		}
+		spew.Dump(rsp)
+	}
+
+	newProject, err := yakit.GetProjectByName(consts.GetGormProfileDatabase(), token2)
+	require.NoError(t, err)
+	require.Equal(t, newProject.ProjectName, token2)
+	defer func() {
+		yakit.DeleteProjectByProjectName(consts.GetGormProfileDatabase(), token2)
+	}()
+
+	_, err = gorm.Open(consts.SQLite, newProject.DatabasePath) // check db whether it is damaged
+	require.NoError(t, err)
 }
