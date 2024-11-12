6e466ec7e731f08e8b3cb35ae3c6cd7a80235d13
Q16G <760046475@qq.com>
2024-11-12 14:27:41 +0800

    feature(grpc-ssaProgram): feature grpc ssaProgram create、update and delete

--- common/schema/ssa_program.go
+++ common/schema/ssa_program.go
@@ -2,6 +2,7 @@ package schema
 
 import (
 	"github.com/jinzhu/gorm"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
 type SSAProgram struct {
@@ -14,4 +15,24 @@ type SSAProgram struct {
 	// program language when set
 	Language      string `json:"language" gorm:"type:varchar(255)"`
 	EngineVersion string `json:"engine_version" gorm:"type:varchar(255)"`
+}
+
+func (s *SSAProgram) ToGrpcProgram() *ypb.SsaProgram {
+	return &ypb.SsaProgram{
+		CreateTime:    s.CreatedAt.String(),
+		Name:          s.Name,
+		Description:   s.Description,
+		Dbpath:        s.DBPath,
+		Language:      s.Language,
+		EngineVersion: s.EngineVersion,
+	}
+}
+func ToSchemaSsaProgram(program *ypb.SsaProgram) *SSAProgram {
+	return &SSAProgram{
+		Name:          program.Name,
+		Description:   program.Description,
+		DBPath:        program.Dbpath,
+		Language:      program.Language,
+		EngineVersion: program.EngineVersion,
+	}
 }
---
+++ common/yakgrpc/grpc_ssa_program_test.go
@@ -0,0 +1,123 @@
+package yakgrpc
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/schema"
+	"github.com/yaklang/yaklang/common/yak/ssa/ssadb"
+	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"testing"
+)
+
+func TestSsaProgramsQuery(t *testing.T) {
+	name := uuid.NewString()
+	version := "test-version"
+	db := consts.GetGormProfileDatabase()
+	yakit.CreateSsaProgram(db, &schema.SSAProgram{
+		Name:          name,
+		Description:   "description",
+		Language:      "java",
+		EngineVersion: version,
+	})
+	ssadb.CreateProgram(name, "application", version)
+	defer func() {
+		yakit.DeleteSsaProgramWithName(db, name)
+	}()
+	queryCheck := func(programs []*ypb.SsaProgram) {
+		var flag bool
+		for _, program := range programs {
+			if program.GetName() == name {
+				flag = true
+			}
+		}
+		require.True(t, flag)
+	}
+	t.Run("query all", func(t *testing.T) {
+		client, err := NewLocalClient(true)
+		require.NoError(t, err)
+		programs, err := client.QuerySsaPrograms(context.Background(), &ypb.QuerySsaProgramRequest{
+			IsAll: true,
+		})
+		require.NoError(t, err)
+		queryCheck(programs.Programs)
+	})
+	t.Run("query with filter name", func(t *testing.T) {
+		client, err := NewLocalClient(true)
+		require.NoError(t, err)
+		programs, err := client.QuerySsaPrograms(context.Background(), &ypb.QuerySsaProgramRequest{
+			Filter: &ypb.SsaProgramFilter{
+				ProgramName: name,
+			},
+		})
+		require.NoError(t, err)
+		queryCheck(programs.Programs)
+	})
+	t.Run("query with filter description", func(t *testing.T) {
+		client, err := NewLocalClient(true)
+		require.NoError(t, err)
+		programs, err := client.QuerySsaPrograms(context.Background(), &ypb.QuerySsaProgramRequest{Filter: &ypb.SsaProgramFilter{
+			Keyword: "crip",
+		}})
+		require.NoError(t, err)
+		queryCheck(programs.Programs)
+	})
+	t.Run("query with Language", func(t *testing.T) {
+		client, err := NewLocalClient(true)
+		require.NoError(t, err)
+		programs, err := client.QuerySsaPrograms(context.Background(), &ypb.QuerySsaProgramRequest{Filter: &ypb.SsaProgramFilter{
+			Language: "java",
+		}})
+		require.NoError(t, err)
+		programs2, err2 := client.QuerySsaPrograms(context.Background(), &ypb.QuerySsaProgramRequest{Filter: &ypb.SsaProgramFilter{
+			Language: "php",
+		}})
+		require.NoError(t, err2)
+		require.True(t, programs2.Total == 0)
+		queryCheck(programs.Programs)
+	})
+}
+
+func TestDeleteProgramDeleteAll(t *testing.T) {
+	name := uuid.NewString()
+	version := "test-version"
+	db := consts.GetGormProfileDatabase()
+	yakit.CreateSsaProgram(db, &schema.SSAProgram{
+		Name:          name,
+		Description:   "description",
+		Language:      "java",
+		EngineVersion: version,
+	})
+	ssadb.CreateProgram(name, "application", version)
+	err := yakit.DeleteSsaProgram(db, &ypb.DeleteSsaProgramRequest{
+		IsAll: true,
+	})
+	require.NoError(t, err)
+	program, err := ssadb.GetProgram(name, "")
+	require.Error(t, err)
+	require.True(t, program == nil)
+}
+
+func TestDeleteProgramWithKeyword(t *testing.T) {
+	name := uuid.NewString()
+	version := "test-version"
+	db := consts.GetGormProfileDatabase()
+	yakit.CreateSsaProgram(db, &schema.SSAProgram{
+		Name:          name,
+		Description:   "description",
+		Language:      "java",
+		EngineVersion: version,
+	})
+	ssadb.CreateProgram(name, "application", version)
+	err := yakit.DeleteSsaProgram(db, &ypb.DeleteSsaProgramRequest{
+		Filter: &ypb.SsaProgramFilter{
+			Keyword: "desc",
+		},
+	})
+	require.NoError(t, err)
+	program, err := ssadb.GetProgram(name, "")
+	require.Error(t, err)
+	require.True(t, program == nil)
+}
