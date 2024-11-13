1a89efde581595bf8c6441c7a4e1bd07e289a36d
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-11-13 16:54:18 +0800

    feat(lib-global): add "callable" function

--- common/yak/yaklang/var.go
+++ common/yak/yaklang/var.go
@@ -1,19 +1,1 @@
 package yaklang
-
-import (
-	"github.com/yaklang/yaklang/common/yak/antlr4yak/yakvm"
-	"reflect"
-)
-
-func IsYakFunction(i interface{}) bool {
-	return IsNewYakFunction(i)
-}
-
-func IsNewYakFunction(i interface{}) bool {
-	_, ok := i.(*yakvm.Function)
-	if ok {
-		return true
-	}
-
-	return reflect.TypeOf(i).Kind() == reflect.Func
-}
