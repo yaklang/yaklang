fc0cc0a7f3336736e0a2a15363be1f255a7b81da
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-11-13 10:54:06 +0800

    fix(doc): fallback use go ast

--- common/yak/document_test.go
+++ common/yak/document_test.go
@@ -3,11 +3,21 @@ package yak
 import (
 	"testing"
 
+	"github.com/yaklang/yaklang/common/binx"
 	"github.com/yaklang/yaklang/common/yak/yaklang"
 )
 
 func TestEngineToDocumentHelperWithVerboseInfo(t *testing.T) {
 	engine := yaklang.New()
 	docs := EngineToDocumentHelperWithVerboseInfo(engine)
+	_ = docs
+}
+
+func TestDocumentHelperWithVerboseInfo(t *testing.T) {
+	t.SkipNow()
+	m := map[string]any{
+		"BinaryRead": binx.BinaryRead,
+	}
+	docs := DocumentHelperWithVerboseInfo(m)
 	_ = docs
 }
