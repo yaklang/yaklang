5a00af7b7c5d75655a16021710f4106aaec4d714
z3 <z3r0ne@163.com>
2024-11-14 12:04:05 +0800

    fix invoke dyn instr & fix throwable exception type

---
+++ common/javaclassparser/classes/static/TryCatch.java
@@ -0,0 +1,19 @@
+package org.benf.cfr.reader;
+
+public class TryCatch {
+	public TryCatch() {
+		return;
+	}
+	 void main() {
+		int var1 = 1;
+		try{
+			var1 = 2;
+			System.out.println(2);
+		}catch(Exception var2){
+			var1 = 3;
+			System.out.println(1);
+		}
+		System.out.println(var1);
+		return;
+	}
+}
