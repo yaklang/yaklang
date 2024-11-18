4f190e6486bfab51e1e398dd7ada4c45071f67ea
wlz <lingze.wl@gmail.com>
2024-11-18 14:39:14 +0800

    fix(classParser):use zipFS not self FS open file

--- common/javaclassparser/tests/decompiler_test.go
+++ common/javaclassparser/tests/decompiler_test.go
@@ -1,10 +1,16 @@
 package tests
 
 import (
+	"embed"
+	"os"
+	"testing"
+
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/yaklang/yaklang/common/javaclassparser"
 	"github.com/yaklang/yaklang/common/javaclassparser/classes"
-	"testing"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils/filesys"
 )
 
 func TestDecompiler(t *testing.T) {
@@ -53,4 +59,43 @@ func TestDecompiler(t *testing.T) {
 		})
 	}
 
+}
+
+//go:embed testfile
+var javazip embed.FS
+
+func TestDisCompilerJar(t *testing.T) {
+	// javaclassparser.NewJarFSFromLocal()
+	dir := os.TempDir()
+	jar, err := javazip.ReadFile("testfile/test.jar")
+	require.NoError(t, err)
+
+	jarPath := dir + "/test.jar"
+	err = os.WriteFile(jarPath, jar, 0644)
+	require.NoError(t, err)
+	// test jar filesystem
+	jarFs, err := javaclassparser.NewJarFSFromLocal(jarPath)
+	require.NoError(t, err)
+
+	t.Run("test jar walk", func(t *testing.T) {
+		fileList := make([]string, 0)
+		filesys.Recursive(
+			".",
+			filesys.WithFileSystem(jarFs),
+			filesys.WithStat(func(isDir bool, pathname string, info os.FileInfo) error {
+				if isDir {
+					return nil
+				}
+				fileList = append(fileList, pathname)
+
+				data, err := jarFs.ReadFile(pathname)
+				if err != nil {
+					require.NoErrorf(t, err, "read file %s failed: %v", pathname, err)
+				}
+				log.Info(string(data))
+				return nil
+			}),
+		)
+		require.True(t, len(fileList) > 0)
+	})
 }
