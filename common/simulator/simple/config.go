cf761ed6590152825ed963795b6071319891f798
baochenyang <baochenyang@4dogs.cn>
2024-11-20 14:23:52 +0800

    remove all must function & update

--- common/simulator/simple/config.go
+++ common/simulator/simple/config.go
@@ -27,13 +27,11 @@ type BrowserConfigOpt func(*BrowserConfig)
 
 // simulator.simple.wsAddress 是一个请求选项 用于输入浏览器的websocket地址
 //
-// Example:
-// ```
-//
+//	Example:
+//	```
 //	proxy = simulator.simple.proxy("http://127.0.0.1:7890")
 //	browser = simulator.simple.createBrowser(proxy)
-//
-// ```
+//	```
 func WithWsAddress(wsAddress string) BrowserConfigOpt {
 	return func(config *BrowserConfig) {
 		config.wsAddress = wsAddress
@@ -42,13 +40,11 @@ func WithWsAddress(wsAddress string) BrowserConfigOpt {
 
 // simulator.simple.exePath 是一个请求选项 用于输入浏览器的websocket地址
 //
-// Example:
-// ```
-//
+//	Example:
+//	```
 //	exePath = simulator.simple.exePath("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
 //	browser = simulator.simple.createBrowser(exePath)
-//
-// ```
+//	```
 func WithExePath(exePath string) BrowserConfigOpt {
 	return func(config *BrowserConfig) {
 		config.exePath = exePath
@@ -57,13 +53,11 @@ func WithExePath(exePath string) BrowserConfigOpt {
 
 // simulator.simple.proxy 是一个请求选项 用于输入代理服务器地址
 //
-// Example:
-// ```
-//
+//	Example:
+//	```
 //	proxy = simulator.simple.proxy("http://127.0.0.1:7890")
 //	browser = simulator.simple.createBrowser(proxy)
-//
-// ```
+//	```
 func WithProxy(proxyAddress string, proxyUserInfo ...string) BrowserConfigOpt {
 	return func(config *BrowserConfig) {
 		config.proxyAddress = proxyAddress
@@ -76,13 +70,11 @@ func WithProxy(proxyAddress string, proxyUserInfo ...string) BrowserConfigOpt {
 
 // simulator.simple.noSandBox 是一个请求选项 用于开启/关闭sandbox
 //
-// Example:
-// ```
-//
+//	Example:
+//	```
 //	sandBox = simulator.simple.noSandBox(true)
 //	browser = simulator.simple.createBrowser(sandBox)
-//
-// ```
+//	```
 func WithNoSandBox(noSandBox bool) BrowserConfigOpt {
 	return func(config *BrowserConfig) {
 		config.noSandBox = noSandBox
@@ -91,13 +83,11 @@ func WithNoSandBox(noSandBox bool) BrowserConfigOpt {
 
 // simulator.simple.headless 是一个请求选项 用于开启关闭headless模式
 //
-// Example:
-// ```
-//
+//	Example:
+//	```
 //	headless = simulator.simple.headless(true)
 //	browser = simulator.simple.createBrowser(headless)
-//
-// ```
+//	```
 func WithHeadless(headless bool) BrowserConfigOpt {
 	return func(config *BrowserConfig) {
 		config.headless = headless
@@ -106,13 +96,11 @@ func WithHeadless(headless bool) BrowserConfigOpt {
 
 // simulator.simple.hijack 是一个请求选项 用于开启流量劫持模式
 //
-// Example:
-// ```
-//
+//	Example:
+//	```
 //	hijack = simulator.simple.hijack(true)
 //	browser = simulator.simple.createBrowser(hijack)
-//
-// ```
+//	```
 func WithHijack(hijack bool) BrowserConfigOpt {
 	return func(config *BrowserConfig) {
 		config.hijack = hijack
@@ -161,13 +149,11 @@ func WithRequestModification(modifyUrl string, modifyTarget ModifyTarget, modify
 
 // simulator.simple.timeout 是一个请求选项 用于设置页面最大加载时间 单位秒
 //
-// Example:
-// ```
-//
+//	Example:
+//	```
 //	timeout = simulator.simple.timeout(30)
 //	browser = simulator.simple.createBrowser(timeout)
-//
-// ```
+//	```
 func WithTimeout(timeout int) BrowserConfigOpt {
 	return func(config *BrowserConfig) {
 		config.timeout = timeout
@@ -177,13 +163,11 @@ func WithTimeout(timeout int) BrowserConfigOpt {
 // simulator.simple.leakless 是一个请求选项 用于设置在程序运行结束时强行杀死浏览器
 // 注意在windows上可能会报毒，windows建议关闭
 //
-// Example:
-// ```
-//
+//	Example:
+//	```
 //	leakless = simulator.simple.leakless(true)
 //	browser = simulator.simple.createBrowser(leakless)
-//
-// ```
+//	```
 func WithLeakless(leakless bool) BrowserConfigOpt {
 	return func(config *BrowserConfig) {
 		config.leakless = leakless
--- common/simulator/simple/page.go
+++ common/simulator/simple/page.go
@@ -3,7 +3,6 @@ package simple
 import (
 	"encoding/base64"
 	"github.com/go-rod/rod"
-	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils"
 	"time"
 )
@@ -18,8 +17,7 @@ func (page *VPage) Navigate(urlStr string, waitFor string) error {
 	page.page = page.page.Timeout(time.Duration(page.timeout) * time.Second)
 	err = page.page.Navigate(urlStr)
 	if err != nil {
-		log.Error(err)
-		return err
+		return utils.Errorf("page navigate %s error: %v", urlStr, err)
 	}
 	if waitFor != "" {
 		err = page.page.WaitElementsMoreThan(waitFor, 0)
@@ -27,17 +25,19 @@ func (page *VPage) Navigate(urlStr string, waitFor string) error {
 		err = page.page.WaitLoad()
 	}
 	if err != nil {
-		log.Error(err)
-		return err
+		return utils.Errorf("page wait load error: %v", err)
 	}
 	//page.page = page.page.CancelTimeout()
 	return nil
 }
 
 // Element 返回页面中匹配css selector的一个元素
-// Example:
-// page, _ = browser.Navigate("https://example.com", "")
-// element, _ = page.Element("#pageName") // 匹配id为pageName的元素
+//
+//	Example:
+//	```
+//	page, _ = browser.Navigate("https://example.com", "")
+//	element, _ = page.Element("#pageName") // 匹配id为pageName的元素
+//	```
 func (page *VPage) Element(selector string) (*VElement, error) {
 	elements, err := page.page.Elements(selector)
 	if err != nil {
@@ -50,9 +50,12 @@ func (page *VPage) Element(selector string) (*VElement, error) {
 }
 
 // Elements 返回页面中匹配css selector的所有元素
-// Example:
-// page, _ = browser.Navigate("https://example.com", "")
-// elements, _ = page.Element("p") // 匹配所有p标签元素
+//
+//	Example:
+//	```
+//	page, _ = browser.Navigate("https://example.com", "")
+//	elements, _ = page.Element("p") // 匹配所有p标签元素
+//	```
 func (page *VPage) Elements(selector string) (VElements, error) {
 	var result VElements
 	elements, err := page.page.Elements(selector)
@@ -66,6 +69,7 @@ func (page *VPage) Elements(selector string) (VElements, error) {
 }
 
 // Click 点击页面中css selector匹配到的元素
+//
 // 同 element.Click()
 func (page *VPage) Click(selector string) error {
 	element, err := page.Element(selector)
@@ -80,6 +84,7 @@ func (page *VPage) Click(selector string) error {
 }
 
 // Input 在页面中css selector匹配到的元素中输入内容
+//
 // 同 element.Input(string)
 func (page *VPage) Input(selector, inputStr string) error {
 	element, err := page.Element(selector)
@@ -94,9 +99,12 @@ func (page *VPage) Input(selector, inputStr string) error {
 }
 
 // HTML 返回整个页面的html内容
-// Example:
-// page, _ = browser.Navigate("https://example.com", "")
-// html, _ = page.HTML()
+//
+//	Example:
+//	```
+//	page, _ = browser.Navigate("https://example.com", "")
+//	html, _ = page.HTML()
+//	```
 func (page *VPage) HTML() (string, error) {
 	html, err := page.page.HTML()
 	if err != nil {
