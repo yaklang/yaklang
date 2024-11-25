8d3ae15ce5f51394b50d08f68c542c2200db80b2
intSheep <1098204193@qq.com>
2024-11-25 10:33:01 +0800

    feat(template2java):add template language to java

---
+++ common/yak/java/template2java/template_parser.go
@@ -0,0 +1,23 @@
+package template2java
+
+import (
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/utils/memedit"
+	"github.com/yaklang/yaklang/common/yak/java/jsp"
+	"github.com/yaklang/yaklang/common/yak/templateLanguage"
+)
+
+var TemplateVisitors = map[templateLanguage.TemplateTyp]templateLanguage.VisitorCreator{
+	templateLanguage.TEMPLATE_JAVA_JSP: jsp.Creator,
+}
+
+func NewTemplateVisitor(editor *memedit.MemEditor, typ templateLanguage.TemplateTyp) (templateLanguage.TemplateVisitor, error) {
+	if visitorCreator, ok := TemplateVisitors[typ]; ok {
+		visitor, err := visitorCreator.Create(editor)
+		if err != nil {
+			return nil, err
+		}
+		return visitor, nil
+	}
+	return nil, utils.Errorf("not support template type: %v", typ)
+}
---
+++ common/yak/java/template2java/test/java_template_test.go
@@ -0,0 +1,55 @@
+package test
+
+import (
+	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/yak/java/java2ssa"
+	"github.com/yaklang/yaklang/common/yak/java/template2java"
+	"path/filepath"
+	"testing"
+)
+
+func checkJavaFront(t *testing.T, code string) {
+	_, err := java2ssa.Frontend(code, false)
+	require.NoError(t, err)
+}
+
+func TestCreateJavaTemplate(t *testing.T) {
+	tests := []struct {
+		fileName  string
+		filePath  string
+		modelName string
+		wants     []string
+	}{
+		{"demo.jsp", "D:\\java_project\\jspDemo\\src\\main\\webapp\\WEB-INF\\jsp", "demo", []string{
+			"package D_.java_project.jspDemo.src.main.webapp.WEB_INF.jsp;",
+			"public class demo_jsp",
+			`out.write("<html>");`,
+			`public void _JavaTemplateService(HttpServletRequest request, HttpServletResponse response)`,
+			`var1 = request.getAttribute("var1")`,
+		}},
+		{"++dmo.jsp.", "../com/org", "demo", []string{
+			"package com.org;",
+			"public class dmo_jsp_",
+		}},
+		{"qqq.ftl", "/com.org/A", "demo", []string{
+			"public class qqq_ftl",
+			"package com.org.A;",
+		}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.fileName, func(t *testing.T) {
+			jt, err := template2java.CreateJavaTemplate(filepath.Join(tt.filePath, tt.fileName))
+			require.NoError(t, err)
+			require.NotNil(t, jt)
+			jt.WritePureText("<html>")
+			jt.WriteOutput("var1")
+			jt.WriteEscapeOutput("var2")
+			jt.Finish()
+			checkJavaFront(t, jt.String())
+			for _, want := range tt.wants {
+				require.Contains(t, jt.String(), want)
+			}
+		})
+	}
+}
