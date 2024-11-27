3a96969989b937baaafe30777e85a479c30db9b7
intSheep <1098204193@qq.com>
2024-11-27 13:12:59 +0800

    feat(grpc):add create and delete sf group

--- common/yakgrpc/grpc_syntaxflow_rule_group.go
+++ common/yakgrpc/grpc_syntaxflow_rule_group.go
@@ -14,3 +14,23 @@ func (s *Server) QuerySyntaxFlowRuleGroup(ctx context.Context, req *ypb.QuerySyn
 	return &ypb.QuerySyntaxFlowRuleGroupResponse{Group: result}, nil
 }
 
+func (s *Server) DeleteSyntaxFlowRuleGroup(ctx context.Context, req *ypb.DeleteSyntaxFlowRuleGroupRequest) (*ypb.DbOperateMessage, error) {
+	msg := &ypb.DbOperateMessage{
+		TableName: "syntax_flow_rule_group",
+		Operation: DbOperationDelete,
+	}
+	count, err := yakit.DeleteSyntaxFlowRuleGroup(s.GetProfileDatabase(), req)
+	msg.EffectRows = count
+	return msg, err
+}
+
+func (s *Server) CreateSyntaxFlowRuleGroup(ctx context.Context, req *ypb.CreateSyntaxFlowRuleGroupRequest) (*ypb.DbOperateMessage, error) {
+	msg := &ypb.DbOperateMessage{
+		TableName:  "syntax_flow_rule_group",
+		Operation:  DbOperationCreate,
+		EffectRows: 1,
+	}
+	count, err := yakit.CreateSyntaxFlowRuleGroup(s.GetProfileDatabase(), req)
+	msg.EffectRows = count
+	return msg, err
+}
