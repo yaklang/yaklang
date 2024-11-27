ae1ed505c84ac55634efa290ec6472b2dd3b0fed
intSheep <1098204193@qq.com>
2024-11-27 10:38:05 +0800

    fix(jsp2java):el expr use elExpr.parse method

--- common/yak/java/template2java/test/jsp_to_java_test.go
+++ common/yak/java/template2java/test/jsp_to_java_test.go
@@ -18,23 +18,24 @@ func TestJSP2Java_Content(t *testing.T) {
 		{"test  JspElementWithOpenTagOnly pure text  ", "<html>", []string{"out = request.getOut(); ", `out.write("<html>")`}},
 		{"test JspElementWithClosingTagOnly pure text  ", "<html/>", []string{`out.write("<html/>");`}},
 		{"test JspElementWithTagAndContent pure text  ", "<title>hello</title>", []string{`out.write("<title>");`, `out.write("hello");`, `out.write("</title>");`}},
-		{"stest style", " <style>\n        .section { margin: 20px; padding: 10px; border: 1px solid #ccc; }\n    </style>", []string{"out.write(\"        .section { margin: 20px; padding: 10px; border: 1px solid #ccc; }\");"}},
+		{"stest style", " <style>\n        .section { margin: 20px; padding: 10px; border: 1px solid #ccc; }\n    </style>", []string{"out.write(\".section { margin: 20px; padding: 10px; border: 1px solid #ccc; }\");"}},
 		{"test jsp java script", "<%\n    int sum = 5 + 10;\n    out.println(\"Sum is: \" + sum);\n%>", []string{"int sum = 5 + 10;", "out.println(\"Sum is: \" + sum);"}},
 		{"test jsp expression script", `<%= request.getParameter("userInput") %>`, []string{`out.print( request.getParameter("userInput") )`}},
 		{"test jsp declaration script", `<%! int count = 0; %>`, []string{`int count = 0;`}},
 		{"test jsp directive script import", `<%@ page import="java.util.*, com.example.model.User" %>`, []string{`import  com.example.model.User;`, `import java.util.*;`}},
 		// core tag
 		// core out tag
-		{"test jstl-core out tag", "<c:out value='${name}'/>", []string{`name = request.getAttribute("name");`, `out.print(escapeHtml(name));`}},
-		{"test jstl-core out tag without escaping", "<c:out value='${name}' escapeXml=\"false\"/>", []string{`name = request.getAttribute("name");`, `out.print(name);`}},
+		{"test jstl-core out tag", "<c:out value='${name}'/>", []string{`out.print(escapeHtml(elExpr.parse("${name}")));`}},
+		{"test jstl-core out tag without escaping", "<c:out value='${name}' escapeXml=\"false\"/>", []string{`out.print(elExpr.parse("${name}"));`}},
 		// core set tag
 		{"test jstl-core set tag", "<c:set var='name' value='John'/>", []string{`request.setAttribute("name", John);`}},
 		// core if tag
-		{"test jstl-core if tag 1", "<c:if test='${age  <  16 }'>Hello John</c:if>", []string{`if (age  <  16 ) {`, `out.write("Hello John");`}},
+		{"test jstl-core if tag 1", "<c:if test='${age  <  16 }'>Hello John</c:if>", []string{`if (elExpr.parse("${age  <  16 }")) {`, `out.write("Hello John");`}},
 		{"test jstl-core if tag 2",
 			" <c:if test=\"${sessionScope.userType == 'admin'}\">\n        <p>Welcome, Admin! Your user type is: ${sessionScope.userType}</p>\n    </c:if>",
-			[]string{`if (sessionScope.userType == "admin") {`, `out.write("<p>");`, `out.print(sessionScope.userType);`, `out.write("Welcome, Admin! Your user type is: ");`},
+			[]string{`if (elExpr.parse("${sessionScope.userType == 'admin'}")) {`, `out.write("<p>");`, `out.print(elExpr.parse("${sessionScope.userType}"));`, `out.write("Welcome, Admin! Your user type is:");`},
 		},
+		//core choose tag
 	}
 	check := func(jspCode string, wants []string) {
 		codeInfo, err := tj.ConvertTemplateToJava(tj.JSP, jspCode, "test.jsp")
