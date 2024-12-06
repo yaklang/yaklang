412041357524c7241c16f9d78aeaaef153fa09fb
wlz <lingze.wl@gmail.com>
2024-12-06 17:06:04 +0800

    fix(filesys):peehole return file content

--- common/utils/filesys/peephole_test.go
+++ common/utils/filesys/peephole_test.go
@@ -1,14 +1,18 @@
-package filesys
+package filesys_test
 
 import (
+	"io/fs"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils/filesys"
+	"github.com/yaklang/yaklang/common/utils/filesys/filesys_interface"
 	fi "github.com/yaklang/yaklang/common/utils/filesys/filesys_interface"
 )
 
 func TestPeephole(t *testing.T) {
-	vfs := NewVirtualFs()
+	vfs := filesys.NewVirtualFs()
 	vfs.AddFile("a1", "a")
 	vfs.AddFile("a2", "a")
 	vfs.AddFile("a3", "a")
@@ -23,10 +27,34 @@ func TestPeephole(t *testing.T) {
 	vfs.AddFile("c/d/e4", "a")
 	vfs.AddFile("c/d/e6", "a")
 
-	count := 0
-	Peephole(vfs, WithPeepholeSize(3), WithPeepholeCallback(func(system fi.FileSystem) {
-		count++
-		TreeView(system)
-	}))
-	assert.Equal(t, count, 6)
+	checkFilesystem := func(t *testing.T, system filesys_interface.FileSystem) {
+		filesys.TreeView(system)
+		fileCount := 0
+		filesys.Recursive(".",
+			filesys.WithFileSystem(system),
+			filesys.WithFileStat(func(s string, fi fs.FileInfo) error {
+				log.Infof("file: %s", s)
+				data, err := system.ReadFile(s)
+				require.NoError(t, err)
+				log.Infof("data: %s\n", string(data))
+				require.Greater(t, len(data), 0)
+				fileCount++
+				return nil
+			}),
+		)
+		require.Greater(t, fileCount, 0)
+	}
+
+	t.Run("test size", func(t *testing.T) {
+		count := 0
+		filesys.Peephole(vfs,
+			filesys.WithPeepholeSize(3),
+			filesys.WithPeepholeCallback(func(system fi.FileSystem) {
+				count++
+				checkFilesystem(t, system)
+			}),
+		)
+		require.Equal(t, count, 6)
+	})
+
 }
---
+++ common/utils/map.go
@@ -0,0 +1,60 @@
+package utils
+
+import "sync"
+
+type SafeMapWithKey[K comparable, V any] struct {
+	mu sync.RWMutex
+	m  map[K]V
+}
+
+type SafeMap[V any] struct {
+	*SafeMapWithKey[string, V]
+}
+
+func NewSafeMap[V any]() *SafeMap[V] {
+	return &SafeMap[V]{
+		SafeMapWithKey: NewSafeMapWithKey[string, V](),
+	}
+}
+
+func NewSafeMapWithKey[K comparable, V any]() *SafeMapWithKey[K, V] {
+	return &SafeMapWithKey[K, V]{
+		m: make(map[K]V),
+	}
+}
+
+func (sm *SafeMapWithKey[K, V]) Get(key K) (V, bool) {
+	sm.mu.RLock()
+	defer sm.mu.RUnlock()
+	val, ok := sm.m[key]
+	return val, ok
+}
+
+func (sm *SafeMapWithKey[K, V]) Set(key K, value V) {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	sm.m[key] = value
+}
+
+func (sm *SafeMapWithKey[K, V]) Delete(key K) {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	delete(sm.m, key)
+}
+
+func (sm *SafeMapWithKey[K, V]) ForEach(f func(key K, value V) bool) {
+	sm.mu.RLock()
+	keys := make([]K, 0, len(sm.m))
+	values := make([]V, 0, len(sm.m))
+	for k, v := range sm.m {
+		keys = append(keys, k)
+		values = append(values, v)
+	}
+	sm.mu.RUnlock()
+
+	for i, k := range keys {
+		if !f(k, values[i]) {
+			break
+		}
+	}
+}
