a18a1be45a240d9e83e55cfc6081a5e442ba89ca
wlz <lingze.wl@gmail.com>
2024-12-06 17:06:04 +0800

    fix(filesys):peehole return file content

--- common/utils/filesys/peephole.go
+++ common/utils/filesys/peephole.go
@@ -1,17 +1,19 @@
 package filesys
 
 import (
+	"io/fs"
+
+	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils"
 	fi "github.com/yaklang/yaklang/common/utils/filesys/filesys_interface"
-	"io/fs"
-	"sync"
 )
 
 type PeepholeConfig struct {
-	Size     int
-	Callback func(f fi.FileSystem)
+	// peephole handler
+	Size int
 
-	cache *sync.Map
+	Callback         func(f fi.FileSystem)
+	fileContentCache *utils.SafeMap[[]byte]
 }
 
 type PeepholeTrigger struct {
@@ -19,19 +21,80 @@ type PeepholeTrigger struct {
 	Infos []fs.FileInfo
 }
 
-type PeepholeConfigOption func(*PeepholeConfig)
+type PeepholeConfigOption func(*PeepholeConfig) error
 
 func WithPeepholeSize(i int) PeepholeConfigOption {
-	return func(c *PeepholeConfig) {
-		if i > 0 {
-			c.Size = i
+	return func(c *PeepholeConfig) error {
+		if i <= 0 {
+			return utils.Errorf("size must be positive")
 		}
+		c.Size = i
+		return nil
 	}
 }
 
 func WithPeepholeCallback(i func(system fi.FileSystem)) PeepholeConfigOption {
-	return func(config *PeepholeConfig) {
+	return func(config *PeepholeConfig) error {
 		config.Callback = i
+		return nil
+	}
+}
+
+func defaultPeepholeConfig(opts ...PeepholeConfigOption) (*PeepholeConfig, error) {
+	config := &PeepholeConfig{
+		Callback: nil,
+		// triggerCache:     utils.NewSafeMap[*PeepholeTrigger](),
+		fileContentCache: utils.NewSafeMap[[]byte](),
+	}
+
+	for _, o := range opts {
+		if err := o(config); err != nil {
+			return nil, err
+		}
+	}
+	if config.Size == 0 {
+		return nil, utils.Errorf("size must be set")
+	}
+	return config, nil
+}
+
+func (c *PeepholeConfig) CallbackFS(f fi.FileSystem, trigger *PeepholeTrigger) {
+	if c == nil || c.Callback == nil {
+		return
+	}
+	// calculate step
+	step := len(trigger.Infos)
+	if c.Size != 0 {
+		step = c.Size
+	}
+
+	// foreach trigger.Infos with size as step
+	for i := 0; i < len(trigger.Infos); i += step {
+		end := i + step
+		if end > len(trigger.Infos) {
+			end = len(trigger.Infos)
+		}
+		infos := trigger.Infos[i:end]
+		path := trigger.Path
+
+		// create virtual fs
+		vfs := NewVirtualFs()
+		for _, i := range infos {
+			name := i.Name()
+			filePath := f.Join(path, name)
+			// get file content
+			raw, ok := c.fileContentCache.Get(filePath)
+			if !ok {
+				var err error
+				raw, err = f.ReadFile(filePath)
+				if err != nil {
+					log.Errorf("read file %s error %v", filePath, err)
+				}
+				c.fileContentCache.Set(filePath, raw)
+			}
+			vfs.AddFile(name, string(raw))
+		}
+		c.Callback(vfs)
 	}
 }
 
@@ -49,61 +112,36 @@ func Peephole(f fi.FileSystem, opts ...PeepholeConfigOption) error {
 	if start == "" {
 		return utils.Error("no entry found")
 	}
-
-	config := &PeepholeConfig{
-		Size:     3,
-		Callback: nil,
-		cache:    new(sync.Map),
+	c, err := defaultPeepholeConfig(opts...)
+	if err != nil {
+		return err
 	}
 
-	for _, o := range opts {
-		o(config)
-	}
-	
-	recursiveErr := Recursive(start, WithFileSystem(f), WithFileStat(func(s string, info fs.FileInfo) error {
-		dirName, fileName := f.PathSplit(s)
-		if fileName == "" {
+	triggerCache := utils.NewSafeMap[*PeepholeTrigger]()
+	recursiveErr := Recursive(
+		start,
+		WithFileSystem(f),
+		WithFileStat(func(s string, info fs.FileInfo) error {
+			dirName, fileName := f.PathSplit(s)
+			if fileName == "" {
+				return nil
+			}
+			trigger, ok := triggerCache.Get(dirName)
+			if !ok {
+				trigger = &PeepholeTrigger{
+					Path: dirName,
+				}
+				triggerCache.Set(dirName, trigger)
+			}
+			trigger.Infos = append(trigger.Infos, info)
 			return nil
-		}
+		}),
+	)
 
-		var trigger *PeepholeTrigger
-		anyTrigger, ok := config.cache.Load(dirName)
-		if !ok {
-			trigger = &PeepholeTrigger{
-				Path: dirName,
-			}
-			config.cache.Store(dirName, trigger)
-		} else {
-			trigger = anyTrigger.(*PeepholeTrigger)
-		}
-		trigger.Infos = append(trigger.Infos, info)
-		if len(trigger.Infos) >= config.Size {
-			vfs := NewVirtualFs()
-			for _, i := range trigger.Infos {
-				raw, _ := f.ReadFile(i.Name())
-				vfs.AddFile(i.Name(), string(raw))
-			}
-			if config.Callback != nil {
-				config.Callback(vfs)
-			}
-			config.cache.Delete(dirName)
-		}
-		return nil
-	}))
-	config.cache.Range(func(key, value any) bool {
-		result, ok := value.(*PeepholeTrigger)
-		if !ok {
-			return true
-		}
-		vfs := NewVirtualFs()
-		for _, i := range result.Infos {
-			raw, _ := f.ReadFile(i.Name())
-			vfs.AddFile(i.Name(), string(raw))
-		}
-		if config.Callback != nil {
-			config.Callback(vfs)
-		}
+	triggerCache.ForEach(func(key string, trigger *PeepholeTrigger) bool {
+		c.CallbackFS(f, trigger)
 		return true
 	})
+
 	return recursiveErr
 }
