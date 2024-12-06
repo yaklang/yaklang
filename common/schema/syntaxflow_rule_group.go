3c576c206958ffb771fb46f33a68258f3c18be4d
intSheep <1098204193@qq.com>
2024-12-06 11:15:58 +0800

    feat(grpc):add syntaxflow group

--- common/schema/syntaxflow_rule_group.go
+++ common/schema/syntaxflow_rule_group.go
@@ -5,11 +5,13 @@ import (
 	"github.com/yaklang/yaklang/common/utils"
 )
 
+// SyntaxFlowRuleGroup Syntax Flow规则和组的关系表
 type SyntaxFlowRuleGroup struct {
 	gorm.Model
-	RuleName  string `gorm:"index"`
-	GroupName string `gorm:"index"`
-	Hash      string `gorm:"unique_index"`
+	RuleName  string `json:"rule_name" gorm:"index"`
+	GroupName string `json:"group_name" gorm:"index"`
+	IsBuildIn bool   `json:"is_build_in"`
+	Hash      string `json:"hash" gorm:"unique_index"`
 }
 
 func (s *SyntaxFlowRuleGroup) BeforeSave() error {
@@ -20,4 +22,10 @@ func (s *SyntaxFlowRuleGroup) BeforeSave() error {
 func (s *SyntaxFlowRuleGroup) CalcHash() string {
 	s.Hash = utils.CalcSha256(s.RuleName, s.GroupName)
 	return s.Hash
+}
+
+type SyntaxFlowGroup struct {
+	gorm.Model
+	GroupName string `json:"group_name" gorm:"unique_index"`
+	IsBuildIn bool   `json:"is_build_in" gorm:"index"`
 }
