8bbedd90052cfdccb8027d4347365b598ccaa953
VillanCh <188228725@qq.com>
2024-12-09 21:06:26 +0800

    add zip lib

--- common/yak/yaklib/zip.go
+++ common/yak/yaklib/zip.go
@@ -5,7 +5,9 @@ import (
 	"bytes"
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/utils/filesys"
 	"github.com/yaklang/yaklang/common/utils/ziputil"
+	"os"
 )
 
 var ZipExports = map[string]interface{}{
@@ -13,9 +15,71 @@ var ZipExports = map[string]interface{}{
 	"Compress": func(zipName string, filenames ...string) error {
 		return ziputil.CompressByName(filenames, zipName)
 	},
-	"CompressRaw": CompressRaw,
+	"CompressRaw":      CompressRaw,
+	"Recursive":        Recursive,
+	"RecursiveFromRaw": RecursiveFromRaw,
 }
 
+// Recursive Decompress decompresses a zip file to a directory
+// Example:
+// ```
+//
+//	zip.Decompress("/tmp/abc.zip", (isDir, pathName, info) => {
+//			log.info("isDir: %v, pathName: %v, info: %v", isDir, pathName, info.Name())
+//	})~
+//
+// ```
+func Recursive(i any, cb func(isDir bool, pathName string, info os.FileInfo) error) error {
+	if result := utils.GetFirstExistedFile(utils.InterfaceToString(i)); result != "" {
+		zfs, err := filesys.NewZipFSFromLocal(result)
+		if err != nil {
+			return utils.Errorf("create zip fs failed: %v", err)
+		}
+		return filesys.SimpleRecursive(filesys.WithFileSystem(zfs), filesys.WithStat(func(isDir bool, pathname string, info os.FileInfo) error {
+			if cb == nil {
+				return utils.Error("callback is nil")
+			}
+			return cb(isDir, pathname, info)
+		}))
+	}
+	return utils.Errorf("file not found: %v", i)
+}
+
+// RecursiveFromRaw decompresses a zip file to a directory
+// Example:
+// ```
+//
+//	raw = file.ReadFile("/tmp/abc.zip")~
+//	zip.RecursiveFromRawBytes(raw, (isDir, pathName, info) => {
+//			log.info("isDir: %v, pathName: %v, info: %v", isDir, pathName, info.Name())
+//	})
+//
+// ```
+func RecursiveFromRaw(i any, cb func(isDir bool, pathName string, info os.FileInfo) error) error {
+	raw := utils.InterfaceToString(i)
+	zfs, err := filesys.NewZipFSFromString(raw)
+	if err != nil {
+		return utils.Errorf("create zip fs failed: %v", err)
+	}
+	return filesys.SimpleRecursive(filesys.WithFileSystem(zfs), filesys.WithStat(func(isDir bool, pathname string, info os.FileInfo) error {
+		if cb == nil {
+			return utils.Error("callback is nil")
+		}
+		return cb(isDir, pathname, info)
+	}))
+}
+
+// CompressRaw compresses a map to a zip file
+// Example:
+// ```
+//
+//	zipBytes = zip.CompressRaw({
+//		 "a.txt": "hello",
+//	     "b.txt": "world",
+//	})~
+//	zipBytes2, err = zip.CompressRaw({ "a.txt": "hello", "b.txt": file.ReadFile("/tmp/external-file-name.txt")~ })
+//
+// ```
 func CompressRaw(i any) ([]byte, error) {
 	if !utils.IsMap(i) {
 		return nil, utils.Error("input must be a map")
@@ -24,7 +88,7 @@ func CompressRaw(i any) ([]byte, error) {
 	var buf bytes.Buffer
 	zipFp := zip.NewWriter(&buf)
 	count := 0
-	for k, v := range utils.InterfaceToMap(i) {
+	for k, v := range utils.InterfaceToGeneralMap(i) {
 		log.Infof("start to compress %s size: %v", k, utils.ByteSize(uint64(len(utils.InterfaceToString(v)))))
 		kw, err := zipFp.Create(k)
 		if err != nil {
