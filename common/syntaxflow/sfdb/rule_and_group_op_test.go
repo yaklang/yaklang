db94eb238df4bc076b94c5c166c4325cb7f102e0
WAY29 <41830147+WAY29@users.noreply.github.com>
2024-12-09 14:59:18 +0800

    refactor(sf_db): sf rule group op utils functions

--- common/syntaxflow/sfdb/rule_and_group_op_test.go
+++ common/syntaxflow/sfdb/rule_and_group_op_test.go
@@ -1,25 +1,30 @@
 package sfdb
 
 import (
+	"testing"
+
 	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/consts"
 	"github.com/yaklang/yaklang/common/schema"
-	"testing"
 )
 
 func TestRule_OP(t *testing.T) {
+	db := consts.GetGormProfileDatabase()
+
 	t.Run("create and delete rule", func(t *testing.T) {
 		ruleName := uuid.NewString()
 		rule := &schema.SyntaxFlowRule{RuleName: ruleName}
 		_, err := CreateRule(rule)
 		require.NoError(t, err)
-		got, err := QueryRuleByName(ruleName)
+		got, err := QueryRuleByName(db, ruleName)
+		require.NoError(t, err)
 		require.Equal(t, ruleName, got.RuleName)
 
 		err = DeleteRuleByRuleName(ruleName)
 		require.NoError(t, err)
-		_, err = QueryRuleByName(ruleName)
+		_, err = QueryRuleByName(db, ruleName)
 		require.ErrorIs(t, err, gorm.ErrRecordNotFound)
 	})
 	t.Run("create and update rule", func(t *testing.T) {
@@ -36,35 +41,36 @@ func TestRule_OP(t *testing.T) {
 		err = UpdateRule(rule)
 		require.NoError(t, err)
 
-		got, err := QueryRuleByName(ruleName)
+		got, err := QueryRuleByName(db, ruleName)
 		require.NoError(t, err)
 		require.Equal(t, "java", got.Language)
 	})
 }
 
 func TestRule_Group_OP(t *testing.T) {
+	db := consts.GetGormProfileDatabase()
 	t.Run("test create and delete group", func(t *testing.T) {
 		groupName := uuid.NewString()
-		_, err := CreateGroupByName(groupName)
+		_, err := CreateGroup(db, groupName)
 		require.NoError(t, err)
 
-		got, err := QueryGroupByName(groupName)
+		got, err := QueryGroupByName(db, groupName)
 		require.NoError(t, err)
 		require.Equal(t, groupName, got.GroupName)
 
-		err = DeleteGroupByName(groupName)
+		err = DeleteGroup(db, groupName)
 		require.NoError(t, err)
-		_, err = QueryGroupByName(groupName)
+		_, err = QueryGroupByName(db, groupName)
 		require.ErrorIs(t, err, gorm.ErrRecordNotFound)
 	})
 
 	t.Run("test add remove rule group", func(t *testing.T) {
 		// create group
 		groupName := uuid.NewString()
-		_, err := CreateGroupByName(groupName)
+		_, err := CreateGroup(db, groupName)
 		require.NoError(t, err)
 		t.Cleanup(func() {
-			err = DeleteGroupByName(groupName)
+			err = DeleteGroup(db, groupName)
 			require.NoError(t, err)
 		})
 		// create rule
@@ -77,33 +83,33 @@ func TestRule_Group_OP(t *testing.T) {
 			require.NoError(t, err)
 		})
 		// add rule to group
-		err = AddGroupForRuleByName(ruleName, groupName)
+		err = AddGroupForRule(db, ruleName, groupName)
 		require.NoError(t, err)
-		require.Equal(t, int32(1), QueryRuleCountInGroup(groupName))
+		require.Equal(t, int32(1), GetRuleCountByGroupName(db, groupName))
 
 		newGroupName := uuid.NewString()
-		err = AddGroupForRuleByName(ruleName, newGroupName)
+		err = AddGroupForRule(db, ruleName, newGroupName)
 		t.Cleanup(func() {
-			err = DeleteGroupByName(newGroupName)
+			err = DeleteGroup(db, newGroupName)
 			require.NoError(t, err)
 		})
 		require.NoError(t, err)
-		require.Equal(t, int32(1), QueryRuleCountInGroup(newGroupName))
+		require.Equal(t, int32(1), GetRuleCountByGroupName(db, newGroupName))
 
-		gotRule, err := QueryRuleByName(ruleName)
+		gotRule, err := QueryRuleByName(db, ruleName)
 		require.NoError(t, err)
 		require.Equal(t, 2, len(gotRule.Groups))
 
 		// remove rule from group
-		err = RemoveGroupForRuleByName(ruleName, groupName)
+		err = RemoveGroupForRule(db, ruleName, groupName)
 		require.NoError(t, err)
-		require.Equal(t, int32(1), QueryGroupCountInRule(ruleName))
+		require.Equal(t, int32(1), GetGroupCountByRuleName(db, ruleName))
 	})
 
 	t.Run("test create and delete group for rule", func(t *testing.T) {
 		// create group
 		groupName := uuid.NewString()
-		_, err := CreateGroupByName(groupName)
+		_, err := CreateGroup(db, groupName)
 		require.NoError(t, err)
 
 		// create rule
@@ -116,15 +122,15 @@ func TestRule_Group_OP(t *testing.T) {
 			require.NoError(t, err)
 		})
 		// add rule to group
-		err = AddGroupForRuleByName(ruleName, groupName)
+		err = AddGroupForRule(db, ruleName, groupName)
 		require.NoError(t, err)
-		require.Equal(t, int32(1), QueryRuleCountInGroup(groupName))
+		require.Equal(t, int32(1), GetRuleCountByGroupName(db, groupName))
 
 		// delete group
-		err = DeleteGroupByName(groupName)
+		err = DeleteGroup(db, groupName)
 		require.NoError(t, err)
 
-		queryRule, err := QueryRuleByName(ruleName)
+		queryRule, err := QueryRuleByName(db, ruleName)
 		require.NoError(t, err)
 		require.Equal(t, 0, len(queryRule.Groups))
 	})
