f3dae6795877ee0f1e17c1f0d9e7252c7c308de1
Rookie <rookie_is@qq.com>
2024-12-10 17:35:38 +0800

    feat(extracted-data):add export extracted data

--- common/yakgrpc/grpc_mitm_extract_data.go
+++ common/yakgrpc/grpc_mitm_extract_data.go
@@ -1,23 +1,29 @@
 package yakgrpc
 
 import (
+	"bufio"
 	"context"
-	"github.com/yaklang/yaklang/common/schema"
-
+	"fmt"
+	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/filter"
 	"github.com/yaklang/yaklang/common/go-funk"
+	"github.com/yaklang/yaklang/common/schema"
 	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/utils/bizhelper"
 	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
 )
 
 func (s *Server) QueryMITMRuleExtractedData(ctx context.Context, req *ypb.QueryMITMRuleExtractedDataRequest) (*ypb.QueryMITMRuleExtractedDataResponse, error) {
 	db := s.GetProjectDatabase()
-	if req.GetHTTPFlowHiddenIndex() != "" {
-		db = db.Where("source_type == 'httpflow' and trace_id = ?", req.GetHTTPFlowHiddenIndex())
-	} else {
-		return nil, utils.Error("httpflow hiddenindex must be set")
+	if len(req.GetFilter().GetTraceID()) == 0 && req.GetHTTPFlowHiddenIndex() == "" && req.GetHTTPFlowHash() == "" {
+		return nil, utils.Error("query mitm rule extracted data need hiddenIndex at last")
 	}
-	p, data, err := yakit.QueryExtractedData(db, req)
+	p, data, err := yakit.QueryExtractedDataPagination(db, req)
 	if err != nil {
 		return nil, err
 	}
@@ -39,4 +45,66 @@ func (s *Server) QueryMITMRuleExtractedData(ctx context.Context, req *ypb.QueryM
 		Total:      int64(p.TotalRecord),
 		Pagination: req.GetPagination(),
 	}, nil
+}
+
+func (s *Server) ExportMITMRuleExtractedData(req *ypb.ExportMITMRuleExtractedDataRequest, stream ypb.Yak_ExportMITMRuleExtractedDataServer) error {
+	db := s.GetProjectDatabase()
+	allCount, err := yakit.CountExtractedData(db, req.GetFilter())
+	if err != nil {
+		return err
+	}
+	db = yakit.FilterExtractedData(db, req.GetFilter())
+	exportPath := req.GetExportFilePath()
+	if exportPath == "" {
+		exportPath = filepath.Join(consts.GetDefaultYakitBaseTempDir(), fmt.Sprintf("mitm_rule_extracted_data_%s.csv", time.Now().Format("20060102150405")))
+	} else if !path.IsAbs(exportPath) {
+		exportPath = filepath.Join(consts.GetDefaultYakitBaseTempDir(), exportPath)
+	}
+	if filepath.Ext(exportPath) != "csv" {
+		exportPath = exportPath + ".csv"
+	}
+
+	exportFp, err := os.OpenFile(exportPath, os.O_CREATE|os.O_RDWR, 0o666)
+	if err != nil {
+		return err
+	}
+	defer exportFp.Close()
+	exportWriter := bufio.NewWriter(exportFp)
+	var currentCount float64
+	sendFeedBack := func(verbose string, increase float64) {
+		currentCount += increase
+		_ = stream.Send(&ypb.ExportMITMRuleExtractedDataResponse{
+			Verbose:        verbose,
+			ExportFilePath: exportPath,
+			Percent:        currentCount / allCount,
+		})
+	}
+
+	_, err = exportWriter.Write([]byte("提取规则名,数据内容\n"))
+	if err != nil {
+		return err
+	}
+
+	duplicateFilter := filter.NewFilter()
+
+	for data := range bizhelper.YieldModel[*schema.ExtractedData](stream.Context(), db) {
+		sendFeedBack(fmt.Sprintf("Exported records"), 1)
+		line := fmt.Sprintf("%s,%s\n", utils.QuoteCSV(data.RuleVerbose), utils.QuoteCSV(data.Data))
+		if duplicateFilter.Exist(line) {
+			continue
+		}
+		duplicateFilter.Insert(line)
+		_, err = exportWriter.Write([]byte(line))
+		if err != nil {
+			return err
+		}
+	}
+	exportWriter.Flush()
+
+	_ = stream.Send(&ypb.ExportMITMRuleExtractedDataResponse{
+		Verbose:        "Exported all records successfully",
+		ExportFilePath: exportPath,
+		Percent:        1,
+	})
+	return nil
 }
