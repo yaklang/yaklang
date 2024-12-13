f429e16947a26e286eef9fa186f2aa3744591cde
z3 <z3r0ne@163.com>
2024-12-13 17:01:44 +0800

    remove this declare

--- common/javaclassparser/classes/static/LambdaTest.java
+++ common/javaclassparser/classes/static/LambdaTest.java
@@ -1,14 +1,18 @@
 package org.benf.cfr.reader;
 
 import java.util.ArrayList;
-import java.util.List;
-
 public class LambdaTest {
-    void main(){
-        List a = new ArrayList<>();
-        a.forEach((Object o)->{
-            System.out.println(o);
-        });
-    }
-
+	public LambdaTest() {
+		return;
+	}
+	 void main() {
+		ArrayList var1 = new ArrayList();
+		ArrayList var2 = var1;
+		var2.forEach((Object var3) -> {
+			int var4 = 1;
+			return;
+		});
+		int var3 = 1;
+		return;
+	}
 }
