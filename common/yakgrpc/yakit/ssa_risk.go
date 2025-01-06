3d6ec2c5ba3061ef2f1340f7fc9c0e9624d1ff03
Rookie <rookie_is@qq.com>
2025-01-06 12:03:03 +0800

    feat(ssa-risk): add new ssa risk

--- common/yakgrpc/yakit/ssa_risk.go
+++ common/yakgrpc/yakit/ssa_risk.go
@@ -3,7 +3,10 @@ package yakit
 import (
 	"github.com/jinzhu/gorm"
 	"github.com/yaklang/yaklang/common/schema"
+	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/bizhelper"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"strings"
 )
 
 func DeleteRiskByProgram(DB *gorm.DB, programNames []string) error {
@@ -19,6 +22,88 @@ func DeleteRiskBySFResult(DB *gorm.DB, resultIDs []int64) error {
 	db := DB.Model(&schema.Risk{})
 	db = bizhelper.ExactQueryInt64ArrayOr(db, "result_id", resultIDs)
 	if db := db.Unscoped().Delete(&schema.Risk{}); db.Error != nil {
+		return db.Error
+	}
+	return nil
+}
+
+func CreateSSARisk(DB *gorm.DB, r *schema.SSARisk) error {
+	if db := DB.Create(r); db.Error != nil {
+		return db.Error
+	}
+	return nil
+}
+
+func GetSSARiskByHash(db *gorm.DB, hash string) (*schema.SSARisk, error) {
+	var r schema.SSARisk
+	if db := db.Model(&schema.SSARisk{}).Where("hash = ?", hash).First(&r); db.Error != nil {
+		return nil, utils.Errorf("get Risk failed: %s", db.Error)
+	}
+	return &r, nil
+}
+
+func FilterSSARisk(db *gorm.DB, filter *ypb.SSARisksFilter) *gorm.DB {
+	db = bizhelper.ExactOrQueryStringArrayOr(db, "program_name", filter.GetProgramName())
+	db = bizhelper.ExactOrQueryStringArrayOr(db, "code_source_url", filter.GetCodeSourceUrl())
+	db = bizhelper.ExactOrQueryStringArrayOr(db, "risk_type", filter.GetRiskType())
+	db = bizhelper.ExactOrQueryStringArrayOr(db, "severity", filter.GetSeverity())
+	db = bizhelper.ExactOrQueryStringArrayOr(db, "from_rule", filter.GetFromRule())
+	db = bizhelper.ExactOrQueryStringArrayOr(db, "runtime_id", filter.GetRuntimeID())
+	db = bizhelper.ExactQueryUint64ArrayOr(db, "result_id", filter.GetResultID())
+	db = bizhelper.FuzzSearchWithStringArrayOrEx(db, []string{"tags"}, filter.GetTags(), false)
+	db = bizhelper.FuzzSearchEx(db, []string{
+		"program_name", "code_source_url",
+		"risk_type", "severity", "from_rule", "tags",
+	}, filter.GetSearch(), false)
+
+	return db
+}
+
+func QuerySSARisk(db *gorm.DB, filter *ypb.SSARisksFilter, paging *ypb.Paging) (*bizhelper.Paginator, []*schema.SSARisk, error) {
+	if filter == nil {
+		return nil, nil, utils.Errorf("empty filter")
+	}
+	if paging == nil {
+		paging = &ypb.Paging{
+			Page:    1,
+			Limit:   30,
+			OrderBy: "updated_at",
+			Order:   "desc",
+		}
+	}
+	var risks []*schema.SSARisk
+
+	db = db.Model(&schema.SSARisk{})
+	db = bizhelper.QueryOrder(db, paging.OrderBy, paging.Order)
+
+	if filter.GetFromId() > 0 {
+		db = db.Where("id > ?", filter.GetFromId())
+	}
+
+	if filter.GetUntilId() > 0 {
+		db = db.Where("id < ?", filter.GetUntilId())
+	}
+
+	db = FilterSSARisk(db, filter)
+	queryPaging, queryDb := bizhelper.Paging(db, int(paging.Page), int(paging.Limit), &risks)
+	if queryDb.Error != nil {
+		return nil, nil, utils.Errorf("paging failed: %s", db.Error)
+	}
+	return queryPaging, risks, nil
+}
+
+func DeleteSSARisks(DB *gorm.DB, filter *ypb.SSARisksFilter) error {
+	db := DB.Model(&schema.SSARisk{})
+	db = FilterSSARisk(db, filter)
+	if db := db.Unscoped().Delete(&schema.SSARisk{}); db.Error != nil {
+		return db.Error
+	}
+	return nil
+}
+
+func UpdateSSARiskTags(DB *gorm.DB, id int64, tags []string) error {
+	db := DB.Model(&schema.SSARisk{})
+	if db := db.Where("id = ?", id).Update("tags", strings.Join(tags, "|")); db.Error != nil {
 		return db.Error
 	}
 	return nil
