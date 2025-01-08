718ad05f9e7f112956808a8719efd037ddd6af50
YWHKKX <2076064543@qq.com>
2025-01-08 13:02:36 +0800

    fix(ssa): add pointer type

--- common/yak/go2ssa/builder_express.go
+++ common/yak/go2ssa/builder_express.go
@@ -25,6 +25,15 @@ func (b *astbuilder) buildExpression(exp *gol.ExpressionContext, IslValue bool) 
 			return b.EmitConstInst(0)
 		}
 	}
+	getVariable := func(single getSingleExpr, i int) *ssa.Variable {
+		if s := single.Expression(i); s != nil {
+			_, rightl := b.buildExpression(s.(*gol.ExpressionContext), IslValue)
+			return rightl
+		} else {
+			b.NewError(ssa.Error, TAG, "can't get expression")
+			return b.CreateVariable("")
+		}
+	}
 
 	// fmt.Printf("exp = %v\n", exp.GetText())
 
@@ -48,9 +57,9 @@ func (b *astbuilder) buildExpression(exp *gol.ExpressionContext, IslValue bool) 
 			case "<-":
 				ssaop = ssa.OpChan
 			case "*":
-				ssaop = ""
+				ssaop = ssa.OpDereference
 			case "&":
-				ssaop = ""
+				ssaop = ssa.OpAddress
 			default:
 				b.NewError(ssa.Error, TAG, UnaryOperatorNotSupport(op.GetText()))
 			}
@@ -60,7 +69,10 @@ func (b *astbuilder) buildExpression(exp *gol.ExpressionContext, IslValue bool) 
 				b.NewError(ssa.Error, TAG, NeedTwoExpression())
 				return b.EmitConstInst(0), b.CreateVariable("")
 			}
-			if ssaop == "" {
+			if ssaop == ssa.OpDereference {
+				if un, ok := op1.(*ssa.UnOp); ok {
+					return un.X, nil
+				}
 				return op1, nil
 			}
 			return b.EmitUnOp(ssaop, op1), nil
@@ -160,7 +172,19 @@ func (b *astbuilder) buildExpression(exp *gol.ExpressionContext, IslValue bool) 
 			return b.EmitBinOp(ssaop, op1, op2), nil
 		}
 	} else { // left
+		if op := exp.GetUnary_op(); op != nil {
+			var ssaop ssa.UnaryOpcode
+			_ = ssaop
+			switch op.GetText() {
+			case "*":
+				ssaop = ssa.OpDereference
+			default:
+				b.NewError(ssa.Error, TAG, UnaryOperatorNotSupport(op.GetText()))
+			}
 
+			va1 := getVariable(exp, 0)
+			return nil, va1
+		}
 	}
 
 	return b.EmitConstInst(0), b.CreateVariable("")
@@ -275,6 +299,9 @@ func (b *astbuilder) buildPrimaryExpression(exp *gol.PrimaryExprContext, IslValu
 				}
 			}
 
+			if un, ok := rv.(*ssa.UnOp); ok && un.Op == ssa.OpAddress {
+				rv = un.X
+			}
 			if typ, ok := ssa.ToObjectType(rv.GetType()); ok {
 				handleObjectType(rv, typ)
 			} else if value, ok := b.GetProgram().ReadImportValueWithPkg(rv.GetName(), test); ok {
