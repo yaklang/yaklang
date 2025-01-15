a73bd446c4a294347fb2f1d94abf187d0027fa14
intSheep <1098204193@qq.com>
2025-01-15 14:02:24 +0800

    fix(grpc_sf):fix delete failed after union table query

--- common/yakgrpc/yakit/syntaxflow_rule.go
+++ common/yakgrpc/yakit/syntaxflow_rule.go
@@ -86,15 +86,21 @@ func DeleteSyntaxFlowNonBuildInRule(db *gorm.DB, params *ypb.DeleteSyntaxFlowRul
 }
 
 func deleteSyntaxFlowRuleEx(db *gorm.DB, params *ypb.DeleteSyntaxFlowRuleRequest, isBuildIn ...bool) (int64, error) {
-	db = db.Model(&schema.SyntaxFlowRule{})
 	if params == nil || params.Filter == nil {
-		return 0, utils.Errorf("delete syntaxFlow rule failed: synatx flow filter is nil")
+		return 0, utils.Errorf("delete syntaxFlow rule failed: syntax flow filter is nil")
 	}
-	db = FilterSyntaxFlowRule(db, params.Filter)
+	db = db.Model(&schema.SyntaxFlowRule{})
+	query := db
+	query = FilterSyntaxFlowRule(query, params.Filter)
 	if len(isBuildIn) > 0 {
-		db = bizhelper.QueryByBool(db, "is_build_in_rule", isBuildIn[0])
+		query = bizhelper.QueryByBool(query, "is_build_in_rule", isBuildIn[0])
 	}
-	db = db.Unscoped().Delete(&schema.SyntaxFlowRule{})
+
+	// 如果filter包含groupName,FilterSyntaxFlowRule会使用联表查询，导致无法直接db.delete()
+	// 所以需要先查出来再删除
+	var ids []uint
+	query.Pluck("syntax_flow_rules.id", &ids)
+	db = db.Where("id IN (?)", ids).Unscoped().Delete(&schema.SyntaxFlowRule{})
 	return db.RowsAffected, db.Error
 }
 
