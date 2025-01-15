d8b81c1633b7f2e71cf5ee770cce0da3bf2fa0e4
Rookie <rookie_is@qq.com>
2025-01-15 16:36:34 +0800

    clear some debug print

--- common/utils/lowhttp/exec.go
+++ common/utils/lowhttp/exec.go
@@ -46,6 +46,16 @@ type RedirectFlow struct {
 	RespRecord *LowhttpResponse
 }
 
+func beforeLog(option *LowhttpExecConfig) {
+	atomic.AddInt64(option.BeforeCount, 1)
+	log.Infof("debug counter: %v, %v", "http call", atomic.LoadInt64(option.BeforeCount))
+}
+
+func afterLog(option *LowhttpExecConfig) {
+	atomic.AddInt64(option.AfterCount, 1)
+	log.Infof("debug counter: %v, %v", "http done", atomic.LoadInt64(option.AfterCount))
+}
+
 func HTTP(opts ...LowhttpOpt) (*LowhttpResponse, error) {
 	option := NewLowhttpOption()
 	for _, opt := range opts {
@@ -706,19 +716,18 @@ RECONNECT:
 	if withConnPool {
 		// 连接池分支
 		pc := conn.(*persistConn)
-		writeErrCh := make(chan error, 1)
+		writeErrCh := make(chan error, 2)
 		if option.BeforeDoRequest != nil {
 			requestPacket = option.BeforeDoRequest(requestPacket)
 		}
-
 		if oldVersionProxyChecking {
 			requestPacket, err = BuildLegacyProxyRequest(requestPacket)
 			if err != nil {
 				return nil, err
 			}
 		}
-		pc.writeCh <- writeRequest{reqPacket: requestPacket, ch: writeErrCh, reqInstance: reqIns}
-		resc := make(chan responseInfo, 1)
+
+		resc := make(chan responseInfo)
 		pc.reqCh <- requestAndResponseCh{
 			reqPacket:   requestPacket,
 			ch:          resc,
@@ -726,43 +735,39 @@ RECONNECT:
 			option:      option,
 			writeErrCh:  writeErrCh,
 		}
-
-		// addDebugCounter("finished submit request to conn pool")
-	LOOP:
-		for {
-			select {
-			case err := <-writeErrCh:
-				// 写入失败，退出等待
-				if err != nil {
-					if pc.shouldRetryRequest(err) {
-						pc.removeConn()
-						goto RECONNECT
-					}
-					return nil, err
-				}
-			case re := <-resc:
-				// 收到响应
-				if (re.resp == nil) == (re.err == nil) {
-					return nil, utils.Errorf("BUG: internal error: exactly one of res or err should be set; nil=%v", re.resp == nil)
-				}
-				if re.err != nil && len(rawBytes) == 0 {
-					if pc.shouldRetryRequest(re.err) {
-						goto RECONNECT
-					}
-					return nil, re.err
-				}
-				firstResponse = re.resp
-				rawBytes = re.respBytes
-				response.MultiResponse = false
-				traceInfo.ServerTime = re.info.ServerTime
-				break LOOP
-			case <-pc.closeCh:
-				if pc.shouldRetryRequest(pc.closed) {
+		pc.writeCh <- writeRequest{reqPacket: requestPacket, ch: writeErrCh, reqInstance: reqIns}
+		//beforeLog(option)
+		select {
+		case err := <-writeErrCh:
+			if pc.shouldRetryRequest(err) {
+				goto RECONNECT
+			}
+			return nil, err
+		case re := <-resc:
+			// 收到响应
+			if (re.resp == nil) == (re.err == nil) {
+				return nil, utils.Errorf("BUG: internal error: exactly one of res or err should be set; nil=%v", re.resp == nil)
+			}
+			if re.err != nil && len(rawBytes) == 0 {
+				if pc.shouldRetryRequest(re.err) {
 					goto RECONNECT
 				}
-				return nil, pc.closed
+				return nil, re.err
 			}
+			firstResponse = re.resp
+			rawBytes = re.respBytes
+			response.MultiResponse = false
+			traceInfo.ServerTime = re.info.ServerTime
+		case <-pc.closeCh:
+			if pc.closed == nil {
+				log.Errorf("BUG: closeCh but closed is nil")
+			}
+			if pc.shouldRetryRequest(pc.closed) {
+				goto RECONNECT
+			}
+			return nil, pc.closed
 		}
+		//afterLog(option)
 
 	} else {
 		// 不使用连接池分支
