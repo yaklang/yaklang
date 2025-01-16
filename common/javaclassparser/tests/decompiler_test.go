a4b756bac3dae91bc908f6e7416bf7e9ce18d729
z3 <z3r0ne@163.com>
2025-01-16 11:28:31 +0800

    fix ternary exp bug

--- common/javaclassparser/tests/decompiler_test.go
+++ common/javaclassparser/tests/decompiler_test.go
@@ -18,6 +18,7 @@ func TestDecompiler(t *testing.T) {
 	testCase := []struct {
 		name string
 	}{
+		{"LogicalOperationMini"},
 		{"SelfOp"},
 		{"ContinuousAssign"},
 		{"TryCatch1"},
@@ -57,24 +58,26 @@ func TestDecompiler(t *testing.T) {
 	}
 	for _, testItem := range testCase {
 		t.Run(testItem.name, func(t *testing.T) {
-			t.Parallel()
-			classRaw, err := classes.FS.ReadFile(testItem.name + ".class")
-			if err != nil {
-				t.Fatal(err)
+			for i := 0; i < 100; i++ {
+				//t.Parallel()
+				classRaw, err := classes.FS.ReadFile(testItem.name + ".class")
+				if err != nil {
+					t.Fatal(err)
+				}
+				sourceCode, err := classes.FS.ReadFile(testItem.name + ".java")
+				if err != nil {
+					t.Fatal(err)
+				}
+				ins, err := javaclassparser.Parse(classRaw)
+				if err != nil {
+					t.Fatal(err)
+				}
+				source, err := ins.Dump()
+				if err != nil {
+					t.Fatal(err)
+				}
+				assert.Equal(t, string(sourceCode), source)
 			}
-			sourceCode, err := classes.FS.ReadFile(testItem.name + ".java")
-			if err != nil {
-				t.Fatal(err)
-			}
-			ins, err := javaclassparser.Parse(classRaw)
-			if err != nil {
-				t.Fatal(err)
-			}
-			source, err := ins.Dump()
-			if err != nil {
-				t.Fatal(err)
-			}
-			assert.Equal(t, string(sourceCode), source)
 		})
 	}
 
