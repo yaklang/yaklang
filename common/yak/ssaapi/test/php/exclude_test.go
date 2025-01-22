a78660ec528dc875d53ec164d06ef19de0918622
Q16G <760046475@qq.com>
2025-01-22 11:03:39 +0800

    feature(ssa-compile): add test

--- common/yak/ssaapi/test/php/exclude_test.go
+++ common/yak/ssaapi/test/php/exclude_test.go
@@ -35,3 +35,32 @@ println(2);
 	require.NoError(t, err)
 	require.True(t, result.GetValues("param").Len() == 0)
 }
+
+func TestExcludeFile(t *testing.T) {
+	fs := filesys.NewVirtualFs()
+	fs.AddFile("/var/www/html/1.php", `<?php
+phpinfo();
+`)
+	fs.AddFile("/var/www/html/vendor/src/main/3.php", `<?php
+println(2);
+`)
+	fs.AddFile("/vendor/src/main/3.php", `<?php
+println(2);
+`)
+	prog, err := ssaapi.ParseProjectWithFS(fs, ssaapi.WithLanguage(ssaapi.PHP))
+	require.NoError(t, err)
+	result, err := prog.SyntaxFlowWithError(`println(* #-> * as $param)`, ssaapi.QueryWithEnableDebug())
+	require.NoError(t, err)
+	values := result.GetValues("param")
+	require.True(t, values.Len() != 0)
+
+	filterFunc, err := ssaapi.DefaultExcludeFunc([]string{"**/vendor/**", "vendor/**"})
+	require.NoError(t, err)
+	prog, err = ssaapi.ParseProjectWithFS(fs, ssaapi.WithLanguage(ssaapi.PHP), filterFunc)
+	require.NoError(t, err)
+	prog.Show()
+	result, err = prog.SyntaxFlowWithError(`println(* #-> * as $param)`, ssaapi.QueryWithEnableDebug())
+	require.NoError(t, err)
+	values = result.GetValues("param")
+	require.True(t, values.Len() == 0)
+}
