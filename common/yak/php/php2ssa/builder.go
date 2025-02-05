d023d40b808d031fed7b2c372560642792faa07a
Q16G <760046475@qq.com>
2025-02-05 14:24:03 +0800

    fix(ssa): fix ssa test

--- common/yak/php/php2ssa/builder.go
+++ common/yak/php/php2ssa/builder.go
@@ -33,6 +33,7 @@ func (*SSABuild) FilterPreHandlerFile(path string) bool {
 func (s *SSABuild) Create() ssa.Builder {
 	return &SSABuild{
 		PreHandlerInit: ssa.NewPreHandlerInit(initHandler).WithLanguageConfigOpts(
+			ssa.LanguageConfigIsWeak,
 			ssa.LanguageConfigIsBinding,
 			ssa.LanguageConfigTryBuildValue,
 			ssa.LanguageConfigIsSupportClass,
