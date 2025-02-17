7b1e2447e318addc084dcdb59e242908a36a0809
wlz <lingze.wl@gmail.com>
2025-02-17 15:45:18 +0800

    feat(grpc):new ssa-risk could be query

--- common/yakgrpc/grpc_ssa_risk_test.go
+++ common/yakgrpc/grpc_ssa_risk_test.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"testing"
+
 	"github.com/bytedance/mockey"
 	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/assert"
 	"github.com/yaklang/yaklang/common/yak/yaklib"
-	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
@@ -157,6 +158,121 @@ func TestSSARisk_MarkRead(t *testing.T) {
 		require.NoError(t, err)
 		check(data, false)
 	}
+}
+
+func TestSSARisk_NewSSARisk(t *testing.T) {
+	client, err := NewLocalClient(true) // use yakit handler local database, this test-case should use local grpc
+	require.NoError(t, err)
+
+	// create risk
+	newrisk1 := uuid.NewString()
+	yakit.CreateSSARisk(ssadb.GetDB(), &schema.SSARisk{
+		Title: newrisk1,
+	})
+	defer func() {
+		yakit.DeleteSSARisks(ssadb.GetDB(), &ypb.SSARisksFilter{
+			Title: newrisk1,
+		})
+	}()
+
+	response, err := client.QuerySSARisks(context.Background(), &ypb.QuerySSARisksRequest{
+		Filter: &ypb.SSARisksFilter{},
+		Pagination: &ypb.Paging{
+			Limit:   1,
+			Page:    1,
+			Order:   "desc",
+			OrderBy: "id",
+		},
+	})
+	require.NoError(t, err)
+	require.Equal(t, 1, len(response.Data))
+	start := response.Data[0]
+	require.NotNil(t, start)
+	startId := start.GetId()
+	require.Greater(t, startId, int64(0))
+	total := response.GetTotal()
+	_ = total
+
+	// test new risk
+	t.Run("test new risk", func(t *testing.T) {
+		newrisk1 := uuid.NewString()
+		yakit.CreateSSARisk(ssadb.GetDB(), &schema.SSARisk{
+			Title: newrisk1,
+		})
+		defer func() {
+			yakit.DeleteSSARisks(ssadb.GetDB(), &ypb.SSARisksFilter{
+				Title: newrisk1,
+			})
+		}()
+
+		response, err := client.QueryNewSSARisks(context.Background(), &ypb.QueryNewSSARisksRequest{
+			AfterID: startId,
+		})
+		require.NoError(t, err)
+
+		// check new ssa-risk
+		require.Equal(t, 1, len(response.Data))
+		require.Equal(t, newrisk1, response.Data[0].GetTitle())
+		// check new ssa-risk count
+		require.Equal(t, int64(1), response.NewRiskTotal)
+
+		// check total
+		require.Equal(t, total+1, response.GetTotal())
+	})
+
+	t.Run("test new risk with read", func(t *testing.T) {
+		newrisk1 := uuid.NewString()
+		yakit.CreateSSARisk(ssadb.GetDB(), &schema.SSARisk{
+			Title:  newrisk1,
+			IsRead: true,
+		})
+		defer func() {
+			yakit.DeleteSSARisks(ssadb.GetDB(), &ypb.SSARisksFilter{
+				Title: newrisk1,
+			})
+		}()
+
+		response, err := client.QueryNewSSARisks(context.Background(), &ypb.QueryNewSSARisksRequest{
+			AfterID: startId,
+		})
+		require.NoError(t, err)
+
+		// check new ssa-risk
+		require.Equal(t, 0, len(response.Data))
+		// check new ssa-risk count
+		require.Equal(t, int64(0), response.NewRiskTotal)
+
+		// check total
+		require.Equal(t, total+1, response.GetTotal())
+	})
+
+	t.Run("test risk data max is 5", func(t *testing.T) {
+		for i := 0; i < 6; i++ {
+			newrisk1 := uuid.NewString()
+			yakit.CreateSSARisk(ssadb.GetDB(), &schema.SSARisk{
+				Title: newrisk1,
+			})
+			defer func() {
+				yakit.DeleteSSARisks(ssadb.GetDB(), &ypb.SSARisksFilter{
+					Title: newrisk1,
+				})
+			}()
+		}
+
+		response, err := client.QueryNewSSARisks(context.Background(), &ypb.QueryNewSSARisksRequest{
+			AfterID: startId,
+		})
+		require.NoError(t, err)
+
+		// check new ssa-risk
+		require.Equal(t, 5, len(response.Data))
+		// check new ssa-risk count
+		require.Equal(t, int64(6), response.NewRiskTotal)
+
+		// check total
+		require.Equal(t, total+6, response.GetTotal())
+	})
+
 }
 
 func TestSSARiskFeedbackToOnline(t *testing.T) {
