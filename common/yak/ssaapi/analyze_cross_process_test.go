0ef2b4b434c1d04c6af9d67868400906ad4458ca
intSheep <intSheep@qq.com>
2025-02-18 23:24:50 +0800

    feat(ssaapi):fix objectStack and add test

--- common/yak/ssaapi/analyze_cross_process_test.go
+++ common/yak/ssaapi/analyze_cross_process_test.go
@@ -2,7 +2,6 @@ package ssaapi_test
 
 import (
 	"github.com/stretchr/testify/require"
-	"github.com/yaklang/yaklang/common/consts"
 	"github.com/yaklang/yaklang/common/yak/ssaapi"
 	"github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
 	"testing"
@@ -21,7 +20,7 @@ func Test_CrossProcess(t *testing.T) {
 	}
 		`
 		/*
-			以上代码会进行两次跨过程分析，不会触防放递归机制
+			以上代码会进行两次跨过程分析，不会触发防递归机制
 			m->
 			  -> FreeValue-A(1)
 				-> Function-A
@@ -45,64 +44,111 @@ func Test_CrossProcess(t *testing.T) {
 func Test_WithinProcess(t *testing.T) {
 	t.Run("Test_WithinProcess_Analysis", func(t *testing.T) {
 		code := `
-	package main
-	
-	import (
-		"html/template"
-		"net/http"
-	)
-	func handler(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.New("example").Parse("<h1>Hello, {{ .Name }}  || {{ .Id }}</h1>"))
-		data := struct {
-			Name string
-			Id   template.HTML
-		}{
-			Name: r.FormValue("name"),
-			Id:   template.HTML(r.FormValue("id")), // xss
-		}
-		tmpl.Execute(w, data)
+	m = {
+		"foo":r.FormValue("name"),
+		"bar":template.HTML(r.FormValue("id")), 
 	}
-	
-	func main() {
-		serve := http.NewServeMux()
-		serve.HandleFunc("/", handler)
-		http.ListenAndServe(":8080", serve)
-	}
+	print(m)
 	`
-		rule := `template?{<fullTypeName()>?{have:"html/template"}}.Must().Execute(* as $input)
-$input #{
-	hook:<<<HOOK
-	* ?{!opcode:call &&  have:'FormValue'}  as $toCheck
-HOOK
-}-> as $res `
-		/*
-				以上例子为过程内分析，不会触发防递归机制
-			make(struct {string,ClassBluePrint: HTML})
-			->make(struct {})
-				-> ParameterMember-parameter[1].FormValue("name")
-					-> ParameterMember-parameter[1].FormValue
-					->"name"
-				->Parameter-r
-			->Undefined-template.HTML(ParameterMember-parameter[1].FormValue("id"))
-			  ->Undefined-template.HTML
-				->ExternLib-template
-			  ->ParameterMember-parameter[1].FormValue("id")
-				->ParameterMember-parameter[1].FormValue
-				->"id"
-			  -> Parameter-r
-				其中`ParameterMember-parameter[1].FormValue`会进去分析两次，虽然两次都是一个过程内，但是他们是不同的路径，
-				因此防递归机制不应该进行拦截。
-		*/
 		ssatest.Check(t, code, func(prog *ssaapi.Program) error {
+			rule := `print(* #{
+			hook:<<<HOOK
+	*?{!opcode:call && have:'FormValue'} as $toCheck
+HOOK
+			}-> as $res)`
 			res, err := prog.SyntaxFlowWithError(rule)
 			require.NoError(t, err)
 			vals := res.GetValues("res")
-			require.Contains(t, vals.String(), "id", "name", "FormValue")
+			vals.ShowDot()
 
 			toCheck := res.GetValues("toCheck")
+			toCheck.Show()
 			effect := toCheck[0].GetEffectOn()
-			require.Equal(t, effect.Len(), 2)
+			require.Equal(t, effect.Len(), 3)
+			require.Contains(t, effect.String(), `Undefined-r.FormValue(valid)("id")`, `Undefined-r.FormValue(valid)("name")`, "Undefined-r")
+
+			/*
+					strict digraph {
+				    rankdir = "BT";
+				    n1 [label="r"]
+				    n10 [label="t19: #7.FormValue(t18)"]
+				    n11 [label="t18: \"id\""]
+				    n15 [label="t20: m.bar=#14.HTML(t19)"]
+				    n18 [label="template"]
+
+				    n2 [label="#7.FormValue"]
+				    n3 [label="t11: m.foo=#7.FormValue(t10)"]
+				    n4 [label="t10: \"name\""]
+				    n16 [label="#14.HTML"]
+
+				    n3 -> n4 [label=""]
+				    n10 -> n11 [label=""]
+				    n10 -> n2 [label=""]
+				    n3 -> n4 [label=""]
+				    n3 -> n2 [label=""]
+				    n15 -> n16 [label=""]
+				    n15 -> n10 [label=""]
+				    n1 -> n2 [label=""]
+				    n3 -> n2 [label=""]
+				    n1 -> n2 [label=""]
+				    n10 -> n11 [label=""]
+				    n10 -> n2 [label=""]
+				    n15 -> n16 [label=""]
+				    n18 -> n16 [label=""]
+				    n18 -> n16 [label=""]
+				    n15 -> n10 [label=""]
+				}
+			*/
 			return nil
-		}, ssaapi.WithLanguage(consts.GO))
+		})
+
+		t.Run("test object", func(t *testing.T) {
+			code := `
+	m = {
+		"foo":foo(a.b),
+		"bar":bar(a.b),
+	}
+	print(m)
+`
+			ssatest.Check(t, code, func(prog *ssaapi.Program) error {
+				rule := `print(* #{
+			hook:<<<HOOK
+	*?{!opcode:call && have:'a.b'} as $toCheck
+HOOK
+			}-> as $res)`
+				res, err := prog.SyntaxFlowWithError(rule)
+				require.NoError(t, err)
+				vals := res.GetValues("res")
+				vals.ShowDot()
+
+				toCheck := res.GetValues("toCheck")
+				toCheck.Show()
+				effect := toCheck[0].GetEffectOn()
+				require.Equal(t, effect.Len(), 3)
+				require.Contains(t, effect.String(), "Undefined-foo(Undefined-a.b(valid))", "Undefined-bar(Undefined-a.b(valid))", "Undefined-a")
+				/*
+					strict digraph {
+						rankdir = "BT";
+						n1 [label="foo"]
+						n2 [label="t1224023: m.foo=foo(#1224020.b)"]
+						n4 [label="#1224020.b"]
+						n6 [label="a"]
+						n9 [label="t1224028: m.bar=bar(#1224020.b)"]
+						n10 [label="bar"]
+						n2 -> n1 [label=""]
+						n2 -> n4 [label=""]
+						n9 -> n10 [label=""]
+						n9 -> n4 [label=""]
+						n2 -> n4 [label=""]
+						n6 -> n4 [label=""]
+						n6 -> n4 [label=""]
+						n9 -> n10 [label=""]
+						n9 -> n4 [label=""]
+						n2 -> n1 [label=""]
+					}
+				*/
+				return nil
+			})
+		})
 	})
 }
