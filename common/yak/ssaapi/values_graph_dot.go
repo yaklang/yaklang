3289eae976164c9a65a9248509607a6a7804623a
intSheep <1098204193@qq.com>
2025-02-18 17:53:52 +0800

    fix(graph):fix ssaapi graph

--- common/yak/ssaapi/values_graph_dot.go
+++ common/yak/ssaapi/values_graph_dot.go
@@ -3,26 +3,16 @@ package ssaapi
 import (
 	"bytes"
 	"fmt"
-
 	"github.com/yaklang/yaklang/common/log"
-
 	"github.com/yaklang/yaklang/common/utils/dot"
 )
 
 type ValueGraph struct {
 	*dot.Graph
 
-	// one ssa.value can be create many ssaapi.Value,
-	// so we use SSA-ID(int64) to graph node-id
-	Value2Node map[int64]int
-
-	// many ssaapi.value can contain different context, even this is same ssa.value
-	// use this map contain node-id to marshaled ssaapi.value
-	// in same node-id, that mean this ssaapi.value is same ssa.value
-	// ! this field just use in graph build
-	marshaledValue map[int]map[*Value]struct{} // node-id ->  ssaapi.value
-	// graph node id to value, this value just use bare ssa.value
-	Node2Value map[int]*Value
+	Value2Node     map[*Value]int   // ssaapi.Value -> node-id
+	marshaledValue map[int]struct{} // node-id ->  ssaapi.value
+	Node2Value     map[int]*Value
 }
 
 func NewValueGraph(v ...*Value) *ValueGraph {
@@ -31,12 +21,11 @@ func NewValueGraph(v ...*Value) *ValueGraph {
 	graph.GraphAttribute("rankdir", "BT")
 	g := &ValueGraph{
 		Graph:          graph,
-		Value2Node:     make(map[int64]int),
-		marshaledValue: make(map[int]map[*Value]struct{}),
+		Value2Node:     make(map[*Value]int),
+		marshaledValue: make(map[int]struct{}),
 		Node2Value:     make(map[int]*Value),
 	}
 	for _, value := range v {
-		// log.Infof("start graph %v", value.GetVerboseName())
 		g.CreateNode(value)
 	}
 	g.marshaledValue = nil
@@ -49,33 +38,42 @@ func (g *ValueGraph) Dot() string {
 	return buf.String()
 }
 
-func (g *ValueGraph) CreateNode(value *Value) int {
+func (g *ValueGraph) ShowDot() {
+	var buf bytes.Buffer
+	g.GenerateDOT(&buf)
+	fmt.Println(buf.String())
+}
+
+func (g *ValueGraph) GetNodeIdByValue(value *Value) int {
 	log.Infof("create node %d: %v, %p", value.GetId(), value.GetVerboseName(), value)
 	// get node id, if existed, no need to create
-	id, ok := g.Value2Node[value.GetId()]
+
+	nodeId, ok := g.Value2Node[value]
 	if !ok {
 		// value.getVerboseName can be same in some different value,
 		// so if value not exist, just create, don't use `GetOrCreateNode`
-		id = g.AddNode(value.GetVerboseName())
-		g.Value2Node[value.GetId()] = id
+		nodeId = g.AddNode(value.GetVerboseName())
+		g.Value2Node[value] = nodeId
 	}
-
 	// marshal
 	// add node2Value, just use bare ssa.value
-	if _, ok := g.Node2Value[id]; !ok {
-		g.Node2Value[id] = value
+	if _, ok := g.Node2Value[nodeId]; !ok {
+		g.Node2Value[nodeId] = value
 	}
-	if g.theValueShouldMarshal(value, id) {
-		g._marshal(id, value)
-	}
-	return id
+	return nodeId
+}
+
+func (g *ValueGraph) CreateNode(value *Value) int {
+	nodeId := g.GetNodeIdByValue(value)
+	g._marshal(nodeId, value)
+	return nodeId
 }
 
 func (g *ValueGraph) _marshal(selfID int, value *Value) {
-	g.marshaledValue[selfID][value] = struct{}{}
-	if len(value.GetDependOn()) == 0 && len(value.GetEffectOn()) == 0 && len(value.GetPredecessors()) == 0 {
+	if _, ok := g.marshaledValue[selfID]; ok {
 		return
 	}
+	g.marshaledValue[selfID] = struct{}{}
 
 	for _, node := range value.GetDependOn() {
 		id := g.CreateNode(node)
@@ -90,7 +88,6 @@ func (g *ValueGraph) _marshal(selfID int, value *Value) {
 		if predecessor.Node == nil {
 			continue
 		}
-
 		predecessorNodeID := g.CreateNode(predecessor.Node)
 		edges := g.GetEdges(predecessorNodeID, selfID)
 
@@ -115,26 +112,20 @@ func (g *ValueGraph) _marshal(selfID int, value *Value) {
 	}
 }
 
-func (g *ValueGraph) theValueShouldMarshal(value *Value, id int) bool {
-	if marshaledValue, ok := g.marshaledValue[id]; ok {
-		// if this node-id not contain this ssaapi.value, marshal
-		if _, ok := marshaledValue[value]; !ok {
-			return true
-		}
-		return false
-	} else {
-		// if this node-id not exist, make and marshal
-		g.marshaledValue[id] = make(map[*Value]struct{})
-		return true
-	}
-}
-
-func (g *ValueGraph) DeepFirstGraph(valueID int64) [][]string {
-	nodeID, ok := g.Value2Node[valueID]
+func (g *ValueGraph) DeepFirstGraphPrev(value *Value) [][]string {
+	nodeID, ok := g.Value2Node[value]
 	if !ok {
 		return nil
 	}
 	return dot.GraphPathPrev(g.Graph, nodeID)
+}
+
+func (g *ValueGraph) DeepFirstGraphNext(value *Value) [][]string {
+	nodeID, ok := g.Value2Node[value]
+	if !ok {
+		return nil
+	}
+	return dot.GraphPathNext(g.Graph, nodeID)
 }
 
 func (V Values) ShowDot() Values {
