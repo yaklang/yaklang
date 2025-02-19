96d18dcbc81bd89c00bcec7376f3a693791d52d2
intSheep <1098204193@qq.com>
2025-02-19 15:13:08 +0800

    feat(ssaapi):values_graph use graph builder

--- common/yak/ssaapi/values_graph_dot.go
+++ common/yak/ssaapi/values_graph_dot.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils/dot"
+	"github.com/yaklang/yaklang/common/utils/graph"
 )
 
 type ValueGraph struct {
@@ -16,19 +17,24 @@ type ValueGraph struct {
 }
 
 func NewValueGraph(v ...*Value) *ValueGraph {
-	graph := dot.New()
-	graph.MakeDirected()
-	graph.GraphAttribute("rankdir", "BT")
+	graphGraph := dot.New()
+	graphGraph.MakeDirected()
+	graphGraph.GraphAttribute("rankdir", "BT")
 	g := &ValueGraph{
-		Graph:          graph,
+		Graph:          graphGraph,
 		Value2Node:     make(map[*Value]int),
 		marshaledValue: make(map[int]struct{}),
 		Node2Value:     make(map[int]*Value),
 	}
+
+	builder := graph.NewGraphBuilder[int, *Value](
+		g.getNodeIdByValue,
+		g.getNeighbors,
+		g.handleEdge,
+	)
 	for _, value := range v {
-		g.CreateNode(value)
+		builder.BuildGraph(value)
 	}
-	g.marshaledValue = nil
 	return g
 }
 
@@ -44,7 +50,7 @@ func (g *ValueGraph) ShowDot() {
 	fmt.Println(buf.String())
 }
 
-func (g *ValueGraph) GetNodeIdByValue(value *Value) int {
+func (g *ValueGraph) getNodeIdByValue(value *Value) int {
 	log.Infof("create node %d: %v, %p", value.GetId(), value.GetVerboseName(), value)
 	// get node id, if existed, no need to create
 
@@ -63,39 +69,53 @@ func (g *ValueGraph) GetNodeIdByValue(value *Value) int {
 	return nodeId
 }
 
-func (g *ValueGraph) CreateNode(value *Value) int {
-	nodeId := g.GetNodeIdByValue(value)
-	g._marshal(nodeId, value)
-	return nodeId
+func (g *ValueGraph) getValueByNodeId(node int) *Value {
+	return g.Node2Value[node]
 }
 
-func (g *ValueGraph) _marshal(selfID int, value *Value) {
-	if _, ok := g.marshaledValue[selfID]; ok {
-		return
-	}
-	g.marshaledValue[selfID] = struct{}{}
-
-	for _, node := range value.GetDependOn() {
-		id := g.CreateNode(node)
-		g.AddEdge(selfID, id, "")
-	}
-	for _, node := range value.GetEffectOn() {
-		id := g.CreateNode(node)
-		g.AddEdge(id, selfID, "")
+func (g *ValueGraph) getNeighbors(node int) []graph.NeighborWithEdgeType[*Value] {
+	value := g.getValueByNodeId(node)
+	if value == nil {
+		return nil
 	}
 
+	var res []graph.NeighborWithEdgeType[*Value]
+	for _, v := range value.GetDependOn() {
+		res = append(res, graph.NewNeighbor(v, "depend_on"))
+	}
+	for _, v := range value.GetEffectOn() {
+		res = append(res, graph.NewNeighbor(v, "effect_on"))
+	}
 	for _, predecessor := range value.GetPredecessors() {
 		if predecessor.Node == nil {
 			continue
 		}
-		predecessorNodeID := g.CreateNode(predecessor.Node)
-		edges := g.GetEdges(predecessorNodeID, selfID)
+		neighbor := graph.NewNeighbor(predecessor.Node, "predecessor")
+		neighbor.AddExtraMsg("label", predecessor.Info.Label)
+		neighbor.AddExtraMsg("step", predecessor.Info.Step)
+	}
+	return res
+}
 
-		edgeLabel := predecessor.Info.Label
-		if predecessor.Info.Step > 0 {
-			edgeLabel = fmt.Sprintf(`step[%v]: %v`, predecessor.Info.Step, edgeLabel)
+func (g *ValueGraph) handleEdge(fromNode int, toNode int, edgeType string, extraMsg map[string]any) {
+	switch edgeType {
+	case "depend_on":
+		g.AddEdge(fromNode, toNode, "")
+	case "effect_on":
+		g.AddEdge(toNode, fromNode, "")
+	case "predecessor":
+		edges := g.GetEdges(fromNode, toNode)
+		var (
+			label, edgeLabel string
+			step             int64
+		)
+		if extraMsg != nil {
+			label = extraMsg["label"].(string)
+			step = extraMsg["step"].(int64)
 		}
-
+		if step > 0 {
+			edgeLabel = fmt.Sprintf(`step[%v]: %v`, step, label)
+		}
 		if len(edges) > 0 {
 			for _, edge := range edges {
 				g.EdgeAttribute(edge, "color", "red")
@@ -104,7 +124,7 @@ func (g *ValueGraph) _marshal(selfID int, value *Value) {
 				g.EdgeAttribute(edge, "label", edgeLabel)
 			}
 		} else {
-			edgeId := g.AddEdge(predecessorNodeID, selfID, edgeLabel)
+			edgeId := g.AddEdge(toNode, fromNode, edgeLabel)
 			g.EdgeAttribute(edgeId, "color", "red")
 			g.EdgeAttribute(edgeId, "fontcolor", "red")
 			g.EdgeAttribute(edgeId, "penwidth", "3.0")
