ad6951514834cceb12a1d5bc2961ffaf7ec733d2
z3 <z3r0ne@163.com>
2025-02-21 14:56:19 +0800

    fix test

--- common/javaclassparser/classes/static/syntax_test/SwitchScopeTest.java
+++ common/javaclassparser/classes/static/syntax_test/SwitchScopeTest.java
@@ -4,21 +4,20 @@ public class SwitchScopeTest {
 	public SwitchScopeTest() {
 	}
 	void main() {
-		int var3;
 		int var1 = 1;
 		switch (var1){
 		case 1:
 			int var2 = 2;
 			switch (var1){
 			case 1:
-				var3 = 3;
+				int var3 = 3;
 			case 2:
 				int var4 = 4;
 			default:
 
 			}
 		case 2:
-			var3 = 5;
+			int var3 = 5;
 		default:
 			return;
 		}
