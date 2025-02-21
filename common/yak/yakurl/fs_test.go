2ee1f2bd03f937ab75c9911b10f0b771688bb213
wlz <lingze.wl@gmail.com>
2025-02-21 11:24:01 +0800

    fix(yakurl):fs url escape path

---
+++ common/yak/yakurl/fs_test.go
@@ -0,0 +1,42 @@
+package yakurl_test
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/yak/ssa/ssadb"
+	"github.com/yaklang/yaklang/common/yakgrpc"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+)
+
+func TestEscape(t *testing.T) {
+	local, err := yakgrpc.NewLocalClient()
+	require.NoError(t, err)
+
+	t.Run("test ()", func(t *testing.T) {
+		progName := uuid.NewString()
+		defer ssadb.DeleteProgram(ssadb.GetDB(), progName)
+
+		// fileName := "high.php"
+		folderName := "a"
+		folderPath := []string{progName, "vulnerabilities", "csrf", "source"}
+		ssadb.SaveFolder(folderName, folderPath)
+		path := fmt.Sprintf("/%s/%s", strings.Join(folderPath, "/"), folderName)
+
+		schema := "ssadb"
+		raw := fmt.Sprintf("%s://%s", schema, path)
+		res, err := local.RequestYakURL(context.Background(), &ypb.RequestYakURLParams{
+			Method: "GET",
+			Url: &ypb.YakURL{
+				FromRaw: raw,
+			},
+		})
+		require.NoError(t, err)
+		spew.Dump(res)
+	})
+}
