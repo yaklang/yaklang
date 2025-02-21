6cc04988446071294fa9fcef282f0d47fdb94739
WAY29 <41830147+WAY29@users.noreply.github.com>
2025-02-21 17:16:43 +0800

    fix(mcp):
    1. sse client timeout
    2.test output
    3. tool:port_scan,exec_yak_script ignore noise message

--- common/mcp/utils.go
+++ common/mcp/utils.go
@@ -3,33 +3,9 @@ package mcp
 import (
 	"encoding/json"
 
-	"github.com/tidwall/gjson"
 	"github.com/yaklang/yaklang/common/mcp/mcp-go/mcp"
 	"github.com/yaklang/yaklang/common/utils"
-	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
-
-func handleExecMessage(exec *ypb.ExecResult) string {
-	content := string(exec.Message)
-	// handle complex message
-	msgContent := gjson.GetBytes(exec.Message, "content")
-	level := msgContent.Get("level").String()
-	switch level {
-	case "feature-status-card-data":
-		data := msgContent.Get("data").String()
-		// ignore empty risk message
-		if gjson.Get(data, "id").String() == "漏洞/风险/指纹" {
-			cardCount := int(gjson.Get(data, "data").Int())
-			if cardCount == 0 {
-				return ""
-			}
-		}
-	case "info", "json":
-		// use content directly
-		content = msgContent.Get("data").String()
-	}
-	return content
-}
 
 func NewCommonCallToolResult(data any) (*mcp.CallToolResult, error) {
 	var result string
