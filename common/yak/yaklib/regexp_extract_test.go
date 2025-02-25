9e93f6089ee1d4e211a34ccfd81ecd016067c2e9
z3 <z3r0ne@163.com>
2025-02-25 16:27:30 +0800

    add test into ci

--- common/yak/yaklib/regexp_extract_test.go
+++ common/yak/yaklib/regexp_extract_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRegexpMatchPathParam(t *testing.T) {
+func TestMUSTPASS_Common_RegexpMatchPathParam(t *testing.T) {
 	assert.Equal(t, []string{"/docs/api/re?name=anonymous"}, RegexpMatchPathParam("agsdjkfkaj yaklang.com/docs/api/re?name=anonymous"))
 	assert.Equal(t, []string{"/docs/api/re?name=anonymous"}, RegexpMatchPathParam("asgdjashttp://yaklang.com/docs/api/re?name=anonymous asdasdggjkasd"))
 	assert.Equal(t, []string{"/docs/api/re?name=anonymous"}, RegexpMatchPathParam("aasd http://yaklang.com/docs/api/re?name=anonymous#tqwyuetasgd"))
