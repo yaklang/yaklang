09832841e3a97680bd326df52608e8d78bc772a5
intSheep <1098204193@qq.com>
2025-02-28 16:01:55 +0800

    fix some error and add tests

--- common/utils/filesys/unified_fs_test.go
+++ common/utils/filesys/unified_fs_test.go
@@ -73,7 +73,14 @@ func TestUnifiedFs_File_Operate(t *testing.T) {
 	})
 
 	t.Run("TestUnifiedFS_Exists", func(t *testing.T) {
-		exist, err := uf.Exists(unifiedPath)
+		fileName := "a$a$c.txt"
+		exist, _ := uf.Exists(fileName)
+		require.False(t, exist)
+
+		data := []byte(uuid.NewString())
+		uf.MkdirAll(fileName, 0755)
+		uf.WriteFile(fileName, data, 0755)
+		exist, err := uf.Exists(fileName)
 		require.NoError(t, err)
 		require.True(t, exist)
 	})
@@ -96,9 +103,9 @@ func TestUnifiedFs_File_Operate(t *testing.T) {
 
 func TestUnifiedFs_ClassToJavaExtMap(t *testing.T) {
 	temp := t.TempDir()
-	lf := NewRelLocalFs(temp)
-	uf := NewUnifiedFS(
-		lf,
+	innerFs := NewRelLocalFs(temp)
+	outerFs := NewUnifiedFS(
+		innerFs,
 		WithUnifiedFsExtMap(".class", ".java"),
 	)
 
@@ -107,7 +114,7 @@ func TestUnifiedFs_ClassToJavaExtMap(t *testing.T) {
 		expectedRealPath := filepath.Join(temp, "Example.class")
 
 		content := uuid.NewString()
-		err := uf.WriteFile(unifiedPath, []byte(content), 0644)
+		err := outerFs.WriteFile(unifiedPath, []byte(content), 0644)
 		require.NoError(t, err)
 
 		realContent, err := os.ReadFile(expectedRealPath)
@@ -120,7 +127,7 @@ func TestUnifiedFs_ClassToJavaExtMap(t *testing.T) {
 		expectedRealPath := filepath.Join(temp, "Example.class")
 		err := os.WriteFile(expectedRealPath, []byte("test content"), 0644)
 		require.NoError(t, err)
-		data, err := uf.ReadFile(unifiedPath)
+		data, err := outerFs.ReadFile(unifiedPath)
 		require.NoError(t, err)
 		require.Equal(t, []byte("test content"), data)
 	})
@@ -134,7 +141,7 @@ func TestUnifiedFs_ClassToJavaExtMap(t *testing.T) {
 		err := os.WriteFile(expectedOldPath, []byte("rename test"), 0644)
 		require.NoError(t, err)
 		// 执行重命名
-		err = uf.Rename(unifiedOld, unifiedNew)
+		err = outerFs.Rename(unifiedOld, unifiedNew)
 		require.NoError(t, err)
 		// 验证新文件存在
 		_, err = os.Stat(expectedNewPath)
@@ -153,7 +160,7 @@ func TestUnifiedFs_ClassToJavaExtMap(t *testing.T) {
 		require.NoError(t, err)
 
 		// 执行删除
-		err = uf.Delete(unifiedPath)
+		err = outerFs.Delete(unifiedPath)
 		require.NoError(t, err)
 
 		// 验证文件已删除
@@ -168,20 +175,20 @@ func TestUnifiedFs_ClassToJavaExtMap(t *testing.T) {
 		err := os.WriteFile(expectedRealPath, []byte("markdown content"), 0644)
 		require.NoError(t, err)
 
-		data, err := uf.ReadFile(unifiedPath)
+		data, err := outerFs.ReadFile(unifiedPath)
 		require.NoError(t, err)
 		require.Equal(t, []byte("markdown content"), data)
 	})
 
 	t.Run("TestExtNoAffected", func(t *testing.T) {
-		ret := uf.Ext("aaa.java")
+		ret := outerFs.Ext("aaa.java")
 		require.Equal(t, ".java", ret)
 	})
 
 	t.Run("TestVirtualExtNoAffected", func(t *testing.T) {
 		unifiedPath := "testAAA.java"
-		uf.WriteFile(unifiedPath, []byte("test content"), 0644)
-		dir, err := uf.ReadDir(".")
+		outerFs.WriteFile(unifiedPath, []byte("test content"), 0644)
+		dir, err := outerFs.ReadDir(".")
 		require.NoError(t, err)
 		check := false
 		for _, entry := range dir {
@@ -193,17 +200,17 @@ func TestUnifiedFs_ClassToJavaExtMap(t *testing.T) {
 	})
 	t.Run("Test state", func(t *testing.T) {
 		test1 := "test1.java"
-		uf.WriteFile(test1, []byte("test1"), 0644)
+		outerFs.WriteFile(test1, []byte("test1"), 0644)
 
-		info, err := uf.Stat(test1)
+		info, err := outerFs.Stat(test1)
 		require.NoError(t, err)
 		require.Equal(t, "test1.java", info.Name())
 	})
 	t.Run("test open", func(t *testing.T) {
 		test2 := "test2.java"
-		uf.WriteFile(test2, []byte("test2"), 0644)
+		outerFs.WriteFile(test2, []byte("test2"), 0644)
 
-		info, err := uf.Open(test2)
+		info, err := outerFs.Open(test2)
 		require.NoError(t, err)
 		require.NotNil(t, info)
 		fi, err := info.Stat()
@@ -213,14 +220,61 @@ func TestUnifiedFs_ClassToJavaExtMap(t *testing.T) {
 
 	t.Run("test openfile ", func(t *testing.T) {
 		test3 := "test3.java"
-		uf.WriteFile(test3, []byte("test3"), 0644)
+		outerFs.WriteFile(test3, []byte("test3"), 0644)
 
-		info, err := uf.OpenFile(test3, os.O_RDONLY, 0644)
+		info, err := outerFs.OpenFile(test3, os.O_RDONLY, 0644)
 		require.NoError(t, err)
 		require.NotNil(t, info)
 		fi, err := info.Stat()
 		require.NoError(t, err)
 		require.Equal(t, "test3.java", fi.Name())
+	})
+
+	t.Run("test exist virtual ext file", func(t *testing.T) {
+		fileName := "test4.class"
+		innerFs.WriteFile(fileName, []byte("test4"), 0644)
+		file, err := outerFs.Open("test4.class")
+		require.NoError(t, err)
+		info, err := file.Stat()
+		require.NoError(t, err)
+		require.Equal(t, fileName, info.Name())
+	})
+
+	t.Run("test rename exist virtual ext file", func(t *testing.T) {
+		fileName := `toRename.class`
+		data := []byte(uuid.NewString())
+		err := innerFs.WriteFile(fileName, data, 0644)
+		require.NoError(t, err)
+		err = outerFs.Rename(fileName, "toRename.java")
+		require.Error(t, err)
+	})
+
+	t.Run("test rename to exist virtual ext file", func(t *testing.T) {
+		to := `renameTo.class`
+		origin := `origin.class`
+		data := []byte(uuid.NewString())
+		err := innerFs.WriteFile(to, data, 0644)
+		require.NoError(t, err)
+		innerFs.WriteFile(origin, data, 0644)
+		require.NoError(t, err)
+		err = outerFs.Rename(origin, to)
+		require.Error(t, err)
+	})
+
+	t.Run("test open existed virtual ext file", func(t *testing.T) {
+		name := uuid.NewString()[:4]
+		fileName := name + ".java"
+
+		data1 := []byte(uuid.NewString())
+		err := innerFs.WriteFile(fileName, data1, 0644)
+		require.NoError(t, err)
+		data2 := []byte(uuid.NewString())
+		err = innerFs.WriteFile(name+".class", data2, 0644)
+		require.NoError(t, err)
+
+		raw, err := outerFs.ReadFile(fileName)
+		require.NoError(t, err)
+		require.Equal(t, data1, raw)
 	})
 }
 
@@ -250,9 +304,9 @@ func TestUnifiedFs_Recursive(t *testing.T) {
 		content = append(content, string(data))
 		return nil
 	}))
-	expectedPath := []string{"a$b$c.java", "a$d.java"}
-	expectedName := []string{"c.java", "d.java"}
-	expectedContent := []string{"c content", "d content"}
+	expectedPath := []string{"a$b$c.java", "a$d.java", "a$e.java"}
+	expectedName := []string{"c.java", "d.java", "e.java"}
+	expectedContent := []string{"c content", "d content", "e content"}
 	require.Equal(t, expectedPath, path)
 	require.Equal(t, expectedName, infoName)
 	require.Equal(t, expectedContent, content)
