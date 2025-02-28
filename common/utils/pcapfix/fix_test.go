9c4a0f7a5d0864b8ab3b32831c8b6817451cd5b8
rookie <rookie_is@qq.com>
2025-02-28 11:01:41 +0800

    refactor(pcap-fix):add pcap fix privilege mode

--- common/utils/pcapfix/fix_test.go
+++ common/utils/pcapfix/fix_test.go
@@ -1,7 +1,17 @@
 package pcapfix
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFix(t *testing.T) {
+	//lookupBpf := func() {
+	//	output, err := exec.Command("sh", "-c", "ls -al /dev/bpf*").Output()
+	//	require.NoError(t, err)
+	//	fmt.Println(string(output))
+	//}
+	//
+	//lookupBpf()
 	Fix()
+	//lookupBpf()
 }
