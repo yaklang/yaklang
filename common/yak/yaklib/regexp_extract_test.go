5bd2df6f80a4a60ac0dff42018d165101bee2c5e
z3 <z3r0ne@163.com>
2025-03-07 15:53:56 +0800

    fix RegexpMatchPathParam

--- common/yak/yaklib/regexp_extract_test.go
+++ common/yak/yaklib/regexp_extract_test.go
@@ -7,6 +7,8 @@ import (
 )
 
 func TestMUSTPASS_Common_RegexpMatchPathParam(t *testing.T) {
+	assert.Equal(t, []string{"/docs/api/re?"}, RegexpMatchPathParam("agsdjkfkaj yaklang.com/docs/api/re? asgdashjd"))
+	assert.Equal(t, []string{"/docs/api/re"}, RegexpMatchPathParam("agsdjkfkaj yaklang.com/docs/api/re asgdashjd"))
 	assert.Equal(t, []string{"/docs/api/re?name=anonymous"}, RegexpMatchPathParam("agsdjkfkaj yaklang.com/docs/api/re?name=anonymous"))
 	assert.Equal(t, []string{"/docs/api/re?name=anonymous"}, RegexpMatchPathParam("asgdjashttp://yaklang.com/docs/api/re?name=anonymous asdasdggjkasd"))
 	assert.Equal(t, []string{"/docs/api/re?name=anonymous"}, RegexpMatchPathParam("aasd http://yaklang.com/docs/api/re?name=anonymous#tqwyuetasgd"))
