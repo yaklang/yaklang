282ec780ce4e0a1ebd48b100b8fefa8b4eae1931
Q16G <760046475@qq.com>
2025-03-19 12:13:46 +0800

    feature(ci): feature ssa ci

---
+++ common/ssa_bootstrapping/build_rule_check/dvwa_rule_test.go
@@ -0,0 +1,38 @@
+package ssa_bootstrapping
+
+import (
+	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/schema"
+	"github.com/yaklang/yaklang/common/yak/ssaapi"
+	"testing"
+)
+
+func TestBuildInRule(t *testing.T) {
+	testCase := []RuleChecker{{
+		Name: "test git repo",
+		ConfigInfo: &ssaapi.ConfigInfo{
+			Kind:   ssaapi.Git,
+			URL:    "https://github.com/digininja/DVWA",
+			Branch: "master",
+		},
+		Language:        string(consts.PHP),
+		RequiredExclude: true,
+		RiskInfo: []*RiskInfo{
+			{
+				Kind:         Checked,
+				RuleName:     "重定向漏洞",
+				FileName:     "/vulnerabilities/open_redirect/source/low.php",
+				Line:         4,
+				StartLine:    4,
+				EndLine:      4,
+				StartColumn:  25,
+				EndColumn:    42,
+				Severity:     schema.SFR_SEVERITY_HIGH,
+				RiskVariable: "high",
+			},
+		},
+	},
+	}
+	require.NoError(t, startCase(testCase))
+}
