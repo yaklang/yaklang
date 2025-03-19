0be4f6aa1fa75ef51843c071c3e7d6d9ff7588ef
intSheep <1098204193@qq.com>
2025-03-19 17:46:39 +0800

    feat(grpc):support httpflow analyze

---
+++ common/yakgrpc/grpc_httpflow_analyze.go
@@ -0,0 +1,314 @@
+package yakgrpc
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/jinzhu/gorm"
+	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/schema"
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/yak"
+	"github.com/yaklang/yaklang/common/yak/yaklib"
+	"github.com/yaklang/yaklang/common/yakgrpc/model"
+	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"sync/atomic"
+)
+
+func (s *Server) AnalyzeHTTPFlow(request *ypb.AnalyzeHTTPFlowRequest, stream ypb.Yak_AnalyzeHTTPFlowServer) error {
+	if request == nil {
+		return utils.Errorf("AnalyzeHTTPFlow request is nil")
+	}
+	rules := request.GetReplacers()
+	analyzedId := uuid.NewString()
+	client := yaklib.NewVirtualYakitClientWithRuntimeID(func(result *ypb.ExecResult) error {
+		result.RuntimeID = analyzedId
+		return stream.Send(&ypb.AnalyzeHTTPFlowResponse{
+			ExecResult: result,
+		})
+	}, analyzedId)
+	manger, err := NewHTTPFlowAnalyzeManger(s.GetProfileDatabase(), stream.Context(), client, stream, rules...)
+	if err != nil {
+		return err
+	}
+
+	if request.GetHotPatchCode() != "" {
+		err = manger.pluginCaller.LoadHotPatch(manger.ctx, nil, request.GetHotPatchCode())
+		if err != nil {
+			return err
+		}
+	}
+
+	err = manger.AnalyzeHTTPFlow(s.GetProjectDatabase(), analyzedId)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) QueryAnalyzedHTTPFlowRule(ctx context.Context, req *ypb.QueryAnalyzedHTTPFlowRuleRequest) (*ypb.QueryAnalyzedHTTPFlowRuleResponse, error) {
+	p, datas, err := yakit.QueryAnalyzedHTTPFlowRule(s.GetProjectDatabase(), req)
+	if err != nil {
+		return nil, err
+	}
+	rsp := &ypb.QueryAnalyzedHTTPFlowRuleResponse{
+		Pagination: req.GetPagination(),
+		Total:      int64(p.TotalRecord),
+	}
+	for _, data := range datas {
+		model := data.ToGRPCModel()
+		rsp.Data = append(rsp.Data, model)
+	}
+	return rsp, nil
+}
+
+type HTTPFlowAnalyzeManger struct {
+	*mitmReplacer
+	client                 *yaklib.YakitClient
+	stream                 ypb.Yak_AnalyzeHTTPFlowServer
+	ctx                    context.Context
+	matchedHTTPFlowCount   int64
+	extractedHTTPFlowCount int64
+	pluginCaller           *yak.MixPluginCaller // for hot patch code exec
+
+	allHTTPFlowCount     int64
+	handledHTTPFlowCount int64
+}
+
+func NewHTTPFlowAnalyzeManger(
+	gorm *gorm.DB,
+	ctx context.Context,
+	client *yaklib.YakitClient,
+	steam ypb.Yak_AnalyzeHTTPFlowServer,
+	rules ...*ypb.MITMContentReplacer,
+) (*HTTPFlowAnalyzeManger, error) {
+	m := &HTTPFlowAnalyzeManger{
+		mitmReplacer: NewMITMReplacerFromDB(gorm),
+		client:       client,
+		stream:       steam,
+		ctx:          ctx,
+	}
+	m.SetRules(rules...)
+	caller, err := yak.NewMixPluginCaller()
+	if err != nil {
+		return nil, err
+	}
+	caller.SetDividedContext(true)
+	caller.SetConcurrent(20)
+	caller.SetLoadPluginTimeout(10)
+	caller.SetCallPluginTimeout(consts.GetGlobalCallerCallPluginTimeout())
+	m.pluginCaller = caller
+	return m, nil
+}
+
+func (m *HTTPFlowAnalyzeManger) AnalyzeHTTPFlow(db *gorm.DB, analyzeId string) (errs error) {
+	defer func() {
+		m.notifyProcess(1)
+		if err := recover(); err != nil {
+			errs = utils.JoinErrors(errs, utils.Errorf("panic: %s", err))
+			return
+		}
+	}()
+
+	// 热加载和规则匹配进度条比例
+	ruleProcessPercent := 0.5
+	if !m.haveHotPatch() {
+		ruleProcessPercent = 1
+	}
+	hotPotProcessPercent := 1 - ruleProcessPercent
+	// 执行规则
+	err := m.ExecReplacerRules(db, analyzeId, ruleProcessPercent)
+	if err != nil {
+		errs = utils.JoinErrors(errs, err)
+	}
+	// 执行热加载
+	if m.haveHotPatch() {
+		m.ExecHotPatch(db, analyzeId, hotPotProcessPercent)
+	}
+	return errs
+}
+
+func (m *HTTPFlowAnalyzeManger) ExecReplacerRules(db *gorm.DB, analyzeId string, processPercent float64) error {
+	if !m.haveRules() {
+		return utils.Error("analyze rules is empty")
+	}
+
+	extractData := func(pattern string, rule *MITMReplaceRule, flow *schema.HTTPFlow, isReq bool) []*schema.ExtractedData {
+		modelFull, err := model.ToHTTPFlowGRPCModelFull(flow)
+		if err != nil {
+			return nil
+		}
+		var packet []byte
+		if isReq {
+			packet = modelFull.Request
+		} else {
+			packet = modelFull.Response
+		}
+		_, matcheds, err := rule.MatchPacket(packet, isReq)
+		if err != nil {
+			log.Infof("match packet failed: %s", err)
+			return nil
+		}
+
+		var extractedData []*schema.ExtractedData
+		for _, matched := range matcheds {
+			e := yakit.ExtractedDataFromHTTPFlow(
+				flow.HiddenIndex,
+				rule.VerboseName,
+				matched,
+				pattern,
+			)
+			// save extracted data
+			err = yakit.CreateOrUpdateExtractedDataEx(-1, e)
+			if err != nil {
+				log.Infof("create or update extracted data failed: %s", err)
+				continue
+			}
+			atomic.AddInt64(&m.extractedHTTPFlowCount, 1)
+			m.notifyExtractedHTTPFlowNum()
+			extractedData = append(extractedData, e)
+		}
+		return extractedData
+	}
+
+	getAnalyzedHTTPFlow := func(rule *MITMReplaceRule, flow *schema.HTTPFlow) *schema.AnalyzedHTTPFlow {
+		atomic.AddInt64(&m.matchedHTTPFlowCount, 1)
+		m.notifyMatchedHTTPFlowNum()
+		analyzeResult := &schema.AnalyzedHTTPFlow{
+			ResultId:        analyzeId,
+			Rule:            rule.GetRule(),
+			RuleVerboseName: rule.VerboseName,
+		}
+		analyzeResult.HTTPFlow = flow
+		return analyzeResult
+	}
+
+	saveAnalyzedHTTPFlow := func(result *schema.AnalyzedHTTPFlow) {
+		err := db.Save(result).Error
+		if err != nil {
+			log.Infof("save analyze result failed: %s", err)
+		}
+		atomic.AddInt64(&m.handledHTTPFlowCount, 1)
+		m.notifyResult(result)
+	}
+
+	handleColorAndTag := func(rule *MITMReplaceRule, flow *schema.HTTPFlow) {
+		err := yakit.HandleAnalyzedHTTPFlowsColorAndTag(
+			db,
+			flow,
+			rule.GetColor(),
+			rule.GetExtraTag()...,
+		)
+		if err != nil {
+			log.Infof("handle analyzed http flows color and tag failed: %s", err)
+		}
+	}
+
+	totalCallBack := func(i int) {
+		atomic.AddInt64(&m.allHTTPFlowCount, int64(i))
+	}
+	// 规则处理
+	for i, rule := range m.rules {
+		m.notifyProcess(float64(i+1) / float64(len(m.rules)) * processPercent)
+		re, err := rule.compile()
+		if err != nil {
+			continue
+		}
+		pattern := re.String()
+		if rule.EnableForRequest {
+			for flow := range yakit.QueryHTTPFlowsByRegexRequest(db, m.ctx, pattern, totalCallBack, rule.EffectiveURL) {
+				atomic.AddInt64(&m.allHTTPFlowCount, 1)
+				result := getAnalyzedHTTPFlow(rule, flow)
+				if result == nil {
+					continue
+				}
+				extracts := extractData(pattern, rule, flow, true)
+				result.ExtractedData = extracts
+				saveAnalyzedHTTPFlow(result)
+				handleColorAndTag(rule, flow)
+			}
+		}
+		if rule.EnableForResponse {
+			for flow := range yakit.QueryHTTPFlowsByRegexResponse(db, m.ctx, pattern, totalCallBack, rule.EffectiveURL) {
+				atomic.AddInt64(&m.allHTTPFlowCount, 1)
+				result := getAnalyzedHTTPFlow(rule, flow)
+				if result == nil {
+					continue
+				}
+				extracts := extractData(pattern, rule, flow, false)
+				result.ExtractedData = extracts
+				saveAnalyzedHTTPFlow(result)
+				handleColorAndTag(rule, flow)
+			}
+		}
+		m.notifyHandleFlowNum()
+	}
+	return nil
+}
+
+func (m *HTTPFlowAnalyzeManger) ExecHotPatch(db *gorm.DB, analyzeId string, processPercent float64) {
+	if m.pluginCaller == nil {
+		return
+	}
+	var totalQueryCount int64
+	totalCallBack := func(i int) {
+		atomic.AddInt64(&m.allHTTPFlowCount, int64(i))
+		atomic.AddInt64(&totalQueryCount, int64(i))
+	}
+	flowCh := yakit.YieldHTTPFlowsEx(db, m.ctx, totalCallBack)
+	extract := func(ruleName string, flow *schema.HTTPFlow) {
+		yakit.UpdateHTTPFlowTags(db, flow)
+		analyzed := &schema.AnalyzedHTTPFlow{
+			ResultId:        analyzeId,
+			Rule:            "热加载规则",
+			RuleVerboseName: ruleName,
+			HTTPFlow:        flow,
+		}
+		err := db.Save(analyzed).Error
+		if err != nil {
+			log.Infof("save analyze result failed: %s", err)
+		}
+		atomic.AddInt64(&m.handledHTTPFlowCount, 1)
+		m.notifyResult(analyzed)
+		m.notifyMatchedHTTPFlowNum()
+		m.notifyHandleFlowNum()
+	}
+	var count int64
+	before := 1 - processPercent
+	for flow := range flowCh {
+		atomic.AddInt64(&count, 1)
+		m.pluginCaller.CallAnalyzeHTTPFlow(m.ctx, flow, extract)
+		m.notifyProcess(before + float64(count)/float64(totalQueryCount)*processPercent)
+	}
+}
+
+func (m *HTTPFlowAnalyzeManger) notifyProcess(process float64) {
+	m.client.YakitSetProgress(process)
+}
+
+func (m *HTTPFlowAnalyzeManger) notifyMatchedHTTPFlowNum() {
+	m.client.StatusCard("符合条件数", m.matchedHTTPFlowCount)
+}
+
+func (m *HTTPFlowAnalyzeManger) notifyExtractedHTTPFlowNum() {
+	m.client.StatusCard("提取数据", m.extractedHTTPFlowCount)
+}
+
+func (m *HTTPFlowAnalyzeManger) notifyHandleFlowNum() {
+	m.client.StatusCard("已处理数/总数", fmt.Sprintf("%d/%d", m.handledHTTPFlowCount, m.allHTTPFlowCount))
+}
+
+func (m *HTTPFlowAnalyzeManger) notifyResult(result *schema.AnalyzedHTTPFlow) {
+	m.stream.Send(&ypb.AnalyzeHTTPFlowResponse{
+		RuleData: result.ToGRPCModel(),
+	})
+}
+
+func (m *HTTPFlowAnalyzeManger) haveHotPatch() bool {
+	if m.pluginCaller == nil {
+		return false
+	}
+	return m.pluginCaller.HaveTheHookFunc(yak.HOOK_Analyze_HTTPFlow)
+}
---
+++ common/yakgrpc/grpc_httpflow_analyze_test.go
@@ -0,0 +1,357 @@
+package yakgrpc
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/schema"
+	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"strings"
+	"testing"
+)
+
+type contentData struct {
+	ID   string `json:"id"`
+	Data string `json:"data"`
+	Tags string `json:"tags"`
+}
+
+func createHTTPFlow(url, req, rsp string) (error, func()) {
+	flow := &schema.HTTPFlow{
+		Request:  req,
+		Response: rsp,
+		Url:      url,
+	}
+	err := yakit.SaveHTTPFlow(consts.GetGormProjectDatabase(), flow)
+	if err != nil {
+		return err, func() {}
+	}
+	return nil, func() {
+		yakit.DeleteHTTPFlowByID(consts.GetGormProjectDatabase(), int64(flow.ID))
+	}
+}
+
+func TestMUSTPASS_AnalyzeHTTPFlow(t *testing.T) {
+	client, err := NewLocalClient()
+	require.NoError(t, err)
+
+	t.Run("test analyze http flow:regex request", func(t *testing.T) {
+		token := uuid.NewString()
+		req := `POST /post HTTP/1.1
+Host: %s
+` + fmt.Sprintf(`
+%s
+`, token)
+		err, deleteFlow := createHTTPFlow("http://www.baidu.com", req, "abc")
+		defer deleteFlow()
+
+		require.NoError(t, err)
+		ruleVerboseName := uuid.NewString()
+		tag := uuid.NewString()
+		color := "red"
+		stream, err := client.AnalyzeHTTPFlow(context.Background(), &ypb.AnalyzeHTTPFlowRequest{
+			HotPatchCode: "",
+			Replacers: []*ypb.MITMContentReplacer{
+				{
+					EnableForRequest: true,
+					Rule:             token,
+					VerboseName:      ruleVerboseName,
+					Color:            color,
+					ExtraTag:         []string{tag},
+				},
+			},
+		})
+		require.NoError(t, err)
+
+		var (
+			resultId     string
+			finalProcess float64
+			finalMatch   string
+		)
+		{
+			// 测试进度条和分析结果
+			for {
+				rsp, err := stream.Recv()
+				if err != nil {
+					break
+				}
+				resultId = rsp.ExecResult.GetRuntimeID()
+				result := rsp.GetExecResult().GetMessage()
+				var msg msg
+				json.Unmarshal(result, &msg)
+				if msg.Type == "progress" {
+					finalProcess = msg.Content.Process
+				}
+				if msg.Type == "log" {
+					var contentData contentData
+					json.Unmarshal([]byte(msg.Content.Data), &contentData)
+					if contentData.ID == "符合条件数" {
+						finalMatch = contentData.Data
+					}
+				}
+
+				ruleData := rsp.GetRuleData()
+				if ruleData != nil {
+					fmt.Println(ruleData)
+				}
+			}
+			require.Equal(t, float64(1), finalProcess)
+			require.Equal(t, "1", finalMatch)
+		}
+
+		{
+			// 测试QueryAnalyzedHTTPFlowRule查询结果
+			ruleRsp, err := client.QueryAnalyzedHTTPFlowRule(context.Background(), &ypb.QueryAnalyzedHTTPFlowRuleRequest{
+				Filter: &ypb.AnalyzedHTTPFlowFilter{
+					ResultIds: []string{resultId},
+				},
+			})
+			require.NoError(t, err)
+			fmt.Println(ruleRsp)
+			require.Equal(t, int64(1), ruleRsp.Total)
+			require.Equal(t, 1, len(ruleRsp.Data))
+			require.Equal(t, ruleVerboseName, ruleRsp.Data[0].RuleVerboseName)
+
+			httpflowId := ruleRsp.Data[0].HTTPFlowId
+			queryFlow, err := client.QueryHTTPFlows(context.Background(), &ypb.QueryHTTPFlowRequest{
+				IncludeId: []int64{httpflowId},
+			})
+			require.NoError(t, err)
+			require.Equal(t, 1, len(queryFlow.Data))
+			fmt.Println(queryFlow.Data[0])
+			// test color and tag
+			require.Contains(t, queryFlow.Data[0].Tags, tag)
+			require.Contains(t, queryFlow.Data[0].Tags, schema.COLORPREFIX+strings.ToUpper(color))
+
+		}
+	})
+
+	t.Run("test analyze http flow :regex response", func(t *testing.T) {
+		token := uuid.NewString()
+		req := `GET /get HTTP/1.1
+Host: %s
+`
+		err, deleteFlow := createHTTPFlow("www.baidu.com", req, "HTTP/1.1 200 OK\n\n"+token+"\n"+token)
+		defer deleteFlow()
+		require.NoError(t, err)
+		ruleVerboseName := uuid.NewString()
+		tag := uuid.NewString()
+		color := "red"
+		stream, err := client.AnalyzeHTTPFlow(context.Background(), &ypb.AnalyzeHTTPFlowRequest{
+			HotPatchCode: "",
+			Replacers: []*ypb.MITMContentReplacer{
+				{
+					EnableForBody:     true,
+					EnableForResponse: true,
+					Rule:              token,
+					VerboseName:       ruleVerboseName,
+					Color:             color,
+					ExtraTag:          []string{tag},
+				},
+			},
+		})
+		require.NoError(t, err)
+		var (
+			resultId       string
+			finalProcess   float64
+			finalMatch     string
+			finalExtracted string
+		)
+		{
+			// 测试进度条
+			for {
+				rsp, err := stream.Recv()
+				if err != nil {
+					break
+				}
+				resultId = rsp.ExecResult.GetRuntimeID()
+				result := rsp.GetExecResult().GetMessage()
+				var msg msg
+				json.Unmarshal(result, &msg)
+				if msg.Type == "progress" {
+					finalProcess = msg.Content.Process
+				}
+				if msg.Type == "log" {
+					var contentData contentData
+					json.Unmarshal([]byte(msg.Content.Data), &contentData)
+					if contentData.ID == "符合条件数" {
+						finalMatch = contentData.Data
+					}
+					if contentData.ID == "提取数据" {
+						finalExtracted = contentData.Data
+					}
+				}
+
+				ruleData := rsp.GetRuleData()
+				if ruleData != nil {
+					fmt.Println(ruleData)
+				}
+			}
+			require.Equal(t, float64(1), finalProcess)
+			require.Equal(t, "1", finalMatch)
+			require.Equal(t, "2", finalExtracted)
+		}
+
+		{
+			// 测试QueryAnalyzedHTTPFlowRule查询结果
+			ruleRsp, err := client.QueryAnalyzedHTTPFlowRule(context.Background(), &ypb.QueryAnalyzedHTTPFlowRuleRequest{
+				Filter: &ypb.AnalyzedHTTPFlowFilter{
+					ResultIds: []string{resultId},
+				},
+			})
+			require.NoError(t, err)
+			fmt.Println(ruleRsp)
+			require.Equal(t, int64(1), ruleRsp.Total)
+			require.Equal(t, 1, len(ruleRsp.Data))
+			require.Equal(t, ruleVerboseName, ruleRsp.Data[0].RuleVerboseName)
+			queryFlow, err := client.QueryHTTPFlows(context.Background(), &ypb.QueryHTTPFlowRequest{
+				IncludeId: []int64{ruleRsp.Data[0].HTTPFlowId},
+			})
+			require.NoError(t, err)
+			require.Equal(t, 1, len(queryFlow.Data))
+			fmt.Println(queryFlow.Data[0])
+			// test color and tag
+			require.Contains(t, queryFlow.Data[0].Tags, tag)
+			require.Contains(t, queryFlow.Data[0].Tags, schema.COLORPREFIX+strings.ToUpper(color))
+		}
+
+	})
+}
+
+func TestMUSTPASS_AnalyzeHTTPFlow_MutliHTTPFlow(t *testing.T) {
+	urlToken := uuid.NewString()
+	rspToken := uuid.NewString()
+	flows := []struct {
+		url string
+		req string
+		rsp string
+	}{
+		{"http://www.baidu.com", `POST /post HTTP/1.1`, "HTTP/1.1 200 OK\n\nabc\nabc" + rspToken},
+		{"http://www.abc.com", `GET /get HTTP/1.1`, "HTTP/1.1 200 OK\n\nabc\nabc"},
+		{"http://www.cab.com", `POST /post HTTP/1.1`, fmt.Sprintf("HTTP/1.1 200 OK\n\n%s", rspToken)},
+		{fmt.Sprintf("http://www.bac%s.com", urlToken), `GET /get HTTP/1.1`, "HTTP/1.1 200 OK\n\nabc\nabc"},
+		{fmt.Sprintf("http://www.cab%s.com", urlToken), `POST /post HTTP/1.1 `, "abc"},
+		{fmt.Sprintf("http://www.cab%s.com", urlToken), `POST /post HTTP/1.1`, fmt.Sprintf("HTTP/1.1 200 OK\n\n%s", rspToken)},
+	}
+
+	for _, flow := range flows {
+		err, deleteFlow := createHTTPFlow(flow.url, flow.req, flow.rsp)
+		require.NoError(t, err)
+		defer deleteFlow()
+	}
+
+	client, err := NewLocalClient()
+	require.NoError(t, err)
+
+	ruleVerboseName := uuid.NewString()
+	tag := uuid.NewString()
+	color := "red"
+	stream, err := client.AnalyzeHTTPFlow(context.Background(), &ypb.AnalyzeHTTPFlowRequest{
+		HotPatchCode: "",
+		Replacers: []*ypb.MITMContentReplacer{
+			{
+				EnableForBody:     true,
+				EnableForResponse: true,
+				EffectiveURL:      urlToken,
+				Rule:              rspToken,
+				VerboseName:       ruleVerboseName,
+				Color:             color,
+				ExtraTag:          []string{tag},
+			},
+		},
+	})
+	var (
+		resultId       string
+		finalProcess   float64
+		finalMatch     string
+		finalExtracted string
+	)
+	{
+		// 测试进度条
+		for {
+			rsp, err := stream.Recv()
+			if err != nil {
+				break
+			}
+			resultId = rsp.ExecResult.GetRuntimeID()
+			result := rsp.GetExecResult().GetMessage()
+			var msg msg
+			json.Unmarshal(result, &msg)
+			if msg.Type == "progress" {
+				finalProcess = msg.Content.Process
+			}
+			if msg.Type == "log" {
+				var contentData contentData
+				json.Unmarshal([]byte(msg.Content.Data), &contentData)
+				if contentData.ID == "符合条件数" {
+					finalMatch = contentData.Data
+				}
+				if contentData.ID == "提取数据" {
+					finalExtracted = contentData.Data
+				}
+			}
+
+			ruleData := rsp.GetRuleData()
+			if ruleData != nil {
+				fmt.Println(ruleData)
+			}
+		}
+		require.Equal(t, float64(1), finalProcess)
+		require.Equal(t, "1", finalMatch)
+		require.Equal(t, "1", finalExtracted)
+	}
+
+	{
+		ruleRsp, err := client.QueryAnalyzedHTTPFlowRule(context.Background(), &ypb.QueryAnalyzedHTTPFlowRuleRequest{
+			Filter: &ypb.AnalyzedHTTPFlowFilter{
+				ResultIds: []string{resultId},
+			},
+		})
+		require.NoError(t, err)
+		fmt.Println(ruleRsp)
+		require.Equal(t, int64(1), ruleRsp.Total)
+		require.Equal(t, 1, len(ruleRsp.Data))
+		require.Equal(t, ruleVerboseName, ruleRsp.Data[0].RuleVerboseName)
+		queryFlow, err := client.QueryHTTPFlows(context.Background(), &ypb.QueryHTTPFlowRequest{
+			IncludeId: []int64{ruleRsp.Data[0].HTTPFlowId},
+		})
+		require.NoError(t, err)
+		require.Equal(t, 1, len(queryFlow.Data))
+		fmt.Println(queryFlow.Data[0])
+		// test color and tag
+		require.Contains(t, queryFlow.Data[0].Tags, tag)
+		require.Contains(t, queryFlow.Data[0].Tags, schema.COLORPREFIX+strings.ToUpper(color))
+	}
+}
+
+func TestMUSTPASS_AnalyzeHTTPFlow_HotPatch(t *testing.T) {
+	client, err := NewLocalClient()
+	require.NoError(t, err)
+
+	stream, err := client.AnalyzeHTTPFlow(context.Background(), &ypb.AnalyzeHTTPFlowRequest{
+		HotPatchCode: `
+	analyzeHTTPFlow = func(flow,extract){
+    if str.Contains(flow.Url, "baidu")  && str.Contains(string(flow.Response),"总书记"){
+        flow.Blue()
+		 extract("热加载",flow)
+    }
+   
+}
+	`,
+	})
+	require.NoError(t, err)
+	for {
+		rsp, err := stream.Recv()
+		if err != nil {
+			break
+		}
+		result := rsp.GetExecResult().GetMessage()
+		var msg msg
+		json.Unmarshal(result, &msg)
+		fmt.Println(msg)
+	}
+}
