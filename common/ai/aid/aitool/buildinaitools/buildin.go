0e5e059dff75ca05179ee13edac6858ac664a09b
VillanCh <188228725@qq.com>
2025-03-22 12:22:09 +0800

    create some buildin aitools

---
+++ common/ai/aid/aitool/buildinaitools/buildin.go
@@ -0,0 +1,37 @@
+package buildintools
+
+import (
+	"github.com/samber/lo"
+	"github.com/yaklang/yaklang/common/ai/aid/aitool"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils"
+	"io"
+	"time"
+)
+
+func GetBasicBuildInTools() []*aitool.Tool {
+	nowTime, err := aitool.New(
+		"now",
+		aitool.WithDescription("get current time"),
+		aitool.WithStringParam(
+			"timezone",
+			aitool.WithParam_Required(false),
+			aitool.WithParam_Description("timezone for now, like 'Asia/Shanghai' or 'UTC' ... "),
+		),
+		aitool.WithCallback(func(params aitool.InvokeParams, stdout io.Writer, stderr io.Writer) (any, error) {
+			return time.Now().String(), nil
+		}),
+	)
+	if err != nil {
+		log.Errorf("create now tool: %v", err)
+	}
+
+	tools := []*aitool.Tool{nowTime}
+	return lo.Filter(tools, func(item *aitool.Tool, index int) bool {
+		if utils.IsNil(item) {
+			log.Errorf("tool is nil")
+			return false
+		}
+		return true
+	})
+}
