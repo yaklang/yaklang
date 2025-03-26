218e45d751d65cf484a7d3ccc5fd9480fdee17c7
intSheep <intSheep@qq.com>
2025-03-26 16:06:19 +0800

    fix(grpc):add update sf rule state

--- common/yakgrpc/grpc_syntaxflow_rule_update.go
+++ common/yakgrpc/grpc_syntaxflow_rule_update.go
@@ -9,12 +9,22 @@ import (
 )
 
 func (s *Server) QuerySFBuildInRuleUpdate(ctx context.Context, req *ypb.QuerySFBuildInRuleUpdateRequest) (*ypb.QuerySFBuildInRuleUpdateResponse, error) {
-	needUpdate := yakit.Get(consts.EmbedSfBuildInRuleKey) == consts.ExistedSyntaxFlowEmbedFSHash
-	return &ypb.QuerySFBuildInRuleUpdateResponse{NeedUpdate: needUpdate}, nil
+	needUpdate := yakit.Get(consts.EmbedSfBuildInRuleKey) != consts.ExistedSyntaxFlowEmbedFSHash
+	if !needUpdate {
+		return &ypb.QuerySFBuildInRuleUpdateResponse{NeedUpdate: needUpdate}, nil
+	}
+	rules := yakit.QueryBuildInRule(s.GetProfileDatabase())
+	var state ypb.UpdateSFRuleState
+	if len(rules) == 0 {
+		state = ypb.UpdateSFRuleState_Rule_Empty
+	} else {
+		state = ypb.UpdateSFRuleState_Rule_To_Update
+	}
+	return &ypb.QuerySFBuildInRuleUpdateResponse{NeedUpdate: needUpdate, State: state}, nil
 }
 
 func (s *Server) UpdateSFBuildInRule(req *ypb.UpdateSFBuildInRuleRequest, stream ypb.Yak_UpdateSFBuildInRuleServer) error {
-	yakit.Set(consts.EmbedSfBuildInRuleKey, consts.ExistedSyntaxFlowEmbedFSHash)
+	defer yakit.Set(consts.EmbedSfBuildInRuleKey, consts.ExistedSyntaxFlowEmbedFSHash)
 	notify := func(process float64, msg string) {
 		stream.Send(&ypb.UpdateSFBuildInRuleResponse{
 			Percent: process,
