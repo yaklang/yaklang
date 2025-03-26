314e3df6fc59dab22b7e7ee87a44bbf53ed4f92f
Q16G <760046475@qq.com>
2025-03-26 10:52:56 +0800

    fix(rule): fix rule for slices index

--- common/yak/ssaapi/sf_native_call.go
+++ common/yak/ssaapi/sf_native_call.go
@@ -683,6 +683,8 @@ func init() {
 	registerNativeCall(NatiCall_ScanCurrent, nc_func(nativeCallScan(Current)))
 	registerNativeCall(NativeCall_SourceCode, nc_func(nativeCallSourceCode))
 	registerNativeCall(NativeCall_OpCodes, nc_func(nativeCallOpCodes))
+
+	//nativeCall-slice
 	registerNativeCall(NativeCall_Slice, nc_func(func(v sfvm.ValueOperator, frame *sfvm.SFFrame, params *sfvm.NativeCallActualParams) (bool, sfvm.ValueOperator, error) {
 		start := params.GetInt(0, "start")
 		index := params.GetInt(0, "index")
