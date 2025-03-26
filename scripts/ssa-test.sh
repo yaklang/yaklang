f34fe98b83ca430cf601db5ff81258a59a104dac
wlz <lingze.wl@gmail.com>
2025-03-26 11:21:59 +0800

    test(script):update ssa-test.sh same with test-ci

--- scripts/ssa-test.sh
+++ scripts/ssa-test.sh
@@ -1,7 +1,38 @@
 #!/bin/sh -e
 
+echo "Start to Test  AST to SSA"
+
+echo "Start to Test Yak SSA"
+# yak ssa
+go test -timeout 20s ./common/yak/yak2ssa/test/...
+go test -timeout 1m ./common/yak/ssaapi/test/yak
+echo "Start to Test JS"
+# Test js
+go test -timeout 60s ./common/yak/JS2ssa/...
+go test -timeout 1m ./common/yak/ssaapi/test/javascript
+
+echo "Start to Test Java"
+go test -timeout 2m ./common/yak/java/...
+go test -timeout 3m ./common/yak/ssaapi/test/java
+
+echo "Start to Test PHP"
+go test -timeout 60s ./common/yak/php/...
+go test -timeout 2m ./common/yak/ssaapi/test/php
+
+echo "Start to Test Go"
+go test -timeout 30s ./common/yak/go2ssa/...
+go test -timeout 30s ./common/yak/antlr4go/...
+go test -timeout 2m ./common/yak/ssaapi/test/golang
+
+
 echo "Start to Test SSAAPI"
-go test -count=1 -timeout 10m ./common/yak/ssaapi/...
+# SSA
+go test -timeout 20s ./common/yak/ssa/...
+# SSAAPI
+go test -timeout 1m ./common/yak/ssaapi
+go test -timeout 1m ./common/yak/ssaapi/ssareducer
+go test -timeout 1m ./common/yak/ssaapi/test/ssatest
+
 
 echo "Start to Test Syntaxflow rule"
 go test -count=1 -timeout 60s -skip "TestBuildInRule_Verify_DEBUG" ./common/syntaxflow/...
@@ -10,23 +41,19 @@ echo "SSA URL"
 go test -count=1 -timeout 1m -run ^TestSFURl$ github.com/yaklang/yaklang/common/yak/yakurl
 
 echo "Start to Test SSA-Analyze"
+
 # SSA plugin rule/option
-go test -count=1 -timeout 20s ./common/yak/static_analyzer/test/...
+go test -timeout 20s ./common/yak/static_analyzer/test/...
 # StaticAnalyze
-go test -count=1 -timeout 20s -run TestAnalyzeMustPASS* ./common/coreplugin
-# SSA UTIL
-go test -count=1 -timeout 20s ./common/yak/ssa/...
-# yak ssa
-go test -count=1 -timeout 20s ./common/yak/yak2ssa/...
-# Test Java Syntax
-go test -count=1 -timeout 10s ./common/yak/java/...
-# Test php syntax
-go test -count=1 -timeout 30s ./common/yak/php/...
-# Test Js Syntax
-go test -count=1 -timeout 60s ./common/yak/JS2ssa/...
-# Test Go syntax
-go test -count=1 -timeout 20s ./common/yak/go2ssa/...
+go test -timeout 20s -run TestAnalyzeMustPASS* ./common/coreplugin
+# BuildIn SyntaxFlow Rule
+go test -timeout 2m ./common/syntaxflow/sfbuildin/...
+go test -timeout 1m ./common/yak/ssaapi/test/syntaxflow
 
 echo "syntaxflow and language grpc"
 go test -count=1 -timeout 5m -run TestGRPCMUSTPASS_SyntaxFlow* ./common/yakgrpc/...
 go test -count=1 -timeout 5m -run TestGRPCMUSTPASS_LANGUAGE* ./common/yakgrpc/...
+
+
+echo "Start to Test SFWEB"
+go test -timeout 1m ./common/sfweb/...
