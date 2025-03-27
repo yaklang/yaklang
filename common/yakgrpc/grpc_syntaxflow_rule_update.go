e4c470816e22cb5b579f1fd00bacebbb1cfc7ab9
intSheep <intSheep@qq.com>
2025-03-27 11:02:41 +0800

    rename grpc interface

--- common/yakgrpc/grpc_syntaxflow_rule_update.go
+++ common/yakgrpc/grpc_syntaxflow_rule_update.go
@@ -8,25 +8,25 @@ import (
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
-func (s *Server) QuerySFBuildInRuleUpdate(ctx context.Context, req *ypb.QuerySFBuildInRuleUpdateRequest) (*ypb.QuerySFBuildInRuleUpdateResponse, error) {
+func (s *Server) CheckSyntaxFlowRuleUpdate(ctx context.Context, req *ypb.CheckSyntaxFlowRuleUpdateRequest) (*ypb.CheckSyntaxFlowRuleUpdateResponse, error) {
 	needUpdate := yakit.Get(consts.EmbedSfBuildInRuleKey) != consts.ExistedSyntaxFlowEmbedFSHash
 	if !needUpdate {
-		return &ypb.QuerySFBuildInRuleUpdateResponse{NeedUpdate: needUpdate}, nil
+		return &ypb.CheckSyntaxFlowRuleUpdateResponse{NeedUpdate: false}, nil
 	}
 	rules := yakit.QueryBuildInRule(s.GetProfileDatabase())
-	var state ypb.UpdateSFRuleState
+	var state ypb.UpdateSyntaxFlowRuleState
 	if len(rules) == 0 {
-		state = ypb.UpdateSFRuleState_Rule_Empty
+		state = ypb.UpdateSyntaxFlowRuleState_Rule_Empty
 	} else {
-		state = ypb.UpdateSFRuleState_Rule_To_Update
+		state = ypb.UpdateSyntaxFlowRuleState_Rule_To_Update
 	}
-	return &ypb.QuerySFBuildInRuleUpdateResponse{NeedUpdate: needUpdate, State: state}, nil
+	return &ypb.CheckSyntaxFlowRuleUpdateResponse{NeedUpdate: true, State: state}, nil
 }
 
-func (s *Server) UpdateSFBuildInRule(req *ypb.UpdateSFBuildInRuleRequest, stream ypb.Yak_UpdateSFBuildInRuleServer) error {
+func (s *Server) ApplySyntaxFlowRuleUpdate(req *ypb.ApplySyntaxFlowRuleUpdateRequest, stream ypb.Yak_ApplySyntaxFlowRuleUpdateServer) error {
 	defer yakit.Set(consts.EmbedSfBuildInRuleKey, consts.ExistedSyntaxFlowEmbedFSHash)
 	notify := func(process float64, msg string) {
-		stream.Send(&ypb.UpdateSFBuildInRuleResponse{
+		stream.Send(&ypb.ApplySyntaxFlowRuleUpdateResponse{
 			Percent: process,
 			Message: msg,
 		})
