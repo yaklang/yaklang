7c087c4243fa179acac8af4a2b7034a41a65c9b6
YWHKKX <2076064543@qq.com>
2025-04-10 23:37:25 +0800

    fix(ssaapi): fix BFS graph and fix buildedge api

--- common/yak/ssaapi/values_db_test.go
+++ common/yak/ssaapi/values_db_test.go
@@ -48,7 +48,6 @@ func TestValuesDB_Save_Audit_Node(t *testing.T) {
 	})
 
 	t.Run("test recursiveSaveValue ", func(t *testing.T) {
-		// TODO: save value with dataflow path
 		progName := uuid.NewString()
 		fmt.Println(progName)
 		prog, err := ssaapi.Parse(``, ssaapi.WithProgramName(progName))
@@ -56,25 +55,18 @@ func TestValuesDB_Save_Audit_Node(t *testing.T) {
 		/*
 			1->2->3
 			1->3->4
+			1->red->3
 		*/
 		value1 := CreateValue(prog, 1)
 		value2 := CreateValue(prog, 2)
-		value3_1 := CreateValue(prog, 3)
-		value3_2 := CreateValue(prog, 3)
+		value3 := CreateValue(prog, 3)
 		value4 := CreateValue(prog, 4)
 		value1.AppendDependOn(value2)
-		value2.AppendDependOn(value3_1)
-		value1.AppendDependOn(value3_2)
-		value3_2.AppendDependOn(value4)
+		value2.AppendDependOn(value3)
+		value1.AppendDependOn(value3)
+		value3.AppendDependOn(value4)
 
-		value3_1.Predecessors = []*ssaapi.PredecessorValue{{
-			Node: value1,
-			Info: &sfvm.AnalysisContext{
-				Step:  -1,
-				Label: "dataflow_topdef",
-			},
-		}}
-		value4.Predecessors = []*ssaapi.PredecessorValue{{
+		value3.Predecessors = []*ssaapi.PredecessorValue{{
 			Node: value1,
 			Info: &sfvm.AnalysisContext{
 				Step:  -1,
@@ -82,7 +74,7 @@ func TestValuesDB_Save_Audit_Node(t *testing.T) {
 			},
 		}}
 
-		values := []*ssaapi.Value{value1, value2, value3_1, value3_2, value4}
+		values := []*ssaapi.Value{value1, value2, value3, value4}
 
 		for _, v := range values {
 			err := ssaapi.SaveValue(v,
@@ -121,6 +113,7 @@ func TestValuesDB_Save_Audit_Node(t *testing.T) {
 		}
 
 		{
+			node1 := getNodeByIrCodeId(1)
 			node2 := getNodeByIrCodeId(2)
 			node4 := getNodeByIrCodeId(4)
 			node3 := getNodeByIrCodeId(3)
@@ -130,27 +123,27 @@ func TestValuesDB_Save_Audit_Node(t *testing.T) {
 				edge3b []uint
 			)
 
-			var edge3_4 []*ssadb.AuditEdge
-			db.Model(&ssadb.AuditEdge{}).Where("program_name = ? AND from_node IN (?) AND to_node IN (?) ", progName, node3, node4).Find(&edge3_4)
-			require.Equal(t, 5, len(edge3_4))
-			for _, e := range edge3_4 {
-				edge3a = append(edge3b, e.FromNode)
-				fmt.Printf("edge3_4: fromNode:%v,toNode:%v,edgeType:%v\n", e.FromNode, e.ToNode, e.EdgeType)
+			var edge3_1 []*ssadb.AuditEdge
+			db.Model(&ssadb.AuditEdge{}).Where("program_name = ? AND from_node IN (?) AND to_node in (?) ", progName, node2, node1).Find(&edge3_1)
+			require.Equal(t, 1, len(edge3_1))
+			for _, e := range edge3_1 {
+				edge3a = append(edge3a, e.FromNode)
+				fmt.Printf("edge3_1: fromNode:%v,toNode:%v,edgeType:%v\n", e.FromNode, e.ToNode, e.EdgeType)
 			}
 
 			var edge3_2 []*ssadb.AuditEdge
 			db.Model(&ssadb.AuditEdge{}).Where("program_name = ? AND from_node IN (?) AND to_node in (?) ", progName, node2, node3).Find(&edge3_2)
-			require.Equal(t, 5, len(edge3_2))
+			require.Equal(t, 1, len(edge3_2))
 			for _, e := range edge3_2 {
 				edge3b = append(edge3b, e.FromNode)
 				fmt.Printf("edge3_2: fromNode:%v,toNode:%v,edgeType:%v\n", e.FromNode, e.ToNode, e.EdgeType)
 			}
 
-			for _, i := range edge3a {
-				for _, j := range edge3b {
-					require.NotEqual(t, i, j)
-				}
-			}
+			var edge3_4 []*ssadb.AuditEdge
+			db = db.Debug()
+			db.Model(&ssadb.AuditEdge{}).Where("program_name = ? AND from_node IN (?) AND to_node IN (?) ", progName, node3, node4).Find(&edge3_4)
+			// node3 -> node4 位于范围外，不会构建边
+			require.Equal(t, 0, len(edge3_4))
 		}
 	})
 }
