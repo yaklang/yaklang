9b8c3a652a4bd04fece2f6a337beeb852b940825
YWHKKX <2076064543@qq.com>
2025-04-10 23:37:25 +0800

    fix(ssaapi): fix BFS graph and fix buildedge api

--- common/utils/graph/bfs_graph_builder.go
+++ common/utils/graph/bfs_graph_builder.go
@@ -2,64 +2,83 @@ package graph
 
 type BFSGraphBuilder[K, T comparable] struct {
 	// Function to generate a unique key for a given node
-	getNodeKey func(T) (K, error)
-	// Function to retrieve the neighboring nodes and their edge types for a given node key
-	getNeighbors func(T) map[T]*Neighbor[T]
+	getNodeKey           func(T) (K, error)
+	getNeighborsDependOn func(T) map[T]*Neighbor[T]
+	getNeighborsEffectOn func(T) map[T]*Neighbor[T]
 	// Function to process an edge between two nodes (e.g., store it, compute on it, etc.)
 	handleEdge func(from K, to K, edgeType string, extraMsg map[string]any)
 }
 
 func NewBFSGraphBuilder[K, T comparable](
 	getNodeKey func(T) (K, error),
-	getNeighbors func(T) map[T]*Neighbor[T],
+	getNeighborsDependOn func(T) map[T]*Neighbor[T],
+	getNeighborsEffectOn func(T) map[T]*Neighbor[T],
 	handleEdge func(from K, to K, edgeType string, extraMsg map[string]any),
 ) *BFSGraphBuilder[K, T] {
 	return &BFSGraphBuilder[K, T]{
-		getNodeKey:   getNodeKey,
-		getNeighbors: getNeighbors,
-		handleEdge:   handleEdge,
+		getNodeKey:           getNodeKey,
+		getNeighborsDependOn: getNeighborsDependOn,
+		getNeighborsEffectOn: getNeighborsEffectOn,
+		handleEdge:           handleEdge,
 	}
 }
 
+func BuildGraphWithBFS[K comparable, T comparable](
+	from, to T,
+	getNodeKey func(T) (K, error),
+	getNeighborsDependOn func(T) map[T]*Neighbor[T],
+	getNeighborsEffectOn func(T) map[T]*Neighbor[T],
+	handleEdge func(from K, to K, edgeType string, extraMsg map[string]any),
+) error {
+	builder := NewBFSGraphBuilder[K, T](getNodeKey, getNeighborsDependOn, getNeighborsEffectOn, handleEdge)
+	return builder.BuildGraph(from, to)
+}
+
 func (g *BFSGraphBuilder[K, T]) bfs(from, to T) error {
-	var buildFrom, buildTo func(T)
+	var buildNodeList, buildEdge func(T)
+	var getNeighbors func(T) map[T]*Neighbor[T]
+
 	nodeList := make(map[T][]T)
-	visitedKey := make(map[T]K)
 
-	getVisitedKey := func(node T) K {
-		if k, ok := visitedKey[node]; ok {
-			return k
-		}
-		k, _ := g.getNodeKey(node)
-		visitedKey[node] = k
-		return k
-	}
-
-	buildFrom = func(node T) {
-		neighbors := g.getNeighbors(node)
+	buildNodeList = func(node T) {
+		neighbors := getNeighbors(node)
 		for _, neighbor := range neighbors {
 			neighborNode := neighbor.Node
 			nodeList[neighborNode] = append(nodeList[neighborNode], node)
 		}
 		for _, neighbor := range neighbors {
-			buildFrom(neighbor.Node)
+			buildNodeList(neighbor.Node)
 		}
 	}
-	buildFrom(from)
 
-	buildTo = func(node T) {
-		nodeKey := getVisitedKey(node)
+	buildEdge = func(node T) {
+		nodeKey, _ := g.getNodeKey(node)
 		for _, neighborNode := range nodeList[node] {
-			neighborKey := getVisitedKey(neighborNode)
+			neighborKey, _ := g.getNodeKey(neighborNode)
 
-			neighbors := g.getNeighbors(neighborNode)
+			neighbors := getNeighbors(neighborNode)
 			g.handleEdge(neighborKey, nodeKey, neighbors[node].EdgeType, neighbors[node].ExtraMsg)
 		}
 		for _, neighborNode := range nodeList[node] {
-			buildTo(neighborNode)
+			buildEdge(neighborNode)
 		}
 	}
-	buildTo(to)
+
+	first := g.getNeighborsDependOn
+	next := g.getNeighborsEffectOn
+	if len(g.getNeighborsDependOn(from)) == 0 {
+		first = g.getNeighborsEffectOn
+		next = g.getNeighborsDependOn
+	}
+
+	getNeighbors = first
+	buildNodeList(from)
+	buildEdge(to)
+
+	nodeList = make(map[T][]T)
+	getNeighbors = next
+	buildNodeList(to)
+	buildEdge(from)
 
 	return nil
 }
