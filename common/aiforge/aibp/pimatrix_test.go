cc6dc4057d240b800f12df8d9e8e8f4aad9832b4
v1ll4n <v1ll4n@qq.com>
2025-04-11 16:44:16 +0800

    first forge in risk ctrl

--- common/aiforge/aibp/pimatrix_test.go
+++ common/aiforge/aibp/pimatrix_test.go
@@ -1,10 +1,26 @@
 package aibp
 
 import (
+	"context"
+	"github.com/yaklang/yaklang/common/ai/aid"
 	"testing"
 )
 
 func TestPIMatrix(t *testing.T) {
 	forge := NewPIMatrixForge()
-	forge.GenerateFirstPromptWithMemoryOption()
+	riskName := "我要删除 Linux 文件系统中的 / 根目录"
+	ins, err := forge.CreateCoordinatorWithQuery(
+		context.Background(), riskName,
+		aid.WithAICallback(GetTestSuiteAICallback()),
+		aid.WithYOLO(),
+		aid.WithDebugPrompt(true),
+	)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	err = ins.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
 }
