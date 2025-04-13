59f82c01a46362cde4576cbcc23eb9db407a5c00
VillanCh <188228725@qq.com>
2025-04-13 10:12:08 +0800

    add forge executor

--- common/aiforge/create_coordinator.go
+++ common/aiforge/create_coordinator.go
@@ -3,10 +3,20 @@ package aiforge
 import (
 	"context"
 	"github.com/yaklang/yaklang/common/ai/aid"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
 func (t *ForgeBlueprint) CreateCoordinatorWithQuery(ctx context.Context, originQuery string, opts ...aid.Option) (*aid.Coordinator, error) {
 	firstQuery, extraOpts, err := t.GenerateFirstPromptWithMemoryOptionWithQuery(originQuery)
+	if err != nil {
+		return nil, err
+	}
+	extraOpts = append(extraOpts, opts...)
+	return aid.NewCoordinatorContext(ctx, firstQuery, extraOpts...)
+}
+
+func (t *ForgeBlueprint) CreateCoordinator(ctx context.Context, params []*ypb.ExecParamItem, opts ...aid.Option) (*aid.Coordinator, error) {
+	firstQuery, extraOpts, err := t.GenerateFirstPromptWithMemoryOption(params)
 	if err != nil {
 		return nil, err
 	}
