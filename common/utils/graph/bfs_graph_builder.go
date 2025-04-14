ef32c598b1edac455356a1a3911ec9552b37467b
YWHKKX <2076064543@qq.com>
2025-04-14 13:44:39 +0800

    fix(ssaapi): add cache for bfs

--- common/utils/graph/bfs_graph_builder.go
+++ common/utils/graph/bfs_graph_builder.go
@@ -7,6 +7,8 @@ type BFSGraphBuilder[K, T comparable] struct {
 	getNeighborsEffectOn func(T) map[T]*Neighbor[T]
 	// Function to process an edge between two nodes (e.g., store it, compute on it, etc.)
 	handleEdge func(from K, to K, edgeType string, extraMsg map[string]any)
+
+	visited *map[T]map[T]bool
 }
 
 func NewBFSGraphBuilder[K, T comparable](
@@ -14,12 +16,14 @@ func NewBFSGraphBuilder[K, T comparable](
 	getNeighborsDependOn func(T) map[T]*Neighbor[T],
 	getNeighborsEffectOn func(T) map[T]*Neighbor[T],
 	handleEdge func(from K, to K, edgeType string, extraMsg map[string]any),
+	visited *map[T]map[T]bool,
 ) *BFSGraphBuilder[K, T] {
 	return &BFSGraphBuilder[K, T]{
 		getNodeKey:           getNodeKey,
 		getNeighborsDependOn: getNeighborsDependOn,
 		getNeighborsEffectOn: getNeighborsEffectOn,
 		handleEdge:           handleEdge,
+		visited:              visited,
 	}
 }
 
@@ -29,22 +33,34 @@ func BuildGraphWithBFS[K comparable, T comparable](
 	getNeighborsDependOn func(T) map[T]*Neighbor[T],
 	getNeighborsEffectOn func(T) map[T]*Neighbor[T],
 	handleEdge func(from K, to K, edgeType string, extraMsg map[string]any),
+	visited *map[T]map[T]bool,
 ) error {
-	builder := NewBFSGraphBuilder[K, T](getNodeKey, getNeighborsDependOn, getNeighborsEffectOn, handleEdge)
+	builder := NewBFSGraphBuilder[K, T](getNodeKey, getNeighborsDependOn, getNeighborsEffectOn, handleEdge, visited)
 	return builder.BuildGraph(from, to)
+}
+
+func (g *BFSGraphBuilder[K, T]) ResetVisited() {
+	g.visited = &map[T]map[T]bool{}
 }
 
 func (g *BFSGraphBuilder[K, T]) bfs(from, to T) error {
 	var buildNodeList, buildEdge func(T)
 	var getNeighbors func(T) map[T]*Neighbor[T]
-	var visited map[T]bool
+	var visitedNode map[T]bool
+	var visitedEdge *map[T]map[T]bool
 	var nodeList map[T][]T
 
+	if g.visited != nil {
+		visitedEdge = g.visited
+	} else {
+		visitedEdge = &map[T]map[T]bool{}
+	}
+
 	buildNodeList = func(node T) {
-		if visited[node] {
+		if visitedNode[node] {
 			return
 		}
-		visited[node] = true
+		visitedNode[node] = true
 		neighbors := getNeighbors(node)
 		for _, neighbor := range neighbors {
 			neighborNode := neighbor.Node
@@ -58,12 +74,19 @@ func (g *BFSGraphBuilder[K, T]) bfs(from, to T) error {
 	buildEdge = func(node T) {
 		nodeKey, _ := g.getNodeKey(node)
 		for _, neighborNode := range nodeList[node] {
-			neighborKey, _ := g.getNodeKey(neighborNode)
+			if (*visitedEdge)[node][neighborNode] {
+				return
+			}
+			if m, ok := (*visitedEdge)[node]; ok {
+				m[neighborNode] = true
+			} else {
+				(*visitedEdge)[node] = map[T]bool{neighborNode: true}
+			}
 
+			neighborKey, _ := g.getNodeKey(neighborNode)
 			neighbors := getNeighbors(neighborNode)
 			g.handleEdge(neighborKey, nodeKey, neighbors[node].EdgeType, neighbors[node].ExtraMsg)
-		}
-		for _, neighborNode := range nodeList[node] {
+
 			buildEdge(neighborNode)
 		}
 	}
@@ -76,13 +99,13 @@ func (g *BFSGraphBuilder[K, T]) bfs(from, to T) error {
 	}
 
 	nodeList = make(map[T][]T)
-	visited = make(map[T]bool)
+	visitedNode = make(map[T]bool)
 	getNeighbors = first
 	buildNodeList(from)
 	buildEdge(to)
 
 	nodeList = make(map[T][]T)
-	visited = make(map[T]bool)
+	visitedNode = make(map[T]bool)
 	getNeighbors = next
 	buildNodeList(to)
 	buildEdge(from)
