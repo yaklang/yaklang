557efa59dc4a5ad3c3ea09133dcb40dfea12608c
VillanCh <188228725@qq.com>
2025-04-20 17:01:03 +0800

    improve task use

---
+++ common/aiforge/aibp/tests/decode_test.go
@@ -0,0 +1,41 @@
+package tests
+
+import (
+	"context"
+	"github.com/yaklang/yaklang/common/ai/aid"
+	"github.com/yaklang/yaklang/common/ai/aid/aitool/buildinaitools/yakscripttools"
+	"github.com/yaklang/yaklang/common/aiforge"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"testing"
+)
+
+func init() {
+	aiforge.RegisterForgeExecutor("codec-test-1", func(ctx context.Context, items []*ypb.ExecParamItem, option ...aid.Option) (*aiforge.ForgeResult, error) {
+		od, err := aiforge.NewForgeBlueprint(
+			"codec-test1",
+			aiforge.WithInitializePrompt(`你正在帮助用户处理一些文本任务，任务来源可能是，CTF这类黑客竞赛，一般会涉及到一些编码解码工作，请你考虑清楚问题回答
+
+ {{ .Forge.UserParams }}
+`),
+			aiforge.WithPersistentPrompt("处理任务的时候，注意深度，使用工具如果失败，无法达到你的目的，需要及时调整工具参数。"),
+			aiforge.WithTools(yakscripttools.GetYakScriptAiTools("encode", "decode")...),
+		).CreateCoordinator(ctx, items, option...)
+		if err != nil {
+			return nil, err
+		}
+		return nil, od.Run()
+	})
+}
+
+func TestCodecTest1(t *testing.T) {
+	aiforge.ExecuteForge(
+		"codec-test-1",
+		context.Background(),
+		[]*ypb.ExecParamItem{
+			{Key: "query", Value: "帮我解答一个CTF题目，题目类型是加解密，附件内容是：RzRaVE1PQldHVTNFR05TREdaQ0RNTUpYR1EzREtOWlRHNUJETU1SVEdRWkRJTVpUR0kyREtSUlRHRVpUTU5LR0dNWlRHTVJWSVlaVE1NWlVHNUNBPT09PQ=="},
+		},
+		aid.WithYOLO(),
+		aid.WithAICallback(aiforge.GetOpenRouterAICallback()),
+		aid.WithDebugPrompt(true),
+	)
+}
