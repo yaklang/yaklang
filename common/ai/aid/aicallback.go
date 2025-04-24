80fc2961e37e92e8c89f508c863abc36819f3b87
v1ll4n <v1ll4n@qq.com>
2025-04-24 17:47:02 +0800

    add auto decode

--- common/ai/aid/aicallback.go
+++ common/ai/aid/aicallback.go
@@ -2,14 +2,13 @@ package aid
 
 import (
 	"context"
-	"io"
-	"os"
-	"strings"
-
 	"github.com/yaklang/yaklang/common/ai/aispec"
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/chanx"
+	"io"
+	"os"
+	"strings"
 )
 
 type AIRequest struct {
