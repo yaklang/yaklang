97d669b782bbdbbbcf2a64adda1e475c816b54f5
intSheep <intSheep@qq.com>
2025-04-25 14:34:17 +0800

    feat(sf):add ruleId for rule

--- common/syntaxflow/sfbuildin/rules_test.go
+++ common/syntaxflow/sfbuildin/rules_test.go
@@ -9,8 +9,9 @@ import (
 	"testing"
 )
 
-func Test_BuildIn_Rule_Ast(t *testing.T) {
+func Test_BuildIn_Rule_Ast_And_Rule_Id(t *testing.T) {
 	fsInstance := filesys.NewEmbedFS(ruleFS)
+	ruleIdMap := make(map[string]bool)
 	filesys.Recursive(".", filesys.WithFileSystem(fsInstance), filesys.WithFileStat(func(s string, info fs.FileInfo) error {
 		_, name := fsInstance.PathSplit(s)
 		if !strings.HasSuffix(name, ".sf") {
@@ -21,8 +22,34 @@ func Test_BuildIn_Rule_Ast(t *testing.T) {
 			require.NoError(t, err)
 
 			content := string(raw)
-			_, err = sfdb.CheckSyntaxFlowRuleContent(content)
+			rule, err := sfdb.CheckSyntaxFlowRuleContent(content)
 			require.NoError(t, err)
+			// check empty rule id
+			if rule.GetInfo().Title != "" && rule.RuleId == "" {
+				t.Fatalf("rule id should not be empty,rule name:%s", rule.RuleName)
+			}
+			// check deduplication of rule id
+			if rule.RuleId == "" {
+				return
+			}
+			ruleId := rule.RuleId
+			if !ruleIdMap[ruleId] {
+				ruleIdMap[ruleId] = true
+			} else {
+				t.Fatalf("The rule ID should not be duplicated:%s", ruleId)
+			}
+
+			// check id length
+			require.Equal(t, 8, len(ruleId))
+			// check language
+			switch strings.Split(name, "-")[0] {
+			case "golang":
+				require.Equal(t, "1", string(ruleId[0]))
+			case "java":
+				require.Equal(t, "2", string(ruleId[0]))
+			case "php":
+				require.Equal(t, "3", string(ruleId[0]))
+			}
 		})
 		return nil
 	}))
