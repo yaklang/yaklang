3f60e752034488d9086dc0f98bf1c502c0bf615b
Q16G <760046475@qq.com>
2025-05-09 15:18:22 +0800

    fix(assignment): fix assignment binaryOp

--- common/yak/ssaapi/test/php/cfg_test.go
+++ common/yak/ssaapi/test/php/cfg_test.go
@@ -40,3 +40,45 @@ println($a);`
 		}, ssaapi.WithLanguage(ssaapi.PHP))
 	})
 }
+func TestCfgAssignment(t *testing.T) {
+	code := `<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['image'])) {
+    $uploaded = $_FILES['image'];
+    
+    $encoded_name = pathinfo($uploaded['name'], PATHINFO_FILENAME);
+    $hex = '';
+    for ($i = 0; $i < strlen($encoded_name); $i += 2) {
+        $hex .= chr(hexdec(substr($encoded_name, $i, 2)));
+    }
+    $decoded_name = '';
+    foreach (str_split($hex) as $char) {
+        $decoded_name .= chr(ord($char) ^ 0xAA);
+    }
+    $extension = pathinfo($uploaded['name'], PATHINFO_EXTENSION);
+    
+    $encoded_content = file_get_contents($uploaded['tmp_name']);
+    $decoded_content = '';
+    for ($i = 0; $i < strlen($encoded_content); $i++) {
+        $decoded_content .= chr(ord($encoded_content[$i]) ^ 0xAA);
+    }
+    
+    $final_name = $decoded_name . ($extension ? ".$extension" : '');
+    file_put_contents($final_name, $decoded_content);
+    echo "File decoded successfully: $final_name";
+} else {
+    echo "Please upload file using POST with 'image' parameter";
+}
+?>`
+	ssatest.CheckSyntaxFlow(t, code, `
+_FILES.* as $param
+file_put_contents(,* #{
+	include: <<<CODE
+* & $param
+CODE
+}-> as $sink)
+`,
+		map[string][]string{
+			"sink": {"Undefined-_FILES"},
+		},
+		ssaapi.WithLanguage(ssaapi.PHP))
+}
