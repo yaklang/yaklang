da5063783c6333d969abe04379b445bb24394503
go0p <gp827782797@qq.com>
2025-05-15 11:38:55 +0800

    feat: add capture started callback to pcaputil and fix route

--- common/utils/netutil/route.go
+++ common/utils/netutil/route.go
@@ -2,6 +2,13 @@ package netutil
 
 import (
 	"context"
+	"net"
+	"os/exec"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+
 	"github.com/dlclark/regexp2"
 	"github.com/gopacket/gopacket/routing"
 	"github.com/pkg/errors"
@@ -10,12 +17,6 @@ import (
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/netutil/netroute"
 	"github.com/yaklang/yaklang/common/utils/netutil/routewrapper"
-	"net"
-	"os/exec"
-	"runtime"
-	"strings"
-	"sync"
-	"time"
 )
 
 func FindInterfaceByIP(ip string) (net.Interface, error) {
@@ -145,7 +146,7 @@ func Route(timeout time.Duration, target string) (iface *net.Interface, gateway,
 	}
 
 	ifIns, ip1, ip2, err := nativeRoute()
-	if err == nil && ip1 != nil {
+	if err == nil {
 		return ifIns, ip1, ip2, nil
 	}
 
@@ -273,28 +274,155 @@ func Route(timeout time.Duration, target string) (iface *net.Interface, gateway,
 		return iface, targetGateway, srcIp, err
 	default:
 		var handleRoute = func(rs []routewrapper.Route) (*net.Interface, net.IP, net.IP, error) {
-			for _, route := range rs {
-				var srcIp net.IP
-				if route.Destination.Contains(net.ParseIP(utils.FixForParseIP(target))) {
-					// 获取 IP 地址
-					addrs, err := route.Interface.Addrs()
-					if err != nil {
-						return nil, nil, nil, errors.Errorf("iface: %v cannot get address: %s", iface.Name, err)
-					}
-					for _, addr := range addrs {
-						raw := utils.FixForParseIP(addr.String())
-						srcIpAddress, _, err := net.ParseCIDR(raw)
-						if err != nil {
-							continue
+			// 找到目标IP的最具体的路由
+			targetIP := net.ParseIP(utils.FixForParseIP(target))
+			if targetIP == nil {
+				return nil, nil, nil, utils.Errorf("invalid target IP: %s", target)
+			}
+
+			log.Debugf("Selecting route for target IP: %s from %d routes", targetIP.String(), len(rs))
+			for i, route := range rs {
+				if route.Destination.IP == nil || route.Destination.Mask == nil {
+					continue
+				}
+
+				ones, _ := route.Destination.Mask.Size()
+				gateway := "none"
+				if route.Gateway != nil {
+					gateway = route.Gateway.String()
+				}
+				log.Debugf("Route[%d]: interface=%s, network=%s/%d, gateway=%s",
+					i, route.Interface.Name, route.Destination.IP.String(), ones, gateway)
+			}
+
+			// 跟踪两个最佳匹配的路由：一个是总体最佳匹配的路由，另一个是最佳直接连接的网络
+			// 直连的优先
+			var bestRoute *routewrapper.Route
+			var bestDirectRoute *routewrapper.Route
+			var bestMaskBits int = -1
+			var bestDirectMaskBits int = -1
+
+			// 第一次遍历：找到包含目标IP的路由，优先考虑具体性
+			for i, route := range rs {
+				if route.Destination.IP == nil || route.Destination.Mask == nil {
+					continue
+				}
+
+				// 检查此路由的网络是否包含我们的目标IP
+				if route.Destination.Contains(targetIP) {
+					ones, _ := route.Destination.Mask.Size()
+
+					// 首先检查此路由是否是直接连接的网络
+					isDirect := false
+					for _, iface := range rs {
+						if iface.Interface.Name == route.Interface.Name {
+							interfaceAddrs, _ := iface.Interface.Addrs()
+							for _, addr := range interfaceAddrs {
+								ipNet, ok := addr.(*net.IPNet)
+								if !ok {
+									continue
+								}
+
+								// 如果接口有一个IP在同一子网中
+								if ipNet.IP.Mask(ipNet.Mask).Equal(route.Destination.IP.Mask(route.Destination.Mask)) {
+									isDirect = true
+									break
+								}
+							}
 						}
-						if utils.IsIPv6(srcIpAddress.String()) == utils.IsIPv6(route.Gateway.String()) {
-							srcIp = srcIpAddress
+						if isDirect {
+							break
 						}
 					}
-					return route.Interface, route.Gateway, srcIp, nil
+
+					if isDirect {
+						// 如果这个路由更具体，更新最佳直接路由
+						if ones > bestDirectMaskBits {
+							bestDirectMaskBits = ones
+							bestDirectRoute = &rs[i]
+							log.Debugf("New best direct route: interface=%s, network=%s/%d",
+								route.Interface.Name, route.Destination.IP.String(), ones)
+						}
+					} else {
+						// 如果这个路由更具体，更新最佳网关路由
+						if ones > bestMaskBits {
+							bestMaskBits = ones
+							bestRoute = &rs[i]
+							log.Debugf("New best gateway route: interface=%s, network=%s/%d, gateway=%s",
+								route.Interface.Name, route.Destination.IP.String(), ones, route.Gateway)
+						}
+					}
 				}
 			}
-			return nil, nil, nil, utils.Errorf("handle route failed: %s", err)
+
+			// 优先选择直接连接的网络而不是网关路由
+			var selectedRoute *routewrapper.Route
+			if bestDirectRoute != nil {
+				selectedRoute = bestDirectRoute
+				log.Debugf("Selected direct route: interface=%s, network=%s/%d",
+					selectedRoute.Interface.Name, selectedRoute.Destination.IP.String(), bestDirectMaskBits)
+			} else if bestRoute != nil {
+				selectedRoute = bestRoute
+				log.Debugf("Selected gateway route: interface=%s, network=%s/%d",
+					selectedRoute.Interface.Name, selectedRoute.Destination.IP.String(), bestMaskBits)
+			} else {
+				// 如果找不到匹配的路由，尝试默认路由（0.0.0.0/0）
+				for i, route := range rs {
+					if route.Destination.IP == nil || route.Destination.Mask == nil {
+						continue
+					}
+
+					ones, bits := route.Destination.Mask.Size()
+					if ones == 0 && bits > 0 {
+						selectedRoute = &rs[i]
+						log.Debugf("Selected default route: interface=%s, gateway=%s",
+							selectedRoute.Interface.Name, selectedRoute.Gateway)
+						break
+					}
+				}
+			}
+
+			if selectedRoute == nil {
+				return nil, nil, nil, utils.Errorf("no suitable route found for: %s", target)
+			}
+
+			// 从接口获取源IP
+			var srcIP net.IP
+			addrs, err := selectedRoute.Interface.Addrs()
+			if err != nil {
+				return nil, nil, nil, errors.Errorf("interface %v cannot get addresses: %s",
+					selectedRoute.Interface.Name, err)
+			}
+
+			// 在接口上找到一个合适的源IP
+			for _, addr := range addrs {
+				ipAddr, _, err := net.ParseCIDR(addr.String())
+				if err != nil {
+					continue
+				}
+
+				// 匹配IP版本（IPv4或IPv6）
+				isIPv6Target := utils.IsIPv6(targetIP.String())
+				isIPv6Addr := utils.IsIPv6(ipAddr.String())
+
+				if isIPv6Target == isIPv6Addr {
+					srcIP = ipAddr
+					break
+				}
+			}
+
+			if srcIP == nil && len(addrs) > 0 {
+				// 如果找不到匹配的IP版本，使用第一个地址
+				ipAddr, _, err := net.ParseCIDR(addrs[0].String())
+				if err == nil {
+					srcIP = ipAddr
+				}
+			}
+
+			log.Debugf("Final route selected: interface=%s, gateway=%s, srcIP=%s for target=%s",
+				selectedRoute.Interface.Name, selectedRoute.Gateway, srcIP, target)
+
+			return selectedRoute.Interface, selectedRoute.Gateway, srcIP, nil
 		}
 
 		r, err := routewrapper.NewRouteWrapper()
@@ -308,6 +436,7 @@ func Route(timeout time.Duration, target string) (iface *net.Interface, gateway,
 
 		ifaceIns, gatewayIns, ipLocal, err := handleRoute(routes)
 		if err != nil {
+			log.Infof("No specific route found for %s, trying default routes", target)
 			routes, err = r.DefaultRoutes()
 			if err != nil {
 				return nil, nil, nil, utils.Errorf("get default routes failed: %s", err)
