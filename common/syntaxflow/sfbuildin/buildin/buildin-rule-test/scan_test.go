afc9b89133c2e1449ae8d6f1f0a2e46172110c4f
intSheep <intSheep@qq.com>
2025-05-15 15:29:32 +0800

    feat(sf):add unreleased database source rule

--- common/syntaxflow/sfbuildin/buildin/buildin-rule-test/scan_test.go
+++ common/syntaxflow/sfbuildin/buildin/buildin-rule-test/scan_test.go
@@ -55,7 +55,7 @@ func TestVerify_DEBUG(t *testing.T) {
 	yakit.InitialDatabase()
 	err := sfbuildin.SyncEmbedRule()
 	require.NoError(t, err)
-	ruleName := "检测 SAXParserFactory() 不安全使用"
+	ruleName := "检测Java Hibernate 数据库Session资源未释放"
 
 	rule, err := sfdb.GetRulePure(ruleName)
 	if err != nil {
