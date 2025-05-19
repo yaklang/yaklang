0480f10b1194c9f3fceb0db5ff17da9ff7c94cf7
intSheep <intSheep@qq.com>
2025-05-19 15:10:07 +0800

    feat(sf):file filter support jsonpath

--- common/yak/ssaapi/test/syntaxflow/file_filter_test.go
+++ common/yak/ssaapi/test/syntaxflow/file_filter_test.go
@@ -1,6 +1,7 @@
 package syntaxflow
 
 import (
+	"github.com/stretchr/testify/require"
 	"testing"
 
 	"github.com/yaklang/yaklang/common/consts"
@@ -53,4 +54,64 @@ ${*.properties}.regexp(/(?i).*access[_-]?[token|key].*\s*=\s*((?!\{\{)(?!(?i)^(t
 		)
 	})
 
+}
+
+func TestFileFilterJson(t *testing.T) {
+	t.Run("test simple json path", func(t *testing.T) {
+		vf := filesys.NewVirtualFs()
+		vf.AddFile("test.json", `{
+  "auths": {
+    "https://index.docker.io/v1/": {
+      "auth": "dXNlcm5hbWU6cGFzc3dvcmQxMjM=",
+      "email": "user@example.com"
+    },
+    "https://private-registry.example.com": {
+      "auth": "YWRtaW46c2VjcmV0cGFzc3dvcmQ=",
+      "email": "admin@example.com"
+    }
+  }
+}`)
+		ssatest.CheckWithFS(vf, t, func(programs ssaapi.Programs) error {
+			vals, err := programs.SyntaxFlowWithError(`${*.json}.json("$.auths.*.auth") as $result`)
+			require.NoError(t, err)
+			result := vals.GetValues("result")
+			require.Contains(t, result.String(), "dXNlcm5hbWU6cGFzc3dvcmQxMjM=")
+			require.Contains(t, result.String(), "YWRtaW46c2VjcmV0cGFzc3dvcmQ=")
+
+			require.Contains(t, result.StringEx(1), "4:16 - 4:44")
+			require.Contains(t, result.StringEx(1), "8:16 - 8:44")
+			return nil
+		})
+	})
+
+	t.Run("test match mutli pos", func(t *testing.T) {
+		vf := filesys.NewVirtualFs()
+		vf.AddFile("test.json", `{
+  "auths": {
+    "https://index.docker.io/v1/": {
+      "auth": "dXNlcm5hbWU6cGFzc3dvcmQxMjM=",
+      "email": "admin@example.com"
+    },
+    "https://private-registry.example.com": {
+      "auth": "YWRtaW46c2VjcmV0cGFzc3dvcmQ=",
+      "email": "admin@example.com"
+    },
+	"other":"admin@example.com"
+  }
+}`)
+		ssatest.CheckWithFS(vf, t, func(programs ssaapi.Programs) error {
+			vals, err := programs.SyntaxFlowWithError(`${*.json}.json("$.auths.*.email") as $result`)
+			require.NoError(t, err)
+			result := vals.GetValues("result")
+			result.ShowWithSource()
+
+			require.Contains(t, result.String(), "admin@example.com")
+			require.Contains(t, result.StringEx(1), "5:17 - 5:34")
+			require.Contains(t, result.StringEx(1), "9:17 - 9:34")
+			// TODO:现在jsonPath的位置使用strings.Index，会导致也匹配到"other":"admin@example.com"也会分析到
+			// 如果需要更准确的位置，可能需要在jsonpath的Parser加入位置信息
+			//require.NotContains(t, result.StringEx(1), "11:11 - 11:28")
+			return nil
+		})
+	})
 }
