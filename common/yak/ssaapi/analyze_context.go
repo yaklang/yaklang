23a00cab8e2fa40304969a2451ad87d542be6f17
intSheep <intSheep@qq.com>
2025-06-06 16:52:53 +0800

    fix(ssaapi):Data flow analysis algorithms can be stopped through context

--- common/yak/ssaapi/analyze_context.go
+++ common/yak/ssaapi/analyze_context.go
@@ -1,6 +1,7 @@
 package ssaapi
 
 import (
+	"context"
 	"sync/atomic"
 
 	"github.com/yaklang/yaklang/common/log"
@@ -107,8 +108,24 @@ func (a *AnalyzeContext) check(v *Value) (needExit bool, recoverStack func()) {
 		a.reachedDepthLimited = true
 		return
 	}
+
+	ctx := a.getContext()
+	select {
+	case <-ctx.Done():
+		log.Warnf("context is done, stop it")
+		return true, recoverStack
+	default:
+	}
+
 	needExit = false
 	return
+}
+
+func (a *AnalyzeContext) getContext() context.Context {
+	if a.config != nil && a.config.ctx != nil {
+		return a.config.ctx
+	}
+	return context.Background()
 }
 
 // needCrossProcess If the SSA-ID of the function from-value ·and to-value is different,
