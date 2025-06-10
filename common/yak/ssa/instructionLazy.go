1bae083b0a0358807d99db65b62299756221d666
wlz <lingze.wl@gmail.com>
2025-06-10 11:20:59 +0800

    fix(ssa):use GetInstruction not NewInstructionLazy

--- common/yak/ssa/instructionLazy.go
+++ common/yak/ssa/instructionLazy.go
@@ -151,6 +151,11 @@ func (lz *LazyInstruction) check() {
 	if utils.IsNil(lz.Instruction) {
 		inst := CreateInstruction(Opcode(lz.GetOpcode()))
 		if inst == nil {
+			ir := ssadb.GetIrCodeById(ssadb.GetDB(), lz.id)
+			if ir != nil && ir.Opcode != 0 {
+				log.Infof("bb")
+			}
+
 			log.Infof("unknown opcode: %d: %s", lz.GetOpcode(), lz.ir.OpcodeName)
 			return
 		}
@@ -227,11 +232,11 @@ func (lz *LazyInstruction) GetOpcode() Opcode {
 }
 
 func (lz *LazyInstruction) String() string {
-	if utils.IsNil(lz.ir) {
+	lz.check()
+	if utils.IsNil(lz.Instruction) {
 		log.Errorf("BUG: lazyInstruction IrCode is nil")
 		return ""
 	}
-	lz.check()
 	return lz.Instruction.String()
 	// return "lz:" + lz.ir.String
 }
