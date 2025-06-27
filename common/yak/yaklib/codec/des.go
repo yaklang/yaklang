6f86660b5f9c61f60893d026af98016b5d443a38
Rookie <rookie_is@qq.com>
2025-06-27 11:47:11 +0800

    feat(codec):add some codec func doc

--- common/yak/yaklib/codec/des.go
+++ common/yak/yaklib/codec/des.go
@@ -165,38 +165,128 @@ import (
 //	return DESCBCDecEx(key, data, iv, true)
 //}
 
+// DESECBEncrypt 是一个便捷函数，用于使用 DES 算法，在 ECB 模式下，使用 零填充来加密数据。
+// 它接受一个密钥（key）、需要加密的数据（data to encrypt）（ecb 模式下 iv 无用）
+// 密钥的长度必须是 8 个字节。
+// example:
+// ```
+// codec.DESECBEncrypt([]byte("12345678"), "hello world")
+// ```
 func DESECBEnc(key []byte, data []byte) ([]byte, error) {
 	return DESEncryptECBWithZeroPadding(key, data, nil)
 }
 
+// TripleDESECBEncrypt 是一个便捷函数，用于使用 Triple DES 算法，在 ECB 模式下，使用 零填充来加密数据。
+// 它接受一个密钥（key）、需要加密的数据（data to encrypt）（ecb 模式下 iv 无用）
+// 密钥的长度必须是 24 个字节（即 3 * 8 字节）。
+// example:
+// ```
+// codec.TripleDESECBEncrypt([]byte("123456789012345678901234"), "hello world")
+// ```
 func TripleDES_ECBEnc(key []byte, data []byte) ([]byte, error) {
 	return TripleDESEncryptECBWithZeroPadding(key, data, nil)
 }
 
+// DESECBDecrypt 是一个便捷函数，用于使用 DES 算法，在 ECB 模式下，使用 零填充来解密数据。
+// 它接受一个密钥（key）、需要解密的数据（data to decrypt）（ecb 模式下 iv 无用）
+// 密钥的长度必须是 8 个字节。
+// example:
+// ```
+// codec.DESECBDecrypt([]byte("12345678"), ciphertext)
+// ```
 func DESECBDec(key []byte, data []byte) ([]byte, error) {
 	return DESDecryptECBWithZeroPadding(key, data, nil)
 }
 
+// TripleDESECBDecrypt 是一个便捷函数，用于使用 Triple DES 算法，在 ECB 模式下，使用 零填充来解密数据。
+// 它接受一个密钥（key）、需要解密的数据（data to decrypt）（ecb 模式下 iv 无用）
+// 密钥的长度必须是 24 个字节（即 3 * 8 字节）。
+// example:
+// ```
+// codec.TripleDESECBDecrypt([]byte("123456789012345678901234"), ciphertext)
+// ```
 func TripleDES_ECBDec(key []byte, data []byte) ([]byte, error) {
 	return TripleDESDecryptECBWithZeroPadding(key, data, nil)
 }
 
 // Des
 var DESEncryptCBCWithPKCSPadding = DESEncFactory(PKCS5Padding, CBC)
-var DESEncryptCBCWithZeroPadding = DESEncFactory(ZeroPadding, CBC)
+
+// DESCBCEncrypt 是一个便捷函数，用于使用 DES 算法，在 CBC 模式下，使用零填充来加密数据。
+// 它接受一个密钥（key）、需要加密的数据（data to encrypt）和一个初始化向量（iv）。
+// 密钥的长度必须是 8 个字节，并且 iv 可以是 nil 或者 8 个字节长。
+// 如果 iv 为 nil，它将被固定为密钥，或者用零填充到 8 个字节。
+// DESCBCEncrypt DESEncrypt 是同一个函数。
+// example:
+// ```
+// codec.DESCBCEncrypt([]byte("12345678"), "hello world", "12345678")
+// ```
+func DESEncryptCBCWithZeroPadding(key []byte, i interface{}, iv []byte) ([]byte, error) {
+	return DESEncFactory(ZeroPadding, CBC)(key, i, iv)
+}
+
 var DESDecryptCBCWithPKCSPadding = DESDecFactory(PKCS5UnPadding, CBC)
-var DESDecryptCBCWithZeroPadding = DESDecFactory(ZeroUnPadding, CBC)
+
+// DESCBCDecrypt 是一个便捷函数，用于使用 DES 算法，在 CBC 模式下，使用零填充来解密数据。
+// 它接受一个密钥（key）、需要解密的数据（data to decrypt）和一个初始化向量（iv）。
+// 密钥的长度必须是 8 个字节，并且 iv 可以是 nil 或者 8 个字节长。
+// 如果 iv 为 nil，它将被固定为密钥，或者用零填充到 8 个字节。
+// DESCBCDecrypt DESDecrypt 是同一个函数。
+// example:
+// ```
+// codec.DESCBCEncrypt([]byte("12345678"), ciphertext, "12345678")
+// ```
+func DESDecryptCBCWithZeroPadding(key []byte, i interface{}, iv []byte) ([]byte, error) {
+	return DESDecFactory(ZeroUnPadding, CBC)(key, i, iv)
+}
 
 var DESEncryptECBWithPKCSPadding = DESEncFactory(PKCS5Padding, ECB)
-var DESEncryptECBWithZeroPadding = DESEncFactory(ZeroPadding, ECB)
+
+// DESECBEncrypt 是一个便捷函数，用于使用 DES 算法，在 ECB 模式下，使用 零填充来加密数据。
+// 它接受一个密钥（key）、需要加密的数据（data to encrypt）。
+// ecb 模式下 iv 无用。
+// 密钥的长度必须是 8 个字节。
+// example:
+// ```
+// codec.DESECBEncrypt([]byte("12345678"), "hello world", nil)
+// ```
+func DESEncryptECBWithZeroPadding(key []byte, i interface{}, iv []byte) ([]byte, error) {
+	return DESEncFactory(ZeroPadding, ECB)(key, i, iv)
+}
+
 var DESDecryptECBWithPKCSPadding = DESDecFactory(PKCS5UnPadding, ECB)
 var DESDecryptECBWithZeroPadding = DESDecFactory(ZeroUnPadding, ECB)
 
 // TripleDes
 var TripleDESEncryptCBCWithPKCSPadding = TripleDESEncFactory(PKCS5Padding, CBC)
-var TripleDESEncryptCBCWithZeroPadding = TripleDESEncFactory(ZeroPadding, CBC)
+
+// TripleDESCBCEncrypt 是一个便捷函数，用于使用 Triple DES 算法，在 CBC 模式下，使用 零填充来加密数据。
+// 它接受一个密钥（key）、需要加密的数据（data to encrypt）和一个初始化向量（iv）。
+// 密钥的长度必须是 24 个字节（即 3 * 8 字节），并且 iv 可以是 nil 或者 8 个字节长。
+// 如果 iv 为 nil，它将被固定为密钥.
+// TripleDESCBCDecrypt TripleDESEncrypt 是同一个函数。
+// example:
+// ```
+// codec.TripleDESCBCEncrypt([]byte("123456789012345678901234"), "hello world", "12345678")
+// ```
+func TripleDESEncryptCBCWithZeroPadding(key []byte, i interface{}, iv []byte) ([]byte, error) {
+	return TripleDESEncFactory(ZeroPadding, CBC)(key, i, iv)
+}
+
 var TripleDESDecryptCBCWithPKCSPadding = TripleDESDecFactory(PKCS5UnPadding, CBC)
-var TripleDESDecryptCBCWithZeroPadding = TripleDESDecFactory(ZeroUnPadding, CBC)
+
+// TripleDESCBCDecrypt 是一个便捷函数，用于使用 Triple DES 算法，在 CBC 模式下，使用 零填充来解密数据。
+// 它接受一个密钥（key）、需要解密的数据（data to decrypt）和一个初始化向量（iv）。
+// 密钥的长度必须是 24 个字节（即 3 * 8 字节），并且 iv 可以是 nil 或者 8 个字节长。
+// 如果 iv 为 nil，它将被固定为密钥，或者用零填充到 8 个字节。
+// TripleDESCBCDecrypt TripleDESDecrypt 是同一个函数。
+// example:
+// ```
+// codec.TripleDESCBCDecrypt([]byte("123456789012345678901234"), ciphertext, "12345678")
+// ```
+func TripleDESDecryptCBCWithZeroPadding(key []byte, i interface{}, iv []byte) ([]byte, error) {
+	return TripleDESDecFactory(ZeroUnPadding, CBC)(key, i, iv)
+}
 
 var TripleDESEncryptECBWithPKCSPadding = TripleDESEncFactory(PKCS5Padding, ECB)
 var TripleDESEncryptECBWithZeroPadding = TripleDESEncFactory(ZeroPadding, ECB)
