a233d9b7e7c1f02efbcf9c48757ed0c37fee49e5
intSheep <intSheep@qq.com>
2025-06-27 14:27:36 +0800

    only use WithAppendPostParam

--- common/utils/lowhttp/poc/poc.go
+++ common/utils/lowhttp/poc/poc.go
@@ -598,8 +598,6 @@ func WithPostParams(i any) PocConfigOption {
 			for k, v := range utils.InterfaceToMap(i) {
 				if len(v) == 0 {
 					WithReplaceHttpPacketPostParam(k, "")(c)
-				} else if len(v) == 1 {
-					WithReplaceHttpPacketPostParam(k, utils.InterfaceToString(v[0]))(c)
 				} else {
 					for _, vv := range v {
 						WithAppendPostParam(k, utils.InterfaceToString(vv))(c)
