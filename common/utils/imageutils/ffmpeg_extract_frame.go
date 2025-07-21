8434da29fd667041e54beafd9324802c8cdc9020
v1ll4n <v1ll4n@qq.com>
2025-07-21 11:10:56 +0800

    add testcase for ffmpeg extract frame

--- common/utils/imageutils/ffmpeg_extract_frame.go
+++ common/utils/imageutils/ffmpeg_extract_frame.go
@@ -1,23 +1,29 @@
 package imageutils
 
 import (
+	"bytes"
 	"context"
 	"errors"
-	"github.com/yaklang/yaklang/common/chunkmaker"
-	"github.com/yaklang/yaklang/common/consts"
-	"github.com/yaklang/yaklang/common/utils"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"time"
+
+	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/mimetype"
+	"github.com/yaklang/yaklang/common/utils"
 )
 
-func ExtractVideoFrameContext(ctx context.Context, input string) (*chunkmaker.ChunkMaker, error) {
+func ExtractVideoFrameContext(ctx context.Context, input string) (chan *ImageResult, error) {
 	if utils.GetFirstExistedFile(input) == "" {
 		return nil, utils.Errorf("%s file not existed", input)
 	}
 
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
 	/*
 		ffmpeg -i vtestdata/demo.mp4 \
 		-vf "scdet=threshold=20,select='eq(n,0) + gt(floor(t), floor(prev_t)) + gt(scene, 0.2)',drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf:text='tffset-timestamp\: %{eif\:t*1000\:d}ms':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:x=(w-tw)/2:y=h-th-10,setpts=N/FR/TB" \
@@ -36,18 +42,77 @@ func ExtractVideoFrameContext(ctx context.Context, input string) (*chunkmaker.Ch
 		return nil, errors.New("ffmpeg binary not found at " + ffmpegBinaryPath)
 	}
 
-	outputFmt := filepath.Join(outputTmp, "core-%04d.jpeg")
+	token := utils.RandStringBytes(10)
+	outputFmt := filepath.Join(outputTmp, "core-"+token+"-%04d.jpeg")
 
-	cmd := exec.CommandContext(
-		ctx,
-		ffmpegBinaryPath, "-i", input,
-		`-vf`, `scdet=threshold=20,select='eq(n,0) + gt(floor(t), floor(prev_t)) + gt(scene, 0.2)',drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf:text='tffset-timestamp\: %{eif\:t*1000\:d}ms':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:x=(w-tw)/2:y=h-th-10,setpts=N/FR/TB`,
-		`-fps_mode`, `vfr`,
-		outputFmt,
-	)
-	err := cmd.Run()
-	if err != nil {
-		return nil, utils.Errorf("ffmpeg command failed: %v", err)
-	}
-	return nil, nil
+	var ch = make(chan *ImageResult)
+	go func() {
+		finishedCtx, cancel := context.WithCancel(ctx)
+		defer cancel()
+
+		var output bytes.Buffer
+
+		cmd := exec.CommandContext(
+			ctx,
+			ffmpegBinaryPath, "-i", input,
+			`-vf`, `scdet=threshold=20,select='eq(n,0) + gt(floor(t), floor(prev_t)) + gt(scene, 0.2)',drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf:text='tffset-timestamp\: %{eif\:t*1000\:d}ms':fontcolor=white:fontsize=24:box=1:boxcolor=black@0.5:x=(w-tw)/2:y=h-th-10,setpts=N/FR/TB`,
+			`-fps_mode`, `vfr`,
+			outputFmt,
+		)
+		cmd.Stdout = &output
+		cmd.Stderr = &output
+
+		go func() {
+			defer close(ch)
+
+			outputIdx := 0
+			filter := map[string]bool{}
+			for {
+				select {
+				case <-time.After(1 * time.Second):
+					// read dir and get all files
+					files, err := os.ReadDir(outputTmp)
+					if err != nil {
+						log.Errorf("read dir failed: %v", err)
+						continue
+					}
+					for _, file := range files {
+						if file.IsDir() {
+							continue
+						}
+						fileName := file.Name()
+						if _, ok := filter[fileName]; ok {
+							continue
+						}
+						filter[fileName] = true
+						outputIdx++
+						data, err := os.ReadFile(filepath.Join(outputTmp, fileName))
+						if err != nil {
+							log.Errorf("read file failed: %v", err)
+							continue
+						}
+						mime := mimetype.Detect(data)
+						ch <- &ImageResult{
+							RawImage: data,
+							MIMEType: mime,
+						}
+					}
+					select {
+					case <-finishedCtx.Done():
+						return
+					default:
+					}
+					continue
+				}
+			}
+		}()
+
+		err := cmd.Run()
+		if err != nil {
+			log.Errorf("ffmpeg command failed: %v", err)
+			log.Errorf("ffmpeg output: %s", output.String())
+			return
+		}
+	}()
+	return ch, nil
 }
