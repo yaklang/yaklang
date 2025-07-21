2c193aabe741b69e7e503843a9a1e9ef9888c680
wlz <lingze.wl@gmail.com>
2025-07-21 17:56:45 +0800

    feat(ssa):save type in compile time

--- common/yak/ssa/ssa_predefined.go
+++ common/yak/ssa/ssa_predefined.go
@@ -290,10 +290,12 @@ type anValue struct {
 	occultation []int64
 }
 
+var defaultAnyType = CreateAnyType()
+
 func NewValue() anValue {
 	return anValue{
 		anInstruction: NewInstruction(),
-		typ:           CreateAnyType(),
+		typ:           defaultAnyType,
 		userList:      make([]int64, 0),
 		member:        omap.NewOrderedMap(map[int64]int64{}),
 
@@ -440,16 +442,8 @@ func (n *anValue) SetType(typ Type) {
 	}
 
 	value := n.GetValueById(n.GetId())
-	// i know is ugle, just is, and i will fix this after remove init value in ssa/next.go
-	if !utils.IsNil(value) {
-		if prog := value.GetProgram(); prog != nil {
-			if application := prog.GetApplication(); application != nil {
-				if cache := application.Cache; cache != nil && cache.HaveDatabaseBackend() {
-					saveType(cache, typ)
-				}
-			}
-		}
-	}
+	saveTypeWithValue(value, typ)
+
 	switch t := typ.(type) {
 	case *Blueprint:
 		n.typ = t.Apply(value)
