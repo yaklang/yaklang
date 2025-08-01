50795ed1d1ee0541d7e62ca99f593854c7f37e85
intSheep <intSheep@qq.com>
2025-08-01 15:36:31 +0800

    fix(sf):Fix the incorrect OK judgment method of value match and the incorrect Predecessor of deepchain

--- common/yak/ssaapi/test/yak/dotgraph_test.go
+++ common/yak/ssaapi/test/yak/dotgraph_test.go
@@ -6,13 +6,45 @@ import (
 	"github.com/yaklang/yaklang/common/yak/ssaapi/test/ssatest"
 )
 
-func TestDotGraphEdgeLabel(t *testing.T) {
+func TestSimpleDotGraphEdgeLabel(t *testing.T) {
 	tests := []struct {
 		name          string
 		code          string
 		sfRule        string
 		expectedEdges []ssatest.EdgeInfo
 	}{
+		{
+			"simple call",
+			"a()",
+			"a() as $result",
+			[]ssatest.EdgeInfo{
+				{VariableName: "result", From: "a", To: "a()", Label: "call"},
+			},
+		},
+		{
+			"exact search",
+			"a.b()",
+			"a.b as $result",
+			[]ssatest.EdgeInfo{
+				{VariableName: "result", From: "a", To: ".b", Label: "search-exact"},
+			},
+		},
+		{
+			"glob search",
+			"a.bb()",
+			"a.b* as $result",
+			[]ssatest.EdgeInfo{
+				{VariableName: "result", From: "a", To: ".bb", Label: "search-glob:b*"},
+			},
+		},
+		{
+			"regex search",
+			"a.bb()",
+			"a./bb/ as $result",
+			[]ssatest.EdgeInfo{
+				{VariableName: "result", From: "a", To: ".bb", Label: "search-regexp:bb"},
+			},
+		},
 		{
 			"get user with dot graph edge label",
 			"a.b()",
@@ -27,6 +59,16 @@ func TestDotGraphEdgeLabel(t *testing.T) {
 			"b<getUsers> as $result",
 			[]ssatest.EdgeInfo{
 				{VariableName: "result", From: ".b", To: "a.b()", Label: "native-call:[getUsers]"},
+			},
+		},
+		{
+			"deep chain test",
+			"a.b.c.d.e.f.g.h().aaa.bbb.ccc()",
+			`a...h as $result1;
+				$result1...ccc() as $result2`,
+			[]ssatest.EdgeInfo{
+				{VariableName: "result1", From: "a", To: ".h", Label: "step[1]: recursive search h"},
+				{VariableName: "result2", From: ".h", To: ".ccc", Label: "step[2]: recursive search ccc"},
 			},
 		},
 	}
