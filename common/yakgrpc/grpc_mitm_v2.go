9fe47eb8b59d94fa61d7c8b5b3b0faec4266d9da
Matrix-Cain <zwh_china@outlook.com>
2025-08-08 11:35:52 +0800

    enhance test && fix data race

--- common/yakgrpc/grpc_mitm_v2.go
+++ common/yakgrpc/grpc_mitm_v2.go
@@ -1816,7 +1816,7 @@ func (s *Server) checkLongRunningTraces(manager *yak.YakToCallerManager, traceUp
 		// 检查运行时间是否超过阈值（5秒）
 		if !trace.StartTime.IsZero() {
 			elapsed := time.Since(trace.StartTime)
-			if elapsed > 5*time.Second {
+			if elapsed > time.Duration(consts.PluginCallDurationThresholdSeconds)*time.Second {
 				// 标记为已推送
 				(*pushedTraces)[trace.TraceID] = true
 
@@ -1870,6 +1870,7 @@ func (s *Server) sendTraceBatch(stream ypb.Yak_PluginTraceServer, traces []*yak.
 	resp := &ypb.PluginTraceResponse{
 		ResponseType: "trace_update",
 		Traces:       pbTraces,
+		Success:      true,
 	}
 
 	if err := stream.Send(resp); err != nil {
