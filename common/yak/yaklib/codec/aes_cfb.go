a6972afb0910558a6f6ce4431d1a92a6f52b4fbd
rookie <rookie_is@qq.com>
2025-08-19 15:33:53 +0800

    export codec aes cfb and basic interface

---
+++ common/yak/yaklib/codec/aes_cfb.go
@@ -0,0 +1,25 @@
+package codec
+
+// AESEncryptCFBWithPKCSPadding 使用 AES 算法，在 CFB 模式下，使用 PKCS5 填充来加密数据。
+// 密钥的长度必须是 16、24 或 32 字节（分别对应 AES-128、AES-192 或 AES-256）。
+// 如果iv为 nil，则使用key的前16字节作为iv。
+func AESEncryptCFBWithPKCSPadding(key []byte, i interface{}, iv []byte) ([]byte, error) {
+	return AESEncFactory(PKCS5Padding, CFB)(key, i, iv)
+}
+
+// AESCFBDecryptWithPKCS7Padding 使用 AES 算法，在 CFB 模式下，使用 PKCS5 填充来解密数据。
+// 密钥的长度必须是 16、24 或 32 字节（分别对应 AES-128、AES-192 或 AES-256）。
+// 如果iv为 nil，则使用key的前16字节作为iv。
+func AESDecryptCFBWithPKCSPadding(key []byte, i interface{}, iv []byte) ([]byte, error) {
+	return AESDecFactory(PKCS5UnPadding, CFB)(key, i, iv)
+}
+
+// AESCFBEncryptWithZeroPadding 使用 AES 算法，在 CFB 模式下，使用 Zero 填充来加密数据。
+func AESEncryptCFBWithZeroPadding(key []byte, i interface{}, iv []byte) ([]byte, error) {
+	return AESEncFactory(ZeroPadding, CFB)(key, i, iv)
+}
+
+// AESCFBDecryptWithZeroPadding 使用 AES 算法，在 CFB 模式下，使用 Zero 填充来解密数据。
+func AESDecryptCFBWithZeroPadding(key []byte, i interface{}, iv []byte) ([]byte, error) {
+	return AESDecFactory(ZeroUnPadding, CFB)(key, i, iv)
+}
