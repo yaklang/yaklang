fadc993868d0636d5e2b4525b7b42155687db119
VillanCh <188228725@qq.com>
2025-08-21 21:38:16 +0800

    add more testcases for ask_for_clarification

---
+++ common/ai/aid/aireact/re-act_interactive_toolcall_test.go
@@ -0,0 +1,140 @@
+package aireact
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/segmentio/ksuid"
+	"github.com/yaklang/yaklang/common/ai/aid/aicommon"
+	"github.com/yaklang/yaklang/common/ai/aid/aitool"
+	"github.com/yaklang/yaklang/common/jsonpath"
+	"github.com/yaklang/yaklang/common/schema"
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"io"
+	"testing"
+	"time"
+)
+
+func mockedToolCalling(i aicommon.AICallerConfigIf, req *aicommon.AIRequest, toolName string) (*aicommon.AIResponse, error) {
+	prompt := req.GetPrompt()
+	if utils.MatchAllOfSubString(prompt, "directly_answer", "request_plan_and_execution", "require_tool") {
+		rsp := i.NewAIResponse()
+		rsp.EmitOutputStream(bytes.NewBufferString(`
+{"@action": "object", "next_action": { "type": "require_tool", "tool_require_payload": "` + toolName + `" },
+"human_readable_thought": "mocked thought for tool calling", "cumulative_summary": "..cumulative-mocked for tool calling.."}
+`))
+		rsp.Close()
+		return rsp, nil
+	}
+
+	if utils.MatchAllOfSubString(prompt, "You need to generate parameters for the tool", "call-tool") {
+		rsp := i.NewAIResponse()
+		rsp.EmitOutputStream(bytes.NewBufferString(`{"@action": "call-tool", "params": { "seconds" : 0.1 }}`))
+		rsp.Close()
+		return rsp, nil
+	}
+
+	return nil, utils.Errorf("unexpected prompt: %s", prompt)
+}
+
+func TestReAct_ToolUse(t *testing.T) {
+	flag := ksuid.New().String()
+	_ = flag
+	in := make(chan *ypb.AIInputEvent, 10)
+	out := make(chan *ypb.AIOutputEvent, 10)
+
+	toolCalled := false
+	sleepTool, err := aitool.New(
+		"sleep",
+		aitool.WithNumberParam("seconds"),
+		aitool.WithSimpleCallback(func(params aitool.InvokeParams, stdout io.Writer, stderr io.Writer) (any, error) {
+			toolCalled = true
+			sleepInt := params.GetFloat("seconds", 0.3)
+			if sleepInt <= 0 {
+				sleepInt = 0.3
+			}
+			time.Sleep(time.Duration(sleepInt) * time.Second)
+			return "done", nil
+		}),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ins, err := NewReAct(
+		WithAICallback(func(i aicommon.AICallerConfigIf, r *aicommon.AIRequest) (*aicommon.AIResponse, error) {
+			return mockedToolCalling(i, r, "sleep")
+		}),
+		WithDebug(true),
+		WithEventInputChan(in),
+		WithEventHandler(func(e *schema.AiOutputEvent) {
+			out <- e.ToGRPC()
+		}),
+		WithTools(sleepTool),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_ = ins
+	go func() {
+		for i := 0; i < 1; i++ {
+			in <- &ypb.AIInputEvent{
+				IsFreeInput: true,
+				FreeInput:   "abc",
+			}
+		}
+	}()
+	after := time.After(5 * time.Second)
+
+	reviewed := false
+	reviewReleased := false
+	toolCallOutputEvent := false
+	var iid string
+LOOP:
+	for {
+		select {
+		case e := <-out:
+			fmt.Println(e.String())
+			if e.Type == string(schema.EVENT_TYPE_TOOL_USE_REVIEW_REQUIRE) {
+				reviewed = true
+				iid = utils.InterfaceToString(jsonpath.FindFirst(string(e.Content), "$.id"))
+				in <- &ypb.AIInputEvent{
+					IsInteractiveMessage: true,
+					InteractiveId:        utils.InterfaceToString(iid),
+					InteractiveJSONInput: `{"suggestion": "continue"}`,
+				}
+			}
+
+			if e.Type == string(schema.EVENT_TYPE_REVIEW_RELEASE) {
+				gotId := utils.InterfaceToString(jsonpath.FindFirst(string(e.Content), "$.id"))
+				if gotId == iid {
+					reviewReleased = true
+				}
+			}
+
+			if e.Type == string(schema.EVENT_TOOL_CALL_DONE) {
+				toolCallOutputEvent = true
+				break LOOP
+			}
+		case <-after:
+			break LOOP
+		}
+	}
+	close(in)
+
+	if !reviewed {
+		t.Fatal("Expected to have at least one review event, but got none")
+	}
+
+	if !reviewReleased {
+		t.Fatal("Expected to have at least one review release event, but got none")
+	}
+
+	if !toolCalled {
+		t.Fatal("Tool was not called")
+	}
+
+	if !toolCallOutputEvent {
+		t.Fatal("Expected to have at least one output event, but got none")
+	}
+}
