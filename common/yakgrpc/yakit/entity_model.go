a7cb0acc6bdf5cfbcdb222feb40bc9fe016a1108
VillanCh <188228725@qq.com>
2025-08-29 09:54:56 +0800

    remove attrs tablue and add some details

--- common/yakgrpc/yakit/entity_model.go
+++ common/yakgrpc/yakit/entity_model.go
@@ -2,7 +2,6 @@ package yakit
 
 import (
 	"github.com/jinzhu/gorm"
-	"github.com/samber/lo"
 	"github.com/yaklang/yaklang/common/schema"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/bizhelper"
@@ -37,7 +36,7 @@ func FilterEntities(db *gorm.DB, entityFilter *EntityFilter) *gorm.DB {
 }
 
 func QueryEntities(db *gorm.DB, entityFilter *EntityFilter) ([]*schema.ERModelEntity, error) {
-	db = db.Model(&schema.ERModelEntity{}).Preload("Attributes").Preload("IncomingRelationships").Preload("OutgoingRelationships")
+	db = db.Model(&schema.ERModelEntity{})
 	db = FilterEntities(db, entityFilter)
 	var entities []*schema.ERModelEntity
 	err := db.Find(&entities).Error
@@ -71,63 +70,63 @@ type AttributeFilter struct {
 	EntityID   []uint
 }
 
-func FilterAttributes(db *gorm.DB, filter *AttributeFilter) *gorm.DB {
-	if filter == nil {
-		return db
-	}
-	db = db.Model(&schema.ERModelAttribute{})
-	db = bizhelper.ExactQueryStringArrayOr(db, "attribute_name", filter.Name)
-	db = bizhelper.ExactQueryStringArrayOr(db, "attribute_value", filter.Value)
-	db = bizhelper.ExactQueryUIntArrayOr(db, "entity_id", filter.EntityID)
-	if filter.UniqueOnly {
-		db = db.Where("unique_identifier = ?", true)
-	}
-	return db
-}
+//func FilterAttributes(db *gorm.DB, filter *AttributeFilter) *gorm.DB {
+//	if filter == nil {
+//		return db
+//	}
+//	db = db.Model(&schema.ERModelAttribute{})
+//	db = bizhelper.ExactQueryStringArrayOr(db, "attribute_name", filter.Name)
+//	db = bizhelper.ExactQueryStringArrayOr(db, "attribute_value", filter.Value)
+//	db = bizhelper.ExactQueryUIntArrayOr(db, "entity_id", filter.EntityID)
+//	if filter.UniqueOnly {
+//		db = db.Where("unique_identifier = ?", true)
+//	}
+//	return db
+//}
 
-func QueryAttributes(db *gorm.DB, filter *AttributeFilter) ([]*schema.ERModelAttribute, error) {
-	db = db.Model(&schema.ERModelAttribute{})
-	db = FilterAttributes(db, filter)
-	var attributes []*schema.ERModelAttribute
-	err := db.Find(&attributes).Error
-	return attributes, err
-}
+//func QueryAttributes(db *gorm.DB, filter *AttributeFilter) ([]*schema.ERModelAttribute, error) {
+//	db = db.Model(&schema.ERModelAttribute{})
+//	db = FilterAttributes(db, filter)
+//	var attributes []*schema.ERModelAttribute
+//	err := db.Find(&attributes).Error
+//	return attributes, err
+//}
+//
+//func UniqueAttributesIndexRelationship(db *gorm.DB, name string, values string) ([]uint, bool) {
+//	db = FilterAttributes(db, &AttributeFilter{
+//		Name:       []string{name},
+//		Value:      []string{values},
+//		UniqueOnly: true,
+//	})
+//	var attribute = make([]*schema.ERModelAttribute, 0)
+//	err := db.Find(&attribute).Error
+//	if err != nil {
+//		return nil, false
+//	}
+//	return lo.Map(attribute, func(item *schema.ERModelAttribute, _ int) uint {
+//		return item.ID
+//	}), true
+//}
+//
+//func UniqueAttributesIndexEntity(db *gorm.DB, name string, values string) ([]uint, bool) {
+//	db = FilterAttributes(db, &AttributeFilter{
+//		Name:       []string{name},
+//		Value:      []string{values},
+//		UniqueOnly: true,
+//	})
+//	var attribute = make([]*schema.ERModelAttribute, 0)
+//	err := db.Find(&attribute).Error
+//	if err != nil {
+//		return nil, false
+//	}
+//	return lo.Map(attribute, func(item *schema.ERModelAttribute, _ int) uint {
+//		return item.ID
+//	}), true
+//}
 
-func UniqueAttributesIndexRelationship(db *gorm.DB, name string, values string) ([]uint, bool) {
-	db = FilterAttributes(db, &AttributeFilter{
-		Name:       []string{name},
-		Value:      []string{values},
-		UniqueOnly: true,
-	})
-	var attribute = make([]*schema.ERModelAttribute, 0)
-	err := db.Find(&attribute).Error
-	if err != nil {
-		return nil, false
-	}
-	return lo.Map(attribute, func(item *schema.ERModelAttribute, _ int) uint {
-		return item.ID
-	}), true
-}
-
-func UniqueAttributesIndexEntity(db *gorm.DB, name string, values string) ([]uint, bool) {
-	db = FilterAttributes(db, &AttributeFilter{
-		Name:       []string{name},
-		Value:      []string{values},
-		UniqueOnly: true,
-	})
-	var attribute = make([]*schema.ERModelAttribute, 0)
-	err := db.Find(&attribute).Error
-	if err != nil {
-		return nil, false
-	}
-	return lo.Map(attribute, func(item *schema.ERModelAttribute, _ int) uint {
-		return item.ID
-	}), true
-}
-
-func CreateAttribute(db *gorm.DB, attribute *schema.ERModelAttribute) error {
-	return db.Create(attribute).Error
-}
+//func CreateAttribute(db *gorm.DB, attribute *schema.ERModelAttribute) error {
+//	return db.Create(attribute).Error
+//}
 
 func AddRelationship(db *gorm.DB, sourceID, targetID uint, RelationshipType, decisionRationale string, attrs map[string]any) error {
 	Relationship := schema.ERModelRelationship{
@@ -152,7 +151,7 @@ func AddRelationship(db *gorm.DB, sourceID, targetID uint, RelationshipType, dec
 
 // RemoveRelationship 删除两个实体之间的永久关系。
 func RemoveRelationship(db *gorm.DB, sourceID, targetID uint, RelationshipType string) error {
-	result := db.Where("source_entity_id = ? AND target_entity_id = ? AND Relationship_type = ?",
+	result := db.Where("source_entity_id = ? AND target_entity_id = ? AND relationship_type = ?",
 		sourceID, targetID, RelationshipType).Delete(&schema.ERModelRelationship{})
 	if result.Error != nil {
 		return result.Error
