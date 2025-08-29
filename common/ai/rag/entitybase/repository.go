d88f39179ad666db7f598298e1a1f3c35c05ad50
rookie <rookie_is@qq.com>
2025-08-29 17:11:13 +0800

    feat(entity-repository):add entity repository grpc

--- common/ai/rag/entitybase/repository.go
+++ common/ai/rag/entitybase/repository.go
@@ -3,15 +3,12 @@ package entitybase
 import (
 	"errors"
 	"fmt"
-	"strings"
-
 	"github.com/jinzhu/gorm"
 	"github.com/yaklang/yaklang/common/ai/rag"
-	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/schema"
 	"github.com/yaklang/yaklang/common/utils"
-	"github.com/yaklang/yaklang/common/utils/dot"
 	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
 const (
@@ -20,74 +17,6 @@ const (
 	META_EntityName   = "entity_name"
 	META_EntityType   = "entity_type"
 )
-
-type ERModel struct {
-	Entities      []*schema.ERModelEntity
-	Relationships []*schema.ERModelRelationship
-}
-
-func (model *ERModel) Dump() string {
-	var sb strings.Builder
-	sb.WriteString("Entities:\n")
-	for _, entity := range model.Entities {
-		sb.WriteString(fmt.Sprintf("- ID: %d\n", entity.ID))
-		sb.WriteString(fmt.Sprintf("  EntityName: %s\n", entity.EntityName))
-		sb.WriteString(fmt.Sprintf("  EntityType: %s\n", entity.EntityType))
-		if entity.Description != "" {
-			sb.WriteString(fmt.Sprintf("  Description: %s\n", utils.ShrinkString(entity.Description, 100)))
-		}
-		if entity.Rationale != "" {
-			sb.WriteString(fmt.Sprintf("  Rationale: %s\n", utils.ShrinkString(entity.Rationale, 100)))
-		}
-		if len(entity.Attributes) > 0 {
-			sb.WriteString("  Attributes:\n")
-			for key, value := range entity.Attributes {
-				sb.WriteString(fmt.Sprintf("    - %s: %s\n", key, utils.ShrinkString(value, 100)))
-			}
-		}
-	}
-	sb.WriteString("Relationships:\n")
-	for _, relationship := range model.Relationships {
-		sb.WriteString(fmt.Sprintf("- Source: %d\n", relationship.SourceEntityID))
-		sb.WriteString(fmt.Sprintf("  Target: %d\n", relationship.TargetEntityID))
-		sb.WriteString(fmt.Sprintf("  Type: %s\n", relationship.RelationshipType))
-		if relationship.DecisionRationale != "" {
-			sb.WriteString(fmt.Sprintf("  Rationale: %s\n", utils.ShrinkString(relationship.DecisionRationale, 100)))
-		}
-		if len(relationship.Attributes) > 0 {
-			sb.WriteString("  Attributes:\n")
-			for key, value := range relationship.Attributes {
-				sb.WriteString(fmt.Sprintf("    - %s: %s\n", key, utils.ShrinkString(value, 100)))
-			}
-		}
-	}
-	return sb.String()
-}
-
-func (model *ERModel) Dot() *dot.Graph {
-	G := dot.New()
-	G.MakeDirected()
-
-	rMap := make(map[uint]int)
-
-	for _, entity := range model.Entities {
-		n := G.AddNode(entity.EntityName)
-		for key, value := range entity.Attributes {
-			G.NodeAttribute(n, key, utils.InterfaceToString(value))
-		}
-		rMap[entity.ID] = n
-	}
-
-	for _, Relationship := range model.Relationships {
-		sid, ok := rMap[Relationship.SourceEntityID]
-		tid, ok2 := rMap[Relationship.TargetEntityID]
-		if !ok || !ok2 {
-			continue
-		}
-		G.AddEdge(sid, tid, Relationship.RelationshipType)
-	}
-	return G
-}
 
 type EntityRepository struct {
 	db        *gorm.DB
@@ -133,9 +62,9 @@ func (eb *EntityRepository) MatchEntities(entity *schema.ERModelEntity) (matchEn
 
 func (eb *EntityRepository) IdentifierSearchEntity(entity *schema.ERModelEntity) ([]*schema.ERModelEntity, error) {
 	// name and type query
-	entities, err := eb.queryEntities(&yakit.EntityFilter{
-		Name: []string{entity.EntityName},
-		Type: []string{entity.EntityType},
+	entities, err := eb.queryEntities(&ypb.EntityFilter{
+		Names: []string{entity.EntityName},
+		Types: []string{entity.EntityType},
 	})
 	if err != nil {
 		return nil, err
@@ -181,14 +110,14 @@ func (eb *EntityRepository) VectorSearchEntity(entity *schema.ERModelEntity) ([]
 		return nil, nil
 	}
 
-	var entityIds []uint
+	var entityIds []uint64
 	for _, res := range results {
 		if res.Score <= 0.8 {
 			continue
 		}
 		id, ok := res.Document.Metadata[META_EntityID]
 		if ok {
-			entityIds = append(entityIds, uint(utils.InterfaceToInt(id)))
+			entityIds = append(entityIds, uint64(utils.InterfaceToInt(id)))
 		}
 	}
 
@@ -196,13 +125,13 @@ func (eb *EntityRepository) VectorSearchEntity(entity *schema.ERModelEntity) ([]
 		return nil, nil
 	}
 
-	return eb.queryEntities(&yakit.EntityFilter{
-		ID: entityIds,
+	return eb.queryEntities(&ypb.EntityFilter{
+		IDs: entityIds,
 	})
 }
 
-func (eb *EntityRepository) queryEntities(filter *yakit.EntityFilter) ([]*schema.ERModelEntity, error) {
-	filter.EntityBaseID = []uint{eb.baseInfo.ID}
+func (eb *EntityRepository) queryEntities(filter *ypb.EntityFilter) ([]*schema.ERModelEntity, error) {
+	filter.BaseID = uint64(eb.baseInfo.ID)
 	return yakit.QueryEntities(eb.db, filter)
 }
 
@@ -269,18 +198,7 @@ func (eb *EntityRepository) QueryIncomingRelationships(entity *schema.ERModelEnt
 }
 
 // --- ER Model Operations ---
-func (eb *EntityRepository) GetSubERModel(keyword string, maxDepths ...int) (*ERModel, error) {
-	allEntities := make([]*schema.ERModelEntity, 0)
-	allRelationships := make([]*schema.ERModelRelationship, 0)
-
-	appendEntity := func(entityList ...*schema.ERModelEntity) {
-		allEntities = append(allEntities, entityList...)
-	}
-
-	appendRelationship := func(RelationshipList ...*schema.ERModelRelationship) {
-		allRelationships = append(allRelationships, RelationshipList...)
-	}
-
+func (eb *EntityRepository) GetSubERModel(keyword string, maxDepths ...int) (*yakit.ERModel, error) {
 	maxDepth := 2
 	if len(maxDepths) > 0 {
 		maxDepth = maxDepths[0]
@@ -295,75 +213,7 @@ func (eb *EntityRepository) GetSubERModel(keyword string, maxDepths ...int) (*ER
 	if startEntity == nil {
 		return nil, utils.Errorf("实体 %s 不存在", keyword)
 	}
-
-	type queueItem struct {
-		entityID uint
-		depth    int
-		e        *schema.ERModelEntity
-	}
-
-	queue := []queueItem{
-		{
-			entityID: startEntity.ID,
-			depth:    0,
-			e:        startEntity,
-		},
-	}
-	visited := map[uint]bool{startEntity.ID: true}
-	visitedRelationships := map[uint]bool{}
-
-	head := 0
-	for head < len(queue) {
-		currentItem := queue[head]
-		head++
-		currentEntity := currentItem.e
-		if maxDepth > 0 && currentItem.depth >= maxDepth {
-			continue
-		}
-		// 准备要遍历的关系列表
-		RelationshipsToExplore := make([]*schema.ERModelRelationship, 0)
-
-		// query outgoing and incoming relationships
-		if outgoings, err := eb.QueryOutgoingRelationships(currentEntity); err == nil {
-			RelationshipsToExplore = append(RelationshipsToExplore, outgoings...)
-		} else {
-			log.Errorf("query outgoing failed: %v", err)
-		}
-
-		if incomings, err := eb.QueryIncomingRelationships(currentEntity); err == nil {
-			RelationshipsToExplore = append(RelationshipsToExplore, incomings...)
-		} else {
-			log.Errorf("query incoming failed: %v", err)
-		}
-
-		for _, Relationship := range RelationshipsToExplore {
-			if visitedRelationships[Relationship.ID] {
-				continue
-			}
-			visitedRelationships[Relationship.ID] = true
-			appendRelationship(Relationship)
-			var neighborID uint
-			if Relationship.SourceEntityID == currentItem.entityID {
-				neighborID = Relationship.TargetEntityID
-			} else {
-				neighborID = Relationship.SourceEntityID
-			}
-			if !visited[neighborID] {
-				neighbor, err := yakit.GetEntityByID(eb.db, neighborID)
-				if err != nil {
-					return nil, err
-				}
-				visited[neighborID] = true
-				appendEntity(neighbor)
-				queue = append(queue, queueItem{entityID: neighborID, depth: currentItem.depth + 1, e: neighbor})
-			}
-		}
-	}
-
-	return &ERModel{
-		Entities:      allEntities,
-		Relationships: allRelationships,
-	}, nil
+	return yakit.EntityRelationshipFind(eb.db, []*schema.ERModelEntity{startEntity}, maxDepth)
 }
 
 func NewEntityRepository(db *gorm.DB, name, description string, opts ...any) (*EntityRepository, error) {
