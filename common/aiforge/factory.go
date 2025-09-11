394be5b4e47c82d8c4af634de14610b75989d810
v1ll4n <v1ll4n@qq.com>
2025-09-11 11:54:28 +0800

    generate schema

--- common/aiforge/factory.go
+++ common/aiforge/factory.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/jinzhu/gorm"
 	"github.com/yaklang/yaklang/common/ai/aid"
+	"github.com/yaklang/yaklang/common/ai/aid/aitool"
 	"github.com/yaklang/yaklang/common/consts"
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/schema"
@@ -73,6 +74,50 @@ func (f *ForgeFactory) GenerateAIForgeListForPrompt(forges []*schema.AIForge) (s
 		"nonce":  utils.RandStringBytes(4),
 	})
 	return result, err
+}
+
+// GetAIForge 根据名称从数据库中获取单个 AIForge
+func (f *ForgeFactory) GetAIForge(name string) (*schema.AIForge, error) {
+	if name == "" {
+		return nil, utils.Errorf("forge name cannot be empty")
+	}
+
+	db := consts.GetGormProfileDatabase()
+	log.Debugf("ForgeFactory.GetAIForge: name=%q", name)
+
+	return yakit.GetAIForgeByName(db, name)
+}
+
+// GenerateAIJSONSchemaOptionsFromSchemaAIForge 从 AIForge 生成对应的 aitool.ToolOption 选项
+// 这个函数解析 AIForge.Params 中的 Yak 语言 CLI 参数定义代码，并生成相应的 aitool.ToolOption 配置
+func (f *ForgeFactory) GenerateAIJSONSchemaFromSchemaAIForge(forge *schema.AIForge) (string, error) {
+	if forge == nil {
+		return "", utils.Errorf("forge cannot be nil")
+	}
+
+	var options []any
+	var params []aitool.ToolOption
+
+	options = append(options, aitool.WithAction("call-ai-blueprint"))
+
+	// 如果 forge.Params 为空，只返回基本选项
+	if forge.Params == "" {
+		params = append(params, aitool.WithStringParam("query", aitool.WithParam_Description("Some input for helping the AI blueprint execute plan and executing")))
+	} else {
+		// 解析 Yak CLI 代码获取参数选项
+		parsedParams := aitool.ConvertYaklangCliCodeToToolOptions(forge.Params)
+		params = append(params, parsedParams...)
+	}
+
+	// 如果有参数，添加到 params 结构体中
+	if len(params) > 0 {
+		options = append(options, aitool.WithStructParam("params", nil, params...))
+	} else {
+		// 如果没有参数，创建一个空的 params 对象
+		options = append(options, aitool.WithStructParam("params", nil))
+	}
+
+	return aitool.NewObjectSchema(options...), nil
 }
 
 // Execute 透明转发到内置 ExecuteForge
