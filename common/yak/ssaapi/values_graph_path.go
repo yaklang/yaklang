61b947db71f87b0cc621112143fab61928f32e9a
wlz <lingze.wl@gmail.com>
2025-09-16 17:07:25 +0800

    fix(ssaapi):dataflow path use prev-dataflow point

--- common/yak/ssaapi/values_graph_path.go
+++ common/yak/ssaapi/values_graph_path.go
@@ -1,85 +1,47 @@
 package ssaapi
 
-import (
-	"github.com/yaklang/yaklang/common/utils"
-	"github.com/yaklang/yaklang/common/utils/graph"
-)
-
 func (v *Value) GetDataflowPath(end ...*Value) []Values {
 	var paths []Values
-	effectPath := v.GetEffectOnPath(end...)
-	dependPath := v.GetDependOnPath(end...)
-	addPath := func(effect, depend Values) {
-		path := make(Values, 0, len(effect)+len(depend)+1)
-		path = append(path, effect...)
-		path = append(path, v)
-		path = append(path, depend...)
-		paths = append(paths, path)
-	}
 
-	if len(effectPath) == 0 { // if no effect, then depend is the path
-		for _, depend := range dependPath {
-			addPath(Values{}, depend)
+	endSet := make(map[int64]struct{})
+	hasEnd := len(end) > 0
+	if hasEnd {
+		for _, e := range end {
+			endSet[e.GetId()] = struct{}{}
 		}
 	}
-	if len(dependPath) == 0 { // if no depend, then effect is the path
-		for _, effect := range effectPath {
-			addPath(effect, Values{})
+
+	var dfs func(current *Value, path Values, visited map[int64]bool)
+	dfs = func(current *Value, path Values, visited map[int64]bool) {
+		if visited[current.GetId()] {
+			return
 		}
-	}
-	for _, effect := range effectPath {
-		for _, depend := range dependPath {
-			addPath(effect, depend) // effect -> v -> depend
-		}
-	}
-	return paths
-}
+		visited[current.GetId()] = true
+		defer func() { visited[current.GetId()] = false }()
 
-func (v *Value) GetEffectOnPath(end ...*Value) []Values {
-	return v.getPathWithDirection(
-		func(node *Value) Values {
-			return node.GetEffectOn()
-		},
-		end...,
-	)
-}
+		newPath := append(path, current)
 
-func (v *Value) GetDependOnPath(end ...*Value) []Values {
-	return v.getPathWithDirection(
-		func(node *Value) Values {
-			return node.GetDependOn()
-		},
-		end...,
-	)
-}
+		_, isEnd := endSet[current.GetId()]
+		succs := current.GetDataFlow()
 
-func (this *Value) getPathWithDirection(next func(*Value) Values, end ...*Value) []Values {
-	ret := graph.GraphPathWithKey[int64, *Value](
-		this,
-		func(node *Value) []*Value { // next
-			if ValueContain(node, end...) {
-				return nil
+		if hasEnd {
+			if isEnd {
+				paths = append(paths, newPath)
+				return
 			}
-			return next(node) // todo: handler endValue in here
-		},
-		func(node *Value) int64 { // getKey
-			return node.GetId()
-		},
-	)
-	paths := make([]Values, 0, len(ret))
-	for _, path := range ret {
-		path = utils.RemoveSliceItem(path, this)
-		if len(end) == 0 {
-			paths = append(paths, path)
-			continue
-		}
-		for _, end := range end {
-			if ValueContain(end, path...) {
-				paths = append(paths, path)
-				break
+		} else {
+			if len(succs) == 0 {
+				paths = append(paths, newPath)
 			}
 		}
+
+		for _, succ := range succs {
+			dfs(succ, newPath, visited)
+		}
 	}
+
+	dfs(v, Values{}, make(map[int64]bool))
+
 	return paths
 }
 
