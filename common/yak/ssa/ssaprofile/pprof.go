783d1fa5c00b9e07764d92f2516766ffcaeceaae
wlz <lingze.wl@gmail.com>
2025-09-17 11:32:40 +0800

    feat(ssacli):support pprof and log-level

--- common/yak/ssa/ssaprofile/pprof.go
+++ common/yak/ssa/ssaprofile/pprof.go
@@ -3,6 +3,7 @@ package ssaprofile
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"runtime"
 	"runtime/pprof"
 	"time"
@@ -13,8 +14,12 @@ import (
 type dumpHeapConfig struct {
 	memThreshold uint64
 	name         string
+	fileName     string
 	disable      bool
+	count        int
+	runtimeGC    bool
 }
+
 type dumpHeapOption func(*dumpHeapConfig)
 
 func NewHeapConfig(opts ...dumpHeapOption) dumpHeapConfig {
@@ -32,6 +37,23 @@ func WithHeapLimit(memThreshold uint64) dumpHeapOption {
 		cfg.memThreshold = memThreshold
 	}
 }
+
+func WithDumpCount(count int) dumpHeapOption {
+	return func(cfg *dumpHeapConfig) {
+		cfg.count = count
+	}
+}
+
+func WithFileName(name string) dumpHeapOption {
+	return func(dhc *dumpHeapConfig) {
+		// check file name suffix is .pb.gz
+		if filepath.Ext(name) != ".pb.gz" {
+			name += ".pb.gz"
+		}
+		dhc.fileName = name
+	}
+}
+
 func WithName(name string) dumpHeapOption {
 	return func(cfg *dumpHeapConfig) {
 		cfg.name = name
@@ -47,6 +69,15 @@ func WithDisable(disable ...bool) dumpHeapOption {
 	}
 }
 
+func WithRuntimeGC(enable ...bool) dumpHeapOption {
+	return func(cfg *dumpHeapConfig) {
+		if len(enable) == 0 {
+			cfg.runtimeGC = true
+		}
+		cfg.runtimeGC = enable[0]
+	}
+}
+
 func DumpHeapProfile(opts ...dumpHeapOption) {
 	cfg := NewHeapConfig(opts...)
 	count := 0
@@ -58,25 +89,42 @@ func DumpHeapProfile(opts ...dumpHeapOption) {
 			return
 		}
 		if m.Alloc > cfg.memThreshold {
-			filename := fmt.Sprintf("heap_profile_%s_%d_%s_%d.pb.gz", time.Now().Format("2006-0102-15:04:05"), bToMb(m.Alloc), cfg.name, count)
+			tmpFile := fmt.Sprintf(
+				"heap_profile_%s_%d_%s_%d.pb.gz",
+				time.Now().Format("2006-01-02-15:04:05"), bToMb(m.Alloc), cfg.name, count,
+			)
 			count++
-			log.Printf("Memory dumping heap profile to %s\n", filename)
+			// log.Printf("Memory dumping heap profile to %s\n", tmpFile)
 
-			f, err := os.Create(filename)
+			// Create temporary file
+			f, err := os.Create(tmpFile)
 			if err != nil {
-				log.Fatalf("Could not create heap profile: %v", err)
+				log.Fatalf("Could not create temporary heap profile file: %v", err)
 			}
+			// Write to temporary file
 			if err := pprof.WriteHeapProfile(f); err != nil {
+				f.Close()
+				os.Remove(tmpFile) // Clean up temporary file on error
 				log.Fatalf("Could not write heap profile: %v", err)
 			}
 			f.Close()
+
+			if filename := cfg.fileName; filename != "" {
+				// Atomically move temporary file to target file
+				if err := os.Rename(tmpFile, filename); err != nil {
+					log.Fatalf("Could not move heap profile to final location: %v", err)
+				}
+				os.Remove(tmpFile) // Clean up temporary file
+			}
 		} else {
 			log.Printf("Current memory usage: %d MB, below threshold (%d MB)\n", bToMb(m.Alloc), bToMb(cfg.memThreshold))
 		}
 	}
 	save()
-	runtime.GC()
-	save()
+	if cfg.runtimeGC {
+		runtime.GC()
+		save()
+	}
 }
 
 func DumpHeapProfileWithInterval(dumpInterval time.Duration, opts ...dumpHeapOption) {
