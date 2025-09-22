2d10179eef8fa7446aa42457c6985230f4d17ef2
v1ll4n <v1ll4n@qq.com>
2025-09-22 15:07:46 +0800

    change ai-forge review selectors

--- common/ai/aid/aicommon/response.go
+++ common/ai/aid/aicommon/response.go
@@ -172,7 +172,6 @@ func (a *AIResponse) GetUnboundStreamReader(haveReason bool) io.Reader {
 }
 
 func (a *AIResponse) GetOutputStreamReader(nodeId string, system bool, emitter *Emitter) io.Reader {
-	system = false
 	pr, pw := utils.NewBufPipe(nil)
 	go func() {
 		cbBuffer := bytes.NewBuffer(make([]byte, 4096))
