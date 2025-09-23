dcec73551fcb45fbf4d6486d87f1e79628f4ca57
go0p <gp827782797@qq.com>
2025-09-23 15:53:15 +0800

    fix: add retry

--- common/utils/lowhttp/http2_serve_test.go
+++ common/utils/lowhttp/http2_serve_test.go
@@ -2,10 +2,14 @@ package lowhttp
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
 	"io"
 	"net"
 	"strings"
+	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/yaklang/yaklang/common/utils"
@@ -30,51 +34,75 @@ func TestH2_Serve(t *testing.T) {
 	defer lis.Close()
 
 	token1, token2 := utils.RandStringBytes(20), utils.RandStringBytes(200)
-	checkPass := false
-	go func() {
-		for {
-			conn, err := lis.Accept()
-			if err != nil {
-				return
-			}
-			go func() {
-				err = serveH2(conn, conn, withH2Handler(func(header []byte, body io.ReadCloser) ([]byte, io.ReadCloser, error) {
-					spew.Dump(header)
-					if strings.Contains(string(header), "GET /"+token1+" HTTP/2") {
-						checkPass = true
-					}
-					resp := []byte(`HTTP/2 200 OK
-Content-Type: text/plain
-Content-Length: 3
 
-abc`)
-					return resp, io.NopCloser(bytes.NewBufferString(token2)), nil
-				}))
-				if err != nil {
-					return
-				}
-			}()
+	var checkPass atomic.Bool
+	connCh := make(chan net.Conn, 1)
+	serveDone := make(chan error, 1)
+
+	go func() {
+		conn, acceptErr := lis.Accept()
+		if acceptErr != nil {
+			serveDone <- acceptErr
+			return
 		}
+		connCh <- conn
+		serveDone <- serveH2(conn, conn, withH2Handler(func(header []byte, body io.ReadCloser) ([]byte, io.ReadCloser, error) {
+			spew.Dump(header)
+
+			if strings.Contains(string(header), "GET /"+token1+" HTTP/2") {
+				checkPass.Store(true)
+			}
+			resp := []byte("HTTP/2 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc")
+			return resp, io.NopCloser(bytes.NewBufferString(token2)), nil
+		}))
 	}()
 
-	err = utils.WaitConnect(utils.HostPort("127.0.0.1", port), 5)
+	reqBytes := []byte(fmt.Sprintf("GET /%s HTTP/2\r\nHost: 127.0.0.1\r\n\r\nabc", token1))
+	rsp, err := HTTPWithoutRedirect(
+		WithHttps(false),
+		WithHttp2(true),
+		WithPacketBytes(reqBytes),
+		WithHost("127.0.0.1"),
+
+		WithPort(port),
+		WithRetryTimes(5),
+	)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatalf("http2 request failed: %v", err)
 	}
 
-	rsp, err := HTTPWithoutRedirect(WithHttps(false), WithHttp2(true), WithPacketBytes([]byte(`GET /`+token1+` HTTP/2
-Host: www.example.com
-
-abc`)), WithHost("127.0.0.1"), WithPort(port), WithRetryTimes(5))
-	if err != nil {
-		panic(err)
+	var conn net.Conn
+	select {
+	case conn = <-connCh:
+	case acceptErr := <-serveDone:
+		if acceptErr != nil {
+			t.Fatalf("accept failed: %v", acceptErr)
+		}
+	case <-time.After(3 * time.Second):
+		t.Fatal("timeout waiting for http2 connection")
 	}
-	spew.Dump(rsp.RawPacket)
-
-	if !checkPass {
+	t.Cleanup(func() {
+		if conn != nil {
+			_ = conn.Close()
+		}
+	})
+	if !checkPass.Load() {
 		t.Fatal("checkPass failed (h2 server cannot serve)")
 	}
 	if !bytes.Contains(rsp.RawPacket, []byte(token2)) {
 		t.Fatal("token2 not found in response")
+	}
+
+	if conn != nil {
+		_ = conn.Close()
+	}
+
+	select {
+	case serveErr := <-serveDone:
+		if serveErr != nil && !errors.Is(serveErr, net.ErrClosed) && !errors.Is(serveErr, io.EOF) && !strings.Contains(serveErr.Error(), "use of closed network connection") {
+			t.Fatalf("serveH2 returned error: %v", serveErr)
+		}
+	case <-time.After(5 * time.Second):
+		t.Fatal("serveH2 did not exit after closing connection")
 	}
 }
