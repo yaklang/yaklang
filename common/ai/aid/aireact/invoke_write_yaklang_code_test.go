9fa6862e941c4ea93ff3d448d703f1145648a931
v1ll4n <v1ll4n@qq.com>
2025-09-24 15:36:53 +0800

    add yaklang basic editor

--- common/ai/aid/aireact/invoke_write_yaklang_code_test.go
+++ common/ai/aid/aireact/invoke_write_yaklang_code_test.go
@@ -3,13 +3,11 @@ package aireact
 import (
 	"bytes"
 	"fmt"
-	"strings"
 	"testing"
 	"time"
 
 	"github.com/segmentio/ksuid"
 	"github.com/yaklang/yaklang/common/ai/aid/aicommon"
-	"github.com/yaklang/yaklang/common/jsonpath"
 	"github.com/yaklang/yaklang/common/schema"
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
@@ -36,7 +34,7 @@ func mockedYaklangWriting(i aicommon.AICallerConfigIf, req *aicommon.AIRequest, 
 
 	if utils.MatchAllOfSubString(prompt, `"query_document"`, `"require_tool"`, `"write_code"`, `"@action"`) {
 		rsp := i.NewAIResponse()
-		rsp.EmitOutputStream(bytes.NewBufferString(`{"@action": "write_code", "code": "println(123)"}`))
+		rsp.EmitOutputStream(bytes.NewBufferString(`{"@action": "write_code", "code": "println(a)"}`))
 		rsp.Close()
 		return rsp, nil
 	}
@@ -80,41 +78,13 @@ func TestReAct_WriteYaklangCode(t *testing.T) {
 	}
 	after := time.After(du * time.Second)
 
-	reviewed := false
-	reviewReleased := false
-	toolCallOutputEvent := false
-	var iid string
 LOOP:
 	for {
 		select {
 		case e := <-out:
 			fmt.Println(e.String())
-			if e.Type == string(schema.EVENT_TYPE_TOOL_USE_REVIEW_REQUIRE) {
-				reviewed = true
-				iid = utils.InterfaceToString(jsonpath.FindFirst(string(e.Content), "$.id"))
-				in <- &ypb.AIInputEvent{
-					IsInteractiveMessage: true,
-					InteractiveId:        utils.InterfaceToString(iid),
-					InteractiveJSONInput: `{"suggestion": "continue"}`,
-				}
-			}
-
-			if e.Type == string(schema.EVENT_TYPE_REVIEW_RELEASE) {
-				gotId := utils.InterfaceToString(jsonpath.FindFirst(string(e.Content), "$.id"))
-				if gotId == iid {
-					reviewReleased = true
-				}
-			}
-
-			if e.Type == string(schema.EVENT_TOOL_CALL_DONE) {
-				toolCallOutputEvent = true
-			}
-
-			if e.NodeId == "react_task_status_changed" {
-				result := jsonpath.FindFirst(e.GetContent(), "$..react_task_now_status")
-				if utils.InterfaceToString(result) == "completed" {
-					break LOOP
-				}
+			if e.Type == string(schema.EVENT_TYPE_YAKLANG_CODE_EDITOR) {
+				break LOOP
 			}
 		case <-after:
 			break LOOP
@@ -122,29 +92,11 @@ LOOP:
 	}
 	close(in)
 
-	if !reviewed {
-		t.Fatal("Expected to have at least one review event, but got none")
-	}
-
-	if !reviewReleased {
-		t.Fatal("Expected to have at least one review release event, but got none")
-	}
-
-	if !toolCallOutputEvent {
-		t.Fatal("Expected to have at least one output event, but got none")
-	}
-
 	fmt.Println("--------------------------------------")
 	tl := ins.DumpTimeline()
 	fmt.Println(tl)
-	if !strings.Contains(tl, `mocked thought for tool calling`) {
-		t.Fatal("timeline does not contain mocked thought")
-	}
-	if !utils.MatchAllOfSubString(tl, `system-question`, "user-answer", "when review") {
-		t.Fatal("timeline does not contain system-question")
-	}
-	if !utils.MatchAllOfSubString(tl, `ReAct iteration 1`, `ReAct loop finished END[1]`) {
-		t.Fatal("timeline does not contain ReAct iteration")
+	if !utils.MatchAllOfSubString(tl, "mocked thought for write-yaklang", "println(a)") {
+		t.Fatal("timeline not match")
 	}
 	fmt.Println("--------------------------------------")
 }
