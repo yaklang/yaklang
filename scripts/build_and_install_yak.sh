ca98cd8ff703e08e263ff1ab653876037b4d049c
wlz <lingze.wl@gmail.com>
2025-09-24 14:41:47 +0800

    script: add time to ssatest.sh

--- scripts/build_and_install_yak.sh
+++ scripts/build_and_install_yak.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-go build -ldflags "-X 'main.goVersion=$(go version)' -X 'main.gitHash=$(git show -s --format=%H)' -X 'main.buildTime=$(git show -s --format=%cd)' -X 'main.yakVersion=$(git describe)'" -o yak common/yak/cmd/yak.go && sudo mv ./yak /usr/local/bin
+go build -ldflags "-X 'main.goVersion=$(go version)' -X 'main.gitHash=$(git show -s --format=%H)' -X 'main.buildTime=$(git show -s --format=%cd)' -X 'main.yakVersion=$(git describe --tag)'" -o yak common/yak/cmd/yak.go && sudo mv ./yak /usr/local/bin
