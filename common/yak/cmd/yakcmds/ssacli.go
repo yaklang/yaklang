4d0dafbfb8e019064769e5c261d0b619d5691e5e
intSheep <intSheep@qq.com>
2025-09-25 17:44:15 +0800

    refactor(sfscan):use ssa config as sfscan config instead of ypb.ScanInput

--- common/yak/cmd/yakcmds/ssacli.go
+++ common/yak/cmd/yakcmds/ssacli.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 
 	"github.com/yaklang/yaklang/common/yak/syntaxflow_scan"
+	sfscan "github.com/yaklang/yaklang/common/yak/syntaxflow_scan"
 
 	"github.com/gobwas/glob"
 	"github.com/jinzhu/gorm"
@@ -31,6 +32,7 @@ import (
 	"github.com/yaklang/yaklang/common/syntaxflow/sfdb"
 	"github.com/yaklang/yaklang/common/utils/bizhelper"
 	"github.com/yaklang/yaklang/common/yak/ssaapi/sfreport"
+	"github.com/yaklang/yaklang/common/yak/ssaapi/ssaconfig"
 	"golang.org/x/exp/slices"
 
 	"github.com/segmentio/ksuid"
@@ -1347,17 +1349,22 @@ var ssaCodeScan = &cli.Command{
 			opt = append(opt, sfreport.WithDataflowPath(true))
 		}
 		reportInstance, err := sfreport.ConvertSyntaxFlowResultToReport(config.Format, opt...)
+
+		scanConfig, err := ssaconfig.NewSyntaxFlowScanConfig(
+			ssaconfig.WithProgramNames(prog.GetProgramName()),
+			ssaconfig.WithRuleFilter(ruleFilter),
+			ssaconfig.WithScanMemory(c.Bool("memory")),
+		)
 		if err != nil {
-			log.Errorf("create report instance failed: %s", err)
+			log.Errorf("create ssaconfig failed: %s", err)
 			return err
 		}
-		err = syntaxflow_scan.StartScan(
+
+		_, err = sfscan.StartScan(
 			ctx,
-			syntaxflow_scan.WithProgramNames(prog.GetProgramName()),
-			syntaxflow_scan.WithRuleFilter(ruleFilter),
-			syntaxflow_scan.WithMemory(c.Bool("memory")),
-			syntaxflow_scan.WithReporter(reportInstance),
-			syntaxflow_scan.WithReporterWriter(config.OutputWriter),
+			sfscan.WithSyntaxFlowScanConfig(scanConfig),
+			sfscan.WithSyntaxFlowScanReporter(reportInstance),
+			sfscan.WithSyntaxFlowScanReporterWriter(config.OutputWriter),
 			syntaxflow_scan.WithProcessCallback(func(taskID, status string, progress float64, info *syntaxflow_scan.RuleProcessInfoList) {
 				log.Infof("task %s status: %s, progress: %.2f%%", taskID, status, progress*100)
 				if info == nil || len(info.Rules) == 0 {
