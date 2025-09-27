b4fa9a657a7231f1415dc553794a73d9d6046a9a
VillanCh <188228725@qq.com>
2025-09-27 10:20:33 +0800

    improve timeline

--- scripts/test-ai.sh
+++ scripts/test-ai.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+set -e
+set -o pipefail
+
 go test -v -timeout 3m ./common/ai/aid/...  | grep -E "(PASS|FAIL|RUN|=== RUN|--- PASS|--- FAIL|TestTemplate|panic:|goroutine.*\[(running|sleep)\]|test timed out|testing\..*panic|recovered)"
 go test -v -timeout 60s ./common/ai/tests/...  | grep -E "(PASS|FAIL|RUN|=== RUN|--- PASS|--- FAIL|TestTemplate|panic:|goroutine.*\[(running|sleep)\]|test timed out|testing\..*panic|recovered)"
 go test -v -timeout 60s ./common/ai/rag/pq/... | grep -E "(PASS|FAIL|RUN|=== RUN|--- PASS|--- FAIL|TestTemplate|panic:|goroutine.*\[(running|sleep)\]|test timed out|testing\..*panic|recovered)"
