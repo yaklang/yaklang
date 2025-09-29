d97c8b414685db2fbee9de7e51f127b95ba10611
v1ll4n <v1ll4n@qq.com>
2025-09-29 18:00:53 +0800

    improve testcase for persistent session

---
+++ common/ai/aid/aireact/re-act_persistence_test.go
@@ -0,0 +1,170 @@
+package aireact
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/segmentio/ksuid"
+	"github.com/yaklang/yaklang/common/ai/aid/aicommon"
+	"github.com/yaklang/yaklang/common/ai/aid/aitool"
+	"github.com/yaklang/yaklang/common/jsonpath"
+	"github.com/yaklang/yaklang/common/schema"
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+	"io"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestReAct_PersistentSession_ToolUse(t *testing.T) {
+	flag := ksuid.New().String()
+	_ = flag
+	in := make(chan *ypb.AIInputEvent, 10)
+	out := make(chan *ypb.AIOutputEvent, 10)
+
+	toolCalled := false
+	sleepTool, err := aitool.New(
+		"sleep",
+		aitool.WithNumberParam("seconds"),
+		aitool.WithSimpleCallback(func(params aitool.InvokeParams, stdout io.Writer, stderr io.Writer) (any, error) {
+			toolCalled = true
+			sleepInt := params.GetFloat("seconds", 0.3)
+			if sleepInt <= 0 {
+				sleepInt = 0.3
+			}
+			time.Sleep(time.Duration(sleepInt) * time.Second)
+			return "done", nil
+		}),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pid := uuid.New()
+	ins, err := NewReAct(
+		WithAICallback(func(i aicommon.AICallerConfigIf, r *aicommon.AIRequest) (*aicommon.AIResponse, error) {
+			return mockedToolCalling(i, r, "sleep")
+		}),
+		WithEventInputChan(in),
+		WithEventHandler(func(e *schema.AiOutputEvent) {
+			out <- e.ToGRPC()
+		}),
+		WithTools(sleepTool),
+		WithPersistentSessionId(pid.String()),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_ = ins
+	go func() {
+		for i := 0; i < 1; i++ {
+			in <- &ypb.AIInputEvent{
+				IsFreeInput: true,
+				FreeInput:   "abc",
+			}
+		}
+	}()
+
+	du := time.Duration(50)
+	if utils.InGithubActions() {
+		du = time.Duration(5)
+	}
+	after := time.After(du * time.Second)
+
+	reviewed := false
+	reviewReleased := false
+	toolCallOutputEvent := false
+	var iid string
+LOOP:
+	for {
+		select {
+		case e := <-out:
+			fmt.Println(e.String())
+			if e.Type == string(schema.EVENT_TYPE_TOOL_USE_REVIEW_REQUIRE) {
+				reviewed = true
+				iid = utils.InterfaceToString(jsonpath.FindFirst(string(e.Content), "$.id"))
+				in <- &ypb.AIInputEvent{
+					IsInteractiveMessage: true,
+					InteractiveId:        utils.InterfaceToString(iid),
+					InteractiveJSONInput: `{"suggestion": "continue"}`,
+				}
+			}
+
+			if e.Type == string(schema.EVENT_TYPE_REVIEW_RELEASE) {
+				gotId := utils.InterfaceToString(jsonpath.FindFirst(string(e.Content), "$.id"))
+				if gotId == iid {
+					reviewReleased = true
+				}
+			}
+
+			if e.Type == string(schema.EVENT_TOOL_CALL_DONE) {
+				toolCallOutputEvent = true
+			}
+
+			if e.NodeId == "react_task_status_changed" {
+				result := jsonpath.FindFirst(e.GetContent(), "$..react_task_now_status")
+				if utils.InterfaceToString(result) == "completed" {
+					break LOOP
+				}
+			}
+		case <-after:
+			break LOOP
+		}
+	}
+	close(in)
+
+	if !reviewed {
+		t.Fatal("Expected to have at least one review event, but got none")
+	}
+
+	if !reviewReleased {
+		t.Fatal("Expected to have at least one review release event, but got none")
+	}
+
+	if !toolCalled {
+		t.Fatal("Tool was not called")
+	}
+
+	if !toolCallOutputEvent {
+		t.Fatal("Expected to have at least one output event, but got none")
+	}
+
+	fmt.Println("--------------------------------------")
+	tl := ins.DumpTimeline()
+	if !strings.Contains(tl, `mocked thought for tool calling`) {
+		t.Fatal("timeline does not contain mocked thought")
+	}
+	if !utils.MatchAllOfSubString(tl, `system-question`, "user-answer", "when review") {
+		t.Fatal("timeline does not contain system-question")
+	}
+	if !utils.MatchAllOfSubString(tl, `ReAct iteration 1`, `ReAct loop finished END[1]`) {
+		t.Fatal("timeline does not contain ReAct iteration")
+	}
+	fmt.Println("--------------------------------------")
+
+	persistentTimeline, err := NewReAct(
+		WithAICallback(func(i aicommon.AICallerConfigIf, r *aicommon.AIRequest) (*aicommon.AIResponse, error) {
+			return mockedToolCalling(i, r, "sleep")
+		}),
+		WithEventInputChan(in),
+		WithEventHandler(func(e *schema.AiOutputEvent) {
+			out <- e.ToGRPC()
+		}),
+		WithTools(sleepTool),
+		WithPersistentSessionId(pid.String()),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	restoredTimeline := persistentTimeline.DumpTimeline()
+	fmt.Println(restoredTimeline)
+	if !strings.Contains(restoredTimeline, `mocked thought for tool calling`) {
+		t.Fatal("timeline does not contain mocked thought")
+	}
+	if !utils.MatchAllOfSubString(restoredTimeline, `system-question`, "user-answer", "when review") {
+		t.Fatal("timeline does not contain system-question")
+	}
+	if !utils.MatchAllOfSubString(restoredTimeline, `ReAct iteration 1`, `ReAct loop finished END[1]`) {
+		t.Fatal("timeline does not contain ReAct iteration")
+	}
+}
