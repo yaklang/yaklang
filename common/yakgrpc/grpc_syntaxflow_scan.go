fb1ea7f008740877b158ab88a6d93d029161c8f4
wlz <lingze.wl@gmail.com>
2025-09-29 11:55:13 +0800

    test(sfscan):fixup test case

--- common/yakgrpc/grpc_syntaxflow_scan.go
+++ common/yakgrpc/grpc_syntaxflow_scan.go
@@ -2,7 +2,6 @@ package yakgrpc
 
 import (
 	"fmt"
-	"sync"
 	"sync/atomic"
 
 	"github.com/yaklang/yaklang/common/yak/syntaxflow_scan"
@@ -32,18 +31,12 @@ func (s *Server) SyntaxFlowScan(stream ypb.Yak_SyntaxFlowScanServer) error {
 		}
 	}()
 
-	lock := sync.RWMutex{}
-	var taskID string
-	var status string
-
-	sendExecResult := func(execResult *ypb.ExecResult) error {
-		lock.RLock()
+	sendExecResult := func(taskId, status string, execResult *ypb.ExecResult) error {
 		ret := &ypb.SyntaxFlowScanResponse{
-			TaskID:     taskID,
+			TaskID:     taskId,
 			Status:     status,
 			ExecResult: execResult,
 		}
-		lock.RUnlock()
 
 		return stream.Send(ret)
 	}
@@ -54,30 +47,19 @@ func (s *Server) SyntaxFlowScan(stream ypb.Yak_SyntaxFlowScanServer) error {
 			return pause.Load()
 		}),
 		syntaxflow_scan.WithProcessCallback(func(tid, s string, progress float64, info *syntaxflow_scan.RuleProcessInfoList) {
-
-			lock.Lock()
-			taskID = tid
-			status = s
-			lock.Unlock()
-
 			// update rule info
-			sendExecResult(yaklib.NewYakitLogExecResult("code", info)) // 发送 rules info
+			sendExecResult(tid, s, yaklib.NewYakitLogExecResult("code", info)) // 发送 rules info
 
 			// update progress
-			sendExecResult(yaklib.NewYakitProgressExecResult("main", progress))
+			sendExecResult(tid, s, yaklib.NewYakitProgressExecResult("main", progress))
 			// status card
-			sendExecResult(yaklib.NewYakitStatusCardExecResult("已执行规则", fmt.Sprintf("%d/%d", info.FinishedQuery, info.TotalQuery), "规则执行状态"))
-			sendExecResult(yaklib.NewYakitStatusCardExecResult("已跳过规则", info.SkippedQuery, "规则执行状态"))
-			sendExecResult(yaklib.NewYakitStatusCardExecResult("执行成功个数", info.SuccessQuery, "规则执行状态"))
-			sendExecResult(yaklib.NewYakitStatusCardExecResult("执行失败个数", info.FailedQuery, "规则执行状态"))
-			sendExecResult(yaklib.NewYakitStatusCardExecResult("检出漏洞/风险个数", info.RiskCount, "漏洞/风险状态"))
+			sendExecResult(tid, s, yaklib.NewYakitStatusCardExecResult("已执行规则", fmt.Sprintf("%d/%d", info.FinishedQuery, info.TotalQuery), "规则执行状态"))
+			sendExecResult(tid, s, yaklib.NewYakitStatusCardExecResult("已跳过规则", info.SkippedQuery, "规则执行状态"))
+			sendExecResult(tid, s, yaklib.NewYakitStatusCardExecResult("执行成功个数", info.SuccessQuery, "规则执行状态"))
+			sendExecResult(tid, s, yaklib.NewYakitStatusCardExecResult("执行失败个数", info.FailedQuery, "规则执行状态"))
+			sendExecResult(tid, s, yaklib.NewYakitStatusCardExecResult("检出漏洞/风险个数", info.RiskCount, "漏洞/风险状态"))
 		}),
 		syntaxflow_scan.WithScanResultCallback(func(sr *syntaxflow_scan.ScanResult) {
-			lock.Lock()
-			taskID = sr.TaskID
-			status = sr.Status
-			lock.Unlock()
-
 			// 发送扫描结果
 			stream.Send(&ypb.SyntaxFlowScanResponse{
 				TaskID:   sr.TaskID,
@@ -86,9 +68,9 @@ func (s *Server) SyntaxFlowScan(stream ypb.Yak_SyntaxFlowScanServer) error {
 				SSARisks: sr.Result.GetGRPCModelRisk(),
 			})
 		}),
-		syntaxflow_scan.WithErrorCallback(func(format string, args ...any) {
+		syntaxflow_scan.WithErrorCallback(func(taskid, status, format string, args ...any) {
 			// 发送错误信息
-			sendExecResult(yaklib.NewYakitLogExecResult("error", format, args...))
+			sendExecResult(taskid, status, yaklib.NewYakitLogExecResult("error", format, args...))
 		}),
 	)
 	return err
