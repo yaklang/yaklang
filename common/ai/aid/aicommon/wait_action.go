f2d6b8310f35e1abe8cc62408943fddda785c391
rookie <rookie_is@qq.com>
2025-09-30 13:57:24 +0800

    fix(waitable_action):add sync mux

--- common/ai/aid/aicommon/wait_action.go
+++ common/ai/aid/aicommon/wait_action.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/yaklang/yaklang/common/ai/aid/aitool"
 	"github.com/yaklang/yaklang/common/utils"
+	"sync"
 )
 
 type WaitableAction struct {
@@ -11,6 +12,7 @@ type WaitableAction struct {
 	name    string
 	params  aitool.InvokeParams
 	barrier *utils.CondBarrier
+	mu      sync.Mutex
 }
 
 func NewWaitableAction(ctx context.Context, name string) *WaitableAction {
@@ -19,11 +21,14 @@ func NewWaitableAction(ctx context.Context, name string) *WaitableAction {
 		name:    name,
 		params:  make(aitool.InvokeParams),
 		barrier: utils.NewCondBarrierContext(ctx),
+		mu:      sync.Mutex{},
 	}
 }
 
 func (w *WaitableAction) Set(key string, value interface{}) {
+	w.mu.Lock()
 	w.params[key] = value
+	w.mu.Unlock()
 	w.barrier.CreateBarrier(key).Done()
 }
 
@@ -40,7 +45,10 @@ func (w *WaitableAction) WaitAnyToString(key string) string {
 	if err != nil {
 		return ""
 	}
-	return w.params.GetAnyToString(key)
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	val := w.params.GetAnyToString(key)
+	return val
 }
 
 func (w *WaitableAction) WaitObject(key string) aitool.InvokeParams {
@@ -48,7 +56,10 @@ func (w *WaitableAction) WaitObject(key string) aitool.InvokeParams {
 	if err != nil {
 		return nil
 	}
-	return w.params.GetObject(key)
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	val := w.params.GetObject(key)
+	return val
 }
 
 func (w *WaitableAction) WaitInt(key string) int64 {
@@ -56,7 +67,10 @@ func (w *WaitableAction) WaitInt(key string) int64 {
 	if err != nil {
 		return 0
 	}
-	return w.params.GetInt(key)
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	val := w.params.GetInt(key)
+	return val
 }
 
 func (w *WaitableAction) WaitBool(key string) bool {
@@ -64,7 +78,10 @@ func (w *WaitableAction) WaitBool(key string) bool {
 	if err != nil {
 		return false
 	}
-	return w.params.GetBool(key)
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	val := w.params.GetBool(key)
+	return val
 }
 
 func (w *WaitableAction) WaitString(key string) string {
@@ -72,7 +89,10 @@ func (w *WaitableAction) WaitString(key string) string {
 	if err != nil {
 		return ""
 	}
-	return w.params.GetString(key)
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	val := w.params.GetString(key)
+	return val
 }
 
 func (w *WaitableAction) WaitObjectArray(key string) []aitool.InvokeParams {
@@ -80,7 +100,10 @@ func (w *WaitableAction) WaitObjectArray(key string) []aitool.InvokeParams {
 	if err != nil {
 		return nil
 	}
-	return w.params.GetObjectArray(key)
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	val := w.params.GetObjectArray(key)
+	return val
 }
 
 func (w *WaitableAction) WaitStringSlice(key string) []string {
@@ -88,7 +111,10 @@ func (w *WaitableAction) WaitStringSlice(key string) []string {
 	if err != nil {
 		return nil
 	}
-	return w.params.GetStringSlice(key)
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	val := w.params.GetStringSlice(key)
+	return val
 }
 
 func (w *WaitableAction) WaitFloat(key string) float64 {
@@ -96,5 +122,8 @@ func (w *WaitableAction) WaitFloat(key string) float64 {
 	if err != nil {
 		return 0
 	}
-	return w.params.GetFloat(key)
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	val := w.params.GetFloat(key)
+	return val
 }
