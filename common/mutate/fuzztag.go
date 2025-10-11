e7073b7e627237beedb6999c4c5bee36f031d076
z3 <47558664+OrangeWatermelon@users.noreply.github.com>
2025-10-11 15:13:05 +0800

    fix repeat tag description (#3359)

--- common/mutate/fuzztag.go
+++ common/mutate/fuzztag.go
@@ -679,8 +679,8 @@ func init() {
 			}
 			return nil
 		},
-		Description:         "重复一个字符串或者一个次数，例如：`{{repeat(abc|3)}}`，结果为：abcabcabc，或者`{{repeat(3)}}`，结果是重复三次",
-		TagNameVerbose:      "重复生成字符串",
+		Description:         "生成字符串数组，例如：`{{repeat(abc|3)}}` 会产生 `['abc','abc','abc']`；若写成 `{{repeat(3)}}` 则产生 `['','','']`。在 WebFuzzer 中，可通过 `{{repeat(n)}}` 指定重复发送 n 个包。",
+		TagNameVerbose:      "生成字符串数组",
 		ArgumentDescription: "{{optional(string_split(abc:字符串))}}{{number(3:重复次数)}}",
 	})
 
