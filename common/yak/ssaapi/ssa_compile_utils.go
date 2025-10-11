5ff35239de614be09a268ee0ae6870a390851aa5
wlz <lingze.wl@gmail.com>
2025-10-11 18:03:32 +0800

    refactor(ssaapi): update handler signature to use ASTSequenceType for orderType

--- common/yak/ssaapi/ssa_compile_utils.go
+++ common/yak/ssaapi/ssa_compile_utils.go
@@ -7,6 +7,7 @@ import (
 	"github.com/yaklang/yaklang/common/utils"
 	"github.com/yaklang/yaklang/common/utils/filesys/filesys_interface"
 	"github.com/yaklang/yaklang/common/yak/ssa"
+	"github.com/yaklang/yaklang/common/yak/ssa/ssaprofile"
 	"github.com/yaklang/yaklang/common/yak/ssaapi/ssareducer"
 )
 
@@ -19,38 +20,43 @@ func (c *config) getFileHandler(
 	filesystem filesys_interface.FileSystem,
 	preHandlerFiles []string,
 	handlerFilesMap map[string]struct{},
-	concurrency int,
 ) <-chan *ssareducer.FileContent {
-	return ssareducer.FilesHandler(
-		c.ctx, filesystem, preHandlerFiles,
-		func(path string, content []byte) (ssa.FrontAST, error) {
-			start := time.Now()
-			defer func() {
-				c.Processf(0, "pre-handler parse ast: %s, cost: %v", path, time.Since(start))
-			}()
+	parse := func(path string, content []byte) (ssa.FrontAST, error) {
+		start := time.Now()
+		defer func() {
+			log.Infof("pre-handler cost:%v parse ast: %s", time.Since(start), path)
+		}()
 
-			defer func() {
-				if r := recover(); r != nil {
-					log.Errorf("pre-handler parse [%s] error %v  ", path, r)
-					utils.PrintCurrentGoroutineRuntimeStack()
-				}
-			}()
-			if _, needBuild := handlerFilesMap[path]; !needBuild {
-				// don't need parse ast
+		defer func() {
+			if r := recover(); r != nil {
+				log.Errorf("pre-handler parse [%s] error %v  ", path, r)
+				utils.PrintCurrentGoroutineRuntimeStack()
+			}
+		}()
+		if _, needBuild := handlerFilesMap[path]; !needBuild {
+			// don't need parse ast
+			return nil, nil
+		}
+
+		if language := c.LanguageBuilder; language != nil {
+			if language.FilterParseAST(path) {
+				var ast ssa.FrontAST
+				var err error
+				ssaprofile.ProfileAdd(true, "ParseAST ", func() {
+					ast, err = language.ParseAST(utils.UnsafeBytesToString(content))
+				})
+				return ast, err
+			} else {
+				log.Debugf("skip parse ast file: %s", path)
 				return nil, nil
 			}
-
-			if language := c.LanguageBuilder; language != nil {
-				if language.FilterParseAST(path) {
-					return language.ParseAST(utils.UnsafeBytesToString(content))
-				} else {
-					log.Debugf("skip parse ast file: %s", path)
-					return nil, nil
-				}
-			}
-			return nil, utils.Errorf("not select language %s", c.language)
-		},
-		int(c.astSequence),
-		concurrency,
+		}
+		return nil, utils.Errorf("not select language %s", c.language)
+	}
+	return ssareducer.FilesHandler(
+		c.ctx, filesystem, preHandlerFiles,
+		parse,
+		c.astSequence,
+		c.concurrency,
 	)
 }
