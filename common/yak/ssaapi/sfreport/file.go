aff8dd68a14b4fd15dc97e7e4bab76e1963bffb0
intSheep <intSheep@qq.com>
2025-10-17 16:31:16 +0800

    fix(sfreport):fix sf file import and add test

--- common/yak/ssaapi/sfreport/file.go
+++ common/yak/ssaapi/sfreport/file.go
@@ -3,6 +3,7 @@ package sfreport
 import (
 	"fmt"
 	"slices"
+	"strings"
 
 	"github.com/jinzhu/gorm"
 	"github.com/yaklang/yaklang/common/utils"
@@ -21,33 +22,17 @@ type File struct {
 }
 
 func NewFile(editor *memedit.MemEditor, r *Report) *File {
-	ret := &File{
-		Path:      editor.GetUrl(),
-		Length:    int64(editor.GetLength()),
-		LineCount: editor.GetLineCount(),
-		Hash: map[string]string{
-			"md5":    editor.SourceCodeMd5(),
-			"sha1":   editor.SourceCodeSha1(),
-			"sha256": editor.SourceCodeSha256(),
-		},
-	}
-
-	if r.ReportType == IRifyFullReportType || r.config.showFileContent {
-		ret.Content = editor.GetSourceCode()
-	} else {
-		// only show the first 100 characters
-		ret.Content = fmt.Sprintf("%s...", editor.GetSourceCode(100))
-	}
-
-	return ret
+	return editor2File(editor, r.ReportType == IRifyFullReportType || r.config.showFileContent)
 }
 
 func (f *File) SaveToDB(db *gorm.DB, programName string) error {
 	if db == nil {
 		return utils.Error("Save File to DB failed: db is nil")
 	}
-	editor := memedit.NewMemEditorWithFileUrl(f.Content, f.Path)
-	editor.SetProgramName(programName)
+	editor, err := file2Editor(f, programName)
+	if err != nil {
+		return err
+	}
 	irSource := ssadb.MarshalFile(editor)
 	if err := irSource.Save(db); err != nil {
 		return utils.Wrapf(err, "Save File to DB failed")
@@ -60,4 +45,54 @@ func (f *File) AddRisk(risk *Risk) {
 		return
 	}
 	f.Risks = append(f.Risks, risk.GetHash())
+}
+
+func editor2File(editor *memedit.MemEditor, fullCode bool) *File {
+	ret := &File{
+		Path:      editor.GetUrl(),
+		Length:    int64(editor.GetLength()),
+		LineCount: editor.GetLineCount(),
+		Hash: map[string]string{
+			"md5":    editor.SourceCodeMd5(),
+			"sha1":   editor.SourceCodeSha1(),
+			"sha256": editor.SourceCodeSha256(),
+		},
+	}
+	if fullCode {
+		ret.Content = editor.GetSourceCode()
+	} else {
+		ret.Content = fmt.Sprintf("%s...", editor.GetSourceCode(100))
+	}
+	return ret
+}
+
+func file2Editor(file *File, programName string) (*memedit.MemEditor, error) {
+	if programName == "" {
+		return nil, utils.Error("file2Editor: programName cannot be empty")
+	}
+
+	editor := memedit.NewMemEditor(file.Content)
+	editor.SetUrl(file.Path)
+	editor.SetProgramName(programName)
+
+	if file.Path == "" {
+		return editor, nil
+	}
+
+	cleanPath := strings.TrimPrefix(file.Path, "/")
+	parts := strings.Split(cleanPath, "/")
+
+	// setFolderPath and fileName
+	if len(parts) >= 2 {
+		fileName := parts[len(parts)-1]
+		editor.SetFileName(fileName)
+
+		if len(parts) > 2 {
+			folderPath := "/" + strings.Join(parts[1:len(parts)-1], "/")
+			editor.SetFolderPath(folderPath)
+		} else {
+			editor.SetFolderPath("/")
+		}
+	}
+	return editor, nil
 }
