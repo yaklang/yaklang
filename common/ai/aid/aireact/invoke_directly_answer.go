f5fdbacedd82a124290fd875458c09d3d18997e0
VillanCh <188228725@qq.com>
2025-10-18 12:10:50 +0800

    improve directly answer

--- common/ai/aid/aireact/invoke_directly_answer.go
+++ common/ai/aid/aireact/invoke_directly_answer.go
@@ -3,15 +3,18 @@ package aireact
 import (
 	"bytes"
 	"io"
+	"sync"
 
 	"github.com/yaklang/yaklang/common/ai/aid/aicommon"
+	"github.com/yaklang/yaklang/common/ai/aid/aicommon/aitag"
 	"github.com/yaklang/yaklang/common/ai/aid/aitool"
 	"github.com/yaklang/yaklang/common/jsonextractor"
+	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/utils"
 )
 
 func (r *ReAct) DirectlyAnswer(query string, tools []*aitool.Tool) (string, error) {
-	prompt, err := r.promptManager.GenerateDirectlyAnswerPrompt(
+	prompt, nonceStr, err := r.promptManager.GenerateDirectlyAnswerPrompt(
 		query,
 		tools,
 	)
@@ -20,12 +23,38 @@ func (r *ReAct) DirectlyAnswer(query string, tools []*aitool.Tool) (string, erro
 	}
 
 	var finalResult string
+	var aiTagResult string
+	var wg = new(sync.WaitGroup)
 	err = aicommon.CallAITransaction(
 		r.config,
 		prompt,
 		r.config.CallAI,
 		func(rsp *aicommon.AIResponse) error {
 			stream := rsp.GetOutputStreamReader("directly_answer", true, r.Emitter)
+			wg.Add(1)
+			stream = utils.CreateUTF8StreamMirror(stream, func(reader io.Reader) {
+				defer func() {
+					wg.Done()
+				}()
+				err := aitag.Parse(
+					utils.UTF8Reader(reader),
+					aitag.WithCallback("FINAL_ANSWER", nonceStr, func(rd io.Reader) {
+						var out bytes.Buffer
+						r.Emitter.EmitTextMarkdownStreamEvent(
+							"re-act-loop-final-answer",
+							io.TeeReader(rd, &out),
+							rsp.GetTaskIndex(),
+							func() {
+								aiTagResult = out.String()
+								r.EmitResultAfterStream(out.String())
+							},
+						)
+					}))
+				if err != nil && err != io.EOF {
+					log.Warnf("DirectlyAnswer failed: %v", err)
+				}
+			})
+
 			action, err := aicommon.ExtractActionFromStreamWithJSONExtractOptions(
 				stream, "object", []string{}, []jsonextractor.CallbackOption{
 					jsonextractor.WithRegisterFieldStreamHandler(
@@ -56,5 +85,12 @@ func (r *ReAct) DirectlyAnswer(query string, tools []*aitool.Tool) (string, erro
 			return utils.Error("answer_payload is required but empty in action")
 		},
 	)
+	wg.Wait()
+	if aiTagResult != "" {
+		return aiTagResult, nil
+	}
+	if finalResult != "" {
+		return finalResult, nil
+	}
 	return finalResult, err
 }
