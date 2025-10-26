8ef7f8cb367e0d5e709e2f8bf06d3a39b5b5e5a3
VillanCh <188228725@qq.com>
2025-10-26 17:41:43 +0800

    fix timeline reducer bug

--- common/utils/filesys/fs.go
+++ common/utils/filesys/fs.go
@@ -269,7 +269,9 @@ func recursive(raw string, c Config, opts ...Option) (retErr error) {
 			targetFile := c.fileSystem.Join(path, d.Name())
 			if err := walkSingleFile(targetFile); err != nil {
 				lastErr = err
-				log.Warnf("walk file %s failed: %v", targetFile, err)
+				if !errors.Is(err, SkipDir) && !errors.Is(err, SkipAll) {
+					log.Warnf("walk file %s failed: %v", targetFile, err)
+				}
 				//return err
 			}
 			if c.isStop() {
