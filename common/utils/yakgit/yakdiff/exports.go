5f57c4ae5848273fc13b525cbf18b23b50414edc
v1ll4n <v1ll4n@qq.com>
2025-10-27 12:02:43 +0800

    add zip diff

--- common/utils/yakgit/yakdiff/exports.go
+++ common/utils/yakgit/yakdiff/exports.go
@@ -6,6 +6,32 @@ import (
 	"github.com/yaklang/yaklang/common/utils/filesys"
 )
 
+// DiffZIPFile compares two ZIP files and returns diff string or invokes the handler for each change
+// This is a high-level wrapper around FileSystemDiff for ZIP files
+func DiffZIPFile(zipFile1, zipFile2 string, handler ...DiffHandler) (string, error) {
+	// Check if files exist
+	if ok, _ := utils.PathExists(zipFile1); !ok {
+		return "", errors.Errorf("zip file %s not existed", zipFile1)
+	}
+	if ok, _ := utils.PathExists(zipFile2); !ok {
+		return "", errors.Errorf("zip file %s not existed", zipFile2)
+	}
+
+	// Create ZIP file systems
+	fs1, err := filesys.NewZipFSFromLocal(zipFile1)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to create zip fs from %s", zipFile1)
+	}
+
+	fs2, err := filesys.NewZipFSFromLocal(zipFile2)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to create zip fs from %s", zipFile2)
+	}
+
+	// Perform filesystem diff
+	return FileSystemDiff(fs1, fs2, handler...)
+}
+
 var Exports = map[string]any{
 	"Diff":               Diff,
 	"DiffFromFileSystem": FileSystemDiff,
@@ -18,4 +44,5 @@ var Exports = map[string]any{
 		}
 		return FileSystemDiff(filesys.NewRelLocalFs(i), filesys.NewRelLocalFs(j))
 	},
+	"DiffZIPFile": DiffZIPFile,
 }
