8d1956d1d399dd454d51c3ab207cbea08e9235f4
z3 <z3r0ne@163.com>
2025-10-27 11:10:41 +0800

    refactor: optimize database synchronization for aitool and aiforge

---
+++ common/utils/resources_monitor/resources_monitor.go
@@ -0,0 +1,40 @@
+package resources_monitor
+
+import (
+	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
+)
+
+type EmbedResourcesMonitor struct {
+	resourceID  string
+	buildInHash string
+}
+
+func NewEmbedResourcesMonitor(resourceID string, buildInHash string) *EmbedResourcesMonitor {
+	return &EmbedResourcesMonitor{
+		resourceID:  resourceID,
+		buildInHash: buildInHash,
+	}
+}
+
+func (m *EmbedResourcesMonitor) getCurrentHash(currentHashGetter func() string) string {
+	if consts.IsDevMode() {
+		return m.buildInHash
+	} else {
+		return currentHashGetter()
+	}
+}
+
+func (m *EmbedResourcesMonitor) MonitorModifiedWithAction(currentHashGetter func() string, callBack func() error) error {
+	var err error
+	resourceHash := m.getCurrentHash(currentHashGetter)
+	if resourceHash != yakit.Get(m.resourceID) {
+		err = callBack()
+	}
+	if err != nil {
+		return err
+	} else {
+		yakit.Set(m.resourceID, resourceHash)
+	}
+	return nil
+}
