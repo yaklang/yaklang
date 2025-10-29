26203a85a23764cdf8a39ed5d4120ec6d6fa7403
wlz <lingze.wl@gmail.com>
2025-10-29 16:45:13 +0800

    fixup(ssaproject):add projectName in db and filter

--- common/yakgrpc/yakit/ssa_program.go
+++ common/yakgrpc/yakit/ssa_program.go
@@ -22,6 +22,7 @@ func FilterSSAProgram(db *gorm.DB, filter *ypb.SSAProgramFilter) *gorm.DB {
 	db = bizhelper.ExactOrQueryStringArrayOr(db, "program_name", filter.GetProgramNames())
 	db = bizhelper.ExactOrQueryStringArrayOr(db, "language", filter.GetLanguages())
 	db = bizhelper.ExactQueryInt64ArrayOr(db, "id", filter.GetIds())
+	db = bizhelper.ExactOrQueryStringArrayOr(db, "project_name", filter.GetProjectNames())
 
 	if word := filter.GetKeyword(); word != "" {
 		db = bizhelper.FuzzSearchEx(db, []string{"program_name", "description"}, word, false)
