1ea731786ae1cb51c3d559149476d87a10fe816a
VillanCh <188228725@qq.com>
2025-11-09 20:48:16 +0800

    fix bug in testcase mustpass/

--- common/yak/yaktest/mustpass/z_debug_test.go
+++ common/yak/yaktest/mustpass/z_debug_test.go
@@ -14,7 +14,7 @@ func TestMustPassDebug(t *testing.T) {
 		return
 	}
 
-	debugName := "mitm_extra_incoming_conn_simple.yak"
+	debugName := "mitm_https_connect_assert.yak"
 	var debugCases [][]string
 	for k, v := range files {
 		if k == debugName {
