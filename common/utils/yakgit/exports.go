de703061fa684a050c1a95d0cee134f2e107a93a
wlz <lingze.wl@gmail.com>
2025-11-10 14:56:35 +0800

    feat(aitool): add GetEmbedFS function and corresponding test for static analysis

--- common/utils/yakgit/exports.go
+++ common/utils/yakgit/exports.go
@@ -44,6 +44,7 @@ var Exports = map[string]any{
 	"checkoutKeep":   WithCheckoutKeep,
 	"noFetchTags":    WithNoFetchTags,
 	"fetchAllTags":   WithFetchAllTags,
+	"withBranch":     WithBranch,
 
 	// inspect
 	"handleCommit":    WithHandleGitCommit,
