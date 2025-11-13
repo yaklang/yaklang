7653b91731d3371cb6e634889877061830bc7cd4
rookie <rookie_is@qq.com>
2025-11-13 17:18:03 +0800

    feat(ai-agent):add ai materials rpc

---
+++ common/utils/bizhelper/random.go
@@ -0,0 +1,28 @@
+package bizhelper
+
+import (
+	"github.com/jinzhu/gorm"
+	"math/rand"
+	"time"
+)
+
+func RandomQuery(db *gorm.DB, limit int, data interface{}) (int, error) {
+	var total int
+	if err := db.Count(&total).Error; err != nil {
+		return 0, err
+	}
+
+	if total < limit {
+		if err := db.Find(data).Error; err != nil {
+			return 0, err
+		}
+	}
+
+	randomSource := rand.New(rand.NewSource(time.Now().UnixNano()))
+	offset := randomSource.Intn(total - limit + 1) // 确保 offset + count <= total
+	if err := db.Offset(offset).Limit(limit).Find(data).Error; err != nil {
+		return 0, err
+	}
+
+	return total, nil
+}
