789893ebeac45d272c2473375b690eba36cec533
YWHKKX <2076064543@qq.com>
2025-11-13 12:51:10 +0800

    fix(ssa): fix member replace

--- common/yak/ssa/use.go
+++ common/yak/ssa/use.go
@@ -2,6 +2,7 @@ package ssa
 
 import (
 	"github.com/samber/lo"
+	"github.com/yaklang/yaklang/common/utils"
 	"golang.org/x/exp/slices"
 )
 
@@ -11,6 +12,9 @@ func filterNilValue(v Value) bool {
 }
 
 func ReplaceAllValue(v Value, to Value) {
+	if utils.IsNil(v) || utils.IsNil(to) || v == to {
+		return
+	}
 	ReplaceValue(v, to, func(i Instruction) bool { return false })
 }
 
@@ -26,6 +30,9 @@ func ReplaceValue(v Value, to Value, skip func(Instruction) bool) {
 
 	for _, variable := range v.GetAllVariables() {
 		// TODO: handler variable replace value
+		if prog := v.GetProgram(); prog != nil && prog.Cache != nil {
+			prog.Cache.RemoveVariable(variable.GetName(), v)
+		}
 		variable.Replace(v, to)
 		// variable = to
 		to.AddVariable(variable)
@@ -220,11 +227,17 @@ func (c *Call) ReplaceValue(v Value, to Value) {
 
 	lo.ForEach(c.ArgMember, func(id int64, index int) {
 		c.ArgMember[index] = to.GetId()
+<<<<<<< HEAD
 	})
 
 	binding, ok := c.Binding[v.GetName()]
 	if ok && binding == v.GetId() {
 		c.Binding[v.GetName()] = to.GetId()
+=======
+	} else {
+		panic("call not use this value")
+		// log.Warnf("call not use this value")
+>>>>>>> a50602101 (fix(ssa): fix member replace)
 	}
 }
 
