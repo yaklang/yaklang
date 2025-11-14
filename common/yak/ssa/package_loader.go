81c00810e44189aec34bb0ad7061b6bd7f31d20f
Matrix-Cain <zwh_china@outlook.com>
2025-11-14 18:09:21 +0800

    fix php include cache not set

--- common/yak/ssa/package_loader.go
+++ common/yak/ssa/package_loader.go
@@ -22,52 +22,73 @@ func (b *FunctionBuilder) BuildFilePackage(filename string, once bool) error {
 		b.Included = currentMode
 		p.Loader.SetIncludePaths(includePaths)
 	}()
+
+	log.Debugf("[INCLUDE] 开始处理 include: %s (once=%v)", filename, once)
+
 	_, editor, err := p.Loader.LoadFilePackage(filename, once)
 	if err != nil {
+		log.Debugf("[INCLUDE] 加载文件失败: %s, error: %v", filename, err)
 		return err
 	}
 	mainProgram := b.GetProgram().GetApplication()
 	editor = mainProgram.CreateEditor([]byte(editor.GetSourceCode()), filename)
-	subProg, exist := mainProgram.UpStream.Get(editor.GetPureSourceHash())
+	fileHash := editor.GetPureSourceHash()
+
+	log.Debugf("[INCLUDE] 文件: %s, hash: %s", filename, fileHash)
+
+	subProg, exist := mainProgram.UpStream.Get(fileHash)
 	if exist {
+		log.Debugf("[INCLUDE] ✓ 缓存命中! 文件: %s (hash: %s) 从缓存读取，跳过编译", filename, fileHash)
 		subProg.LazyBuild()
 		b.includeStack.Push(subProg)
 		return nil
 	}
+
+	log.Debugf("[INCLUDE] ✗ 缓存未命中! 文件: %s (hash: %s) 需要重新编译", filename, fileHash)
 	//针对yak这种，不经过lazyBuild，就需要进行特殊处理，如果我们没有拿到，但是符合后缀，我们就
 	languageConfig := mainProgram.config
 	if !languageConfig.ShouldBuild(filename) {
+		log.Debugf("[INCLUDE] 跳过构建: %s (不符合构建条件)", filename)
 		return nil
 	}
-	// todo: modify config parse init build，fix main/@main
-	// 目前代码仅仅为yak实现，因为yak不经过lazy build，并且yak的include过于特殊
-	// program := mainProgram.createSubProgram(editor.GetPureSourceHash(), Library)
-	program := mainProgram
+
+	log.Debugf("[INCLUDE] >>> 开始编译: %s <<<", filename)
+
+	// 创建子程序用于存储include文件的编译结果
+	program := mainProgram.GetSubProgram(fileHash)
 	builder := program.GetAndCreateFunctionBuilder("", string(MainFunctionName))
-	// 模拟编译，编译两次
-	origin := program.PreHandler()
-	defer program.SetPreHandler(origin)
-	program.SetPreHandler(true)
+
 	languageBuilder := languageConfig.LanguageBuilder
 	if languageBuilder == nil {
 		log.Errorf("language builder is nil")
 		return nil
 	}
+
+	// 解析AST
 	ast, err := languageBuilder.ParseAST(editor.GetSourceCode(), nil)
 	if err != nil {
+		log.Debugf("[INCLUDE] 解析AST失败: %s, error: %v", filename, err)
 		return utils.Errorf("parse file %s error: %v", filename, err)
 	}
-	// mainProgram.SetEditor(filename, editor)
-	// program.cre
-	languageBuilder.PreHandlerFile(ast, editor, builder)
-	program.SetPreHandler(false)
+
+	// 直接使用mainProgram.Build编译，它会自动处理PreHandler和正式Build
 	err = mainProgram.Build(ast, editor, builder)
 	if err != nil {
+		log.Debugf("[INCLUDE] 构建失败: %s, error: %v", filename, err)
 		return err
 	}
+
 	program.LazyBuild()
 	builder.Finish()
 	b.includeStack.Push(program)
+
+	log.Debugf("[INCLUDE] <<< 编译完成: %s >>>", filename)
+
+	// 修复：将子程序的编译结果存入缓存
+	log.Debugf("[INCLUDE] 将文件 %s 的编译结果存入缓存 (hash: %s)", filename, fileHash)
+	mainProgram.UpStream.Set(fileHash, program) // program是子程序，不是mainProgram
+	log.Debugf("[INCLUDE] ✓ 缓存已更新! 下次include %s 将直接使用缓存", filename)
+
 	return nil
 }
 
