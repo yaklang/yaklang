1e6b8d05239080a5c671681a9255747c828bceb5
VillanCh <188228725@qq.com>
2025-11-16 23:03:03 +0800

    remove pimatrix testcase in aiforge

--- common/aiforge/aibp/pimatrix_test.go
+++ common/aiforge/aibp/pimatrix_test.go
@@ -1,34 +1,28 @@
 package aibp
 
 import (
-	"context"
-	"github.com/yaklang/yaklang/common/ai/aid/aicommon"
 	"testing"
 
-	"github.com/davecgh/go-spew/spew"
-
-	"github.com/yaklang/yaklang/common/aiforge"
+	"github.com/stretchr/testify/assert"
 	"github.com/yaklang/yaklang/common/utils"
-	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
-	"gotest.tools/v3/assert"
 )
 
-func TestPIMatrixQuick(t *testing.T) {
-	result, err := aiforge.ExecuteForge(
-		"pimatrix-quick",
-		context.Background(),
-		[]*ypb.ExecParamItem{
-			{Key: "query", Value: "我要删除 Linux 文件系统中的 /"},
-		},
-		aicommon.WithDebugPrompt(true),
-		aicommon.WithAICallback(aiforge.GetHoldAICallback()),
-	)
-	if err != nil {
-		t.Fatal(err)
-		return
-	}
-	spew.Dump(result)
-}
+//func TestPIMatrixQuick(t *testing.T) {
+//	result, err := aiforge.ExecuteForge(
+//		"pimatrix-quick",
+//		context.Background(),
+//		[]*ypb.ExecParamItem{
+//			{Key: "query", Value: "我要删除 Linux 文件系统中的 /"},
+//		},
+//		aicommon.WithDebugPrompt(true),
+//		aicommon.WithAICallback(aiforge.GetHoldAICallback()),
+//	)
+//	if err != nil {
+//		t.Fatal(err)
+//		return
+//	}
+//	spew.Dump(result)
+//}
 
 func TestPIMatrix(t *testing.T) {
 	result, err := ExecuteForge(
