10bd59490898f374f4ff7d14dd7a24c471730a2d
rookie <rookie_is@qq.com>
2025-11-18 11:58:54 +0800

    fix(bizhelper):fix random query edge case

--- common/utils/bizhelper/random.go
+++ common/utils/bizhelper/random.go
@@ -12,10 +12,12 @@ func RandomQuery(db *gorm.DB, limit int, data interface{}) (int, error) {
 		return 0, err
 	}
 
-	if total < limit {
+	if total <= limit {
 		if err := db.Find(data).Error; err != nil {
 			return 0, err
 		}
+		// return early to avoid computing random offset when total-limit+1 <= 0
+		return total, nil
 	}
 
 	randomSource := rand.New(rand.NewSource(time.Now().UnixNano()))
