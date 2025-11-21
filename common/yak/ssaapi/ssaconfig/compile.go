c2290e2c41e2deafbc142071b5b5503e502a9071
intSheep <intSheep@qq.com>
2025-11-21 11:22:18 +0800

    fix some sf scan bug

--- common/yak/ssaapi/ssaconfig/compile.go
+++ common/yak/ssaapi/ssaconfig/compile.go
@@ -105,7 +105,7 @@ func WithProgramNames(programName ...string) Option {
 		if err := c.ensureBase("Program Name"); err != nil {
 			return err
 		}
-		c.BaseInfo.ProgramNames = append(c.BaseInfo.ProgramNames, programName...)
+		c.BaseInfo.ProgramNames = programName
 		return nil
 	}
 }
