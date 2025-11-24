5b85be0b5307eb31a0f1836796300694454e4bad
VillanCh <188228725@qq.com>
2025-11-24 23:40:11 +0800

    fix case for plugins_rag

--- common/ai/rag/knowledgebase/test/integration/query_test.go
+++ common/ai/rag/knowledgebase/test/integration/query_test.go
@@ -14,6 +14,8 @@ import (
 	"github.com/yaklang/yaklang/common/yak/depinjector"
 	"github.com/yaklang/yaklang/common/yak/depinjector/aiforge"
 	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
+
+	_ "github.com/yaklang/yaklang/common/ai/rag/plugins_rag"
 )
 
 func init() {
