566aba35abbca4e7efcae8ab31b1ee6e78323e21
rookie <rookie_is@qq.com>
2025-11-25 18:20:53 +0800

    feat(invoke_blueprint):add review case "input_params" , use input applicant

--- common/ai/aid/aireact/invoke_blueprint_modify_params_test.go
+++ common/ai/aid/aireact/invoke_blueprint_modify_params_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"github.com/yaklang/yaklang/common/ai/aid/aitool"
 	"strings"
 	"testing"
 	"time"
@@ -208,4 +209,134 @@ LOOP:
 		t.Fatal("timeline does not contain codec.Sha256(flag)", flag)
 	}
 	fmt.Println(timeline)
+}
+
+func TestReAct_RequireBlueprint_InputParams(t *testing.T) {
+	// t.Skip()
+
+	flag := aitool.InvokeParams(map[string]interface{}{
+		"key": ksuid.New().String(),
+	})
+	in := make(chan *ypb.AIInputEvent, 10)
+	out := make(chan *ypb.AIOutputEvent, 10)
+
+	forgeExecute := false
+	forgeHaveFlag := false
+
+	abort, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	ins, err := NewTestReAct(
+		aicommon.WithAICallback(func(i aicommon.AICallerConfigIf, r *aicommon.AIRequest) (*aicommon.AIResponse, error) {
+			return mockedRequireBlueprint_ModifyParams(i, r, "")
+		}),
+		aicommon.WithDebug(false),
+		aicommon.WithEventInputChan(in),
+		aicommon.WithEventHandler(func(e *schema.AiOutputEvent) {
+			out <- e.ToGRPC()
+		}),
+		aicommon.WithHijackPERequest(func(ctx context.Context, planPayload string) error {
+			forgeExecute = true
+			if strings.Contains(codec.Sha256(planPayload), codec.Sha256(utils.InterfaceToString(flag))) {
+				forgeHaveFlag = true
+			}
+			go func() {
+				time.Sleep(time.Second * 3)
+				cancel()
+			}()
+			return nil
+		}),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_ = ins
+	go func() {
+		for i := 0; i < 1; i++ {
+			in <- &ypb.AIInputEvent{
+				IsFreeInput: true,
+				FreeInput:   "abc",
+			}
+		}
+	}()
+	after := time.After(60 * time.Second)
+
+	endforge := false
+	reActFinished := false
+	reviewed := false
+LOOP:
+	for {
+		select {
+		case e := <-out:
+			fmt.Println(e.String())
+			if e.GetType() == string(schema.EVENT_TYPE_ITERATION) {
+				result := utils.InterfaceToInt(jsonpath.FindFirst(e.GetContent(), "$.current"))
+				if result > 20 {
+					break LOOP
+				}
+			}
+
+			if e.GetType() == string(schema.EVENT_TYPE_EXEC_AIFORGE_REVIEW_REQUIRE) {
+				fmt.Println(string(e.GetContent()))
+				epid := utils.InterfaceToString(jsonpath.FindFirst(e.GetContent(), "$.id"))
+				in <- &ypb.AIInputEvent{
+					IsInteractiveMessage: true,
+					InteractiveId:        epid,
+					InteractiveJSONInput: fmt.Sprintf(`{"suggestion": "input_params", "params": %s}`, utils.InterfaceToString(flag)),
+				}
+				reviewed = true
+				continue
+			}
+
+			if e.GetType() == string(schema.EVENT_TYPE_END_PLAN_AND_EXECUTION) {
+				endforge = true
+			}
+
+			if endforge && e.GetType() == string(schema.EVENT_TYPE_STRUCTURED) {
+				if e.GetNodeId() == "react_task_status_changed" {
+					if utils.InterfaceToString(jsonpath.FindFirst(e.GetContent(), "$.react_task_now_status")) == "completed" {
+						reActFinished = true
+						break LOOP
+					}
+				}
+			}
+		case <-after:
+			break LOOP
+		case <-abort.Done():
+			break LOOP
+		}
+	}
+
+	if !reviewed {
+		t.Fatal("no reviewed (modified params)")
+	}
+
+	if !forgeExecute {
+		t.Fatal("forged plan and execute not executed")
+	}
+
+	if !forgeHaveFlag {
+		t.Fatal("forged plan and execute not have flag")
+	}
+
+	if !reActFinished {
+		t.Fatal("re-act not finished")
+	}
+
+	if !endforge {
+		t.Fatal("not receive end of forge")
+	}
+
+	timeline := ins.DumpTimeline()
+	if !strings.Contains(timeline, utils.InterfaceToString(flag)) {
+		t.Fatal("timeline does not contain flag", flag)
+	}
+	fmt.Println(timeline)
+}
+
+func TestAac(t *testing.T) {
+	flag := aitool.InvokeParams(map[string]interface{}{
+		"key": ksuid.New().String(),
+	})
+
+	fmt.Printf(`{"suggestion": "input_params", "params": %s}`, utils.InterfaceToString(flag))
 }
