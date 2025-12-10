6504a7a03a7ff70b05a357461b60b4f1a6a51dae
z3 <z3r0ne@163.com>
2025-12-10 13:11:53 +0800

    feat: support ai service option for kb query

--- common/yak/depinjector/aiforge/aiforge.go
+++ common/yak/depinjector/aiforge/aiforge.go
@@ -3,6 +3,7 @@ package aiforge
 import (
 	"context"
 
+	"github.com/yaklang/yaklang/common/ai/aid/aicommon"
 	"github.com/yaklang/yaklang/common/ai/aid/aitool"
 	"github.com/yaklang/yaklang/common/aiforge"
 	"github.com/yaklang/yaklang/common/aiforge/contracts"
@@ -10,6 +11,30 @@ import (
 )
 
 type SimpleAiForge struct {
+}
+
+func (s *SimpleAiForge) SimpleExecuteWithOptions(ctx context.Context, input string, aitoolOptions []aitool.ToolOption, options ...aicommon.ConfigOption) (aitool.InvokeParams, error) {
+	param := aitool.WithStructParam("result", []aitool.PropertyOption{}, aitoolOptions...)
+	lf, err := aiforge.NewLiteForge(
+		"simple_ai_forge",
+		aiforge.WithLiteForge_Prompt(input),
+		aiforge.WithLiteForge_OutputSchemaRaw("object", aitool.NewObjectSchemaWithAction(param)),
+		aiforge.WithExtendLiteForge_AIOption(options...),
+	)
+	if err != nil {
+		return nil, err
+	}
+	result, err := lf.Execute(ctx, []*ypb.ExecParamItem{
+		{
+			Key:   "input",
+			Value: input,
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	res := result.GetInvokeParams("result")
+	return res, nil
 }
 
 func (s *SimpleAiForge) SimpleExecute(ctx context.Context, input string, aitoolOptions []aitool.ToolOption) (aitool.InvokeParams, error) {
