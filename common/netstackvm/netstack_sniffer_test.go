b42fe59bcf8d74fab1f730f4956fc834bd1f4573
rookie <rookie_is@qq.com>
2025-12-11 17:47:28 +0800

    feat(route-manager): add hijack black list

--- common/netstackvm/netstack_sniffer_test.go
+++ common/netstackvm/netstack_sniffer_test.go
@@ -1,8 +1,6 @@
 package netstackvm
 
 import (
-	"context"
-	"fmt"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/require"
 	"github.com/yaklang/yaklang/common/utils"
@@ -22,26 +20,6 @@ func TestNetstackMonitor(t *testing.T) {
 			time.Sleep(5 * time.Second)
 			spew.Dump(m.GetAliveIP())
 			spew.Dump(m.GetAliveDomain())
-		}
-	}()
-
-	select {}
-}
-
-func TestNetstackCache(t *testing.T) {
-	if utils.InGithubActions() {
-		t.Skip("skip in github actions")
-	}
-	m, err := StartNetReserveStackDNSCache(context.Background())
-	require.NoError(t, err)
-
-	go func() {
-		for {
-			time.Sleep(5 * time.Second)
-			domainList := m.ReserveResolve("183.2.172.177")
-			if len(domainList) > 0 {
-				fmt.Printf("found domain %v\n", domainList)
-			}
 		}
 	}()
 
