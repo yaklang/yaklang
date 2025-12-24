638d7b1e808c86e6ecf51587b3f5264f88e98890
YWHKKX <2076064543@qq.com>
2025-12-24 17:03:33 +0800

    fix(scan): fix programs in manager

--- common/yak/syntaxflow_scan/manager_test.go
+++ common/yak/syntaxflow_scan/manager_test.go
@@ -6,8 +6,14 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/consts"
 	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/schema"
+	"github.com/yaklang/yaklang/common/yak/ssa/ssadb"
+	"github.com/yaklang/yaklang/common/yak/ssaapi"
 	"github.com/yaklang/yaklang/common/yak/ssaapi/ssaconfig"
+	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
 
 func TestManager(t *testing.T) {
@@ -102,6 +108,84 @@ func TestManager(t *testing.T) {
 		require.Equal(t, task1.taskRecorder.ScanBatch+1, task2.taskRecorder.ScanBatch)
 		log.Infof("Same program - Task1 scan batch: %d, Task3 scan batch: %d",
 			task1.taskRecorder.ScanBatch, task3.taskRecorder.ScanBatch)
+	})
+
+	t.Run("test programs field saved when using ProjectID", func(t *testing.T) {
+		// 创建一个简单的程序并保存到数据库
+		programName := uuid.NewString()
+		prog, err := ssaapi.Parse(`print("test")`,
+			ssaapi.WithProgramName(programName),
+			ssaapi.WithLanguage(ssaconfig.Yak),
+		)
+		require.NoError(t, err)
+		require.NotNil(t, prog)
+		defer func() {
+			ssadb.DeleteProgram(ssadb.GetDB(), programName)
+		}()
+
+		// 获取程序的数据库记录 ID
+		progFromDB, err := ssadb.GetProgram(programName, ssadb.Application)
+		require.NoError(t, err)
+		require.NotNil(t, progFromDB)
+
+		// 创建一个 SSA Project
+		projectName := uuid.NewString()
+		projectReq := &ypb.CreateSSAProjectRequest{
+			Project: &ypb.SSAProject{
+				ProjectName: projectName,
+				Language:    "yak",
+			},
+		}
+		project, err := yakit.CreateSSAProject(consts.GetGormProfileDatabase(), projectReq)
+		require.NoError(t, err)
+		require.NotNil(t, project)
+		defer func() {
+			// 清理项目
+			yakit.DeleteSSAProject(consts.GetGormProfileDatabase(), &ypb.DeleteSSAProjectRequest{
+				Filter: &ypb.SSAProjectFilter{
+					IDs: []int64{int64(project.ID)},
+				},
+			})
+		}()
+
+		// 更新程序的 ProjectID
+		err = yakit.UpdateIrProgramProjectID(ssadb.GetDB(), uint(progFromDB.ID), uint64(project.ID))
+		require.NoError(t, err)
+
+		// 通过 ProjectID 创建扫描任务（不传 ProgramNames）
+		taskId := uuid.NewString()
+		task, err := createSyntaxflowTaskById(context.Background(), "",
+			taskId,
+			newConfig(
+				ssaconfig.WithRuleFilterLanguage("yak"),
+				ssaconfig.WithProjectID(uint64(project.ID)),
+				// 注意：这里不传 ProgramNames
+			),
+		)
+		require.NoError(t, err)
+		require.NotNil(t, task)
+
+		// 验证 initByConfig 后 BaseInfo.ProgramNames 被正确设置
+		require.NotNil(t, task.Config)
+		require.NotNil(t, task.Config.Config)
+		require.NotNil(t, task.Config.Config.BaseInfo)
+		require.NotEmpty(t, task.Config.Config.BaseInfo.ProgramNames, "BaseInfo.ProgramNames should be set after initByConfig")
+		require.Equal(t, programName, task.Config.Config.BaseInfo.ProgramNames[0], "ProgramNames should match the queried program name")
+
+		// 验证 Programs 字段被设置
+		require.NotEmpty(t, task.Config.Programs, "Programs should be set after initByConfig")
+		require.Equal(t, programName, task.Config.Programs[0].GetProgramName(), "Program name should match")
+
+		// 保存任务
+		err = task.SaveTask()
+		require.NoError(t, err)
+
+		// 从数据库查询任务，验证 programs 字段不为空
+		savedTask, err := schema.GetSyntaxFlowScanTaskById(ssadb.GetDB(), taskId)
+		require.NoError(t, err)
+		require.NotNil(t, savedTask)
+		require.NotEmpty(t, savedTask.Programs, "programs field should not be empty after SaveTask")
+		require.Equal(t, programName, savedTask.Programs, "programs field should contain the program name")
 	})
 
 }
