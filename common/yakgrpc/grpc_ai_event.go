b752679b07e99a858bf34dc650ab3d5eb3cb9265
z3 <z3r0ne@163.com>
2026-01-04 12:09:20 +0800

    feat: support paging in QueryAIEvent, add YieldAIEvent/QueryAIEventPaging, limit max event IDs for ProcessID paging

--- common/yakgrpc/grpc_ai_event.go
+++ common/yakgrpc/grpc_ai_event.go
@@ -6,7 +6,6 @@ import (
 	"github.com/samber/lo"
 	"github.com/yaklang/yaklang/common/schema"
 	"github.com/yaklang/yaklang/common/utils"
-	"github.com/yaklang/yaklang/common/utils/bizhelper"
 	"github.com/yaklang/yaklang/common/yakgrpc/yakit"
 	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
 )
@@ -16,26 +15,67 @@ func (s *Server) QueryAIEvent(ctx context.Context, req *ypb.AIEventQueryRequest)
 	if filter == nil {
 		filter = &ypb.AIEventFilter{}
 	}
+
 	db := s.GetProjectDatabase()
-	if req.GetProcessID() != "" {
-		eventIDs, err := yakit.QueryAIEventIDByProcessID(db, req.GetProcessID())
+
+	queryAll := func() []*schema.AiOutputEvent {
+		eventCh := yakit.YieldAIEvent(ctx, db, filter)
+		var events []*schema.AiOutputEvent
+		for event := range eventCh {
+			events = append(events, event)
+		}
+		return events
+	}
+	paging := req.GetPagination()
+	// 不带分页查询
+	if paging == nil || lo.IsEmpty(paging) {
+		if req.GetProcessID() != "" {
+			eventIDs, err := yakit.QueryAIEventIDByProcessID(db, req.GetProcessID())
+			if err != nil {
+				return nil, err
+			}
+			if len(eventIDs) == 0 {
+				return nil, utils.Errorf("no events found for process ID: %s", req.GetProcessID())
+			}
+			filter.EventUUIDS = append(filter.EventUUIDS, eventIDs...)
+		}
+		events := queryAll()
+		return &ypb.AIEventQueryResponse{
+			Events: lo.Map(events, func(item *schema.AiOutputEvent, _ int) *ypb.AIOutputEvent {
+				return item.ToGRPC()
+			}),
+		}, nil
+	} else {
+		var isQueryEventUUIDByProcessID bool
+		if req.GetProcessID() != "" {
+			eventIDs, err := yakit.QueryAIEventIDByProcessID(db, req.GetProcessID())
+			if err != nil {
+				return nil, err
+			}
+			if len(eventIDs) == 0 {
+				return nil, utils.Errorf("no events found for process ID: %s", req.GetProcessID())
+			}
+			filter.EventUUIDS = append(filter.EventUUIDS, eventIDs...)
+			isQueryEventUUIDByProcessID = true
+		}
+		if isQueryEventUUIDByProcessID && len(filter.EventUUIDS) > 10 {
+			return nil, utils.Errorf("event UUIDs by process ID is too many, max 10")
+		}
+		paginator, events, err := yakit.QueryAIEventPaging(db, filter, paging)
 		if err != nil {
 			return nil, err
 		}
-		if len(eventIDs) == 0 {
-			return nil, utils.Errorf("no events found for process ID: %s", req.GetProcessID())
-		}
-		filter.EventUUIDS = append(filter.EventUUIDS, eventIDs...)
+		return &ypb.AIEventQueryResponse{
+			Events: lo.Map(events, func(item *schema.AiOutputEvent, _ int) *ypb.AIOutputEvent {
+				return item.ToGRPC()
+			}),
+			Total: int64(paginator.TotalRecord),
+			Pagination: &ypb.Paging{
+				Page:    int64(paginator.Page),
+				Limit:   int64(paginator.Limit),
+				OrderBy: paging.GetOrderBy(),
+				Order:   paging.GetOrder(),
+			},
+		}, nil
 	}
-	paginator, err := bizhelper.NewPagination(db, 100)
-	event, err := yakit.QueryAIEvent(db, filter)
-	if err != nil {
-		return nil, err
-	}
-	return &ypb.AIEventQueryResponse{
-		Events: lo.Map(event, func(item *schema.AiOutputEvent, _ int) *ypb.AIOutputEvent {
-			return item.ToGRPC()
-		}),
-	}, nil
-
 }
