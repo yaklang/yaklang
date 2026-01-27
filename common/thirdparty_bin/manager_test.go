32c9f20df1e81caa03ce34eba59d55badf161e86
z3 <z3r0ne@163.com>
2026-01-27 15:06:53 +0800

    update ffmpeg cfg

--- common/thirdparty_bin/manager_test.go
+++ common/thirdparty_bin/manager_test.go
@@ -6,16 +6,7 @@ import (
 )
 
 func TestManager_Start(t *testing.T) {
-	// err := DefaultManager.Install("ffmpeg", &InstallOptions{
-	// 	Force:      true,
-	// 	Context:    context.Background(),
-	// 	SystemType: "linux-amd64",
-	// })
-	// if err != nil {
-	// 	t.Fatalf("failed to install ffmpeg: %v", err)
-	// }
-
-	err := DefaultManager.Install("pandoc", &InstallOptions{
+	err := DefaultManager.Install("ffmpeg", &InstallOptions{
 		Force:      true,
 		Context:    context.Background(),
 		SystemType: "linux-amd64",
@@ -23,4 +14,13 @@ func TestManager_Start(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to install ffmpeg: %v", err)
 	}
+
+	// err := DefaultManager.Install("pandoc", &InstallOptions{
+	// 	Force:      true,
+	// 	Context:    context.Background(),
+	// 	SystemType: "linux-amd64",
+	// })
+	// if err != nil {
+	// 	t.Fatalf("failed to install ffmpeg: %v", err)
+	// }
 }
