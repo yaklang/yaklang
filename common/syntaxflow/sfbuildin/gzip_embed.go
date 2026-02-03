c4e80d0c28c8ea62731bf9580dda08dec527e2b9
YWHKKX <2076064543@qq.com>
2026-02-03 10:45:02 +0800

    fix check function

--- common/syntaxflow/sfbuildin/gzip_embed.go
+++ common/syntaxflow/sfbuildin/gzip_embed.go
@@ -65,9 +65,3 @@ func GetRuleFileSystem() filesys_interface.FileSystem {
 	// ruleFSWithHash 在 gzip 版本中已经是 PreprocessingEmbed，实现了 FileSystem 接口
 	return ruleFSWithHash.(filesys_interface.FileSystem)
 }
-
-// CheckDuplicateTitles 检查规则中的 title 和 title_zh 是否重复（gzip 版本不需要检查）
-func CheckDuplicateTitles(fsInstance filesys_interface.FileSystem) error {
-	// Gzip 版本跳过重复检查，因为 tar.gz 文件在打包时已经验证过
-	return nil
-}
