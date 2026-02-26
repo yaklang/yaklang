a2bab177af1b9e059255a90fcdc8d25b05ae2e80
rookie <rookie_is@qq.com>
2026-02-26 16:50:31 +0800

    ai: add config key and provider store

---
+++ common/consts/ai_config_keys.go
@@ -0,0 +1,5 @@
+package consts
+
+const (
+	AI_GLOBAL_CONFIG_KEY = "AI_GLOBAL_CONFIG"
+)
---
+++ common/yakgrpc/yakit/ai_provider.go
@@ -0,0 +1,101 @@
+package yakit
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/yaklang/yaklang/common/schema"
+	"github.com/yaklang/yaklang/common/utils"
+)
+
+func CreateAIProvider(db *gorm.DB, provider *schema.AIThirdPartyConfig) error {
+	if db == nil {
+		return utils.Error("no set database")
+	}
+	if provider == nil {
+		return utils.Error("provider is nil")
+	}
+	return db.Create(provider).Error
+}
+
+func UpdateAIProvider(db *gorm.DB, id int64, provider *schema.AIThirdPartyConfig) error {
+	if db == nil {
+		return utils.Error("no set database")
+	}
+	if provider == nil {
+		return utils.Error("provider is nil")
+	}
+	updates := map[string]interface{}{
+		"type":            provider.Type,
+		"api_key":         provider.APIKey,
+		"user_identifier": provider.UserIdentifier,
+		"user_secret":     provider.UserSecret,
+		"namespace":       provider.Namespace,
+		"domain":          provider.Domain,
+		"webhook_url":     provider.WebhookURL,
+		"extra_params":    provider.ExtraParams,
+		"disabled":        provider.Disabled,
+	}
+	return db.Model(&schema.AIThirdPartyConfig{}).Where("id = ?", id).Updates(updates).Error
+}
+
+func UpsertAIProvider(db *gorm.DB, provider *schema.AIThirdPartyConfig) (*schema.AIThirdPartyConfig, error) {
+	if db == nil {
+		return nil, utils.Error("no set database")
+	}
+	if provider == nil {
+		return nil, utils.Error("provider is nil")
+	}
+	if provider.ID > 0 {
+		if err := UpdateAIProvider(db, int64(provider.ID), provider); err != nil {
+			return nil, err
+		}
+		return GetAIProvider(db, int64(provider.ID))
+	}
+	if err := CreateAIProvider(db, provider); err != nil {
+		return nil, err
+	}
+	return provider, nil
+}
+
+func GetAIProvider(db *gorm.DB, id int64) (*schema.AIThirdPartyConfig, error) {
+	if db == nil {
+		return nil, utils.Error("no set database")
+	}
+	var provider schema.AIThirdPartyConfig
+	if err := db.Model(&schema.AIThirdPartyConfig{}).Where("id = ?", id).First(&provider).Error; err != nil {
+		return nil, err
+	}
+	return &provider, nil
+}
+
+func ListAIProviders(db *gorm.DB) ([]*schema.AIThirdPartyConfig, error) {
+	if db == nil {
+		return nil, utils.Error("no set database")
+	}
+	var providers []*schema.AIThirdPartyConfig
+	if err := db.Model(&schema.AIThirdPartyConfig{}).Order("id asc").Find(&providers).Error; err != nil {
+		return nil, err
+	}
+	return providers, nil
+}
+
+func DeleteAIProvider(db *gorm.DB, id int64) error {
+	if db == nil {
+		return utils.Error("no set database")
+	}
+	return db.Model(&schema.AIThirdPartyConfig{}).Where("id = ?", id).Unscoped().Delete(&schema.AIThirdPartyConfig{}).Error
+}
+
+func LoadAIProviderMap(db *gorm.DB) (map[int64]*schema.AIThirdPartyConfig, error) {
+	providers, err := ListAIProviders(db)
+	if err != nil {
+		return nil, err
+	}
+	result := make(map[int64]*schema.AIThirdPartyConfig, len(providers))
+	for _, p := range providers {
+		if p == nil {
+			continue
+		}
+		result[int64(p.ID)] = p
+	}
+	return result, nil
+}
