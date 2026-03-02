78dde19f86882879ac6ae1fe42928c25a619636a
v1ll4n <v1ll4n@qq.com>
2026-03-02 14:58:14 +0800

    modify some details

--- common/yakgrpc/ai_test.go
+++ common/yakgrpc/ai_test.go
@@ -1,36 +1,29 @@
 package yakgrpc
 
 import (
-	"fmt"
 	"testing"
-
-	"github.com/stretchr/testify/require"
-	"github.com/yaklang/yaklang/common/ai/aid/aicommon"
-	"github.com/yaklang/yaklang/common/ai/aid/aireact"
-	"github.com/yaklang/yaklang/common/ai/aid/aireact/reactloops"
-	"github.com/yaklang/yaklang/common/schema"
 )
 
 func TestAcc(t *testing.T) {
-	r, err := aireact.NewReAct(aicommon.WithDebug(true))
-	require.NoError(t, err)
+	// r, err := aireact.NewReAct(aicommon.WithDebug(true))
+	// require.NoError(t, err)
 
-	currentTask := aicommon.NewStatefulTaskBase(
-		"plan",
-		"对www.a.com进行web漏洞扫描，这是我的内网域名，不要问我其他问题了",
-		r.GetConfig().GetContext(),
-		r.Emitter,
-	)
+	// currentTask := aicommon.NewStatefulTaskBase(
+	// 	"plan",
+	// 	"对www.a.com进行web漏洞扫描，这是我的内网域名，不要问我其他问题了",
+	// 	r.GetConfig().GetContext(),
+	// 	r.Emitter,
+	// )
 
-	task, err := r.ExecuteLoopTask(schema.AI_REACT_LOOP_NAME_PLAN, currentTask, reactloops.WithOnPostIteraction(func(loop *reactloops.ReActLoop, iteration int, task aicommon.AIStatefulTask, isDone bool, reason any, operator *reactloops.OnPostIterationOperator) {
-		if isDone {
-			fmt.Println(loop.Get("plan_data"))
-		}
+	// task, err := r.ExecuteLoopTask(schema.AI_REACT_LOOP_NAME_PLAN, currentTask, reactloops.WithOnPostIteraction(func(loop *reactloops.ReActLoop, iteration int, task aicommon.AIStatefulTask, isDone bool, reason any, operator *reactloops.OnPostIterationOperator) {
+	// 	if isDone {
+	// 		fmt.Println(loop.Get("plan_data"))
+	// 	}
 
-	}))
-	if err != nil {
-		return
-	}
-	require.NotNil(t, task)
-	require.NoError(t, err)
+	// }))
+	// if err != nil {
+	// 	return
+	// }
+	// require.NotNil(t, task)
+	// require.NoError(t, err)
 }
