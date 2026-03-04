1f77ed9195eefbccd8054d6f0a01018afaa32621
rookie <rookie_is@qq.com>
2026-03-04 16:10:20 +0800

    fix: add mutex for thread-safe stream sending and handle context errors

--- common/ai/aid/aicommon/config.go
+++ common/ai/aid/aicommon/config.go
@@ -363,11 +363,6 @@ func NewConfig(ctx context.Context, opts ...ConfigOption) *Config {
 		}
 	}
 
-	// Auto Set up work dir
-	if config.Workdir == "" {
-		config.Workdir = config.GetOrCreateWorkDir()
-	}
-
 	return config
 }
 
