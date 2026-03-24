aefe38a940b0a51c9c91dfc69b17814dff247fca
z3 <z3r0ne@163.com>
2026-03-24 15:20:44 +0800

    fix: enhance unicode decoding for auto decode

--- common/yak/yaklib/codec/json_unicode.go
+++ common/yak/yaklib/codec/json_unicode.go
@@ -3,8 +3,8 @@ package codec
 import (
 	"bytes"
 	"fmt"
-	"regexp"
 	"strconv"
+	"unicode/utf8"
 )
 
 func JsonUnicodeEncode(i string) string {
@@ -18,21 +18,82 @@ func JsonUnicodeEncode(i string) string {
 }
 
 func JsonUnicodeDecode(i string) string {
-	s := i
-	var (
-		jsonUnicodeDecodeRe = regexp.MustCompile(`\\u[0-9a-fA-F]{4}`)
-	)
-	return jsonUnicodeDecodeRe.ReplaceAllStringFunc(s, func(found string) (ret string) {
-		defer func() {
-			if err := recover(); err != nil {
-				ret = found
-			}
-		}()
-		numStr := found[2:]
-		n, err := strconv.ParseInt(numStr, 16, 32)
-		if err != nil {
-			return found
+	if len(i) == 0 {
+		return i
+	}
+
+	var buf bytes.Buffer
+	for index := 0; index < len(i); {
+		if decoded, size, ok := decodeJSONUnicodeAt(i, index); ok {
+			buf.WriteString(decoded)
+			index += size
+			continue
 		}
-		return string([]rune{rune(n)})
-	})
+		buf.WriteByte(i[index])
+		index++
+	}
+	return buf.String()
+}
+
+func decodeJSONUnicodeAt(s string, index int) (decoded string, size int, ok bool) {
+	if index >= len(s) || s[index] != '\\' {
+		return "", 0, false
+	}
+
+	if index+2 < len(s) && s[index+1] == '\\' {
+		switch s[index+2] {
+		case 'u':
+			if index+7 <= len(s) && parseJSONUnicodeRune(s[index+3:index+7]) {
+				return s[index+1 : index+7], 7, true
+			}
+		case 'U':
+			if index+11 <= len(s) && parseJSONUnicodeRune(s[index+3:index+11]) {
+				return s[index+1 : index+11], 11, true
+			}
+		}
+	}
+
+	if index+1 >= len(s) {
+		return "", 0, false
+	}
+
+	switch s[index+1] {
+	case 'u':
+		if index+6 > len(s) {
+			return "", 0, false
+		}
+		r, ok := decodeJSONUnicodeRune(s[index+2 : index+6])
+		if !ok {
+			return "", 0, false
+		}
+		return string(r), 6, true
+	case 'U':
+		if index+10 > len(s) {
+			return "", 0, false
+		}
+		r, ok := decodeJSONUnicodeRune(s[index+2 : index+10])
+		if !ok {
+			return "", 0, false
+		}
+		return string(r), 10, true
+	default:
+		return "", 0, false
+	}
+}
+
+func parseJSONUnicodeRune(hex string) bool {
+	_, ok := decodeJSONUnicodeRune(hex)
+	return ok
+}
+
+func decodeJSONUnicodeRune(hex string) (rune, bool) {
+	n, err := strconv.ParseInt(hex, 16, 32)
+	if err != nil {
+		return 0, false
+	}
+	r := rune(n)
+	if !utf8.ValidRune(r) {
+		return 0, false
+	}
+	return r, true
 }
