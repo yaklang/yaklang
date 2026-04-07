e1f761d3e908fd8bce4f5cbf52c6a9d1cd158b28
wlz <lingze.wl@gmail.com>
2026-04-07 17:16:05 +0800

    feat(ssa2llvm): add native llvm obfuscation passes

--- common/yak/ssa2llvm/obfuscation/builtin/builtin.go
+++ common/yak/ssa2llvm/obfuscation/builtin/builtin.go
@@ -4,4 +4,7 @@ import (
 	_ "github.com/yaklang/yaklang/common/yak/ssa2llvm/obfuscation/addsub"
 	_ "github.com/yaklang/yaklang/common/yak/ssa2llvm/obfuscation/callret"
 	_ "github.com/yaklang/yaklang/common/yak/ssa2llvm/obfuscation/llvmxor"
+	_ "github.com/yaklang/yaklang/common/yak/ssa2llvm/obfuscation/mba"
+	_ "github.com/yaklang/yaklang/common/yak/ssa2llvm/obfuscation/opaque"
+	_ "github.com/yaklang/yaklang/common/yak/ssa2llvm/obfuscation/virtualize"
 )
