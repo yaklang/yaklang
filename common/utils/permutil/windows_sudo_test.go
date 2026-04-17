4f3e15b9c18404b21ffa7b75c5fdd54ee8ea2ec8
go0p <gp827782797@qq.com>
2026-04-17 12:54:09 +0800

    feat(permutil): add Windows UAC batch processing utilities and tests

--- common/utils/permutil/windows_sudo_test.go
+++ common/utils/permutil/windows_sudo_test.go
@@ -2,10 +2,17 @@ package permutil
 
 import (
 	"os"
+	"runtime"
 	"testing"
 )
 
 func TestWindowsSudo(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		t.Skip("manual Windows-only test")
+	}
+	if os.Getenv("YAK_RUN_MANUAL_WINDOWS_SUDO_TEST") != "1" {
+		t.Skip("set YAK_RUN_MANUAL_WINDOWS_SUDO_TEST=1 to run manual UAC test")
+	}
 	var err = WindowsSudo("yak version", WithStdout(os.Stdout), WithStderr(os.Stdout))
 	if err != nil {
 		panic(err)
