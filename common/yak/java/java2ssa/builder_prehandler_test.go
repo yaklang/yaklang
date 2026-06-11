432ade8a3e8fb287e7a2888aa6c8e82e7e04077d
wlz <lingze.wl@gmail.com>
2026-06-11 16:52:43 +0800

    fix(ssa): release generated template AST roots

--- common/yak/java/java2ssa/builder_prehandler_test.go
+++ common/yak/java/java2ssa/builder_prehandler_test.go
@@ -3,7 +3,11 @@ package java2ssa
 import (
 	"testing"
 
+	"github.com/antlr/antlr4/runtime/Go/antlr/v4"
 	"github.com/stretchr/testify/require"
+	"github.com/yaklang/yaklang/common/utils/filesys"
+	"github.com/yaklang/yaklang/common/utils/memedit"
+	"github.com/yaklang/yaklang/common/yak/ssa"
 )
 
 func TestSSABuilder_FilterPreHandlerFile(t *testing.T) {
@@ -39,5 +43,52 @@ func TestSSABuilder_FilterPreHandlerFile(t *testing.T) {
 		".github/workflows/maven.yml",
 	} {
 		require.False(t, builder.FilterPreHandlerFile(path), path)
+	}
+}
+
+func TestSSABuilder_PreHandlerProjectReleasesGeneratedTemplateASTRoot(t *testing.T) {
+	tests := []struct {
+		name   string
+		path   string
+		source string
+	}{
+		{
+			name:   "jsp",
+			path:   "src/main/webapp/index.jsp",
+			source: `<%= request.getParameter("name") %>`,
+		},
+		{
+			name:   "freemarker",
+			path:   "src/main/resources/templates/index.ftl",
+			source: `<#if enabled>ok</#if>`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			builder := CreateBuilder().(*SSABuilder)
+			prog := ssa.NewTmpProgram("template-release-test")
+			fb := prog.GetAndCreateFunctionBuilder("", string(ssa.MainFunctionName))
+			fs := filesys.NewVirtualFs()
+
+			var directChildren []antlr.Tree
+			prog.Build = func(ast ssa.FrontAST, _ *memedit.MemEditor, _ *ssa.FunctionBuilder) error {
+				tree, ok := ast.(antlr.Tree)
+				require.True(t, ok, "generated Java AST should be an ANTLR tree")
+				directChildren = append([]antlr.Tree(nil), tree.GetChildren()...)
+				require.NotEmpty(t, directChildren, "precondition: generated Java root should have children during build")
+				for _, child := range directChildren {
+					require.Equal(t, tree, child.GetParent(), "precondition: direct child should still point to the root during build")
+				}
+				return nil
+			}
+
+			editor := prog.CreateEditor([]byte(tt.source), tt.path, false)
+			require.NoError(t, builder.PreHandlerProject(fs, nil, fb, editor))
+			require.NotEmpty(t, directChildren)
+			for _, child := range directChildren {
+				require.Nil(t, child.GetParent(), "generated Java root direct child should be detached after build")
+			}
+		})
 	}
 }
