c98d68ddb2d261f06be5be7561a3d1f31f8eb881
YWHKKX <2076064543@qq.com>
2026-06-25 11:37:12 +0800

    feat(java_decompiler): implement code source management and directory listing

---
+++ common/yak/yakurl/java_decompiler/export_code_source.go
@@ -0,0 +1,157 @@
+package java_decompiler
+
+import (
+	"archive/zip"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/yaklang/yaklang/common/consts"
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/utils/filesys"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+)
+
+func (a *Action) exportDecompiledCodeSource(rootPath string) (*ypb.RequestYakURLResponse, error) {
+	if err := validateCodeSourceRoot(rootPath); err != nil {
+		return nil, err
+	}
+	cs, err := a.resolveCodeSource(rootPath)
+	if err != nil {
+		return nil, err
+	}
+
+	exportedFileName := cs.exportBaseName() + "-decompiled.zip"
+	exportDir := filepath.Join(consts.GetDefaultYakitBaseTempDir(), "java-decompiled")
+	if err := os.MkdirAll(exportDir, 0o755); err != nil {
+		return nil, utils.Wrapf(err, "failed to create export dir: %s", exportDir)
+	}
+	zipFilePath := filepath.Join(exportDir, exportedFileName)
+	zipFile, err := os.Create(zipFilePath)
+	if err != nil {
+		return nil, utils.Wrapf(err, "failed to create zip file: %s", zipFilePath)
+	}
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
+	walkFS := cs.walkFS()
+	walkRoot := cs.walkRoot()
+
+	err = filesys.Recursive(walkRoot, filesys.WithFileSystem(walkFS), filesys.WithFileStat(func(s string, info fs.FileInfo) error {
+		if info.IsDir() {
+			targetPath, err := cs.exportZipEntryPath(s)
+			if err != nil {
+				return err
+			}
+			_, err = zipWriter.Create(targetPath + "/")
+			return err
+		}
+
+		targetPath, err := cs.exportZipEntryPath(s)
+		if err != nil {
+			return err
+		}
+
+		var fileContent []byte
+		if strings.HasSuffix(strings.ToLower(s), ".class") {
+			decompiled, err := cs.readFileForExport(s)
+			if err != nil {
+				log.Warnf("Failed to decompile %s: %v", s, err)
+				fileContent, err = cs.readRawForExport(s)
+				if err != nil {
+					return utils.Wrapf(err, "failed to read class file: %s", s)
+				}
+			} else {
+				fileContent = decompiled
+				targetPath = strings.TrimSuffix(targetPath, ".class") + ".java"
+			}
+		} else {
+			fileContent, err = cs.readRawForExport(s)
+			if err != nil {
+				return utils.Wrapf(err, "failed to read file: %s", s)
+			}
+		}
+
+		entry, err := zipWriter.Create(targetPath)
+		if err != nil {
+			return utils.Wrapf(err, "failed to create zip entry for: %s", targetPath)
+		}
+		_, err = entry.Write(fileContent)
+		return err
+	}))
+	if err != nil {
+		return nil, utils.Wrapf(err, "failed to process code source files")
+	}
+
+	if err := zipWriter.Close(); err != nil {
+		return nil, utils.Wrapf(err, "failed to close zip writer")
+	}
+
+	zipSize := int64(0)
+	if fi, err := zipFile.Stat(); err == nil {
+		zipSize = fi.Size()
+	}
+
+	resourceURL := &ypb.YakURL{
+		Schema: "javaDec",
+		Path:   "/export",
+		Query: []*ypb.KVPair{
+			{Key: "jar", Value: rootPath},
+		},
+	}
+
+	resource := &ypb.YakURLResource{
+		ResourceName:      exportedFileName,
+		VerboseName:       exportedFileName,
+		ResourceType:      "file",
+		VerboseType:       "decompiled-jar-zip",
+		Size:              zipSize,
+		SizeVerbose:       utils.ByteSize(uint64(zipSize)),
+		ModifiedTimestamp: time.Now().Unix(),
+		Path:              zipFilePath,
+		Url:               resourceURL,
+		Extra: []*ypb.KVPair{
+			{Key: "path", Value: zipFilePath},
+		},
+	}
+
+	return &ypb.RequestYakURLResponse{
+		Resources: []*ypb.YakURLResource{resource},
+		Total:     1,
+		Page:      1,
+		PageSize:  1,
+	}, nil
+}
+
+func (cs *codeSource) exportZipEntryPath(absOrRelativePath string) (string, error) {
+	if cs.isDirectory {
+		rel, err := filepath.Rel(cs.rootPath, filepath.Clean(absOrRelativePath))
+		if err != nil {
+			return "", err
+		}
+		return filepath.ToSlash(rel), nil
+	}
+	return normalizeJarInternalPath(absOrRelativePath), nil
+}
+
+func (cs *codeSource) readFileForExport(path string) ([]byte, error) {
+	if cs.isDirectory {
+		return cs.expandedFS.ReadFile(path)
+	}
+	return cs.jarParser.GetJarFS().ReadFile(path)
+}
+
+func (cs *codeSource) readRawForExport(path string) ([]byte, error) {
+	if cs.isDirectory {
+		return cs.expandedFS.ReadFile(path)
+	}
+	jarFS := cs.jarParser.GetJarFS()
+	data, err := jarFS.ZipFS.ReadFile(path)
+	if err == nil {
+		return data, nil
+	}
+	return jarFS.ReadFile(path)
+}
---
+++ common/yak/yakurl/java_decompiler/list_aifix.go
@@ -0,0 +1,144 @@
+package java_decompiler
+
+import (
+	"path"
+	"strings"
+
+	"github.com/yaklang/yaklang/common/log"
+	"github.com/yaklang/yaklang/common/utils"
+	"github.com/yaklang/yaklang/common/yakgrpc/ypb"
+)
+
+func (a *Action) listJarAifix(rootPath, dirPath string) (*ypb.RequestYakURLResponse, error) {
+	if err := validateCodeSourceRoot(rootPath); err != nil {
+		return nil, err
+	}
+	cs, err := a.resolveCodeSource(rootPath)
+	if err != nil {
+		return nil, err
+	}
+
+	dirPath = normalizeJarInternalPath(dirPath)
+	entries, err := cs.listDirectory(dirPath)
+	if err != nil {
+		return nil, utils.Wrapf(err, "failed to list directory: %s in code source: %s", dirPath, rootPath)
+	}
+
+	innerClassesByOuter := make(map[string][]string)
+	for _, entry := range entries {
+		if !entry.IsDir() && strings.HasSuffix(entry.Name(), ".class") {
+			className := entry.Name()
+			dollarIndex := strings.Index(className, "$")
+			if dollarIndex > 0 {
+				outerClassName := className[:dollarIndex] + ".class"
+				entryPath := path.Join(dirPath, className)
+				outerClassPath := path.Join(dirPath, outerClassName)
+				innerClassesByOuter[outerClassPath] = append(innerClassesByOuter[outerClassPath], entryPath)
+			}
+		}
+	}
+
+	innerClassMap := make(map[string]string)
+	for outerClass, innerClasses := range innerClassesByOuter {
+		for _, innerClass := range innerClasses {
+			innerClassMap[innerClass] = outerClass
+		}
+	}
+
+	resources := make([]*ypb.YakURLResource, 0, len(entries))
+	for _, entry := range entries {
+		resourceURL := &ypb.YakURL{
+			Schema: "javaDec",
+			Path:   "/jar-aifix",
+			Query: []*ypb.KVPair{
+				{Key: "jar", Value: rootPath},
+				{Key: "dir", Value: dirPath},
+			},
+		}
+		entryPath := path.Join(dirPath, entry.Name())
+		fileInfo, err := entry.Info()
+		if err != nil {
+			return nil, err
+		}
+
+		if entry.IsDir() || javaclassparserIsArchiveLeaf(entry.Name()) {
+			resourceURL.Query = []*ypb.KVPair{
+				{Key: "jar", Value: rootPath},
+				{Key: "dir", Value: entryPath},
+			}
+		} else if strings.HasSuffix(entry.Name(), ".class") {
+			resourceURL.Path = "/class-aifix"
+			resourceURL.Query = []*ypb.KVPair{
+				{Key: "jar", Value: rootPath},
+				{Key: "class", Value: entryPath},
+			}
+		} else {
+			resourceURL.Query = []*ypb.KVPair{
+				{Key: "jar", Value: rootPath},
+				{Key: "dir", Value: path.Dir(entryPath)},
+			}
+		}
+
+		resource := a.createResourceFromFileInfo(resourceURL, fileInfo, entryPath)
+
+		if entry.IsDir() || javaclassparserIsArchiveLeaf(entry.Name()) {
+			resource.ResourceType = "dir"
+			resource.VerboseType = "java-directory"
+			resource.VerboseName = entry.Name()
+			resource.HaveChildrenNodes = true
+		} else {
+			resource.ResourceType = "file"
+			resource.VerboseType = "java-file"
+			resource.VerboseName = entry.Name()
+
+			if strings.HasSuffix(entry.Name(), ".class") {
+				if innerClasses, hasInnerClasses := innerClassesByOuter[entryPath]; hasInnerClasses {
+					for _, innerClassPath := range innerClasses {
+						resource.Extra = append(resource.Extra, &ypb.KVPair{
+							Key:   "innerClass",
+							Value: innerClassPath,
+						})
+					}
+				}
+				if outerClass, isInnerClass := innerClassMap[entryPath]; isInnerClass {
+					resource.Extra = append(resource.Extra, &ypb.KVPair{
+						Key:   "hide",
+						Value: "true",
+					})
+					resource.Extra = append(resource.Extra, &ypb.KVPair{
+						Key:   "outerClass",
+						Value: outerClass,
+					})
+				}
+			}
+		}
+
+		if entry.IsDir() || javaclassparserIsArchiveLeaf(entry.Name()) {
+			subEntries, err := cs.listDirectory(entryPath)
+			if err == nil {
+				resource.HaveChildrenNodes = len(subEntries) > 0
+			} else {
+				log.Warnf("failed to probe children for %s: %v", entryPath, err)
+			}
+		}
+
+		resources = append(resources, resource)
+	}
+
+	return &ypb.RequestYakURLResponse{
+		Resources: resources,
+		Total:     int64(len(resources)),
+		Page:      1,
+		PageSize:  int64(len(resources)),
+	}, nil
+}
+
+func javaclassparserIsArchiveLeaf(name string) bool {
+	lower := strings.ToLower(name)
+	for _, ext := range []string{".jar", ".war", ".ear", ".zip"} {
+		if strings.HasSuffix(lower, ext) {
+			return true
+		}
+	}
+	return false
+}
