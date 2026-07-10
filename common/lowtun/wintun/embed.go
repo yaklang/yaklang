1e4127a14673ef017b820b22c39b30dfce150402
YWHKKX <2076064543@qq.com>
2026-07-10 11:47:51 +0800

    feat(ci): add Windows ARM64 support across workflows

--- common/lowtun/wintun/embed.go
+++ common/lowtun/wintun/embed.go
@@ -4,14 +4,25 @@ package wintun
 
 import (
 	_ "embed"
+	"runtime"
 
 	"github.com/yaklang/yaklang/common/utils"
 )
 
-//go:embed wintun.dll.gz
-var wintunDLL []byte
+//go:embed wintun_amd64.dll.gz
+var wintunDLLAMD64 []byte
+
+//go:embed wintun_arm64.dll.gz
+var wintunDLLARM64 []byte
 
 func GetWintunDLLData() []byte {
-	buf, _ := utils.GzipDeCompress(wintunDLL)
+	var compressed []byte
+	switch runtime.GOARCH {
+	case "arm64":
+		compressed = wintunDLLARM64
+	default:
+		compressed = wintunDLLAMD64
+	}
+	buf, _ := utils.GzipDeCompress(compressed)
 	return buf
 }
