69467edb405de41dda95d663a930f2a769c94019
Matrix-Cain <zwh_china@outlook.com>
2026-07-24 22:01:21 +0800

    rebase main

--- common/yakgrpc/aivizhttp/server.go
+++ common/yakgrpc/aivizhttp/server.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
+	"github.com/yaklang/gorm"
 	"github.com/yaklang/yaklang/common/consts"
 	"github.com/yaklang/yaklang/common/log"
 	"github.com/yaklang/yaklang/common/schema"
