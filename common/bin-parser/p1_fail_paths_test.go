137dcaf24f636a332bdac79e0c8b0b0428660bc9
v1ll4n <v1ll4n@qq.com>
2026-09-05 05:22:05 +0800

    fix(bin-parser): parse ICMPv6 MLD and Redirect
    RFC 2710 types 130-132 Multicast Listener Query/Report/Done (MRC and
    group address) and RFC 4861 type 137 Redirect (Target/Destination)
    were still the ICMPV6 Payload default arm. Wire those PDUs, cover
    L1/L2 named fields, and add trunc-mld.

--- common/bin-parser/p1_fail_paths_test.go
+++ common/bin-parser/p1_fail_paths_test.go
@@ -21,6 +21,7 @@ var p1FailCases = []p1FailCase{
 		{"internet_control_message_protocol_v6", []string{"ICMPV6"}, []byte{0x80, 0x00}, "trunc-hdr"},
 		{"internet_control_message_protocol_v6", []string{"ICMPV6"}, []byte{0x02, 0x00, 0x00, 0x00, 0x00, 0x05}, "trunc-ptb"},
 		{"internet_control_message_protocol_v6", []string{"ICMPV6"}, []byte{0x04, 0x01, 0x00, 0x00, 0x00, 0x06}, "trunc-param"},
+		{"internet_control_message_protocol_v6", []string{"ICMPV6"}, []byte{0x82, 0x00, 0x00, 0x00, 0x27, 0x10, 0x00, 0x00}, "trunc-mld"},
 		{"vxlan", []string{"VXLAN"}, nil, "empty"},
 		{"vxlan", []string{"VXLAN"}, []byte{0x00}, "trunc"},
 		{"vxlan", []string{"VXLAN"}, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, "no-i-flag"},
