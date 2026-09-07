d950a71b0586a93c452b5c005e36ff9907f83b7e
intSheep <intSheep@qq.com>
2026-09-07 16:15:30 +0800

    feat(scannode): apply managed resource policy through heartbeat

--- scannode/node.go
+++ scannode/node.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/yaklang/yaklang/common/log"
@@ -16,6 +17,7 @@ import (
 )
 
 type ScanNode struct {
+	resourcePolicyMu  sync.Mutex
 	node              *node.NodeBase
 	manager           *TaskManager
 	capabilityManager *CapabilityManager
@@ -90,6 +92,9 @@ func NewScanNode(cfg node.BaseConfig, options ...ScanNodeOption) (*ScanNode, err
 	}
 	if cfg.NodeType == "" {
 		cfg.NodeType = spec.NodeType_Scanner
+	}
+	if strings.TrimSpace(cfg.Kind) != "ai_session" {
+		cfg.CapabilityKeys = append(cfg.CapabilityKeys, "node.resource_policy.v1")
 	}
 	cfg.CapabilityKeys = normalizeScanNodeCapabilityKeys(cfg.CapabilityKeys)
 	if cfg.StatusProvider == nil {
@@ -254,6 +259,8 @@ func (s *ScanNode) releaseSSAGitScopeLockAfterTasks() {
 }
 
 func (s *ScanNode) Snapshot() node.RuntimeStatus {
+	s.resourcePolicyMu.Lock()
+	defer s.resourcePolicyMu.Unlock()
 	status := node.RuntimeStatus{
 		LifecycleState: node.DefaultLifecycleState,
 		RunningJobs:    s.invokeLimiter.activeCount(),
