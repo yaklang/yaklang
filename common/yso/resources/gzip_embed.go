90ef7ce0fb004cfb3250fd7d6b5d572c5c11b298
rookie <rookie_is@qq.com>
2026-09-08 14:44:41 +0800

    add trimpath for crossbuild

--- common/yso/resources/gzip_embed.go
+++ common/yso/resources/gzip_embed.go
@@ -10,6 +10,10 @@ import (
 	"github.com/yaklang/yaklang/common/utils/gzip_embed"
 )
 
+// ysoXorKey 用于 XOR 编码嵌入的 static.tar.gz，避免杀软直接解压
+// 扫描到 Java class 和序列化 payload（RuntimeExec、TcpReverseShell 等）。
+const ysoXorKey = "yaklang-yso-v1"
+
 //go:embed static.tar.gz
 var resourceFS embed.FS
 
@@ -17,7 +21,7 @@ var YsoResourceFS fi.FileSystem
 
 func InitEmbedFS() {
 	var err error
-	YsoResourceFS, err = gzip_embed.NewPreprocessingEmbed(&resourceFS, "static.tar.gz", true)
+	YsoResourceFS, err = gzip_embed.NewPreprocessingEmbedWithXORKey(&resourceFS, "static.tar.gz", true, []byte(ysoXorKey))
 	if err != nil {
 		log.Errorf("init embed failed: %v", err)
 		YsoResourceFS = gzip_embed.NewEmptyPreprocessingEmbed()
