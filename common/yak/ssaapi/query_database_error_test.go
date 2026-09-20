69aba8c248c654f4f34ad1ffd0591efbbf58a339
wlz <lingze.wl@gmail.com>
2026-09-20 14:52:00 +0800

    fix(syntaxflow): keep nested queries within the owning rule lifetime

--- common/yak/ssaapi/query_database_error_test.go
+++ common/yak/ssaapi/query_database_error_test.go
@@ -42,3 +42,48 @@ func TestSyntaxFlowQueryRestoresSharedVMContext(t *testing.T) {
 		require.NoError(t, parent.Err())
 	}
 }
+
+func TestSyntaxFlowNestedDataflowContext(t *testing.T) {
+	prog, err := Parse(`a = source(); b = a + "suffix"; if cond { b = b + a }; sink(b); sink(a)`)
+	require.NoError(t, err)
+	result, err := prog.SyntaxFlowWithError(`
+ source() as $extraValue
+ sink(* as $param)
+ $param?{<self> #{include: <<<CODE
+* & $extraValue
+CODE}->} as $sink
+ $sink<dataflow(include=<<<CODE
+* & $extraValue as $__next__
+CODE,exclude=<<<CODE
+*?{opcode: call} as $__next__
+CODE)> as $high
+ $sink<dataflow(include=<<<CODE
+* & $extraValue as $__next__
+CODE,exclude=<<<CODE
+<self>?{opcode: call && <self><getCallee> & $function} as $__next__
+CODE)> as $highAndMid
+ alert $high
+ `)
+	require.NoError(t, err)
+	require.NotNil(t, result)
+}
+
+func TestSyntaxFlowNestedQuerySharesRuleLifetime(t *testing.T) {
+	prog, err := Parse("a = 1")
+	require.NoError(t, err)
+	var childContext context.Context
+	sfvm.RegisterNativeCall("testCaptureRuleContext", func(v sfvm.Values, f *sfvm.SFFrame, _ *sfvm.NativeCallActualParams) (bool, sfvm.Values, error) {
+		childContext = f.GetContext()
+		return true, v, nil
+	})
+	sfvm.RegisterNativeCall("testNestedRuleContext", func(v sfvm.Values, f *sfvm.SFFrame, _ *sfvm.NativeCallActualParams) (bool, sfvm.Values, error) {
+		_, err := prog.SyntaxFlowWithError("a<testCaptureRuleContext>", QueryWithSFConfig(f.GetConfig()))
+		require.NoError(t, err)
+		require.NotNil(t, childContext)
+		require.NoError(t, childContext.Err(), "nested result contexts must remain usable until the owning rule ends")
+		return true, v, nil
+	})
+	_, err = prog.SyntaxFlowWithError("a<testNestedRuleContext>")
+	require.NoError(t, err)
+	require.ErrorIs(t, childContext.Err(), context.Canceled, "rule exit releases the whole query lifetime")
+}
